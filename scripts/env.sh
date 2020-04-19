f3f55108b9bf60021e641816ec1f6bb4fdf8edd7
Determinant <tederminant@gmail.com>
2020-04-19 18:08:08 -0400

    use salticidae-go v0.1.0

--- scripts/env.sh
+++ scripts/env.sh
@@ -9,5 +9,5 @@ SALTICIDAE_GO_HOME="$GOPATH/src/github.com/ava-labs/salticidae-go/"
 if [[ -f "$SALTICIDAE_GO_HOME/salticidae/libsalticidae.a" ]]; then
     source "$SALTICIDAE_GO_HOME/scripts/env.sh"
 else
-    source /dev/stdin <<<"$(curl -sS https://raw.githubusercontent.com/ava-labs/salticidae-go/master/setup.sh)"
+    source /dev/stdin <<<"$(curl -sS https://raw.githubusercontent.com/ava-labs/salticidae-go/v0.1.0/setup.sh)"
 fi
