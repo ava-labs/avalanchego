ff8a9f1e063d421e515e718e3cc751eb3bfc7a53
Dan Laine <daniel.laine@avalabs.org>
2020-05-05 13:40:14 -0400

    update genStaker to allow keys to be put in places besides gopath

--- keys/genStaker.sh
+++ keys/genStaker.sh
@@ -1,13 +1,19 @@
-#!/bin/sh
-set -ex
+#!/bin/sh 
+set -e
 
-keypath=$GOPATH/src/github.com/ava-labs/gecko/keys
+src_dir=$(cd $(dirname $0) && pwd)
 
-if test -f "$keypath/staker.key" || test -f "$keypath/staker.crt"; then
-    echo "staker.key or staker.crt already exists. Not generating new key/certificiate."
+if test -f "${src_dir}/staker.key" || test -f "${src_dir}/staker.crt"; then
+    echo "${src_dir}/staker.key or ${src_dir}/staker.crt already exists. Not generating new key/certificiate."
     exit 1
 fi
 
 openssl genrsa -out `dirname "$0"`/staker.key 4096
 openssl req -new -sha256 -key `dirname "$0"`/staker.key -subj "/C=US/ST=NY/O=Avalabs/CN=ava" -out `dirname "$0"`/staker.csr
 openssl x509 -req -in `dirname "$0"`/staker.csr -CA `dirname "$0"`/rootCA.crt -CAkey `dirname "$0"`/rootCA.key -CAcreateserial -out `dirname "$0"`/staker.crt -days 365250 -sha256
+
+if test -f "${src_dir}/staker.key" && test -f "${src_dir}/staker.crt"; then
+    echo "Staking key/certificate generated successfully"
+else
+    echo "Error: staking key/certificate not generated"
+fi
