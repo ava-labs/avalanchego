4712478c8f7cac220a695a96c226898d4088aa61
Dan Laine <daniel.laine@avalabs.org>
2020-05-13 13:17:47 -0400

    gecko's env.sh sources from salticidae-go's env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,5 +5,6 @@ export CORETH_PATH=$GOPATH/pkg/mod/github.com/ava-labs/coreth@$CORETH_VER
 
 export SALTICIDAE_GO_VER="v0.1.1" # Must match salticidae version in go.mod
 export SALTICIDAE_GO_PATH=$GOPATH/pkg/mod/github.com/ava-labs/salticidae-go@$SALTICIDAE_GO_VER
+source $SALTICIDAE_GO_PATH/scripts/env.sh
 
 export BUILD_DIR="${GECKO_PATH}/build" # Where binaries go
