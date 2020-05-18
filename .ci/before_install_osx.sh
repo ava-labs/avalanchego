1bcf6ad8d37fce52e24040748c27ffe356620745
Fabio Barone <holistic.computing@gmail.com>
2020-05-18 11:32:37 -0500

    try with the existing salticidae version

--- .ci/before_install_osx.sh
+++ .ci/before_install_osx.sh
@@ -4,6 +4,6 @@ set -ev
 
 go get -d -v github.com/ava-labs/gecko/...
 
-brew update
-sudo chown -R $(whoami) $(brew --prefix)/*
-brew install Determinant/salticidae/salticidae@31
+#brew update
+#sudo chown -R $(whoami) $(brew --prefix)/*
+#brew install Determinant/salticidae/salticidae@31
