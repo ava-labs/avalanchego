b3edb8637a043eb7145a50483bb2e85a3fe7e09f
danlaine <daniel.laine@avalabs.org>
2020-06-03 14:18:28 -0400

    lower log level (#43)
    * lower log level
    
    * lower log for 'asserions are enabled'

--- main/main.go
+++ main/main.go
@@ -41,7 +41,7 @@ func main() {
 	defer Config.DB.Close()
 
 	if Config.StakingIP.IsZero() {
-		log.Warn("NAT traversal has failed. If this node becomes a staker, it may lose its reward due to being unreachable.")
+		log.Warn("NAT traversal has failed. It will be able to connect to less nodes.")
 	}
 
 	// Track if sybil control is enforced
@@ -62,7 +62,7 @@ func main() {
 
 	// Track if assertions should be executed
 	if Config.LoggingConfig.Assertions {
-		log.Warn("assertions are enabled. This may slow down execution")
+		log.Debug("assertions are enabled. This may slow down execution")
 	}
 
 	mapper := nat.NewDefaultMapper(log, Config.Nat, nat.TCP, "gecko")
