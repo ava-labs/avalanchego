a523fb184aba68b5e2c6991a6db4e8bf17e0bda6
galenmarchetti <galenmarchetti@gmail.com>
2020-06-20 10:24:02 -0300

    cleaning CI run script

--- .ci/run_e2e_tests.sh
+++ .ci/run_e2e_tests.sh
@@ -4,7 +4,7 @@ SRC_PATH=$(dirname "${SCRIPTS_PATH}")
 bash "${SRC_PATH}"/scripts/build_image.sh
 GECKO_IMAGE=$(docker image ls --format="{{.Repository}}" | head -n 1)
 
+# Turn off GO111MODULE to pull e2e test source code in order to get run script.
 GO111MODULE=off go get -t -v github.com/kurtosis-tech/ava-e2e-tests/...
-
 cd "${GOPATH}"/src/github.com/kurtosis-tech/ava-e2e-tests/ || exit
 ./scripts/full_rebuild_and_run.sh
