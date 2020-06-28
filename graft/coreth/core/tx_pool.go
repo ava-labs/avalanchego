f09a360e8f384c34111e0f414f749c22e104422a
Determinant <tederminant@gmail.com>
2020-06-28 16:55:59 -0400

    ...

--- graft/coreth/core/tx_pool.go
+++ graft/coreth/core/tx_pool.go
@@ -528,6 +528,7 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 	}
 	// Ensure the transaction doesn't exceed the current block limit gas.
 	if pool.currentMaxGas < tx.Gas() {
+		fmt.Println(pool.currentMaxGas, tx.Gas())
 		return ErrGasLimit
 	}
 	// Make sure the transaction is signed properly
