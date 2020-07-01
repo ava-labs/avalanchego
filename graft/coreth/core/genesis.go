260ad2a6125aa8aab301162acc5885335f6a44c8
Determinant <tederminant@gmail.com>
2020-07-01 16:34:08 -0400

    embed mc lib into genesis

--- graft/coreth/core/genesis.go
+++ graft/coreth/core/genesis.go
@@ -266,6 +266,7 @@ func (g *Genesis) ToBlock(db ethdb.Database) *types.Block {
 		}
 		if account.MCBalance != nil {
 			statedb.ForceEnableMultiCoin(addr)
+			fmt.Println("enable", addr.Hex())
 			for coinID, value := range account.MCBalance {
 				statedb.AddBalanceMultiCoin(addr, coinID, value)
 			}
