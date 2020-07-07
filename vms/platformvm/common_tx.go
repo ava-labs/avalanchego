a5460461ab5e56252e2dc27c03c88d3c0d6b1099
Dan Laine <daniel.laine@avalabs.org>
2020-07-07 18:44:50 -0400

    update commonTx format to match avm.baseTx

--- vms/platformvm/common_tx.go
+++ vms/platformvm/common_tx.go
@@ -7,6 +7,9 @@ import (
 
 // CommonTx contains fields common to many transaction types
 // Should be embedded in transaction implementations
+// The serialized fields of this struct should be exactly the same as those of avm.BaseTx
+// Do not change this struct's serialized fields without doing the same on avm.BaseTx
+// TODO: Factor out this and avm.BaseTX
 type CommonTx struct {
 	vm *VM
 	// true iff this transaction has already passed syntactic verification
@@ -19,10 +22,12 @@ type CommonTx struct {
 	bytes []byte
 	// ID of the network on which this tx was issued
 	NetworkID uint32 `serialize:"true"`
+	// ID of this blockchain. In practice is always the empty ID.
+	// This is only here to match avm.BaseTx's format
+	BlockchainID ids.ID                    `serialize:"true"`
+	Outputs      []*ava.TransferableOutput `serialize:"true"`
 	// Input UTXOs
 	Inputs []*ava.TransferableInput `serialize:"true"`
-	// Output UTXOs
-	Outputs []*ava.TransferableOutput `serialize:"true"`
 }
 
 // UnsignedBytes returns the byte representation of this unsigned tx
