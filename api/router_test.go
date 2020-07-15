d9523b2a7d8b0622b6f8240ba13be983692ce966
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-07-15 12:34:53 -0400

    Revert changes for restarting chain in API server

--- api/router_test.go
+++ api/router_test.go
@@ -55,6 +55,12 @@ func TestAliasing(t *testing.T) {
 	} else if handler != handler1 {
 		t.Fatalf("Registered unknown handler")
 	}
+
+	if handler, err := r.GetHandler("7", ""); err != nil {
+		t.Fatalf("Should have added %s", "7")
+	} else if handler != handler1 {
+		t.Fatalf("Registered unknown handler")
+	}
 }
 
 func TestBlock(t *testing.T) {
@@ -67,45 +73,5 @@ func TestBlock(t *testing.T) {
 	handler1 := &testHandler{}
 	if err := r.AddRouter("1", "", handler1); err == nil {
 		t.Fatalf("Permanently locked %s", "1")
-	}
-}
-
-func TestReplaceRouter(t *testing.T) {
-	originalHandler := &testHandler{}
-	replaceHandler := &testHandler{}
-	r := newRouter()
-
-	if err := r.AddRouter("base", "endpoint", originalHandler); err != nil {
-		t.Fatalf("Failed to add route")
-	}
-
-	if err := r.AddAlias("base", "alias-base1"); err != nil {
-		t.Fatalf("Failed to add aliases for base")
-	}
-
-	if err := r.AddAlias("alias-base1", "secondary-alias"); err != nil {
-		t.Fatalf("Failed to add secondary alias")
-	}
-
-	if err := r.ReplaceRouter("base", "endpoint", replaceHandler); err != nil {
-		t.Fatalf("Failed to replace handler")
-	}
-
-	if handler, err := r.GetHandler("base", "endpoint"); err != nil {
-		t.Fatalf("Couldn't get replaced handler")
-	} else if handler != replaceHandler {
-		t.Fatalf("Handler for base was not replaced by target")
-	}
-
-	if handler, err := r.GetHandler("alias-base1", "endpoint"); err != nil {
-		t.Fatalf("Couldn't get replaced handler")
-	} else if handler != replaceHandler {
-		t.Fatalf("Handler for alias was not replaced by target")
-	}
-
-	if handler, err := r.GetHandler("secondary-alias", "endpoint"); err != nil {
-		t.Fatalf("Couldn't get replaced handler")
-	} else if handler != replaceHandler {
-		t.Fatalf("Handler for secondary alias was not replaced by target")
 	}
 }
--- api/server.go
+++ api/server.go
@@ -100,38 +100,6 @@ func (s *Server) RegisterChain(ctx *snow.Context, vmIntf interface{}) {
 	}
 }
 
-func (s *Server) RestartChain(ctx *snow.Context, vmIntf interface{}) {
-	vm, ok := vmIntf.(common.VM)
-	if !ok {
-		return
-	}
-
-	// all subroutes to a chain begin with "bc/<the chain's ID>"
-	chainID := ctx.ChainID.String()
-	defaultEndpoint := "bc/" + chainID
-	httpLogger, err := s.factory.MakeChain(chainID, "http")
-	if err != nil {
-		s.log.Error("Failed to create new http logger: %s", err)
-		return
-	}
-	s.log.Verbo("About to replace API endpoints for chain with ID %s", ctx.ChainID)
-
-	// Register each endpoint
-	for extension, service := range vm.CreateHandlers() {
-		// Validate that the route being added is valid
-		// e.g. "/foo" and "" are ok but "\n" is not
-		_, err := url.ParseRequestURI(extension)
-		if extension != "" && err != nil {
-			s.log.Warn("could not add route to chain's API handler because route is malformed: %s", extension)
-			continue
-		}
-		s.log.Info("replacing API endpoint: %s", defaultEndpoint+extension)
-		if err := s.ReplaceRoute(service, &ctx.Lock, defaultEndpoint, extension, httpLogger); err != nil {
-			s.log.Error("error replacing route: %s", err)
-		}
-	}
-}
-
 // AddRoute registers the appropriate endpoint for the vm given an endpoint
 func (s *Server) AddRoute(handler *common.HTTPHandler, lock *sync.RWMutex, base, endpoint string, log logging.Logger) error {
 	url := fmt.Sprintf("%s/%s", baseURL, base)
@@ -142,17 +110,6 @@ func (s *Server) AddRoute(handler *common.HTTPHandler, lock *sync.RWMutex, base,
 	}
 
 	return s.router.AddRouter(url, endpoint, h)
-}
-
-func (s *Server) ReplaceRoute(handler *common.HTTPHandler, lock *sync.RWMutex, base, endpoint string, log logging.Logger) error {
-	url := fmt.Sprintf("%s/%s", baseURL, base)
-	s.log.Info("replacing route %s%s", url, endpoint)
-	h, err := s.createMiddlewareHandler(handler, lock, log)
-	if err != nil {
-		return err
-	}
-
-	return s.router.ReplaceRouter(url, endpoint, h)
 }
 
 func (s *Server) createMiddlewareHandler(handler *common.HTTPHandler, lock *sync.RWMutex, log logging.Logger) (http.Handler, error) {
