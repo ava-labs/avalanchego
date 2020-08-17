c63ceeb1158db155260d7c788f615cde33bef705
Fabio Barone <holistic.computing@gmail.com>
2020-08-17 15:14:52 -0500

    introduce cross-platform automatic builds

---
+++ .github/workflows/build-deb-pkg.sh
@@ -0,0 +1,32 @@
+PKG_ROOT=/tmp/avalanche
+DEBIAN_BASE_DIR=$PKG_ROOT/debian
+AVALANCHE_BUILD_BIN_DIR=$DEBIAN_BASE_DIR/usr/local/bin
+AVALANCHE_LIB_DIR=$DEBIAN_BASE_DIR/usr/local/lib/avalanche
+TEMPLATE=.github/workflows/debian/template 
+DEBIAN_CONF=$DEBIAN_BASE_DIR/DEBIAN
+
+mkdir -p $PKG_ROOT/$DEBIAN_BASE_DIR
+mkdir -p $DEBIAN_CONF
+mkdir -p $AVALANCHE_BUILD_BIN_DIR
+mkdir -p $AVALANCHE_LIB_DIR
+
+OK=`cp ./build/avalanche $AVALANCHE_BUILD_BIN_DIR`
+if [[ $OK -ne 0 ]]; then
+  exit $OK;
+fi
+OK=`cp ./build/plugins/evm $AVALANCHE_LIB_DIR`
+if [[ $OK -ne 0 ]]; then
+  exit $OK;
+fi
+OK=`cp $TEMPLATE/control $DEBIAN_CONF`
+if [[ $OK -ne 0 ]]; then
+  exit $OK;
+fi
+
+echo "Build debian package..."
+cd $PKG_ROOT
+echo "Tag: $TAG"
+NEW_VERSION_STRING="Version: $TAG"
+sed -i "s/Version.*/$NEW_VERSION_STRING/g" debian/DEBIAN/control
+dpkg-deb --build debian avalanche-linux_$TAG.deb
+aws s3 cp avalanche-linux_$TAG.deb s3://avalanche-public-builds/linux/
