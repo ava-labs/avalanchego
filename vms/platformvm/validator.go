9fc3cac79fc30f251398d15279f14d7fd3b868d8
Dan Laine <daniel.laine@avalabs.org>
2020-08-20 11:58:52 -0400

    remove Vdr()

--- vms/platformvm/validator.go
+++ vms/platformvm/validator.go
@@ -7,37 +7,33 @@ import (
 	"time"
 
 	"github.com/ava-labs/gecko/ids"
-	"github.com/ava-labs/gecko/snow/validators"
 )
 
-// Validator ...
+// Validator is a validator.
 type Validator struct {
-	// Node ID of the staker
+	// Node ID of the validator
 	NodeID ids.ShortID `serialize:"true" json:"nodeID"`
 
-	// Unix time this staker starts validating
+	// Unix time this validator starts validating
 	Start uint64 `serialize:"true" json:"start"`
 
-	// Unix time this staker stops validating
+	// Unix time this validator stops validating
 	End uint64 `serialize:"true" json:"end"`
 
 	// Weight of this validator used when sampling
 	Wght uint64 `serialize:"true" json:"weight"`
 }
 
-// ID returns the node ID of the staker
+// ID returns the node ID of the validator
 func (v *Validator) ID() ids.ShortID { return v.NodeID }
 
-// Vdr returns this validator
-func (v *Validator) Vdr() validators.Validator { return v }
-
-// StartTime is the time that this staker will enter the validator set
+// StartTime is the time that this validator will enter the validator set
 func (v *Validator) StartTime() time.Time { return time.Unix(int64(v.Start), 0) }
 
-// EndTime is the time that this staker will leave the validator set
+// EndTime is the time that this validator will leave the validator set
 func (v *Validator) EndTime() time.Time { return time.Unix(int64(v.End), 0) }
 
-// Duration is the amount of time that this staker will be in the validator set
+// Duration is the amount of time that this validator will be in the validator set
 func (v *Validator) Duration() time.Duration { return v.EndTime().Sub(v.StartTime()) }
 
 // Weight is this validator's weight when sampling
@@ -67,7 +63,7 @@ func (v *Validator) BoundedBy(startTime, endTime time.Time) bool {
 	return !v.StartTime().Before(startTime) && !v.EndTime().After(endTime)
 }
 
-// SubnetValidator validates a blockchain on the Avalanche network.
+// SubnetValidator validates a subnet on the Avalanche network.
 type SubnetValidator struct {
 	Validator `serialize:"true"`
 
