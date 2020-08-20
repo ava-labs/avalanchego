40d95bb709f52cbf90001f34bfc5404366f136c9
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-08-20 17:21:27 -0400

    Update tag and docker images for run e2e tests ci script

--- .ci/run_e2e_tests.sh
+++ .ci/run_e2e_tests.sh
@@ -8,15 +8,15 @@ DOCKER_REPO="avaplatform"
 
 echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-TESTING_CONTROLLER_IMAGE="$DOCKER_REPO/avalanche-e2e-tests_controller-e3c1df0-5209dcbd:latest"
-BYZANTINE_IMAGE="$DOCKER_REPO/gecko-byzantine:c2504a6-df19a710-13ebd8b"
+TESTING_CONTROLLER_IMAGE="$DOCKER_REPO/avalanche-e2e-tests_controller:everest-latest"
+BYZANTINE_IMAGE="$DOCKER_REPO/gecko-byzantine:everest-latest"
 
 docker pull "$TESTING_CONTROLLER_IMAGE"
 docker pull "${BYZANTINE_IMAGE}"
 
 
 E2E_TESTING_REMOTE="https://github.com/ava-labs/avalanche-testing.git"
-E2E_TAG="v0.7.3-dev"
+E2E_TAG="v0.8.2-dev"
 
 mkdir -p "$E2E_TEST_HOME"
 git clone "$E2E_TESTING_REMOTE" "$E2E_TEST_HOME"
@@ -26,5 +26,4 @@ git checkout "tags/$E2E_TAG" -b "$E2E_TAG"
 
 go mod edit -replace github.com/ava-labs/gecko="$GECKO_HOME"
 bash "./scripts/rebuild_initializer_binary.sh"
-CONTROLLER_IMAGE="kurtosistech/ava-e2e-tests_controller:latest"
 ./build/avalanche-e2e-tests --gecko-image-name="${GECKO_IMAGE}" --test-controller-image-name="${TESTING_CONTROLLER_IMAGE}" --byzantine-image-name="${BYZANTINE_IMAGE}"
