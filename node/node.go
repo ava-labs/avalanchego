f0facbaa2fdd7aea7ff7e9951a236cf6289d01e1
StephenButtolph <stephen@avalabs.org>
2020-08-24 16:53:54 -0400

    v0.6.4 version bump

--- node/node.go
+++ node/node.go
@@ -63,7 +63,7 @@ var (
 	genesisHashKey = []byte("genesisID")
 
 	// Version is the version of this code
-	Version       = version.NewDefaultVersion("avalanche", 0, 6, 3)
+	Version       = version.NewDefaultVersion("avalanche", 0, 6, 4)
 	versionParser = version.NewDefaultParser()
 )
 
