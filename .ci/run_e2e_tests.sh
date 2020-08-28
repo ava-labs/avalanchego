496c05023b82b787d74bb86595d1d1b8a4668dde
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-08-28 00:26:53 -0400

    Update CI script to match breaking API name changes

--- .ci/run_e2e_tests.sh
+++ .ci/run_e2e_tests.sh
@@ -8,15 +8,15 @@ DOCKER_REPO="avaplatform"
 
 echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-TESTING_CONTROLLER_IMAGE="$DOCKER_REPO/avalanche-e2e-tests_controller:everest-latest"
-BYZANTINE_IMAGE="$DOCKER_REPO/gecko-byzantine:everest-latest"
+TESTING_CONTROLLER_IMAGE="$DOCKER_REPO/avalanche-testing_controller:everest-dev"
+BYZANTINE_IMAGE="$DOCKER_REPO/gecko-byzantine:everest-dev"
 
 docker pull "$TESTING_CONTROLLER_IMAGE"
 docker pull "${BYZANTINE_IMAGE}"
 
 
 E2E_TESTING_REMOTE="https://github.com/ava-labs/avalanche-testing.git"
-E2E_TAG="v0.8.2-dev"
+E2E_TAG="v0.8.4-dev"
 
 mkdir -p "$E2E_TEST_HOME"
 git clone "$E2E_TESTING_REMOTE" "$E2E_TEST_HOME"
@@ -26,4 +26,4 @@ git checkout "tags/$E2E_TAG" -b "$E2E_TAG"
 
 go mod edit -replace github.com/ava-labs/gecko="$GECKO_HOME"
 bash "./scripts/rebuild_initializer_binary.sh"
-./build/avalanche-e2e-tests --gecko-image-name="${GECKO_IMAGE}" --test-controller-image-name="${TESTING_CONTROLLER_IMAGE}" --byzantine-image-name="${BYZANTINE_IMAGE}"
+./build/avalanche-testing --gecko-image-name="${GECKO_IMAGE}" --test-controller-image-name="${TESTING_CONTROLLER_IMAGE}" --byzantine-image-name="${BYZANTINE_IMAGE}"
