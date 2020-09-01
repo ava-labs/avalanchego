48c776bf61fbdd217e59ecd12e0eddd1d6d238f7
Fabio Barone <holistic.computing@gmail.com>
2020-09-01 14:35:51 -0500

    don't push to docker hub on a PR from fork

--- .ci/after_success.sh
+++ .ci/after_success.sh
@@ -8,7 +8,8 @@ TRAVIS_TAG="$DOCKERHUB_REPO:travis-$TRAVIS_BUILD_NUMBER"
 docker tag $DOCKERHUB_REPO:$COMMIT "$TRAVIS_TAG"
 
 # don't push to dockerhub if this is not being run on the main public repo
-if [[ $TRAVIS_REPO_SLUG != "ava-labs/gecko" ]]; then
+# or if it's a PR from a fork ( => secret vars not set )
+if [[ $TRAVIS_REPO_SLUG != "ava-labs/gecko" || -z "$DOCKER_USERNAME"  ]]; then
   exit 0;
 fi
 
