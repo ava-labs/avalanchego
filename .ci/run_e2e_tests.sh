4dc6e4324d0ad8746338eafdf60e981776ef1120
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-09-09 20:10:01 -0400

    Update tags for CI

--- .ci/run_e2e_tests.sh
+++ .ci/run_e2e_tests.sh
@@ -7,7 +7,7 @@ AVALANCHE_IMAGE=$(docker image ls --format="{{.Repository}}" | head -n 1)
 DOCKER_REPO="avaplatform"
 
 E2E_TESTING_REMOTE="https://github.com/ava-labs/avalanche-testing.git"
-E2E_TAG="v0.9.1-dev"
+E2E_TAG="v0.9.2-dev"
 
 mkdir -p "$E2E_TEST_HOME"
 git clone "$E2E_TESTING_REMOTE" "$E2E_TEST_HOME"
@@ -19,17 +19,17 @@ go mod edit -replace github.com/ava-labs/avalanche-go="$AVALANCHE_HOME"
 bash "./scripts/rebuild_initializer_binary.sh"
 
 
-TESTING_CONTROLLER_IMAGE="$DOCKER_REPO/avalanche-testing_controller:everest-0.9.1-dev"
-BYZANTINE_IMAGE="$DOCKER_REPO/gecko-byzantine:everest-api-update"
+TESTING_CONTROLLER_IMAGE="$DOCKER_REPO/avalanche-testing_controller:everest-v0.9.2-dev"
+BYZANTINE_IMAGE="$DOCKER_REPO/gecko-byzantine:everest-name-change"
 
 docker pull "$TESTING_CONTROLLER_IMAGE"
 
 # If Docker Credentials are not available skip the Byzantine Tests
 if [[ ${#DOCKER_USERNAME} == 0 ]]; then
     echo "Skipping Byzantine Tests because Docker Credentials were not present."
-    ./build/avalanche-testing --gecko-image-name="${AVALANCHE_IMAGE}" --test-controller-image-name="${TESTING_CONTROLLER_IMAGE}"
+    ./build/avalanche-testing --avalanche-image-name="${AVALANCHE_IMAGE}" --test-controller-image-name="${TESTING_CONTROLLER_IMAGE}"
 else
     echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
     docker pull "${BYZANTINE_IMAGE}"
-    ./build/avalanche-testing --gecko-image-name="${AVALANCHE_IMAGE}" --test-controller-image-name="${TESTING_CONTROLLER_IMAGE}" --byzantine-image-name="${BYZANTINE_IMAGE}"
+    ./build/avalanche-testing --avalanche-image-name="${AVALANCHE_IMAGE}" --test-controller-image-name="${TESTING_CONTROLLER_IMAGE}" --byzantine-image-name="${BYZANTINE_IMAGE}"
 fi
