2f15d9f73d71d5377895aad72b919900695da3c2
StephenButtolph <stephen@avalabs.org>
2020-09-13 10:59:46 -0400

    wip

--- genesis/main/main.go
+++ genesis/main/main.go
@@ -16,10 +16,14 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/ava-labs/avalanche-go/utils/constants"
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/constants"
 	"github.com/ava-labs/avalanchego/utils/formatting"
 	"github.com/ava-labs/avalanchego/utils/units"
+	"github.com/ava-labs/coreth/core"
+	"github.com/ava-labs/coreth/params"
+	"github.com/ava-labs/coreth/plugin/evm"
+	"github.com/ava-labs/go-ethereum/common"
 
 	safemath "github.com/ava-labs/avalanche-go/utils/math"
 )
@@ -127,6 +131,49 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(360*units.MegaAvax - initialSupply)
+}
+
+func makeEthGenesis() {
+	// Specify the genesis state of Athereum (the built-in instance of the EVM)
+	alloc := core.GenesisAlloc{}
+	alloc[common.HexToAddress("0100000000000000000000000000000000000000")] = core.GenesisAccount{
+		Code: common.Hex2Bytes("0x730000000000000000000000000000000000000000301460806040526004361061004b5760003560e01c80631e01043914610050578063abb24ba014610092578063b6510bb3146100a9575b600080fd5b61007c6004803603602081101561006657600080fd5b8101908080359060200190929190505050610118565b6040518082815260200191505060405180910390f35b81801561009e57600080fd5b506100a761013b565b005b8180156100b557600080fd5b50610116600480360360808110156100cc57600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff16906020019092919080359060200190929190803590602001909291908035906020019092919050505061013e565b005b60003073ffffffffffffffffffffffffffffffffffffffff1682905d9050919050565b5c565b8373ffffffffffffffffffffffffffffffffffffffff1681836108fc8690811502906040516000604051808303818888878c8af69550505050505015801561018a573d6000803e3d6000fd5b505050505056fea2646970667358221220ed2100d6623a884d196eceefabe5e03da4309a2562bb25262f3874f1acb31cd764736f6c634300060a0033"),
+	}
+	evmArgs := core.Genesis{
+		Config: &params.ChainConfig{
+			ChainID:             big.NewInt(43110),
+			HomesteadBlock:      big.NewInt(0),
+			DAOForkBlock:        big.NewInt(0),
+			DAOForkSupport:      true,
+			EIP150Block:         big.NewInt(0),
+			EIP150Hash:          common.HexToHash("0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0"),
+			EIP155Block:         big.NewInt(0),
+			EIP158Block:         big.NewInt(0),
+			ByzantiumBlock:      big.NewInt(0),
+			ConstantinopleBlock: big.NewInt(0),
+			PetersburgBlock:     big.NewInt(0),
+		},
+		Nonce:      0,
+		Timestamp:  0,
+		ExtraData:  []byte{0},
+		GasLimit:   100000000,
+		Difficulty: big.NewInt(0),
+		Mixhash:    common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
+		Coinbase:   common.HexToAddress("0x0000000000000000000000000000000000000000"),
+		Alloc:      alloc,
+		Number:     0,
+		GasUsed:    0,
+		ParentHash: common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
+	}
+	evmSS := evm.StaticService{}
+	evmReply, err := evmSS.BuildGenesis(nil, &evmArgs)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := ioutil.WriteFile("./eth_genesis.txt", []byte(hex.Dump(evmReply.Bytes)), 0644); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func parseLine(line string) (ids.ShortID, ids.ShortID, LockedAmount, []LockedAmount, error) {
