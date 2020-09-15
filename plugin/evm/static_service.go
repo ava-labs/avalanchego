d4326196d7c2713710f47f0b68811bf7e0f176cb
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-09-15 13:13:27 -0400

    Update to avalanchego v0.8.3

--- plugin/evm/static_service.go
+++ plugin/evm/static_service.go
@@ -8,7 +8,7 @@ import (
 	"encoding/json"
 
 	"github.com/ava-labs/coreth/core"
-	"github.com/ava-labs/avalanche-go/utils/formatting"
+	"github.com/ava-labs/avalanchego/utils/formatting"
 )
 
 // StaticService defines the static API services exposed by the evm
