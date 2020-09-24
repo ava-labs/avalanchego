7afa2a89586514639291ebe39017078c09b78ec5
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-09-24 16:35:55 -0400

    Bump avalanche-testing to v0.9.6

--- .ci/run_e2e_tests.sh
+++ .ci/run_e2e_tests.sh
@@ -7,7 +7,7 @@ AVALANCHE_IMAGE=$(docker image ls --format="{{.Repository}}" | head -n 1)
 DOCKER_REPO="avaplatform"
 
 E2E_TESTING_REMOTE="https://github.com/ava-labs/avalanche-testing.git"
-E2E_TAG="v0.9.5"
+E2E_TAG="v0.9.6"
 
 mkdir -p "$E2E_TEST_HOME"
 git clone "$E2E_TESTING_REMOTE" "$E2E_TEST_HOME"
@@ -18,7 +18,7 @@ git checkout "tags/$E2E_TAG" -b "$E2E_TAG"
 bash "./scripts/rebuild_initializer_binary.sh"
 
 
-TESTING_CONTROLLER_IMAGE="$DOCKER_REPO/avalanche-testing_controller:v0.9.5"
+TESTING_CONTROLLER_IMAGE="$DOCKER_REPO/avalanche-testing_controller:v0.9.6"
 BYZANTINE_IMAGE="$DOCKER_REPO/avalanche-byzantine:v0.1.1"
 
 docker pull "$TESTING_CONTROLLER_IMAGE"
