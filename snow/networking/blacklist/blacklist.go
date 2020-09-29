c6a863fc3156fe7dd9d432a277274c88c2504ec8
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-09-29 13:38:37 -0400

    Implemenet query blacklisting logic

---
+++ snow/networking/blacklist/blacklist.go
@@ -0,0 +1,234 @@
+package common
+
+import (
+	"container/list"
+	"sync"
+	"time"
+
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/snow/validators"
+	"github.com/ava-labs/avalanchego/utils/timer"
+
+	safemath "github.com/ava-labs/avalanchego/utils/math"
+)
+
+// Problem: register queries and the responses
+// to ensure that nodes are responding to messages
+
+// If a peer consistently does not respond to queries, it will
+// increase latencies on the network whenever that peer is polled.
+// If we cannot terminate the poll early, then the poll will wait
+// the full timeout before finalizing the poll and making progress.
+// This can increase network latencies to an undesirable level.
+
+// Therefore, a blacklist is used as a heurstic to immediately fail
+// queries to nodes that are consistently not responding.
+
+type queryBlacklist struct {
+	vdrs validators.Set
+	// Map of peerIDs to pending query requests
+	pendingQueries map[[20]byte]map[uint32]struct{}
+	// Map of consecutive query failures
+	consecutiveFailures map[[20]byte]int
+
+	// Maintain blacklist
+	blacklistTimes map[[20]byte]time.Time
+	blacklistOrder *list.List
+	blacklistSet   ids.ShortSet
+
+	threshold  int
+	duration   time.Duration
+	maxPortion float64
+
+	clock timer.Clock
+
+	lock sync.Mutex
+}
+
+// BlacklistConfig defines the configuration for a blacklist
+type BlacklistConfig struct {
+	Validators validators.Set
+	Threshold  int
+	Duration   time.Duration
+	MaxPortion float64
+}
+
+// NewQueryBlacklist ...
+func NewQueryBlacklist(config BlacklistConfig) Blacklist {
+	return &queryBlacklist{
+		pendingQueries:      make(map[[20]byte]map[uint32]struct{}),
+		consecutiveFailures: make(map[[20]byte]int),
+		blacklistTimes:      make(map[[20]byte]time.Time),
+		blacklistOrder:      list.New(),
+		blacklistSet:        ids.ShortSet{},
+		threshold:           config.Threshold,
+		duration:            config.Duration,
+		vdrs:                config.Validators,
+		maxPortion:          config.MaxPortion,
+	}
+}
+
+// Blacklist ...
+type Blacklist interface {
+	// RegisterQuery registers a sent query and returns whether the query is subject to blacklist
+	RegisterQuery(ids.ShortID, uint32) bool
+	// RegisterResponse registers the response to a query message
+	RegisterResponse(ids.ShortID, uint32)
+	// QueryFailed registers that a query did not receive a response within our synchrony bound
+	QueryFailed(ids.ShortID, uint32)
+}
+
+// RegisterQuery attempts to register a query from [validatorID] and returns true
+// if that request is not subject to the blacklist
+func (b *queryBlacklist) RegisterQuery(validatorID ids.ShortID, requestID uint32) bool {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	key := validatorID.Key()
+	if blacklisted := b.blacklisted(validatorID); blacklisted {
+		return false
+	}
+	validatorRequests, ok := b.pendingQueries[key]
+	if !ok {
+		validatorRequests = make(map[uint32]struct{})
+		b.pendingQueries[key] = validatorRequests
+	}
+	validatorRequests[requestID] = struct{}{}
+
+	return true
+}
+
+// RegisterResponse removes the query from pending
+func (b *queryBlacklist) RegisterResponse(validatorID ids.ShortID, requestID uint32) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	if ok := b.removeQuery(validatorID, requestID); !ok {
+		return
+	}
+
+	// Reset consecutive failures on success
+	delete(b.consecutiveFailures, validatorID.Key())
+}
+
+// QueryFailed notes a failure and blacklists [validatorID] if necessary
+func (b *queryBlacklist) QueryFailed(validatorID ids.ShortID, requestID uint32) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+
+	if ok := b.removeQuery(validatorID, requestID); !ok {
+		return
+	}
+
+	key := validatorID.Key()
+	// Add a failure and blacklist [validatorID] if it has
+	// passed the threshold
+	b.consecutiveFailures[key]++
+	if b.consecutiveFailures[key] >= b.threshold {
+		b.blacklist(validatorID)
+	}
+}
+
+func (b *queryBlacklist) blacklist(validatorID ids.ShortID) {
+	if b.blacklistSet.Contains(validatorID) {
+		return
+	}
+
+	key := validatorID.Key()
+
+	// Add to blacklist times
+	b.blacklistTimes[key] = b.clock.Time().Add(b.duration)
+	b.blacklistOrder.PushBack(validatorID)
+	b.blacklistSet.Add(validatorID)
+
+	// Note: there could be a memory leak if a large number of
+	// validators were added, sampled, and then were blacklisted
+	// and never sampled again. Due to the minimum staking amount
+	// and durations this is not a concern.
+	b.cleanup()
+}
+
+// blacklisted checks if [validatorID] is currently blacklisted
+// and calls cleanup if its blacklist period has elapsed
+func (b *queryBlacklist) blacklisted(validatorID ids.ShortID) bool {
+	key := validatorID.Key()
+
+	end, ok := b.blacklistTimes[key]
+	if !ok {
+		return false
+	}
+
+	if b.clock.Time().Before(end) {
+		return true
+	}
+
+	// If a blacklisted item has expired, cleanup the blacklist
+	b.cleanup()
+	return false
+}
+
+// cleanup ensures that we have not blacklisted too much stake
+// and removes anything from the blacklist whose time has expired
+func (b *queryBlacklist) cleanup() {
+	currentWeight, err := b.vdrs.SubsetWeight(b.blacklistSet)
+	if err != nil {
+		b.reset()
+		return
+	}
+
+	maxBlacklistWeight := uint64(float64(b.vdrs.Weight()) * b.maxPortion)
+
+	// Iterate over elements of the blacklist in order of expiration
+	for e := b.blacklistOrder.Front(); e != nil; e = e.Next() {
+		validatorID := e.Value.(ids.ShortID)
+		// Remove elements with the next expiration until
+		key := validatorID.Key()
+		end := b.blacklistTimes[key]
+		// Note: this creates an edge case where blacklisting a validator
+		// with a sufficient stake may clear the blacklist
+		if b.clock.Time().Before(end) && currentWeight < maxBlacklistWeight {
+			break
+		}
+
+		removeWeight, ok := b.vdrs.GetWeight(validatorID)
+		if ok {
+			newWeight, err := safemath.Sub64(currentWeight, removeWeight)
+			if err != nil {
+				b.reset()
+				return
+			}
+			currentWeight = newWeight
+		}
+
+		b.blacklistOrder.Remove(e)
+		delete(b.blacklistTimes, key)
+		b.blacklistSet.Remove(validatorID)
+	}
+}
+
+func (b *queryBlacklist) reset() {
+	b.pendingQueries = make(map[[20]byte]map[uint32]struct{})
+	b.consecutiveFailures = make(map[[20]byte]int)
+	b.blacklistTimes = make(map[[20]byte]time.Time)
+	b.blacklistOrder.Init()
+	b.blacklistSet.Clear()
+}
+
+// removeQuery returns true if the query was present
+func (b *queryBlacklist) removeQuery(validatorID ids.ShortID, requestID uint32) bool {
+	key := validatorID.Key()
+
+	validatorRequests, ok := b.pendingQueries[key]
+	if !ok {
+		return false
+	}
+
+	_, ok = validatorRequests[requestID]
+	if ok {
+		delete(validatorRequests, requestID)
+		if len(validatorRequests) == 0 {
+			delete(b.pendingQueries, key)
+		}
+	}
+	return ok
+}
---
+++ snow/networking/blacklist/blacklist_test.go
@@ -0,0 +1,186 @@
+package common
+
+import (
+	"testing"
+	"time"
+
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/snow/validators"
+)
+
+// Test that blacklist will stop registering queries after a threshold of failures
+func TestBlackList(t *testing.T) {
+	vdrs := validators.NewSet()
+	vdr0 := validators.GenerateRandomValidator(50)
+	vdr1 := validators.GenerateRandomValidator(50)
+	vdr2 := validators.GenerateRandomValidator(50)
+	vdr3 := validators.GenerateRandomValidator(50)
+	vdr4 := validators.GenerateRandomValidator(50)
+	vdrs.AddWeight(vdr0.ID(), vdr0.Weight())
+	vdrs.AddWeight(vdr1.ID(), vdr1.Weight())
+	vdrs.AddWeight(vdr2.ID(), vdr2.Weight())
+	vdrs.AddWeight(vdr3.ID(), vdr3.Weight())
+	vdrs.AddWeight(vdr4.ID(), vdr4.Weight())
+
+	config := BlacklistConfig{
+		Threshold:  3,
+		Duration:   time.Minute,
+		MaxPortion: 0.5,
+		Validators: vdrs,
+	}
+	blacklist := NewQueryBlacklist(config).(*queryBlacklist)
+
+	currentTime := time.Now()
+	blacklist.clock.Set(currentTime)
+
+	requestID := uint32(0)
+
+	for i := 0; i < config.Threshold; i++ {
+		if ok := blacklist.RegisterQuery(vdr0.ID(), requestID); !ok {
+			t.Fatalf("RegisterQuery failed early for vdr0 on iteration: %d", i)
+		}
+		blacklist.QueryFailed(vdr0.ID(), requestID)
+		requestID++
+		if ok := blacklist.RegisterQuery(vdr1.ID(), requestID); !ok {
+			t.Fatalf("RegisterQuery failed for vdr1 on iteration: %d", i)
+		}
+		blacklist.RegisterResponse(vdr1.ID(), requestID)
+
+		requestID++
+		if ok := blacklist.RegisterQuery(vdr2.ID(), requestID); !ok {
+			t.Fatalf("RegisterQuery failed early for vdr2 on iteration: %d", i)
+		}
+		blacklist.QueryFailed(vdr2.ID(), requestID)
+		requestID++
+	}
+
+	if ok := blacklist.RegisterQuery(vdr0.ID(), requestID); ok {
+		t.Fatal("RegisterQuery should have blacklisted query from unresponsive peer: vdr0")
+	}
+	if ok := blacklist.RegisterQuery(vdr2.ID(), requestID); ok {
+		t.Fatal("RegisterQuery should have blacklisted query from unresponsive peer: vdr2")
+	}
+	requestID++
+	if ok := blacklist.RegisterQuery(vdr1.ID(), requestID); !ok {
+		t.Fatal("RegisterQuery should have been successful for responsive peer: vdr1")
+	}
+
+	blacklist.clock.Set(currentTime.Add(config.Duration))
+	if ok := blacklist.RegisterQuery(vdr0.ID(), requestID); !ok {
+		t.Fatal("RegisterQuery should have succeeded after blacklisting time elapsed for vdr0")
+	}
+	if ok := blacklist.RegisterQuery(vdr2.ID(), requestID); !ok {
+		t.Fatal("RegisterQuery should have succeeded after blacklisting time elapsed for vdr2")
+	}
+}
+
+func TestBlacklistDoesNotGetStuck(t *testing.T) {
+	vdrs := validators.NewSet()
+	vdr0 := validators.GenerateRandomValidator(50)
+	vdr1 := validators.GenerateRandomValidator(50)
+	vdr2 := validators.GenerateRandomValidator(50)
+	vdrs.AddWeight(vdr0.ID(), vdr0.Weight())
+	vdrs.AddWeight(vdr1.ID(), vdr1.Weight())
+	vdrs.AddWeight(vdr2.ID(), vdr2.Weight())
+
+	config := BlacklistConfig{
+		Threshold:  3,
+		Duration:   time.Minute,
+		MaxPortion: 0.5,
+		Validators: vdrs,
+	}
+	blacklist := NewQueryBlacklist(config).(*queryBlacklist)
+
+	currentTime := time.Now()
+	blacklist.clock.Set(currentTime)
+
+	requestID := uint32(0)
+
+	for i := 0; i < config.Threshold; i++ {
+		if ok := blacklist.RegisterQuery(vdr0.ID(), requestID); !ok {
+			t.Fatalf("RegisterQuery failed early on iteration: %d", i)
+		}
+		blacklist.QueryFailed(vdr0.ID(), requestID)
+		requestID++
+	}
+
+	// Check that calling QueryFailed repeatedly does not change
+	// the blacklist end time after it's already been blacklisted
+	for i := 0; i < config.Threshold; i++ {
+		blacklist.QueryFailed(vdr0.ID(), requestID)
+		requestID++
+	}
+
+	if ok := blacklist.RegisterQuery(vdr0.ID(), requestID); ok {
+		t.Fatal("RegisterQuery should have blacklisted query from consistently failing peer")
+	}
+	requestID++
+
+	blacklist.clock.Set(currentTime.Add(config.Duration))
+	if ok := blacklist.RegisterQuery(vdr0.ID(), requestID); !ok {
+		t.Fatal("RegisterQuery should have succeeded after blacklisting time elapsed")
+	}
+}
+
+func TestBlacklistDoesNotExceedThreshold(t *testing.T) {
+	vdrs := validators.NewSet()
+	vdr0 := validators.GenerateRandomValidator(50)
+	vdr1 := validators.GenerateRandomValidator(50)
+	vdr2 := validators.GenerateRandomValidator(50)
+	vdrs.AddWeight(vdr0.ID(), vdr0.Weight())
+	vdrs.AddWeight(vdr1.ID(), vdr1.Weight())
+	vdrs.AddWeight(vdr2.ID(), vdr2.Weight())
+
+	config := BlacklistConfig{
+		Threshold:  3,
+		Duration:   time.Minute,
+		MaxPortion: 0.5,
+		Validators: vdrs,
+	}
+	blacklist := NewQueryBlacklist(config).(*queryBlacklist)
+
+	currentTime := time.Now()
+	blacklist.clock.Set(currentTime)
+
+	requestID := uint32(0)
+
+	for i := 0; i < config.Threshold; i++ {
+		if ok := blacklist.RegisterQuery(vdr0.ID(), requestID); !ok {
+			t.Fatalf("RegisterQuery failed early for vdr0 on iteration: %d", i)
+		}
+		if ok := blacklist.RegisterQuery(vdr1.ID(), requestID); !ok {
+			t.Fatalf("RegisterQuery failed early for vdr1 on iteration: %d", i)
+		}
+		blacklist.QueryFailed(vdr0.ID(), requestID)
+		blacklist.QueryFailed(vdr1.ID(), requestID)
+		requestID++
+	}
+
+	ok0 := blacklist.RegisterQuery(vdr0.ID(), requestID)
+	ok1 := blacklist.RegisterQuery(vdr1.ID(), requestID)
+	if !ok0 && !ok1 {
+		t.Fatal("Blacklisted staking weight past the allowed threshold")
+	}
+
+	blacklist.clock.Set(currentTime.Add(config.Duration))
+	if ok := blacklist.RegisterQuery(vdr0.ID(), requestID); !ok {
+		t.Fatal("RegisterQuery should have succeeded after blacklisting time elapsed")
+	}
+	if ok := blacklist.RegisterQuery(vdr1.ID(), requestID); !ok {
+		t.Fatal("RegisterQuery should have succeeded after blacklisting time elapsed")
+	}
+}
+
+type noBlacklist struct{}
+
+// NewNoBlacklist returns an empty blacklist that will never stop any queries
+func NewNoBlacklist() Blacklist { return &noBlacklist{} }
+
+// RegisterQuery ...
+func (b *noBlacklist) RegisterQuery(validatorID ids.ShortID, requestID uint32) bool { return true }
+
+// RegisterResponse ...
+func (b *noBlacklist) RegisterResponse(validatorID ids.ShortID, requestID uint32) {}
+
+// QueryFailed ...
+func (b *noBlacklist) QueryFailed(validatorID ids.ShortID, requestID uint32) {}
