db86c6eadca1f6953e5c1657ea45e8aa98cd4dcd
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-10-20 12:06:08 -0400

    Set txpool API to be enabled by default

--- graft/coreth/plugin/params.go
+++ graft/coreth/plugin/params.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"os"
 
 	"github.com/ava-labs/avalanchego/utils/wrappers"
 	"github.com/ava-labs/coreth/plugin/evm"
@@ -22,8 +23,15 @@ func init() {
 	fs := flag.NewFlagSet(name, flag.ContinueOnError)
 
 	config := fs.String("coreth-config", "default", "Pass in CLI Config to set runtime attributes for Coreth")
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		errs.Add(err)
+		return
+	}
+
 	if *config == "default" {
 		cliConfig.EthAPIEnabled = true
+		cliConfig.TxPoolAPIEnabled = true
 	} else {
 		errs.Add(json.Unmarshal([]byte(*config), &cliConfig))
 	}
