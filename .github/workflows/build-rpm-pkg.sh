99fb61b0a87e0a53dedef44acb86172d73ce2c59
Charlie Wyse <charlie.wyse@pubnub.com>
2020-10-27 15:26:13 -0700

    made release based on -

--- .github/workflows/build-rpm-pkg.sh
+++ .github/workflows/build-rpm-pkg.sh
@@ -17,7 +17,9 @@ if [[ $OK -ne 0 ]]; then
 fi
 
 echo "Build rpm package..."
-VER=$(echo $TAG | tr -d 'v' | sed "s/-/_/")
+VER=$(echo $TAG | gawk -F- '{print$1}' | tr -d 'v' )
+REL=$(echo $TAG | gawk -F- '{print$2}')
+[ -z "$REL" ] && REL=0 
 echo "Tag: $VER"
-rpmbuild --bb --define "version $VER" --buildroot $RPM_BASE_DIR .github/workflows/yum/specfile/avalanchego.spec
+rpmbuild --bb --define "version $VER" --define "release $REL" --buildroot $RPM_BASE_DIR .github/workflows/yum/specfile/avalanchego.spec
 aws s3 cp ~/rpmbuild/RPMS/x86_64/avalanchego-*.rpm s3://$BUCKET/linux/rpm
