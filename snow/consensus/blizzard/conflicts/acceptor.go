809b34148610e5c027acc47ee3a73ed7caf92d84
StephenButtolph <stephen@avalabs.org>
2020-10-28 15:55:37 -0400

    wip

---
+++ snow/consensus/blizzard/conflicts/acceptor.go
@@ -0,0 +1,34 @@
+// (c) 2019-2020, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package conflicts
+
+import (
+	"github.com/ava-labs/avalanchego/ids"
+)
+
+// acceptor implements Blockable
+type acceptor struct {
+	c        *conflicts
+	deps     ids.Set
+	rejected bool
+	tx       Tx
+}
+
+func (a *acceptor) Dependencies() ids.Set { return a.deps }
+
+func (a *acceptor) Fulfill(id ids.ID) {
+	a.deps.Remove(id)
+	a.Update()
+}
+
+func (a *acceptor) Abandon(id ids.ID) { a.rejected = true }
+
+func (a *acceptor) Update() {
+	// If I was rejected or I am still waiting on dependencies to finish or an
+	// error has occurred, I shouldn't do anything.
+	if a.rejected || a.deps.Len() != 0 {
+		return
+	}
+	a.c.acceptable = append(a.c.acceptable, a.tx)
+}
--- snow/consensus/blizzard/conflicts/conflicts.go
+++ snow/consensus/blizzard/conflicts/conflicts.go
@@ -8,41 +8,21 @@ import (
 
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow/choices"
+	"github.com/ava-labs/avalanchego/snow/consensus/blizzard"
 	"github.com/ava-labs/avalanchego/snow/events"
 )
-
-type Conflicts interface {
-	// Add this transaction to conflict tracking
-	Add(tx choices.Decidable) error
-
-	// Conflicts returns true if there are no transactions currently tracked
-	IsVirtuous(tx choices.Decidable) (bool, error)
-
-	// Conflicts returns the transactions that conflict with the provided
-	// transaction
-	Conflicts(tx choices.Decidable) ([]choices.Decidable, error)
-
-	// Mark this transaction as conditionally accepted
-	Accept(txID ids.ID)
-
-	// Updateable returns the transactions that can be accepted and rejected.
-	// Assumes that returned transactions are accepted or rejected before the
-	// next time this function is called. Acceptable transactions must have been
-	// identified as having been confitionally accepted. If an acceptable
-	// transaction was marked as having a conflict, then that conflict should be
-	// returned in the same call as the acceptable transaction was returned or
-	// in a prior call.
-	Updateable() (acceptable []choices.Decidable, rejectable []choices.Decidable)
-}
 
 var (
 	errInvalidTxType = errors.New("invalid tx type")
 
-	_ Conflicts = &conflicts{}
+	_ blizzard.Conflicts = &conflicts{}
 )
 
 type conflicts struct {
-	txs   map[[32]byte]Tx
+	// track the currently processing txs
+	txs map[[32]byte]Tx
+
+	// track which txs are currently consuming which utxos
 	utxos map[[32]byte]ids.Set
 
 	// keeps track of whether dependencies have been accepted
@@ -51,10 +31,17 @@ type conflicts struct {
 	// keeps track of whether dependencies have been rejected
 	pendingReject events.Blocker
 
+	// track txs that have been marked as ready to accept
 	acceptable []choices.Decidable
+
+	// track txs that have been marked as ready to reject
 	rejectable []choices.Decidable
 }
 
+// Add this tx to the conflict set. If this tx is of the correct type, this tx
+// will be added to the set of processing txs. It is assumed this tx wasn't
+// already processing. This will mark the consumed utxos and register a rejector
+// that will be notified if a dependency of this tx was rejected.
 func (c *conflicts) Add(txIntf choices.Decidable) error {
 	tx, ok := txIntf.(Tx)
 	if !ok {
@@ -68,9 +55,27 @@ func (c *conflicts) Add(txIntf choices.Decidable) error {
 		spenders.Add(txID)
 		c.utxos[inputKey] = spenders
 	}
+
+	toReject := &rejector{
+		c:  c,
+		tx: tx,
+	}
+	for _, dependency := range tx.Dependencies() {
+		if dependency.Status() != choices.Accepted {
+			// If the dependency isn't accepted, then it must be processing.
+			// This tx should be accepted after this tx is accepted. Note that
+			// the dependencies can't already be rejected, because it is assumed
+			// that this tx is currently considered valid.
+			toReject.deps.Add(dependency.ID())
+		}
+	}
+	c.pendingReject.Register(toReject)
 	return nil
 }
 
+// IsVirtuous checks the currently processing txs for conflicts. It is allowed
+// to call with function with txs that aren't yet processing or currently
+// processing txs.
 func (c *conflicts) IsVirtuous(txIntf choices.Decidable) (bool, error) {
 	tx, ok := txIntf.(Tx)
 	if !ok {
@@ -88,6 +93,9 @@ func (c *conflicts) IsVirtuous(txIntf choices.Decidable) (bool, error) {
 	return true, nil
 }
 
+// Conflicts returns the collection of txs that are currently processing that
+// conflict with the provided tx. It is allowed to call with function with txs
+// that aren't yet processing or currently processing txs.
 func (c *conflicts) Conflicts(txIntf choices.Decidable) ([]choices.Decidable, error) {
 	tx, ok := txIntf.(Tx)
 	if !ok {
@@ -113,55 +121,76 @@ func (c *conflicts) Conflicts(txIntf choices.Decidable) ([]choices.Decidable, er
 	return conflicts, nil
 }
 
-func (c *conflicts) Accept(txID ids.ID) {}
-
-func (c *conflicts) Updateable() (acceptable []choices.Decidable, rejectable []choices.Decidable) {
-	return nil, nil
-}
-
-// acceptor implements Blockable
-type acceptor struct {
-	c        *conflicts
-	deps     ids.Set
-	rejected bool
-	tx       Tx
-}
-
-func (a *acceptor) Dependencies() ids.Set { return a.deps }
-
-func (a *acceptor) Fulfill(id ids.ID) {
-	a.deps.Remove(id)
-	a.Update()
-}
-
-func (a *acceptor) Abandon(id ids.ID) { a.rejected = true }
-
-func (a *acceptor) Update() {
-	// If I was rejected or I am still waiting on dependencies to finish or an
-	// error has occurred, I shouldn't do anything.
-	if a.rejected || a.deps.Len() != 0 {
+// Accept notifies this conflict manager that a tx has been conditionally
+// accepted. This means that assuming all the txs this tx depends on are
+// accepted, then this tx should be accepted as well. This
+func (c *conflicts) Accept(txID ids.ID) {
+	tx, exists := c.txs[txID.Key()]
+	if !exists {
 		return
 	}
-	a.c.acceptable = append(a.c.acceptable, a.tx)
-}
 
-// rejector implements Blockable
-type rejector struct {
-	c        *conflicts
-	deps     ids.Set
-	rejected bool // true if the tx has been rejected
-	tx       Tx
-}
-
-func (r *rejector) Dependencies() ids.Set { return r.deps }
-
-func (r *rejector) Fulfill(ids.ID) {
-	if r.rejected {
-		return
+	toAccept := &acceptor{
+		c:  c,
+		tx: tx,
 	}
-	r.rejected = true
-	r.c.rejectable = append(r.c.rejectable, r.tx)
+	for _, dependency := range tx.Dependencies() {
+		if dependency.Status() != choices.Accepted {
+			// If the dependency isn't accepted, then it must be processing.
+			// This tx should be accepted after this tx is accepted. Note that
+			// the dependencies can't already be rejected, because it is assumed
+			// that this tx is currently considered valid.
+			toAccept.deps.Add(dependency.ID())
+		}
+	}
+	c.pendingAccept.Register(toAccept)
 }
 
-func (*rejector) Abandon(ids.ID) {}
-func (*rejector) Update()        {}
+func (c *conflicts) Updateable() ([]choices.Decidable, []choices.Decidable) {
+	acceptable := c.acceptable
+	c.acceptable = nil
+	for _, tx := range acceptable {
+		txID := tx.ID()
+
+		tx := tx.(Tx)
+		for inputKey := range tx.InputIDs() {
+			spenders := c.utxos[inputKey]
+			spenders.Remove(txID)
+			if spenders.Len() == 0 {
+				spenders.Remove(txID)
+			} else {
+				c.utxos[inputKey] = spenders
+			}
+		}
+
+		delete(c.txs, txID.Key())
+		c.pendingAccept.Fulfill(txID)
+		c.pendingReject.Abandon(txID)
+
+		conflicts, _ := c.Conflicts(tx)
+
+	}
+
+	rejectable := c.rejectable
+	c.rejectable = nil
+	for _, tx := range rejectable {
+		txID := tx.ID()
+
+		tx := tx.(Tx)
+		for inputKey := range tx.InputIDs() {
+			spenders := c.utxos[inputKey]
+			spenders.Remove(txID)
+			if spenders.Len() == 0 {
+				spenders.Remove(txID)
+			} else {
+				c.utxos[inputKey] = spenders
+			}
+		}
+
+		delete(c.txs, txID.Key())
+		c.pendingAccept.Abandon(txID)
+		c.pendingReject.Fulfill(txID)
+	}
+
+	return acceptable, rejectable
+}
