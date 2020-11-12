a6ce04ef83a46c09e36712212fb10fe2ec785249
Dan Laine <daniel.laine@avalabs.org>
2020-11-12 16:55:35 -0500

    add encoding tests

--- utils/formatting/encoding_test.go
+++ utils/formatting/encoding_test.go
@@ -4,8 +4,68 @@
 package formatting
 
 import (
+	"encoding/json"
 	"testing"
+
+	"gotest.tools/assert"
 )
+
+func TestEncodingMarshalJSON(t *testing.T) {
+	enc := Hex
+	jsonBytes, err := enc.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(jsonBytes) != "\"hex\"" {
+		t.Fatal("should be 'hex'")
+	}
+
+	enc2 := CB58
+	jsonBytes, err = enc2.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(jsonBytes) != "\"cb58\"" {
+		t.Fatal("should be 'cb58'")
+	}
+}
+
+func TestEncodingUnmarshalJSON(t *testing.T) {
+	jsonBytes := []byte("\"hex\"")
+	var enc Encoding
+	if err := json.Unmarshal(jsonBytes, &enc); err != nil {
+		t.Fatal(err)
+	}
+	if enc != Hex {
+		t.Fatal("should be hex")
+	}
+
+	jsonBytes = []byte("\"cb58\"")
+	if err := json.Unmarshal(jsonBytes, &enc); err != nil {
+		t.Fatal(err)
+	}
+	if enc != CB58 {
+		t.Fatal("should be cb58")
+	}
+
+	jsonBytes = []byte("")
+	if err := json.Unmarshal(jsonBytes, &enc); err == nil {
+		t.Fatal("should have errored due to invalid encoding")
+	}
+
+	jsonBytes = []byte("\"\"")
+	if err := json.Unmarshal(jsonBytes, &enc); err == nil {
+		t.Fatal("should have errored due to invalid encoding")
+	}
+}
+
+func TestEncodingString(t *testing.T) {
+	enc := Hex
+	assert.Equal(t, enc.String(), "hex")
+
+	enc2 := CB58
+	assert.Equal(t, enc2.String(), "cb58")
+}
 
 func TestEncodingManager(t *testing.T) {
 	m, err := NewEncodingManager(Hex)
