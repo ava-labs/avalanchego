c3db2f58c2d941684febce25a3488601214a8d34
dhrubabasu <dhruba@avalabs.org>
2020-11-12 20:31:34 -0500

    replace travis with github actions

--- .ci/build_and_test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ev
-
-cd $TRAVIS_BUILD_DIR 
-./scripts/build.sh
-./scripts/build_test.sh
--- .ci/runscript_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -ev
-
-docker run --rm -v "$PWD:$AVALANCHE_HOME" $DOCKERHUB_REPO:$COMMIT bash "$AVALANCHE_HOME/scripts/build_test.sh"
-docker run --rm -v "$PWD:$AVALANCHE_HOME" $DOCKERHUB_REPO:$COMMIT bash "$AVALANCHE_HOME/scripts/build.sh"
