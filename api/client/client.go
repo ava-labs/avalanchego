3d3a6594779dd26c7d1b6e55f26ad291a89946c4
Pedro Gomes <otherview@gmail.com>
2020-11-24 17:27:17 +0000

    fixing imports

--- api/client/client.go
+++ api/client/client.go
@@ -3,13 +3,14 @@ package client
 import (
 	"time"
 
-	"github.com/ava-labs/avalanche-testing/avalanche_client/apis/avm"
-	"github.com/ava-labs/avalanche-testing/avalanche_client/apis/platform"
+	"github.com/ava-labs/avalanchego/vms/platformvm"
+
 	"github.com/ava-labs/avalanchego/api/admin"
 	"github.com/ava-labs/avalanchego/api/health"
 	"github.com/ava-labs/avalanchego/api/info"
 	"github.com/ava-labs/avalanchego/api/ipcs"
 	"github.com/ava-labs/avalanchego/api/keystore"
+	avmclient "github.com/ava-labs/avalanchego/vms/avm/apiclient"
 )
 
 const (
@@ -18,32 +19,32 @@ const (
 
 type Client struct {
 	admin    *admin.Client
-	xChain   *avm.Client
+	xChain   *avmclient.Client
 	health   *health.Client
 	info     *info.Client
 	ipcs     *ipcs.Client
 	keystore *keystore.Client
-	platform *platform.Client
+	platform *platformvm.Client
 }
 
 // Returns a Client for interacting with the P Chain endpoint
 func NewClient(uri string, requestTimeout time.Duration) *Client {
 	return &Client{
 		admin:    admin.NewClient(uri, requestTimeout),
-		xChain:   avm.NewClient(uri, XChain, requestTimeout),
+		xChain:   avmclient.NewClient(uri, XChain, requestTimeout),
 		health:   health.NewClient(uri, requestTimeout),
 		info:     info.NewClient(uri, requestTimeout),
 		ipcs:     ipcs.NewClient(uri, requestTimeout),
 		keystore: keystore.NewClient(uri, requestTimeout),
-		platform: platform.NewClient(uri, requestTimeout),
+		platform: platformvm.NewClient(uri, requestTimeout),
 	}
 }
 
-func (c *Client) PChainAPI() *platform.Client {
+func (c *Client) PChainAPI() *platformvm.Client {
 	return c.platform
 }
 
-func (c *Client) XChainAPI() *avm.Client {
+func (c *Client) XChainAPI() *avmclient.Client {
 	return c.xChain
 }
 
