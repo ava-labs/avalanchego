07e9ecc301ff1fdf6f7c927f26607fc2e2c644ae
StephenButtolph <stephen@avalabs.org>
2020-11-24 12:45:42 -0500

    factored out new vertex interface

--- snow/engine/avalanche/state/unique_vertex.go
+++ snow/engine/avalanche/state/unique_vertex.go
@@ -212,6 +212,16 @@ func (vtx *uniqueVertex) Height() (uint64, error) {
 	return vtx.v.vtx.height, nil
 }
 
+func (vtx *uniqueVertex) Epoch() (uint32, error) {
+	vtx.refresh()
+
+	if vtx.v.vtx == nil {
+		return 0, fmt.Errorf("failed to get epoch for vertex with status: %s", vtx.v.status)
+	}
+
+	return vtx.v.vtx.epoch, nil
+}
+
 func (vtx *uniqueVertex) Txs() ([]snowstorm.Tx, error) {
 	vtx.refresh()
 
