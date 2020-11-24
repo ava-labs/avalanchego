c5875ce485d9d3f55356d6d64740ed725ef0da58
StephenButtolph <stephen@avalabs.org>
2020-11-24 18:32:31 -0500

    cleaned up vertex heap definition

--- snow/engine/avalanche/vertex/manager.go
+++ snow/engine/avalanche/vertex/manager.go
@@ -9,18 +9,32 @@ import (
 	"github.com/ava-labs/avalanchego/snow/consensus/snowstorm"
 )
 
-// Manager defines the persistent storage that is required by the consensus
-// engine
-type Manager interface {
+// Builder builds a vertex given a set of parentIDs and transactions.
+type Builder interface {
 	// Create a new vertex from the contents of a vertex
 	BuildVertex(parentIDs []ids.ID, txs []snowstorm.Tx) (avalanche.Vertex, error)
+}
 
+// Parser parses bytes into a vertex.
+type Parser interface {
 	// Attempt to convert a stream of bytes into a vertex
 	ParseVertex(vertex []byte) (avalanche.Vertex, error)
+}
 
-	// GetVertex attempts to load a vertex by hash from storage
+// Storage defines the persistent storage that is required by the consensus
+// engine.
+type Storage interface {
+	// GetVertex attempts to load a vertex by hash from storage.
 	GetVertex(vtxID ids.ID) (avalanche.Vertex, error)
 
-	// Edge returns a list of accepted vertex IDs with no accepted children
+	// Edge returns a list of accepted vertex IDs with no accepted children.
 	Edge() (vtxIDs []ids.ID)
+}
+
+// Manager defines all the vertex related functionality that is required by the
+// consensus engine.
+type Manager interface {
+	Builder
+	Parser
+	Storage
 }
