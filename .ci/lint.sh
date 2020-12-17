5f6acbf70cb34f8e7978ae5ef4b2c323b2605ff4
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-12-17 14:14:45 -0500

    Reduce overhead in ci lint script

--- .ci/lint.sh
+++ .ci/lint.sh
@@ -1,9 +1,5 @@
 # binary will be $(go env GOPATH)/bin/golangci-lint
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.30.0
-# or install it into ./bin/
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.30.0
-# In alpine linux (as it does not come with curl by default)
-wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.30.0
 
 export PATH=$PATH:$(go env GOPATH)/bin
 
