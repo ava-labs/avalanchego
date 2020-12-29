b3c775cbe6e1b77b6e26535fae1f6a2f1814ddcc
StephenButtolph <stephen@avalabs.org>
2020-12-29 17:32:12 -0500

    added state support for restrictions

--- snow/engine/avalanche/state/tx.go
+++ snow/engine/avalanche/state/tx.go
@@ -4,6 +4,8 @@
 package state
 
 import (
+	"fmt"
+
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow/choices"
 	"github.com/ava-labs/avalanchego/snow/consensus/snowstorm/conflicts"
@@ -15,25 +17,69 @@ var (
 )
 
 type tx struct {
-	tx vertex.StatelessTx
-	tr conflicts.Transition
+	serializer *Serializer
+	tx         vertex.StatelessTx
+	tr         conflicts.Transition
 }
 
 func (t *tx) ID() ids.ID { return t.tx.ID() }
 
-func (t *tx) Accept() error { return t.tr.Accept(t.tx.Epoch()) }
+func (t *tx) Accept() error {
+	epoch := t.tx.Epoch()
+	if err := t.tr.Accept(epoch); err != nil {
+		return err
+	}
+	if err := t.serializer.state.SetTxStatus(t.ID(), choices.Accepted); err != nil {
+		return err
+	}
+	for _, restriction := range t.tx.Restrictions() {
+		previousEpoch := t.serializer.state.TrRestriction(restriction)
+		if previousEpoch >= epoch {
+			continue
+		}
+		if err := t.serializer.state.SetTrRestriction(restriction, epoch); err != nil {
+			return err
+		}
+	}
+	return t.serializer.db.Commit()
+}
 
-func (t *tx) Reject() error { return t.tr.Reject(t.tx.Epoch()) }
+func (t *tx) Reject() error {
+	if err := t.tr.Reject(t.tx.Epoch()); err != nil {
+		return err
+	}
+	if err := t.serializer.state.SetTxStatus(t.ID(), choices.Rejected); err != nil {
+		return err
+	}
+	return t.serializer.db.Commit()
+}
 
-// TODO: This status implementation might not make sense
-func (t *tx) Status() choices.Status { return t.tr.Status() }
+func (t *tx) Status() choices.Status {
+	txStatus := t.serializer.state.TxStatus(t.ID())
+	if txStatus != choices.Unknown {
+		return txStatus
+	}
+	trStatus := t.tr.Status()
+	if trStatus == choices.Accepted && t.tx.Epoch() == t.tr.Epoch() {
+		return choices.Accepted
+	}
+	return trStatus
+}
 
 func (t *tx) Transition() conflicts.Transition { return t.tr }
 
-func (t *tx) Epoch() uint32 { return t.tr.Epoch() }
+func (t *tx) Epoch() uint32 { return t.tx.Epoch() }
 
 func (t *tx) Restrictions() []ids.ID { return t.tx.Restrictions() }
 
-func (t *tx) Verify() error { return t.tr.Verify(t.tx.Epoch()) }
+func (t *tx) Verify() error {
+	trID := t.tr.ID()
+	epoch := t.tx.Epoch()
+	restriction := t.serializer.state.TrRestriction(trID)
+	if restriction < epoch {
+		return fmt.Errorf("transition %s was restriction to epoch %d", trID, epoch)
+	}
+	return t.tr.Verify(t.tx.Epoch())
+}
 
 func (t *tx) Bytes() []byte { return t.tx.Bytes() }
