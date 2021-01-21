ef4bf01739d69f0eb83312ce0c68d06909e1b048
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-01-21 15:30:24 -0500

    Reduce number of docker image pulls

--- .ci/run_e2e_tests.sh
+++ .ci/run_e2e_tests.sh
@@ -1,5 +1,12 @@
 SCRIPTS_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
 SRC_PATH=$(dirname "${SCRIPTS_PATH}")
+
+# Skip the E2E Tests if this is not a PR and it 
+if [[ $BRANCH != "master" && $BRANCH != "dev" ]]; then
+    echo "Skipping E2E Tests for $BRANCH"
+    exit 0
+fi
+
 # Build the runnable Avalanche docker image
 bash "${SRC_PATH}"/scripts/build_image.sh
 
