f9b29608ad66d32a56eca30ef53e3f203fe28741
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-01-27 22:12:02 -0500

    Remove unnecessary interface

--- vms/platformvm/state.go
+++ vms/platformvm/state.go
@@ -18,7 +18,6 @@ import (
 	"github.com/ava-labs/avalanchego/utils/hashing"
 	"github.com/ava-labs/avalanchego/utils/wrappers"
 	"github.com/ava-labs/avalanchego/vms/components/avax"
-	"github.com/ava-labs/avalanchego/vms/components/state"
 
 	safemath "github.com/ava-labs/avalanchego/utils/math"
 )
@@ -480,7 +479,7 @@ func (vm *VM) removeReferencingUTXO(db database.Database, addrBytes []byte, utxo
 func (vm *VM) GetUTXOs(
 	db database.Database,
 	addrs ids.ShortSet,
-	startAddr state.Marshaller,
+	startAddr ids.ShortID,
 	startUTXOID ids.ID,
 	limit int,
 	paginate bool,
@@ -498,7 +497,7 @@ func (vm *VM) GetUTXOs(
 func (vm *VM) getPaginatedUTXOs(
 	db database.Database,
 	addrs ids.ShortSet,
-	startAddr state.Marshaller,
+	startAddr ids.ShortID,
 	startUTXOID ids.ID,
 	limit int,
 ) ([]*avax.UTXO, ids.ShortID, ids.ID, error) {
