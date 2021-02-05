2192db4a63a1eb5b83058548fcc9cbe13eee62ef
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-02-05 16:23:41 -0500

    Fix repair chain for tx indexing

--- plugin/evm/vm.go
+++ plugin/evm/vm.go
@@ -64,7 +64,7 @@ var (
 var (
 	lastAcceptedKey = []byte("snowman_lastAccepted")
 	acceptedPrefix  = []byte("snowman_accepted")
-	repairedKey     = []byte("repaired")
+	repairedKey     = []byte("repair_chain")
 )
 
 const (
