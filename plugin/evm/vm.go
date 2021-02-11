ba2373d72fb4b99e573402d22d812d3bf6dd015b
StephenButtolph <stephen@avalabs.org>
2021-02-10 20:11:57 -0500

    updated chain index

--- plugin/evm/vm.go
+++ plugin/evm/vm.go
@@ -64,7 +64,7 @@ var (
 var (
 	lastAcceptedKey = []byte("snowman_lastAccepted")
 	acceptedPrefix  = []byte("snowman_accepted")
-	repairedKey     = []byte("chain_repaired")
+	repairedKey     = []byte("chain_is_repaired")
 )
 
 const (
