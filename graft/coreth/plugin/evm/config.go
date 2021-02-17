31b3583d66bbaade40a776b051618e53904eff53
Patrick O'Grady <prohb125@gmail.com>
2021-02-17 10:48:39 -0800

    linter

--- graft/coreth/plugin/evm/config.go
+++ graft/coreth/plugin/evm/config.go
@@ -24,7 +24,7 @@ type CommandLineConfig struct {
 	Web3APIEnabled     bool `json:"web3-api-enabled"`
 
 	// Eth Settings
-	LocalTxsEnabled bool `json:"local-txs-enabled"`
+	LocalTxsEnabled     bool  `json:"local-txs-enabled"`
 	APIMaxDuration      int64 `json:"api-max-duration"`
 	MaxBlocksPerRequest int64 `json:"api-max-blocks-per-request"`
 
