58eb11ce235f10346c0e328ca4a1fed7fb55d906
StephenButtolph <stephen@avalabs.org>
2021-03-01 16:25:10 -0500

    bumped avalanchego version

--- graft/coreth/plugin/evm/health.go
+++ graft/coreth/plugin/evm/health.go
@@ -6,7 +6,7 @@ package evm
 // Health returns nil if this chain is healthy.
 // Also returns details, which should be one of:
 // string, []byte, map[string]string
-func (vm *VM) Health() (interface{}, error) {
+func (vm *VM) HealthCheck() (interface{}, error) {
 	// TODO perform actual health check
 	return nil, nil
 }
