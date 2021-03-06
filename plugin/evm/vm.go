7ebdcc30c14ea5c9a41b79c4334bf84054fd0492
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-03-05 21:28:31 -0500

    Rename repair scripts

--- plugin/evm/vm.go
+++ plugin/evm/vm.go
@@ -66,10 +66,10 @@ var (
 )
 
 var (
-	lastAcceptedKey = []byte("snowman_lastAccepted")
-	acceptedPrefix  = []byte("snowman_accepted")
-	repairedKey1    = []byte("chain_repaired_20210212")
-	repairedKey2    = []byte("chain_repaired_20210305")
+	lastAcceptedKey              = []byte("snowman_lastAccepted")
+	acceptedPrefix               = []byte("snowman_accepted")
+	historicalCanonicalRepairKey = []byte("chain_repaired_20210212")
+	tipCanonicalRepairKey        = []byte("chain_repaired_20210305")
 )
 
 const (
@@ -457,71 +457,71 @@ func (vm *VM) Initialize(
 // occurred.
 // assumes that the genesisHash and [lastAccepted] block have already been set.
 func (vm *VM) repairCanonicalChain() error {
-	if ran, err := vm.repair1(); err != nil {
+	if ran, err := vm.repairHistorical(); err != nil {
 		return err
 	} else if ran {
 		return nil
 	}
 
-	_, err := vm.repair2()
+	_, err := vm.repairTip()
 	return err
 }
 
-// repair1 writes the canonical chain index from the last accepted block back to the
+// repairHistorical writes the canonical chain index from the last accepted block back to the
 // genesis block to overwrite any corruption that might have occurred.
 // assumes that the genesis hash and [lastAccepted] block have already been set.
 // returns true if the repair occurs during this call. If the repair occurred on a
 // prior run, then false is returned.
-func (vm *VM) repair1() (bool, error) {
-	// Check if the canonical chain repair 1 has already occurred.
-	if has, err := vm.db.Has(repairedKey1); err != nil {
+func (vm *VM) repairHistorical() (bool, error) {
+	// Check if the historical canonical chain repair has already occurred.
+	if has, err := vm.db.Has(historicalCanonicalRepairKey); err != nil {
 		return false, err
 	} else if has {
 		return false, nil
 	}
 
 	start := time.Now()
-	log.Info("starting canonical chain repair 1", "startTime", start)
+	log.Info("starting historical canonical chain repair", "startTime", start)
 	genesisBlock := vm.chain.GetGenesisBlock()
 	if genesisBlock == nil {
-		return false, fmt.Errorf("failed to fetch genesis block from chain during repair 1")
+		return false, fmt.Errorf("failed to fetch genesis block from chain during historical chain repair")
 	}
 	if err := vm.chain.WriteCanonicalFromCurrentBlock(genesisBlock); err != nil {
-		return false, fmt.Errorf("canonical chain repair 1 failed after %v due to: %w", time.Since(start), err)
+		return false, fmt.Errorf("historical canonical chain repair failed after %v due to: %w", time.Since(start), err)
 	}
-	log.Info("finished canonical chain repair 1", "timeElapsed", time.Since(start))
-	if err := vm.db.Put(repairedKey1, []byte("finished")); err != nil {
-		return false, fmt.Errorf("failed to mark flag for canonical chain repair 1: %w", err)
+	log.Info("finished historical canonical chain repair", "timeElapsed", time.Since(start))
+	if err := vm.db.Put(historicalCanonicalRepairKey, []byte("finished")); err != nil {
+		return false, fmt.Errorf("failed to mark flag for historical canonical chain repair: %w", err)
 	}
 	if err := vm.chain.ValidateCanonicalChain(); err != nil {
-		return false, fmt.Errorf("failed to validate canonical chain after repair 1 due to: %w", err)
+		return false, fmt.Errorf("failed to validate historical canonical chain repair due to: %w", err)
 	}
 
 	return true, nil
 }
 
-// repair2 writes the canonical chain index from the current block in the canonical chain
+// repairTip writes the canonical chain index from the current block in the canonical chain
 // back to the last accepted block to overwrite any corruption of non-finalized blocks that
 // might have occurred.
 // assumes that the genesis hash and [lastAccepted] block have already been set.
 // returns true if the repair occurs during this call. If the repair occurred on a
 // prior run, then false is returned.
-func (vm *VM) repair2() (bool, error) {
-	// Check if the canonical chain repair 2 has already occurred.
-	if has, err := vm.db.Has(repairedKey2); err != nil {
+func (vm *VM) repairTip() (bool, error) {
+	// Check if the canonical chain tip repair has already occurred.
+	if has, err := vm.db.Has(tipCanonicalRepairKey); err != nil {
 		return false, err
 	} else if has {
 		return false, nil
 	}
 
 	start := time.Now()
-	log.Info("starting canonical chain repair 2", "startTime", start)
+	log.Info("starting canonical chain tip repair", "startTime", start)
 	if err := vm.chain.WriteCanonicalFromCurrentBlock(vm.lastAccepted.ethBlock); err != nil {
-		return false, fmt.Errorf("canonical chain repair 2 failed after %v due to: %w", time.Since(start), err)
+		return false, fmt.Errorf("canonical chain tip repair failed after %v due to: %w", time.Since(start), err)
 	}
-	log.Info("finished canonical chain repair 2", "timeElapsed", time.Since(start))
-	if err := vm.db.Put(repairedKey2, []byte("finished")); err != nil {
-		return false, fmt.Errorf("failed to mark flag for canonical chain repair 2 due to: %w", err)
+	log.Info("finished canonical chain tip repair", "timeElapsed", time.Since(start))
+	if err := vm.db.Put(tipCanonicalRepairKey, []byte("finished")); err != nil {
+		return false, fmt.Errorf("failed to mark flag for canonical chain tip repair due to: %w", err)
 	}
 
 	return true, nil
