9f174603c1ca0e253b590b34cc21bb7a618cad60
Patrick O'Grady <prohb125@gmail.com>
2021-03-16 09:50:52 -0700

    remove random header addition

--- graft/coreth/plugin/evm/vm.go
+++ graft/coreth/plugin/evm/vm.go
@@ -4,7 +4,6 @@
 package evm
 
 import (
-	"crypto/rand"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -327,14 +326,6 @@ func (vm *VM) Initialize(
 	chain := coreth.NewETHChain(&config, &nodecfg, nil, vm.chaindb, vm.CLIConfig.EthBackendSettings())
 	vm.chain = chain
 	vm.networkID = config.NetworkId
-	chain.SetOnHeaderNew(func(header *types.Header) {
-		hid := make([]byte, 32)
-		_, err := rand.Read(hid)
-		if err != nil {
-			panic("cannot generate hid")
-		}
-		header.Extra = append(header.Extra, hid...)
-	})
 	chain.SetOnFinalizeAndAssemble(func(state *state.StateDB, txs []*types.Transaction) ([]byte, error) {
 		select {
 		case atx := <-vm.pendingAtomicTxs:
