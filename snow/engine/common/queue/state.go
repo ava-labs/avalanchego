7623ff42e25805404aef63a42304ecb8013508fc
Dan Laine <daniel.laine@avalabs.org>
2021-03-19 11:34:31 -0400

    add jobs cache

--- snow/engine/common/queue/state.go
+++ snow/engine/common/queue/state.go
@@ -12,7 +12,8 @@ import (
 )
 
 const (
-	cacheSize = 1024
+	dependentsCacheSize = 1024
+	jobsCacheSize       = 2048
 )
 
 var (
@@ -25,6 +26,7 @@ var (
 type state struct {
 	parser         Parser
 	runnableJobIDs linkeddb.LinkedDB
+	jobsCache      cache.Cacher
 	jobs           database.Database
 	// Should be prefixed with the jobID that we are attempting to find the
 	// dependencies of. This prefixdb.Database should then be wrapped in a
@@ -39,9 +41,10 @@ type state struct {
 func newState(db database.Database) *state {
 	return &state{
 		runnableJobIDs:  linkeddb.NewDefault(prefixdb.New(runnableJobIDsKey, db)),
+		jobsCache:       &cache.LRU{Size: jobsCacheSize},
 		jobs:            prefixdb.New(jobsKey, db),
 		dependencies:    prefixdb.New(dependenciesKey, db),
-		dependentsCache: &cache.LRU{Size: cacheSize},
+		dependentsCache: &cache.LRU{Size: dependentsCacheSize},
 		missingJobIDs:   linkeddb.NewDefault(prefixdb.New(missingJobIDsKey, db)),
 	}
 }
@@ -81,16 +84,23 @@ func (ps *state) RemoveRunnableJob() (Job, error) {
 // PutJob adds the job to the queue
 func (ps *state) PutJob(job Job) error {
 	id := job.ID()
+	ps.jobsCache.Put(id, job)
 	return ps.jobs.Put(id[:], job.Bytes())
 }
 
 // HasJob returns true if the job [id] is in the queue
 func (ps *state) HasJob(id ids.ID) (bool, error) {
+	if _, exists := ps.jobsCache.Get(id); exists {
+		return true, nil
+	}
 	return ps.jobs.Has(id[:])
 }
 
 // GetJob returns the job [id]
 func (ps *state) GetJob(id ids.ID) (Job, error) {
+	if job, exists := ps.jobsCache.Get(id); exists {
+		return job.(Job), nil
+	}
 	jobBytes, err := ps.jobs.Get(id[:])
 	if err != nil {
 		return nil, err
