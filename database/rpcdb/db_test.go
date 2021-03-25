3ae492fea13fd6e81a111345bcdffa9c6533bac4
Whirligig <sentient_whirligig@protonmail.com>
2021-03-25 10:09:35 +0100

    database: Potential fix for rpcdb recreation, seems heavy.

--- database/rpcdb/db_test.go
+++ database/rpcdb/db_test.go
@@ -51,32 +51,33 @@ func TestInterface(t *testing.T) {
 }
 
 func BenchmarkInterface(b *testing.B) {
-	listener := bufconn.Listen(bufSize)
-	server := grpc.NewServer()
-	rpcdbproto.RegisterDatabaseServer(server, NewServer(memdb.New()))
-	go func() {
-		if err := server.Serve(listener); err != nil {
-			log.Fatalf("Server exited with error: %v", err)
-		}
-	}()
-
-	dialer := grpc.WithContextDialer(
-		func(context.Context, string) (net.Conn, error) {
-			return listener.Dial()
-		})
-
-	ctx := context.Background()
-	conn, err := grpc.DialContext(ctx, "", dialer, grpc.WithInsecure())
-	if err != nil {
-		b.Fatalf("Failed to dial: %s", err)
-	}
-
-	defer conn.Close()
-
 	for _, bench := range database.Benchmarks {
 		for _, size := range []int{32, 64, 128, 256, 512, 1024, 2048, 4096} {
+			listener := bufconn.Listen(bufSize)
+			server := grpc.NewServer()
+			rpcdbproto.RegisterDatabaseServer(server, NewServer(memdb.New()))
+			go func() {
+				if err := server.Serve(listener); err != nil {
+					log.Fatalf("Server exited with error: %v", err)
+				}
+			}()
+
+			dialer := grpc.WithContextDialer(
+				func(context.Context, string) (net.Conn, error) {
+					return listener.Dial()
+				})
+
+			ctx := context.Background()
+			conn, err := grpc.DialContext(ctx, "", dialer, grpc.WithInsecure())
+			if err != nil {
+				b.Fatalf("Failed to dial: %s", err)
+			}
+
 			db := NewClient(rpcdbproto.NewDatabaseClient(conn))
+
 			bench(b, db, "rpcdb", 1000, size)
+
+			conn.Close()
 		}
 	}
 }
