abaae310c5f333dc97e43f0d3b135995c2eb1c44
Dan Laine <daniel.laine@avalabs.org>
2021-04-07 19:32:55 -0400

    nits

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -2,8 +2,8 @@
 
 GOPATH="$(go env GOPATH)"
 AVALANCHE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd ) # Directory above this script
-AVALANCHEGO_VER="v1.3.2"
-CORETH_VER="v0.4.0-rc.7"
+AVALANCHEGO_VER="v1.3.2" # Current version of AvalancheGo to use
+CORETH_VER="v0.4.0-rc.7" # Current version of Coreth to use
 PREV_AVALANCHEGO_VER="v1.3.1" # Last release of AvalancheGo before the database upgrade
 CORETH_PATH="$GOPATH/pkg/mod/github.com/ava-labs/coreth@$CORETH_VER"
 
@@ -15,5 +15,4 @@ BINARY_MANAGER_PATH="$BUILD_DIR/avalanchego"
 
 PREV_BUILD_DIR="$AVALANCHE_PATH/build/avalanchego-$PREV_AVALANCHEGO_VER" # Where AvalancheGo binary goes
 PREV_PLUGIN_DIR="$PREV_BUILD_DIR/plugins"
-PREV_EVM_PATH="$PLUGIN_DIR/evm"
 PREV_AVALANCHEGO_INNER_PATH="$PREV_BUILD_DIR/avalanchego-inner"
