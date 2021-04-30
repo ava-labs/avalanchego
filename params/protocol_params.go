8d0d309e12f6c0272598364de1d10da589feb174
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-04-30 03:12:47 -0400

    Update native asset call gas cost

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -153,7 +153,7 @@ const (
 	// Avalanche Stateful Precompile Params
 	// Gas price for native asset balance lookup. Based on the cost of an SLOAD operation since native
 	// asset balances are kept in state storage.
-	AssetBalanceApricot uint64 = 800
+	AssetBalanceApricot uint64 = 2100
 	// Gas price for native asset call. This gas price reflects the additional work done for the native
 	// asset transfer itself, which is a write to state storage. The cost of creating a new account and
 	// normal value transfer is assessed separately from this cost.
