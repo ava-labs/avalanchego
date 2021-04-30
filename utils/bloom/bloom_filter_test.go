d43d51bc2e27c6a9c4d4680927161ed9135b2a80
Todd Stock <todd@avalabs.org>
2021-04-30 12:43:35 -0400

    no tests

--- utils/bloom/bloom_filter_test.go
+++ utils/bloom/bloom_filter_test.go
@@ -2,8 +2,6 @@ package bloom
 
 import (
 	"testing"
-
-	streakKnife "github.com/steakknife/bloomfilter"
 )
 
 const MaxBytes = 1 * 1024 * 1024
@@ -11,10 +9,6 @@ const MaxBytes = 1 * 1024 * 1024
 func TestSteakKnifeFilterSize(t *testing.T) {
 	var maxN uint64 = 10000
 	var p = 0.1
-	m := streakKnife.OptimalM(maxN, p)
-	k := streakKnife.OptimalK(m, maxN)
-	msize := (m + 63) / 64
-	msize += k
 	f, _ := NewSteakKnifeFilter(maxN, p)
 
 	f.Add([]byte("hello"))
