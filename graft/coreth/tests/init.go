f23157c9f5859ba22308676a0abd5dc53eb658ce
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-04-30 18:39:40 -0400

    Clarify Berlin is included in ApricotPhase2 and add test configs for apricot

--- graft/coreth/tests/init.go
+++ graft/coreth/tests/init.go
@@ -163,19 +163,30 @@ var Forks = map[string]*params.ChainConfig{
 		IstanbulBlock:       big.NewInt(0),
 		YoloV3Block:         big.NewInt(0),
 	},
-	// This specification is subject to change, but is for now identical to YOLOv3
-	// for cross-client testing purposes
-	"Berlin": {
-		ChainID:             big.NewInt(1),
-		HomesteadBlock:      big.NewInt(0),
-		EIP150Block:         big.NewInt(0),
-		EIP155Block:         big.NewInt(0),
-		EIP158Block:         big.NewInt(0),
-		ByzantiumBlock:      big.NewInt(0),
-		ConstantinopleBlock: big.NewInt(0),
-		PetersburgBlock:     big.NewInt(0),
-		IstanbulBlock:       big.NewInt(0),
-		// BerlinBlock:         big.NewInt(0),
+	"ApricotPhase1": {
+		ChainID:                     big.NewInt(1),
+		HomesteadBlock:              big.NewInt(0),
+		EIP150Block:                 big.NewInt(0),
+		EIP155Block:                 big.NewInt(0),
+		EIP158Block:                 big.NewInt(0),
+		ByzantiumBlock:              big.NewInt(0),
+		ConstantinopleBlock:         big.NewInt(0),
+		PetersburgBlock:             big.NewInt(0),
+		IstanbulBlock:               big.NewInt(0),
+		ApricotPhase1BlockTimestamp: big.NewInt(0),
+	},
+	"ApricotPhase2": {
+		ChainID:                     big.NewInt(1),
+		HomesteadBlock:              big.NewInt(0),
+		EIP150Block:                 big.NewInt(0),
+		EIP155Block:                 big.NewInt(0),
+		EIP158Block:                 big.NewInt(0),
+		ByzantiumBlock:              big.NewInt(0),
+		ConstantinopleBlock:         big.NewInt(0),
+		PetersburgBlock:             big.NewInt(0),
+		IstanbulBlock:               big.NewInt(0),
+		ApricotPhase1BlockTimestamp: big.NewInt(0),
+		ApricotPhase2BlockTimestamp: big.NewInt(0),
 	},
 }
 
