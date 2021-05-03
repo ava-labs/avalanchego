1de1e13fa80f4af8d4eea95c8ebce7e22d37fac3
Pedro Gomes <otherview@gmail.com>
2021-05-03 17:07:29 +0100

    using a pushed image

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,4 +2,4 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'kurtosis_upgrade'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.3.2'}
