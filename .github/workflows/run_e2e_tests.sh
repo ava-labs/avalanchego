8299a921fbe08b7bcb247b71e0e97a9a95f6865e
Pedro Gomes <otherview@gmail.com>
2021-05-03 16:38:32 +0100

    first push

---
+++ .github/workflows/run_e2e_tests.sh
@@ -0,0 +1,77 @@
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Testing specific variables
+avalanche_testing_repo="avaplatform/avalanche-testing"
+
+# Define avalanche testing version to use
+avalanche_testing_image="avaplatform/avalanche-testing:master"
+
+# Fetch the images
+# If Docker Credentials are not available fail
+if [[ -z ${DOCKER_USERNAME} ]]; then
+    echo "Skipping Tests because Docker Credentials were not present."
+    exit 1
+fi
+
+# Avalanche root directory
+AVALANCHE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd ../.. && pwd )
+
+# Load the versions
+source "$AVALANCHE_PATH"/scripts/versions.sh
+
+# Load the constants
+source "$AVALANCHE_PATH"/scripts/constants.sh
+
+# Login to docker
+echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+# Checks available docker tags exist
+function docker_tag_exists() {
+    TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USERNAME}'", "password": "'${DOCKER_PASS}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+    curl --silent -H "Authorization: JWT ${TOKEN}" -f --head -lL https://hub.docker.com/v2/repositories/$1/tags/$2/ > /dev/null
+}
+
+# Defines the avalanche-testing tag to use
+# Either uses the same tag as the current branch or uses the default
+if docker_tag_exists $avalanche_testing_repo $current_branch; then
+    echo "$avalanche_testing_repo:$current_branch exists; using this image to run e2e tests"
+    avalanche_testing_image="$avalanche_testing_repo:$current_branch"
+else
+    echo "$avalanche_testing_repo $current_branch does NOT exist; using the default image to run e2e tests"
+fi
+
+echo "Using $avalanche_testing_image for e2e tests"
+
+# pulling the avalanche-testing image
+docker pull $avalanche_testing_image
+
+# Setting the build ID
+git_commit_id=$( git rev-list -1 HEAD )
+
+# Build current avalanchego
+"$AVALANCHE_PATH"/scripts/build_image.sh
+
+# Target built version to use in avalanche-testing
+avalanche_image="avaplatform/avalanchego:$current_branch"
+
+echo "Running Avalanche Image: ${avalanche_image}"
+echo "Running Avalanche Image Tag: $current_branch"
+echo "Running Avalanche Testing Image: ${avalanche_testing_image}"
+echo "Git Commit ID : ${git_commit_id}"
+
+
+# >>>>>>>> avalanche-testing custom parameters <<<<<<<<<<<<<
+custom_params_json="{
+    \"isKurtosisCoreDevMode\": false,
+    \"avalanchegoImage\":\"${avalanche_image}\",
+    \"avalanchegoByzantineImage\":\"\"
+}"
+# >>>>>>>> avalanche-testing custom parameters <<<<<<<<<<<<<
+
+bash "$AVALANCHE_PATH/.kurtosis/kurtosis.sh" \
+    --tests "Virtuous CChain Tests,PChain WorkFlow" \
+    --custom-params "${custom_params_json}" \
+    "${avalanche_testing_image}" \
+    $@
--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -4,16 +4,22 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Build AvalancheGo image using local verion of coreth and the AvalancheGo version set here.
-SRC_DIR="$(dirname "${BASH_SOURCE[0]}")"
-AVALANCHE_VERSION="v1.3.0"
-CORETH_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd ) # Directory above this script
-DOCKERHUB_REPO="avaplatform/avalanchego"
+# Avalanche root directory
+AVALANCHE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
+
+# Load the versions
+source "$AVALANCHE_PATH"/scripts/versions.sh
+
+# Load the constants
+source "$AVALANCHE_PATH"/scripts/constants.sh
 
 # WARNING: this will use the most recent commit even if there are un-committed changes present
-FULL_CORETH_COMMIT="$(git --git-dir="$CORETH_PATH/.git" rev-parse HEAD)"
+FULL_CORETH_COMMIT="$(git --git-dir="$AVALANCHE_PATH/.git" rev-parse HEAD)"
 # Use an abbreviated version of the full commit to tag the image.
 CORETH_COMMIT="${FULL_CORETH_COMMIT::8}"
 
-echo "Building Docker Image: $DOCKERHUB_REPO:$AVALANCHE_VERSION-$CORETH_COMMIT"
-docker build -t "$DOCKERHUB_REPO:$AVALANCHE_VERSION-$CORETH_COMMIT" "$CORETH_PATH" -f "$CORETH_PATH/Dockerfile" --build-arg AVALANCHE_VERSION="$AVALANCHE_VERSION" --build-arg CORETH_COMMIT="$FULL_CORETH_COMMIT"
+echo "Building Docker Image: $dockerhub_repo:$avalanche_version-$CORETH_COMMIT"
+docker build -t "$dockerhub_repo:$avalanche_version-$CORETH_COMMIT" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile" \
+  --build-arg AVALANCHE_VERSION="$avalanche_version" \
+  --build-arg CORETH_COMMIT="$FULL_CORETH_COMMIT" \
+  --build-arg CURRENT_BRANCH="$current_branch"
