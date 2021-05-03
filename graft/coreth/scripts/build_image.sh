725a980189452354f330f5568fa4459ee987cf57
Pedro Gomes <otherview@gmail.com>
2021-05-03 17:30:01 +0100

    using locally built image name

--- graft/coreth/scripts/build_image.sh
+++ graft/coreth/scripts/build_image.sh
@@ -13,13 +13,8 @@ source "$AVALANCHE_PATH"/scripts/versions.sh
 # Load the constants
 source "$AVALANCHE_PATH"/scripts/constants.sh
 
-# WARNING: this will use the most recent commit even if there are un-committed changes present
-FULL_CORETH_COMMIT="$(git --git-dir="$AVALANCHE_PATH/.git" rev-parse HEAD)"
-# Use an abbreviated version of the full commit to tag the image.
-CORETH_COMMIT="${FULL_CORETH_COMMIT::8}"
-
-echo "Building Docker Image: $dockerhub_repo:$avalanche_version-$CORETH_COMMIT"
-docker build -t "$dockerhub_repo:$avalanche_version-$CORETH_COMMIT" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile" \
+echo "Building Docker Image: $dockerhub_repo:$build_image_id"
+docker build -t "$dockerhub_repo:$build_image_id" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile" \
   --build-arg AVALANCHE_VERSION="$avalanche_version" \
-  --build-arg CORETH_COMMIT="$FULL_CORETH_COMMIT" \
+  --build-arg CORETH_COMMIT="$coreth_commit" \
   --build-arg CURRENT_BRANCH="$current_branch"
--- graft/coreth/scripts/constants.sh
+++ graft/coreth/scripts/constants.sh
@@ -12,3 +12,12 @@ dockerhub_repo="avaplatform/avalanchego"
 # Current branch
 current_branch=${CURRENT_BRANCH:-$(git branch --show-current)}
 echo "Using branch: ${current_branch}"
+
+# Image build id
+# Use an abbreviated version of the full commit to tag the image.
+
+# WARNING: this will use the most recent commit even if there are un-committed changes present
+coreth_commit="$(git --git-dir="$AVALANCHE_PATH/.git" rev-parse HEAD)"
+coreth_commit_id="${coreth_commit::8}"
+
+build_image_id=${BUILD_IMAGE_ID:-"$avalanche_version-$coreth_commit_id"}
