b996eabac0d4ba224e34ea4dc072aec8e49c0106
Pedro Gomes <otherview@gmail.com>
2021-05-03 16:38:32 +0100

    first push

---
+++ graft/coreth/scripts/constants.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Set the PATHS
+GOPATH="$(go env GOPATH)"
+
+# Set default binary location
+binary_path="$GOPATH/src/github.com/ava-labs/avalanchego/build/plugins/evm"
+
+# Avalabs docker hub
+dockerhub_repo="avaplatform/avalanchego"
+
+# Current branch
+current_branch=${CURRENT_BRANCH:-$(git branch --show-current)}
---
+++ graft/coreth/scripts/versions.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Set up the versions to be used
+# Don't export them as their used in the context of other calls
+avalanche_version=${AVALANCHE_VERSION:-'kurtosis_upgrade'}
