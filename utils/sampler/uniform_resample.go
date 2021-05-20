9acf81f842e1775cf302ebbdf22a03dde85172d2
StephenButtolph <stephen@avalabs.org>
2021-05-20 00:25:40 -0400

    first pass of repeatable sampling

--- utils/sampler/uniform_resample.go
+++ utils/sampler/uniform_resample.go
@@ -30,6 +30,7 @@ func (s *uniformResample) Initialize(length uint64) error {
 		return errOutOfRange
 	}
 	s.length = length
+	s.drawn = make(map[uint64]struct{})
 	return nil
 }
 
@@ -38,12 +39,8 @@ func (s *uniformResample) Sample(count int) ([]uint64, error) {
 		return nil, errOutOfRange
 	}
 
-	if s.drawn == nil {
-		s.drawn = make(map[uint64]struct{}, count)
-	} else {
-		for k := range s.drawn {
-			delete(s.drawn, k)
-		}
+	for k := range s.drawn {
+		delete(s.drawn, k)
 	}
 
 	results := make([]uint64, count)
@@ -61,4 +58,28 @@ func (s *uniformResample) Sample(count int) ([]uint64, error) {
 	}
 
 	return results, nil
+}
+
+func (s *uniformResample) Reset() {
+	for k := range s.drawn {
+		delete(s.drawn, k)
+	}
+}
+
+func (s *uniformResample) Next() (uint64, error) {
+	i := uint64(len(s.drawn))
+	if i >= s.length {
+		return 0, errOutOfRange
+	}
+
+	for {
+		// We don't use a cryptographically secure source of randomness here, as
+		// there's no need to ensure a truly random sampling.
+		draw := uint64(rand.Int63n(int64(s.length))) // #nosec G404
+		if _, ok := s.drawn[draw]; ok {
+			continue
+		}
+		s.drawn[draw] = struct{}{}
+		return draw, nil
+	}
 }
