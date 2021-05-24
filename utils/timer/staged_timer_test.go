850325f610fe9d988c2366a44df76fa172b78856
Ceyhun Onur <ceyhunonur54@gmail.com>
2021-05-24 18:52:14 +0300

    increase ms in tests & use channels for errors

--- utils/timer/staged_timer_test.go
+++ utils/timer/staged_timer_test.go
@@ -4,11 +4,12 @@
 package timer
 
 import (
+	"fmt"
 	"sync"
 	"testing"
 	"time"
 
-	"gotest.tools/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSingleStagedTimer(t *testing.T) {
@@ -47,11 +48,12 @@ func TestMultiStageTimer(t *testing.T) {
 }
 
 func TestCancelSimpleStagedStimer(t *testing.T) {
+	fatalErrors := make(chan error)
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 
 	msTimer := NewStagedTimer(func() (time.Duration, bool) {
-		t.Fatal("Timer should have been canceled before being called")
+		fatalErrors <- fmt.Errorf("Timer should have been canceled before being called")
 		return 0, false
 	})
 	cancelTimer := NewTimer(func() {
@@ -64,32 +66,40 @@ func TestCancelSimpleStagedStimer(t *testing.T) {
 
 	// Set [msTimer] with a larger timeout than [cancelTimer]. This should cause
 	// [msTimer] to be cancelled before being called.
-	msTimer.SetTimeoutIn(5 * time.Millisecond)
+	msTimer.SetTimeoutIn(20 * time.Millisecond)
 	cancelTimer.SetTimeoutIn(time.Millisecond)
 
 	wg.Wait()
-	// Sleep for 10 milliseconds, so that if cancellation was not successful
+	// Sleep for 25 milliseconds, so that if cancellation was not successful
 	// the timeout will fire and cause the test to fail.
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(25 * time.Millisecond)
+
+	select {
+	case err := <-fatalErrors:
+		close(fatalErrors)
+		assert.NoError(t, err)
+	default:
+	}
 }
 
 func TestCancelStagedTimer(t *testing.T) {
+	fatalErrors := make(chan error)
 	wg := sync.WaitGroup{}
 	// Add 2 to waitgroup, as we expect one tick from the [msTimer] and
 	// a second tick from [cancelTimer].
 	wg.Add(2)
-
 	i := 0
 
 	// Set [msTimer] to go off successfully once, and fail on the second
 	// callback.
 	msTimer := NewStagedTimer(func() (time.Duration, bool) {
 		if i > 0 {
-			t.Fatal("Timer should have been cancelled before second callback")
+			fatalErrors <- fmt.Errorf("Timer should have been canceled before being called")
+			return 0, false // no need to call it again
 		}
 		defer wg.Done()
 		i++
-		return 10 * time.Millisecond, true
+		return 20 * time.Millisecond, true
 	})
 	cancelTimer := NewTimer(func() {
 		defer wg.Done()
@@ -102,10 +112,16 @@ func TestCancelStagedTimer(t *testing.T) {
 	// Set [msTimer] with a shorter timeout, so it fires once and then [cancelTimer]
 	// should stop the second callback.
 	msTimer.SetTimeoutIn(time.Millisecond)
-	cancelTimer.SetTimeoutIn(5 * time.Millisecond)
+	cancelTimer.SetTimeoutIn(10 * time.Millisecond)
 
 	wg.Wait()
-	// Sleep for 10 milliseconds, so that if cancellation was not successful
+	// Sleep for 25 milliseconds, so that if cancellation was not successful
 	// the timeout will fire and cause the test to fail.
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(25 * time.Millisecond)
+	select {
+	case err := <-fatalErrors:
+		close(fatalErrors)
+		assert.NoError(t, err)
+	default:
+	}
 }
