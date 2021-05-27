d7b02cf1cd1f800e4309ab3cee28402d44cd60b9
Manthan Dave <manthan.dave@avalabs.org>
2021-05-27 18:18:30 +0100

    AV-502 Limit number of outbound connections a node tries to open at once
    Polymorphism updates

--- network/throttler.go
+++ network/throttler.go
@@ -7,71 +7,107 @@ import (
 	"time"
 )
 
+type BackoffPolicy interface {
+	Backoff(attempt int)
+}
+
+type StaticBackoffPolicy struct {
+	backoffDuration time.Duration
+}
+
+func (p StaticBackoffPolicy) GetBackoffDuration() time.Duration {
+	return p.backoffDuration
+}
+
+func (p StaticBackoffPolicy) Backoff(_ int) {
+	time.Sleep(p.GetBackoffDuration())
+}
+
+type IncrementalBackoffPolicy struct {
+	backoffDuration   time.Duration
+	incrementDuration time.Duration
+}
+
+func (n IncrementalBackoffPolicy) GetBackoffDuration(attempt int) time.Duration {
+	incrementDurationMillis := n.GetIncrementDuration().Milliseconds()
+	backoffDurationMillis := n.backoffDuration.Milliseconds()
+	sleepMillis := backoffDurationMillis + int64(math.Pow(float64(incrementDurationMillis), float64(attempt)))
+	return time.Duration(sleepMillis) * time.Millisecond
+}
+
+func (n IncrementalBackoffPolicy) GetIncrementDuration() time.Duration {
+	return n.incrementDuration
+}
+
+func (n IncrementalBackoffPolicy) Backoff(attempt int) {
+	time.Sleep(n.GetBackoffDuration(attempt))
+}
+
+type RandomisedBackoffPolicy struct {
+	minDuration time.Duration
+	maxDuration time.Duration
+}
+
+// GetBackoffDuration If this function is called outside of the `Backoff` method, its value
+// (randomised) is not the one to be used when the actual Backoff happens since the Backoff method
+// calls this internally.
+func (r RandomisedBackoffPolicy) GetBackoffDuration() time.Duration {
+	randMillis := rand.Float64() * float64(r.maxDuration-r.minDuration)
+	return r.minDuration + time.Duration(randMillis)
+}
+
+func (r RandomisedBackoffPolicy) Backoff(_ int) {
+	time.Sleep(r.GetBackoffDuration())
+}
+
 type Throttler interface {
 	Acquire()
 }
 
 type BlockingThrottler struct {
-	limiter   *rate.Limiter
-	onBackOff func(int)
+	limiter       *rate.Limiter
+	backoffPolicy BackoffPolicy
 }
 
 func (t BlockingThrottler) Acquire() {
-	attempt := 0
+	attempt := 1
 	for {
 		if t.limiter.Allow() {
 			break
 		}
 
+		t.backoffPolicy.Backoff(attempt)
 		attempt += 1
-		t.onBackOff(attempt)
 	}
 }
 
-func NewThrottler(throttleLimit int, onBackOffFn func(int)) Throttler {
+func NewThrottler(throttleLimit int, backoffPolicy BackoffPolicy) Throttler {
 	return BlockingThrottler{
-		limiter:   rate.NewLimiter(rate.Limit(throttleLimit), throttleLimit),
-		onBackOff: onBackOffFn,
+		limiter:       rate.NewLimiter(rate.Limit(throttleLimit), throttleLimit),
+		backoffPolicy: backoffPolicy,
 	}
 }
 
 func NewStaticBackoffThrottler(throttleLimit int, backOffDuration time.Duration) Throttler {
 	return BlockingThrottler{
-		limiter:   rate.NewLimiter(rate.Limit(throttleLimit), throttleLimit),
-		onBackOff: staticBackoffFn(backOffDuration),
-	}
-}
-
-func staticBackoffFn(backOffDuration time.Duration) func(attempt int) {
-	return func(_ int) {
-		time.Sleep(backOffDuration)
+		limiter:       rate.NewLimiter(rate.Limit(throttleLimit), throttleLimit),
+		backoffPolicy: StaticBackoffPolicy{backoffDuration: backOffDuration},
 	}
 }
 
 func NewIncrementalBackoffThrottler(throttleLimit int, backOffDuration time.Duration, incrementDuration time.Duration) Throttler {
 	return BlockingThrottler{
-		limiter:   rate.NewLimiter(rate.Limit(throttleLimit), throttleLimit),
-		onBackOff: incrementalBackoffFn(backOffDuration, incrementDuration),
-	}
-}
-
-func incrementalBackoffFn(backOffDuration time.Duration, incrementDuration time.Duration) func(attempt int) {
-	return func(attempt int) {
-		sleepMillis := float64(backOffDuration.Milliseconds()) + math.Pow(float64(incrementDuration.Milliseconds()), float64(attempt))
-		time.Sleep(time.Duration(sleepMillis) * time.Millisecond)
+		limiter:       rate.NewLimiter(rate.Limit(throttleLimit), throttleLimit),
+		backoffPolicy: IncrementalBackoffPolicy{backoffDuration: backOffDuration, incrementDuration: incrementDuration},
 	}
 }
 
 func NewRandomisedBackoffThrottler(throttleLimit int, minDuration, maxDuration time.Duration) Throttler {
 	return BlockingThrottler{
-		limiter:   rate.NewLimiter(rate.Limit(throttleLimit), throttleLimit),
-		onBackOff: randomisedBackoffFn(maxDuration, minDuration),
-	}
-}
-
-func randomisedBackoffFn(maxDuration time.Duration, minDuration time.Duration) func(attempt int) {
-	return func(attempt int) {
-		randMillis := (rand.Int63() * (maxDuration.Milliseconds() - minDuration.Milliseconds())) + minDuration.Milliseconds()
-		time.Sleep(time.Duration(randMillis) * time.Millisecond)
+		limiter: rate.NewLimiter(rate.Limit(throttleLimit), throttleLimit),
+		backoffPolicy: RandomisedBackoffPolicy{
+			minDuration: minDuration,
+			maxDuration: maxDuration,
+		},
 	}
 }
--- network/throttler_test.go
+++ network/throttler_test.go
@@ -7,6 +7,25 @@ import (
 	"time"
 )
 
+func TestStaticBackoffPolicy_Backoff(t *testing.T) {
+	policy := StaticBackoffPolicy{backoffDuration: 1 * time.Second}
+	backoffDuration := policy.GetBackoffDuration()
+	if backoffDuration != 1*time.Second {
+		t.Fatalf("Expected backoff duration to be static 1 second")
+	}
+}
+
+func TestIncrementalBackoffPolicy(t *testing.T) {
+	policy := IncrementalBackoffPolicy{
+		backoffDuration:   3 * time.Second,
+		incrementDuration: 5 * time.Second,
+	}
+	attempt0Duration := policy.GetBackoffDuration(0)
+	assert.Equal(t, (3*time.Second)+(1*time.Millisecond), attempt0Duration)
+	attempt1Duration := policy.GetBackoffDuration(1)
+	assert.Equal(t, (3*time.Second)+(5*time.Second), attempt1Duration)
+}
+
 func TestAcquireLock(t *testing.T) {
 	thr := NewStaticBackoffThrottler(3, time.Duration(2)*time.Second)
 	t1 := time.Now()
