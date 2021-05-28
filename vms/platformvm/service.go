290425695f3de0f8699887fd3d7e0638b7667d8e
Dan Laine <daniel.laine@avalabs.org>
2021-05-28 12:13:10 -0400

    fix errant warning

--- vms/platformvm/service.go
+++ vms/platformvm/service.go
@@ -2089,8 +2089,10 @@ func (service *Service) getStakeHelper(tx *Tx, addrs ids.ShortSet) (uint64, []av
 		outs = staker.Stake
 	case *UnsignedAddValidatorTx:
 		outs = staker.Stake
+	case *UnsignedAddSubnetValidatorTx:
+		return 0, nil, nil
 	default:
-		service.vm.ctx.Log.Warn("expected *UnsignedAddDelegatorTx or *UnsignedAddValidatorTx but got %T", tx.UnsignedTx)
+		service.vm.ctx.Log.Warn("expected *UnsignedAddDelegatorTx, *UnsignedAddValidatorTx or *UnsignedAddSubnetValidatorTx but got %T", tx.UnsignedTx)
 	}
 
 	var (
