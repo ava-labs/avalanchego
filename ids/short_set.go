8a7027f46f0366be1cdd112f85d470a56c3a9cfd
StephenButtolph <stephen@avalabs.org>
2021-06-09 11:31:13 -0400

    improved set comment

--- ids/short_set.go
+++ ids/short_set.go
@@ -130,8 +130,8 @@ func (ids ShortSet) String() string {
 	return sb.String()
 }
 
-// Removes and returns an element at random, if any exist.
-// If none exist, does nothing and returns false.
+// Removes and returns an element. If the set is empty, does nothing and returns
+// false
 func (ids *ShortSet) Pop() (ShortID, bool) {
 	for id := range *ids {
 		delete(*ids, id)
