9780fe784ba9d14a6f0bc70f97fa45932457c138
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-06-14 18:38:41 -0400

    Remove unused lint script and extra space

--- .github/workflows/lint.sh
+++
@@ -1,8 +0,0 @@
-# binary will be $(go env GOPATH)/bin/golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.30.0
-
-export PATH=$PATH:$(go env GOPATH)/bin
-
-golangci-lint --version
-
-golangci-lint run --max-same-issues 0
