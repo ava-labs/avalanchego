a1b0731a176c14ba625e2e8da1374af190703a17
manthanhd <manthanhd@live.com>
2021-06-21 12:31:50 +0100

    Add tests for IsCompressable and IsCompressed

--- network/compression_test.go
+++ network/compression_test.go
@@ -19,6 +19,26 @@ func TestCompressDecompress(t *testing.T) {
 	assert.EqualValues(t, data, decompressedBytes)
 }
 
+func TestGzipCompressor_IsCompressable(t *testing.T) {
+	compressor := NewCompressor()
+	data := "abc123"
+	assert.False(t, compressor.IsCompressable([]byte(data)))
+
+	data = randomString(1000)
+	assert.True(t, compressor.IsCompressable([]byte(data)))
+}
+
+func TestGzipCompressor_IsCompressed(t *testing.T) {
+	compressor := NewCompressor()
+	data := randomString(128)
+	dataBytes := []byte(data)
+	assert.False(t, compressor.IsCompressed(dataBytes))
+
+	cmpBytes, err := compressor.Compress(dataBytes)
+	assert.NoError(t, err)
+	assert.True(t, compressor.IsCompressed(cmpBytes))
+}
+
 func randomString(n int) string {
 	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !%$*#@|/.,<>?[]{}-=_+()&^")
 
