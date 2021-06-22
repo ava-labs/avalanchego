e87645604c9f43a1bdb9a4fbe216202064d7b9e2
Dan Laine <daniel.laine@avalabs.org>
2021-06-22 13:33:58 -0400

    add test

--- network/compression_test.go
+++ network/compression_test.go
@@ -10,16 +10,33 @@ import (
 func TestCompressDecompress(t *testing.T) {
 	data := make([]byte, 4096)
 	for i := 0; i < len(data); i++ {
-		data[i] = byte(rand.Intn(256))
+		data[i] = byte(rand.Intn(256)) // #nosec G404
+	}
+
+	data2 := make([]byte, 4096)
+	for i := 0; i < len(data); i++ {
+		data2[i] = byte(rand.Intn(256)) // #nosec G404
 	}
 
 	compressor := NewGzipCompressor(minCompressSize)
-	compressedBytes, err := compressor.Compress(data)
+
+	dataCompressed, err := compressor.Compress(data)
 	assert.NoError(t, err)
 
-	decompressedBytes, err := compressor.Decompress(compressedBytes)
+	data2Compressed, err := compressor.Compress(data2)
 	assert.NoError(t, err)
-	assert.EqualValues(t, data, decompressedBytes)
+
+	dataDecompressed, err := compressor.Decompress(dataCompressed)
+	assert.NoError(t, err)
+	assert.EqualValues(t, data, dataDecompressed)
+
+	data2Decompressed, err := compressor.Decompress(data2Compressed)
+	assert.NoError(t, err)
+	assert.EqualValues(t, data2, data2Decompressed)
+
+	nonGzipData := []byte{1, 2, 3}
+	_, err = compressor.Decompress(nonGzipData)
+	assert.Error(t, err)
 }
 
 func TestNoCompressor(t *testing.T) {
