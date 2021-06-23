6a59c002833891f140634b2babd1652026b11246
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-06-23 17:35:27 -0400

    Bump avalanchego base image to v1.4.8

--- graft/coreth/scripts/versions.sh
+++ graft/coreth/scripts/versions.sh
@@ -2,4 +2,4 @@
 
 # Set up the versions to be used
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.4.7'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.4.8'}
