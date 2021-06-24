4ab1467cb396abd370f21fa328edfe82b108e7a0
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-06-24 12:26:06 -0400

    Bump coreth to v0.5.4-rc.8

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,6 +2,6 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-coreth_version=${CORETH_VERSION:-'v0.5.4-rc.7'}
+coreth_version=${CORETH_VERSION:-'v0.5.4-rc.8'}
 # Release of AvalancheGo compatible with previous database version
 prev_avalanchego_version=${PREV_AVALANCHEGO_VERSION:-'v1.4.5-preupgrade.4' }
