62fef8e39846cd69f5cc8b362ea30b5ce8036a35
manthanhd <manthanhd@live.com>
2021-06-25 15:39:46 +0100

    Introduce AddressTxsIndexer struct

--- vms/avm/index.go
+++ vms/avm/index.go
@@ -6,6 +6,8 @@ package avm
 import (
 	"encoding/binary"
 
+	"github.com/ava-labs/avalanchego/database/versiondb"
+	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/vms/components/avax"
 
 	"github.com/ava-labs/avalanchego/database"
@@ -16,6 +18,92 @@ import (
 )
 
 var IndexingEnabled = false
+
+// AddressTxsIndexer indexes transactions for given an address and an assetID
+type AddressTxsIndexer struct {
+	// Maps address -> []AssetID Set
+	addressAssetIDTxMap map[ids.ShortID]map[ids.ID]struct{}
+	db                  *versiondb.Database
+	logger              logging.Logger
+}
+
+func (i *AddressTxsIndexer) AddTransferOutput(assetID ids.ID, transferOutput *secp256k1fx.TransferOutput) {
+	for _, address := range transferOutput.Addrs {
+		if _, exists := i.addressAssetIDTxMap[address]; !exists {
+			i.addressAssetIDTxMap[address] = make(map[ids.ID]struct{})
+		}
+		i.addressAssetIDTxMap[address][assetID] = struct{}{}
+	}
+}
+
+func (i *AddressTxsIndexer) AddUTXOs(outputUTXOs []*avax.UTXO) {
+	for _, utxo := range outputUTXOs {
+		out, ok := utxo.Out.(*secp256k1fx.TransferOutput)
+		if !ok {
+			i.logger.Debug("Skipping output utxo %s for export indexing because it is not of secp256k1fx.TransferOutput", utxo.InputID().String())
+			continue
+		}
+
+		i.AddTransferOutput(utxo.AssetID(), out)
+	}
+}
+
+// this feels weird
+func (i *AddressTxsIndexer) CommitIndex(txID ids.ID) error {
+	for address, assetIDMap := range i.addressAssetIDTxMap {
+		addressPrefixDB := prefixdb.New(address[:], i.db)
+		for assetID := range assetIDMap {
+			assetPrefixDB := prefixdb.New(assetID[:], addressPrefixDB)
+
+			var idx uint64
+			idxBytes, err := assetPrefixDB.Get(idxKey)
+			switch {
+			case err != nil && err != database.ErrNotFound:
+				// Unexpected error
+				i.logger.Fatal("Error checking idx value exists: %s", err)
+				return err
+			case err == database.ErrNotFound:
+				// idx not found; this must be the first entry.
+				idx = 0
+				idxBytes = make([]byte, wrappers.LongLen)
+				binary.BigEndian.PutUint64(idxBytes, idx)
+			default:
+				// Parse [idxBytes]
+				idx = binary.BigEndian.Uint64(idxBytes)
+				i.logger.Debug("fetched index %d", idx)
+			}
+
+			i.logger.Debug("Writing at index %d txID %s", idx, txID)
+			if err := assetPrefixDB.Put(idxBytes, txID[:]); err != nil {
+				i.logger.Fatal("Failed to save transaction to the address, assetID prefix DB %s", err)
+				return err
+			}
+
+			// increment and store the index for next use
+			idx++
+			binary.BigEndian.PutUint64(idxBytes, idx)
+			i.logger.Debug("New index %d", idx)
+
+			if err := assetPrefixDB.Put(idxKey, idxBytes); err != nil {
+				i.logger.Fatal("Failed to save transaction index to the address, assetID prefix DB: %s", err)
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (i *AddressTxsIndexer) Reset() {
+	i.addressAssetIDTxMap = make(map[ids.ShortID]map[ids.ID]struct{})
+}
+
+func NewAddressTxsIndexer(db *versiondb.Database, logger logging.Logger) *AddressTxsIndexer {
+	return &AddressTxsIndexer{
+		addressAssetIDTxMap: make(map[ids.ShortID]map[ids.ID]struct{}),
+		db:                  db,
+		logger:              logger,
+	}
+}
 
 // CommitIndex commits given addresses map to the database. The database structure is thus:
 // [address]
