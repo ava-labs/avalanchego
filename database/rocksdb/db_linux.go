00101c1d1d8851d872f7953d0bf9c39234ea8be8
StephenButtolph <stephen@avalabs.org>
2021-07-01 16:53:57 -0400

    restrict rocksdb to linux

--- database/rocksdb/db_linux.go
+++ database/rocksdb/db_linux.go
@@ -1,4 +1,4 @@
-// +build !windows
+// +build linux
 
 // (c) 2019-2020, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
--- database/rocksdb/db_linux_test.go
+++ database/rocksdb/db_linux_test.go
@@ -1,3 +1,5 @@
+// +build linux
+
 // (c) 2019-2020, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
