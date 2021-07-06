742002aedf8fead5ea548e8191a9b3256e0d74ad
Dan Laine <daniel.laine@avalabs.org>
2021-07-06 17:07:10 -0400

    attach node id as header node-id in API responses

--- node/node.go
+++ node/node.go
@@ -493,6 +493,7 @@ func (n *Node) initAPIServer() error {
 			n.Config.HTTPHost,
 			n.Config.HTTPPort,
 			n.Config.APIAllowedOrigins,
+			n.ID,
 		)
 		return nil
 	}
@@ -508,6 +509,7 @@ func (n *Node) initAPIServer() error {
 		n.Config.HTTPHost,
 		n.Config.HTTPPort,
 		n.Config.APIAllowedOrigins,
+		n.ID,
 		a,
 	)
 
