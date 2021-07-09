295db29e04a990e4bae891a779028d3e2f4f3dad
StephenButtolph <stephen@avalabs.org>
2021-07-09 16:01:00 -0400

    attempt to connect to beacons - regardless of if we think they are validators or not

--- node/node.go
+++ node/node.go
@@ -12,6 +12,8 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/hashicorp/go-plugin"
+
 	"github.com/ava-labs/avalanchego/api/admin"
 	"github.com/ava-labs/avalanchego/api/auth"
 	"github.com/ava-labs/avalanchego/api/health"
@@ -54,7 +56,6 @@ import (
 	"github.com/ava-labs/avalanchego/vms/propertyfx"
 	"github.com/ava-labs/avalanchego/vms/rpcchainvm"
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
-	"github.com/hashicorp/go-plugin"
 
 	ipcsapi "github.com/ava-labs/avalanchego/api/ipcs"
 )
