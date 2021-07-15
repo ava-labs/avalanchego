d43f3ab267441b893221e9617c099bfefcc136ff
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-07-15 11:44:44 -0400

    Bump version to v0.5.6

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.5.5'}
+coreth_version=${CORETH_VERSION:-'v0.5.6'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.4.10-rc.3'}
