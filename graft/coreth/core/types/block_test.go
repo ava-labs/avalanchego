68fdca27a2af6df7dae0c856f60d5f1d19c4a22a
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-07-15 11:47:43 -0400

    Update block test

--- graft/coreth/core/types/block_test.go
+++ graft/coreth/core/types/block_test.go
@@ -110,7 +110,7 @@ func TestEIP1559BlockEncoding(t *testing.T) {
 	check("Nonce", block.Nonce(), uint64(0xa13a5a8c8f2bb1c4))
 	check("Time", block.Time(), uint64(1426516743))
 	check("Size", block.Size(), common.StorageSize(len(blockEnc)))
-	check("BaseFee", block.BaseFee(), new(big.Int).SetUint64(params.InitialBaseFee))
+	check("BaseFee", block.BaseFee(), new(big.Int).SetUint64(1_000_000_000))
 
 	tx1 := NewTransaction(0, common.HexToAddress("095e7baea6a6c7c4c2dfeb977efac326af552d87"), big.NewInt(10), 50000, big.NewInt(10), nil)
 	tx1, _ = tx1.WithSignature(HomesteadSigner{}, common.Hex2Bytes("9bea4c4daac7c7c52e093e6a4c35dbbcf8856f1af7b059ba20253e70848d094f8a8fae537ce25ed8cb5af9adac3f141af69bd515bd2ba031522df09b97dd72b100"))
