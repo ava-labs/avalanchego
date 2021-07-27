9e92fbb74106498667e32173873654e9ac78971c
Patrick O'Grady <prohb125@gmail.com>
2021-07-26 22:23:03 -0700

    don't do compaction

--- core/state/snapshot/wipe_test.go
+++ core/state/snapshot/wipe_test.go
@@ -99,7 +99,7 @@ func TestWipe(t *testing.T) {
 		t.Errorf("snapshot block root marker mismatch: have %#x, want <not-nil>", hash)
 	}
 	// Wipe all snapshot entries from the database
-	<-wipeSnapshot(db, true)
+	<-wipeSnapshot(db, true, true)
 
 	// Iterate over the database end ensure no snapshot information remains
 	it = db.NewIterator(rawdb.SnapshotAccountPrefix, nil)
