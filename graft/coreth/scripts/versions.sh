e558dbc35c88df42f53020987104099a103d714e
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-07-29 16:22:20 -0400

    Bump avalanchego to v1.4.12-rc.1

--- graft/coreth/scripts/versions.sh
+++ graft/coreth/scripts/versions.sh
@@ -3,4 +3,4 @@
 # Set up the versions to be used
 coreth_version=${CORETH_VERSION:-'v0.5.7'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.4.12-rc.0'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.4.12-rc.1'}
