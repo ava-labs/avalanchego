4166c966c3e8223bc0238e918d7e3e5596457fe1
alberto.benegiamo <alberto.benegiamo@gmail.com>
2021-08-04 15:20:50 +0200

    first pass at C-Chain mempool gossiping

--- graft/coreth/plugin/evm/import_tx_test.go
+++ graft/coreth/plugin/evm/import_tx_test.go
@@ -157,7 +157,7 @@ func TestImportTxVerify(t *testing.T) {
 }
 
 func TestImportTxSemanticVerifyApricotPhase0(t *testing.T) {
-	_, vm, _, sharedMemory := GenesisVM(t, false, genesisJSONApricotPhase0, "", "")
+	_, vm, _, sharedMemory, _ := GenesisVM(t, false, genesisJSONApricotPhase0, "", "")
 
 	defer func() {
 		if err := vm.Shutdown(); err != nil {
@@ -337,7 +337,7 @@ func TestImportTxSemanticVerifyApricotPhase0(t *testing.T) {
 }
 
 func TestImportTxSemanticVerifyApricotPhase2(t *testing.T) {
-	_, vm, _, sharedMemory := GenesisVM(t, false, genesisJSONApricotPhase2, "", "")
+	_, vm, _, sharedMemory, _ := GenesisVM(t, false, genesisJSONApricotPhase2, "", "")
 
 	defer func() {
 		if err := vm.Shutdown(); err != nil {
@@ -552,7 +552,7 @@ func TestNewImportTx(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			_, vm, _, sharedMemory := GenesisVM(t, true, genesisJSONApricotPhase2, "", "")
+			_, vm, _, sharedMemory, _ := GenesisVM(t, true, genesisJSONApricotPhase2, "", "")
 
 			defer func() {
 				if err := vm.Shutdown(); err != nil {
