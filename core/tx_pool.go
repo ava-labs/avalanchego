ad9f68c1d10604592d9e750a3275d9358c1c838c
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-08-09 14:54:31 -0400

    Enforce minimum fee and update min priority gas price for ap3

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -238,6 +238,7 @@ type TxPool struct {
 	chainconfig *params.ChainConfig
 	chain       blockChain
 	gasPrice    *big.Int
+	minimumFee  *big.Int
 	txFeed      event.Feed
 	headFeed    event.Feed
 	reorgFeed   event.Feed
@@ -643,6 +644,10 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 	// Drop non-local transactions under our own minimal accepted gas price or tip
 	if !local && tx.GasTipCapIntCmp(pool.gasPrice) < 0 {
 		return fmt.Errorf("%w: address %s have gas tip cap (%d) < pool gas tip cap (%d)", ErrUnderpriced, from.Hex(), tx.GasTipCap(), pool.gasPrice)
+	}
+	// Drop the transaction if the gas fee cap is below the pool's minimum fee
+	if pool.minimumFee != nil && tx.GasFeeCapIntCmp(pool.minimumFee) < 0 {
+		return fmt.Errorf("%w: address %s have gas fee cap (%d) < pool minimum fee cap (%d)", ErrUnderpriced, from.Hex(), tx.GasFeeCap(), pool.minimumFee)
 	}
 	// Ensure the transaction adheres to nonce ordering
 	if currentNonce, txNonce := pool.currentState.GetNonce(from), tx.Nonce(); currentNonce > txNonce {
@@ -1581,6 +1586,7 @@ func (pool *TxPool) startPeriodicFeeUpdate() {
 	// Call updateBaseFee here to ensure that there is not a [baseFeeUpdateInterval] delay
 	// when starting up in ApricotPhase3 before the base fee is updated.
 	if time.Now().After(time.Unix(pool.chainconfig.ApricotPhase3BlockTimestamp.Int64(), 0)) {
+		pool.minimumFee = big.NewInt(params.ApricotPhase3MinBaseFee)
 		pool.updateBaseFee()
 	}
 
