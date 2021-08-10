a552b604a508b3d69dce27096d159253f7a1f83e
manthanhd <manthanhd@live.com>
2021-08-10 10:22:09 +0100

    add test for shared dependency

--- snow/engine/avalanche/voter_test.go
+++ snow/engine/avalanche/voter_test.go
@@ -237,3 +237,136 @@ func TestVotingFinishesWithAbandonDepMiddleRequest(t *testing.T) {
 	// expect all polls to have finished
 	assert.Equal(t, 0, transitive.polls.Len())
 }
+
+func TestSharedDependency(t *testing.T) {
+	transitive := &Transitive{}
+
+	config := DefaultConfig()
+	config.Manager = vertex.NewTestManager(t)
+	err := transitive.Initialize(config)
+	assert.NoError(t, err)
+
+	// prepare 3 validators
+	vdr1 := ids.ShortID{1}
+	vdr2 := ids.ShortID{2}
+	vdr3 := ids.ShortID{3}
+
+	vdrs := ids.ShortBag{}
+	vdrs.Add(
+		vdr1,
+		vdr2,
+	)
+
+	// add poll for request 1
+	transitive.polls.Add(1, vdrs)
+
+	vdrs = ids.ShortBag{}
+	vdrs.Add(
+		vdr1,
+		vdr3,
+	)
+
+	// add poll for request 2
+	transitive.polls.Add(2, vdrs)
+
+	vdrs = ids.ShortBag{}
+	vdrs.Add(
+		vdr2,
+		vdr3,
+	)
+
+	// add poll for request 3
+	transitive.polls.Add(3, vdrs)
+
+	// expect 3 pending polls
+	assert.Equal(t, 3, transitive.polls.Len())
+
+	vote1 := ids.GenerateTestID()
+	vote2 := ids.GenerateTestID()
+	vote3 := ids.GenerateTestID()
+
+	// req3 voters all vote
+
+	req3Voter1 := &voter{
+		t:         transitive,
+		requestID: 3,
+		response:  []ids.ID{vote3},
+		deps:      ids.NewSet(0),
+		vdr:       vdr3,
+	}
+
+	req3Voter1.Update()
+
+	req3Voter2 := &voter{
+		t:         transitive,
+		requestID: 3,
+		response:  []ids.ID{vote3},
+		deps:      ids.NewSet(0),
+		vdr:       vdr2,
+	}
+
+	req3Voter2.Update()
+
+	// 3 polls pending because req 2 and 1 have not voted
+	assert.Equal(t, 3, transitive.polls.Len())
+
+	// setup common dependency
+	dep := ids.GenerateTestID()
+	depSet := ids.NewSet(1)
+	depSet.Add(dep)
+
+	req2Voter1 := &voter{
+		t:         transitive,
+		requestID: 2,
+		response:  []ids.ID{vote2},
+		deps:      depSet,
+		vdr:       vdr1,
+	}
+
+	// does nothing because dependency is unfulfilled
+	req2Voter1.Update()
+
+	req2Voter2 := &voter{
+		t:         transitive,
+		requestID: 2,
+		response:  []ids.ID{vote2},
+		deps:      ids.NewSet(0),
+		vdr:       vdr3,
+	}
+
+	req2Voter2.Update()
+
+	// 3 polls pending as req 2 dependency is unfulfilled and 1 has not voted
+	assert.Equal(t, 3, transitive.polls.Len())
+
+	req1Voter1 := &voter{
+		t:         transitive,
+		requestID: 1,
+		response:  []ids.ID{vote1},
+		deps:      depSet,
+		vdr:       vdr1,
+	}
+
+	// does nothing because dependency is unfulfilled
+	req1Voter1.Update()
+
+	req1Voter2 := &voter{
+		t:         transitive,
+		requestID: 1,
+		response:  []ids.ID{vote1},
+		deps:      ids.NewSet(0),
+		vdr:       vdr2,
+	}
+
+	req1Voter2.Update()
+
+	// 3 polls pending as req2 and req 1 dependencies are unfulfilled
+	assert.Equal(t, 3, transitive.polls.Len())
+
+	// abandon dependency
+	req1Voter1.Abandon(dep)
+	req2Voter1.Abandon(dep)
+
+	// expect no pending polls
+	assert.Equal(t, 0, transitive.polls.Len())
+}
