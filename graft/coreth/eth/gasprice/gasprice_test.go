322124cdb16d2ca5858edd3a5f871444dc3682ef
Patrick O'Grady <prohb125@gmail.com>
2021-08-25 22:16:44 -0700

    pass existing tests

--- graft/coreth/eth/gasprice/gasprice_test.go
+++ graft/coreth/eth/gasprice/gasprice_test.go
@@ -149,10 +149,10 @@ func TestSuggestTipCap(t *testing.T) {
 		expect *big.Int // Expected gasprice suggestion
 	}{
 		{nil, big.NewInt(params.GWei * int64(225))},
-		{big.NewInt(0), big.NewInt(params.GWei * int64(10))},  // Fork point in genesis
-		{big.NewInt(1), big.NewInt(params.GWei * int64(10))},  // Fork point in first block
-		{big.NewInt(32), big.NewInt(params.GWei * int64(10))}, // Fork point in last block
-		{big.NewInt(33), big.NewInt(params.GWei * int64(10))}, // Fork point in the future
+		{big.NewInt(0), big.NewInt(0)},  // Fork point in genesis
+		{big.NewInt(1), big.NewInt(0)},  // Fork point in first block
+		{big.NewInt(32), big.NewInt(0)}, // Fork point in last block
+		{big.NewInt(33), big.NewInt(0)}, // Fork point in the future
 	}
 	for _, c := range cases {
 		backend := newTestBackend(t, c.fork)
