c37cbe185c7487ae6cba06655aebc07e9fd96be5
StephenButtolph <stephen@avalabs.org>
2021-09-06 15:22:11 -0400

    re-order methods

--- vms/proposervm/vm.go
+++ vms/proposervm/vm.go
@@ -103,72 +103,6 @@ func (vm *VM) Initialize(
 	return vm.repairAcceptedChain()
 }
 
-func (vm *VM) repairAcceptedChain() error {
-	lastAcceptedID, err := vm.GetLastAccepted()
-	if err == database.ErrNotFound {
-		// If the last accepted block isn't indexed yet, then the underlying
-		// chain is the only chain and there is nothing to repair.
-		return nil
-	}
-	if err != nil {
-		return err
-	}
-
-	// Revert accepted blocks that weren't committed to the database.
-	for {
-		lastAccepted, err := vm.getPostForkBlock(lastAcceptedID)
-		if err == database.ErrNotFound {
-			// If the post fork block can't be found, it's because we're
-			// reverting past the fork boundary. If this is the case, then there
-			// is only one database to keep consistent, so there is nothing to
-			// repair anymore.
-			if err := vm.State.DeleteLastAccepted(); err != nil {
-				return err
-			}
-			return vm.db.Commit()
-		}
-		if err != nil {
-			return err
-		}
-
-		shouldBeAccepted := lastAccepted.getInnerBlk()
-
-		// If the inner block is accepted, then we don't need to revert any more
-		// blocks.
-		if shouldBeAccepted.Status() == choices.Accepted {
-			return vm.db.Commit()
-		}
-
-		// Advance to the parent block
-		lastAcceptedID = lastAccepted.Parent()
-
-		lastAccepted.setStatus(choices.Processing)
-		if err := vm.State.SetLastAccepted(lastAcceptedID); err != nil {
-			return err
-		}
-		if err := vm.State.PutBlock(lastAccepted.getStatelessBlk(), choices.Processing); err != nil {
-			return err
-		}
-	}
-}
-
-func (vm *VM) verifyAndRecordInnerBlk(postFork PostForkBlock) error {
-	// If inner block's Verify returned true, don't call it again.
-	// Note that if [postFork.getInnerBlk().Verify] returns nil,
-	// this method returns nil. This must always remain the case to
-	// maintain the inner block's invariant that if it's Verify()
-	// returns nil, it is eventually accepted/rejected.
-	if !vm.Tree.Contains(postFork.getInnerBlk()) {
-		if err := postFork.getInnerBlk().Verify(); err != nil {
-			return err
-		}
-		vm.Tree.Add(postFork.getInnerBlk())
-	}
-
-	vm.verifiedBlocks[postFork.ID()] = postFork
-	return nil
-}
-
 func (vm *VM) BuildBlock() (snowman.Block, error) {
 	preferredBlock, err := vm.getBlock(vm.preferred)
 	if err != nil {
@@ -242,56 +176,53 @@ func (vm *VM) LastAccepted() (ids.ID, error) {
 	return lastAccepted, err
 }
 
-func (vm *VM) getBlock(id ids.ID) (Block, error) {
-	if blk, err := vm.getPostForkBlock(id); err == nil {
-		return blk, nil
+func (vm *VM) repairAcceptedChain() error {
+	lastAcceptedID, err := vm.GetLastAccepted()
+	if err == database.ErrNotFound {
+		// If the last accepted block isn't indexed yet, then the underlying
+		// chain is the only chain and there is nothing to repair.
+		return nil
 	}
-	return vm.getPreForkBlock(id)
-}
-
-func (vm *VM) getPostForkBlock(blkID ids.ID) (PostForkBlock, error) {
-	block, exists := vm.verifiedBlocks[blkID]
-	if exists {
-		return block, nil
-	}
-
-	statelessBlock, status, err := vm.State.GetBlock(blkID)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	innerBlkBytes := statelessBlock.Block()
-	innerBlk, err := vm.ChainVM.ParseBlock(innerBlkBytes)
-	if err != nil {
-		return nil, err
-	}
+	// Revert accepted blocks that weren't committed to the database.
+	for {
+		lastAccepted, err := vm.getPostForkBlock(lastAcceptedID)
+		if err == database.ErrNotFound {
+			// If the post fork block can't be found, it's because we're
+			// reverting past the fork boundary. If this is the case, then there
+			// is only one database to keep consistent, so there is nothing to
+			// repair anymore.
+			if err := vm.State.DeleteLastAccepted(); err != nil {
+				return err
+			}
+			return vm.db.Commit()
+		}
+		if err != nil {
+			return err
+		}
 
-	if statelessSignedBlock, ok := statelessBlock.(statelessblock.SignedBlock); ok {
-		return &postForkBlock{
-			SignedBlock: statelessSignedBlock,
-			postForkCommonComponents: postForkCommonComponents{
-				vm:       vm,
-				innerBlk: innerBlk,
-				status:   status,
-			},
-		}, nil
-	}
-	return &postForkOption{
-		Block: statelessBlock,
-		postForkCommonComponents: postForkCommonComponents{
-			vm:       vm,
-			innerBlk: innerBlk,
-			status:   status,
-		},
-	}, nil
-}
+		shouldBeAccepted := lastAccepted.getInnerBlk()
 
-func (vm *VM) getPreForkBlock(blkID ids.ID) (*preForkBlock, error) {
-	blk, err := vm.ChainVM.GetBlock(blkID)
-	return &preForkBlock{
-		Block: blk,
-		vm:    vm,
-	}, err
+		// If the inner block is accepted, then we don't need to revert any more
+		// blocks.
+		if shouldBeAccepted.Status() == choices.Accepted {
+			return vm.db.Commit()
+		}
+
+		// Advance to the parent block
+		lastAcceptedID = lastAccepted.Parent()
+
+		lastAccepted.setStatus(choices.Processing)
+		if err := vm.State.SetLastAccepted(lastAcceptedID); err != nil {
+			return err
+		}
+		if err := vm.State.PutBlock(lastAccepted.getStatelessBlk(), choices.Processing); err != nil {
+			return err
+		}
+	}
 }
 
 func (vm *VM) parsePostForkBlock(b []byte) (PostForkBlock, error) {
@@ -346,9 +277,78 @@ func (vm *VM) parsePreForkBlock(b []byte) (*preForkBlock, error) {
 	}, err
 }
 
+func (vm *VM) getBlock(id ids.ID) (Block, error) {
+	if blk, err := vm.getPostForkBlock(id); err == nil {
+		return blk, nil
+	}
+	return vm.getPreForkBlock(id)
+}
+
+func (vm *VM) getPostForkBlock(blkID ids.ID) (PostForkBlock, error) {
+	block, exists := vm.verifiedBlocks[blkID]
+	if exists {
+		return block, nil
+	}
+
+	statelessBlock, status, err := vm.State.GetBlock(blkID)
+	if err != nil {
+		return nil, err
+	}
+
+	innerBlkBytes := statelessBlock.Block()
+	innerBlk, err := vm.ChainVM.ParseBlock(innerBlkBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	if statelessSignedBlock, ok := statelessBlock.(statelessblock.SignedBlock); ok {
+		return &postForkBlock{
+			SignedBlock: statelessSignedBlock,
+			postForkCommonComponents: postForkCommonComponents{
+				vm:       vm,
+				innerBlk: innerBlk,
+				status:   status,
+			},
+		}, nil
+	}
+	return &postForkOption{
+		Block: statelessBlock,
+		postForkCommonComponents: postForkCommonComponents{
+			vm:       vm,
+			innerBlk: innerBlk,
+			status:   status,
+		},
+	}, nil
+}
+
+func (vm *VM) getPreForkBlock(blkID ids.ID) (*preForkBlock, error) {
+	blk, err := vm.ChainVM.GetBlock(blkID)
+	return &preForkBlock{
+		Block: blk,
+		vm:    vm,
+	}, err
+}
+
 func (vm *VM) storePostForkBlock(blk PostForkBlock) error {
 	if err := vm.State.PutBlock(blk.getStatelessBlk(), blk.Status()); err != nil {
 		return err
 	}
 	return vm.db.Commit()
+}
+
+func (vm *VM) verifyAndRecordInnerBlk(postFork PostForkBlock) error {
+	// If inner block's Verify returned true, don't call it again.
+	//
+	// Note that if [innerBlk.Verify] returns nil, this method returns nil. This
+	// must always remain the case to maintain the inner block's invariant that
+	// if it's Verify() returns nil, it is eventually accepted or rejected.
+	if innerBlk := postFork.getInnerBlk(); !vm.Tree.Contains(innerBlk) {
+		if err := innerBlk.Verify(); err != nil {
+			return err
+		}
+		vm.Tree.Add(innerBlk)
+	}
+
+	vm.verifiedBlocks[postFork.ID()] = postFork
+	return nil
 }
