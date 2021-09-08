bc2b98526aaa81333a8b7078e80a78a974b0cd84
StephenButtolph <stephen@avalabs.org>
2021-09-08 17:41:59 -0400

    bumped avalanchego version

--- version/constants.go
+++ version/constants.go
@@ -13,7 +13,7 @@ import (
 var (
 	String                       string // Printed when CLI arg --version is used
 	GitCommit                    string // Set in the build script (i.e. at compile time)
-	Current                      = NewDefaultVersion(1, 5, 2)
+	Current                      = NewDefaultVersion(1, 5, 3)
 	CurrentApp                   = NewDefaultApplication(constants.PlatformName, Current.Major(), Current.Minor(), Current.Patch())
 	MinimumCompatibleVersion     = NewDefaultApplication(constants.PlatformName, 1, 5, 0)
 	PrevMinimumCompatibleVersion = NewDefaultApplication(constants.PlatformName, 1, 4, 5)
