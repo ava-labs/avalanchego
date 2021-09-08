0642a90646a969701b91e56f0eb0f659d908f951
Patrick O'Grady <prohb125@gmail.com>
2021-09-08 09:21:20 -0700

    traced failure down to wg

--- plugin/evm/network_eth_gossiping_test.go
+++ plugin/evm/network_eth_gossiping_test.go
@@ -3,82 +3,83 @@
 
 package evm
 
-// import (
-// 	"crypto/ecdsa"
-// 	"encoding/json"
-// 	"math/big"
-// 	"sync"
-// 	"testing"
-// 	"time"
-//
-// 	"github.com/ava-labs/avalanchego/ids"
-//
-// 	"github.com/ethereum/go-ethereum/common"
-// 	"github.com/ethereum/go-ethereum/crypto"
-// 	"github.com/ethereum/go-ethereum/rlp"
-//
-// 	"github.com/stretchr/testify/assert"
-//
-// 	"github.com/ava-labs/coreth/core"
-// 	"github.com/ava-labs/coreth/core/types"
-// 	"github.com/ava-labs/coreth/params"
-// 	"github.com/ava-labs/coreth/plugin/evm/message"
-// )
-//
-// func fundAddressByGenesis(addr common.Address) (string, error) {
-// 	balance := big.NewInt(0xffffffffffffff)
-// 	genesis := &core.Genesis{
-// 		Difficulty: common.Big0,
-// 		GasLimit:   uint64(5000000),
-// 	}
-// 	funds := make(map[common.Address]core.GenesisAccount)
-// 	funds[addr] = core.GenesisAccount{
-// 		Balance: balance,
-// 	}
-// 	genesis.Alloc = funds
-//
-// 	genesis.Config = &params.ChainConfig{
-// 		ChainID: params.AvalancheLocalChainID,
-// 	}
-//
-// 	bytes, err := json.Marshal(genesis)
-// 	return string(bytes), err
-// }
-//
-// func getValidEthTxs(key *ecdsa.PrivateKey) []*types.Transaction {
-// 	res := make([]*types.Transaction, 0)
-//
-// 	nonce := uint64(0)
-// 	to := common.Address{}
-// 	amount := big.NewInt(10000)
-// 	gaslimit := uint64(100000)
-// 	gasprice := big.NewInt(1)
-//
-// 	tx1, _ := types.SignTx(
-// 		types.NewTransaction(nonce,
-// 			to,
-// 			amount,
-// 			gaslimit,
-// 			gasprice,
-// 			nil),
-// 		types.HomesteadSigner{}, key)
-// 	res = append(res, tx1)
-//
-// 	nonce++
-// 	tx2, _ := types.SignTx(
-// 		types.NewTransaction(
-// 			nonce,
-// 			to,
-// 			amount,
-// 			gaslimit,
-// 			gasprice,
-// 			nil,
-// 		),
-// 		types.HomesteadSigner{}, key)
-// 	res = append(res, tx2)
-// 	return res
-// }
-//
+import (
+	"crypto/ecdsa"
+	"encoding/json"
+	// "fmt"
+	"math/big"
+	// "sync"
+	"testing"
+	"time"
+
+	"github.com/ava-labs/avalanchego/ids"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	// "github.com/ethereum/go-ethereum/rlp"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ava-labs/coreth/core"
+	"github.com/ava-labs/coreth/core/types"
+	"github.com/ava-labs/coreth/params"
+	"github.com/ava-labs/coreth/plugin/evm/message"
+)
+
+func fundAddressByGenesis(addr common.Address) (string, error) {
+	balance := big.NewInt(0xffffffffffffff)
+	genesis := &core.Genesis{
+		Difficulty: common.Big0,
+		GasLimit:   uint64(5000000),
+	}
+	funds := make(map[common.Address]core.GenesisAccount)
+	funds[addr] = core.GenesisAccount{
+		Balance: balance,
+	}
+	genesis.Alloc = funds
+
+	genesis.Config = &params.ChainConfig{
+		ChainID: params.AvalancheLocalChainID,
+	}
+
+	bytes, err := json.Marshal(genesis)
+	return string(bytes), err
+}
+
+func getValidEthTxs(key *ecdsa.PrivateKey) []*types.Transaction {
+	res := make([]*types.Transaction, 0)
+
+	nonce := uint64(0)
+	to := common.Address{}
+	amount := big.NewInt(10000)
+	gaslimit := uint64(100000)
+	gasprice := big.NewInt(1)
+
+	tx1, _ := types.SignTx(
+		types.NewTransaction(nonce,
+			to,
+			amount,
+			gaslimit,
+			gasprice,
+			nil),
+		types.HomesteadSigner{}, key)
+	res = append(res, tx1)
+
+	nonce++
+	tx2, _ := types.SignTx(
+		types.NewTransaction(
+			nonce,
+			to,
+			amount,
+			gaslimit,
+			gasprice,
+			nil,
+		),
+		types.HomesteadSigner{}, key)
+	res = append(res, tx2)
+	return res
+}
+
 // // show that locally issued eth txs are gossiped
 // // Note: channel through which coreth mempool push txs to vm is injected here
 // // to ease up UT, which target only VM behaviors in response to coreth mempool
@@ -118,76 +119,77 @@ package evm
 // 		assert.NoError(err, "failed adding coreth tx to mempool")
 // 	}
 //
+// 	fmt.Println("waiting for tx")
 // 	wg.Wait()
 // }
-//
-// // show that a geth tx discovered from gossip is requested to the same node that
-// // gossiped it
-// func TestMempoolEthTxsAppGossipHandling(t *testing.T) {
-// 	assert := assert.New(t)
-//
-// 	key, err := crypto.GenerateKey()
-// 	assert.NoError(err)
-//
-// 	addr := crypto.PubkeyToAddress(key.PublicKey)
-//
-// 	cfgJson, err := fundAddressByGenesis(addr)
-// 	assert.NoError(err)
-//
-// 	_, vm, _, _, sender := GenesisVM(t, true, cfgJson, "", "")
-// 	defer func() {
-// 		err := vm.Shutdown()
-// 		assert.NoError(err)
-// 	}()
-// 	vm.gossipActivationTime = time.Unix(0, 0) // enable mempool gossiping
-// 	vm.chain.GetTxPool().SetGasPrice(common.Big1)
-// 	vm.chain.GetTxPool().SetMinFee(common.Big0)
-//
-// 	nodeID := ids.GenerateTestShortID()
-//
-// 	var (
-// 		txRequested         bool
-// 		txRequestedFromNode bool
-// 	)
-// 	sender.SendAppRequestF = func(nodes ids.ShortSet, _ uint32, _ []byte) error {
-// 		txRequested = true
-// 		if nodes.Contains(nodeID) {
-// 			txRequestedFromNode = true
-// 		}
-// 		return nil
-// 	}
-//
-// 	// prepare a tx
-// 	tx := getValidEthTxs(key)[0]
-// 	txSender, err := types.Sender(types.LatestSigner(vm.chainConfig), tx)
-// 	assert.NoError(err, "could not retrieve sender")
-//
-// 	// show that unknown coreth hashes is requested
-// 	msg := message.EthTxsNotify{
-// 		Txs: []message.EthTxNotify{{
-// 			Hash:   tx.Hash(),
-// 			Sender: txSender,
-// 			Nonce:  tx.Nonce(),
-// 		}},
-// 	}
-// 	msgBytes, err := message.Build(&msg)
-// 	assert.NoError(err)
-//
-// 	err = vm.AppGossip(nodeID, msgBytes)
-// 	assert.NoError(err)
-// 	assert.True(txRequested, "unknown txID should have been requested")
-// 	assert.True(txRequestedFromNode, "unknown txID should have been requested to the same node")
-//
-// 	// show that known coreth tx is not requested
-// 	txRequested = false
-// 	err = vm.chain.GetTxPool().AddLocal(tx)
-// 	assert.NoError(err)
-//
-// 	err = vm.AppGossip(nodeID, msgBytes)
-// 	assert.NoError(err)
-// 	assert.False(txRequested, "known txID should not be requested")
-// }
-//
+
+// show that a geth tx discovered from gossip is requested to the same node that
+// gossiped it
+func TestMempoolEthTxsAppGossipHandling(t *testing.T) {
+	assert := assert.New(t)
+
+	key, err := crypto.GenerateKey()
+	assert.NoError(err)
+
+	addr := crypto.PubkeyToAddress(key.PublicKey)
+
+	cfgJson, err := fundAddressByGenesis(addr)
+	assert.NoError(err)
+
+	_, vm, _, _, sender := GenesisVM(t, true, cfgJson, "", "")
+	defer func() {
+		err := vm.Shutdown()
+		assert.NoError(err)
+	}()
+	vm.gossipActivationTime = time.Unix(0, 0) // enable mempool gossiping
+	vm.chain.GetTxPool().SetGasPrice(common.Big1)
+	vm.chain.GetTxPool().SetMinFee(common.Big0)
+
+	nodeID := ids.GenerateTestShortID()
+
+	var (
+		txRequested         bool
+		txRequestedFromNode bool
+	)
+	sender.SendAppRequestF = func(nodes ids.ShortSet, _ uint32, _ []byte) error {
+		txRequested = true
+		if nodes.Contains(nodeID) {
+			txRequestedFromNode = true
+		}
+		return nil
+	}
+
+	// prepare a tx
+	tx := getValidEthTxs(key)[0]
+	txSender, err := types.Sender(types.LatestSigner(vm.chainConfig), tx)
+	assert.NoError(err, "could not retrieve sender")
+
+	// show that unknown coreth hashes is requested
+	msg := message.EthTxsNotify{
+		Txs: []message.EthTxNotify{{
+			Hash:   tx.Hash(),
+			Sender: txSender,
+			Nonce:  tx.Nonce(),
+		}},
+	}
+	msgBytes, err := message.Build(&msg)
+	assert.NoError(err)
+
+	err = vm.AppGossip(nodeID, msgBytes)
+	assert.NoError(err)
+	assert.True(txRequested, "unknown txID should have been requested")
+	assert.True(txRequestedFromNode, "unknown txID should have been requested to the same node")
+
+	// show that known coreth tx is not requested
+	txRequested = false
+	err = vm.chain.GetTxPool().AddLocal(tx)
+	assert.NoError(err)
+
+	err = vm.AppGossip(nodeID, msgBytes)
+	assert.NoError(err)
+	assert.False(txRequested, "known txID should not be requested")
+}
+
 // // show that a tx discovered by a GossipResponse is re-gossiped if it is added
 // // to the mempool
 // func TestMempoolEthTxsAppResponseHandling(t *testing.T) {
@@ -214,6 +216,8 @@ package evm
 // 		txGossiped bool
 // 		wg         sync.WaitGroup
 // 	)
+//
+// 	wg.Add(1)
 // 	sender.SendAppGossipF = func([]byte) error {
 // 		txGossiped = true
 // 		wg.Done()
@@ -252,10 +256,10 @@ package evm
 // 		txs[0].Hash(): {},
 // 	}
 // 	vm.requestsEthContent[0] = reqs
-// 	wg.Add(1)
 // 	err = vm.AppResponse(nodeID, 0, msgBytes)
 // 	assert.NoError(err)
 //
+// 	fmt.Println("waiting for tx")
 // 	wg.Wait()
 //
 // 	has = pool.Has(txs[0].Hash())
