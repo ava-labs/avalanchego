39212cf98cc7d8a64789279283e3374b3a7e3f44
Patrick O'Grady <prohb125@gmail.com>
2021-09-09 02:20:47 -0700

    make sense of networking layer

--- plugin/evm/message/handler.go
+++ plugin/evm/message/handler.go
@@ -12,13 +12,20 @@ import (
 var _ Handler = NoopHandler{}
 
 type Handler interface {
+	HandleAtomicTxNotify(nodeID ids.ShortID, requestID uint32, msg *AtomicTxNotify) error
 	HandleAtomicTxRequest(nodeID ids.ShortID, requestID uint32, msg *AtomicTxRequest) error
 	HandleAtomicTx(nodeID ids.ShortID, requestID uint32, msg *AtomicTx) error
+	HandleEthTxsNotify(nodeID ids.ShortID, requestID uint32, msg *EthTxsNotify) error
 	HandleEthTxsRequest(nodeID ids.ShortID, requestID uint32, msg *EthTxsRequest) error
 	HandleEthTxs(nodeID ids.ShortID, requestID uint32, msg *EthTxs) error
 }
 
 type NoopHandler struct{}
+
+func (NoopHandler) HandleAtomicTxNotify(nodeID ids.ShortID, requestID uint32, _ *AtomicTxNotify) error {
+	log.Debug("dropping unexpected AtomicTxRequest message", "peerID", nodeID, "requestID", requestID)
+	return nil
+}
 
 func (NoopHandler) HandleAtomicTxRequest(nodeID ids.ShortID, requestID uint32, _ *AtomicTxRequest) error {
 	log.Debug("dropping unexpected AtomicTxRequest message", "peerID", nodeID, "requestID", requestID)
@@ -27,6 +34,11 @@ func (NoopHandler) HandleAtomicTxRequest(nodeID ids.ShortID, requestID uint32, _
 
 func (NoopHandler) HandleAtomicTx(nodeID ids.ShortID, requestID uint32, _ *AtomicTx) error {
 	log.Debug("dropping unexpected HandleAtomicTx message", "peerID", nodeID, "requestID", requestID)
+	return nil
+}
+
+func (NoopHandler) HandleEthTxsNotify(nodeID ids.ShortID, requestID uint32, _ *EthTxsNotify) error {
+	log.Debug("dropping unexpected EthTxsRequest message", "peerID", nodeID, "requestID", requestID)
 	return nil
 }
 
