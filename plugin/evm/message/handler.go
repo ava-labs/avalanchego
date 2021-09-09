5e690f966e3949315376c6fc9ec2e669aa710b31
Patrick O'Grady <prohb125@gmail.com>
2021-09-09 02:04:49 -0700

    work on matching on network layer

--- plugin/evm/message/handler.go
+++ plugin/evm/message/handler.go
@@ -12,16 +12,16 @@ import (
 var _ Handler = NoopHandler{}
 
 type Handler interface {
-	HandleAtomicTxNotify(nodeID ids.ShortID, requestID uint32, msg *AtomicTxNotify) error
+	HandleAtomicTxRequest(nodeID ids.ShortID, requestID uint32, msg *AtomicTxRequest) error
 	HandleAtomicTx(nodeID ids.ShortID, requestID uint32, msg *AtomicTx) error
-	HandleEthTxsNotify(nodeID ids.ShortID, requestID uint32, msg *EthTxsNotify) error
+	HandleEthTxsRequest(nodeID ids.ShortID, requestID uint32, msg *EthTxsRequest) error
 	HandleEthTxs(nodeID ids.ShortID, requestID uint32, msg *EthTxs) error
 }
 
 type NoopHandler struct{}
 
-func (NoopHandler) HandleAtomicTxNotify(nodeID ids.ShortID, requestID uint32, _ *AtomicTxNotify) error {
-	log.Debug("dropping unexpected AtomicTxNotify message", "peerID", nodeID, "requestID", requestID)
+func (NoopHandler) HandleAtomicTxRequest(nodeID ids.ShortID, requestID uint32, _ *AtomicTxRequest) error {
+	log.Debug("dropping unexpected AtomicTxRequest message", "peerID", nodeID, "requestID", requestID)
 	return nil
 }
 
@@ -30,8 +30,8 @@ func (NoopHandler) HandleAtomicTx(nodeID ids.ShortID, requestID uint32, _ *Atomi
 	return nil
 }
 
-func (NoopHandler) HandleEthTxsNotify(nodeID ids.ShortID, requestID uint32, _ *EthTxsNotify) error {
-	log.Debug("dropping unexpected EthTxsNotify message", "peerID", nodeID, "requestID", requestID)
+func (NoopHandler) HandleEthTxsRequest(nodeID ids.ShortID, requestID uint32, _ *EthTxsRequest) error {
+	log.Debug("dropping unexpected EthTxsRequest message", "peerID", nodeID, "requestID", requestID)
 	return nil
 }
 
