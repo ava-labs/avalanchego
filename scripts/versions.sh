6caf5edac664367df97333a722b961b1fbf48f8b
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-09-12 12:18:29 -0400

    bump coreth to v0.6.4-rc.0

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -7,4 +7,4 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-coreth_version=${CORETH_VERSION:-'v0.6.3-rc.1'}
+coreth_version=${CORETH_VERSION:-'v0.6.4-rc.0'}
