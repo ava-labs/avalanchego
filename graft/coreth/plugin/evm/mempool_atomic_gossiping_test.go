0aa736257c7284e77dfe12b38779d820329e887d
Patrick O'Grady <prohb125@gmail.com>
2021-09-13 19:33:59 -0700

    cleanup tests

--- graft/coreth/plugin/evm/mempool_atomic_gossiping_test.go
+++ graft/coreth/plugin/evm/mempool_atomic_gossiping_test.go
@@ -5,7 +5,6 @@ package evm
 
 import (
 	"testing"
-	"time"
 
 	"github.com/ava-labs/avalanchego/vms/components/chain"
 
@@ -14,138 +13,77 @@ import (
 
 // shows that a locally generated AtomicTx can be added to mempool and then
 // removed by inclusion in a block
-// TODO: also shows conflicting can't be added
-// TODO: attempt export conflicting
-func TestMempoolAddLocallyCreateAtomicImportTx(t *testing.T) {
-	assert := assert.New(t)
+func TestMempoolAddLocallyCreateAtomicTx(t *testing.T) {
+	for _, name := range []string{"import", "export"} {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
 
-	issuer, vm, _, sharedMemory, _ := GenesisVM(t, true, genesisJSONApricotPhase4, "", "")
-	defer func() {
-		err := vm.Shutdown()
-		assert.NoError(err)
-	}()
-	mempool := vm.mempool
+			// we use AP3 genesis here to not trip any block fees
+			issuer, vm, _, sharedMemory, _ := GenesisVM(t, true, genesisJSONApricotPhase3, "", "")
+			defer func() {
+				err := vm.Shutdown()
+				assert.NoError(err)
+			}()
+			mempool := vm.mempool
 
-	// generate a valid and conflicting tx
-	tx, conflictingTx := getValidImportTx(vm, sharedMemory, t)
-	txID := tx.ID()
-	conflictingTxID := conflictingTx.ID()
+			// generate a valid and conflicting tx
+			var tx, conflictingTx *Tx
+			if name == "import" {
+				tx, conflictingTx = getValidImportTx(vm, sharedMemory, t)
+			} else {
+				tx, conflictingTx = getValidExportTx(vm, issuer, sharedMemory, t)
+			}
+			txID := tx.ID()
+			conflictingTxID := conflictingTx.ID()
 
-	// add a tx to the mempool
-	err := vm.issueTx(tx, true /*=local*/)
-	assert.NoError(err)
-	has := mempool.has(txID)
-	assert.True(has, "valid tx not recorded into mempool")
+			// add a tx to the mempool
+			err := vm.issueTx(tx, true /*=local*/)
+			assert.NoError(err)
+			has := mempool.has(txID)
+			assert.True(has, "valid tx not recorded into mempool")
 
-	// try to add a conflicting tx
-	err = vm.issueTx(conflictingTx, true /*=local*/)
-	assert.ErrorIs(err, errConflictingAtomicTx)
-	has = mempool.has(conflictingTxID)
-	assert.False(has, "conflicting tx in mempool")
+			// try to add a conflicting tx
+			err = vm.issueTx(conflictingTx, true /*=local*/)
+			assert.ErrorIs(err, errConflictingAtomicTx)
+			has = mempool.has(conflictingTxID)
+			assert.False(has, "conflicting tx in mempool")
 
-	<-issuer
+			<-issuer
 
-	has = mempool.has(txID)
-	assert.True(has, "valid tx not recorded into mempool")
+			has = mempool.has(txID)
+			assert.True(has, "valid tx not recorded into mempool")
 
-	// show that build block include that tx and tx is still in mempool
-	blk, err := vm.BuildBlock()
-	assert.NoError(err, "could not build block out of mempool")
+			// show that build block include that tx and tx is still in mempool
+			blk, err := vm.BuildBlock()
+			assert.NoError(err, "could not build block out of mempool")
 
-	evmBlk, ok := blk.(*chain.BlockWrapper).Block.(*Block)
-	assert.True(ok, "unknown block type")
+			evmBlk, ok := blk.(*chain.BlockWrapper).Block.(*Block)
+			assert.True(ok, "unknown block type")
 
-	retrievedTx, err := vm.extractAtomicTx(evmBlk.ethBlock)
-	assert.NoError(err, "could not extract atomic tx")
-	assert.Equal(txID, retrievedTx.ID(), "block does not include expected transaction")
+			retrievedTx, err := vm.extractAtomicTx(evmBlk.ethBlock)
+			assert.NoError(err, "could not extract atomic tx")
+			assert.Equal(txID, retrievedTx.ID(), "block does not include expected transaction")
 
-	has = mempool.has(txID)
-	assert.True(has, "tx should stay in mempool until block is accepted")
+			has = mempool.has(txID)
+			assert.True(has, "tx should stay in mempool until block is accepted")
 
-	err = blk.Verify()
-	assert.NoError(err)
+			err = blk.Verify()
+			assert.NoError(err)
 
-	err = blk.Accept()
-	assert.NoError(err)
+			err = blk.Accept()
+			assert.NoError(err)
 
-	has = mempool.has(txID)
-	assert.False(has, "tx shouldn't be in mempool after block is accepted")
+			has = mempool.has(txID)
+			assert.False(has, "tx shouldn't be in mempool after block is accepted")
 
-	// try to add a conflicting tx again (don't use issueTx because will fail
-	// verification)
-	err = mempool.AddTx(conflictingTx)
-	assert.ErrorIs(err, errConflictingAtomicTx)
-	has = mempool.has(conflictingTxID)
-	assert.False(has, "conflicting tx in mempool")
-}
-
-// shows that a locally generated AtomicTx can be added to mempool and then
-// removed by inclusion in a block
-// TODO: also shows conflicting can't be added
-// TODO: attempt export conflicting
-func TestMempoolAddLocallyCreateAtomicExportTx(t *testing.T) {
-	assert := assert.New(t)
-
-	issuer, vm, _, sharedMemory, _ := GenesisVM(t, true, genesisJSONApricotPhase4, "", "")
-	defer func() {
-		err := vm.Shutdown()
-		assert.NoError(err)
-	}()
-	mempool := vm.mempool
-
-	// generate a valid and conflicting tx
-	tx, conflictingTx := getValidExportTx(vm, issuer, sharedMemory, t)
-	txID := tx.ID()
-	conflictingTxID := conflictingTx.ID()
-
-	// add a tx to the mempool
-	err := vm.issueTx(tx, true /*=local*/)
-	assert.NoError(err)
-	has := mempool.has(txID)
-	assert.True(has, "valid tx not recorded into mempool")
-
-	// try to add a conflicting tx
-	err = vm.issueTx(conflictingTx, true /*=local*/)
-	assert.ErrorIs(err, errConflictingAtomicTx)
-	has = mempool.has(conflictingTxID)
-	assert.False(has, "conflicting tx in mempool")
-
-	<-issuer
-
-	time.Sleep(2 * time.Second)
-
-	has = mempool.has(txID)
-	assert.True(has, "valid tx not recorded into mempool")
-
-	// show that build block include that tx and tx is still in mempool
-	blk, err := vm.BuildBlock()
-	assert.NoError(err, "could not build block out of mempool")
-
-	evmBlk, ok := blk.(*chain.BlockWrapper).Block.(*Block)
-	assert.True(ok, "unknown block type")
-
-	retrievedTx, err := vm.extractAtomicTx(evmBlk.ethBlock)
-	assert.NoError(err, "could not extract atomic tx")
-	assert.Equal(txID, retrievedTx.ID(), "block does not include expected transaction")
-
-	has = mempool.has(txID)
-	assert.True(has, "tx should stay in mempool until block is accepted")
-
-	err = blk.Verify()
-	assert.NoError(err)
-
-	err = blk.Accept()
-	assert.NoError(err)
-
-	has = mempool.has(txID)
-	assert.False(has, "tx shouldn't be in mempool after block is accepted")
-
-	// try to add a conflicting tx again (don't use issueTx because will fail
-	// verification)
-	err = mempool.AddTx(conflictingTx)
-	assert.ErrorIs(err, errConflictingAtomicTx)
-	has = mempool.has(conflictingTxID)
-	assert.False(has, "conflicting tx in mempool")
+			// try to add a conflicting tx again (don't use issueTx because will fail
+			// verification)
+			err = mempool.AddTx(conflictingTx)
+			assert.ErrorIs(err, errConflictingAtomicTx)
+			has = mempool.has(conflictingTxID)
+			assert.False(has, "conflicting tx in mempool")
+		})
+	}
 }
 
 // a valid tx shouldn't be added to the mempool if this would exceed the
