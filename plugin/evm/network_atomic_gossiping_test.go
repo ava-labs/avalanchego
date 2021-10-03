99f5ac9c490cbda8f059dc8ca542ef7ae1bb4f83
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-10-03 12:47:52 -0400

    Cleanup txIDs and creation of conflicting txs for tests

--- plugin/evm/network_atomic_gossiping_test.go
+++ plugin/evm/network_atomic_gossiping_test.go
@@ -8,164 +8,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ava-labs/avalanchego/chains/atomic"
 	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/snow/engine/common"
-	"github.com/ava-labs/avalanchego/utils/crypto"
-	"github.com/ava-labs/avalanchego/vms/components/avax"
-	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/ava-labs/coreth/plugin/evm/message"
 )
-
-// getValidImportTx returns 2 transactions that conflict with each other (both valid)
-func getValidImportTx(vm *VM, sharedMemory *atomic.Memory, t *testing.T) (*Tx, *Tx) {
-	importAmount := uint64(50000000)
-	utxoID := avax.UTXOID{
-		TxID: ids.ID{
-			0x0f, 0x2f, 0x4f, 0x6f, 0x8e, 0xae, 0xce, 0xee,
-			0x0d, 0x2d, 0x4d, 0x6d, 0x8c, 0xac, 0xcc, 0xec,
-			0x0b, 0x2b, 0x4b, 0x6b, 0x8a, 0xaa, 0xca, 0xea,
-			0x09, 0x29, 0x49, 0x69, 0x88, 0xa8, 0xc8, 0xe8,
-		},
-	}
-
-	utxo := &avax.UTXO{
-		UTXOID: utxoID,
-		Asset:  avax.Asset{ID: vm.ctx.AVAXAssetID},
-		Out: &secp256k1fx.TransferOutput{
-			Amt: importAmount,
-			OutputOwners: secp256k1fx.OutputOwners{
-				Threshold: 1,
-				Addrs:     []ids.ShortID{testKeys[0].PublicKey().Address()},
-			},
-		},
-	}
-	utxoBytes, err := vm.codec.Marshal(codecVersion, utxo)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	xChainSharedMemory := sharedMemory.NewSharedMemory(vm.ctx.XChainID)
-	inputID := utxo.InputID()
-	if err := xChainSharedMemory.Apply(map[ids.ID]*atomic.Requests{vm.ctx.ChainID: {PutRequests: []*atomic.Element{{
-		Key:   inputID[:],
-		Value: utxoBytes,
-		Traits: [][]byte{
-			testKeys[0].PublicKey().Address().Bytes(),
-		},
-	}}}}); err != nil {
-		t.Fatal(err)
-	}
-
-	importTx, err := vm.newImportTx(vm.ctx.XChainID, testEthAddrs[0], initialBaseFee, []*crypto.PrivateKeySECP256K1R{testKeys[0]})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	importTx2, err := vm.newImportTx(vm.ctx.XChainID, testEthAddrs[1], initialBaseFee, []*crypto.PrivateKeySECP256K1R{testKeys[0]})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return importTx, importTx2
-}
-
-// getValidExportTx returns 2 transactions that conflict with each other (both valid)
-func getValidExportTx(vm *VM, issuer chan common.Message, sharedMemory *atomic.Memory, t *testing.T) (*Tx, *Tx) {
-	importAmount := uint64(50000000)
-	utxoID := avax.UTXOID{
-		TxID: ids.ID{
-			0x0f, 0x2f, 0x4f, 0x6f, 0x8e, 0xae, 0xce, 0xee,
-			0x0d, 0x2d, 0x4d, 0x6d, 0x8c, 0xac, 0xcc, 0xec,
-			0x0b, 0x2b, 0x4b, 0x6b, 0x8a, 0xaa, 0xca, 0xea,
-			0x09, 0x29, 0x49, 0x69, 0x88, 0xa8, 0xc8, 0xe8,
-		},
-	}
-
-	utxo := &avax.UTXO{
-		UTXOID: utxoID,
-		Asset:  avax.Asset{ID: vm.ctx.AVAXAssetID},
-		Out: &secp256k1fx.TransferOutput{
-			Amt: importAmount,
-			OutputOwners: secp256k1fx.OutputOwners{
-				Threshold: 1,
-				Addrs:     []ids.ShortID{testKeys[0].PublicKey().Address()},
-			},
-		},
-	}
-	utxoBytes, err := vm.codec.Marshal(codecVersion, utxo)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	xChainSharedMemory := sharedMemory.NewSharedMemory(vm.ctx.XChainID)
-	inputID := utxo.InputID()
-	if err := xChainSharedMemory.Apply(map[ids.ID]*atomic.Requests{vm.ctx.ChainID: {PutRequests: []*atomic.Element{{
-		Key:   inputID[:],
-		Value: utxoBytes,
-		Traits: [][]byte{
-			testKeys[0].PublicKey().Address().Bytes(),
-		},
-	}}}}); err != nil {
-		t.Fatal(err)
-	}
-
-	importTx, err := vm.newImportTx(vm.ctx.XChainID, testEthAddrs[0], initialBaseFee, []*crypto.PrivateKeySECP256K1R{testKeys[0]})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err := vm.issueTx(importTx, true /*=local*/); err != nil {
-		t.Fatal(err)
-	}
-
-	<-issuer
-
-	blk, err := vm.BuildBlock()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err := blk.Verify(); err != nil {
-		t.Fatal(err)
-	}
-
-	if err := vm.SetPreference(blk.ID()); err != nil {
-		t.Fatal(err)
-	}
-
-	if err := blk.Accept(); err != nil {
-		t.Fatal(err)
-	}
-
-	exportAmount := uint64(5000000)
-
-	testKeys1Addr := GetEthAddress(testKeys[0])
-	exportId1, err := ids.ToShortID(testKeys1Addr[:])
-	if err != nil {
-		t.Fatal(err)
-	}
-	testKeys2Addr := GetEthAddress(testKeys[1])
-	exportId2, err := ids.ToShortID(testKeys2Addr[:])
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	exportTx1, err := vm.newExportTx(vm.ctx.AVAXAssetID, exportAmount, vm.ctx.XChainID, exportId1, initialBaseFee, []*crypto.PrivateKeySECP256K1R{testKeys[0]})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	exportTx2, err := vm.newExportTx(vm.ctx.AVAXAssetID, exportAmount, vm.ctx.XChainID, exportId2, initialBaseFee, []*crypto.PrivateKeySECP256K1R{testKeys[0]})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return exportTx1, exportTx2
-}
 
 // locally issued txs should be gossiped
 func TestMempoolAtmTxsIssueTxAndGossiping(t *testing.T) {
@@ -176,8 +24,9 @@ func TestMempoolAtmTxsIssueTxAndGossiping(t *testing.T) {
 		assert.NoError(vm.Shutdown())
 	}()
 
-	// Create a simple tx
-	tx, conflictingTx := getValidImportTx(vm, sharedMemory, t)
+	// Create conflicting transactions
+	importTxs := createImportTxOptions(t, vm, sharedMemory)
+	tx, conflictingTx := importTxs[0], importTxs[1]
 
 	var gossiped int
 	var gossipedLock sync.Mutex // needed to prevent race
@@ -254,8 +103,9 @@ func TestMempoolAtmTxsAppGossipHandling(t *testing.T) {
 		return nil
 	}
 
-	// create a tx
-	tx, conflictingTx := getValidImportTx(vm, sharedMemory, t)
+	// Create conflicting transactions
+	importTxs := createImportTxOptions(t, vm, sharedMemory)
+	tx, conflictingTx := importTxs[0], importTxs[1]
 
 	// gossip tx and check it is accepted and gossiped
 	msg := message.AtomicTx{
@@ -322,18 +172,20 @@ func TestMempoolAtmTxsAppGossipHandlingDiscardedTx(t *testing.T) {
 		return nil
 	}
 
-	// create a tx and mark as invalid
-	tx, conflictingTx := getValidImportTx(vm, sharedMemory, t)
+	// Create a transaction and mark it as invalid by discarding it
+	importTxs := createImportTxOptions(t, vm, sharedMemory)
+	tx, conflictingTx := importTxs[0], importTxs[1]
 	txID := tx.ID()
 
 	mempool.AddTx(tx)
 	mempool.NextTx()
 	mempool.DiscardCurrentTx()
 
-	has := mempool.has(txID)
-	assert.False(has)
+	// Check the mempool does not contain the discarded transaction
+	assert.False(mempool.has(txID))
 
-	// gossip tx and check it isn't accepted or gossiped
+	// Gossip the transaction to the VM and ensure that it is not added to the mempool
+	// and is not re-gossipped.
 	nodeID := ids.GenerateTestShortID()
 	msg := message.AtomicTx{
 		Tx: tx.Bytes(),
@@ -347,7 +199,11 @@ func TestMempoolAtmTxsAppGossipHandlingDiscardedTx(t *testing.T) {
 	assert.Zero(txGossiped, "tx should not have been gossiped")
 	txGossipedLock.Unlock()
 
-	// gossip conflicting tx and ensure it is accepted and gossiped
+	assert.False(mempool.has(txID))
+
+	// Gossip the transaction that conflicts with the originally
+	// discarded tx and ensure it is accepted into the mempool and gossipped
+	// to the network.
 	nodeID = ids.GenerateTestShortID()
 	msg = message.AtomicTx{
 		Tx: conflictingTx.Bytes(),
@@ -361,4 +217,7 @@ func TestMempoolAtmTxsAppGossipHandlingDiscardedTx(t *testing.T) {
 	txGossipedLock.Lock()
 	assert.Equal(1, txGossiped, "conflicting tx should have been gossiped")
 	txGossipedLock.Unlock()
+
+	assert.False(mempool.has(txID))
+	assert.True(mempool.has(conflictingTx.ID()))
 }
