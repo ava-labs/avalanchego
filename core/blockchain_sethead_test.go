faf134cec0037f34566fbd27977def920701b02f
Felipe Madero <felipe.madero@gmail.com>
2021-10-06 08:29:31 -0300

    aux functions to blockchain sethead

--- core/blockchain_sethead_test.go
+++ core/blockchain_sethead_test.go
@@ -22,20 +22,12 @@ package core
 import (
 	"testing"
 
+	"github.com/ava-labs/avalanchego/database"
+	"github.com/ava-labs/avalanchego/database/leveldb"
+	"github.com/ava-labs/coreth/core/rawdb"
 	"github.com/ava-labs/coreth/core/types"
+	"github.com/ava-labs/coreth/ethdb"
 )
-
-// rewindTest is a test case for chain rollback upon user request.
-type rewindTest struct {
-	canonicalBlocks int    // Number of blocks to generate for the canonical chain (heavier)
-	sidechainBlocks int    // Number of blocks to generate for the side chain (lighter)
-	commitBlock     uint64 // Block number for which to commit the state to disk
-
-	expCanonicalBlocks int    // Number of canonical blocks expected to remain in the database (excl. genesis)
-	expSidechainBlocks int    // Number of sidechain blocks expected to remain in the database (excl. genesis)
-	expHeadHeader      uint64 // Block number of the expected head header
-	expHeadBlock       uint64 // Block number of the expected head full block
-}
 
 // verifyNoGaps checks that there are no gaps after the initial set of blocks in
 // the database and errors if found.
@@ -142,3 +134,49 @@ func verifyCutoff(t *testing.T, chain *BlockChain, canonical bool, inserted type
 		}
 	}
 }
+
+// NewLevelDBDatabase creates a persistent key-value database without a freezer
+// moving immutable chain segments into cold storage.
+func NewLevelDBDatabase(file string, cache int, handles int, namespace string, readonly bool) (ethdb.Database, error) {
+	db, err := leveldb.New(file, nil)
+	if err != nil {
+		return nil, err
+	}
+	return rawdb.NewDatabase(Database{db}), nil
+}
+
+// Database implements ethdb.Database
+type Database struct{ database.Database }
+
+// NewBatch implements ethdb.Database
+func (db Database) NewBatch() ethdb.Batch { return Batch{db.Database.NewBatch()} }
+
+// NewIterator implements ethdb.Database
+//
+// Note: This method assumes that the prefix is NOT part of the start, so there's
+// no need for the caller to prepend the prefix to the start.
+func (db Database) NewIterator(prefix []byte, start []byte) ethdb.Iterator {
+	// avalanchego's database implementation assumes that the prefix is part of the
+	// start, so it is added here (if it is provided).
+	if len(prefix) > 0 {
+		newStart := make([]byte, len(prefix)+len(start))
+		copy(newStart, prefix)
+		copy(newStart[len(prefix):], start)
+		start = newStart
+	}
+	return db.Database.NewIteratorWithStartAndPrefix(start, prefix)
+}
+
+// NewIteratorWithStart implements ethdb.Database
+func (db Database) NewIteratorWithStart(start []byte) ethdb.Iterator {
+	return db.Database.NewIteratorWithStart(start)
+}
+
+// Batch implements ethdb.Batch
+type Batch struct{ database.Batch }
+
+// ValueSize implements ethdb.Batch
+func (batch Batch) ValueSize() int { return batch.Batch.Size() }
+
+// Replay implements ethdb.Batch
+func (batch Batch) Replay(w ethdb.KeyValueWriter) error { return batch.Batch.Replay(w) }
