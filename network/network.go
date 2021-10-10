be20b53269d4e43107156ff420dc5aff05d1689f
abenegia <alberto.benegiamo@gmail.com>
2021-10-10 10:37:52 +0200

    removed code duplication from network

--- network/network.go
+++ network/network.go
@@ -117,7 +117,6 @@ type network struct {
 	dummyNodeID uint32
 	clock       timer.Clock
 	mc          message.MsgCreator
-	*msgsProcessor
 
 	stateLock sync.RWMutex
 	closed    utils.AtomicBool
@@ -307,14 +306,126 @@ func NewNetwork(
 	}
 	netw.outboundMsgThrottler = outboundMsgThrottler
 
-	netw.msgsProcessor = newMsgsProcessor(netw)
-
 	netw.peers.initialize()
 	netw.sendFailRateCalculator = math.NewSyncAverager(math.NewAverager(0, config.MaxSendFailRateHalflife, netw.clock.Time()))
 	if err := netw.metrics.initialize(config.Namespace, metricsRegisterer); err != nil {
 		return nil, fmt.Errorf("initializing network failed with: %s", err)
 	}
 	return netw, nil
+}
+
+// Assumes [n.stateLock] is not held.
+func (n *network) Send(msgType constants.MsgType, msg message.OutboundMessage, nodeIDs ids.ShortSet) ids.ShortSet {
+	var (
+		now    = n.clock.Time()
+		msgLen = len(msg.Bytes())
+		res    = ids.NewShortSet(nodeIDs.Len())
+	)
+
+	op := msg.Op()
+
+	canModifyMsg := false
+	switch op {
+	case message.AcceptedFrontier,
+		message.Accepted,
+		message.GetAncestors,
+		message.MultiPut,
+		message.Get,
+		message.Put,
+		message.Chits,
+		message.AppResponse:
+		canModifyMsg = true
+	}
+
+	msgMetrics := n.metrics.message(op)
+	if msgMetrics == nil {
+		n.log.Error("unregistered metric for message with op %s. Dropping it", op)
+		return res
+	}
+
+	for _, peerElement := range n.getPeers(nodeIDs) {
+		peer := peerElement.peer
+		nodeID := peerElement.id
+		if peer != nil && peer.finishedHandshake.GetValue() && peer.Send(msg, canModifyMsg) {
+			res.Add(nodeID)
+
+			// record metrics for success
+			n.sendFailRateCalculator.Observe(0, now)
+			msgMetrics.numSent.Inc()
+			msgMetrics.sentBytes.Add(float64(msgLen))
+			if saved := msg.BytesSavedCompression(); saved != 0 {
+				msgMetrics.savedSentBytes.Observe(float64(saved))
+			}
+			continue
+		}
+
+		// record metrics for failure
+		n.sendFailRateCalculator.Observe(1, now)
+		msgMetrics.numFailed.Inc()
+	}
+	return res
+}
+
+// Assumes [n.stateLock] is not held.
+func (n *network) Gossip(msgType constants.MsgType, msg message.OutboundMessage, subnetID ids.ID) bool {
+	switch msgType {
+	case constants.AppGossipMsg:
+		return n.appGossip(msg, subnetID)
+	case constants.GossipMsg:
+		return n.gossipContainer(msg, subnetID, int(n.config.GossipOnAcceptSize))
+	default:
+		n.log.Error("Unhandled message type %v. Gossiping nothing.", msgType)
+		return false
+	}
+}
+
+func (n *network) appGossip(msg message.OutboundMessage, subnetID ids.ID) bool {
+	now := n.clock.Time()
+	n.stateLock.RLock()
+	// Gossip the message to [n.config.AppGossipNonValidatorSize] random nodes
+	// in the network.
+	peersAll, err := n.peers.sample(subnetID, false, int(n.config.AppGossipNonValidatorSize))
+	if err != nil {
+		n.log.Debug("failed to sample %d peers for AppGossip: %s", n.config.AppGossipNonValidatorSize, err)
+		n.stateLock.RUnlock()
+		return false
+	}
+
+	// Gossip the message to [n.config.AppGossipValidatorSize] random validators
+	// in the network. This does not gossip by stake - but uniformly to the
+	// validator set.
+	peersValidators, err := n.peers.sample(subnetID, true, int(n.config.AppGossipValidatorSize))
+	n.stateLock.RUnlock()
+	if err != nil {
+		n.log.Debug("failed to sample %d validators for AppGossip: %s", n.config.AppGossipValidatorSize, err)
+		return false
+	}
+
+	sentPeers := ids.ShortSet{}
+	for _, peers := range [][]*peer{peersAll, peersValidators} {
+		for _, peer := range peers {
+			if sentPeers.Contains(peer.nodeID) {
+				continue
+			}
+			sentPeers.Add(peer.nodeID)
+
+			sent := peer.Send(msg, false)
+			if !sent {
+				n.log.Debug("failed to send AppGossip(%s)", peer.nodeID)
+				n.metrics.appGossip.numFailed.Inc()
+				n.sendFailRateCalculator.Observe(1, now)
+				continue
+			}
+
+			n.metrics.appGossip.numSent.Inc()
+			n.metrics.appGossip.sentBytes.Add(float64(len(msg.Bytes())))
+			n.sendFailRateCalculator.Observe(0, now)
+			if saved := msg.BytesSavedCompression(); saved != 0 {
+				n.metrics.appGossip.savedSentBytes.Observe(float64(saved))
+			}
+		}
+	}
+	return true
 }
 
 // Accept is called after every consensus decision
@@ -559,6 +670,38 @@ func (n *network) Track(ip utils.IPDesc, nodeID ids.ShortID) {
 
 func (n *network) IP() utils.IPDesc {
 	return n.currentIP.IP()
+}
+
+// Assumes [n.stateLock] is not held.
+func (n *network) gossipContainer(msg message.OutboundMessage, subnetID ids.ID, sampleSize int) bool {
+	now := n.clock.Time()
+
+	n.stateLock.RLock()
+	peers, err := n.peers.sample(subnetID, false, sampleSize)
+	n.stateLock.RUnlock()
+	if err != nil {
+		return false
+	}
+
+	res := false // gossip is successful if at least one node gets gossiped
+	for _, peer := range peers {
+		sent := peer.Send(msg, false)
+		if !sent {
+			n.sendFailRateCalculator.Observe(1, now)
+			n.metrics.put.numFailed.Inc()
+			continue
+		}
+
+		res = true
+		n.metrics.put.numSent.Inc()
+		n.metrics.put.sentBytes.Add(float64(len(msg.Bytes())))
+		// assume that if [saved] == 0, [msg] wasn't compressed
+		if saved := msg.BytesSavedCompression(); saved != 0 {
+			n.metrics.put.savedSentBytes.Observe(float64(saved))
+		}
+		n.sendFailRateCalculator.Observe(0, now)
+	}
+	return res
 }
 
 // assumes the stateLock is held.
