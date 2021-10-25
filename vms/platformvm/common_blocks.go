1c1c96b7d3c3ad9f71d66f3d9bafc7eeabb8844f
StephenButtolph <stephen@avalabs.org>
2021-10-25 17:26:47 -0400

    nit cleanup comments

--- vms/platformvm/common_blocks.go
+++ vms/platformvm/common_blocks.go
@@ -225,7 +225,7 @@ func (b *CommonBlock) Reject() error {
 	defer b.free()
 
 	b.status = choices.Rejected
-	// remove this one too? (no rejected in db)
+	// TODO: don't write rejected blocks to disk
 	b.vm.internalState.AddBlock(b.self)
 	return b.vm.internalState.Commit()
 }
@@ -272,7 +272,7 @@ func (cdb *CommonDecisionBlock) Reject() error {
 	defer cdb.free()
 
 	cdb.status = choices.Rejected
-	// remove this one too? (no rejected in db)
+	// TODO: don't write rejected blocks to disk
 	cdb.vm.internalState.AddBlock(cdb.self)
 	return cdb.vm.internalState.Commit()
 }
