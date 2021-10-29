6e4177b33d83d4bf7ce5f6b99ccc5c6106bfa79d
Jamie Prince <78679039+xrpdevs@users.noreply.github.com>
2021-10-29 23:08:38 +0100

    Update db.go

--- database/rocksdb/db.go
+++ database/rocksdb/db.go
@@ -13,7 +13,7 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"github.com/linxGnu/grocksdb"
+	"github.com/xrpdevs/grocksdb"
 
 	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/database/nodb"
