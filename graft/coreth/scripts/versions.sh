a5caac651de997d8ecbd138dbe50d2088665a4af
StephenButtolph <stephen@avalabs.org>
2021-10-29 17:08:19 -0400

    fixed version

--- graft/coreth/scripts/versions.sh
+++ graft/coreth/scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.7.1'}
+coreth_version=${CORETH_VERSION:-'v0.7.3'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.6.2-rc.2'}
