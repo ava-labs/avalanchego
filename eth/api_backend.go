a07f894b3c5000844a869894ed87fc1bfde7eb58
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-11-08 12:04:08 -0500

    Migrate eth/tracers/ and related code

--- eth/api_backend.go
+++ eth/api_backend.go
@@ -427,7 +427,7 @@ func (b *EthAPIBackend) GetMaxBlocksPerRequest() int64 {
 	return b.eth.settings.MaxBlocksPerRequest
 }
 
-func (b *EthAPIBackend) StateAtBlock(ctx context.Context, block *types.Block, reexec uint64, base *state.StateDB, checkLive bool) (*state.StateDB, error) {
+func (b *EthAPIBackend) StateAtBlock(ctx context.Context, block *types.Block, reexec uint64, base *state.StateDB, checkLive bool, preferDisk bool) (*state.StateDB, error) {
 	return b.eth.stateAtBlock(block, reexec, base, checkLive)
 }
 
