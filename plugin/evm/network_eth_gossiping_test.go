18d98816f8072b255e0af14a0b2429828057a465
Patrick O'Grady <prohb125@gmail.com>
2021-11-09 15:59:43 -0800

    tests passing

--- plugin/evm/network_eth_gossiping_test.go
+++ plugin/evm/network_eth_gossiping_test.go
@@ -69,6 +69,7 @@ func getValidEthTxs(key *ecdsa.PrivateKey, count int, gasPrice *big.Int) []*type
 				gasPrice,
 				[]byte(strings.Repeat("aaaaaaaaaa", 100))),
 			types.HomesteadSigner{}, key)
+		tx.SetFirstSeen(time.Now().Add(-1 * time.Minute))
 		res[i] = tx
 	}
 	return res
