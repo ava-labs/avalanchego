9a740e18bd9f94581e57d57442b92188c646ac3a
Gyuho Lee <gyuho.lee@avalabs.org>
2021-11-16 16:05:37 -0800

    *: update license header
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- ipcs/socket/socket_unix.go
+++ ipcs/socket/socket_unix.go
@@ -1,7 +1,7 @@
 //go:build !windows && !plan9 && !js
 // +build !windows,!plan9,!js
 
-// (c) 2020, Ava Labs, Inc. All rights reserved.
+// Copyright (C) 2019-2021, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
 package socket
