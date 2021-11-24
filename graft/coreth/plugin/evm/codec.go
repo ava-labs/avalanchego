915f0d30a453020a36b03bb62fcb28328a7c52ae
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-11-24 10:24:57 -0500

    Export extract atomic txs helpers

--- graft/coreth/plugin/evm/codec.go
+++ graft/coreth/plugin/evm/codec.go
@@ -4,10 +4,13 @@
 package evm
 
 import (
+	"fmt"
+
 	"github.com/ava-labs/avalanchego/codec"
 	"github.com/ava-labs/avalanchego/codec/linearcodec"
 	"github.com/ava-labs/avalanchego/utils/wrappers"
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
+	"github.com/ava-labs/coreth/core/types"
 )
 
 // Codec does serialization and deserialization
@@ -37,4 +40,55 @@ func init() {
 	if errs.Errored() {
 		panic(errs.Err)
 	}
+}
+
+// extractAtomicTxs returns the atomic transactions in [block] if
+// they exist.
+func ExtractAtomicTxs(block *types.Block, isApricotPhase5 bool) ([]*Tx, error) {
+	atomicTxBytes := block.ExtData()
+	if len(atomicTxBytes) == 0 {
+		return nil, nil
+	}
+
+	if !isApricotPhase5 {
+		return ExtractAtomicTxsPreApricotPhase5(atomicTxBytes)
+	}
+	return ExtractAtomicTxsPostApricotPhase5(atomicTxBytes)
+}
+
+// [ExtractAtomicTxsPreApricotPhase5] extracts a singular atomic transaction from [atomicTxBytes]
+// and returns a slice of atomic transactions for compatibility with the type returned post
+// ApricotPhase5.
+// Note: this function assumes [atomicTxBytes] is non-empty.
+func ExtractAtomicTxsPreApricotPhase5(atomicTxBytes []byte) ([]*Tx, error) {
+	atomicTx := new(Tx)
+	if _, err := Codec.Unmarshal(atomicTxBytes, atomicTx); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal atomic transaction (pre-AP3): %w", err)
+	}
+	if err := atomicTx.Sign(Codec, nil); err != nil {
+		return nil, fmt.Errorf("failed to initialize singleton atomic tx due to: %w", err)
+	}
+	return []*Tx{atomicTx}, nil
+}
+
+// [ExtractAtomicTxsPostApricotPhase5] extracts a slice of atomic transactions from [atomicTxBytes].
+// Note: this function assumes [atomicTxBytes] is non-empty.
+func ExtractAtomicTxsPostApricotPhase5(atomicTxBytes []byte) ([]*Tx, error) {
+	var atomicTxs []*Tx
+	if _, err := Codec.Unmarshal(atomicTxBytes, &atomicTxs); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal atomic tx (AP5) due to %w", err)
+	}
+
+	// Do not allow non-empty extra data field to contain zero atomic transactions. This would allow
+	// people to construct a block that contains useless data.
+	if len(atomicTxs) == 0 {
+		return nil, errMissingAtomicTxs
+	}
+
+	for index, atx := range atomicTxs {
+		if err := atx.Sign(Codec, nil); err != nil {
+			return nil, fmt.Errorf("failed to initialize atomic tx at index %d: %w", index, err)
+		}
+	}
+	return atomicTxs, nil
 }
