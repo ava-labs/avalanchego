6dee9bac4d2bf63716fb26b72f19b0a9d37fc4f2
manthanhd <manthanhd@live.com>
2021-11-25 17:53:39 +0000

    memory optimisation for apricot phase 5 (-18%)

--- plugin/evm/atomic_tx_repository.go
+++ plugin/evm/atomic_tx_repository.go
@@ -24,7 +24,7 @@ var (
 // AtomicTxRepository defines an entity that manages storage and indexing of
 // atomic transactions
 type AtomicTxRepository interface {
-	Initialize() error
+	Initialize(apricotPhase5Height uint64) error
 	GetByTxID(txID ids.ID) (*Tx, uint64, error)
 	GetByHeight(height uint64) ([]*Tx, error)
 	Write(height uint64, txs []*Tx) error
@@ -59,7 +59,7 @@ func NewAtomicTxRepository(db database.Database, codec codec.Manager) AtomicTxRe
 	}
 }
 
-func (a *atomicTxRepository) Initialize() error {
+func (a *atomicTxRepository) Initialize(apricotPhase5Height uint64) error {
 	startTime := time.Now()
 	indexHeight := uint64(0)
 	exists, indexHeight, err := a.getIndexHeight()
@@ -101,15 +101,20 @@ func (a *atomicTxRepository) Initialize() error {
 			return err
 		}
 
-		// If this height is already processed, get existing txs for that height.
-		if _, exists := seenHeights[height]; exists {
-			existingTxs, err := a.GetByHeight(height)
-			if err != nil {
-				return err
+		// Past apricotPhase5 we allow multiple txs per block
+		// This ensures that we keep the memory footprint low
+		// by not storing block heights for blocks prior
+		if height >= apricotPhase5Height {
+			// If this height is already processed, get existing txs for that height.
+			if _, exists := seenHeights[height]; exists {
+				existingTxs, err := a.GetByHeight(height)
+				if err != nil {
+					return err
+				}
+				txs = append(existingTxs, txs...)
 			}
-			txs = append(existingTxs, txs...)
+			seenHeights[height] = struct{}{}
 		}
-		seenHeights[height] = struct{}{}
 
 		txsBytes, err := a.codec.Marshal(codecVersion, txs)
 		if err != nil {
