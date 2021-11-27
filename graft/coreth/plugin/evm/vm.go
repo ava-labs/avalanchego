a36e53a09c88ab1bb0d21d0db43063068cf6d1dc
Darioush Jalali <darioush.jalali@avalabs.org>
2021-11-26 18:10:59 -0600

    commit each 10mb, test cleanup

--- graft/coreth/plugin/evm/vm.go
+++ graft/coreth/plugin/evm/vm.go
@@ -379,12 +379,8 @@ func (vm *VM) Initialize(
 	lastAccepted := vm.chain.LastAcceptedBlock()
 
 	vm.atomicTxRepository = NewAtomicTxRepository(vm.db, vm.codec)
-	if err := vm.atomicTxRepository.Initialize(); err != nil {
+	if err := vm.atomicTxRepository.Initialize(vm.db.Commit); err != nil {
 		log.Error("failed to initialise atomic tx repository", "err", err)
-		return err
-	}
-
-	if err := vm.db.Commit(); err != nil {
 		return err
 	}
 
