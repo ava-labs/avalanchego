a3ba3feb0479c04ba4e8e847cec29aa0c252c699
Felipe Madero <felipe.madero@gmail.com>
2021-12-28 10:00:20 -0300

    fix test compilation error
    address PR comments on dummy consensus modes

--- graft/coreth/core/bench_test.go
+++ graft/coreth/core/bench_test.go
@@ -234,7 +234,6 @@ func makeChainForBench(db ethdb.Database, full bool, count uint64) {
 
 		rawdb.WriteHeader(db, header)
 		rawdb.WriteCanonicalHash(db, hash, n)
-		rawdb.WriteTd(db, hash, n, big.NewInt(int64(n+1)))
 
 		if full || n == 0 {
 			block := types.NewBlockWithHeader(header)
