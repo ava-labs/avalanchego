3d2810d53ebced96484eabf0bec45634846f5d4b
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2022-01-04 18:28:00 +0100

    Slimmed down chain and chains.registrant interface (#1102)

--- chains/registrant.go
+++ chains/registrant.go
@@ -4,14 +4,13 @@
 package chains
 
 import (
-	"github.com/ava-labs/avalanchego/snow"
 	"github.com/ava-labs/avalanchego/snow/engine/common"
 )
 
 // Registrant can register the existence of a chain
 type Registrant interface {
-	// Called when the chain described by [ctx] and [engine] is created
+	// Called when the chain described by [engine] is created
 	// This function is called before the chain starts processing messages
 	// [engine] should be an avalanche.Engine or snowman.Engine
-	RegisterChain(name string, ctx *snow.ConsensusContext, engine common.Engine)
+	RegisterChain(name string, engine common.Engine)
 }
