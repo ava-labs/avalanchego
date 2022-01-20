d060edd9798c6907da952268fc0c7179f79b7d58
aaronbuchwald <aaron.buchwald56@gmail.com>
2022-01-20 13:29:23 -0500

    Remove redundant error check from bootstrapper (#1144)

--- snow/engine/snowman/bootstrap/bootstrapper.go
+++ snow/engine/snowman/bootstrap/bootstrapper.go
@@ -476,8 +476,5 @@ func (b *bootstrapper) finish() error {
 	}
 
 	// Start consensus
-	if err := b.OnFinished(b.Config.SharedCfg.RequestID); err != nil {
-		return err
-	}
-	return nil
+	return b.OnFinished(b.Config.SharedCfg.RequestID)
 }
