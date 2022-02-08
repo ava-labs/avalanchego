ba536c5cc6dc3ef67851a57a303d514cf0d60eb9
abenegia <alberto.benegiamo@gmail.com>
2022-02-08 13:16:17 +0100

    nits

--- snow/engine/common/state_syncable_vm.go
+++ snow/engine/common/state_syncable_vm.go
@@ -21,18 +21,20 @@ type Summary struct {
 // rather then boostrapping from genesis.
 // common.StateSyncableVM can be detailed for Snowman or Avalanche-like VMs by extending the interface.
 type StateSyncableVM interface {
-	// Register nodes from which state sync summaries can be downloaded
+	// RegisterStateSyncer registers nodes from which state sync summaries can be downloaded
 	RegisterStateSyncer(stateSyncer []ids.ShortID) error
 
-	// Enabled indicates whether the state sync is enabled for this VM
+	// StateSyncEnabled indicates whether the state sync is enabled for this VM
 	StateSyncEnabled() (bool, error)
-	// StateSummary returns latest Summary with an optional error
+
+	// StateSyncGetLastSummary returns latest Summary with an optional error
 	StateSyncGetLastSummary() (Summary, error)
-	// IsAccepted returns true if input []bytes represent a valid state summary
+
+	// StateSyncIsSummaryAccepted returns true if input []bytes represent a valid state summary
 	// for state sync.
 	StateSyncIsSummaryAccepted(key []byte) (bool, error)
 
-	// SyncState is called with a list of valid summaries to sync from.
+	// StateSync is called with a list of valid summaries to sync from.
 	// These summaries were collected from peers and validated with validators.
 	// VM will use information inside the summary to choose one and sync
 	// its state to that summary. Normal bootstrapping resumes after this
