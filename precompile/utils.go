40fa4da342955ee3615fdfc0bd150522882c3f2e
Aaron Buchwald <aaron.buchwald56@gmail.com>
2022-02-26 13:43:27 -0500

    Add helper for calculating a function selector

--- precompile/utils.go
+++ precompile/utils.go
@@ -5,7 +5,16 @@ package precompile
 
 import (
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 )
+
+// CalculateFunctionSelector returns the 4 byte function selector that results from [functionSignature]
+// Ex. the function setBalance(addr address, balance uint256) should be passed in as the string:
+// "setBalance(address,uint256)"
+func CalculateFunctionSelector(functionSignature string) []byte {
+	hash := crypto.Keccak256([]byte(functionSignature))
+	return hash[:4]
+}
 
 // CreateAddressKey converts [address] into a [common.Hash] value to be used as a storage slot key
 func CreateAddressKey(address common.Address) common.Hash {
