a572355567762dd7dd22802a57c4a883443509f0
Patrick O'Grady <prohb125@gmail.com>
2022-02-28 10:18:22 -0800

    limit number of blocks on a fetch

--- eth/gasprice/feehistory.go
+++ eth/gasprice/feehistory.go
@@ -204,9 +204,9 @@ func (oracle *Oracle) FeeHistory(ctx context.Context, blocks int, unresolvedLast
 	if blocks < 1 {
 		return common.Big0, nil, nil, nil, nil // returning with no data and no error means there are no retrievable blocks
 	}
-	if blocks > oracle.maxBlockHistory {
-		log.Warn("Sanitizing fee history length", "requested", blocks, "truncated", oracle.maxBlockHistory)
-		blocks = oracle.maxBlockHistory
+	if blocks > oracle.maxCallBlockHistory {
+		log.Warn("Sanitizing fee history length", "requested", blocks, "truncated", oracle.maxCallBlockHistory)
+		blocks = oracle.maxCallBlockHistory
 	}
 	for i, p := range rewardPercentiles {
 		if p < 0 || p > 100 {
