c5750601f1752f8f0025fc5bbd126263f2c1eb6c
abenegia <alberto.benegiamo@gmail.com>
2022-03-01 14:16:39 +0100

    added UT

--- snow/engine/snowman/syncer/state_syncer_test.go
+++ snow/engine/snowman/syncer/state_syncer_test.go
@@ -6,6 +6,7 @@ package syncer
 import (
 	"testing"
 
+	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow"
 	"github.com/ava-labs/avalanchego/snow/engine/common"
 	"github.com/ava-labs/avalanchego/snow/engine/common/tracker"
@@ -77,4 +78,67 @@ func TestStateSyncIsSkippedIfNoBeaconIsProvided(t *testing.T) {
 
 	// check that StateSync is called immediately with no frontiers
 	assert.True(stateSyncEmpty)
+}
+
+func TestBeaconsAreReachedForFrontierUponStartup(t *testing.T) {
+	assert := assert.New(t)
+
+	beacons := validators.NewSet()
+	beaconsIDs := []ids.ShortID{
+		ids.GenerateTestShortID(),
+		ids.GenerateTestShortID(),
+		ids.GenerateTestShortID(),
+	}
+	for _, beaconID := range beaconsIDs {
+		assert.NoError(beacons.AddWeight(beaconID, uint64(1)))
+	}
+
+	sender := &common.SenderTest{T: t}
+	fullVM := &fullVM{
+		TestVM: &block.TestVM{
+			TestVM: common.TestVM{T: t},
+		},
+		TestStateSyncableVM: &block.TestStateSyncableVM{
+			TestStateSyncableVM: common.TestStateSyncableVM{T: t},
+		},
+	}
+
+	commonCfg := common.Config{
+		Ctx:     snow.DefaultConsensusContextTest(),
+		Beacons: beacons,
+		SampleK: beacons.Len(),
+		Alpha:   (beacons.Weight() + 1) / 2,
+		Sender:  sender,
+	}
+	dummyGetter, err := getter.New(fullVM, commonCfg)
+	assert.NoError(err)
+	dummyWeightTracker := tracker.NewWeightTracker(beacons, commonCfg.StartupAlpha)
+
+	cfg, err := NewConfig(
+		commonCfg,
+		nil,
+		dummyGetter,
+		fullVM,
+		dummyWeightTracker)
+	assert.NoError(err)
+	commonSyncer := New(cfg, func(lastReqID uint32) error { return nil })
+	syncer, ok := commonSyncer.(*stateSyncer)
+	assert.True(ok)
+	assert.True(syncer.stateSyncVM != nil)
+
+	// set sender to track nodes reached out
+	contactedBeacons := ids.NewShortSet(3)
+	sender.CantSendGetStateSummaryFrontier = true
+	sender.SendGetStateSummaryFrontierF = func(ss ids.ShortSet, u uint32) {
+		contactedBeacons.Union(ss)
+	}
+
+	// check Start returns no errors
+	assert.NoError(syncer.Start(uint32(0) /*startReqID*/))
+
+	// check that all the beacons are reached out for frontiers
+	assert.True(len(beaconsIDs) == len(contactedBeacons))
+	for _, beaconID := range beaconsIDs {
+		assert.True(contactedBeacons.Contains(beaconID))
+	}
 }
