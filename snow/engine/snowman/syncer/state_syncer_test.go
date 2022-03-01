2f6164f2834257a898bb385edc48601470e7169f
abenegia <alberto.benegiamo@gmail.com>
2022-03-01 16:12:14 +0100

    Added UT

--- snow/engine/snowman/syncer/state_syncer_test.go
+++ snow/engine/snowman/syncer/state_syncer_test.go
@@ -5,6 +5,7 @@ package syncer
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 
 	"github.com/ava-labs/avalanchego/ids"
@@ -150,7 +151,7 @@ func TestBeaconsAreReachedForFrontierUponStartup(t *testing.T) {
 	assert.True(len(syncer.weightedSummaries) == 0)
 }
 
-func TestUnrequestedStateSummaryFrontiersAreDropped(t *testing.T) {
+func TestUnRequestedStateSummaryFrontiersAreDropped(t *testing.T) {
 	assert := assert.New(t)
 
 	beacons := validators.NewSet()
@@ -208,7 +209,7 @@ func TestUnrequestedStateSummaryFrontiersAreDropped(t *testing.T) {
 	// check Start returns no errors
 	assert.NoError(syncer.Start(uint32(0) /*startReqID*/))
 
-	// pick one of the beacons that have been reached out
+	// mock VM to simulate a valid summary is returned
 	key := []byte{'k', 'e', 'y'}
 	summary := []byte{'s', 'u', 'm', 'm', 'a', 'r', 'y'}
 	hash := []byte{'h', 'a', 's', 'h'}
@@ -218,6 +219,7 @@ func TestUnrequestedStateSummaryFrontiersAreDropped(t *testing.T) {
 		return key, hash, nil
 	}
 
+	// pick one of the beacons that have been reached out
 	responsiveBeaconID := beaconsIDs[0]
 	responsiveBeaconReqID := contactedBeacons[responsiveBeaconID]
 
@@ -245,7 +247,96 @@ func TestUnrequestedStateSummaryFrontiersAreDropped(t *testing.T) {
 		responsiveBeaconReqID,
 		summary,
 	))
+
+	// responsiveBeacon not pending anymore
+	assert.False(syncer.hasSeederBeenContacted(responsiveBeaconID))
+
+	// valid summary is recorded
 	ws, ok := syncer.weightedSummaries[string(hash)]
 	assert.True(ok)
 	assert.True(bytes.Equal(ws.Summary, summary))
+}
+
+func TestMalformedStateSummaryFrontiersAreDropped(t *testing.T) {
+	assert := assert.New(t)
+
+	beacons := validators.NewSet()
+	beaconsIDs := []ids.ShortID{
+		ids.GenerateTestShortID(),
+	}
+	for _, beaconID := range beaconsIDs {
+		assert.NoError(beacons.AddWeight(beaconID, uint64(1)))
+	}
+
+	sender := &common.SenderTest{T: t}
+	fullVM := &fullVM{
+		TestVM: &block.TestVM{
+			TestVM: common.TestVM{T: t},
+		},
+		TestStateSyncableVM: &block.TestStateSyncableVM{
+			TestStateSyncableVM: common.TestStateSyncableVM{T: t},
+		},
+	}
+
+	commonCfg := common.Config{
+		Ctx:     snow.DefaultConsensusContextTest(),
+		Beacons: beacons,
+		SampleK: beacons.Len(),
+		Alpha:   (beacons.Weight() + 1) / 2,
+		Sender:  sender,
+	}
+	dummyGetter, err := getter.New(fullVM, commonCfg)
+	assert.NoError(err)
+	dummyWeightTracker := tracker.NewWeightTracker(beacons, commonCfg.StartupAlpha)
+
+	cfg, err := NewConfig(
+		commonCfg,
+		nil,
+		dummyGetter,
+		fullVM,
+		dummyWeightTracker)
+	assert.NoError(err)
+	commonSyncer := New(cfg, func(lastReqID uint32) error { return nil })
+	syncer, ok := commonSyncer.(*stateSyncer)
+	assert.True(ok)
+	assert.True(syncer.stateSyncVM != nil)
+
+	// set sender to track nodes reached out
+	contactedBeacons := make(map[ids.ShortID]uint32) // nodeID -> reqID map
+	sender.CantSendGetStateSummaryFrontier = true
+	sender.SendGetStateSummaryFrontierF = func(ss ids.ShortSet, reqID uint32) {
+		for nodeID := range ss {
+			contactedBeacons[nodeID] = reqID
+		}
+	}
+
+	// check Start returns no errors
+	assert.NoError(syncer.Start(uint32(0) /*startReqID*/))
+
+	// mock VM to simulate an invalid summary is returned
+	summary := []byte{'s', 'u', 'm', 'm', 'a', 'r', 'y'}
+	isSummaryDecoded := false
+	fullVM.CantStateSyncGetKeyHash = true
+	fullVM.StateSyncGetKeyHashF = func(s common.Summary) (common.SummaryKey, common.SummaryHash, error) {
+		isSummaryDecoded = true
+		return nil, nil, fmt.Errorf("invalid state summary")
+	}
+
+	// pick one of the beacons that have been reached out
+	responsiveBeaconID := beaconsIDs[0]
+	responsiveBeaconReqID := contactedBeacons[responsiveBeaconID]
+
+	// response is valid, but invalid summary is not recorded
+	assert.NoError(syncer.StateSummaryFrontier(
+		responsiveBeaconID,
+		responsiveBeaconReqID,
+		summary,
+	))
+
+	// responsiveBeacon not pending anymore
+	assert.False(syncer.hasSeederBeenContacted(responsiveBeaconID))
+
+	// invalid summary is not recorded
+	assert.True(isSummaryDecoded)
+	assert.True(len(syncer.weightedSummaries) == 0)
 }
