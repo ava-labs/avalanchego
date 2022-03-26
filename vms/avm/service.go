8a42519fe8fb6145bd988d1b3ba0cd1bcfce8ac8
c4t <c4t@videvago.com>
2022-03-26 12:28:53 +0000

    Return feeAsset when requestion empty avm.getAssetDescription

--- vms/avm/service.go
+++ vms/avm/service.go
@@ -298,9 +298,16 @@ type GetAssetDescriptionReply struct {
 func (service *Service) GetAssetDescription(_ *http.Request, args *GetAssetDescriptionArgs, reply *GetAssetDescriptionReply) error {
 	service.vm.ctx.Log.Debug("AVM: GetAssetDescription called with %s", args.AssetID)
 
-	assetID, err := service.vm.lookupAssetID(args.AssetID)
-	if err != nil {
-		return err
+	var assetID ids.ID
+	if args.AssetID == "" {
+		assetID = service.vm.feeAssetID
+	} else {
+		var err error
+
+		assetID, err = service.vm.lookupAssetID(args.AssetID)
+		if err != nil {
+			return err
+		}
 	}
 
 	tx := &UniqueTx{
