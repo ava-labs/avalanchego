098fc9c5c75bab4352c629c33df698dc0262eaca
Aaron Buchwald <aaron.buchwald56@gmail.com>
2022-04-07 11:36:56 -0400

    Require min go version of v1.17.9

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+go_version_minimum="1.17.9"
+
+go_version() {
+    go version | sed -nE -e 's/[^0-9.]+([0-9.]+).+/\1/p'
+}
+
+version_lt() {
+    # Return true if $1 is a lower version than than $2,
+    local ver1=$1
+    local ver2=$2
+    # Reverse sort the versions, if the 1st item != ver1 then ver1 < ver2
+    if  [[ $(echo -e -n "$ver1\n$ver2\n" | sort -rV | head -n1) != "$ver1" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if version_lt "$(go_version)" "$go_version_minimum"; then
+    echo "AvalancheGo requires Go >= $go_version_minimum, Go $(go_version) found." >&2
+    exit 1
+fi
+
 # Avalanche root directory
 SUBNET_EVM_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
 
