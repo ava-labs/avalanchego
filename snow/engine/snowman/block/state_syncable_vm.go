65d40dcabb35639bc1ec29d2d7aa46d36ba48583
abenegia <alberto.benegiamo@gmail.com>
2022-04-07 19:21:25 +0200

    comment nits

--- snow/engine/snowman/block/state_syncable_vm.go
+++ snow/engine/snowman/block/state_syncable_vm.go
@@ -13,9 +13,9 @@ type StateSyncableVM interface {
 
 	// VM State Sync process must run asynchronously; morever, once it is done,
 	// the full block associated with synced summary must be downloaded from
-	// the network. StateSyncGetResult returns
-	// height and ID of this block to allow its retrival from network
-	// error state of the whole StateSync process so far
+	// the network. StateSyncGetResult returns:
+	// 1- height and ID of this block to allow its retrival from network
+	// 2- error state of the whole StateSync process so far
 	StateSyncGetResult() (ids.ID, uint64, error)
 
 	// StateSyncSetLastSummaryBlock pass to VM the network-retrieved block associated with its last state summary
