378bc73fa83fd02ba996059851efa77163f4f860
abenegia <alberto.benegiamo@gmail.com>
2022-04-20 18:57:12 +0200

    nit

--- network/network.go
+++ network/network.go
@@ -358,7 +358,6 @@ func (n *network) Connected(nodeID ids.ShortID) {
 // provided nodeID. If the node is attempting to connect to the minimum number
 // of peers, then it should only connect if this node is a validator, or the
 // peer is a validator/beacon.
-// WantsConnection requires n.peersLock already locked
 func (n *network) AllowConnection(nodeID ids.ShortID) bool {
 	return !n.config.RequireValidatorToConnect ||
 		n.config.Validators.Contains(constants.PrimaryNetworkID, n.config.MyNodeID) ||
