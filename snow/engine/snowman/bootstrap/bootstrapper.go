a1b4a4426830e4651f4157962ec7aa1adf3d19c9
aaronbuchwald <aaron.buchwald56@gmail.com>
2022-04-22 13:15:24 -0400

    Add error message to snowman bootstrap debug logs (#1375)

--- snow/engine/snowman/bootstrap/bootstrapper.go
+++ snow/engine/snowman/bootstrap/bootstrapper.go
@@ -130,7 +130,7 @@ func (b *bootstrapper) Ancestors(vdr ids.ShortID, requestID uint32, blks [][]byt
 
 	blocks, err := block.BatchedParseBlock(b.VM, blks)
 	if err != nil { // the provided blocks couldn't be parsed
-		b.Ctx.Log.Debug("failed to parse blocks in Ancestors from %s with ID %d", vdr, requestID)
+		b.Ctx.Log.Debug("failed to parse blocks in Ancestors from %s with ID %d: %s", vdr, requestID, err)
 		return b.fetch(wantedBlkID)
 	}
 
