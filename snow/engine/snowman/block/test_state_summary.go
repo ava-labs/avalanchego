d1403f72d617e61864060bd9efa52b3443e7988a
Darioush Jalali <darioush.jalali@avalabs.org>
2022-04-25 17:10:24 -0500

    remove blockID from summary

--- snow/engine/snowman/block/test_state_summary.go
+++ snow/engine/snowman/block/test_state_summary.go
@@ -17,20 +17,18 @@ var (
 )
 
 type TestSummary struct {
-	HeightV  uint64
-	IDV      ids.ID
-	BytesV   []byte
-	BlockIDV ids.ID
+	HeightV uint64
+	IDV     ids.ID
+	BytesV  []byte
 
 	T          *testing.T
 	CantAccept bool
 	AcceptF    func() (bool, error)
 }
 
-func (s *TestSummary) Bytes() []byte   { return s.BytesV }
-func (s *TestSummary) Height() uint64  { return s.HeightV }
-func (s *TestSummary) ID() ids.ID      { return s.IDV }
-func (s *TestSummary) BlockID() ids.ID { return s.IDV }
+func (s *TestSummary) Bytes() []byte  { return s.BytesV }
+func (s *TestSummary) Height() uint64 { return s.HeightV }
+func (s *TestSummary) ID() ids.ID     { return s.IDV }
 func (s *TestSummary) Accept() (bool, error) {
 	if s.AcceptF != nil {
 		return s.AcceptF()
