26d56cf98c9adc889d63d5611bdbd1c75a913ab1
Stephen Buttolph <stephen@avalabs.org>
2022-05-02 16:09:46 -0400

    Add StateSummary definition to the ProposerVM (#1421)
    Co-authored-by: abenegia <alberto.benegiamo@gmail.com>
    Co-authored-by: Darioush Jalali <darioush.jalali@avalabs.org>

---
+++ vms/proposervm/summary/build_test.go
@@ -0,0 +1,22 @@
+// Copyright (C) 2019-2021, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package summary
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuild(t *testing.T) {
+	assert := assert.New(t)
+
+	block := []byte("blockBytes")
+	coreSummary := []byte("coreSummary")
+	builtSummary, err := Build(block, coreSummary)
+	assert.NoError(err)
+
+	assert.Equal(builtSummary.BlockBytes(), block)
+	assert.Equal(builtSummary.InnerSummaryBytes(), coreSummary)
+}
---
+++ vms/proposervm/summary/parse_test.go
@@ -0,0 +1,37 @@
+// Copyright (C) 2019-2021, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package summary
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParse(t *testing.T) {
+	assert := assert.New(t)
+
+	block := []byte("blockBytes")
+	coreSummary := []byte("coreSummary")
+	builtSummary, err := Build(block, coreSummary)
+	assert.NoError(err)
+
+	summaryBytes := builtSummary.Bytes()
+	parsedSummary, err := Parse(summaryBytes)
+	assert.NoError(err)
+
+	assert.Equal(builtSummary.Bytes(), parsedSummary.Bytes())
+	assert.Equal(builtSummary.ID(), parsedSummary.ID())
+	assert.Equal(builtSummary.BlockBytes(), parsedSummary.BlockBytes())
+	assert.Equal(builtSummary.InnerSummaryBytes(), parsedSummary.InnerSummaryBytes())
+}
+
+func TestParseGibberish(t *testing.T) {
+	assert := assert.New(t)
+
+	bytes := []byte{0, 1, 2, 3, 4, 5}
+
+	_, err := Parse(bytes)
+	assert.Error(err)
+}
