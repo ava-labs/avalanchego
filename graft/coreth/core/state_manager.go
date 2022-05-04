7c50faf080319e775d8c0ff7a12a48b3caf012b0
aaronbuchwald <aaron.buchwald56@gmail.com>
2022-05-04 12:15:13 -0400

    Supply random seed in state manager (#735)

--- graft/coreth/core/state_manager.go
+++ graft/coreth/core/state_manager.go
@@ -29,11 +29,16 @@ package core
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/ava-labs/coreth/core/types"
 	"github.com/ava-labs/coreth/ethdb"
 	"github.com/ethereum/go-ethereum/common"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 const (
 	commitInterval = 4096
