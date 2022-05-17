1b8b3da0f80f8fe3ef06e9902b889964f789e6ab
Stephen <stephen@avalabs.org>
2022-05-16 20:17:22 -0400

    remove useless code

--- utils/formatting/address/converter.go
+++ utils/formatting/address/converter.go
@@ -7,26 +7,6 @@ import (
 	"github.com/ava-labs/avalanchego/ids"
 )
 
-// ConvertAddresses converts a list of addresses with arbitrary chains and HRPs
-// (e.g. X-local1....) to a list of addresses with the provided format
-// (e.g. P-custom1...).
-func ConvertAddresses(destChain string, toHRP string, addresses []string) ([]string, error) {
-	convertedAddrs := make([]string, len(addresses))
-	for i, addr := range addresses {
-		_, _, addrBytes, err := Parse(addr)
-		if err != nil {
-			return nil, err
-		}
-
-		newAddrStr, err := Format(destChain, toHRP, addrBytes)
-		if err != nil {
-			return nil, err
-		}
-		convertedAddrs[i] = newAddrStr
-	}
-	return convertedAddrs, nil
-}
-
 func ParseToID(addrStr string) (ids.ShortID, error) {
 	_, _, addrBytes, err := Parse(addrStr)
 	if err != nil {
