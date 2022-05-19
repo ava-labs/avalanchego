b565fb00a152cd574dc2cfb149bdc224d88a485e
Felipe Madero <felipe.madero@gmail.com>
2022-05-19 19:26:37 -0300

    add another unit test case

--- vms/avm/client_test.go
+++ vms/avm/client_test.go
@@ -34,55 +34,92 @@ func (mc *mockClient) SendRequest(
 func TestCreateAsset(t *testing.T) {
 	assert := assert.New(t)
 	client := client{}
-	clientHolders := []*ClientHolder{
-		{
-			Amount:  11,
-			Address: ids.GenerateTestShortID(),
-		},
+	{
+		// empty slices
+		clientHolders := []*ClientHolder{}
+		clientMinters := []ClientOwners{}
+		clientFrom := []ids.ShortID{}
+		clientChangeAddr := ids.GenerateTestShortID()
+		serviceHolders := []*Holder{}
+		serviceMinters := []Owners{}
+		serviceFrom := []string{}
+		serviceChangeAddr := clientChangeAddr.String()
+		expectedInData := &CreateAssetArgs{
+			JSONSpendHeader: api.JSONSpendHeader{
+				JSONFromAddrs:  api.JSONFromAddrs{From: serviceFrom},
+				JSONChangeAddr: api.JSONChangeAddr{ChangeAddr: serviceChangeAddr},
+			},
+			InitialHolders: serviceHolders,
+			MinterSets:     serviceMinters,
+		}
+		client.requester = &mockClient{
+			assert:         assert,
+			expectedInData: expectedInData,
+		}
+		_, _ = client.CreateAsset(
+			context.Background(),
+			api.UserPass{},
+			clientFrom,
+			clientChangeAddr,
+			"",
+			"",
+			0,
+			clientHolders,
+			clientMinters,
+		)
 	}
-	clientMinters := []ClientOwners{
-		{
-			Threshold: 22,
-			Minters:   []ids.ShortID{ids.GenerateTestShortID()},
-		},
+	{
+		// non empty slices
+		clientHolders := []*ClientHolder{
+			{
+				Amount:  11,
+				Address: ids.GenerateTestShortID(),
+			},
+		}
+		clientMinters := []ClientOwners{
+			{
+				Threshold: 22,
+				Minters:   []ids.ShortID{ids.GenerateTestShortID()},
+			},
+		}
+		clientFrom := []ids.ShortID{ids.GenerateTestShortID()}
+		clientChangeAddr := ids.GenerateTestShortID()
+		serviceHolders := []*Holder{
+			{
+				Amount:  json.Uint64(clientHolders[0].Amount),
+				Address: clientHolders[0].Address.String(),
+			},
+		}
+		serviceMinters := []Owners{
+			{
+				Threshold: json.Uint32(clientMinters[0].Threshold),
+				Minters:   []string{clientMinters[0].Minters[0].String()},
+			},
+		}
+		serviceFrom := []string{clientFrom[0].String()}
+		serviceChangeAddr := clientChangeAddr.String()
+		expectedInData := &CreateAssetArgs{
+			JSONSpendHeader: api.JSONSpendHeader{
+				JSONFromAddrs:  api.JSONFromAddrs{From: serviceFrom},
+				JSONChangeAddr: api.JSONChangeAddr{ChangeAddr: serviceChangeAddr},
+			},
+			InitialHolders: serviceHolders,
+			MinterSets:     serviceMinters,
+		}
+		client.requester = &mockClient{
+			assert:         assert,
+			expectedInData: expectedInData,
+		}
+		_, _ = client.CreateAsset(
+			context.Background(),
+			api.UserPass{},
+			clientFrom,
+			clientChangeAddr,
+			"",
+			"",
+			0,
+			clientHolders,
+			clientMinters,
+		)
 	}
-	clientFrom := []ids.ShortID{ids.GenerateTestShortID()}
-	clientChangeAddr := ids.GenerateTestShortID()
-	serviceHolders := []*Holder{
-		{
-			Amount:  json.Uint64(clientHolders[0].Amount),
-			Address: clientHolders[0].Address.String(),
-		},
-	}
-	serviceMinters := []Owners{
-		{
-			Threshold: json.Uint32(clientMinters[0].Threshold),
-			Minters:   []string{clientMinters[0].Minters[0].String()},
-		},
-	}
-	serviceFrom := []string{clientFrom[0].String()}
-	serviceChangeAddr := clientChangeAddr.String()
-	expectedInData := &CreateAssetArgs{
-		JSONSpendHeader: api.JSONSpendHeader{
-			JSONFromAddrs:  api.JSONFromAddrs{From: serviceFrom},
-			JSONChangeAddr: api.JSONChangeAddr{ChangeAddr: serviceChangeAddr},
-		},
-		InitialHolders: serviceHolders,
-		MinterSets:     serviceMinters,
-	}
-	client.requester = &mockClient{
-		assert:         assert,
-		expectedInData: expectedInData,
-	}
-	client.CreateAsset(
-		context.Background(),
-		api.UserPass{},
-		clientFrom,
-		clientChangeAddr,
-		"",
-		"",
-		0,
-		clientHolders,
-		clientMinters,
-	)
 }
