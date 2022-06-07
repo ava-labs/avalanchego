4614e1c0844ec2cb82ec4069fad9e0c2acf5f21f
Darioush Jalali <darioush.jalali@avalabs.org>
2022-06-07 10:26:01 -0700

    minor sync eta fix (#799)
    * minor sync eta fix
    
    * favoring a more explicit approach
    
    * add eta to log message
    
    * add comment
    
    * pr comments
    
    * fix compile

--- graft/coreth/sync/statesync/state_syncer_eta.go
+++ graft/coreth/sync/statesync/state_syncer_eta.go
@@ -15,31 +15,40 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 )
 
+var _ SyncETA = &syncETA{}
+
 const updateInterval = 1 * time.Minute
 
-type syncETA struct {
-	lock            sync.Mutex
-	largestTrieEta  time.Duration
-	largestTrieRoot common.Hash
-	mainTrieRoot    common.Hash
+type SyncETA interface {
+	// NotifyProgress is called when leafs are received to estimate progress and
+	// updates the overall ETA if needed, logging a message if it has been more
+	// than [updateInterval] since the last update.
+	NotifyProgress(root common.Hash, startTime time.Time, startKey []byte, key []byte)
 
-	lastUpdate time.Time
+	// RemoveSyncedTrie is called when a trie is done syncing, so it will no longer
+	// be considered for calculating the ETA.
+	RemoveSyncedTrie(root common.Hash, skipped bool)
+}
+
+type syncETA struct {
+	lock         sync.Mutex
+	mainTrieRoot common.Hash
+	etas         map[common.Hash]time.Duration
+	lastUpdate   time.Time
 
 	// counters to display progress
 	triesSynced   uint32
 	triesFromDisk uint32
 }
 
-// notifyProgress is called when leafs are received to estimate progress and
-// updates the overall ETA if needed.
-func (s *syncETA) notifyProgress(root common.Hash, startTime time.Time, startKey []byte, key []byte) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-
-	if time.Since(s.lastUpdate) < updateInterval {
-		return
+func NewSyncEta(mainTrieRoot common.Hash) SyncETA {
+	return &syncETA{
+		mainTrieRoot: mainTrieRoot,
+		etas:         make(map[common.Hash]time.Duration),
 	}
+}
 
+func (s *syncETA) NotifyProgress(root common.Hash, startTime time.Time, startKey []byte, key []byte) {
 	// use first 16 bits of [startKey] and [key] to estimate progress
 	startPos := bytesToUint16(startKey)
 	currentPos := bytesToUint16(key)
@@ -50,26 +59,51 @@ func (s *syncETA) notifyProgress(root common.Hash, startTime time.Time, startKey
 
 	timeSpent := time.Since(startTime)
 	estimatedTotalDuration := float64(timeSpent) * float64(math.MaxUint16-startPos) / float64(currentPos-startPos)
-	eta := time.Duration(estimatedTotalDuration) - timeSpent
 
-	if eta > s.largestTrieEta || s.largestTrieRoot == root || s.largestTrieRoot == (common.Hash{}) {
-		s.largestTrieEta = eta
-		s.largestTrieRoot = root
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	// track eta for all tries in progress so the next large trie can be identified
+	// after the largest trie completes. this is done before the following early
+	// return on [lastUpdate], so if [root] becomes the largest trie after the current
+	// largest trie is removed, a more accurate ETA is logged.
+	s.etas[root] = time.Duration(estimatedTotalDuration) - timeSpent
 
-		s.lastUpdate = time.Now()
-		log.Info(
-			"state sync in progress",
-			"eta", roundETA(eta),
-			"triesComplete", s.triesSynced+s.triesFromDisk, // intended as a monotonically increasing stat the user can monitor as ETA may fluctuate
-		)
-		log.Debug(
-			"state sync progress details",
-			"root", root,
-			"key", common.BytesToHash(key),
-			"triesSynced", s.triesSynced,
-			"triesFromDisk", s.triesFromDisk,
-			"mainTrie", root == s.mainTrieRoot,
-		)
+	if time.Since(s.lastUpdate) < updateInterval {
+		return
+	}
+	s.lastUpdate = time.Now()
+
+	var maxEta time.Duration
+	for _, eta := range s.etas {
+		if maxEta < eta {
+			maxEta = eta
+		}
+	}
+	log.Info(
+		"state sync in progress",
+		"ETA", roundETA(maxEta),
+		"triesComplete", s.triesSynced+s.triesFromDisk, // intended as a monotonically increasing stat the user can monitor as ETA may fluctuate
+	)
+	log.Debug(
+		"state sync progress details",
+		"root", root,
+		"trieETA", s.etas[root],
+		"key", common.BytesToHash(key),
+		"triesSynced", s.triesSynced,
+		"triesFromDisk", s.triesFromDisk,
+		"isMainTrie", root == s.mainTrieRoot,
+	)
+}
+
+func (s *syncETA) RemoveSyncedTrie(root common.Hash, skipped bool) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	delete(s.etas, root)
+	if skipped {
+		s.triesFromDisk += 1
+	} else {
+		s.triesSynced += 1
 	}
 }
 
@@ -80,21 +114,6 @@ func bytesToUint16(key []byte) uint16 {
 		return 0
 	}
 	return binary.BigEndian.Uint16(key)
-}
-
-func (s *syncETA) notifyTrieSynced(root common.Hash, skipped bool) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-
-	if root == s.largestTrieRoot {
-		s.largestTrieRoot = common.Hash{}
-	}
-
-	if skipped {
-		s.triesFromDisk += 1
-	} else {
-		s.triesSynced += 1
-	}
 }
 
 // roundETA rounds [d] to a minute and chops off the "0s" suffix
