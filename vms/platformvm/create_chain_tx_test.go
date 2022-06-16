954cd2e4d3d762c2cd5dd04e0ec45f094989da9f
Stephen <stephen@avalabs.org>
2022-06-16 17:29:09 -0400

    nit

--- vms/platformvm/create_chain_tx_test.go
+++ vms/platformvm/create_chain_tx_test.go
@@ -150,9 +150,9 @@ func TestUnsignedCreateChainTxVerify(t *testing.T) {
 
 		createChainTx := tx.Unsigned.(*txs.CreateChainTx)
 		createChainTx.SyntacticallyVerified = false
-		tx.Unsigned = test.setup(tx.Unsigned.(*txs.CreateChainTx))
+		tx.Unsigned = test.setup(createChainTx)
 		if err := tx.SyntacticVerify(vm.ctx); err != nil && !test.shouldErr {
-			t.Fatalf("test '%s' shouldn't have errored but got: %s", test.description, err)
+			t.Fatalf("test '%s' shouldn't have erred but got: %s", test.description, err)
 		} else if err == nil && test.shouldErr {
 			t.Fatalf("test '%s' didn't error but should have", test.description)
 		}
