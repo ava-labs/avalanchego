4cb81959082f26e81fc7c532b03c7856aed6e005
David Boehm <91908103+dboehm-avalabs@users.noreply.github.com>
2022-07-05 17:49:50 -0400

    [AV-1864] Cleanup node when context is cancelled in msg byte throttler (#1631)

--- network/throttling/inbound_msg_byte_throttler_test.go
+++ network/throttling/inbound_msg_byte_throttler_test.go
@@ -84,10 +84,10 @@ func TestInboundMsgByteThrottlerCancelContext(t *testing.T) {
 
 	// ensure the throttler has recorded that vdr2 is waiting
 	throttler.lock.Lock()
-	assert.Len(throttler.nodeToWaitingMsgIDs, 1)
-	assert.Len(throttler.nodeToWaitingMsgIDs[vdr2ID], 1)
+	assert.Len(throttler.nodeToWaitingMsgID, 1)
+	assert.Contains(throttler.nodeToWaitingMsgID, vdr2ID)
 	assert.EqualValues(1, throttler.waitingToAcquire.Len())
-	_, exists := throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgIDs[vdr2ID][0])
+	_, exists := throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgID[vdr2ID])
 	assert.True(exists)
 	throttler.lock.Unlock()
 
@@ -99,6 +99,8 @@ func TestInboundMsgByteThrottlerCancelContext(t *testing.T) {
 	case <-time.After(50 * time.Millisecond):
 		t.Fatal("channel should signal because ctx was cancelled")
 	}
+
+	assert.NotContains(throttler.nodeToWaitingMsgID, vdr2ID)
 }
 
 func TestInboundMsgByteThrottler(t *testing.T) {
@@ -168,7 +170,7 @@ func TestInboundMsgByteThrottler(t *testing.T) {
 	assert.EqualValues(throttler.nodeToVdrBytesUsed[vdr2ID], config.VdrAllocSize/2)
 	assert.Len(throttler.nodeToVdrBytesUsed, 2)
 	assert.Len(throttler.nodeToAtLargeBytesUsed, 1)
-	assert.Len(throttler.nodeToWaitingMsgIDs, 0)
+	assert.Len(throttler.nodeToWaitingMsgID, 0)
 	assert.EqualValues(0, throttler.waitingToAcquire.Len())
 
 	// vdr1 should be able to acquire the rest of the validator allocation
@@ -191,10 +193,10 @@ func TestInboundMsgByteThrottler(t *testing.T) {
 	case <-time.After(50 * time.Millisecond):
 	}
 	throttler.lock.Lock()
-	assert.Len(throttler.nodeToWaitingMsgIDs, 1)
-	assert.Len(throttler.nodeToWaitingMsgIDs[vdr1ID], 1)
+	assert.Len(throttler.nodeToWaitingMsgID, 1)
+	assert.Contains(throttler.nodeToWaitingMsgID, vdr1ID)
 	assert.EqualValues(1, throttler.waitingToAcquire.Len())
-	_, exists := throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgIDs[vdr1ID][0])
+	_, exists := throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgID[vdr1ID])
 	assert.True(exists)
 	throttler.lock.Unlock()
 
@@ -209,10 +211,11 @@ func TestInboundMsgByteThrottler(t *testing.T) {
 	case <-time.After(50 * time.Millisecond):
 	}
 	throttler.lock.Lock()
-	assert.Len(throttler.nodeToWaitingMsgIDs, 2)
-	assert.Len(throttler.nodeToWaitingMsgIDs[vdr2ID], 1)
+	assert.Len(throttler.nodeToWaitingMsgID, 2)
+
+	assert.Contains(throttler.nodeToWaitingMsgID, vdr2ID)
 	assert.EqualValues(2, throttler.waitingToAcquire.Len())
-	_, exists = throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgIDs[vdr2ID][0])
+	_, exists = throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgID[vdr2ID])
 	assert.True(exists)
 	throttler.lock.Unlock()
 
@@ -228,10 +231,10 @@ func TestInboundMsgByteThrottler(t *testing.T) {
 	case <-time.After(50 * time.Millisecond):
 	}
 	throttler.lock.Lock()
-	assert.Len(throttler.nodeToWaitingMsgIDs, 3)
-	assert.Len(throttler.nodeToWaitingMsgIDs[nonVdrID], 1)
+	assert.Len(throttler.nodeToWaitingMsgID, 3)
+	assert.Contains(throttler.nodeToWaitingMsgID, nonVdrID)
 	assert.EqualValues(3, throttler.waitingToAcquire.Len())
-	_, exists = throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgIDs[nonVdrID][0])
+	_, exists = throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgID[nonVdrID])
 	assert.True(exists)
 	throttler.lock.Unlock()
 
@@ -253,14 +256,14 @@ func TestInboundMsgByteThrottler(t *testing.T) {
 	assert.Len(throttler.nodeToVdrBytesUsed, 1)
 	assert.EqualValues(0, throttler.nodeToVdrBytesUsed[vdr1ID])
 	assert.EqualValues(config.AtLargeAllocSize/2-2, throttler.remainingAtLargeBytes)
-	assert.Len(throttler.nodeToWaitingMsgIDs, 0)
+	assert.Len(throttler.nodeToWaitingMsgID, 0)
 	assert.EqualValues(0, throttler.waitingToAcquire.Len())
 
 	// Non-validator should be able to take the rest of the at-large bytes
 	throttler.Acquire(context.Background(), config.AtLargeAllocSize/2-2, nonVdrID)
 	assert.EqualValues(0, throttler.remainingAtLargeBytes)
 	assert.EqualValues(config.AtLargeAllocSize/2-1, throttler.nodeToAtLargeBytesUsed[nonVdrID])
-	assert.Len(throttler.nodeToWaitingMsgIDs, 0)
+	assert.Len(throttler.nodeToWaitingMsgID, 0)
 	assert.EqualValues(0, throttler.waitingToAcquire.Len())
 
 	// But should block on subsequent Acquires
@@ -274,10 +277,10 @@ func TestInboundMsgByteThrottler(t *testing.T) {
 	case <-time.After(50 * time.Millisecond):
 	}
 	throttler.lock.Lock()
-	assert.Len(throttler.nodeToWaitingMsgIDs, 1)
-	assert.Len(throttler.nodeToWaitingMsgIDs[nonVdrID], 1)
+	assert.Contains(throttler.nodeToWaitingMsgID, nonVdrID)
+	assert.Contains(throttler.nodeToWaitingMsgID, nonVdrID)
 	assert.EqualValues(1, throttler.waitingToAcquire.Len())
-	_, exists = throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgIDs[nonVdrID][0])
+	_, exists = throttler.waitingToAcquire.Get(throttler.nodeToWaitingMsgID[nonVdrID])
 	assert.True(exists)
 	throttler.lock.Unlock()
 
@@ -291,7 +294,7 @@ func TestInboundMsgByteThrottler(t *testing.T) {
 	assert.EqualValues(config.VdrAllocSize, throttler.remainingVdrBytes)
 	assert.Len(throttler.nodeToVdrBytesUsed, 0)
 	assert.EqualValues(0, throttler.remainingAtLargeBytes)
-	assert.Len(throttler.nodeToWaitingMsgIDs, 0)
+	assert.NotContains(throttler.nodeToWaitingMsgID, nonVdrID)
 	assert.EqualValues(0, throttler.waitingToAcquire.Len())
 
 	// Release all of vdr1's messages
@@ -301,7 +304,7 @@ func TestInboundMsgByteThrottler(t *testing.T) {
 	assert.EqualValues(config.VdrAllocSize, throttler.remainingVdrBytes)
 	assert.EqualValues(config.AtLargeAllocSize/2, throttler.remainingAtLargeBytes)
 	assert.EqualValues(0, throttler.nodeToAtLargeBytesUsed[vdr1ID])
-	assert.Len(throttler.nodeToWaitingMsgIDs, 0)
+	assert.NotContains(throttler.nodeToWaitingMsgID, nonVdrID)
 	assert.EqualValues(0, throttler.waitingToAcquire.Len())
 
 	// Release nonVdr's messages
@@ -313,7 +316,7 @@ func TestInboundMsgByteThrottler(t *testing.T) {
 	assert.EqualValues(config.AtLargeAllocSize, throttler.remainingAtLargeBytes)
 	assert.Len(throttler.nodeToAtLargeBytesUsed, 0)
 	assert.EqualValues(0, throttler.nodeToAtLargeBytesUsed[nonVdrID])
-	assert.Len(throttler.nodeToWaitingMsgIDs, 0)
+	assert.NotContains(throttler.nodeToWaitingMsgID, nonVdrID)
 	assert.EqualValues(0, throttler.waitingToAcquire.Len())
 }
 
@@ -355,17 +358,6 @@ func TestSybilMsgThrottlerMaxNonVdr(t *testing.T) {
 	nonVdrNodeID2 := ids.GenerateTestNodeID()
 	throttler.Acquire(context.Background(), config.NodeMaxAtLargeBytes, nonVdrNodeID2)
 
-	// Acquiring more should block
-	go func() {
-		throttler.Acquire(context.Background(), 1, nonVdrNodeID1)
-		nonVdrDone <- struct{}{}
-	}()
-	select {
-	case <-nonVdrDone:
-		t.Fatal("should block on acquiring any more bytes")
-	case <-time.After(50 * time.Millisecond):
-	}
-
 	// Validator should only be able to take [MaxAtLargeBytes]
 	throttler.Acquire(context.Background(), config.NodeMaxAtLargeBytes+1, vdr1ID)
 	assert.EqualValues(config.NodeMaxAtLargeBytes, throttler.nodeToAtLargeBytesUsed[vdr1ID])
@@ -375,9 +367,8 @@ func TestSybilMsgThrottlerMaxNonVdr(t *testing.T) {
 	assert.EqualValues(config.AtLargeAllocSize-config.NodeMaxAtLargeBytes*3, throttler.remainingAtLargeBytes)
 }
 
-// Test that messages waiting to be acquired by a given node
-// are handled in FIFO order
-func TestSybilMsgThrottlerFIFO(t *testing.T) {
+// Test that messages waiting to be acquired by a given node execute next
+func TestMsgThrottlerNextMsg(t *testing.T) {
 	assert := assert.New(t)
 	config := MsgByteThrottlerConfig{
 		VdrAllocSize:        1024,
@@ -390,71 +381,66 @@ func TestSybilMsgThrottlerFIFO(t *testing.T) {
 	nonVdrNodeID := ids.GenerateTestNodeID()
 
 	maxVdrBytes := config.VdrAllocSize + config.AtLargeAllocSize
-	maxNonVdrBytes := config.AtLargeAllocSize
-	// Test for both validator and non-validator
-	for _, nodeID := range []ids.NodeID{vdr1ID, nonVdrNodeID} {
-		maxBytes := maxVdrBytes
-		if nodeID == nonVdrNodeID {
-			maxBytes = maxNonVdrBytes
-		}
-		throttler, err := newInboundMsgByteThrottler(
-			logging.NoLog{},
-			"",
-			prometheus.NewRegistry(),
-			vdrs,
-			config,
-		)
-		assert.NoError(err)
-		// node uses up all but 1 byte
-		throttler.Acquire(context.Background(), maxBytes-1, nodeID)
-		// node uses the last byte
-		throttler.Acquire(context.Background(), 1, nodeID)
+	maxBytes := maxVdrBytes
+	throttler, err := newInboundMsgByteThrottler(
+		logging.NoLog{},
+		"",
+		prometheus.NewRegistry(),
+		vdrs,
+		config,
+	)
+	assert.NoError(err)
 
-		// First message wants to acquire a lot of bytes
-		done := make(chan struct{})
-		go func() {
-			throttler.Acquire(context.Background(), maxBytes-1, nodeID)
-			done <- struct{}{}
-		}()
-		select {
-		case <-done:
-			t.Fatal("should block on acquiring any more bytes")
-		case <-time.After(50 * time.Millisecond):
-		}
+	// validator uses up all but 1 byte
+	throttler.Acquire(context.Background(), maxBytes-1, vdr1ID)
+	// validator uses the last byte
+	throttler.Acquire(context.Background(), 1, vdr1ID)
 
-		// Next message only wants to acquire 1 byte
-		go func() {
-			throttler.Acquire(context.Background(), 1, nodeID)
-			done <- struct{}{}
-		}()
-		select {
-		case <-done:
-			t.Fatal("should block on acquiring any more bytes")
-		case <-time.After(50 * time.Millisecond):
-		}
-
-		// Release 1 byte
-		throttler.release(&msgMetadata{msgSize: 1}, nodeID)
-		// Byte should have gone toward first message
-		assert.EqualValues(2, throttler.waitingToAcquire.Len())
-		assert.Len(throttler.nodeToWaitingMsgIDs[nodeID], 2)
-		firstMsgID := throttler.nodeToWaitingMsgIDs[nodeID][0]
-		firstMsg, exists := throttler.waitingToAcquire.Get(firstMsgID)
-		assert.True(exists)
-		assert.EqualValues(maxBytes-2, firstMsg.(*msgMetadata).bytesNeeded)
-
-		// Since messages are processed FIFO for a given validator,
-		// the first message should return from Acquire first
-		select {
-		case <-done:
-			t.Fatal("should still be blocking")
-		case <-time.After(50 * time.Millisecond):
-		}
-
-		// Release the rest of the bytes
-		throttler.release(&msgMetadata{msgSize: maxBytes - 1}, nodeID)
-		// Both should be done acquiring now
-		<-done
-		<-done
+	// validator wants to acquire a lot of bytes
+	doneVdr := make(chan struct{})
+	go func() {
+		throttler.Acquire(context.Background(), maxBytes-1, vdr1ID)
+		doneVdr <- struct{}{}
+	}()
+	select {
+	case <-doneVdr:
+		t.Fatal("should block on acquiring any more bytes")
+	case <-time.After(50 * time.Millisecond):
 	}
+
+	// nonvalidator tries to acquire more bytes
+	done := make(chan struct{})
+	go func() {
+		throttler.Acquire(context.Background(), 1, nonVdrNodeID)
+		done <- struct{}{}
+	}()
+	select {
+	case <-done:
+		t.Fatal("should block on acquiring any more bytes")
+	case <-time.After(50 * time.Millisecond):
+	}
+
+	// Release 1 byte
+	throttler.release(&msgMetadata{msgSize: 1}, vdr1ID)
+	// Byte should have gone toward next validator message
+	assert.EqualValues(2, throttler.waitingToAcquire.Len())
+	assert.Contains(throttler.nodeToWaitingMsgID, vdr1ID)
+	firstMsgID := throttler.nodeToWaitingMsgID[vdr1ID]
+	firstMsg, exists := throttler.waitingToAcquire.Get(firstMsgID)
+	assert.True(exists)
+	assert.EqualValues(maxBytes-2, firstMsg.(*msgMetadata).bytesNeeded)
+
+	select {
+	case <-doneVdr:
+		t.Fatal("should still be blocking")
+	case <-time.After(50 * time.Millisecond):
+	}
+
+	// Release the rest of the bytes
+	throttler.release(&msgMetadata{msgSize: maxBytes - 1}, vdr1ID)
+	// next validator message should finish
+	<-doneVdr
+	throttler.release(&msgMetadata{msgSize: maxBytes - 1}, vdr1ID)
+	// next non validator message should finish
+	<-done
 }
