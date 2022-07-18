6d8147f201eb89dda0180b132ee4ff06af593d38
Dan Laine <daniel.laine@avalabs.org>
2022-07-18 18:26:21 -0400

    add test

--- vms/platformvm/blocks/stateful/verifier_test.go
+++ vms/platformvm/blocks/stateful/verifier_test.go
@@ -9,9 +9,11 @@ import (
 
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow"
+	"github.com/ava-labs/avalanchego/snow/choices"
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/vms/components/verify"
 	"github.com/ava-labs/avalanchego/vms/platformvm/blocks/stateless"
+	"github.com/ava-labs/avalanchego/vms/platformvm/config"
 	"github.com/ava-labs/avalanchego/vms/platformvm/state"
 	"github.com/ava-labs/avalanchego/vms/platformvm/status"
 	"github.com/ava-labs/avalanchego/vms/platformvm/txs"
@@ -30,7 +32,6 @@ func TestVerifierVisitProposalBlock(t *testing.T) {
 	mempool := mempool.NewMockMempool(ctrl)
 	parentID := ids.GenerateTestID()
 	parentStatelessBlk := stateless.NewMockBlock(ctrl)
-	parentStatelessBlk.EXPECT().Height().Return(uint64(1)).Times(1)
 	verifier := &verifier{
 		txExecutorBackend: executor.Backend{},
 		backend: &backend{
@@ -72,10 +73,13 @@ func TestVerifierVisitProposalBlock(t *testing.T) {
 
 	// Set expectations for dependencies.
 	timestamp := time.Now()
+	parentStatelessBlk.EXPECT().Height().Return(uint64(1)).Times(1)
 	mempool.EXPECT().RemoveProposalTx(blk.Tx).Times(1)
 	onCommitState.EXPECT().AddTx(blk.Tx, status.Committed).Times(1)
 	onAbortState.EXPECT().AddTx(blk.Tx, status.Aborted).Times(1)
 	onAbortState.EXPECT().GetTimestamp().Return(timestamp).Times(1)
+
+	// Visit the block
 	err = verifier.VisitProposalBlock(blk)
 	assert.NoError(err)
 	assert.Contains(verifier.backend.blkIDToState, blk.ID())
@@ -83,5 +87,82 @@ func TestVerifierVisitProposalBlock(t *testing.T) {
 	assert.Equal(blk, gotBlkState.statelessBlock)
 	assert.Equal(onCommitState, gotBlkState.onCommitState)
 	assert.Equal(onAbortState, gotBlkState.onAbortState)
+	assert.Equal(timestamp, gotBlkState.timestamp)
+}
+
+func TestVerifierVisitAtomicBlock(t *testing.T) {
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	s := state.NewMockState(ctrl)
+	mempool := mempool.NewMockMempool(ctrl)
+	parentID := ids.GenerateTestID()
+	parentStatelessBlk := stateless.NewMockBlock(ctrl)
+	grandparentID := ids.GenerateTestID()
+	stateVersions := state.NewMockVersions(ctrl)
+	parentState := state.NewMockState(ctrl)
+	verifier := &verifier{
+		txExecutorBackend: executor.Backend{
+			Config: &config.Config{
+				ApricotPhase5Time: time.Now().Add(time.Hour),
+			},
+		},
+		backend: &backend{
+			blkIDToState: map[ids.ID]*blockState{
+				parentID: {
+					statelessBlock: parentStatelessBlk,
+				},
+			},
+			Mempool:       mempool,
+			state:         s,
+			stateVersions: stateVersions,
+			ctx: &snow.Context{
+				Log: logging.NoLog{},
+			},
+		},
+	}
+
+	onAccept := state.NewMockDiff(ctrl)
+	blkTx := txs.NewMockUnsignedTx(ctrl)
+	blkTx.EXPECT().Visit(gomock.AssignableToTypeOf(&executor.AtomicTxExecutor{})).DoAndReturn(
+		func(e *executor.AtomicTxExecutor) error {
+			e.OnAccept = onAccept
+			e.Inputs = ids.Set{}
+			return nil
+		},
+	).Times(1)
+	blkTx.EXPECT().Initialize(gomock.Any()).Times(2)
+
+	blk, err := stateless.NewAtomicBlock(
+		parentID,
+		2,
+		&txs.Tx{
+			Unsigned: blkTx,
+			Creds:    []verify.Verifiable{},
+		},
+	)
+	assert.NoError(err)
+
+	// Set expectations for dependencies.
+	timestamp := time.Now()
+	parentState.EXPECT().GetTimestamp().Return(timestamp).Times(1)
+	stateVersions.EXPECT().GetState(blk.Parent()).Return(parentState, true).Times(1)
+	parentStatelessBlk.EXPECT().Height().Return(uint64(1)).Times(1)
+	parentStatelessBlk.EXPECT().Parent().Return(grandparentID).Times(1)
+	s.EXPECT().GetStatelessBlock(parentID).Return(parentStatelessBlk, choices.Accepted, nil).Times(1)
+	mempool.EXPECT().RemoveDecisionTxs([]*txs.Tx{blk.Tx}).Times(1)
+	onAccept.EXPECT().AddTx(blk.Tx, status.Committed).Times(1)
+	onAccept.EXPECT().GetTimestamp().Return(timestamp).Times(1)
+	stateVersions.EXPECT().SetState(blk.ID(), onAccept).Times(1)
+
+	err = verifier.VisitAtomicBlock(blk)
+	assert.NoError(err)
+
+	assert.Contains(verifier.backend.blkIDToState, blk.ID())
+	gotBlkState := verifier.backend.blkIDToState[blk.ID()]
+	assert.Equal(blk, gotBlkState.statelessBlock)
+	assert.Equal(onAccept, gotBlkState.onAcceptState)
+	assert.Equal(ids.Set{}, gotBlkState.inputs)
 	assert.Equal(timestamp, gotBlkState.timestamp)
 }
