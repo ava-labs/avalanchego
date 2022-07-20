e0db5ee2b72bdb2a080baab37858c0a64efbaaa1
Dan Laine <daniel.laine@avalabs.org>
2022-07-20 13:02:16 -0400

    add state.Diff unit tests

--- vms/platformvm/state/diff_test.go
+++ vms/platformvm/state/diff_test.go
@@ -5,9 +5,12 @@ package state
 
 import (
 	"testing"
+	"time"
 
+	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/ids"
 )
 
@@ -45,6 +48,184 @@ func TestDiffCurrentSupply(t *testing.T) {
 	d.SetCurrentSupply(newCurrentSupply)
 	assert.Equal(newCurrentSupply, d.GetCurrentSupply())
 	assert.Equal(initialCurrentSupply, state.GetCurrentSupply())
+}
+
+func TestDiffCurrentValidator(t *testing.T) {
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	lastAcceptedID := ids.GenerateTestID()
+	state := NewMockState(ctrl)
+	// Called in NewDiff
+	state.EXPECT().GetTimestamp().Return(time.Now()).Times(1)
+	state.EXPECT().GetCurrentSupply().Return(uint64(1337)).Times(1)
+
+	states := NewMockVersions(ctrl)
+	states.EXPECT().GetState(lastAcceptedID).Return(state, true).AnyTimes()
+
+	d, err := NewDiff(lastAcceptedID, states)
+	assert.NoError(err)
+
+	// Put a current validator
+	currentValidator := &Staker{
+		TxID:     ids.GenerateTestID(),
+		SubnetID: ids.GenerateTestID(),
+		NodeID:   ids.GenerateTestNodeID(),
+	}
+	d.PutCurrentValidator(currentValidator)
+
+	// Assert that we get the current validator back
+	gotCurrentValidator, err := d.GetCurrentValidator(currentValidator.SubnetID, currentValidator.NodeID)
+	assert.NoError(err)
+	assert.Equal(currentValidator, gotCurrentValidator)
+
+	// Delete the current validator
+	d.DeleteCurrentValidator(currentValidator)
+
+	// Make sure the deletion worked
+	_, err = d.GetCurrentValidator(currentValidator.SubnetID, currentValidator.NodeID)
+	assert.ErrorIs(err, database.ErrNotFound)
+}
+
+func TestDiffPendingValidator(t *testing.T) {
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	lastAcceptedID := ids.GenerateTestID()
+	state := NewMockState(ctrl)
+	// Called in NewDiff
+	state.EXPECT().GetTimestamp().Return(time.Now()).Times(1)
+	state.EXPECT().GetCurrentSupply().Return(uint64(1337)).Times(1)
+
+	states := NewMockVersions(ctrl)
+	states.EXPECT().GetState(lastAcceptedID).Return(state, true).AnyTimes()
+
+	d, err := NewDiff(lastAcceptedID, states)
+	assert.NoError(err)
+
+	// Put a pending validator
+	pendingValidator := &Staker{
+		TxID:     ids.GenerateTestID(),
+		SubnetID: ids.GenerateTestID(),
+		NodeID:   ids.GenerateTestNodeID(),
+	}
+	d.PutPendingValidator(pendingValidator)
+
+	// Assert that we get the pending validator back
+	gotPendingValidator, err := d.GetPendingValidator(pendingValidator.SubnetID, pendingValidator.NodeID)
+	assert.NoError(err)
+	assert.Equal(pendingValidator, gotPendingValidator)
+
+	// Delete the pending validator
+	d.DeletePendingValidator(pendingValidator)
+
+	// Make sure the deletion worked
+	_, err = d.GetPendingValidator(pendingValidator.SubnetID, pendingValidator.NodeID)
+	assert.ErrorIs(err, database.ErrNotFound)
+}
+
+func TestDiffCurrentDelegator(t *testing.T) {
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	currentDelegator := &Staker{
+		TxID:     ids.GenerateTestID(),
+		SubnetID: ids.GenerateTestID(),
+		NodeID:   ids.GenerateTestNodeID(),
+	}
+
+	state := NewMockState(ctrl)
+	// Called in NewDiff
+	state.EXPECT().GetTimestamp().Return(time.Now()).Times(1)
+	state.EXPECT().GetCurrentSupply().Return(uint64(1337)).Times(1)
+
+	states := NewMockVersions(ctrl)
+	lastAcceptedID := ids.GenerateTestID()
+	states.EXPECT().GetState(lastAcceptedID).Return(state, true).AnyTimes()
+
+	d, err := NewDiff(lastAcceptedID, states)
+	assert.NoError(err)
+
+	// Put a current delegator
+	d.PutCurrentDelegator(currentDelegator)
+
+	// Assert that we get the current delegator back
+	// Mock iterator for [state] returns no delegators.
+	stateCurrentDelegatorIter := NewMockStakerIterator(ctrl)
+	stateCurrentDelegatorIter.EXPECT().Next().Return(false).Times(2)
+	stateCurrentDelegatorIter.EXPECT().Release().Times(2)
+	state.EXPECT().GetCurrentDelegatorIterator(
+		currentDelegator.SubnetID,
+		currentDelegator.NodeID,
+	).Return(stateCurrentDelegatorIter, nil).Times(2)
+	gotCurrentDelegatorIter, err := d.GetCurrentDelegatorIterator(currentDelegator.SubnetID, currentDelegator.NodeID)
+	assert.NoError(err)
+	// The iterator should have the 1 delegator we put in [d]
+	assert.True(gotCurrentDelegatorIter.Next())
+	assert.Equal(gotCurrentDelegatorIter.Value(), currentDelegator)
+
+	// Delete the current delegator
+	d.DeleteCurrentDelegator(currentDelegator)
+
+	// Make sure the deletion worked.
+	// The iterator should have no elements.
+	gotCurrentDelegatorIter, err = d.GetCurrentDelegatorIterator(currentDelegator.SubnetID, currentDelegator.NodeID)
+	assert.NoError(err)
+	assert.False(gotCurrentDelegatorIter.Next())
+}
+
+func TestDiffPendingDelegator(t *testing.T) {
+	assert := assert.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	pendingDelegator := &Staker{
+		TxID:     ids.GenerateTestID(),
+		SubnetID: ids.GenerateTestID(),
+		NodeID:   ids.GenerateTestNodeID(),
+	}
+
+	state := NewMockState(ctrl)
+	// Called in NewDiff
+	state.EXPECT().GetTimestamp().Return(time.Now()).Times(1)
+	state.EXPECT().GetCurrentSupply().Return(uint64(1337)).Times(1)
+
+	states := NewMockVersions(ctrl)
+	lastAcceptedID := ids.GenerateTestID()
+	states.EXPECT().GetState(lastAcceptedID).Return(state, true).AnyTimes()
+
+	d, err := NewDiff(lastAcceptedID, states)
+	assert.NoError(err)
+
+	// Put a pending delegator
+	d.PutPendingDelegator(pendingDelegator)
+
+	// Assert that we get the pending delegator back
+	// Mock iterator for [state] returns no delegators.
+	statePendingDelegatorIter := NewMockStakerIterator(ctrl)
+	statePendingDelegatorIter.EXPECT().Next().Return(false).Times(2)
+	statePendingDelegatorIter.EXPECT().Release().Times(2)
+	state.EXPECT().GetPendingDelegatorIterator(
+		pendingDelegator.SubnetID,
+		pendingDelegator.NodeID,
+	).Return(statePendingDelegatorIter, nil).Times(2)
+	gotPendingDelegatorIter, err := d.GetPendingDelegatorIterator(pendingDelegator.SubnetID, pendingDelegator.NodeID)
+	assert.NoError(err)
+	// The iterator should have the 1 delegator we put in [d]
+	assert.True(gotPendingDelegatorIter.Next())
+	assert.Equal(gotPendingDelegatorIter.Value(), pendingDelegator)
+
+	// Delete the pending delegator
+	d.DeletePendingDelegator(pendingDelegator)
+
+	// Make sure the deletion worked.
+	// The iterator should have no elements.
+	gotPendingDelegatorIter, err = d.GetPendingDelegatorIterator(pendingDelegator.SubnetID, pendingDelegator.NodeID)
+	assert.NoError(err)
+	assert.False(gotPendingDelegatorIter.Next())
 }
 
 func assertChainsEqual(t *testing.T, expected, actual Chain) {
