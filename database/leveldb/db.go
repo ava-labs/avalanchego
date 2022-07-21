d1bd9c14dad1299ebab549be739a694d1213ed42
Ceyhun Onur <ceyhun.onur@avalabs.org>
2022-07-21 19:37:24 +0300

    AV-1665: structurize database pkg logs (#1531)
    * structurize logs
    
    * use any to log config
    
    * use reflect instead of any
    
    * Revert "use any to log config"
    
    This reverts commit 6ec714e9ab48c02f6b8bae89cbc619f5adcce784.
    
    Co-authored-by: Stephen <stephen@avalabs.org>

--- database/leveldb/db.go
+++ database/leveldb/db.go
@@ -19,6 +19,8 @@ import (
 	"github.com/syndtr/goleveldb/leveldb/opt"
 	"github.com/syndtr/goleveldb/leveldb/util"
 
+	"go.uber.org/zap"
+
 	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/utils"
 	"github.com/ava-labs/avalanchego/utils/logging"
@@ -177,11 +179,10 @@ func New(file string, configBytes []byte, log logging.Logger, namespace string, 
 			return nil, fmt.Errorf("failed to parse db config: %w", err)
 		}
 	}
-	configJSON, err := json.Marshal(&parsedConfig)
-	if err != nil {
-		return nil, err
-	}
-	log.Info("leveldb config: %s", string(configJSON))
+
+	log.Info("creating new leveldb",
+		zap.Reflect("config", parsedConfig),
+	)
 
 	// Open the db and recover any potential corruptions
 	db, err := leveldb.OpenFile(file, &opt.Options{
