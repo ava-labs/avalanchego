65943f4d7a2e015e71587d397e6906478f7df521
Stephen <stephen@avalabs.org>
2022-08-02 18:39:38 -0400

    remove useless comments

--- vms/platformvm/blocks/common_block.go
+++ vms/platformvm/blocks/common_block.go
@@ -23,14 +23,7 @@ func (b *CommonBlock) initialize(bytes []byte) error {
 	return nil
 }
 
-// ID returns the ID of this block
-func (b *CommonBlock) ID() ids.ID { return b.id }
-
-// Bytes returns the binary representation of this block
-func (b *CommonBlock) Bytes() []byte { return b.bytes }
-
-// Parent returns this block's parent's ID
+func (b *CommonBlock) ID() ids.ID     { return b.id }
 func (b *CommonBlock) Parent() ids.ID { return b.PrntID }
-
-// Height returns this block's height. The genesis block has height 0.
+func (b *CommonBlock) Bytes() []byte  { return b.bytes }
 func (b *CommonBlock) Height() uint64 { return b.Hght }
