6c896aadcdecddf8db5d50c77fbe48533eb9fbcb
Stephen <stephen@avalabs.org>
2022-08-02 18:37:45 -0400

    80c

--- vms/platformvm/blocks/codec.go
+++ vms/platformvm/blocks/codec.go
@@ -45,7 +45,8 @@ func init() {
 
 // RegisterBlockTypes allows registering relevant type of blocks package
 // in the right sequence. Following repackaging of platformvm package, a few
-// subpackage-level codecs were introduced, each handling serialization of specific types.
+// subpackage-level codecs were introduced, each handling serialization of
+// specific types.
 func RegisterBlockTypes(targetCodec codec.Registry) error {
 	errs := wrappers.Errs{}
 	errs.Add(
