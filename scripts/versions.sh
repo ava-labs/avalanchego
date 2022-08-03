97cf5c4ff8eee05c46a11b7f5c9a9b0abb386af3
aaronbuchwald <aaron.buchwald56@gmail.com>
2022-08-03 19:31:16 -0400

    Bump version to v0.8.15 (#916)

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.8.14'}
+coreth_version=${CORETH_VERSION:-'v0.8.15'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.7.14-rc.8'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.7.16'}
