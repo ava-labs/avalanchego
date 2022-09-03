5b963ef60e328ff74039cd18a62745f5e675e39f
aaronbuchwald <aaron.buchwald56@gmail.com>
2022-09-03 10:09:36 -0400

    Bump coreth version to v0.9.0 (#997)

--- graft/coreth/scripts/versions.sh
+++ graft/coreth/scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.8.17'}
+coreth_version=${CORETH_VERSION:-'v0.9.0'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.7.18'}
