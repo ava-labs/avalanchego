a82e1489f2d9170d1760e9126cc186d77fe89e54
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2022-09-06 16:05:24 -0400

    chore(peer): diff tracker implementation

---
+++ network/peer/gossip_tracker.go
@@ -0,0 +1,165 @@
+// Copyright (C) 2019-2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package peer
+
+import (
+	"sync"
+
+	"github.com/ava-labs/avalanchego/ids"
+)
+
+// GossipTracker tracks the peers that we're currently aware of, as well as the
+// peers we've told other peers about.
+type GossipTracker struct {
+	// a bitset of the peers that we are aware of
+	local ids.BigBitSet
+
+	// a mapping of peer => the peers we know we sent to them
+	knownPeers map[ids.NodeID]ids.BigBitSet
+	// a mapping of peers => the index they occupy in the bitsets
+	peersToIndices map[ids.NodeID]int
+	// a mapping of indices in the bitsets => the peer they correspond to
+	indicesToPeers map[int]ids.NodeID
+
+	// tail always points to an empty slot where new peers are added
+	tail int
+	lock sync.RWMutex
+}
+
+// NewGossipTracker returns an instance of GossipTracker
+func NewGossipTracker() *GossipTracker {
+	return &GossipTracker{
+		local:          ids.NewBigBitSet(),
+		knownPeers:     make(map[ids.NodeID]ids.BigBitSet),
+		peersToIndices: make(map[ids.NodeID]int),
+		indicesToPeers: make(map[int]ids.NodeID),
+	}
+}
+
+// Contains returns if a peer is being tracked
+func (d *GossipTracker) Contains(id ids.NodeID) bool {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
+
+	_, ok := d.knownPeers[id]
+	return ok
+}
+
+// Add starts tracking a peer
+func (d *GossipTracker) Add(id ids.NodeID) bool {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	// Don't add the peer if it's already being tracked
+	if _, ok := d.peersToIndices[id]; ok {
+		return false
+	}
+
+	// add the peer
+	d.peersToIndices[id] = d.tail
+	d.knownPeers[id] = ids.NewBigBitSet()
+	d.indicesToPeers[d.tail] = id
+
+	d.local.Add(d.tail)
+
+	d.tail++
+
+	return true
+}
+
+// Remove stops tracking a given peer
+func (d *GossipTracker) Remove(id ids.NodeID) bool {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	// Only remove peers that are actually being tracked
+	idx, ok := d.peersToIndices[id]
+	if !ok {
+		return false
+	}
+
+	evicted := d.indicesToPeers[idx]
+	d.tail--
+
+	// swap the peer-to-be-removed with the tail peer
+	// if the element we're swapping with is ourselves, we can skip this swap
+	// since we only need to delete instead
+	if idx != d.tail {
+		lastPeer := d.indicesToPeers[d.tail]
+
+		d.indicesToPeers[idx] = lastPeer
+		d.peersToIndices[lastPeer] = idx
+	}
+
+	// remove the peer from everyone else's peer lists
+	for _, knownPeers := range d.knownPeers {
+		// swap with the last element
+		if knownPeers.Contains(d.tail) {
+			knownPeers.Remove(d.tail)
+			knownPeers.Add(idx)
+		} else {
+			knownPeers.Remove(idx)
+		}
+	}
+
+	delete(d.knownPeers, evicted)
+	delete(d.peersToIndices, evicted)
+	delete(d.indicesToPeers, d.tail)
+
+	d.local.Remove(d.tail)
+
+	return true
+}
+
+// UpdateKnown adds to the peers that a peer knows about
+// invariants:
+// 1. nodeIDs must not contain duplicates
+// 2. nodeIDs must only contain nodeIDs that have been tracked with Add()
+func (d *GossipTracker) UpdateKnown(id ids.NodeID, learned []ids.NodeID) bool {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	known, ok := d.knownPeers[id]
+	if !ok {
+		return false
+	}
+
+	bs := ids.NewBigBitSetFromBits()
+	for _, nodeID := range learned {
+		bs.Add(d.peersToIndices[nodeID])
+	}
+
+	known.Union(bs)
+
+	return true
+}
+
+// GetUnknown returns the peers that we haven't sent to this peer
+func (d *GossipTracker) GetUnknown(id ids.NodeID) (map[ids.NodeID]struct{}, bool) {
+	d.lock.RLock()
+	defer d.lock.RUnlock()
+
+	unsent := ids.NewBigBitSet()
+	unsent.Union(d.local)
+
+	knownPeers, ok := d.knownPeers[id]
+	if !ok {
+		return nil, false
+	}
+
+	unsent.Difference(knownPeers)
+
+	result := make(map[ids.NodeID]struct{}, unsent.Len())
+
+	for i := 0; i < unsent.Len(); i++ {
+		if !unsent.Contains(i) {
+			continue
+		}
+
+		p := d.indicesToPeers[i]
+		result[p] = struct{}{}
+	}
+
+	return result, true
+}
---
+++ network/peer/gossip_tracker_test.go
@@ -0,0 +1,278 @@
+// Copyright (C) 2019-2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package peer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ava-labs/avalanchego/ids"
+)
+
+var (
+	id0 = ids.GenerateTestNodeID()
+	id1 = ids.GenerateTestNodeID()
+	id2 = ids.GenerateTestNodeID()
+)
+
+func TestGossipTracker_Contains(t *testing.T) {
+	tests := []struct {
+		name     string
+		add      []ids.NodeID
+		contains ids.NodeID
+		expected bool
+	}{
+		{
+			name:     "empty",
+			add:      []ids.NodeID{},
+			contains: id0,
+			expected: false,
+		},
+		{
+			name:     "populated - does not contain",
+			add:      []ids.NodeID{id0, id1},
+			contains: id2,
+			expected: false,
+		},
+		{
+			name:     "populated - contains",
+			add:      []ids.NodeID{id0, id1, id2},
+			contains: id2,
+			expected: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			r := require.New(t)
+			gossipTracker := NewGossipTracker()
+
+			for _, add := range test.add {
+				r.True(gossipTracker.Add(add))
+			}
+
+			r.Equal(test.expected, gossipTracker.Contains(test.contains))
+		})
+	}
+}
+
+func TestGossipTracker_Add(t *testing.T) {
+	tests := []struct {
+		name     string
+		toAdd    []ids.NodeID
+		expected []bool
+	}{
+		{
+			// Adding new elements always works
+			name:     "unique adds",
+			toAdd:    []ids.NodeID{id0, id1, id2},
+			expected: []bool{true, true, true},
+		},
+		{
+			// We shouldn't be able to add an item more than once
+			name:     "duplicate adds",
+			toAdd:    []ids.NodeID{id0, id0, id0},
+			expected: []bool{true, false, false},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			r := require.New(t)
+			gossipTracker := NewGossipTracker()
+
+			for i, p := range test.toAdd {
+				r.Equal(test.expected[i], gossipTracker.Add(p))
+			}
+		})
+	}
+}
+
+func TestGossipTracker_Remove(t *testing.T) {
+	tests := []struct {
+		name           string
+		toAdd          []ids.NodeID
+		expectedAdd    []bool
+		toRemove       []ids.NodeID
+		expectedRemove []bool
+	}{
+		{
+			// We should be able to remove things that we are tracking
+			name:           "remove existing elements",
+			toAdd:          []ids.NodeID{id0, id1, id2},
+			toRemove:       []ids.NodeID{id0, id1, id2},
+			expectedRemove: []bool{true, true, true},
+		},
+		{
+			// We shouldn't be able to remove something once it's gone
+			name:           "duplicate remove",
+			toAdd:          []ids.NodeID{id0},
+			toRemove:       []ids.NodeID{id0, id0},
+			expectedRemove: []bool{true, false},
+		},
+		{
+			// We shouldn't be able to remove elements we aren't tracking
+			name:           "remove non-existent elements",
+			toAdd:          []ids.NodeID{},
+			expectedAdd:    []bool{},
+			toRemove:       []ids.NodeID{id0, id1, id2},
+			expectedRemove: []bool{false, false, false},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			r := require.New(t)
+			gossipTracker := NewGossipTracker()
+
+			for _, add := range test.toAdd {
+				r.True(gossipTracker.Add(add))
+			}
+
+			for i, p := range test.toRemove {
+				r.Equal(test.expectedRemove[i], gossipTracker.Remove(p))
+			}
+		})
+	}
+}
+
+func TestGossipTracker_UpdateKnown(t *testing.T) {
+	type args struct {
+		id    ids.NodeID
+		known []ids.NodeID
+	}
+
+	tests := []struct {
+		name     string
+		add      []ids.NodeID
+		args     args
+		expected bool
+	}{
+		{
+			// We should not be able to update an untracked peer
+			name:     "update untracked peer - empty",
+			add:      []ids.NodeID{},
+			args:     args{id0, []ids.NodeID{}},
+			expected: false,
+		},
+		{
+			// We should not be able to update an untracked peer
+			name:     "update untracked peer - populated",
+			add:      []ids.NodeID{id1, id2},
+			args:     args{id0, []ids.NodeID{}},
+			expected: false,
+		},
+		{
+			// We should be able to update tracked peer
+			name:     "update tracked peer",
+			add:      []ids.NodeID{id0, id1, id2},
+			args:     args{id0, []ids.NodeID{}},
+			expected: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			r := require.New(t)
+			gossipTracker := NewGossipTracker()
+
+			for _, add := range test.add {
+				r.True(gossipTracker.Add(add))
+			}
+
+			r.Equal(test.expected, gossipTracker.UpdateKnown(test.args.id, test.args.known))
+		})
+	}
+}
+
+func TestGossipTracker_GetUnknown(t *testing.T) {
+	r := require.New(t)
+	d := NewGossipTracker()
+
+	// we should get an empty unknown since we're not tracking anything
+	unknown, ok := d.GetUnknown(id0)
+	r.Empty(unknown)
+	r.False(ok)
+
+	// we should get a unknown of [+id0, +id1] since we know about id0 and id1,
+	// but id0 and id1 both don't know anything yet
+	d.Add(id0)
+	d.Add(id1)
+
+	// check id0's unknown
+	unknown, ok = d.GetUnknown(id0)
+	r.Contains(unknown, id0)
+	r.Contains(unknown, id1)
+	r.Len(unknown, 2)
+	r.True(ok)
+
+	// check id1's unknown
+	unknown, ok = d.GetUnknown(id1)
+	r.Contains(unknown, id0)
+	r.Contains(unknown, id1)
+	r.Len(unknown, 2)
+	r.True(ok)
+
+	// id0 now knows about id0, but not id1, so it should see [+id1] in its unknown
+	// id1 still knows nothing, so it should see both
+	p0 := []ids.NodeID{id0}
+	r.True(d.UpdateKnown(id0, p0))
+
+	// id0 should have a unknown of [id1], since it knows id0
+	unknown, ok = d.GetUnknown(id0)
+	r.Contains(unknown, id1)
+	r.Len(unknown, 1)
+	r.True(ok)
+
+	// id1 should have a unknown of [id0, id1], since it knows nothing
+	unknown, ok = d.GetUnknown(id1)
+	r.Contains(unknown, id0)
+	r.Contains(unknown, id1)
+	r.Len(unknown, 2)
+	r.True(ok)
+
+	// add id2, who knows of id0, id1, and id2
+	// id0 and id1 don't know of id2
+	p2 := []ids.NodeID{id0, id1, id2}
+	d.Add(id2)
+	r.True(d.UpdateKnown(id2, p2))
+
+	// id0 doesn't know about [id1, id2]
+	unknown, ok = d.GetUnknown(id0)
+	r.Contains(unknown, id1)
+	r.Contains(unknown, id2)
+	r.Len(unknown, 2)
+	r.True(ok)
+
+	// id1 doesn't know about [id0, id1, id2]
+	unknown, ok = d.GetUnknown(id1)
+	r.Contains(unknown, id0)
+	r.Contains(unknown, id1)
+	r.Contains(unknown, id2)
+	r.Len(unknown, 3)
+	r.True(ok)
+
+	// id2 knows about everyone
+	unknown, ok = d.GetUnknown(id2)
+	r.Empty(unknown)
+	r.True(ok)
+
+	// stop tracking id1
+	r.True(d.Remove(id1))
+	unknown, ok = d.GetUnknown(id1)
+	r.Nil(unknown)
+	r.False(ok)
+
+	// id0 doesn't know about [id2], we shouldn't care about id1 anymore
+	unknown, ok = d.GetUnknown(id0)
+	r.Contains(unknown, id2)
+	r.Len(unknown, 1)
+	r.True(ok)
+
+	// id2 knows everyone
+	unknown, ok = d.GetUnknown(id2)
+	r.Empty(unknown)
+	r.True(ok)
+}
