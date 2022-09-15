4af4338409d4e00e2d409608302fb2f5c6b9c215
Gyuho Lee <gyuho.lee@avalabs.org>
2022-09-15 11:50:17 -0700

    network/peer/test_peer: revert
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- network/peer/test_peer.go
+++ network/peer/test_peer.go
@@ -1,12 +1,11 @@
 // Copyright (C) 2019-2022, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-package test
+package peer
 
 import (
 	"context"
 	"crypto"
-	"crypto/tls"
 	"net"
 	"time"
 
@@ -14,7 +13,6 @@ import (
 
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/message"
-	network_peer "github.com/ava-labs/avalanchego/network/peer"
 	"github.com/ava-labs/avalanchego/network/throttling"
 	"github.com/ava-labs/avalanchego/snow/networking/router"
 	"github.com/ava-labs/avalanchego/snow/networking/tracker"
@@ -30,42 +28,7 @@ import (
 
 const maxMessageToSend = 1024
 
-type Op struct {
-	networkID   uint32
-	stakingKey  string
-	stakingCert string
-	useProto    bool
-}
-
-type OpFunc func(*Op)
-
-func (op *Op) applyOpts(opts []OpFunc) {
-	for _, opt := range opts {
-		opt(op)
-	}
-}
-
-func WithNetworkID(networkID uint32) OpFunc {
-	return func(op *Op) {
-		op.networkID = networkID
-	}
-}
-
-func WithStakingCert(key string, cert string) OpFunc {
-	return func(op *Op) {
-		op.stakingKey = key
-		op.stakingCert = cert
-	}
-}
-
-// "true" to use protobufs message creator.
-func WithProtoMessage(useProto bool) OpFunc {
-	return func(op *Op) {
-		op.useProto = useProto
-	}
-}
-
-// Start provides a simple interface to create a peer that has finished
+// StartTestPeer provides a simple interface to create a peer that has finished
 // the p2p handshake.
 //
 // This function will generate a new TLS key to use when connecting to the peer.
@@ -79,56 +42,42 @@ func WithProtoMessage(useProto bool) OpFunc {
 //     will be returned.
 //   - [router] will be called with all non-handshake messages received by the
 //     peer.
-func Start(ctx context.Context, ip ips.IPPort, router router.InboundHandler, opts ...OpFunc) (network_peer.Peer, error) {
-	ret := &Op{networkID: constants.LocalID}
-	ret.applyOpts(opts)
-
+func StartTestPeer(
+	ctx context.Context,
+	ip ips.IPPort,
+	networkID uint32,
+	router router.InboundHandler,
+) (Peer, error) {
 	dialer := net.Dialer{}
 	conn, err := dialer.DialContext(ctx, constants.NetworkType, ip.String())
 	if err != nil {
 		return nil, err
 	}
 
-	var tlsCert *tls.Certificate
-	if ret.stakingKey != "" && ret.stakingCert != "" {
-		tlsCert, err = staking.LoadTLSCertFromFiles(ret.stakingKey, ret.stakingCert)
-	} else {
-		tlsCert, err = staking.NewTLSCert()
-	}
+	tlsCert, err := staking.NewTLSCert()
 	if err != nil {
 		return nil, err
 	}
 
-	tlsConfg := network_peer.TLSConfig(*tlsCert, nil)
-	clientUpgrader := network_peer.NewTLSClientUpgrader(tlsConfg)
+	tlsConfg := TLSConfig(*tlsCert, nil)
+	clientUpgrader := NewTLSClientUpgrader(tlsConfg)
 
 	peerID, conn, cert, err := clientUpgrader.Upgrade(conn)
 	if err != nil {
 		return nil, err
 	}
 
-	reg := prometheus.NewRegistry()
-	var mc message.Creator
-	if !ret.useProto {
-		mc, err = message.NewCreator(
-			reg,
-			"",
-			true,
-			10*time.Second,
-		)
-	} else {
-		mc, err = message.NewCreatorWithProto(
-			reg,
-			"",
-			true,
-			10*time.Second,
-		)
-	}
+	mc, err := message.NewCreator(
+		prometheus.NewRegistry(),
+		"",
+		true,
+		10*time.Second,
+	)
 	if err != nil {
 		return nil, err
 	}
 
-	metrics, err := network_peer.NewMetrics(
+	metrics, err := NewMetrics(
 		logging.NoLog{},
 		"",
 		prometheus.NewRegistry(),
@@ -146,15 +95,15 @@ func Start(ctx context.Context, ip ips.IPPort, router router.InboundHandler, opt
 		return nil, err
 	}
 
-	peer := network_peer.Start(
-		&network_peer.Config{
+	peer := Start(
+		&Config{
 			Metrics:             metrics,
 			MessageCreator:      mc,
 			Log:                 logging.NoLog{},
 			InboundMsgThrottler: throttling.NewNoInboundThrottler(),
-			Network: network_peer.NewTestNetwork(
+			Network: NewTestNetwork(
 				mc,
-				ret.networkID,
+				networkID,
 				ipPort,
 				version.CurrentApp,
 				tlsCert.PrivateKey.(crypto.Signer),
@@ -162,10 +111,10 @@ func Start(ctx context.Context, ip ips.IPPort, router router.InboundHandler, opt
 				100,
 			),
 			Router:               router,
-			VersionCompatibility: version.GetCompatibility(ret.networkID),
+			VersionCompatibility: version.GetCompatibility(networkID),
 			MySubnets:            ids.Set{},
 			Beacons:              validators.NewSet(),
-			NetworkID:            ret.networkID,
+			NetworkID:            networkID,
 			PingFrequency:        constants.DefaultPingFrequency,
 			PongTimeout:          constants.DefaultPingPongTimeout,
 			MaxClockDifference:   time.Minute,
@@ -174,7 +123,7 @@ func Start(ctx context.Context, ip ips.IPPort, router router.InboundHandler, opt
 		conn,
 		cert,
 		peerID,
-		network_peer.NewBlockingMessageQueue(
+		NewBlockingMessageQueue(
 			metrics,
 			logging.NoLog{},
 			maxMessageToSend,
