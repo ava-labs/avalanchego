c16981d70afd88cf396d9f6b0f9dd8ebbfb01405
Patrick O'Grady <prohb125@gmail.com>
2022-09-19 19:12:04 -0700

    add envs everywhere

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -4,4 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Set the CGO flags to build the portable version of BLST
+CGO_CFLAGS_ALLOW="-O -D__BLST_PORTABLE__"
+CGO_CFLAGS="-O -D__BLST_PORTABLE__"
+
 go test -race -timeout="120s" -coverprofile="coverage.out" -covermode="atomic" $(go list ./... | grep -v /mocks | grep -v proto | grep -v tests)
