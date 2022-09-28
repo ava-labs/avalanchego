de547dbbc103137ceefe65b03dcb4bdfd086334d
Darioush Jalali <darioush.jalali@avalabs.org>
2022-09-28 16:07:54 -0700

    bump version for banff release (#1051)
    * bump version for banff release
    
    * bump versions.sh

--- graft/coreth/scripts/versions.sh
+++ graft/coreth/scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.10.0'}
+coreth_version=${CORETH_VERSION:-'v0.11.0'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.8.5'}
