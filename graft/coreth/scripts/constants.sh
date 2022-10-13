16dc9245740da8afa81c7f9472221b04c00e747e
Dan Laine <daniel.laine@avalabs.org>
2022-10-12 20:10:16 -0400

    Use generic math (#1056)
    * use generic math
    
    * update avalanchego version
    
    * nit
    
    * take 2
    
    * take 3
    
    * take 4
    
    * fix blst cgo
    
    * whoopse
    
    Co-authored-by: Stephen <stephen@avalabs.org>

--- graft/coreth/scripts/constants.sh
+++ graft/coreth/scripts/constants.sh
@@ -21,3 +21,9 @@ coreth_commit="$(git --git-dir="$CORETH_PATH/.git" rev-parse HEAD)"
 coreth_commit_id="${coreth_commit::8}"
 
 build_image_id=${BUILD_IMAGE_ID:-"$avalanche_version-$coreth_commit_id"}
+
+# Set the CGO flags to use the portable version of BLST
+#
+# We use "export" here instead of just setting a bash variable because we need
+# to pass this flag to all child processes spawned by the shell.
+export CGO_CFLAGS="-O -D__BLST_PORTABLE__"
