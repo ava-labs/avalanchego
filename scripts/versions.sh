c74c6662cc682f50ef74c2c6b6ba1309a12a9660
Dan Laine <daniel.laine@avalabs.org>
2022-10-20 17:37:34 -0400

    revert coreth_version

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -7,4 +7,4 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-coreth_version=${CORETH_VERSION:-'28867416e032c0a64dd6c4d760d123d534a4c5ad'}
+coreth_version=${CORETH_VERSION:-'v0.11.1-rc.2'}
