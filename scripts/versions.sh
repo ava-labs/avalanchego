bd098eb6bb00cf46ced44cbb794761392ed1125d
aaronbuchwald <aaron.buchwald56@gmail.com>
2022-10-20 14:44:02 -0700

    Bump coreth version to v0.11.1 (#1090)

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.11.0'}
+coreth_version=${CORETH_VERSION:-'v0.11.1'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.9.1-rc.3'}
