d17a587bd96fef7c260122402ddd7f28579e38d0
Dan Laine <daniel.laine@avalabs.org>
2022-10-21 15:59:03 -0400

    nit

--- config/flags.go
+++ config/flags.go
@@ -19,6 +19,7 @@ import (
 	"github.com/ava-labs/avalanchego/database/leveldb"
 	"github.com/ava-labs/avalanchego/database/memdb"
 	"github.com/ava-labs/avalanchego/genesis"
+	"github.com/ava-labs/avalanchego/trace"
 	"github.com/ava-labs/avalanchego/utils/constants"
 	"github.com/ava-labs/avalanchego/utils/ulimit"
 	"github.com/ava-labs/avalanchego/utils/units"
@@ -357,7 +358,7 @@ func addNodeFlags(fs *flag.FlagSet) {
 
 	// Opentelemetry tracing
 	fs.Bool(TracingEnabledKey, false, "If true, enable opentelemetry tracing. If false, uses no-op tracer")
-	fs.String(TracingExporterTypeKey, "grpc", "Type of exporter to use for tracing. Options are [grpc, http]")
+	fs.String(TracingExporterTypeKey, trace.GRPC.String(), fmt.Sprintf("Type of exporter to use for tracing. Options are [%s, %s]", trace.GRPC, trace.HTTP))
 	fs.String(TracingEndpointKey, "localhost:4317", "The endpoint to send trace data to")
 	fs.Float64(TracingSampleRateKey, 0.1, "The fraction of traces to sample. If >= 1, always sample. If <= 0, never sample")
 	// TODO add flag to take in headers to send from exporter
