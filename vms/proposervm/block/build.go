havan/caminogo

[ProposerVM] Recover nodeId from certificate

vms/proposervm/block/build.go
@@ -55,6 +55,7 @@ func Build(
 	parentID ids.ID,
 	timestamp time.Time,
 	pChainHeight uint64,
+	nodeID ids.ShortID,
 	cert *x509.Certificate,
 	blockBytes []byte,
 	chainID ids.ID,
@@ -70,7 +71,7 @@ func Build(
 		},
 		timestamp: timestamp,
 		cert:      cert,
-		proposer:  hashing.ComputeHash160Array(hashing.ComputeHash256(cert.Raw)),
+		proposer:  nodeID,
 	}
 	var blockIntf SignedBlock = block
 

ava-labs/avalanchego

vms/proposervm/block/build.go
@@ -42,6 +42,7 @@ func Build(
 	parentID ids.ID,
 	timestamp time.Time,
 	pChainHeight uint64,
+	nodeID ids.NodeID,
 	cert *x509.Certificate,
 	blockBytes []byte,
 	chainID ids.ID,
@@ -57,7 +58,7 @@ func Build(
 		},
 		timestamp: timestamp,
 		cert:      cert,
-		proposer:  ids.NodeIDFromCert(cert),
+		proposer:  nodeID,
 	}
 	var blockIntf SignedBlock = block
 