2519829ff3e47085c99c26313cf4a43839124f15
aaronbuchwald <aaron.buchwald56@gmail.com>
2022-11-07 18:37:18 -0500

    Start v0.4.4 release cycle (#349)

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-subnet_evm_version=${SUBNET_EVM_VERSION:-'v0.4.3'}
+subnet_evm_version=${SUBNET_EVM_VERSION:-'v0.4.4'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.9.2'}
 network_runner_version=${NETWORK_RUNNER_VERSION:-'35be10cd3867a94fbe960a1c14a455f179de60d9'}
