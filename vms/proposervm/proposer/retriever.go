c30eec97f436e4afe5099f961781812fcb8e32a2
Tedi Mitiku <tedi.m52@gmail.com>
2022-11-15 21:11:27 -0500

    implement GetCurrentProposers function in retriever

--- vms/proposervm/proposer/retriever.go
+++ vms/proposervm/proposer/retriever.go
@@ -1,14 +1,17 @@
 package proposer
 
 import (
+	"sort"
+
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow/validators"
+	"github.com/ava-labs/avalanchego/utils/math"
 	"github.com/ava-labs/avalanchego/utils/sampler"
 	"github.com/ava-labs/avalanchego/utils/wrappers"
 )
 
 const (
-	NumProposers = 6
+	NumProposers = 2
 )
 
 type Retriever interface {
@@ -51,7 +54,59 @@ func (r *retriever) SetChainHeight(chainHeight uint64){
 func (r *retriever) GetCurrentProposers() (ids.NodeIDSet, error) {
 	proposers := ids.NewNodeIDSet(NumProposers)
 
-	// compute current proposers
+  	// get the validator set by the p-chain height
+	validatorsMap, err := r.state.GetValidatorSet(r.pChainHeight, r.subnetID)
+	if err != nil {
+		return proposers, err
+	}
+
+	// convert the map of validators to a slice
+	validators := make(validatorsSlice, 0, len(validatorsMap))
+	weight := uint64(0)
+	for k, v := range validatorsMap {
+		validators = append(validators, validatorData{
+			id:     k,
+			weight: v,
+		})
+		newWeight, err := math.Add64(weight, v)
+		if err != nil {
+			return proposers, err
+		}
+		weight = newWeight
+	}
+
+  	// canonically sort validators
+	// Note: validators are sorted by ID, sorting by weight would not create a
+	// canonically sorted list
+	sort.Sort(validators)
+
+	// convert the slice of validators to a slice of weights
+	validatorWeights := make([]uint64, len(validators))
+	for i, v := range validators {
+		validatorWeights[i] = v.weight
+	}
+
+	if err := r.sampler.Initialize(validatorWeights); err != nil {
+		return proposers, err
+	}
+
+	numToSample := NumProposers
+	if weight < uint64(numToSample) {
+		numToSample = int(weight)
+	}
+	
+	seed := r.chainHeight ^ r.chainSource
+	r.sampler.Seed(int64(seed))
+
+	indices, err := r.sampler.Sample(numToSample)
+	if err != nil {
+		return proposers, err
+	}
+
+	for _, index := range indices {
+		proposerNodeID := validators[index].id
+		proposers.Add(proposerNodeID)
+	}
 
 	return proposers, nil
 }
