b1b7c46b1b4e7293f8d010e59190dffc93ed2a22
aaronbuchwald <aaron.buchwald56@gmail.com>
2022-11-18 12:16:41 -0500

    Start v0.11.4 release cycle (#1129)

--- graft/coreth/scripts/versions.sh
+++ graft/coreth/scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.11.3'}
+coreth_version=${CORETH_VERSION:-'v0.11.4'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.9.3-rc.1'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.9.3'}
