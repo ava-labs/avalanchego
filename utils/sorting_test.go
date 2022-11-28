3cdcd7714f1011ed4ee080012b7b73794cf322dd
Dan Laine <daniel.laine@avalabs.org>
2022-11-28 14:32:30 -0500

    add test

--- utils/sorting_test.go
+++ utils/sorting_test.go
@@ -109,3 +109,29 @@ func TestIsUnique(t *testing.T) {
 	s = []int{1, 2, 1}
 	require.False(IsUnique(s))
 }
+
+func TestSortByHash(t *testing.T) {
+	require := require.New(t)
+
+	s := [][]byte{}
+	SortByHash(s)
+	require.Len(s, 0)
+
+	s = [][]byte{{1}}
+	SortByHash(s)
+	require.Len(s, 1)
+	require.Equal([]byte{1}, s[0])
+
+	s = [][]byte{{1}, {2}}
+	SortByHash(s)
+	require.Len(s, 2)
+	require.Equal([]byte{1}, s[0])
+	require.Equal([]byte{2}, s[1])
+
+	for i := byte(0); i < 100; i++ {
+		s = [][]byte{{i}, {i + 1}, {i + 2}}
+		SortByHash(s)
+		require.Len(s, 3)
+		require.True(IsSortedAndUniqueByHash(s))
+	}
+}
