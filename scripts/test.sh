725ac24b13b2ff5c6147bd7be3cfd530e76f1915
peak3d <pfau@peak3d.de>
2022-11-30 12:12:50 +0100

    [CI] Disable race for Mac / win tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CGO_CFLAGS="-O -D__BLST_PORTABLE__" go test -race -timeout="120s" -coverprofile="coverage.out" -covermode="atomic" $(go list ./... | grep -v /mocks | grep -v proto | grep -v tests)
+CGO_CFLAGS="-O -D__BLST_PORTABLE__" go test ${1-} -timeout="120s" -coverprofile="coverage.out" -covermode="atomic" $(go list ./... | grep -v /mocks | grep -v proto | grep -v tests)
