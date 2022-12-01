ac4de1368bb522f06abde262ecb5b3867810222c
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2022-12-01 17:28:39 -0500

    document invariant

--- vms/platformvm/vm.go
+++ vms/platformvm/vm.go
@@ -474,6 +474,7 @@ func (vm *VM) Disconnected(_ context.Context, nodeID ids.NodeID) error {
 
 // GetValidatorSet returns the validator set at the specified height for the
 // provided subnetID.
+// Invariant: TxIDs are not populated in the validators.Validator struct.
 func (vm *VM) GetValidatorSet(ctx context.Context, height uint64, subnetID ids.ID) (map[ids.NodeID]*validators.Validator, error) {
 	validatorSetsCache, exists := vm.validatorSetCaches[subnetID]
 	if !exists {
