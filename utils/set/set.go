5b9757ab5507cca38ee380f06fae70eeebffefda
Dan Laine <daniel.laine@avalabs.org>
2022-12-02 17:39:11 -0500

    use maps.Equals

--- utils/set/set.go
+++ utils/set/set.go
@@ -136,15 +136,7 @@ func (s Set[T]) CappedList(size int) []T {
 
 // Equals returns true if the sets contain the same elements
 func (s Set[T]) Equals(other Set[T]) bool {
-	if s.Len() != other.Len() {
-		return false
-	}
-	for elt := range other {
-		if _, contains := s[elt]; !contains {
-			return false
-		}
-	}
-	return true
+	return maps.Equal(s, other)
 }
 
 // Removes and returns an element.
