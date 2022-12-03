455cd5611fd1363b741dc2368c9ec1fecb8e82c8
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2022-12-02 20:30:27 -0500

    remove GossipTracker#HasValidator

--- network/peer/gossip_tracker.go
+++ network/peer/gossip_tracker.go
@@ -54,9 +54,6 @@ type GossipTracker interface {
 	// 	bool: False if [peerID] was not tracked. True otherwise.
 	StopTrackingPeer(peerID ids.NodeID) bool
 
-	// HasValidator returns if [validator] exists in the gossip tracker
-	// 	bool: False if [validator] is not present. True otherwise.
-	HasValidator(validator ValidatorID) bool
 	// AddValidator adds a validator that can be gossiped about
 	// 	bool: False if [validator] was already present. True otherwise.
 	AddValidator(validator ValidatorID) bool
@@ -149,18 +146,6 @@ func (g *gossipTracker) StopTrackingPeer(peerID ids.NodeID) bool {
 	g.metrics.trackedPeersSize.Set(float64(len(g.trackedPeers)))
 
 	return true
-}
-
-func (g *gossipTracker) HasValidator(validator ValidatorID) bool {
-	g.lock.RLock()
-	defer g.lock.RUnlock()
-
-	idx, ok := g.validatorsToIndices[validator.NodeID]
-	if !ok {
-		return false
-	}
-
-	return validator == g.validators[idx]
 }
 
 func (g *gossipTracker) AddValidator(validator ValidatorID) bool {
