80964f46a1fd7608fa130bc94c815d66cb21a68a
Stephen <stephen@avalabs.org>
2022-12-21 22:15:15 -0500

    bump coreth

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -7,4 +7,4 @@
 
 # Set up the versions to be used
 # Don't export them as their used in the context of other calls
-coreth_version=${CORETH_VERSION:-'v0.11.5-0.20221213204439-7bd1eeaeaa9c'}
+coreth_version=${CORETH_VERSION:-'v0.11.5-rc.0'}
