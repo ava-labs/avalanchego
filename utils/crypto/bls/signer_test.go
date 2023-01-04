6c33a0c84357c24ce0c41087fd55d7a0617050d2
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2023-01-04 21:44:06 +0900

    unit tests

--- utils/crypto/bls/signer_test.go
+++ utils/crypto/bls/signer_test.go
@@ -5,8 +5,55 @@ package bls
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestSign(t *testing.T) {
-	// TODO
+	type args struct {
+		msg []byte
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "nil message",
+			args: args{
+				msg: nil,
+			},
+		},
+		{
+			name: "empty message",
+			args: args{
+				msg: []byte{},
+			},
+		},
+		{
+			name: "non-empty message",
+			args: args{
+				msg: []byte{1, 2, 3, 3, 5},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			r := require.New(t)
+
+			sk, err := NewSecretKey()
+			r.NoError(err)
+
+			signer := NewSigner(sk)
+			sig, err := signer.Sign(test.args.msg)
+			r.NoError(err)
+
+			sigBytes, err := SignatureFromBytes(sig)
+			r.NoError(err)
+
+			expected := Sign(sk, test.args.msg)
+			r.Equal(expected, sigBytes)
+		})
+	}
 }
