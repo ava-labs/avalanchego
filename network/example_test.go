e13b273c0648a461fe6c3f6af366c4db4fd81934
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2023-01-27 01:44:02 +0900

    Add p2p network client helper (#2511)
    Co-authored-by: Stephen <stephen@avalabs.org>

---
+++ network/example_test.go
@@ -0,0 +1,154 @@
+// Copyright (C) 2019-2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package network
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/ava-labs/avalanchego/genesis"
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/message"
+	"github.com/ava-labs/avalanchego/snow/networking/router"
+	"github.com/ava-labs/avalanchego/snow/validators"
+	"github.com/ava-labs/avalanchego/utils/constants"
+	"github.com/ava-labs/avalanchego/utils/ips"
+	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/ava-labs/avalanchego/utils/set"
+	"github.com/ava-labs/avalanchego/version"
+)
+
+var _ router.ExternalHandler = (*testExternalHandler)(nil)
+
+// Note: all of the external handler's methods are called on peer goroutines. It
+// is possible for multiple concurrent calls to happen with different NodeIDs.
+// However, a given NodeID will only be performing one call at a time.
+type testExternalHandler struct {
+	log logging.Logger
+}
+
+// Note: HandleInbound will be called with raw P2P messages, the networking
+// implementation does not implicitly register timeouts, so this handler is only
+// called by messages explicitly sent by the peer. If timeouts are required,
+// that must be handled by the user of this utility.
+func (t *testExternalHandler) HandleInbound(_ context.Context, message message.InboundMessage) {
+	t.log.Info(
+		"receiving message",
+		zap.Stringer("op", message.Op()),
+	)
+}
+
+func (t *testExternalHandler) Connected(nodeID ids.NodeID, version *version.Application, subnetID ids.ID) {
+	t.log.Info(
+		"connected",
+		zap.Stringer("nodeID", nodeID),
+		zap.Stringer("version", version),
+		zap.Stringer("subnetID", subnetID),
+	)
+}
+
+func (t *testExternalHandler) Disconnected(nodeID ids.NodeID) {
+	t.log.Info(
+		"disconnected",
+		zap.Stringer("nodeID", nodeID),
+	)
+}
+
+type testAggressiveValidatorSet struct {
+	validators.Set
+}
+
+func (*testAggressiveValidatorSet) Contains(ids.NodeID) bool {
+	return true
+}
+
+func ExampleNewTestNetwork() {
+	log := logging.NewLogger(
+		"networking",
+		logging.NewWrappedCore(
+			logging.Info,
+			os.Stdout,
+			logging.Colors.ConsoleEncoder(),
+		),
+	)
+
+	// Needs to be periodically updated by the caller to have the latest
+	// validator set
+	validators := &testAggressiveValidatorSet{
+		Set: validators.NewSet(),
+	}
+
+	// If we want to be able to communicate with non-primary network subnets, we
+	// should register them here.
+	trackedSubnets := set.Set[ids.ID]{}
+
+	// Messages and connections are handled by the external handler.
+	handler := &testExternalHandler{
+		log: log,
+	}
+
+	network, err := NewTestNetwork(
+		log,
+		constants.FujiID,
+		validators,
+		trackedSubnets,
+		handler,
+	)
+	if err != nil {
+		log.Fatal(
+			"failed to create test network",
+			zap.Error(err),
+		)
+		return
+	}
+
+	// We need to initially connect to some nodes in the network before peer
+	// gossip will enable connecting to all the remaining nodes in the network.
+	beaconIPs, beaconIDs := genesis.SampleBeacons(constants.FujiID, 5)
+	for i, beaconIDStr := range beaconIDs {
+		beaconID, err := ids.NodeIDFromString(beaconIDStr)
+		if err != nil {
+			log.Fatal(
+				"failed to parse beaconID",
+				zap.String("beaconID", beaconIDStr),
+				zap.Error(err),
+			)
+			return
+		}
+
+		beaconIPStr := beaconIPs[i]
+		ipPort, err := ips.ToIPPort(beaconIPStr)
+		if err != nil {
+			log.Fatal(
+				"failed to parse beaconIP",
+				zap.String("beaconIP", beaconIPStr),
+				zap.Error(err),
+			)
+			return
+		}
+
+		network.ManuallyTrack(beaconID, ipPort)
+	}
+
+	// Typically network.StartClose() should be called based on receiving a
+	// SIGINT or SIGTERM. For the example, we close the network after 15s.
+	go log.RecoverAndPanic(func() {
+		time.Sleep(15 * time.Second)
+		network.StartClose()
+	})
+
+	// network.Send(...) and network.Gossip(...) can be used here to send
+	// messages to peers.
+
+	// Calling network.Dispatch() will block until a fatal error occurs or
+	// network.StartClose() is called.
+	err = network.Dispatch()
+	log.Info(
+		"network exited",
+		zap.Error(err),
+	)
+}
---
+++ network/test_network.go
@@ -0,0 +1,248 @@
+// Copyright (C) 2019-2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package network
+
+import (
+	"crypto"
+	"errors"
+	"math"
+	"net"
+	"runtime"
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/message"
+	"github.com/ava-labs/avalanchego/network/dialer"
+	"github.com/ava-labs/avalanchego/network/peer"
+	"github.com/ava-labs/avalanchego/network/throttling"
+	"github.com/ava-labs/avalanchego/snow/networking/router"
+	"github.com/ava-labs/avalanchego/snow/networking/tracker"
+	"github.com/ava-labs/avalanchego/snow/uptime"
+	"github.com/ava-labs/avalanchego/snow/validators"
+	"github.com/ava-labs/avalanchego/staking"
+	"github.com/ava-labs/avalanchego/utils/constants"
+	"github.com/ava-labs/avalanchego/utils/ips"
+	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/ava-labs/avalanchego/utils/math/meter"
+	"github.com/ava-labs/avalanchego/utils/resource"
+	"github.com/ava-labs/avalanchego/utils/set"
+	"github.com/ava-labs/avalanchego/utils/units"
+)
+
+var (
+	errClosed = errors.New("closed")
+
+	_ net.Listener = (*noopListener)(nil)
+)
+
+type noopListener struct {
+	once   sync.Once
+	closed chan struct{}
+}
+
+func newNoopListener() net.Listener {
+	return &noopListener{
+		closed: make(chan struct{}),
+	}
+}
+
+func (l *noopListener) Accept() (net.Conn, error) {
+	<-l.closed
+	return nil, errClosed
+}
+
+func (l *noopListener) Close() error {
+	l.once.Do(func() {
+		close(l.closed)
+	})
+	return nil
+}
+
+func (*noopListener) Addr() net.Addr {
+	return &net.TCPAddr{
+		IP:   net.IPv4zero,
+		Port: 0,
+	}
+}
+
+func NewTestNetwork(
+	log logging.Logger,
+	networkID uint32,
+	currentValidators validators.Set,
+	trackedSubnets set.Set[ids.ID],
+	router router.ExternalHandler,
+) (Network, error) {
+	metrics := prometheus.NewRegistry()
+	msgCreator, err := message.NewCreator(
+		metrics,
+		"",
+		constants.DefaultNetworkCompressionEnabled,
+		constants.DefaultNetworkMaximumInboundTimeout,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	networkConfig := Config{
+		ThrottlerConfig: ThrottlerConfig{
+			InboundConnUpgradeThrottlerConfig: throttling.InboundConnUpgradeThrottlerConfig{
+				UpgradeCooldown:        constants.DefaultInboundConnUpgradeThrottlerCooldown,
+				MaxRecentConnsUpgraded: int(math.Ceil(constants.DefaultInboundThrottlerMaxConnsPerSec * constants.DefaultInboundConnUpgradeThrottlerCooldown.Seconds())),
+			},
+
+			InboundMsgThrottlerConfig: throttling.InboundMsgThrottlerConfig{
+				MsgByteThrottlerConfig: throttling.MsgByteThrottlerConfig{
+					VdrAllocSize:        constants.DefaultInboundThrottlerVdrAllocSize,
+					AtLargeAllocSize:    constants.DefaultInboundThrottlerAtLargeAllocSize,
+					NodeMaxAtLargeBytes: constants.DefaultInboundThrottlerNodeMaxAtLargeBytes,
+				},
+
+				BandwidthThrottlerConfig: throttling.BandwidthThrottlerConfig{
+					RefillRate:   constants.DefaultInboundThrottlerBandwidthRefillRate,
+					MaxBurstSize: constants.DefaultInboundThrottlerBandwidthMaxBurstSize,
+				},
+
+				CPUThrottlerConfig: throttling.SystemThrottlerConfig{
+					MaxRecheckDelay: constants.DefaultInboundThrottlerCPUMaxRecheckDelay,
+				},
+
+				DiskThrottlerConfig: throttling.SystemThrottlerConfig{
+					MaxRecheckDelay: constants.DefaultInboundThrottlerDiskMaxRecheckDelay,
+				},
+
+				MaxProcessingMsgsPerNode: constants.DefaultInboundThrottlerMaxProcessingMsgsPerNode,
+			},
+			OutboundMsgThrottlerConfig: throttling.MsgByteThrottlerConfig{
+				VdrAllocSize:        constants.DefaultOutboundThrottlerVdrAllocSize,
+				AtLargeAllocSize:    constants.DefaultOutboundThrottlerAtLargeAllocSize,
+				NodeMaxAtLargeBytes: constants.DefaultOutboundThrottlerNodeMaxAtLargeBytes,
+			},
+
+			MaxInboundConnsPerSec: constants.DefaultInboundThrottlerMaxConnsPerSec,
+		},
+
+		HealthConfig: HealthConfig{
+			Enabled:                      true,
+			MinConnectedPeers:            constants.DefaultNetworkHealthMinPeers,
+			MaxTimeSinceMsgReceived:      constants.DefaultNetworkHealthMaxTimeSinceMsgReceived,
+			MaxTimeSinceMsgSent:          constants.DefaultNetworkHealthMaxTimeSinceMsgSent,
+			MaxPortionSendQueueBytesFull: constants.DefaultNetworkHealthMaxPortionSendQueueFill,
+			MaxSendFailRate:              constants.DefaultNetworkHealthMaxSendFailRate,
+			SendFailRateHalflife:         constants.DefaultHealthCheckAveragerHalflife,
+		},
+
+		ProxyEnabled:           constants.DefaultNetworkTCPProxyEnabled,
+		ProxyReadHeaderTimeout: constants.DefaultNetworkTCPProxyReadTimeout,
+
+		DialerConfig: dialer.Config{
+			ThrottleRps:       constants.DefaultOutboundConnectionThrottlingRps,
+			ConnectionTimeout: constants.DefaultOutboundConnectionTimeout,
+		},
+
+		TimeoutConfig: TimeoutConfig{
+			PingPongTimeout:      constants.DefaultPingPongTimeout,
+			ReadHandshakeTimeout: constants.DefaultNetworkReadHandshakeTimeout,
+		},
+
+		PeerListGossipConfig: PeerListGossipConfig{
+			PeerListNumValidatorIPs:        constants.DefaultNetworkPeerListNumValidatorIPs,
+			PeerListValidatorGossipSize:    constants.DefaultNetworkPeerListValidatorGossipSize,
+			PeerListNonValidatorGossipSize: constants.DefaultNetworkPeerListNonValidatorGossipSize,
+			PeerListPeersGossipSize:        constants.DefaultNetworkPeerListPeersGossipSize,
+			PeerListGossipFreq:             constants.DefaultNetworkPeerListGossipFreq,
+		},
+
+		DelayConfig: DelayConfig{
+			InitialReconnectDelay: constants.DefaultNetworkInitialReconnectDelay,
+			MaxReconnectDelay:     constants.DefaultNetworkMaxReconnectDelay,
+		},
+
+		MaxClockDifference:           constants.DefaultNetworkMaxClockDifference,
+		CompressionEnabled:           constants.DefaultNetworkCompressionEnabled,
+		PingFrequency:                constants.DefaultPingFrequency,
+		AllowPrivateIPs:              constants.DefaultNetworkAllowPrivateIPs,
+		UptimeMetricFreq:             constants.DefaultUptimeMetricFreq,
+		MaximumInboundMessageTimeout: constants.DefaultNetworkMaximumInboundTimeout,
+
+		RequireValidatorToConnect: constants.DefaultNetworkRequireValidatorToConnect,
+		PeerReadBufferSize:        constants.DefaultNetworkPeerReadBufferSize,
+		PeerWriteBufferSize:       constants.DefaultNetworkPeerWriteBufferSize,
+	}
+
+	networkConfig.NetworkID = networkID
+	networkConfig.TrackedSubnets = trackedSubnets
+
+	tlsCert, err := staking.NewTLSCert()
+	if err != nil {
+		return nil, err
+	}
+	tlsConfig := peer.TLSConfig(*tlsCert, nil)
+	networkConfig.TLSConfig = tlsConfig
+	networkConfig.TLSKey = tlsCert.PrivateKey.(crypto.Signer)
+
+	validatorManager := validators.NewManager()
+	beacons := validators.NewSet()
+	networkConfig.Validators = validatorManager
+	networkConfig.Validators.Add(constants.PrimaryNetworkID, currentValidators)
+	networkConfig.Beacons = beacons
+	// This never actually does anything because we never initialize the P-chain
+	networkConfig.UptimeCalculator = uptime.NoOpCalculator
+
+	// TODO actually monitor usage
+	// TestNetwork doesn't use disk so we don't need to track it, but we should
+	// still have guardrails around cpu/memory usage.
+	networkConfig.ResourceTracker, err = tracker.NewResourceTracker(
+		metrics,
+		resource.NoUsage,
+		&meter.ContinuousFactory{},
+		constants.DefaultHealthCheckAveragerHalflife,
+	)
+	if err != nil {
+		return nil, err
+	}
+	networkConfig.CPUTargeter = tracker.NewTargeter(
+		&tracker.TargeterConfig{
+			VdrAlloc:           float64(runtime.NumCPU()),
+			MaxNonVdrUsage:     .8 * float64(runtime.NumCPU()),
+			MaxNonVdrNodeUsage: float64(runtime.NumCPU()) / 8,
+		},
+		currentValidators,
+		networkConfig.ResourceTracker.CPUTracker(),
+	)
+	networkConfig.DiskTargeter = tracker.NewTargeter(
+		&tracker.TargeterConfig{
+			VdrAlloc:           1000 * units.GiB,
+			MaxNonVdrUsage:     1000 * units.GiB,
+			MaxNonVdrNodeUsage: 1000 * units.GiB,
+		},
+		currentValidators,
+		networkConfig.ResourceTracker.DiskTracker(),
+	)
+
+	networkConfig.MyIPPort = ips.NewDynamicIPPort(net.IPv4zero, 0)
+
+	networkConfig.GossipTracker, err = peer.NewGossipTracker(metrics, "")
+	if err != nil {
+		return nil, err
+	}
+
+	return NewNetwork(
+		&networkConfig,
+		msgCreator,
+		metrics,
+		log,
+		newNoopListener(),
+		dialer.NewDialer(
+			constants.NetworkType,
+			dialer.Config{
+				ThrottleRps:       constants.DefaultOutboundConnectionThrottlingRps,
+				ConnectionTimeout: constants.DefaultOutboundConnectionTimeout,
+			},
+			log,
+		),
+		router,
+	)
+}
