e3d2daccf8284c430b21bae5bdcfdd402a74b206
Stephen Buttolph <stephen@avalabs.org>
2023-02-24 12:37:51 -0500

    Always return error in BadBlockReason (#1201)

--- graft/coreth/core/blockchain.go
+++ graft/coreth/core/blockchain.go
@@ -1545,7 +1545,7 @@ type BadBlockReason struct {
 	Receipts    types.Receipts      `json:"receipts"`
 	Number      uint64              `json:"number"`
 	Hash        common.Hash         `json:"hash"`
-	Error       error               `json:"error"`
+	Error       string              `json:"error"`
 }
 
 func (b *BadBlockReason) String() string {
@@ -1563,7 +1563,7 @@ func (b *BadBlockReason) String() string {
 	Hash: %#x
 	%v
 	
-	Error: %v
+	Error: %s
 	##############################
 	`, b.ChainConfig, b.Number, b.Hash, receiptString, b.Error)
 
@@ -1601,7 +1601,7 @@ func (bc *BlockChain) reportBlock(block *types.Block, receipts types.Receipts, e
 		Receipts:    receipts,
 		Number:      block.NumberU64(),
 		Hash:        block.Hash(),
-		Error:       err,
+		Error:       err.Error(),
 	}
 
 	badBlockCounter.Inc(1)
