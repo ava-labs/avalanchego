402db2fd9ce4935dfb53867233c586877c5d17bb
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-03-02 09:08:34 +0100

    wip:bumped coreth version for dyn sync state readiness fix

--- node/node.go
+++ node/node.go
@@ -21,7 +21,7 @@ import (
 
 	"go.uber.org/zap"
 
-	// coreth "github.com/ava-labs/coreth/plugin/evm"
+	coreth "github.com/ava-labs/coreth/plugin/evm"
 
 	"github.com/ava-labs/avalanchego/api/admin"
 	"github.com/ava-labs/avalanchego/api/auth"
@@ -800,7 +800,7 @@ func (n *Node) initVMs() error {
 				CreateAssetTxFee: n.Config.CreateAssetTxFee,
 			},
 		}),
-		// vmRegisterer.Register(context.TODO(), constants.EVMID, &coreth.Factory{}),
+		vmRegisterer.Register(context.TODO(), constants.EVMID, &coreth.Factory{}),
 		n.Config.VMManager.RegisterFactory(context.TODO(), secp256k1fx.ID, &secp256k1fx.Factory{}),
 		n.Config.VMManager.RegisterFactory(context.TODO(), nftfx.ID, &nftfx.Factory{}),
 		n.Config.VMManager.RegisterFactory(context.TODO(), propertyfx.ID, &propertyfx.Factory{}),
