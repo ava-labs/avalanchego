4c4bf62c040cc1d175c2346019667768eec864ac
Stephen Buttolph <stephen@avalabs.org>
2023-03-03 19:04:59 -0500

    Drop writes to `NoLog` (#2698)

--- utils/logging/test_log.go
+++ utils/logging/test_log.go
@@ -4,7 +4,6 @@
 package logging
 
 import (
-	"errors"
 	"io"
 
 	"go.uber.org/zap"
@@ -14,15 +13,13 @@ var (
 	// Discard is a mock WriterCloser that drops all writes and close requests
 	Discard io.WriteCloser = discard{}
 
-	errNoLoggerWrite = errors.New("NoLogger can't write")
-
 	_ Logger = NoLog{}
 )
 
 type NoLog struct{}
 
-func (NoLog) Write([]byte) (int, error) {
-	return 0, errNoLoggerWrite
+func (NoLog) Write(b []byte) (int, error) {
+	return len(b), nil
 }
 
 func (NoLog) Fatal(string, ...zap.Field) {}
