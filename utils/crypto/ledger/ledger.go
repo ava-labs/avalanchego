c1dfcbcfb220c88c3987b49b48ff17c454b4b8f3
felipemadero <felipe.madero@avalabs.org>
2023-03-03 22:23:55 -0300

    Add ledger tx parsing support (#2695)

--- utils/crypto/ledger/ledger.go
+++ utils/crypto/ledger/ledger.go
@@ -10,10 +10,15 @@ import (
 
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/crypto/keychain"
+	"github.com/ava-labs/avalanchego/utils/hashing"
 	"github.com/ava-labs/avalanchego/version"
 )
 
-const rootPath = "m/44'/9000'/0'"
+const (
+	rootPath          = "m/44'/9000'/0'"
+	ledgerBufferLimit = 8192
+	ledgerPathSize    = 9
+)
 
 var _ keychain.Ledger = (*Ledger)(nil)
 
@@ -69,6 +74,35 @@ func (l *Ledger) SignHash(hash []byte, addressIndices []uint32) ([][]byte, error
 		return nil, fmt.Errorf("%w: unable to sign hash", err)
 	}
 	responses := make([][]byte, len(addressIndices))
+	for i, index := range strIndices {
+		sig, ok := response.Signature[index]
+		if !ok {
+			return nil, fmt.Errorf("missing signature %s", index)
+		}
+		responses[i] = sig
+	}
+	return responses, nil
+}
+
+func (l *Ledger) Sign(txBytes []byte, addressIndices []uint32) ([][]byte, error) {
+	// will pass to the ledger addressIndices both as signing paths and change paths
+	numSigningPaths := len(addressIndices)
+	numChangePaths := len(addressIndices)
+	if len(txBytes)+(numSigningPaths+numChangePaths)*ledgerPathSize > ledgerBufferLimit {
+		// There is a limit on the tx length that can be parsed by the ledger
+		// app. When the tx that is being signed is too large, we sign with hash
+		// instead.
+		//
+		// Ref: https://github.com/ava-labs/avalanche-wallet-sdk/blob/9a71f05e424e06b94eaccf21fd32d7983ed1b040/src/Wallet/Ledger/provider/ZondaxProvider.ts#L68
+		unsignedHash := hashing.ComputeHash256(txBytes)
+		return l.SignHash(unsignedHash, addressIndices)
+	}
+	strIndices := convertToSigningPaths(addressIndices)
+	response, err := l.device.Sign(rootPath, strIndices, txBytes, strIndices)
+	if err != nil {
+		return nil, fmt.Errorf("%w: unable to sign transaction", err)
+	}
+	responses := make([][]byte, len(strIndices))
 	for i, index := range strIndices {
 		sig, ok := response.Signature[index]
 		if !ok {
