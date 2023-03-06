f6fefd3d1f7a47b7163fc0e418a35eafb6d54534
dboehm-avalabs <david.boehm@avalabs.org>
2023-03-06 10:30:21 -0500

    Update db.go

--- x/merkledb/db.go
+++ x/merkledb/db.go
@@ -824,7 +824,7 @@ func (db *Database) commitChanges(ctx context.Context, trieToCommit *trieView) e
 
 // CommitToDB is a No Op for db since it is already in sync with itself
 // here to satify TrieView interface
-func (db *Database) CommitToDB() error {
+func (db *Database) CommitToDB(_ context.Context) error {
 	return nil
 }
 
