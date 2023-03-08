562dac9b8077c39b689de048b92db2b0e16b63b1
aaronbuchwald <aaron.buchwald56@gmail.com>
2023-03-08 09:58:21 -0500

    Fix goimports changes from complying with linting rules (#1214)

--- graft/coreth/accounts/abi/reflect.go
+++ graft/coreth/accounts/abi/reflect.go
@@ -183,13 +183,10 @@ func setStruct(dst, src reflect.Value) error {
 
 // mapArgNamesToStructFields maps a slice of argument names to struct fields.
 // first round: for each Exportable field that contains a `abi:""` tag
-//
-//	and this field name exists in the given argument name list, pair them together.
-//
+// and this field name exists in the given argument name list, pair them together.
 // second round: for each argument name that has not been already linked,
-//
-//	find what variable is expected to be mapped into, if it exists and has not been
-//	used, pair them.
+// find what variable is expected to be mapped into, if it exists and has not been
+// used, pair them.
 //
 // Note this function assumes the given value is a struct value.
 func mapArgNamesToStructFields(argNames []string, value reflect.Value) (map[string]string, error) {
