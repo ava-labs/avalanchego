d0a55ba57a5d8ebaa0717a812994f4a3df49c9d8
Dan Laine <daniel.laine@avalabs.org>
2023-03-23 19:07:27 -0400

    Update year in license header (#2742)
    Co-authored-by: Stephen <stephen@avalabs.org>

--- tests/e2e/describe.go
+++ tests/e2e/describe.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2022, Ava Labs, Inc. All rights reserved.
+// Copyright (C) 2019-2023, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
 package e2e
