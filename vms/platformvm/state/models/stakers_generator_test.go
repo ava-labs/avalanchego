43cf22a7e834c46c97a8a5f812119d08fae101d8
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-03-29 14:22:54 +0200

    stakers store model property tests

--- vms/platformvm/state/models/stakers_generator_test.go
+++ vms/platformvm/state/models/stakers_generator_test.go
@@ -20,25 +20,36 @@ import (
 	"github.com/leanovate/gopter/prop"
 )
 
-var StakerGenerator = genStakerTimeData.FlatMap(
-	func(v interface{}) gopter.Gen {
-		macro := v.(stakerTimeData)
+func stakerGenerator(prio *priorityType, subnet *ids.ID, nodeID *ids.NodeID) gopter.Gen {
+	return genStakerTimeData(prio).FlatMap(
+		func(v interface{}) gopter.Gen {
+			macro := v.(stakerTimeData)
 
-		return gen.Struct(reflect.TypeOf(state.Staker{}), map[string]gopter.Gen{
-			"TxID":            genID,
-			"NodeID":          genNodeID,
-			"PublicKey":       genBlsKey,
-			"SubnetID":        genID,
-			"Weight":          gen.UInt64(),
-			"StartTime":       gen.Const(macro.StartTime),
-			"EndTime":         gen.Const(macro.EndTime),
-			"PotentialReward": gen.UInt64(),
-			"NextTime":        gen.Const(macro.NextTime),
-			"Priority":        gen.Const(macro.Priority),
-		})
-	},
-	reflect.TypeOf(stakerTimeData{}),
-)
+			genStakerSubnetID := genID
+			genStakerNodeID := genNodeID
+			if subnet != nil {
+				genStakerSubnetID = gen.Const(*subnet)
+			}
+			if nodeID != nil {
+				genStakerNodeID = gen.Const(*nodeID)
+			}
+
+			return gen.Struct(reflect.TypeOf(state.Staker{}), map[string]gopter.Gen{
+				"TxID":            genID,
+				"NodeID":          genStakerNodeID,
+				"PublicKey":       genBlsKey,
+				"SubnetID":        genStakerSubnetID,
+				"Weight":          gen.UInt64(),
+				"StartTime":       gen.Const(macro.StartTime),
+				"EndTime":         gen.Const(macro.EndTime),
+				"PotentialReward": gen.UInt64(),
+				"NextTime":        gen.Const(macro.NextTime),
+				"Priority":        gen.Const(macro.Priority),
+			})
+		},
+		reflect.TypeOf(stakerTimeData{}),
+	)
+}
 
 func TestGeneratedStakersValidity(t *testing.T) {
 	properties := gopter.NewProperties(nil)
@@ -51,7 +62,7 @@ func TestGeneratedStakersValidity(t *testing.T) {
 			}
 			return ""
 		},
-		StakerGenerator,
+		stakerGenerator(nil, nil, nil),
 	))
 
 	properties.Property("NextTime coherent with priority", prop.ForAll(
@@ -84,7 +95,24 @@ func TestGeneratedStakersValidity(t *testing.T) {
 				return fmt.Sprintf("priority %v unhandled in test", p)
 			}
 		},
-		StakerGenerator,
+		stakerGenerator(nil, nil, nil),
+	))
+
+	subnetID := ids.GenerateTestID()
+	nodeID := ids.GenerateTestNodeID()
+	properties.Property("EndTime never before StartTime", prop.ForAll(
+		func(s state.Staker) string {
+			if s.SubnetID != subnetID {
+				return fmt.Sprintf("unexpected subnetID, expected %v, got %v",
+					subnetID, s.SubnetID)
+			}
+			if s.NodeID != nodeID {
+				return fmt.Sprintf("unexpected nodeID, expected %v, got %v",
+					nodeID, s.NodeID)
+			}
+			return ""
+		},
+		stakerGenerator(nil, &subnetID, &nodeID),
 	))
 
 	properties.TestingRun(t)
@@ -102,39 +130,41 @@ type stakerTimeData struct {
 	NextTime  time.Time
 }
 
-var genStakerTimeData = genStakerMicroData.FlatMap(
-	func(v interface{}) gopter.Gen {
-		micro := v.(stakerMicroData)
+func genStakerTimeData(prio *priorityType) gopter.Gen {
+	return genStakerMicroData(prio).FlatMap(
+		func(v interface{}) gopter.Gen {
+			micro := v.(stakerMicroData)
 
-		var (
-			startTime = micro.StartTime
-			endTime   = micro.StartTime.Add(time.Duration(micro.Duration * int64(time.Hour)))
-			priority  = micro.Priority
-		)
+			var (
+				startTime = micro.StartTime
+				endTime   = micro.StartTime.Add(time.Duration(micro.Duration * int64(time.Hour)))
+				priority  = micro.Priority
+			)
 
-		startTimeGen := gen.Const(startTime)
-		endTimeGen := gen.Const(endTime)
-		priorityGen := gen.Const(priority)
-		var nextTimeGen gopter.Gen
-		if priority == txs.SubnetPermissionedValidatorCurrentPriority ||
-			priority == txs.SubnetPermissionlessDelegatorCurrentPriority ||
-			priority == txs.SubnetPermissionlessValidatorCurrentPriority ||
-			priority == txs.PrimaryNetworkDelegatorCurrentPriority ||
-			priority == txs.PrimaryNetworkValidatorCurrentPriority {
-			nextTimeGen = gen.Const(endTime)
-		} else {
-			nextTimeGen = gen.Const(startTime)
-		}
+			startTimeGen := gen.Const(startTime)
+			endTimeGen := gen.Const(endTime)
+			priorityGen := gen.Const(priority)
+			var nextTimeGen gopter.Gen
+			if priority == txs.SubnetPermissionedValidatorCurrentPriority ||
+				priority == txs.SubnetPermissionlessDelegatorCurrentPriority ||
+				priority == txs.SubnetPermissionlessValidatorCurrentPriority ||
+				priority == txs.PrimaryNetworkDelegatorCurrentPriority ||
+				priority == txs.PrimaryNetworkValidatorCurrentPriority {
+				nextTimeGen = gen.Const(endTime)
+			} else {
+				nextTimeGen = gen.Const(startTime)
+			}
 
-		return gen.Struct(reflect.TypeOf(stakerTimeData{}), map[string]gopter.Gen{
-			"StartTime": startTimeGen,
-			"EndTime":   endTimeGen,
-			"Priority":  priorityGen,
-			"NextTime":  nextTimeGen,
-		})
-	},
-	reflect.TypeOf(stakerMicroData{}),
-)
+			return gen.Struct(reflect.TypeOf(stakerTimeData{}), map[string]gopter.Gen{
+				"StartTime": startTimeGen,
+				"EndTime":   endTimeGen,
+				"Priority":  priorityGen,
+				"NextTime":  nextTimeGen,
+			})
+		},
+		reflect.TypeOf(stakerMicroData{}),
+	)
+}
 
 // stakerMicroData holds seed attributes to generate stakerMacroData
 type stakerMicroData struct {
@@ -144,23 +174,68 @@ type stakerMicroData struct {
 }
 
 // genStakerMicroData is the helper to generate stakerMicroData
-var genStakerMicroData = gen.Struct(reflect.TypeOf(&stakerMicroData{}), map[string]gopter.Gen{
-	"StartTime": gen.Time(),
-	"Duration":  gen.Int64Range(1, 365*24),
-	"Priority": gen.OneConstOf(
-		txs.PrimaryNetworkDelegatorApricotPendingPriority,
-		txs.PrimaryNetworkValidatorPendingPriority,
-		txs.PrimaryNetworkDelegatorBanffPendingPriority,
-		txs.SubnetPermissionlessValidatorPendingPriority,
-		txs.SubnetPermissionlessDelegatorPendingPriority,
-		txs.SubnetPermissionedValidatorPendingPriority,
-		txs.SubnetPermissionedValidatorCurrentPriority,
-		txs.SubnetPermissionlessDelegatorCurrentPriority,
-		txs.SubnetPermissionlessValidatorCurrentPriority,
-		txs.PrimaryNetworkDelegatorCurrentPriority,
-		txs.PrimaryNetworkValidatorCurrentPriority,
-	),
-})
+func genStakerMicroData(prio *priorityType) gopter.Gen {
+	return gen.Struct(reflect.TypeOf(&stakerMicroData{}), map[string]gopter.Gen{
+		"StartTime": gen.Time(),
+		"Duration":  gen.Int64Range(1, 365*24),
+		"Priority":  genPriority(prio),
+	})
+}
+
+type priorityType uint8
+
+const (
+	currentValidator priorityType = iota + 1
+	currentDelegator
+	pendingValidator
+	pendingDelegator
+)
+
+func genPriority(p *priorityType) gopter.Gen {
+	if p == nil {
+		return gen.OneConstOf(
+			txs.PrimaryNetworkDelegatorApricotPendingPriority,
+			txs.PrimaryNetworkValidatorPendingPriority,
+			txs.PrimaryNetworkDelegatorBanffPendingPriority,
+			txs.SubnetPermissionlessValidatorPendingPriority,
+			txs.SubnetPermissionlessDelegatorPendingPriority,
+			txs.SubnetPermissionedValidatorPendingPriority,
+			txs.SubnetPermissionedValidatorCurrentPriority,
+			txs.SubnetPermissionlessDelegatorCurrentPriority,
+			txs.SubnetPermissionlessValidatorCurrentPriority,
+			txs.PrimaryNetworkDelegatorCurrentPriority,
+			txs.PrimaryNetworkValidatorCurrentPriority,
+		)
+	}
+
+	switch *p {
+	case currentValidator:
+		return gen.OneConstOf(
+			txs.SubnetPermissionedValidatorCurrentPriority,
+			txs.SubnetPermissionlessValidatorCurrentPriority,
+			txs.PrimaryNetworkValidatorCurrentPriority,
+		)
+	case currentDelegator:
+		return gen.OneConstOf(
+			txs.SubnetPermissionlessDelegatorCurrentPriority,
+			txs.PrimaryNetworkDelegatorCurrentPriority,
+		)
+	case pendingValidator:
+		return gen.OneConstOf(
+			txs.PrimaryNetworkValidatorPendingPriority,
+			txs.SubnetPermissionlessValidatorPendingPriority,
+			txs.SubnetPermissionedValidatorPendingPriority,
+		)
+	case pendingDelegator:
+		return gen.OneConstOf(
+			txs.PrimaryNetworkDelegatorApricotPendingPriority,
+			txs.PrimaryNetworkDelegatorBanffPendingPriority,
+			txs.SubnetPermissionlessDelegatorPendingPriority,
+		)
+	default:
+		panic("unhandled priority type")
+	}
+}
 
 var genBlsKey = gen.SliceOfN(lengthID, gen.UInt8()).FlatMap(
 	func(v interface{}) gopter.Gen {
