d0ea2a04fac17c565e70b31a231d0d716e243eee
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-03-30 09:37:06 +0200

    chain state property tests

---
+++ vms/platformvm/state/models/helpers_test.go
@@ -0,0 +1,131 @@
+// Copyright (C) 2019-2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package models
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ava-labs/avalanchego/chains"
+	"github.com/ava-labs/avalanchego/database/manager"
+	"github.com/ava-labs/avalanchego/database/versiondb"
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/snow"
+	"github.com/ava-labs/avalanchego/snow/uptime"
+	"github.com/ava-labs/avalanchego/snow/validators"
+	"github.com/ava-labs/avalanchego/utils"
+	"github.com/ava-labs/avalanchego/utils/constants"
+	"github.com/ava-labs/avalanchego/utils/formatting"
+	"github.com/ava-labs/avalanchego/utils/json"
+	"github.com/ava-labs/avalanchego/utils/units"
+	"github.com/ava-labs/avalanchego/version"
+	"github.com/ava-labs/avalanchego/vms/platformvm/api"
+	"github.com/ava-labs/avalanchego/vms/platformvm/config"
+	"github.com/ava-labs/avalanchego/vms/platformvm/metrics"
+	"github.com/ava-labs/avalanchego/vms/platformvm/reward"
+	"github.com/ava-labs/avalanchego/vms/platformvm/state"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	xChainID    = ids.Empty.Prefix(0)
+	cChainID    = ids.Empty.Prefix(1)
+	avaxAssetID = ids.ID{'y', 'e', 'e', 't'}
+
+	defaultMinStakingDuration = 24 * time.Hour
+	defaultMaxStakingDuration = 365 * 24 * time.Hour
+	defaultGenesisTime        = time.Date(1997, 1, 1, 0, 0, 0, 0, time.UTC)
+	defaultValidateStartTime  = defaultGenesisTime
+	defaultValidateEndTime    = defaultValidateStartTime.Add(10 * defaultMinStakingDuration)
+	defaultTxFee              = uint64(100)
+
+	testNetworkID = 10 // To be used in tests
+)
+
+func buildChainState() (state.State, error) {
+	baseDBManager := manager.NewMemDB(version.Semantic1_0_0)
+	baseDB := versiondb.New(baseDBManager.Current().Database)
+
+	cfg := defaultConfig()
+
+	ctx := snow.DefaultContextTest()
+	ctx.NetworkID = 10
+	ctx.XChainID = xChainID
+	ctx.CChainID = cChainID
+	ctx.AVAXAssetID = avaxAssetID
+
+	genesisBytes, err := buildGenesisTest(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	rewardsCalc := reward.NewCalculator(cfg.RewardConfig)
+	return state.New(
+		baseDB,
+		genesisBytes,
+		prometheus.NewRegistry(),
+		cfg,
+		ctx,
+		metrics.Noop,
+		rewardsCalc,
+		&utils.Atomic[bool]{},
+	)
+}
+
+func defaultConfig() *config.Config {
+	vdrs := validators.NewManager()
+	primaryVdrs := validators.NewSet()
+	_ = vdrs.Add(constants.PrimaryNetworkID, primaryVdrs)
+	return &config.Config{
+		Chains:                 chains.TestManager,
+		UptimeLockedCalculator: uptime.NewLockedCalculator(),
+		Validators:             vdrs,
+		TxFee:                  defaultTxFee,
+		CreateSubnetTxFee:      100 * defaultTxFee,
+		CreateBlockchainTxFee:  100 * defaultTxFee,
+		MinValidatorStake:      5 * units.MilliAvax,
+		MaxValidatorStake:      500 * units.MilliAvax,
+		MinDelegatorStake:      1 * units.MilliAvax,
+		MinStakeDuration:       defaultMinStakingDuration,
+		MaxStakeDuration:       defaultMaxStakingDuration,
+		RewardConfig: reward.Config{
+			MaxConsumptionRate: .12 * reward.PercentDenominator,
+			MinConsumptionRate: .10 * reward.PercentDenominator,
+			MintingPeriod:      365 * 24 * time.Hour,
+			SupplyCap:          720 * units.MegaAvax,
+		},
+		ApricotPhase3Time: defaultValidateEndTime,
+		ApricotPhase5Time: defaultValidateEndTime,
+		BanffTime:         time.Time{}, // neglecting fork ordering this for package tests
+	}
+}
+
+func buildGenesisTest(ctx *snow.Context) ([]byte, error) {
+	// no UTXOs, not nor validators in this genesis
+	genesisUTXOs := make([]api.UTXO, 0)
+	genesisValidators := make([]api.PermissionlessValidator, 0)
+	buildGenesisArgs := api.BuildGenesisArgs{
+		NetworkID:     json.Uint32(testNetworkID),
+		AvaxAssetID:   ctx.AVAXAssetID,
+		UTXOs:         genesisUTXOs,
+		Validators:    genesisValidators,
+		Chains:        nil,
+		Time:          json.Uint64(defaultGenesisTime.Unix()),
+		InitialSupply: json.Uint64(360 * units.MegaAvax),
+		Encoding:      formatting.Hex,
+	}
+
+	buildGenesisResponse := api.BuildGenesisReply{}
+	platformvmSS := api.StaticService{}
+	if err := platformvmSS.BuildGenesis(nil, &buildGenesisArgs, &buildGenesisResponse); err != nil {
+		return nil, fmt.Errorf("problem while building platform chain's genesis state: %w", err)
+	}
+
+	genesisBytes, err := formatting.Decode(buildGenesisResponse.Encoding, buildGenesisResponse.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return genesisBytes, nil
+}
