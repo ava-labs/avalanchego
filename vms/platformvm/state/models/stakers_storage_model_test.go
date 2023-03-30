0e48267078e54d9986e1e5d2a0cd01ccb8a6d9ac
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-03-30 22:36:56 +0200

    wip: more stakers storage stateful testing

--- vms/platformvm/state/models/stakers_storage_model_test.go
+++ vms/platformvm/state/models/stakers_storage_model_test.go
@@ -6,6 +6,7 @@ package models
 import (
 	"fmt"
 	"reflect"
+	"sync/atomic"
 	"testing"
 
 	"github.com/ava-labs/avalanchego/ids"
@@ -15,6 +16,13 @@ import (
 	"github.com/leanovate/gopter/gen"
 )
 
+var (
+	_ state.Versions   = (*sysUnderTest)(nil)
+	_ commands.Command = (*putCurrentValidatorCommand)(nil)
+	_ commands.Command = (*deleteCurrentValidatorCommand)(nil)
+	_ commands.Command = (*addTopDiffCommand)(nil)
+)
+
 func TestStateAndDiffComparisonToStorageModel(t *testing.T) {
 	properties := gopter.NewProperties(nil)
 	properties.Property("state comparison to storage model", commands.Prop(stakersCommands))
@@ -22,8 +30,38 @@ func TestStateAndDiffComparisonToStorageModel(t *testing.T) {
 }
 
 type sysUnderTest struct {
-	blkIDList         []ids.ID
+	baseState         state.State
+	blkIDsByHeight    []ids.ID
 	blkIDToChainState map[ids.ID]state.Chain
+}
+
+func newSysUnderTest(baseState state.State) *sysUnderTest {
+	baseBlkID := baseState.GetLastAccepted()
+	sys := &sysUnderTest{
+		baseState:         baseState,
+		blkIDToChainState: map[ids.ID]state.Chain{},
+		blkIDsByHeight:    []ids.ID{baseBlkID},
+	}
+	return sys
+}
+
+func (s *sysUnderTest) GetState(blkID ids.ID) (state.Chain, bool) {
+	if state, found := s.blkIDToChainState[blkID]; found {
+		return state, found
+	}
+	return s.baseState, blkID == s.baseState.GetLastAccepted()
+}
+
+func (s *sysUnderTest) addDiffOnTop() {
+	seed := uint64(len(s.blkIDsByHeight))
+	newTopBlkID := ids.Empty.Prefix(atomic.AddUint64(&seed, 1))
+	topBlkID := s.blkIDsByHeight[len(s.blkIDsByHeight)-1]
+	newTopDiff, err := state.NewDiff(topBlkID, s)
+	if err != nil {
+		panic(err)
+	}
+	s.blkIDsByHeight = append(s.blkIDsByHeight, newTopBlkID)
+	s.blkIDToChainState[newTopBlkID] = newTopDiff
 }
 
 // stakersCommands creates/destroy the system under test and generates
@@ -54,25 +92,22 @@ var stakersCommands = &commands.ProtoCommands{
 			}
 		}
 
-		baseBlkID := baseState.GetLastAccepted()
-		sys := &sysUnderTest{
-			blkIDList: []ids.ID{baseBlkID},
-			blkIDToChainState: map[ids.ID]state.Chain{
-				baseBlkID: baseState,
-			},
-		}
-		return sys
+		return newSysUnderTest(baseState)
 	},
 	DestroySystemUnderTestFunc: func(sut commands.SystemUnderTest) {
 		// retrieve base state and close it
 		sys := sut.(*sysUnderTest)
-		baseState := sys.blkIDToChainState[sys.blkIDList[0]]
-		err := baseState.(state.State).Close()
+		err := sys.baseState.Close()
 		if err != nil {
 			panic(err)
 		}
 	},
-	InitialStateGen: gen.Const(newStakersStorageModel()), // TODO ABENEGIA: consider adding initial state
+	// TODO ABENEGIA: using gen.Const(newStakersStorageModel()) would not recreated model
+	// among calls. Hence just use a dummy generated with sole purpose of recreating model
+	InitialStateGen: gen.IntRange(1, 2).Map(func(int) *stakersStorageModel {
+		return newStakersStorageModel()
+	}),
+
 	InitialPreConditionFunc: func(state commands.State) bool {
 		return true // nothing to do for now
 	},
@@ -84,19 +119,20 @@ var stakersCommands = &commands.ProtoCommands{
 			// genDeleteCurrentDelegatorCommand,
 
 			// genApplyBottomDiffCommand,
-			// genAddTopDiffCommand,
+			genAddTopDiffCommand,
 			// genCommitBottomStateCommand,
 		)
 	},
 }
 
+// PutCurrentValidator section
 type putCurrentValidatorCommand state.Staker
 
 func (v *putCurrentValidatorCommand) Run(sut commands.SystemUnderTest) commands.Result {
 	staker := (*state.Staker)(v)
 	sys := sut.(*sysUnderTest)
-	topDiffID := sys.blkIDList[len(sys.blkIDList)-1]
-	topDiff := sys.blkIDToChainState[topDiffID]
+	topBlkID := sys.blkIDsByHeight[len(sys.blkIDsByHeight)-1]
+	topDiff, _ := sys.GetState(topBlkID)
 	topDiff.PutCurrentValidator(staker)
 	return sys
 }
@@ -116,7 +152,7 @@ func (*putCurrentValidatorCommand) PostCondition(cmdState commands.State, res co
 	model := cmdState.(*stakersStorageModel)
 	sys := res.(*sysUnderTest)
 
-	if checkSystemAndModelContent(model, *sys) {
+	if checkSystemAndModelContent(model, sys) {
 		return &gopter.PropResult{Status: gopter.PropTrue}
 	}
 
@@ -127,30 +163,21 @@ func (v *putCurrentValidatorCommand) String() string {
 	return fmt.Sprintf("PutCurrentValidator(subnetID: %s, nodeID: %s, txID: %s)", v.SubnetID, v.NodeID, v.TxID)
 }
 
-// We want to have a generator for put commands for arbitrary int values.
-// In this case the command is actually shrinkable, e.g. if the property fails
-// by putting a 1000, it might already fail for a 500 as well ...
 var genPutCurrentValidatorCommand = stakerGenerator(anyPriority, nil, nil).Map(
 	func(staker state.Staker) commands.Command {
 		cmd := (*putCurrentValidatorCommand)(&staker)
 		return cmd
 	},
-).WithShrinker(
-	func(v interface{}) gopter.Shrink {
-		return gen.IntShrinker(v.(putCurrentValidatorCommand)).Map(func(staker state.Staker) *putCurrentValidatorCommand {
-			cmd := (*putCurrentValidatorCommand)(&staker)
-			return cmd
-		})
-	},
 )
 
+// DeleteCurrentValidator section
 type deleteCurrentValidatorCommand state.Staker
 
 func (v *deleteCurrentValidatorCommand) Run(sut commands.SystemUnderTest) commands.Result {
 	staker := (*state.Staker)(v)
 	sys := sut.(*sysUnderTest)
-	topDiffID := sys.blkIDList[len(sys.blkIDList)-1]
-	topDiff := sys.blkIDToChainState[topDiffID]
+	topBlkID := sys.blkIDsByHeight[len(sys.blkIDsByHeight)-1]
+	topDiff, _ := sys.GetState(topBlkID)
 	topDiff.DeleteCurrentValidator(staker)
 	return sys // returns sys to allow comparison with state in PostCondition
 }
@@ -170,7 +197,7 @@ func (*deleteCurrentValidatorCommand) PostCondition(cmdState commands.State, res
 	model := cmdState.(*stakersStorageModel)
 	sys := res.(*sysUnderTest)
 
-	if checkSystemAndModelContent(model, *sys) {
+	if checkSystemAndModelContent(model, sys) {
 		return &gopter.PropResult{Status: gopter.PropTrue}
 	}
 
@@ -181,27 +208,55 @@ func (v *deleteCurrentValidatorCommand) String() string {
 	return fmt.Sprintf("DeleteCurrentValidator(subnetID: %s, nodeID: %s, txID: %s)", v.SubnetID, v.NodeID, v.TxID)
 }
 
-// We want to have a generator for put commands for arbitrary int values.
-// In this case the command is actually shrinkable, e.g. if the property fails
-// by putting a 1000, it might already fail for a 500 as well ...
 var genDeleteCurrentValidatorCommand = stakerGenerator(anyPriority, nil, nil).Map(
 	func(staker state.Staker) commands.Command {
 		cmd := (*deleteCurrentValidatorCommand)(&staker)
 		return cmd
 	},
-).WithShrinker(
-	func(v interface{}) gopter.Shrink {
-		return gen.IntShrinker(v.(deleteCurrentValidatorCommand)).Map(func(staker state.Staker) *deleteCurrentValidatorCommand {
-			cmd := (*deleteCurrentValidatorCommand)(&staker)
-			return cmd
-		})
+)
+
+// addTopDiffCommand section
+type addTopDiffCommand struct{}
+
+func (*addTopDiffCommand) Run(sut commands.SystemUnderTest) commands.Result {
+	sys := sut.(*sysUnderTest)
+	sys.addDiffOnTop()
+	return sys
+}
+
+func (*addTopDiffCommand) NextState(cmdState commands.State) commands.State {
+	return cmdState // model has no diffs
+}
+
+func (*addTopDiffCommand) PreCondition(commands.State) bool {
+	return true
+}
+
+func (*addTopDiffCommand) PostCondition(cmdState commands.State, res commands.Result) *gopter.PropResult {
+	model := cmdState.(*stakersStorageModel)
+	sys := res.(*sysUnderTest)
+
+	if checkSystemAndModelContent(model, sys) {
+		return &gopter.PropResult{Status: gopter.PropTrue}
+	}
+
+	return &gopter.PropResult{Status: gopter.PropFalse}
+}
+
+func (*addTopDiffCommand) String() string {
+	return "AddTopDiffCommand"
+}
+
+var genAddTopDiffCommand = stakerGenerator(anyPriority, nil, nil).Map(
+	func(state.Staker) commands.Command {
+		return &addTopDiffCommand{}
 	},
 )
 
-func checkSystemAndModelContent(model *stakersStorageModel, sys sysUnderTest) bool {
+func checkSystemAndModelContent(model *stakersStorageModel, sys *sysUnderTest) bool {
 	// top view content must always match model content
-	topDiffID := sys.blkIDList[len(sys.blkIDList)-1]
-	topDiff := sys.blkIDToChainState[topDiffID]
+	topBlkID := sys.blkIDsByHeight[len(sys.blkIDsByHeight)-1]
+	topDiff, _ := sys.GetState(topBlkID)
 
 	modelIt, err := model.GetCurrentStakerIterator()
 	if err != nil {
