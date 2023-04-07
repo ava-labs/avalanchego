4e2eab7f3d22cfb636d0f2748c0f46118c7e5ac9
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-04-07 18:13:46 +0300

    move runner and export it (#539)
    * move runner and export it
    
    * add version str

--- plugin/main.go
+++ plugin/main.go
@@ -4,31 +4,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"os"
 
-	"github.com/ava-labs/avalanchego/utils/logging"
-	"github.com/ava-labs/avalanchego/utils/ulimit"
 	"github.com/ava-labs/avalanchego/version"
-	"github.com/ava-labs/avalanchego/vms/rpcchainvm"
-
 	"github.com/ava-labs/subnet-evm/plugin/evm"
+	"github.com/ava-labs/subnet-evm/plugin/runner"
 )
 
 func main() {
-	printVersion, err := PrintVersion()
-	if err != nil {
-		fmt.Printf("couldn't get config: %s", err)
-		os.Exit(1)
-	}
-	if printVersion {
-		fmt.Printf("Subnet-EVM/%s [AvalancheGo=%s, rpcchainvm=%d]\n", evm.Version, version.Current, version.RPCChainVMProtocol)
-		os.Exit(0)
-	}
-	if err := ulimit.Set(ulimit.DefaultFDLimit, logging.NoLog{}); err != nil {
-		fmt.Printf("failed to set fd limit correctly due to: %s", err)
-		os.Exit(1)
-	}
-	rpcchainvm.Serve(context.Background(), &evm.VM{})
+	versionString := fmt.Sprintf("Subnet-EVM/%s [AvalancheGo=%s, rpcchainvm=%d]", evm.Version, version.Current, version.RPCChainVMProtocol)
+	runner.Run(versionString)
 }
---
+++ plugin/runner/runner.go
@@ -0,0 +1,33 @@
+// (c) 2019-2020, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package runner
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/ava-labs/avalanchego/utils/ulimit"
+	"github.com/ava-labs/avalanchego/vms/rpcchainvm"
+
+	"github.com/ava-labs/subnet-evm/plugin/evm"
+)
+
+func Run(versionStr string) {
+	printVersion, err := PrintVersion()
+	if err != nil {
+		fmt.Printf("couldn't get config: %s", err)
+		os.Exit(1)
+	}
+	if printVersion && versionStr != "" {
+		fmt.Printf(versionStr)
+		os.Exit(0)
+	}
+	if err := ulimit.Set(ulimit.DefaultFDLimit, logging.NoLog{}); err != nil {
+		fmt.Printf("failed to set fd limit correctly due to: %s", err)
+		os.Exit(1)
+	}
+	rpcchainvm.Serve(context.Background(), &evm.VM{})
+}
