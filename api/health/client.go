5fbcac007fd8033de47deb014d1af1f5302a43b3
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-04-10 21:21:32 +0300

    Add Health Check Tags (#1304)

--- api/health/client.go
+++ api/health/client.go
@@ -17,11 +17,11 @@ var _ Client = (*client)(nil)
 // AwaitHealthy, and AwaitAlive.
 type Client interface {
 	// Readiness returns if the node has finished initialization
-	Readiness(context.Context, ...rpc.Option) (*APIReply, error)
+	Readiness(ctx context.Context, tags []string, options ...rpc.Option) (*APIReply, error)
 	// Health returns a summation of the health of the node
-	Health(context.Context, ...rpc.Option) (*APIReply, error)
+	Health(ctx context.Context, tags []string, options ...rpc.Option) (*APIReply, error)
 	// Liveness returns if the node is in need of a restart
-	Liveness(context.Context, ...rpc.Option) (*APIReply, error)
+	Liveness(ctx context.Context, tags []string, options ...rpc.Option) (*APIReply, error)
 }
 
 // Client implementation for Avalanche Health API Endpoint
@@ -36,53 +36,54 @@ func NewClient(uri string) Client {
 	)}
 }
 
-func (c *client) Readiness(ctx context.Context, options ...rpc.Option) (*APIReply, error) {
+func (c *client) Readiness(ctx context.Context, tags []string, options ...rpc.Option) (*APIReply, error) {
 	res := &APIReply{}
-	err := c.requester.SendRequest(ctx, "health.readiness", struct{}{}, res, options...)
+	err := c.requester.SendRequest(ctx, "health.readiness", &APIArgs{Tags: tags}, res, options...)
 	return res, err
 }
 
-func (c *client) Health(ctx context.Context, options ...rpc.Option) (*APIReply, error) {
+func (c *client) Health(ctx context.Context, tags []string, options ...rpc.Option) (*APIReply, error) {
 	res := &APIReply{}
-	err := c.requester.SendRequest(ctx, "health.health", struct{}{}, res, options...)
+	err := c.requester.SendRequest(ctx, "health.health", &APIArgs{Tags: tags}, res, options...)
 	return res, err
 }
 
-func (c *client) Liveness(ctx context.Context, options ...rpc.Option) (*APIReply, error) {
+func (c *client) Liveness(ctx context.Context, tags []string, options ...rpc.Option) (*APIReply, error) {
 	res := &APIReply{}
-	err := c.requester.SendRequest(ctx, "health.liveness", struct{}{}, res, options...)
+	err := c.requester.SendRequest(ctx, "health.liveness", &APIArgs{Tags: tags}, res, options...)
 	return res, err
 }
 
 // AwaitReady polls the node every [freq] until the node reports ready.
 // Only returns an error if [ctx] returns an error.
-func AwaitReady(ctx context.Context, c Client, freq time.Duration, options ...rpc.Option) (bool, error) {
-	return await(ctx, freq, c.Readiness, options...)
+func AwaitReady(ctx context.Context, c Client, freq time.Duration, tags []string, options ...rpc.Option) (bool, error) {
+	return await(ctx, freq, c.Readiness, tags, options...)
 }
 
 // AwaitHealthy polls the node every [freq] until the node reports healthy.
 // Only returns an error if [ctx] returns an error.
-func AwaitHealthy(ctx context.Context, c Client, freq time.Duration, options ...rpc.Option) (bool, error) {
-	return await(ctx, freq, c.Health, options...)
+func AwaitHealthy(ctx context.Context, c Client, freq time.Duration, tags []string, options ...rpc.Option) (bool, error) {
+	return await(ctx, freq, c.Health, tags, options...)
 }
 
 // AwaitAlive polls the node every [freq] until the node reports liveness.
 // Only returns an error if [ctx] returns an error.
-func AwaitAlive(ctx context.Context, c Client, freq time.Duration, options ...rpc.Option) (bool, error) {
-	return await(ctx, freq, c.Liveness, options...)
+func AwaitAlive(ctx context.Context, c Client, freq time.Duration, tags []string, options ...rpc.Option) (bool, error) {
+	return await(ctx, freq, c.Liveness, tags, options...)
 }
 
 func await(
 	ctx context.Context,
 	freq time.Duration,
-	check func(context.Context, ...rpc.Option) (*APIReply, error),
+	check func(ctx context.Context, tags []string, options ...rpc.Option) (*APIReply, error),
+	tags []string,
 	options ...rpc.Option,
 ) (bool, error) {
 	ticker := time.NewTicker(freq)
 	defer ticker.Stop()
 
 	for {
-		res, err := check(ctx, options...)
+		res, err := check(ctx, tags, options...)
 		if err == nil && res.Healthy {
 			return true, nil
 		}
