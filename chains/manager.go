4ad7ab49e56a73029caf0eb176bf5647559b5d10
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-04-10 11:20:11 +0300

    Update chains/manager.go
    Co-authored-by: Dan Laine <daniel.laine@avalabs.org>

--- chains/manager.go
+++ chains/manager.go
@@ -1324,7 +1324,7 @@ func (m *manager) registerBootstrappedHealthChecks() error {
 		if len(subnetIDs) != 0 {
 			return subnetIDs, errNotBootstrapped
 		}
-		return []ids.ID{}, nil
+		return nil, nil
 	})
 	if err := m.Health.RegisterReadinessCheck("bootstrapped", bootstrappedCheck); err != nil {
 		return fmt.Errorf("couldn't register bootstrapped readiness check: %w", err)
