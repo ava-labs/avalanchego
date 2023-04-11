e31d13d4407b46ed1ce7af6bbf63ce0ba3e23c10
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-04-10 21:41:36 -0400

    Add sign/verify bls benchmarks

--- utils/crypto/bls/bls_benchmark_test.go
+++ utils/crypto/bls/bls_benchmark_test.go
@@ -12,21 +12,57 @@ import (
 	"github.com/ava-labs/avalanchego/utils"
 )
 
+var sizes = []int{
+	2,
+	4,
+	8,
+	16,
+	32,
+	64,
+	128,
+	256,
+	512,
+	1024,
+	2048,
+	4096,
+}
+
+func BenchmarkSign(b *testing.B) {
+	require := require.New(b)
+
+	privateKey, err := NewSecretKey()
+	require.NoError(err)
+	for _, messageSize := range sizes {
+		b.Run(fmt.Sprintf("%d", messageSize), func(b *testing.B) {
+			message := utils.RandomBytes(messageSize)
+
+			b.ResetTimer()
+
+			for n := 0; n < b.N; n++ {
+				_ = Sign(privateKey, message)
+			}
+		})
+	}
+}
+
 func BenchmarkVerify(b *testing.B) {
 	require := require.New(b)
 
 	privateKey, err := NewSecretKey()
 	require.NoError(err)
-
-	message := utils.RandomBytes(512)
-
 	publicKey := PublicFromSecretKey(privateKey)
-	signature := Sign(privateKey, message)
 
-	b.ResetTimer()
+	for _, messageSize := range sizes {
+		b.Run(fmt.Sprintf("%d", messageSize), func(b *testing.B) {
+			message := utils.RandomBytes(messageSize)
+			signature := Sign(privateKey, message)
 
-	for n := 0; n < b.N; n++ {
-		require.True(Verify(publicKey, signature, message))
+			b.ResetTimer()
+
+			for n := 0; n < b.N; n++ {
+				require.True(Verify(publicKey, signature, message))
+			}
+		})
 	}
 }
 
@@ -39,20 +75,6 @@ func BenchmarkAggregatePublicKeys(b *testing.B) {
 		keys[i] = PublicFromSecretKey(privateKey)
 	}
 
-	sizes := []int{
-		2,
-		4,
-		8,
-		16,
-		32,
-		64,
-		128,
-		256,
-		512,
-		1024,
-		2048,
-		4096,
-	}
 	for _, size := range sizes {
 		b.Run(fmt.Sprintf("%d", size), func(b *testing.B) {
 			require := require.New(b)
