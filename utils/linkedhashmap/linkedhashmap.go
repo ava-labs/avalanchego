90cb8375bb69c8fb94602c84cf92b9e64e0634bf
dboehm-avalabs <david.boehm@avalabs.org>
2023-04-12 10:43:08 -0400

    Update linkedhashmap.go

--- utils/linkedhashmap/linkedhashmap.go
+++ utils/linkedhashmap/linkedhashmap.go
@@ -71,22 +71,22 @@ func (lh *linkedHashmap[K, V]) Delete(key K) {
 }
 
 func (lh *linkedHashmap[K, V]) Len() int {
-	lh.lock.Lock()
-	defer lh.lock.Unlock()
+	lh.lock.RLock()
+	defer lh.lock.RUnlock()
 
 	return lh.len()
 }
 
 func (lh *linkedHashmap[K, V]) Oldest() (K, V, bool) {
-	lh.lock.Lock()
-	defer lh.lock.Unlock()
+	lh.lock.RLock()
+	defer lh.lock.RUnlock()
 
 	return lh.oldest()
 }
 
 func (lh *linkedHashmap[K, V]) Newest() (K, V, bool) {
-	lh.lock.Lock()
-	defer lh.lock.Unlock()
+	lh.lock.RLock()
+	defer lh.lock.RUnlock()
 
 	return lh.newest()
 }
