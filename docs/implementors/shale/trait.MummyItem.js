c317e17bef03809dc8d9a2ea4de489e9ed6f42c1
Patrick O'Grady <prohb125@gmail.com>
2023-04-12 09:57:48 -0700

    Add website (#4)
    * add website files
    
    * cleanup website

---
+++ docs/implementors/shale/trait.MummyItem.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"firewood":[["impl <a class=\"trait\" href=\"shale/trait.MummyItem.html\" title=\"trait shale::MummyItem\">MummyItem</a> for <a class=\"struct\" href=\"firewood/merkle/struct.Hash.html\" title=\"struct firewood::merkle::Hash\">Hash</a>"],["impl <a class=\"trait\" href=\"shale/trait.MummyItem.html\" title=\"trait shale::MummyItem\">MummyItem</a> for <a class=\"struct\" href=\"firewood/merkle/struct.Node.html\" title=\"struct firewood::merkle::Node\">Node</a>"]],
+"shale":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
