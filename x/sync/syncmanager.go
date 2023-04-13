548ecb4f6d83716fd413daf3333cb05d68e2fc8f
dboehm-avalabs <david.boehm@avalabs.org>
2023-04-13 14:56:35 -0400

    Update syncmanager.go

--- x/sync/syncmanager.go
+++ x/sync/syncmanager.go
@@ -432,6 +432,8 @@ func (m *StateSyncManager) findNextKey(
 			localIndex--
 		}
 
+		// I believe the change to changeproofs when their largest key is a deleted keys fixes this,
+		// but leave this safeguard in place in case I am wrong.
 		// TODO: Figure out the scenario where this can happen and fix
 		if receivedKeyPath.NibbleLength <= branchNode.KeyPath.NibbleLength {
 			return receivedKeyPath.Value, nil
