a6cceb1db2e5738ef2906ed6a67615532f1cb447
dboehm-avalabs <david.boehm@avalabs.org>
2023-04-13 16:33:53 -0400

    Update network_server_test.go

--- x/sync/network_server_test.go
+++ x/sync/network_server_test.go
@@ -34,7 +34,7 @@ func Test_Server_GetRangeProof(t *testing.T) {
 		nodeID                   ids.NodeID
 		proofNil                 bool
 	}{
-		"proof too small": {
+		"proof too large": {
 			request: &RangeProofRequest{
 				Root:       smallTrieRoot,
 				KeyLimit:   defaultRequestKeyLimit,
