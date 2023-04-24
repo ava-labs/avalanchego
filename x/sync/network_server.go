ea3a1401c577163f29cc088566fc728d67db837d
dboehm-avalabs <david.boehm@avalabs.org>
2023-04-24 11:51:08 -0400

    Update network_server.go

--- x/sync/network_server.go
+++ x/sync/network_server.go
@@ -211,7 +211,7 @@ func (s *NetworkServer) HandleChangeProofRequest(
 			return s.appSender.SendAppResponse(ctx, nodeID, requestID, proofBytes)
 		}
 		// the proof size was too large, try to shrink it
-		keyLimit = uint16(len(changeProof.KeyChanges)) / 2
+		keyLimit = uint32(len(changeProof.KeyChanges)) / 2
 	}
 	return ErrMinProofSizeIsTooLarge
 }
