f554e0749f611d6007d99f0c1c974ac0f9f66cef
peak3d <pfau@peak3d.de>
2023-04-26 02:16:28 +0200

    [Version] Bump 0.4.9

--- version/constants.go
+++ version/constants.go
@@ -35,7 +35,7 @@ var (
 	Current = &Semantic{
 		Major: 0,
 		Minor: 4,
-		Patch: 8,
+		Patch: 9,
 	}
 	CurrentApp = &Application{
 		Major: Current.Major,
@@ -45,12 +45,12 @@ var (
 	MinimumCompatibleVersion = &Application{
 		Major: 0,
 		Minor: 4,
-		Patch: 8,
+		Patch: 9,
 	}
 	PrevMinimumCompatibleVersion = &Application{
 		Major: 0,
 		Minor: 4,
-		Patch: 8,
+		Patch: 9,
 	}
 
 	CurrentDatabase = DatabaseVersion1_4_5
