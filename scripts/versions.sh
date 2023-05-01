26818b3fcdd9831cf31f15c7e65faeecb78f3e70
aaronbuchwald <aaron.buchwald56@gmail.com>
2023-05-01 16:59:39 -0400

    Bump for v0.12.1 (#1261)
    * Remvoe RELEASES.md
    
    * Remove unnecessary version var and bump version to v0.12.1
    
    * Bump avalanchego dependency to v1.10.1
    
    * Revert "Remvoe RELEASES.md"
    
    This reverts commit 089b995557aaf018b0fdb1f086265df5c0b19db5.
    
    * Add release notes for v0.12.1

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-# Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.12.0'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.9.16'}
