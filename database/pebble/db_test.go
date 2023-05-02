7de76603fb381b94b5b579b493fa9ed6b2143191
Qian (Tony) Zhang <tonyqzhang@yahoo.com>
2023-05-01 23:07:21 -0700

    gofmt issue

--- database/pebble/db_test.go
+++ database/pebble/db_test.go
@@ -16,7 +16,7 @@ import (
 
 func TestInterface(t *testing.T) {
 	for idx, test := range database.Tests {
-		if idx == 9  || idx == 20 || idx == 21 || idx == 22 || idx ==23 {
+		if idx == 9 || idx == 20 || idx == 21 || idx == 22 || idx == 23 {
 			continue
 		}
 
