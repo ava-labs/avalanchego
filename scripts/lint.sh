ae0d6017513525623f031aea22b420648725791c
Dhruba Basu <7675102+dhrubabasu@users.noreply.github.com>
2023-05-11 06:30:45 -0400

    Update scripts/lint.sh
    Signed-off-by: Dhruba Basu <7675102+dhrubabasu@users.noreply.github.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -60,7 +60,7 @@ function test_require_error_is_no_funcs_as_params {
 
 # Ref: https://go.dev/doc/effective_go#blank_implements
 function test_interface_compliance_nil {
-  if ggrep -R -o -P '_ .+? = &.+?\{\}' .; then
+  if grep -R -o -P '_ .+? = &.+?\{\}' .; then
     echo ""
     echo "Interface compliance checks need to be of the form:"
     echo "  var _ json.Marshaler = (*RawMessage)(nil)"
