70a15f4f113909ac04d5a7f24f9cd9dd195032a8
dboehm-avalabs <david.boehm@avalabs.org>
2023-05-19 17:15:13 -0400

    Update trie_test.go

--- x/merkledb/trie_test.go
+++ x/merkledb/trie_test.go
@@ -36,7 +36,6 @@ func getNodeValue(t ReadOnlyTrie, key string) ([]byte, error) {
 
 		return closestNode.value.value, nil
 	}
-
 	if asDatabases, ok := t.(*Database); ok {
 		view, err := asDatabases.NewView()
 		if err != nil {
