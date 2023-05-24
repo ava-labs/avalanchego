416c96fd0429b40b3c8c643f2ea4001fcd9cf0f5
dboehm-avalabs <david.boehm@avalabs.org>
2023-05-24 13:16:01 -0400

    Update db.go

--- x/merkledb/db.go
+++ x/merkledb/db.go
@@ -742,14 +742,12 @@ func (db *merkleDB) NewIteratorWithStartAndPrefix(start, prefix []byte) database
 // As soon as [db.nodeCache] no longer has [node], [db.nodeDB] does.
 // Non-nil error is fatal -- causes [db] to close.
 func (db *merkleDB) onEviction(n *node) error {
-	if n.hasValue() {
-		return nil
-	}
-
 	batch := db.nodeDB.NewBatch()
 
-	if err := writeNodeToBatch(batch, n); err != nil {
-		return err
+	if n != nil && !n.hasValue() {
+		if err := writeNodeToBatch(batch, n); err != nil {
+			return err
+		}
 	}
 
 	// Evict the oldest [evictionBatchSize] nodes from the cache
@@ -759,12 +757,12 @@ func (db *merkleDB) onEviction(n *node) error {
 	var err error
 	for removedCount := 0; removedCount < evictionBatchSize; removedCount++ {
 		_, n, exists := db.nodeCache.removeOldest()
-		if n.hasValue() {
-			continue
-		}
 		if !exists {
 			// The cache is empty.
 			break
+		}
+		if n == nil || n.hasValue() {
+			continue
 		}
 		// Note this must be = not := since we check
 		// [err] outside the loop.
