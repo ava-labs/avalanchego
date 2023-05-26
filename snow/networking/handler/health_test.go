647a306a9c8eb88ac0319c64607852e61a98e943
Stephen Buttolph <stephen@avalabs.org>
2023-05-25 20:29:42 -0400

    nit

--- snow/networking/handler/health_test.go
+++ snow/networking/handler/health_test.go
@@ -79,6 +79,7 @@ func TestHealthCheckSubnet(t *testing.T) {
 				sb,
 				peerTracker,
 			)
+			require.NoError(err)
 
 			bootstrapper := &common.BootstrapperTest{
 				BootstrapableTest: common.BootstrapableTest{
@@ -118,34 +119,35 @@ func TestHealthCheckSubnet(t *testing.T) {
 			vdrIDs := set.NewSet[ids.NodeID](testVdrCount)
 			for i := 0; i < testVdrCount; i++ {
 				vdrID := ids.GenerateTestNodeID()
-				require.NoError(err)
+				vdrIDs.Add(vdrID)
+
 				err := vdrs.Add(vdrID, nil, ids.Empty, 100)
 				require.NoError(err)
-				vdrIDs.Add(vdrID)
 			}
 
 			for index, vdr := range vdrs.List() {
 				err := peerTracker.Connected(context.Background(), vdr.NodeID, nil)
 				require.NoError(err)
+
 				details, err := handlerIntf.HealthCheck(context.Background())
 				connectedPerc := float64(index+1) / float64(testVdrCount)
 				conf := sb.Config()
 				minStake := conf.ConsensusParameters.MinPercentConnectedStakeHealthy()
 				if connectedPerc >= minStake {
 					require.NoError(err)
-				} else {
-					detailsMap, ok := details.(map[string]interface{})
-					require.True(ok)
-					networkingMap, ok := detailsMap["networking"]
-					require.True(ok)
-					networkingDetails, ok := networkingMap.(map[string]float64)
-					require.True(ok)
-					percentConnected, ok := networkingDetails["percentConnected"]
-					require.True(ok)
-					require.Equal(connectedPerc, percentConnected)
-
-					require.ErrorIs(err, ErrNotConnectedEnoughStake)
+					continue
 				}
+				require.ErrorIs(err, ErrNotConnectedEnoughStake)
+
+				detailsMap, ok := details.(map[string]interface{})
+				require.True(ok)
+				networkingMap, ok := detailsMap["networking"]
+				require.True(ok)
+				networkingDetails, ok := networkingMap.(map[string]float64)
+				require.True(ok)
+				percentConnected, ok := networkingDetails["percentConnected"]
+				require.True(ok)
+				require.Equal(connectedPerc, percentConnected)
 			}
 		})
 	}
