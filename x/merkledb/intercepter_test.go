f14bb1a7d62677ac1aa568489cfa48fb955c0130
Stephen Buttolph <stephen@avalabs.org>
2023-06-22 16:21:07 -0400

    add more test

--- x/merkledb/intercepter_test.go
+++ x/merkledb/intercepter_test.go
@@ -51,6 +51,26 @@ func Test_Intercepter_non_empty_initial_db(t *testing.T) {
 	})
 }
 
+func Test_Intercepter_non_empty_initial_db_with_delete(t *testing.T) {
+	require := require.New(t)
+
+	db, err := getBasicDB()
+	require.NoError(err)
+
+	require.NoError(db.Put([]byte{0}, []byte{2}))
+
+	testIntercepter(require, db, []KeyChange{
+		{
+			Key:   []byte{0},
+			Value: Nothing[[]byte](),
+		},
+		{
+			Key:   []byte{1},
+			Value: Some([]byte{1, 2}),
+		},
+	})
+}
+
 type proofNode struct {
 	key  []byte
 	node []byte
