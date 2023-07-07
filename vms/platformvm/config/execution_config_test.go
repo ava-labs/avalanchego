02dd7ff38bbe7115797c6694050801554db3085e
najeal <haim.nathan@icloud.com>
2023-07-07 12:30:37 +0200

    vms/pmvm/config: add ChecksumsEnabled in tests + add test for all keys unmarshalled

--- vms/platformvm/config/execution_config_test.go
+++ vms/platformvm/config/execution_config_test.go
@@ -23,6 +23,7 @@ func TestExecutionConfigUnmarshal(t *testing.T) {
 			RewardUTXOsCacheSize:         rewardUTXOsCacheSize,
 			ChainCacheSize:               chainCacheSize,
 			ChainDBCacheSize:             chainDBCacheSize,
+			ChecksumsEnabled:             checksumsEnabled,
 		}
 		require.Equal(expected, *ec)
 	})
@@ -40,6 +41,7 @@ func TestExecutionConfigUnmarshal(t *testing.T) {
 			RewardUTXOsCacheSize:         rewardUTXOsCacheSize,
 			ChainCacheSize:               chainCacheSize,
 			ChainDBCacheSize:             chainDBCacheSize,
+			ChecksumsEnabled:             checksumsEnabled,
 		}
 		require.Equal(expected, *ec)
 	})
@@ -57,6 +59,34 @@ func TestExecutionConfigUnmarshal(t *testing.T) {
 			RewardUTXOsCacheSize:         rewardUTXOsCacheSize,
 			ChainCacheSize:               chainCacheSize,
 			ChainDBCacheSize:             chainDBCacheSize,
+			ChecksumsEnabled:             checksumsEnabled,
+		}
+		require.Equal(expected, *ec)
+	})
+
+	t.Run("all values extracted from json", func(t *testing.T) {
+		require := require.New(t)
+		b := []byte(`{
+			"blockCacheSize":1,
+			"txCacheSize":2,
+			"transformedSubnetTxCacheSize":3,
+			"validatorDiffsCacheSize": 4,
+			"rewardUTXOsCacheSize": 5,
+			"chainCacheSize": 6,
+			"chainDBCacheSize": 7,
+			"checksums-enabled": true
+		}`)
+		ec, err := GetExecutionConfig(b)
+		require.NoError(err)
+		expected := ExecutionConfig{
+			BlockCacheSize:               1,
+			TxCacheSize:                  2,
+			TransformedSubnetTxCacheSize: 3,
+			ValidatorDiffsCacheSize:      4,
+			RewardUTXOsCacheSize:         5,
+			ChainCacheSize:               6,
+			ChainDBCacheSize:             7,
+			ChecksumsEnabled:             true,
 		}
 		require.Equal(expected, *ec)
 	})
