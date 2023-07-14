de1107b9d32d73fdc414f1725fc7d33b7729f8eb
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-07-14 18:38:56 +0200

    fix defer use

--- vms/proposervm/vm_byzantine_test.go
+++ vms/proposervm/vm_byzantine_test.go
@@ -35,7 +35,9 @@ func TestInvalidByzantineProposerParent(t *testing.T) {
 
 	forkTime := time.Unix(0, 0) // enable ProBlks
 	coreVM, _, proVM, gBlock, _ := initTestProposerVM(t, forkTime, 0)
-	defer require.NoError(proVM.Shutdown(context.Background()))
+	defer func() {
+		require.NoError(proVM.Shutdown(context.Background()))
+	}()
 
 	xBlock := &snowman.TestBlock{
 		TestDecidable: choices.TestDecidable{
@@ -103,7 +105,9 @@ func TestInvalidByzantineProposerOracleParent(t *testing.T) {
 
 	coreVM, _, proVM, coreGenBlk, _ := initTestProposerVM(t, time.Time{}, 0)
 	proVM.Set(coreGenBlk.Timestamp())
-	defer require.NoError(proVM.Shutdown(context.Background()))
+	defer func() {
+		require.NoError(proVM.Shutdown(context.Background()))
+	}()
 
 	xBlockID := ids.GenerateTestID()
 	xBlock := &TestOptionsBlock{
@@ -209,7 +213,9 @@ func TestInvalidByzantineProposerPreForkParent(t *testing.T) {
 
 	forkTime := time.Unix(0, 0) // enable ProBlks
 	coreVM, _, proVM, gBlock, _ := initTestProposerVM(t, forkTime, 0)
-	defer require.NoError(proVM.Shutdown(context.Background()))
+	defer func() {
+		require.NoError(proVM.Shutdown(context.Background()))
+	}()
 
 	xBlock := &snowman.TestBlock{
 		TestDecidable: choices.TestDecidable{
@@ -296,7 +302,9 @@ func TestBlockVerify_PostForkOption_FaultyParent(t *testing.T) {
 
 	coreVM, _, proVM, coreGenBlk, _ := initTestProposerVM(t, time.Time{}, 0)
 	proVM.Set(coreGenBlk.Timestamp())
-	defer require.NoError(proVM.Shutdown(context.Background()))
+	defer func() {
+		require.NoError(proVM.Shutdown(context.Background()))
+	}()
 
 	xBlock := &TestOptionsBlock{
 		TestBlock: snowman.TestBlock{
@@ -393,7 +401,9 @@ func TestBlockVerify_InvalidPostForkOption(t *testing.T) {
 
 	coreVM, _, proVM, coreGenBlk, _ := initTestProposerVM(t, time.Time{}, 0)
 	proVM.Set(coreGenBlk.Timestamp())
-	defer require.NoError(proVM.Shutdown(context.Background()))
+	defer func() {
+		require.NoError(proVM.Shutdown(context.Background()))
+	}()
 
 	// create an Oracle pre-fork block X
 	xBlockID := ids.GenerateTestID()
@@ -577,7 +587,9 @@ func TestGetBlock_MutatedSignature(t *testing.T) {
 	require := require.New(t)
 
 	coreVM, valState, proVM, coreGenBlk, _ := initTestProposerVM(t, time.Time{}, 0)
-	defer require.NoError(proVM.Shutdown(context.Background()))
+	defer func() {
+		require.NoError(proVM.Shutdown(context.Background()))
+	}()
 
 	// Make sure that we will be sampled to perform the proposals.
 	valState.GetValidatorSetF = func(context.Context, uint64, ids.ID) (map[ids.NodeID]*validators.GetValidatorOutput, error) {
