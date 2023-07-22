69c98d598cbc857302ab5045135e7725e7a620e4
nathan haim <haim.nathan@icloud.com>
2023-07-22 03:11:37 +0200

    vms/platformvm: Allow configuration of state cache sizes (#1677)

--- vms/platformvm/state/state_test.go
+++ vms/platformvm/state/state_test.go
@@ -34,8 +34,6 @@ import (
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 )
 
-const trackChecksum = false
-
 var (
 	initialTxID             = ids.GenerateTestID()
 	initialNodeID           = ids.GenerateTestNodeID()
@@ -498,12 +496,14 @@ func newStateFromDB(require *require.Assertions, db database.Database) State {
 	vdrs := validators.NewManager()
 	primaryVdrs := validators.NewSet()
 	_ = vdrs.Add(constants.PrimaryNetworkID, primaryVdrs)
+	execCfg, _ := config.GetExecutionConfig(nil)
 	state, err := new(
 		db,
 		metrics.Noop,
 		&config.Config{
 			Validators: vdrs,
 		},
+		execCfg,
 		&snow.Context{},
 		prometheus.NewRegistry(),
 		reward.NewCalculator(reward.Config{
@@ -513,7 +513,6 @@ func newStateFromDB(require *require.Assertions, db database.Database) State {
 			SupplyCap:          720 * units.MegaAvax,
 		}),
 		&utils.Atomic[bool]{},
-		trackChecksum,
 	)
 	require.NoError(err)
 	require.NotNil(state)
