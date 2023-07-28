e6edf802f0712970f31c8db698f5f074053acd87
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2023-07-28 16:30:39 -0400

    nit

--- x/p2p/handler.go
+++ x/p2p/handler.go
@@ -21,24 +21,24 @@ type responder struct {
 	client  *Client
 }
 
-func (a responder) AppRequest(ctx context.Context, nodeID ids.NodeID, requestID uint32, deadline time.Time, request []byte) error {
-	appResponse, err := a.handler.AppRequest(ctx, nodeID, requestID, deadline, request)
+func (r responder) AppRequest(ctx context.Context, nodeID ids.NodeID, requestID uint32, deadline time.Time, request []byte) error {
+	appResponse, err := r.handler.AppRequest(ctx, nodeID, requestID, deadline, request)
 	if err != nil {
 		return err
 	}
 
-	return a.client.AppResponse(ctx, nodeID, requestID, appResponse)
+	return r.client.AppResponse(ctx, nodeID, requestID, appResponse)
 }
 
-func (a responder) AppGossip(ctx context.Context, nodeID ids.NodeID, msg []byte) error {
-	return a.handler.AppGossip(ctx, nodeID, msg)
+func (r responder) AppGossip(ctx context.Context, nodeID ids.NodeID, msg []byte) error {
+	return r.handler.AppGossip(ctx, nodeID, msg)
 }
 
-func (a responder) CrossChainAppRequest(ctx context.Context, chainID ids.ID, requestID uint32, deadline time.Time, request []byte) error {
-	appResponse, err := a.handler.CrossChainAppRequest(ctx, chainID, requestID, deadline, request)
+func (r responder) CrossChainAppRequest(ctx context.Context, chainID ids.ID, requestID uint32, deadline time.Time, request []byte) error {
+	appResponse, err := r.handler.CrossChainAppRequest(ctx, chainID, requestID, deadline, request)
 	if err != nil {
 		return err
 	}
 
-	return a.client.CrossChainAppResponse(ctx, chainID, requestID, appResponse)
+	return r.client.CrossChainAppResponse(ctx, chainID, requestID, appResponse)
 }
