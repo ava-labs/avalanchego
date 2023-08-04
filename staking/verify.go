7606315d6285e7e60d84dbe01729a2415fe993c8
Stephen Buttolph <stephen@avalabs.org>
2023-08-04 00:43:59 -0400

    nit remove unneeded vars

--- staking/verify.go
+++ staking/verify.go
@@ -22,16 +22,13 @@ var ErrInvalidPublicKey = errors.New("invalid public key")
 // CheckSignature verifies that the signature is a valid signature over signed
 // from the certificate.
 func CheckSignature(cert *Certificate, signed []byte, signature []byte) error {
-	details, ok := signatureAlgorithmVerificationDetails[cert.SignatureAlgorithm]
+	verificationDetails, ok := signatureAlgorithmVerificationDetails[cert.SignatureAlgorithm]
 	if !ok {
 		return x509.ErrUnsupportedAlgorithm
 	}
 
-	hashType := details.hash
-	pubKeyAlgo := details.pubKeyAlgo
-
-	if hashType != crypto.Hash(0) {
-		h := hashType.New()
+	if verificationDetails.hash != crypto.Hash(0) {
+		h := verificationDetails.hash.New()
 		// TODO: should we handle this error?
 		_, _ = h.Write(signed)
 		signed = h.Sum(nil)
@@ -39,27 +36,27 @@ func CheckSignature(cert *Certificate, signed []byte, signature []byte) error {
 
 	switch pub := cert.PublicKey.(type) {
 	case *rsa.PublicKey:
-		if pubKeyAlgo != x509.RSA {
-			return signaturePublicKeyAlgoMismatchError(pubKeyAlgo, pub)
+		if verificationDetails.pubKeyAlgo != x509.RSA {
+			return signaturePublicKeyAlgoMismatchError(verificationDetails.pubKeyAlgo, pub)
 		}
 		if bitLen := pub.N.BitLen(); bitLen > MaxRSAKeyBitLen {
 			return fmt.Errorf("%w: bitLen=%d > maxBitLen=%d", ErrInvalidPublicKey, bitLen, MaxRSAKeyBitLen)
 		}
 		if isRSAPSS(cert.SignatureAlgorithm) {
-			return rsa.VerifyPSS(pub, hashType, signed, signature, &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthEqualsHash})
+			return rsa.VerifyPSS(pub, verificationDetails.hash, signed, signature, &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthEqualsHash})
 		}
-		return rsa.VerifyPKCS1v15(pub, hashType, signed, signature)
+		return rsa.VerifyPKCS1v15(pub, verificationDetails.hash, signed, signature)
 	case *ecdsa.PublicKey:
-		if pubKeyAlgo != x509.ECDSA {
-			return signaturePublicKeyAlgoMismatchError(pubKeyAlgo, pub)
+		if verificationDetails.pubKeyAlgo != x509.ECDSA {
+			return signaturePublicKeyAlgoMismatchError(verificationDetails.pubKeyAlgo, pub)
 		}
 		if !ecdsa.VerifyASN1(pub, signed, signature) {
 			return errors.New("x509: ECDSA verification failure")
 		}
 		return nil
 	case ed25519.PublicKey:
-		if pubKeyAlgo != x509.Ed25519 {
-			return signaturePublicKeyAlgoMismatchError(pubKeyAlgo, pub)
+		if verificationDetails.pubKeyAlgo != x509.Ed25519 {
+			return signaturePublicKeyAlgoMismatchError(verificationDetails.pubKeyAlgo, pub)
 		}
 		if !ed25519.Verify(pub, signed, signature) {
 			return errors.New("x509: Ed25519 verification failure")
