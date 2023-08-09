37fc2ca7dc0ec7e47375ff537202ec09aa720c72
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-08-09 17:12:21 -0400

    less jank

--- .github/workflows/check-clean-branch.sh
+++ .github/workflows/check-clean-branch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # Exits if any uncommitted changes are found.
 
-clean=$(git status | grep "nothing to commit, working tree clean")
-if [ -z "$clean" ]; then
-    exit 1
-fi
+git update-index --really-refresh >> /dev/null
+git diff-index --quiet HEAD
