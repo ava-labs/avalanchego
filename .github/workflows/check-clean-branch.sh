d0fde6414662ab5d772ac745a1b1e99c4dab4910
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-08-09 12:08:23 -0400

    fix wording

--- .github/workflows/check-clean-branch.sh
+++ .github/workflows/check-clean-branch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Exits if any uncommitted changes are found.
 
-clean=$(git status | grep "nothing to commit (working directory clean)")
+clean=$(git status | grep "nothing to commit, working tree clean")
 if [ -z "$clean" ]; then
     exit 1
 fi
