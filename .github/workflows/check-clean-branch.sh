635349e9528967b066b2711888094d39a1334edb
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-08-16 12:23:52 -0400

    add boilerplate

--- .github/workflows/check-clean-branch.sh
+++ .github/workflows/check-clean-branch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Exits if any uncommitted changes are found.
 
+set -o errexit # Ensure script failure on error. Without it execution continues in most cases (excepting some explicit conditional checks).
+set -o nounset # Ensures an error when an unset variable is referenced (i.e. requires explicit variable initialization).
+set -o pipefail # Ensures an error when a pipe call (both with `|` and `$( )`) fails.
+
 git update-index --really-refresh >> /dev/null
 git diff-index --quiet HEAD
