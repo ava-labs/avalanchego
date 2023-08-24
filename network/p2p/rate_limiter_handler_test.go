4662d65c26b3be5793dafe36dea5d62a565bafa6
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2023-08-24 11:25:13 -0400

    uts

--- network/p2p/rate_limiter_handler_test.go
+++ network/p2p/rate_limiter_handler_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/ava-labs/avalanchego/ids"
 )
 
-func TestRateLimitedHandler(t *testing.T) {
+func TestRateLimiterHandler_AppGossip(t *testing.T) {
 	tests := []struct {
 		name        string
 		rateLimiter *rate.Limiter
@@ -41,6 +41,70 @@ func TestRateLimitedHandler(t *testing.T) {
 			}
 
 			err := handler.AppGossip(context.Background(), ids.GenerateTestNodeID(), []byte("foobar"))
+			require.ErrorIs(err, tt.expected)
+		})
+	}
+}
+
+func TestRateLimiterHandler_AppRequest(t *testing.T) {
+	tests := []struct {
+		name        string
+		rateLimiter *rate.Limiter
+		expected    error
+	}{
+		{
+			name:        "rate limit exceeded",
+			rateLimiter: rate.NewLimiter(rate.Every(time.Second), 0),
+			expected:    ErrRateLimitExceeded,
+		},
+		{
+			name:        "handler called",
+			rateLimiter: rate.NewLimiter(rate.Every(time.Second), 1),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+
+			handler := RateLimiterHandler{
+				Handler:     NoOpHandler{},
+				RateLimiter: tt.rateLimiter,
+			}
+
+			_, err := handler.AppRequest(context.Background(), ids.GenerateTestNodeID(), time.Time{}, []byte("foobar"))
+			require.ErrorIs(err, tt.expected)
+		})
+	}
+}
+
+func TestRateLimiterHandler_CrossChainAppRequest(t *testing.T) {
+	tests := []struct {
+		name        string
+		rateLimiter *rate.Limiter
+		expected    error
+	}{
+		{
+			name:        "rate limit exceeded",
+			rateLimiter: rate.NewLimiter(rate.Every(time.Second), 0),
+			expected:    ErrRateLimitExceeded,
+		},
+		{
+			name:        "handler called",
+			rateLimiter: rate.NewLimiter(rate.Every(time.Second), 1),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+
+			handler := RateLimiterHandler{
+				Handler:     NoOpHandler{},
+				RateLimiter: tt.rateLimiter,
+			}
+
+			_, err := handler.CrossChainAppRequest(context.Background(), ids.GenerateTestID(), time.Time{}, []byte("foobar"))
 			require.ErrorIs(err, tt.expected)
 		})
 	}
