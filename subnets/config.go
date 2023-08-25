9eb82f037a42ff625598aaf23f93216144cabd59
Stephen Buttolph <stephen@avalabs.org>
2023-08-25 19:58:16 -0400

    idk grammar - be consistent

--- subnets/config.go
+++ subnets/config.go
@@ -51,7 +51,7 @@ type Config struct {
 	//
 	// Invariant: This value must be set such that the proposervm never needs to
 	// roll back more blocks than have been deleted. On startup, the proposervm
-	// rolls back it's accepted chain to match the innerVM's accepted chain. If
+	// rolls back its accepted chain to match the innerVM's accepted chain. If
 	// the innerVM is not persisting its last accepted block quickly enough, the
 	// database can become corrupted.
 	//
