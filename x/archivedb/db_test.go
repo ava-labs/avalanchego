6c791c698e049878efe13f4abc13dfd0dace8be8
Cesar <cesar.rodas@avalabs.org>
2023-08-25 00:29:07 -0400

    Improve NewBatch()
    NewBatch() no longer takes the height as an argument, instead it will take the
    next available number from the last written.
    
    Get() will also fail if the requested height is higher than the last height
    stored. Also introduced GetLastBlock() that is a tiny wrapper on top of Get()
    passing the last known height

--- x/archivedb/db_test.go
+++ x/archivedb/db_test.go
@@ -1,3 +1,5 @@
+// Copyright (C) 2019-2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
 package archivedb
 
 import (
@@ -21,44 +23,59 @@ func TestDbEntries(t *testing.T) {
 	db, err := getBasicDB()
 	require.NoError(t, err)
 
-	writer := db.NewBatch(10)
+	writer := db.NewBatch()
+	require.Equal(t, writer.Height(), uint64(1))
+	require.NoError(t, writer.Write())
+
+	writer = db.NewBatch()
 	require.NoError(t, writer.Put([]byte("key1"), []byte("value1@10")))
 	require.NoError(t, writer.Put([]byte("key2"), []byte("value2@10")))
+	require.Equal(t, writer.Height(), uint64(2))
 	require.NoError(t, writer.Write())
 
-	writer = db.NewBatch(100)
+	writer = db.NewBatch()
+	require.Equal(t, writer.Height(), uint64(3))
+	require.NoError(t, writer.Write())
+
+	writer = db.NewBatch()
 	require.NoError(t, writer.Put([]byte("key1"), []byte("value1@100")))
+	require.Equal(t, writer.Height(), uint64(4))
 	require.NoError(t, writer.Write())
 
-	writer = db.NewBatch(1000)
+	writer = db.NewBatch()
+	require.Equal(t, writer.Height(), uint64(5))
+	require.NoError(t, writer.Write())
+
+	writer = db.NewBatch()
 	require.NoError(t, writer.Put([]byte("key1"), []byte("value1@1000")))
 	require.NoError(t, writer.Put([]byte("key2"), []byte("value2@1000")))
+	require.Equal(t, writer.Height(), uint64(6))
 	require.NoError(t, writer.Write())
 
-	value, height, err := db.Get([]byte("key1"), 10000)
+	value, height, err := db.Get([]byte("key1"), 2)
 	require.NoError(t, err)
-	require.Equal(t, uint64(1000), height)
-	require.Equal(t, []byte("value1@1000"), value)
+	require.Equal(t, uint64(2), height)
+	require.Equal(t, []byte("value1@10"), value)
 
-	value, height, err = db.Get([]byte("key1"), 999)
+	value, height, err = db.Get([]byte("key1"), 4)
 	require.NoError(t, err)
-	require.Equal(t, uint64(100), height)
+	require.Equal(t, uint64(4), height)
 	require.Equal(t, []byte("value1@100"), value)
 
-	value, height, err = db.Get([]byte("key2"), 1999)
+	value, height, err = db.Get([]byte("key2"), 6)
 	require.NoError(t, err)
-	require.Equal(t, uint64(1000), height)
+	require.Equal(t, uint64(6), height)
 	require.Equal(t, []byte("value2@1000"), value)
 
-	value, height, err = db.Get([]byte("key2"), 999)
+	value, height, err = db.Get([]byte("key2"), 4)
 	require.NoError(t, err)
-	require.Equal(t, uint64(10), height)
+	require.Equal(t, uint64(2), height)
 	require.Equal(t, []byte("value2@10"), value)
 
-	_, _, err = db.Get([]byte("key1"), 9)
+	_, _, err = db.Get([]byte("key1"), 1)
 	require.ErrorIs(t, err, database.ErrNotFound)
 
-	_, _, err = db.Get([]byte("key3"), 1999)
+	_, _, err = db.Get([]byte("key3"), 6)
 	require.ErrorIs(t, err, database.ErrNotFound)
 }
 
@@ -66,33 +83,36 @@ func TestDelete(t *testing.T) {
 	db, err := getBasicDB()
 	require.NoError(t, err)
 
-	writer := db.NewBatch(10)
+	writer := db.NewBatch()
 	require.NoError(t, writer.Put([]byte("key1"), []byte("value1@10")))
 	require.NoError(t, writer.Put([]byte("key2"), []byte("value2@10")))
+	require.Equal(t, writer.Height(), uint64(1))
 	require.NoError(t, writer.Write())
 
-	writer = db.NewBatch(100)
+	writer = db.NewBatch()
+	require.Equal(t, writer.Height(), uint64(2))
 	require.NoError(t, writer.Put([]byte("key1"), []byte("value1@100")))
 	require.NoError(t, writer.Write())
 
-	writer = db.NewBatch(1000)
+	writer = db.NewBatch()
+	require.Equal(t, writer.Height(), uint64(3))
 	require.NoError(t, writer.Delete([]byte("key1")))
 	require.NoError(t, writer.Delete([]byte("key2")))
 	require.NoError(t, writer.Write())
 
-	value, height, err := db.Get([]byte("key1"), 999)
+	value, height, err := db.Get([]byte("key1"), 2)
 	require.NoError(t, err)
-	require.Equal(t, uint64(100), height)
+	require.Equal(t, uint64(2), height)
 	require.Equal(t, []byte("value1@100"), value)
 
-	value, height, err = db.Get([]byte("key2"), 999)
+	value, height, err = db.Get([]byte("key2"), 1)
 	require.NoError(t, err)
-	require.Equal(t, uint64(10), height)
+	require.Equal(t, uint64(1), height)
 	require.Equal(t, []byte("value2@10"), value)
 
-	_, _, err = db.Get([]byte("key2"), 1000)
+	_, _, err = db.Get([]byte("key2"), 3)
 	require.ErrorIs(t, err, database.ErrNotFound)
 
-	_, _, err = db.Get([]byte("key1"), 10000)
+	_, _, err = db.Get([]byte("key1"), 3)
 	require.ErrorIs(t, err, database.ErrNotFound)
 }
