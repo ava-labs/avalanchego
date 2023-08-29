3f5dc8a60f6a73a718596fcfe1d0f924c32298b4
marun <maru.newby@avalabs.org>
2023-08-29 10:17:05 -0700

    e2e: Add avalanchego e2e job (#305)
    * e2e: Add avalanchego e2e job
    
    * fixup: Use currently supported golang version
    
    * fixup: Enable configurable avalanchego clone path

---
+++ scripts/tests.e2e.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Run AvalancheGo e2e tests from the target version against the current state of coreth.
+
+# e.g.,
+# ./scripts/tests.e2e.sh
+# AVALANCHE_VERSION=v1.10.x ./scripts/tests.e2e.sh
+if ! [[ "$0" =~ scripts/tests.e2e.sh ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+# Coreth root directory
+CORETH_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
+
+# Allow configuring the clone path to point to an existing clone
+AVALANCHEGO_CLONE_PATH="${AVALANCHEGO_CLONE_PATH:-avalanchego}"
+
+# Load the version
+source "$CORETH_PATH"/scripts/versions.sh
+
+# Always return to the coreth path on exit
+function cleanup {
+  cd "${CORETH_PATH}"
+}
+trap cleanup EXIT
+
+echo "checking out target AvalancheGo version ${avalanche_version}"
+if [[ -d "${AVALANCHEGO_CLONE_PATH}" ]]; then
+  echo "updating existing clone"
+  cd "${AVALANCHEGO_CLONE_PATH}"
+  git fetch
+  git checkout -B "${avalanche_version}"
+else
+  echo "creating new clone"
+  git clone -b "${avalanche_version}"\
+      --single-branch https://github.com/ava-labs/avalanchego.git\
+      "${AVALANCHEGO_CLONE_PATH}"
+  cd "${AVALANCHEGO_CLONE_PATH}"
+fi
+
+echo "updating coreth dependency to point to ${CORETH_PATH}"
+go mod edit -replace "github.com/ava-labs/coreth=${CORETH_PATH}"
+go mod tidy
+
+echo "building avalanchego"
+./scripts/build.sh -r
+
+echo "running AvalancheGo e2e tests"
+./scripts/tests.e2e.sh ./build/avalanchego
