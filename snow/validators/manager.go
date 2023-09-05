7c591812575d3da04ee66262d1bb62c07dd3c4b8
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-09-05 12:32:27 +0300

    relax locks

--- snow/validators/manager.go
+++ snow/validators/manager.go
@@ -112,14 +112,18 @@ type manager struct {
 }
 
 func (m *manager) AddStaker(subnetID ids.ID, nodeID ids.NodeID, pk *bls.PublicKey, txID ids.ID, weight uint64) error {
+	// Use write lock in case we need to create a new set
 	m.lock.Lock()
-	defer m.lock.Unlock()
-
 	set, exists := m.subnetToVdrs[subnetID]
 	if !exists {
 		set = newSet()
 		m.subnetToVdrs[subnetID] = set
 	}
+	m.lock.Unlock()
+
+	// Use read lock for all other operations
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 
 	return set.Add(nodeID, pk, txID, weight)
 }
@@ -174,8 +178,6 @@ func (m *manager) SubsetWeight(subnetID ids.ID, validatorIDs set.Set[ids.NodeID]
 
 func (m *manager) RemoveWeight(subnetID ids.ID, nodeID ids.NodeID, weight uint64) error {
 	m.lock.RLock()
-	defer m.lock.RUnlock()
-
 	set, exists := m.subnetToVdrs[subnetID]
 	if !exists {
 		return errMissingValidator
@@ -184,7 +186,11 @@ func (m *manager) RemoveWeight(subnetID ids.ID, nodeID ids.NodeID, weight uint64
 	if err := set.RemoveWeight(nodeID, weight); err != nil {
 		return err
 	}
+	m.lock.RUnlock()
 
+	// Use write lock to remove the subnet if it is empty
+	m.lock.Lock()
+	defer m.lock.Unlock()
 	// If this was the last validator in the subnet
 	// and no callback listeners are registered, remove the subnet
 	if set.Len() == 0 && !set.HasCallbackRegistered() {
@@ -259,14 +265,18 @@ func (m *manager) GetMap(subnetID ids.ID) map[ids.NodeID]*GetValidatorOutput {
 }
 
 func (m *manager) RegisterCallbackListener(subnetID ids.ID, listener SetCallbackListener) {
+	// Use write lock in case we need to create a new set
 	m.lock.Lock()
-	defer m.lock.Unlock()
-
 	set, exists := m.subnetToVdrs[subnetID]
 	if !exists {
 		set = newSet()
 		m.subnetToVdrs[subnetID] = set
 	}
+	m.lock.Unlock()
+
+	// Use read lock for all other operations
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 
 	set.RegisterCallbackListener(listener)
 }
