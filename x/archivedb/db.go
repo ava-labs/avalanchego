72cee4c8e1ec92f31bbb7fd7528a292a9e304577
Cesar <137245636+nytzuga@users.noreply.github.com>
2023-09-07 01:27:32 -0400

    Introduce GetHeightReader() function (#1956)
    Signed-off-by: Cesar <137245636+nytzuga@users.noreply.github.com>
    Co-authored-by: aaronbuchwald <aaron.buchwald56@gmail.com>

--- x/archivedb/db.go
+++ x/archivedb/db.go
@@ -4,12 +4,16 @@
 package archivedb
 
 import (
-	"bytes"
 	"context"
 	"encoding/binary"
 	"errors"
 
 	"github.com/ava-labs/avalanchego/database"
+)
+
+var (
+	_ database.Batch          = (*batchWithHeight)(nil)
+	_ database.KeyValueReader = (*dbHeightReader)(nil)
 )
 
 // ArchiveDb
@@ -85,61 +89,35 @@ func (db *archiveDB) GetLastBlock(key []byte) ([]byte, uint64, error) {
 	return db.Get(key, db.currentHeight)
 }
 
+// Returns an object which implements the database.KeyValueReader trait for all
+// keys defined at the given height.
+func (db *archiveDB) GetHeightReader(height uint64) (dbHeightReader, error) {
+	if height > db.currentHeight || height == 0 {
+		return dbHeightReader{}, ErrUnknownHeight
+	}
+	return dbHeightReader{
+		db:                 db,
+		height:             height,
+		heightLastFoundKey: 0,
+	}, nil
+}
+
 // Fetches the value of a given prefix at a given height.
 //
 // If the value does not exists or it was actually removed an error is returned.
 // Otherwise a value does exists it will be returned, alongside with the height
 // at which it was updated prior the requested height.
 func (db *archiveDB) Get(key []byte, height uint64) ([]byte, uint64, error) {
-	if height > db.currentHeight || height == 0 {
-		return nil, db.currentHeight, ErrUnknownHeight
+	reader, err := db.GetHeightReader(height)
+	if err != nil {
+		return nil, 0, err
+	}
+	value, err := reader.Get(key)
+	if err != nil {
+		return nil, 0, err
 	}
 
-	internalKey := newInternalKey(key, height)
-	iterator := db.rawDB.NewIteratorWithStart(internalKey.Bytes())
-	keyLength := len(key)
-
-	defer iterator.Release()
-
-	for {
-		if !iterator.Next() {
-			// There is no available key with the requested prefix
-			return nil, 0, database.ErrNotFound
-		}
-
-		internalKey, err := parseKey(iterator.Key())
-		if err != nil {
-			return nil, 0, err
-		}
-
-		if !bytes.Equal(internalKey.key, key) {
-			if keyLength < len(internalKey.key) {
-				// The current key is a longer than the requested key, now check
-				// if they match at the same length as `key`, if that is the
-				// case we should continue to the next key, until the exact
-				// requested key is found or anothe prefix is found and by that
-				// point it would exit
-				if bytes.Equal(internalKey.key[0:keyLength], key) {
-					// Same prefix, read the next key until the prefix is
-					// different or the exact requested key is found
-					continue
-				}
-			}
-			// The previous key that was found does has another prefix, because the
-			// iterator is not aware of prefixes. If this happens it means the
-			// prefix at the requested height does not exists.
-			return nil, 0, database.ErrNotFound
-		}
-
-		if internalKey.isDeleted {
-			// The database is append only, so when removing a record creates a new
-			// record with an special flag is being created. Before returning the
-			// value we check if the deleted flag is present or not.
-			return nil, 0, database.ErrNotFound
-		}
-
-		return iterator.Value(), internalKey.height, nil
-	}
+	return value, reader.heightLastFoundKey, nil
 }
 
 // Creates a new batch to append database changes in a given height
@@ -195,4 +173,12 @@ func (c *batchWithHeight) Size() int {
 // Removed all pending writes and deletes to the database
 func (c *batchWithHeight) Reset() {
 	c.batch.Reset()
+}
+
+func (c *batchWithHeight) Inner() database.Batch {
+	return c.batch
+}
+
+func (c *batchWithHeight) Replay(w database.KeyValueWriterDeleter) error {
+	return c.batch.Replay(w)
 }
