d26c2ae3a7b7119ce5855f9505baa278350b8df0
dboehm-avalabs <david.boehm@avalabs.org>
2023-09-08 09:54:15 -0400

    Update helpers_test.go

--- x/merkledb/helpers_test.go
+++ x/merkledb/helpers_test.go
@@ -9,128 +9,109 @@ import (
 )
 
 func protoProofsEqual(proof1, proof2 *pb.Proof) bool {
-	if (proof1 == nil && proof2 != nil) || (proof2 == nil && proof1 != nil) {
-		return false
-	}
-	if proof1 == nil && proof2 == nil {
-		return true
-	}
-	if len(proof1.Proof) != len(proof2.Proof) {
-		return false
-	}
+	return (proof1 == nil && proof2 == nil) ||
+		(proof1 != nil &&
+			proof2 != nil &&
+			len(proof1.Proof) == len(proof2.Proof) &&
+			bytes.Equal(proof1.Key, proof2.Key) &&
+			protoMaybeBytesEqual(proof1.Value, proof2.Value))
+}
 
-	for i := 0; i < len(proof1.Proof); i++ {
-		if !protoProofNodeEqual(proof1.Proof[i], proof2.Proof[i]) {
+func protoProofNodesEqual(proof1, proof2 []*pb.ProofNode) bool {
+	if len(proof1) != len(proof2) {
+		return false
+	}
+	for i, node := range proof1 {
+		if !protoProofNodeEqual(node, proof2[i]) {
 			return false
 		}
 	}
-	return bytes.Equal(proof1.Key, proof2.Key) && protoMaybeBytesEqual(proof1.Value, proof2.Value)
+	return true
 }
 
 func protoRangeProofsEqual(proof1, proof2 *pb.RangeProof) bool {
-	if (proof1 == nil && proof2 != nil) || (proof2 == nil && proof1 != nil) {
-		return false
-	}
 	if proof1 == nil && proof2 == nil {
 		return true
 	}
-	if len(proof1.Start) != len(proof2.Start) {
-		return false
-	}
-	if len(proof1.End) != len(proof2.End) {
-		return false
-	}
-	if len(proof1.KeyValues) != len(proof2.KeyValues) {
+	if (proof1 == nil ||
+		proof2 == nil) ||
+		len(proof1.Start) != len(proof2.Start) ||
+		len(proof1.End) != len(proof2.End) ||
+		len(proof1.KeyValues) != len(proof2.KeyValues) {
 		return false
 	}
 
-	for i := 0; i < len(proof1.Start); i++ {
-		if !protoProofNodeEqual(proof1.Start[i], proof2.Start[i]) {
-			return false
-		}
-	}
-	for i := 0; i < len(proof1.End); i++ {
-		if !protoProofNodeEqual(proof1.End[i], proof2.End[i]) {
-			return false
-		}
-	}
 	for i := 0; i < len(proof1.KeyValues); i++ {
 		if !protoKeyValueEqual(proof1.KeyValues[i], proof2.KeyValues[i]) {
 			return false
 		}
 	}
-	return true
+
+	return protoProofNodesEqual(proof1.Start, proof2.Start) &&
+		protoProofNodesEqual(proof1.End, proof2.End)
 }
 
 func protoChangeProofsEqual(proof1, proof2 *pb.ChangeProof) bool {
-	if (proof1 == nil && proof2 != nil) || (proof2 == nil && proof1 != nil) {
-		return false
-	}
 	if proof1 == nil && proof2 == nil {
 		return true
 	}
-	if len(proof1.StartProof) != len(proof2.StartProof) {
-		return false
-	}
-	if len(proof1.EndProof) != len(proof2.EndProof) {
-		return false
-	}
-	if len(proof1.KeyChanges) != len(proof2.KeyChanges) {
+
+	if proof1 == nil ||
+		proof2 == nil ||
+		len(proof1.StartProof) != len(proof2.StartProof) ||
+		len(proof1.EndProof) != len(proof2.EndProof) ||
+		len(proof1.KeyChanges) != len(proof2.KeyChanges) {
 		return false
 	}
 
-	for i := 0; i < len(proof1.StartProof); i++ {
-		if !protoProofNodeEqual(proof1.StartProof[i], proof2.StartProof[i]) {
-			return false
-		}
-	}
-	for i := 0; i < len(proof1.EndProof); i++ {
-		if !protoProofNodeEqual(proof1.EndProof[i], proof2.EndProof[i]) {
-			return false
-		}
-	}
 	for i := 0; i < len(proof1.KeyChanges); i++ {
 		if !protoKeyChangeEqual(proof1.KeyChanges[i], proof2.KeyChanges[i]) {
 			return false
 		}
 	}
-	return true
+	return protoProofNodesEqual(proof1.StartProof, proof2.StartProof) &&
+		protoProofNodesEqual(proof1.EndProof, proof2.EndProof)
 }
 
 func protoProofNodeEqual(node1, node2 *pb.ProofNode) bool {
-	if (node1 == nil && node2 != nil) || (node2 == nil && node1 != nil) {
-		return false
-	}
-	if node1 == nil && node2 == nil {
-		return true
-	}
-
-	return protoSerializedPathEqual(node1.Key, node2.Key) &&
-		protoMaybeBytesEqual(node1.ValueOrHash, node2.ValueOrHash) &&
-		protoChildrenEqual(node1.Children, node2.Children)
+	return (node1 == nil && node2 == nil) ||
+		(node2 != nil &&
+			node1 != nil &&
+			protoSerializedPathEqual(node1.Key, node2.Key) &&
+			protoMaybeBytesEqual(node1.ValueOrHash, node2.ValueOrHash) &&
+			protoChildrenEqual(node1.Children, node2.Children))
 }
 
 func protoSerializedPathEqual(path1, path2 *pb.SerializedPath) bool {
-	return (path1 == nil && path2 == nil) || (path1 != nil && path2 != nil && bytes.Equal(path1.Value, path2.Value) && path1.NibbleLength == path2.NibbleLength)
+	return (path1 == nil && path2 == nil) ||
+		(path1 != nil &&
+			path2 != nil &&
+			bytes.Equal(path1.Value, path2.Value) &&
+			path1.NibbleLength == path2.NibbleLength)
 }
 
 func protoKeyValueEqual(change1, change2 *pb.KeyValue) bool {
-	return (change1 == nil && change2 == nil) || (change1 != nil && change2 != nil && bytes.Equal(change1.Key, change2.Key) && bytes.Equal(change1.Value, change2.Value))
+	return (change1 == nil && change2 == nil) ||
+		(change1 != nil &&
+			change2 != nil &&
+			bytes.Equal(change1.Key, change2.Key) &&
+			bytes.Equal(change1.Value, change2.Value))
 }
 
 func protoKeyChangeEqual(change1, change2 *pb.KeyChange) bool {
-	return (change1 == nil && change2 == nil) || (change1 != nil && change2 != nil && bytes.Equal(change1.Key, change2.Key) && protoMaybeBytesEqual(change1.Value, change2.Value))
+	return (change1 == nil && change2 == nil) ||
+		(change1 != nil &&
+			change2 != nil &&
+			bytes.Equal(change1.Key, change2.Key) &&
+			protoMaybeBytesEqual(change1.Value, change2.Value))
 }
 
 func protoMaybeBytesEqual(maybe1, maybe2 *pb.MaybeBytes) bool {
-	if (maybe1 == nil && maybe2 != nil) || (maybe2 == nil && maybe1 != nil) {
-		return false
-	}
-	if maybe1 == nil && maybe2 == nil {
-		return true
-	}
-
-	return maybe1.IsNothing == maybe2.IsNothing && bytes.Equal(maybe1.Value, maybe2.Value)
+	return (maybe1 == nil && maybe2 == nil) ||
+		(maybe1 != nil &&
+			maybe2 != nil &&
+			maybe1.IsNothing == maybe2.IsNothing &&
+			bytes.Equal(maybe1.Value, maybe2.Value))
 }
 
 func protoChildrenEqual(children1, children2 map[uint32][]byte) bool {
@@ -146,32 +127,19 @@ func protoChildrenEqual(children1, children2 map[uint32][]byte) bool {
 }
 
 func rangeProofsEqual(proof1, proof2 RangeProof) bool {
-	if len(proof1.StartProof) != len(proof2.StartProof) {
-		return false
-	}
-	if len(proof1.EndProof) != len(proof2.EndProof) {
-		return false
-	}
-	if len(proof1.KeyValues) != len(proof2.KeyValues) {
+	if len(proof1.StartProof) != len(proof2.StartProof) ||
+		len(proof1.EndProof) != len(proof2.EndProof) ||
+		len(proof1.KeyValues) != len(proof2.KeyValues) {
 		return false
 	}
 
-	for i := 0; i < len(proof1.StartProof); i++ {
-		if !proofNodeEqual(proof1.StartProof[i], proof2.StartProof[i]) {
-			return false
-		}
-	}
-	for i := 0; i < len(proof1.EndProof); i++ {
-		if !proofNodeEqual(proof1.EndProof[i], proof2.EndProof[i]) {
-			return false
-		}
-	}
 	for i := 0; i < len(proof1.KeyValues); i++ {
 		if !keyValueEqual(proof1.KeyValues[i], proof2.KeyValues[i]) {
 			return false
 		}
 	}
-	return true
+	return proofNodesEqual(proof1.StartProof, proof2.StartProof) &&
+		proofNodesEqual(proof1.EndProof, proof2.EndProof)
 }
 
 func proofsEqual(proof1, proof2 Proof) bool {
@@ -184,32 +152,33 @@ func proofsEqual(proof1, proof2 Proof) bool {
 			return false
 		}
 	}
-	return bytes.Equal(proof1.Key, proof2.Key) && maybe.Equal(proof1.Value, proof2.Value, bytes.Equal)
+	return bytes.Equal(proof1.Key, proof2.Key) &&
+		maybe.Equal(proof1.Value, proof2.Value, bytes.Equal)
 }
 
 func changeProofsEqual(proof1, proof2 ChangeProof) bool {
-	if len(proof1.StartProof) != len(proof2.StartProof) {
-		return false
-	}
-	if len(proof1.EndProof) != len(proof2.EndProof) {
-		return false
-	}
-	if len(proof1.KeyChanges) != len(proof2.KeyChanges) {
+	if len(proof1.StartProof) != len(proof2.StartProof) ||
+		len(proof1.EndProof) != len(proof2.EndProof) ||
+		len(proof1.KeyChanges) != len(proof2.KeyChanges) {
 		return false
 	}
 
-	for i := 0; i < len(proof1.StartProof); i++ {
-		if !proofNodeEqual(proof1.StartProof[i], proof2.StartProof[i]) {
-			return false
-		}
-	}
-	for i := 0; i < len(proof1.EndProof); i++ {
-		if !proofNodeEqual(proof1.EndProof[i], proof2.EndProof[i]) {
-			return false
-		}
-	}
 	for i := 0; i < len(proof1.KeyChanges); i++ {
 		if !keyChangeEqual(proof1.KeyChanges[i], proof2.KeyChanges[i]) {
+			return false
+		}
+	}
+
+	return proofNodesEqual(proof1.StartProof, proof2.StartProof) &&
+		proofNodesEqual(proof1.EndProof, proof2.EndProof)
+}
+
+func proofNodesEqual(proof1, proof2 []ProofNode) bool {
+	if len(proof1) != len(proof2) {
+		return false
+	}
+	for i, node := range proof1 {
+		if !proofNodeEqual(node, proof2[i]) {
 			return false
 		}
 	}
@@ -223,15 +192,18 @@ func proofNodeEqual(node1, node2 ProofNode) bool {
 }
 
 func serializedPathEqual(path1, path2 SerializedPath) bool {
-	return bytes.Equal(path1.Value, path2.Value) && path1.NibbleLength == path2.NibbleLength
+	return bytes.Equal(path1.Value, path2.Value) &&
+		path1.NibbleLength == path2.NibbleLength
 }
 
 func keyValueEqual(change1, change2 KeyValue) bool {
-	return bytes.Equal(change1.Key, change2.Key) && bytes.Equal(change1.Value, change2.Value)
+	return bytes.Equal(change1.Key, change2.Key) &&
+		bytes.Equal(change1.Value, change2.Value)
 }
 
 func keyChangeEqual(change1, change2 KeyChange) bool {
-	return bytes.Equal(change1.Key, change2.Key) && maybe.Equal(change1.Value, change2.Value, bytes.Equal)
+	return bytes.Equal(change1.Key, change2.Key) &&
+		maybe.Equal(change1.Value, change2.Value, bytes.Equal)
 }
 
 func childrenEqual(children1, children2 map[byte]ids.ID) bool {
