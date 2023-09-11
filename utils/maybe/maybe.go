cb67fe921353750cb58d12ac475aa42c7953d4e0
dboehm-avalabs <david.boehm@avalabs.org>
2023-09-10 20:09:07 -0400

    Update maybe.go

--- utils/maybe/maybe.go
+++ utils/maybe/maybe.go
@@ -65,8 +65,12 @@ func Bind[T, U any](m Maybe[T], f func(T) U) Maybe[U] {
 
 // Equal returns true if both m1 and m2 are nothing or have the same value according to [equalFunc].
 func Equal[T any](m1 Maybe[T], m2 Maybe[T], equalFunc func(T, T) bool) bool {
-	return (m1.IsNothing() && m2.IsNothing()) ||
-		(m1.HasValue() &&
-			m2.HasValue() &&
-			equalFunc(m1.Value(), m2.Value()))
+	if m1.IsNothing() {
+		return m2.IsNothing()
+	}
+
+	if m2.IsNothing() {
+		return false
+	}
+	return equalFunc(m1.Value(), m2.Value())
 }
