51dd4430b01a2ac1e58d6a78f4316b406ba6eab8
Dan Sover <dan.sover@avalabs.org>
2023-09-15 17:09:15 -0400

    ci: Add multiarch avalanchego image builds
    Signed-off-by: Dan Sover <dan.sover@avalabs.org>

--- scripts/build_local_image.sh
+++ scripts/build_local_image.sh
@@ -14,6 +14,9 @@ source "$AVALANCHE_PATH"/scripts/constants.sh
 full_commit_hash="$(git --git-dir="$AVALANCHE_PATH/.git" rev-parse HEAD)"
 commit_hash="${full_commit_hash::8}"
 
+echo "Creating docker buildx builder"
+docker buildx create --name mybuilder --use --bootstrap
+
 echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$commit_hash , $avalanchego_dockerhub_repo:$current_branch"
-docker build -t "$avalanchego_dockerhub_repo:$commit_hash" \
+docker buildx build --platform linux/amd64,linux/arm64 -t "$avalanchego_dockerhub_repo:$commit_hash" \
         -t "$avalanchego_dockerhub_repo:$current_branch" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile"
