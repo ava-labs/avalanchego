879d8ff88a140404ed538c3209a57b47c812b95b
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-09-18 14:02:23 +0200

    reduced diff

--- ids/short.go
+++ ids/short.go
@@ -26,22 +26,9 @@ var (
 // ShortID wraps a 20 byte hash as an identifier
 type ShortID [ShortIDLen]byte
 
-// Bytes returns the 20 byte hash as a slice. It is assumed this slice is not
-// modified.
-func (id ShortID) Bytes() []byte {
-	return id[:]
-}
-
 // ToShortID attempt to convert a byte slice into an id
 func ToShortID(bytes []byte) (ShortID, error) {
 	return hashing.ToHash160(bytes)
-}
-
-func (id ShortID) String() string {
-	// We assume that the maximum size of a byte slice that
-	// can be stringified is at least the length of an ID
-	str, _ := cb58.Encode(id.Bytes())
-	return str
 }
 
 // ShortFromString is the inverse of ShortID.String()
@@ -51,6 +38,15 @@ func ShortFromString(idStr string) (ShortID, error) {
 		return ShortID{}, err
 	}
 	return ToShortID(bytes)
+}
+
+// ShortFromPrefixedString returns a ShortID assuming the cb58 format is
+// prefixed
+func ShortFromPrefixedString(idStr, prefix string) (ShortID, error) {
+	if !strings.HasPrefix(idStr, prefix) {
+		return ShortID{}, fmt.Errorf("ID: %s is missing the prefix: %s", idStr, prefix)
+	}
+	return ShortFromString(strings.TrimPrefix(idStr, prefix))
 }
 
 func (id ShortID) MarshalJSON() ([]byte, error) {
@@ -83,25 +79,14 @@ func (id *ShortID) UnmarshalJSON(b []byte) error {
 	return err
 }
 
-func (id ShortID) MarshalText() ([]byte, error) {
-	return []byte(id.String()), nil
-}
-
 func (id *ShortID) UnmarshalText(text []byte) error {
 	return id.UnmarshalJSON(text)
 }
 
-func (id ShortID) Less(other ShortID) bool {
-	return bytes.Compare(id[:], other[:]) == -1
-}
-
-// ShortFromPrefixedString returns a ShortID assuming the cb58 format is
-// prefixed
-func ShortFromPrefixedString(idStr, prefix string) (ShortID, error) {
-	if !strings.HasPrefix(idStr, prefix) {
-		return ShortID{}, fmt.Errorf("ID: %s is missing the prefix: %s", idStr, prefix)
-	}
-	return ShortFromString(strings.TrimPrefix(idStr, prefix))
+// Bytes returns the 20 byte hash as a slice. It is assumed this slice is not
+// modified.
+func (id ShortID) Bytes() []byte {
+	return id[:]
 }
 
 // Hex returns a hex encoded string of this id.
@@ -109,9 +94,24 @@ func (id ShortID) Hex() string {
 	return hex.EncodeToString(id.Bytes())
 }
 
+func (id ShortID) String() string {
+	// We assume that the maximum size of a byte slice that
+	// can be stringified is at least the length of an ID
+	str, _ := cb58.Encode(id.Bytes())
+	return str
+}
+
 // PrefixedString returns the String representation with a prefix added
 func (id ShortID) PrefixedString(prefix string) string {
 	return prefix + id.String()
+}
+
+func (id ShortID) MarshalText() ([]byte, error) {
+	return []byte(id.String()), nil
+}
+
+func (id ShortID) Less(other ShortID) bool {
+	return bytes.Compare(id[:], other[:]) == -1
 }
 
 // ShortIDsToStrings converts an array of shortIDs to an array of their string
