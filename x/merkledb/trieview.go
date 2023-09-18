2c4405189a3b82889fffc26a66a2cd168e6e8c24
dboehm-avalabs <david.boehm@avalabs.org>
2023-09-18 15:41:10 -0400

    Update trieview.go

--- x/merkledb/trieview.go
+++ x/merkledb/trieview.go
@@ -357,8 +357,10 @@ func (t *trieView) getProof(ctx context.Context, key []byte) (*Proof, error) {
 		return proof, nil
 	}
 
-	childPath := closestNode.key.Append(nextIndex).Extend(child.compressedPath)
-	childNode, err := t.getNodeFromParent(closestNode, childPath, child.hasValue)
+	childNode, err := t.getNodeWithID(
+		child.id,
+		closestNode.key.Append(nextIndex).Extend(child.compressedPath),
+		child.hasValue)
 	if err != nil {
 		return nil, err
 	}
@@ -687,7 +689,7 @@ func (t *trieView) compressNodePath(parent, node *node) error {
 			childEntry = entry
 		}
 
-		nextNode, err := t.getNodeFromParent(node, childPath, childEntry.hasValue)
+		nextNode, err := t.getNodeWithID(childEntry.id, childPath, childEntry.hasValue)
 		if err != nil {
 			return err
 		}
@@ -994,19 +996,6 @@ func (t *trieView) recordValueChange(key Path, value maybe.Maybe[[]byte]) error 
 		after:  value,
 	}
 	return nil
-}
-
-// Retrieves the node with the given [key], which is a child of [parent], and
-// uses the [parent] node to initialize the child node's ID.
-// If the node is loaded from the baseDB, [hasValue] determines which database the node is stored in.
-// Returns database.ErrNotFound if the child doesn't exist.
-func (t *trieView) getNodeFromParent(parent *node, key Path, hasValue bool) (*node, error) {
-	// confirm the child exists and get its ID before attempting to load it
-	if child, exists := parent.children[key.Token(parent.key.length)]; exists {
-		return t.getNodeWithID(child.id, key, hasValue)
-	}
-
-	return nil, database.ErrNotFound
 }
 
 // Retrieves a node with the given [key].
