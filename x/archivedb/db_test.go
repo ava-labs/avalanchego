26f5a62c57796fe8500d0a5748e1635bbf0d38a6
Cesar <137245636+nytzuga@users.noreply.github.com>
2023-09-19 18:40:28 -0400

    Add more unit tests

--- x/archivedb/db_test.go
+++ x/archivedb/db_test.go
@@ -191,3 +191,32 @@ func TestInvalidBatchHeight(t *testing.T) {
 	err = writer.Write()
 	require.ErrorIs(t, err, ErrInvalidBatchHeight)
 }
+
+func TestDBMoreEfficientLookups(t *testing.T) {
+	require := require.New(t)
+	var (
+		key          = []byte("key")
+		value        = []byte("value")
+		maliciousKey = []byte("key\xff\xff\xff\xff\xff\xff\xff\xfd")
+	)
+	db, err := NewArchiveDB(
+		context.Background(),
+		&limitIterationDB{
+			Database: memdb.New(),
+		},
+	)
+	require.NoError(err)
+	writer := db.NewBatch(1)
+	require.NoError(writer.Put(key, value))
+	require.Equal(uint64(1), writer.Height())
+	require.NoError(writer.Write())
+	for i := 2; i < 10000; i++ {
+		writer = db.NewBatch(uint64(i))
+		require.NoError(writer.Put(maliciousKey, []byte{byte(i)}))
+		require.NoError(writer.Write())
+	}
+	val, height, err := db.Get(key, db.currentHeight)
+	require.NoError(err)
+	require.Equal(uint64(1), height)
+	require.Equal(value, val)
+}
