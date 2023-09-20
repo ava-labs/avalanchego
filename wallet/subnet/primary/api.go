985615973c3c3b0bcec777413569ebc4a1267a84
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-09-20 12:41:30 +0200

    leftover

--- wallet/subnet/primary/api.go
+++ wallet/subnet/primary/api.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/ava-labs/coreth/ethclient"
 	"github.com/ava-labs/coreth/plugin/evm"
+
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/ava-labs/avalanchego/api/info"
