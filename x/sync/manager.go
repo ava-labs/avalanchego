c93eb9954dd4cd0c2a5e366ce6b3a1837c164e2c
dboehm-avalabs <david.boehm@avalabs.org>
2023-09-22 15:00:23 -0400

    Update manager.go

--- x/sync/manager.go
+++ x/sync/manager.go
@@ -466,7 +466,7 @@ func (m *Manager) findNextKey(
 	receivedProofNodeIndex := len(endProof) - 1
 
 	// traverse the two proofs from the deepest nodes up to the root until a difference is found
-	for localProofNodeIndex >= 0 && receivedProofNodeIndex >= 0 {
+	for localProofNodeIndex >= 0 && receivedProofNodeIndex >= 0 && nextKey.IsNothing() {
 		localProofNode := localProofNodes[localProofNodeIndex]
 		receivedProofNode := endProof[receivedProofNodeIndex]
 
