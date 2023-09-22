8696f778b13d36dbe4993d172d406474fcde4fd6
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-09-22 16:30:14 -0400

    fix

--- utils/timer/adaptive_timeout_manager_test.go
+++ utils/timer/adaptive_timeout_manager_test.go
@@ -84,7 +84,7 @@ func TestAdaptiveTimeoutManagerInit(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		_, err := NewAdaptiveTimeoutManager(&test.config, "", prometheus.NewRegistry())
+		_, err := NewAdaptiveTimeoutManager(&test.config, "", prometheus.NewRegistry()) //nolint:gosec
 		require.ErrorIs(t, err, test.expectedErr)
 	}
 }
