3c55f2e965c65f2c83853bcf538a49c50c74b766
Dhruba Basu <7675102+dhrubabasu@users.noreply.github.com>
2023-09-26 18:46:34 -0400

    Place stricter bounds on warp codec (#900)
    * Place stricter bounds on warp codec
    
    * Update warp/payload/codec.go

--- warp/payload/codec.go
+++ warp/payload/codec.go
@@ -5,23 +5,31 @@ package payload
 
 import (
 	"errors"
-	"math"
 
 	"github.com/ava-labs/avalanchego/codec"
 	"github.com/ava-labs/avalanchego/codec/linearcodec"
+	"github.com/ava-labs/avalanchego/utils/units"
 	"github.com/ava-labs/avalanchego/utils/wrappers"
 )
 
 var errWrongType = errors.New("wrong payload type")
 
-const codecVersion = 0
+const (
+	codecVersion = 0
+
+	MaxMessageSize = 24 * units.KiB
+
+	// Note: Modifying this variable can have subtle implications on memory
+	// usage when parsing malformed payloads.
+	MaxSliceLen = 24 * units.KiB
+)
 
 // Codec does serialization and deserialization for Warp messages.
 var c codec.Manager
 
 func init() {
-	c = codec.NewManager(math.MaxInt)
-	lc := linearcodec.NewCustomMaxLength(math.MaxInt32)
+	c = codec.NewManager(MaxMessageSize)
+	lc := linearcodec.NewCustomMaxLength(MaxSliceLen)
 
 	errs := wrappers.Errs{}
 	errs.Add(
