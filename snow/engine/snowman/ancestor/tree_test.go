7c7c550a872fb4820f730793117396ecaa1016c2
Stephen Buttolph <stephen@avalabs.org>
2023-09-26 13:42:52 -0400

    cleanup tests

--- snow/engine/snowman/ancestor/tree_test.go
+++ snow/engine/snowman/ancestor/tree_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/set"
 )
 
 var (
@@ -16,166 +17,353 @@ var (
 	id2 = ids.GenerateTestID()
 	id3 = ids.GenerateTestID()
 	id4 = ids.GenerateTestID()
-	id5 = ids.GenerateTestID()
 )
 
 func TestAdd(t *testing.T) {
 	tests := map[string]struct {
-		method func(require *require.Assertions, at Tree)
+		initial  Tree
+		blkID    ids.ID
+		parentID ids.ID
+		expected Tree
 	}{
-		"should return self if not found": {
-			method: func(require *require.Assertions, at Tree) {
-				id := at.GetAncestor(id1)
-				require.Equal(id1, id)
+		"add to empty tree": {
+			initial: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
+			},
+			blkID:    id1,
+			parentID: id2,
+			expected: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
 			},
 		},
-		"should add to tree and return id2 root": {
-			method: func(require *require.Assertions, at Tree) {
-				require.Zero(at.Len())
-				at.Add(id1, id2)
-				require.Equal(1, at.Len())
-				require.True(at.Has(id1))
-				result := at.GetAncestor(id1)
-				require.Equal(id2, result)
+		"add new parent": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
+			},
+			blkID:    id3,
+			parentID: id4,
+			expected: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+					id3: id4,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+					id4: set.Of(id3),
+				},
 			},
 		},
-		"should return ancestor id3 through id2": {
-			method: func(require *require.Assertions, at Tree) {
-				require.Zero(at.Len())
-				at.Add(id1, id2)
-				require.Equal(1, at.Len())
-				at.Add(id2, id3)
-				require.Equal(2, at.Len())
-				require.True(at.Has(id2))
-				result := at.GetAncestor(id1)
-				require.Equal(id3, result)
+		"add new block to same parent": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
 			},
-		},
-		"should also return root id3 for another child": {
-			method: func(require *require.Assertions, at Tree) {
-				require.Zero(at.Len())
-				at.Add(id1, id2)
-				require.Equal(1, at.Len())
-				at.Add(id2, id3)
-				require.Equal(2, at.Len())
-				at.Add(id4, id2)
-				require.Equal(3, at.Len())
-				result := at.GetAncestor(id4)
-				require.Equal(id3, result)
+			blkID:    id3,
+			parentID: id2,
+			expected: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+					id3: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1, id3),
+				},
 			},
 		},
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			require := require.New(t)
-			at := NewTree()
-			test.method(require, at)
+			at := test.initial
+			at.Add(test.blkID, test.parentID)
+			require.Equal(t, test.expected, at)
 		})
 	}
 }
 
 func TestRemove(t *testing.T) {
 	tests := map[string]struct {
-		method func(require *require.Assertions, at Tree)
+		initial  Tree
+		blkID    ids.ID
+		expected Tree
 	}{
-		"removing root should not affect child roots": {
-			method: func(require *require.Assertions, at Tree) {
-				require.Zero(at.Len())
-				at.Add(id1, id2)
-				require.Equal(1, at.Len())
-				at.Add(id2, id3)
-				require.Equal(2, at.Len())
-				at.Remove(id3)
-				require.Equal(2, at.Len())
-				require.True(at.Has(id1))
-				require.True(at.Has(id2))
-				require.False(at.Has(id3))
-				id := at.GetAncestor(id2)
-				require.Equal(id3, id)
-				id = at.GetAncestor(id1)
-				require.Equal(id3, id)
+		"remove from empty tree": {
+			initial: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
+			},
+			blkID: id1,
+			expected: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
 			},
 		},
-		"removing parent should change root": {
-			method: func(require *require.Assertions, at Tree) {
-				require.Zero(at.Len())
-				at.Add(id1, id2)
-				require.Equal(1, at.Len())
-				at.Add(id2, id3)
-				require.Equal(2, at.Len())
-				at.Add(id3, id4)
-				require.Equal(3, at.Len())
-				id := at.GetAncestor(id1)
-				require.Equal(id4, id)
-				at.Remove(id3)
-				require.Equal(2, at.Len())
-				id = at.GetAncestor(id1)
-				require.Equal(id3, id)
+		"remove block and parent from tree": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
+			},
+			blkID: id1,
+			expected: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
+			},
+		},
+		"remove block and not parent from tree": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+					id3: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1, id3),
+				},
+			},
+			blkID: id1,
+			expected: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id3: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id3),
+				},
+			},
+		},
+		"remove untracked block from tree": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
+			},
+			blkID: id2,
+			expected: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
 			},
 		},
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			require := require.New(t)
-			at := NewTree()
-			test.method(require, at)
+			at := test.initial
+			at.Remove(test.blkID)
+			require.Equal(t, test.expected, at)
 		})
 	}
 }
 
 func TestRemoveDescendants(t *testing.T) {
 	tests := map[string]struct {
-		method func(require *require.Assertions, at Tree)
+		initial  Tree
+		blkID    ids.ID
+		expected Tree
 	}{
-		"remove root's descendants": {
-			method: func(require *require.Assertions, at Tree) {
-				require.Zero(at.Len())
-				at.Add(id1, id2)
-				require.Equal(1, at.Len())
-				at.Add(id2, id3)
-				require.Equal(2, at.Len())
-				at.RemoveDescendants(id3)
-				require.Zero(at.Len())
-				require.False(at.Has(id1))
-				require.False(at.Has(id2))
-				require.False(at.Has(id3))
-				id := at.GetAncestor(id2)
-				require.Equal(id2, id)
-				id = at.GetAncestor(id1)
-				require.Equal(id1, id)
+		"remove from empty tree": {
+			initial: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
+			},
+			blkID: id1,
+			expected: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
 			},
 		},
-		"remove descendants": {
-			method: func(require *require.Assertions, at Tree) {
-				require.Zero(at.Len())
-				at.Add(id1, id2)
-				require.Equal(1, at.Len())
-				at.Add(id2, id3)
-				require.Equal(2, at.Len())
-				at.Add(id3, id4)
-				require.Equal(3, at.Len())
-				at.Add(id4, id5)
-				require.Equal(4, at.Len())
-				at.RemoveDescendants(id3)
-				require.Equal(1, at.Len())
-				require.False(at.Has(id1))
-				require.False(at.Has(id2))
-				require.False(at.Has(id3))
-				id := at.GetAncestor(id1)
-				require.Equal(id, id1)
-				id = at.GetAncestor(id3)
-				require.Equal(id, id3)
-				require.True(at.Has(id4))
-				id = at.GetAncestor(id4)
-				require.Equal(id5, id)
+		"remove block and parent from tree": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
+			},
+			blkID: id1,
+			expected: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
+			},
+		},
+		"remove block and not parent from tree": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+					id3: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1, id3),
+				},
+			},
+			blkID: id1,
+			expected: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id3: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id3),
+				},
+			},
+		},
+		"remove untracked block from tree": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
+			},
+			blkID: id3,
+			expected: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
+			},
+		},
+		"remove child from tree": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+				},
+			},
+			blkID: id2,
+			expected: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
+			},
+		},
+		"remove children from tree": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+					id3: id2,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1, id3),
+				},
+			},
+			blkID: id2,
+			expected: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
+			},
+		},
+		"remove grand child from tree": {
+			initial: &tree{
+				childToParent: map[ids.ID]ids.ID{
+					id1: id2,
+					id2: id3,
+				},
+				parentToChildren: map[ids.ID]set.Set[ids.ID]{
+					id2: set.Of(id1),
+					id3: set.Of(id2),
+				},
+			},
+			blkID: id3,
+			expected: &tree{
+				childToParent:    make(map[ids.ID]ids.ID),
+				parentToChildren: make(map[ids.ID]set.Set[ids.ID]),
 			},
 		},
 	}
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			require := require.New(t)
-			at := NewTree()
-			test.method(require, at)
+			at := test.initial
+			at.RemoveDescendants(test.blkID)
+			require.Equal(t, test.expected, at)
 		})
 	}
+}
+
+func TestHas(t *testing.T) {
+	require := require.New(t)
+
+	at := NewTree()
+	require.False(at.Has(id1))
+	require.False(at.Has(id2))
+	require.False(at.Has(id3))
+
+	at.Add(id1, id2)
+	require.True(at.Has(id1))
+	require.False(at.Has(id2))
+	require.False(at.Has(id3))
+
+	at.Add(id2, id3)
+	require.True(at.Has(id1))
+	require.True(at.Has(id2))
+	require.False(at.Has(id3))
+}
+
+func TestGetAncestor(t *testing.T) {
+	require := require.New(t)
+
+	at := NewTree()
+	require.Equal(id1, at.GetAncestor(id1))
+	require.Equal(id2, at.GetAncestor(id2))
+	require.Equal(id3, at.GetAncestor(id3))
+	require.Equal(id4, at.GetAncestor(id4))
+
+	at.Add(id1, id2)
+	require.Equal(id2, at.GetAncestor(id1))
+	require.Equal(id2, at.GetAncestor(id2))
+	require.Equal(id3, at.GetAncestor(id3))
+	require.Equal(id4, at.GetAncestor(id4))
+
+	at.Add(id2, id3)
+	require.Equal(id3, at.GetAncestor(id1))
+	require.Equal(id3, at.GetAncestor(id2))
+	require.Equal(id3, at.GetAncestor(id3))
+	require.Equal(id4, at.GetAncestor(id4))
+
+	at.Add(id4, id3)
+	require.Equal(id3, at.GetAncestor(id1))
+	require.Equal(id3, at.GetAncestor(id2))
+	require.Equal(id3, at.GetAncestor(id3))
+	require.Equal(id3, at.GetAncestor(id4))
+}
+
+func TestLen(t *testing.T) {
+	require := require.New(t)
+
+	at := NewTree()
+	require.Zero(at.Len())
+
+	at.Add(id1, id2)
+	require.Equal(1, at.Len())
+
+	at.Add(id2, id3)
+	require.Equal(2, at.Len())
+
+	at.Add(id4, id3)
+	require.Equal(3, at.Len())
 }
