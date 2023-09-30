56103e26c9cf9abbb94cd379338b8af1e647d4f7
dboehm-avalabs <david.boehm@avalabs.org>
2023-09-29 20:52:56 -0400

    cleanup

--- x/merkledb/path.go
+++ x/merkledb/path.go
@@ -113,10 +113,12 @@ func (p Path) HasPrefix(prefix Path) bool {
 
 	// check that the tokens in the partially filled final byte of [prefix] are
 	// equal to the tokens in the final byte of [p].
-	for i := prefix.tokensLength - remainderTokens; i < prefix.tokensLength; i++ {
-		if p.Token(i) != prefix.Token(i) {
-			return false
-		}
+	remainderBitsMask := byte(0xFF << (8 - remainderTokens*int(p.tokenBitSize)))
+	partialLastPrefixByte := prefix.value[len(prefix.value)-1] & remainderBitsMask
+	partialLastByte := p.value[len(prefix.value)-1] & remainderBitsMask
+
+	if partialLastPrefixByte != partialLastByte {
+		return false
 	}
 
 	// Note that this will never be an index OOB because len(prefix.value) > 0.
@@ -203,11 +205,7 @@ func (p Path) bitsToShift(index int) byte {
 
 // bytesNeeded returns the number of bytes needed to store the passed number of tokens
 func (p Path) bytesNeeded(tokens int) int {
-	bytesNeeded := tokens / p.tokensPerByte
-	if tokens%p.tokensPerByte > 0 {
-		bytesNeeded++
-	}
-	return bytesNeeded
+	return (tokens + p.tokensPerByte - 1) / p.tokensPerByte
 }
 
 // Extend returns a new Path that equals the passed Path appended to the current Path
@@ -262,7 +260,7 @@ func (p Path) Extend(path Path) Path {
 func shiftCopy(dst []byte, src string, shift byte) {
 	i := 0
 	for ; i < len(src)-1; i++ {
-		dst[i] = src[i]<<shift + src[i+1]>>(8-shift)
+		dst[i] = src[i]<<shift | src[i+1]>>(8-shift)
 	}
 
 	if i < len(dst) {
@@ -324,7 +322,7 @@ func (p Path) Take(tokensToTake int) Path {
 	// We want to zero out everything to the right of the last token, which is at index [tokensToTake] - 1
 	// Mask will be (8-bitsToShift) number of 1's followed by (bitsToShift) number of 0's
 	mask := byte(0xFF << p.bitsToShift(tokensToTake-1))
-	buffer[len(buffer)-1] = buffer[len(buffer)-1] & mask
+	buffer[len(buffer)-1] &= mask
 
 	result.value = byteSliceToString(buffer)
 	return result
