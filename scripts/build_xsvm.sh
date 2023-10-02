3eae23a21cacb496354f6ab0ecd5cf34fb53b1fe
Maru Newby <maru.newby@avalabs.org>
2023-10-02 13:50:29 +0200

    fixup: Update xsvm build script to symlink plugin to ~/.avalanchego/plugins

--- scripts/build_xsvm.sh
+++ scripts/build_xsvm.sh
@@ -9,22 +9,11 @@ fi
 
 source ./scripts/constants.sh
 
-PLUGIN_FILENAME="v3m4wPxaHpvGr8qfMeyK6PRW3idZrPHmYcMTt7oXdK47yurVH"
+echo "Building xsvm plugin..."
+go build -o ./build/xsvm ./vms/example/xsvm/cmd/xsvm/
 
-# Set the PATHS
-GOPATH="$(go env GOPATH)"
-if [[ $# -eq 1 ]]; then
-    BINARY_DIRECTORY=$1
-elif [[ $# -eq 0 ]]; then
-    BINARY_DIRECTORY="${GOPATH}/src/github.com/ava-labs/avalanchego/build/plugins/${PLUGIN_FILENAME}"
-else
-    echo "Invalid arguments to build xsvm. Requires either no arguments (default) or one arguments to specify plugin location."
-    exit 1
-fi
-
-echo "Building xsvm plugin into ${BINARY_DIRECTORY}"
-go build -o "${BINARY_DIRECTORY}" ./vms/example/xsvm/cmd/xsvm/
-
-echo "Symlinking ${BINARY_DIRECTORY} to ./build/xsvm"
-mkdir -p build
-ln -sf "${BINARY_DIRECTORY}" ./build/xsvm
+PLUGIN_DIR="$HOME/.avalanchego/plugins"
+PLUGIN_PATH="${PLUGIN_DIR}/v3m4wPxaHpvGr8qfMeyK6PRW3idZrPHmYcMTt7oXdK47yurVH"
+echo "Symlinking ./build/xsvm to ${PLUGIN_PATH}"
+mkdir -p "${PLUGIN_DIR}"
+ln -sf "${PWD}/build/xsvm" "${PLUGIN_PATH}"
