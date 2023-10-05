5f9bb081204f8775e786b763d8f72d453dff1706
Stephen Buttolph <stephen@avalabs.org>
2023-10-05 15:55:31 -0400

    Add height voting for chits (#2102)
    Signed-off-by: Stephen Buttolph <stephen@avalabs.org>
    Co-authored-by: Dan Laine <daniel.laine@avalabs.org>

--- snow/networking/sender/traced_sender.go
+++ snow/networking/sender/traced_sender.go
@@ -157,36 +157,39 @@ func (s *tracedSender) SendPut(ctx context.Context, nodeID ids.NodeID, requestID
 	s.sender.SendPut(ctx, nodeID, requestID, container)
 }
 
-func (s *tracedSender) SendPushQuery(ctx context.Context, nodeIDs set.Set[ids.NodeID], requestID uint32, container []byte) {
+func (s *tracedSender) SendPushQuery(ctx context.Context, nodeIDs set.Set[ids.NodeID], requestID uint32, container []byte, requestedHeight uint64) {
 	ctx, span := s.tracer.Start(ctx, "tracedSender.SendPushQuery", oteltrace.WithAttributes(
 		attribute.Int64("requestID", int64(requestID)),
 		attribute.Int("containerLen", len(container)),
+		attribute.Int64("requestedHeight", int64(requestedHeight)),
 	))
 	defer span.End()
 
-	s.sender.SendPushQuery(ctx, nodeIDs, requestID, container)
+	s.sender.SendPushQuery(ctx, nodeIDs, requestID, container, requestedHeight)
 }
 
-func (s *tracedSender) SendPullQuery(ctx context.Context, nodeIDs set.Set[ids.NodeID], requestID uint32, containerID ids.ID) {
+func (s *tracedSender) SendPullQuery(ctx context.Context, nodeIDs set.Set[ids.NodeID], requestID uint32, containerID ids.ID, requestedHeight uint64) {
 	ctx, span := s.tracer.Start(ctx, "tracedSender.SendPullQuery", oteltrace.WithAttributes(
 		attribute.Int64("requestID", int64(requestID)),
 		attribute.Stringer("containerID", containerID),
+		attribute.Int64("requestedHeight", int64(requestedHeight)),
 	))
 	defer span.End()
 
-	s.sender.SendPullQuery(ctx, nodeIDs, requestID, containerID)
+	s.sender.SendPullQuery(ctx, nodeIDs, requestID, containerID, requestedHeight)
 }
 
-func (s *tracedSender) SendChits(ctx context.Context, nodeID ids.NodeID, requestID uint32, preferredID ids.ID, acceptedID ids.ID) {
+func (s *tracedSender) SendChits(ctx context.Context, nodeID ids.NodeID, requestID uint32, preferredID ids.ID, preferredIDAtHeight ids.ID, acceptedID ids.ID) {
 	ctx, span := s.tracer.Start(ctx, "tracedSender.SendChits", oteltrace.WithAttributes(
 		attribute.Stringer("recipients", nodeID),
 		attribute.Int64("requestID", int64(requestID)),
 		attribute.Stringer("preferredID", preferredID),
+		attribute.Stringer("preferredIDAtHeight", preferredIDAtHeight),
 		attribute.Stringer("acceptedID", acceptedID),
 	))
 	defer span.End()
 
-	s.sender.SendChits(ctx, nodeID, requestID, preferredID, acceptedID)
+	s.sender.SendChits(ctx, nodeID, requestID, preferredID, preferredIDAtHeight, acceptedID)
 }
 
 func (s *tracedSender) SendCrossChainAppRequest(ctx context.Context, chainID ids.ID, requestID uint32, appRequestBytes []byte) error {
