4e4ffcd8128cbd2e60c416ede548ec42263a7a55
Dan Laine <daniel.laine@avalabs.org>
2023-10-16 12:33:01 -0400

    nits

--- x/merkledb/trie.go
+++ x/merkledb/trie.go
@@ -38,9 +38,8 @@ type ReadOnlyTrie interface {
 	// database.ErrNotFound if the key is not present
 	getValue(key Path) ([]byte, error)
 
-	// get the node with the given key path
-	// hasValue indicates which db to look in (value or intermediate)
-	// forChild indicates that the requested node is for a child view
+	// get the node with the given key.
+	// hasValue indicates which db to look in (value or intermediate).
 	getNode(key Path, hasValue bool) (*node, error)
 
 	// GetRangeProof returns a proof of up to [maxLength] key-value pairs with
