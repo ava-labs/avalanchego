14def095640c3e14830aa5e12fc2de265a3c5668
Dan Laine <daniel.laine@avalabs.org>
2023-10-19 11:57:33 -0400

    remove errant test

--- x/merkledb/history_test.go
+++ x/merkledb/history_test.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/ava-labs/avalanchego/database/memdb"
 	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/utils/crypto/bls"
 	"github.com/ava-labs/avalanchego/utils/maybe"
 )
 
@@ -780,20 +779,4 @@ func TestHistoryGetChangesToRoot(t *testing.T) {
 			tt.validateFunc(require, got)
 		})
 	}
-}
-
-func TestGenerateBLSSig(t *testing.T) {
-	require := require.New(t)
-
-	// Generate a key
-	key, err := bls.NewSecretKey()
-	require.NoError(err)
-
-	// Generate a message
-	message := []byte("hello world")
-
-	// Generate a signature
-	sig := bls.Sign(key, message)
-
-	t.Log("Signature:", sig)
 }
