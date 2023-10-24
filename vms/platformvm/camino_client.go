6d48010d5584b780f8a33e5d1acb1b71ba440fa0
Kleonikos Kyriakis <nikosgr8@gmail.com>
2023-10-24 09:21:35 +0300

    [Client] Add client getter for deposit offers (#279)

--- vms/platformvm/camino_client.go
+++ vms/platformvm/camino_client.go
@@ -16,6 +16,8 @@ type CaminoClient interface {
 
 	// GetMultisigAlias returns the alias definition of the given multisig address
 	GetMultisigAlias(ctx context.Context, multisigAddress string, options ...rpc.Option) (*GetMultisigAliasReply, error)
+
+	GetAllDepositOffers(ctx context.Context, getAllDepositOffersArgs *GetAllDepositOffersArgs, options ...rpc.Option) (*GetAllDepositOffersReply, error)
 }
 
 func (c *client) GetConfiguration(ctx context.Context, options ...rpc.Option) (*GetConfigurationReply, error) {
@@ -29,5 +31,11 @@ func (c *client) GetMultisigAlias(ctx context.Context, multisigAddress string, o
 	err := c.requester.SendRequest(ctx, "platform.getMultisigAlias", &api.JSONAddress{
 		Address: multisigAddress,
 	}, res, options...)
+	return res, err
+}
+
+func (c *client) GetAllDepositOffers(ctx context.Context, getAllDepositOffersArgs *GetAllDepositOffersArgs, options ...rpc.Option) (*GetAllDepositOffersReply, error) {
+	res := &GetAllDepositOffersReply{}
+	err := c.requester.SendRequest(ctx, "platform.getAllDepositOffers", &getAllDepositOffersArgs, res, options...)
 	return res, err
 }
