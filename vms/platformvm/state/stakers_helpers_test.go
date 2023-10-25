e88902c780870d36606d557c56f8e0709d406e8e
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-10-25 15:01:52 +0200

    fixed merge

--- vms/platformvm/state/stakers_helpers_test.go
+++ vms/platformvm/state/stakers_helpers_test.go
@@ -85,13 +85,10 @@ func buildStateCtx() *snow.Context {
 }
 
 func defaultConfig() *config.Config {
-	vdrs := validators.NewManager()
-	primaryVdrs := validators.NewSet()
-	_ = vdrs.Add(constants.PrimaryNetworkID, primaryVdrs)
 	return &config.Config{
 		Chains:                 chains.TestManager,
 		UptimeLockedCalculator: uptime.NewLockedCalculator(),
-		Validators:             vdrs,
+		Validators:             validators.NewManager(),
 		TxFee:                  defaultTxFee,
 		CreateSubnetTxFee:      100 * defaultTxFee,
 		CreateBlockchainTxFee:  100 * defaultTxFee,
