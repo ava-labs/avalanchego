54729aaf4e44de92948bbee159fa9c4250fa6529
Dan Laine <daniel.laine@avalabs.org>
2023-10-25 14:22:30 -0400

    refactor HasPrefix partial byte checking

--- x/merkledb/key.go
+++ x/merkledb/key.go
@@ -103,9 +103,9 @@ func (k Key) HasPrefix(prefix Key) bool {
 
 	// check that the tokens in the partially filled final byte of [prefix] are
 	// equal to the tokens in the final byte of [k].
-	remainderBitsMask := byte(0xFF << (8 - remainderBitCount))
-	prefixRemainderTokens := prefix.value[len(prefix.value)-1] & remainderBitsMask
-	remainderTokens := k.value[len(prefix.value)-1] & remainderBitsMask
+	remainderBitsMask := byte(0xFF >> remainderBitCount)
+	prefixRemainderTokens := prefix.value[len(prefix.value)-1] | remainderBitsMask
+	remainderTokens := k.value[len(prefix.value)-1] | remainderBitsMask
 
 	if prefixRemainderTokens != remainderTokens {
 		return false
