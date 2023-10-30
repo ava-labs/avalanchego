8a7867a612be548ea7c2e6c868520a1b5c9ac1f4
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-10-31 00:07:25 +0100

    moved up proposerVM block backfilling

--- vms/proposervm/state_syncable_vm.go
+++ vms/proposervm/state_syncable_vm.go
@@ -161,27 +161,10 @@ func (vm *VM) buildStateSummary(ctx context.Context, innerSummary block.StateSum
 	}, nil
 }
 
-func (vm *VM) BackfillBlocksEnabled(ctx context.Context) (ids.ID, error) {
-	if vm.ssVM == nil {
-		return ids.Empty, nil
-	}
-
-	return vm.ssVM.BackfillBlocksEnabled(ctx)
+func (*VM) BackfillBlocksEnabled(context.Context) (ids.ID, error) {
+	return ids.Empty, block.ErrBlockBackfillingNotEnabled
 }
 
-func (vm *VM) BackfillBlocks(ctx context.Context, blksBytes [][]byte) (ids.ID, error) {
-	innerBlksBytes := make([][]byte, 0, len(blksBytes))
-	for i, blkBytes := range blksBytes {
-		blk, err := vm.parseProposerBlock(ctx, blkBytes)
-		if err != nil {
-			return ids.Empty, fmt.Errorf("failed parsing backfilled block, index %d, %w", i, err)
-		}
-		// TODO: consider validating that block is at least below last accepted block
-		// or that block has not been stored yet
-		if err := blk.acceptOuterBlk(); err != nil {
-			return ids.Empty, fmt.Errorf("failed indexing backfilled block, index %d, %w", i, err)
-		}
-		innerBlksBytes = append(innerBlksBytes, blk.getInnerBlk().Bytes())
-	}
-	return vm.ssVM.BackfillBlocks(ctx, innerBlksBytes)
+func (*VM) BackfillBlocks(context.Context, [][]byte) (ids.ID, error) {
+	return ids.Empty, block.ErrStopBlockBackfilling
 }
