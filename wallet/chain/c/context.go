6c1a40a894e56da0ba1be0b848bfff14b67b44cc
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-10-30 08:28:32 +0100

    cut coreth dependency

--- wallet/chain/c/context.go
+++ wallet/chain/c/context.go
@@ -3,100 +3,100 @@
 
 package c
 
-import (
-	stdcontext "context"
+// import (
+// 	stdcontext "context"
 
-	"github.com/ava-labs/avalanchego/api/info"
-	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/snow"
-	"github.com/ava-labs/avalanchego/utils/constants"
-	"github.com/ava-labs/avalanchego/utils/logging"
-	"github.com/ava-labs/avalanchego/vms/avm"
-)
+// 	"github.com/ava-labs/avalanchego/api/info"
+// 	"github.com/ava-labs/avalanchego/ids"
+// 	"github.com/ava-labs/avalanchego/snow"
+// 	"github.com/ava-labs/avalanchego/utils/constants"
+// 	"github.com/ava-labs/avalanchego/utils/logging"
+// 	"github.com/ava-labs/avalanchego/vms/avm"
+// )
 
-const Alias = "C"
+// const Alias = "C"
 
-var _ Context = (*context)(nil)
+// var _ Context = (*context)(nil)
 
-type Context interface {
-	NetworkID() uint32
-	BlockchainID() ids.ID
-	AVAXAssetID() ids.ID
-}
+// type Context interface {
+// 	NetworkID() uint32
+// 	BlockchainID() ids.ID
+// 	AVAXAssetID() ids.ID
+// }
 
-type context struct {
-	networkID    uint32
-	blockchainID ids.ID
-	avaxAssetID  ids.ID
-}
+// type context struct {
+// 	networkID    uint32
+// 	blockchainID ids.ID
+// 	avaxAssetID  ids.ID
+// }
 
-func NewContextFromURI(ctx stdcontext.Context, uri string) (Context, error) {
-	infoClient := info.NewClient(uri)
-	xChainClient := avm.NewClient(uri, "X")
-	return NewContextFromClients(ctx, infoClient, xChainClient)
-}
+// func NewContextFromURI(ctx stdcontext.Context, uri string) (Context, error) {
+// 	infoClient := info.NewClient(uri)
+// 	xChainClient := avm.NewClient(uri, "X")
+// 	return NewContextFromClients(ctx, infoClient, xChainClient)
+// }
 
-func NewContextFromClients(
-	ctx stdcontext.Context,
-	infoClient info.Client,
-	xChainClient avm.Client,
-) (Context, error) {
-	networkID, err := infoClient.GetNetworkID(ctx)
-	if err != nil {
-		return nil, err
-	}
+// func NewContextFromClients(
+// 	ctx stdcontext.Context,
+// 	infoClient info.Client,
+// 	xChainClient avm.Client,
+// ) (Context, error) {
+// 	networkID, err := infoClient.GetNetworkID(ctx)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	chainID, err := infoClient.GetBlockchainID(ctx, Alias)
-	if err != nil {
-		return nil, err
-	}
+// chainID, err := infoClient.GetBlockchainID(ctx, Alias)
+// if err != nil {
+// 	return nil, err
+// }
 
-	asset, err := xChainClient.GetAssetDescription(ctx, "AVAX")
-	if err != nil {
-		return nil, err
-	}
+// 	asset, err := xChainClient.GetAssetDescription(ctx, "AVAX")
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return NewContext(
-		networkID,
-		chainID,
-		asset.AssetID,
-	), nil
-}
+// 	return NewContext(
+// 		networkID,
+// 		chainID,
+// 		asset.AssetID,
+// 	), nil
+// }
 
-func NewContext(
-	networkID uint32,
-	blockchainID ids.ID,
-	avaxAssetID ids.ID,
-) Context {
-	return &context{
-		networkID:    networkID,
-		blockchainID: blockchainID,
-		avaxAssetID:  avaxAssetID,
-	}
-}
+// func NewContext(
+// 	networkID uint32,
+// 	blockchainID ids.ID,
+// 	avaxAssetID ids.ID,
+// ) Context {
+// 	return &context{
+// 		networkID:    networkID,
+// 		blockchainID: blockchainID,
+// 		avaxAssetID:  avaxAssetID,
+// 	}
+// }
 
-func (c *context) NetworkID() uint32 {
-	return c.networkID
-}
+// func (c *context) NetworkID() uint32 {
+// 	return c.networkID
+// }
 
-func (c *context) BlockchainID() ids.ID {
-	return c.blockchainID
-}
+// func (c *context) BlockchainID() ids.ID {
+// 	return c.blockchainID
+// }
 
-func (c *context) AVAXAssetID() ids.ID {
-	return c.avaxAssetID
-}
+// func (c *context) AVAXAssetID() ids.ID {
+// 	return c.avaxAssetID
+// }
 
-func newSnowContext(c Context) (*snow.Context, error) {
-	chainID := c.BlockchainID()
-	lookup := ids.NewAliaser()
-	return &snow.Context{
-		NetworkID:   c.NetworkID(),
-		SubnetID:    constants.PrimaryNetworkID,
-		ChainID:     chainID,
-		CChainID:    chainID,
-		AVAXAssetID: c.AVAXAssetID(),
-		Log:         logging.NoLog{},
-		BCLookup:    lookup,
-	}, lookup.Alias(chainID, Alias)
-}
+// func newSnowContext(c Context) (*snow.Context, error) {
+// 	chainID := c.BlockchainID()
+// 	lookup := ids.NewAliaser()
+// 	return &snow.Context{
+// 		NetworkID:   c.NetworkID(),
+// 		SubnetID:    constants.PrimaryNetworkID,
+// 		ChainID:     chainID,
+// 		CChainID:    chainID,
+// 		AVAXAssetID: c.AVAXAssetID(),
+// 		Log:         logging.NoLog{},
+// 		BCLookup:    lookup,
+// 	}, lookup.Alias(chainID, Alias)
+// }
--- wallet/chain/c/wallet_with_options.go
+++ wallet/chain/c/wallet_with_options.go
@@ -3,80 +3,80 @@
 
 package c
 
-import (
-	"github.com/ava-labs/coreth/plugin/evm"
+// import (
+// 	"github.com/ava-labs/coreth/plugin/evm"
 
-	ethcommon "github.com/ethereum/go-ethereum/common"
+// 	ethcommon "github.com/ethereum/go-ethereum/common"
 
-	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
-	"github.com/ava-labs/avalanchego/wallet/subnet/primary/common"
-)
+// 	"github.com/ava-labs/avalanchego/ids"
+// 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
+// 	"github.com/ava-labs/avalanchego/wallet/subnet/primary/common"
+// )
 
-var _ Wallet = (*walletWithOptions)(nil)
+// var _ Wallet = (*walletWithOptions)(nil)
 
-func NewWalletWithOptions(
-	wallet Wallet,
-	options ...common.Option,
-) Wallet {
-	return &walletWithOptions{
-		Wallet:  wallet,
-		options: options,
-	}
-}
+// func NewWalletWithOptions(
+// 	wallet Wallet,
+// 	options ...common.Option,
+// ) Wallet {
+// 	return &walletWithOptions{
+// 		Wallet:  wallet,
+// 		options: options,
+// 	}
+// }
 
-type walletWithOptions struct {
-	Wallet
-	options []common.Option
-}
+// type walletWithOptions struct {
+// 	Wallet
+// 	options []common.Option
+// }
 
-func (w *walletWithOptions) Builder() Builder {
-	return NewBuilderWithOptions(
-		w.Wallet.Builder(),
-		w.options...,
-	)
-}
+// func (w *walletWithOptions) Builder() Builder {
+// 	return NewBuilderWithOptions(
+// 		w.Wallet.Builder(),
+// 		w.options...,
+// 	)
+// }
 
-func (w *walletWithOptions) IssueImportTx(
-	chainID ids.ID,
-	to ethcommon.Address,
-	options ...common.Option,
-) (*evm.Tx, error) {
-	return w.Wallet.IssueImportTx(
-		chainID,
-		to,
-		common.UnionOptions(w.options, options)...,
-	)
-}
+// func (w *walletWithOptions) IssueImportTx(
+// 	chainID ids.ID,
+// 	to ethcommon.Address,
+// 	options ...common.Option,
+// ) (*evm.Tx, error) {
+// 	return w.Wallet.IssueImportTx(
+// 		chainID,
+// 		to,
+// 		common.UnionOptions(w.options, options)...,
+// 	)
+// }
 
-func (w *walletWithOptions) IssueExportTx(
-	chainID ids.ID,
-	outputs []*secp256k1fx.TransferOutput,
-	options ...common.Option,
-) (*evm.Tx, error) {
-	return w.Wallet.IssueExportTx(
-		chainID,
-		outputs,
-		common.UnionOptions(w.options, options)...,
-	)
-}
+// func (w *walletWithOptions) IssueExportTx(
+// 	chainID ids.ID,
+// 	outputs []*secp256k1fx.TransferOutput,
+// 	options ...common.Option,
+// ) (*evm.Tx, error) {
+// 	return w.Wallet.IssueExportTx(
+// 		chainID,
+// 		outputs,
+// 		common.UnionOptions(w.options, options)...,
+// 	)
+// }
 
-func (w *walletWithOptions) IssueUnsignedAtomicTx(
-	utx evm.UnsignedAtomicTx,
-	options ...common.Option,
-) (*evm.Tx, error) {
-	return w.Wallet.IssueUnsignedAtomicTx(
-		utx,
-		common.UnionOptions(w.options, options)...,
-	)
-}
+// func (w *walletWithOptions) IssueUnsignedAtomicTx(
+// 	utx evm.UnsignedAtomicTx,
+// 	options ...common.Option,
+// ) (*evm.Tx, error) {
+// 	return w.Wallet.IssueUnsignedAtomicTx(
+// 		utx,
+// 		common.UnionOptions(w.options, options)...,
+// 	)
+// }
 
-func (w *walletWithOptions) IssueAtomicTx(
-	tx *evm.Tx,
-	options ...common.Option,
-) error {
-	return w.Wallet.IssueAtomicTx(
-		tx,
-		common.UnionOptions(w.options, options)...,
-	)
-}
+// func (w *walletWithOptions) IssueAtomicTx(
+// 	tx *evm.Tx,
+// 	options ...common.Option,
+// ) error {
+// 	return w.Wallet.IssueAtomicTx(
+// 		tx,
+// 		common.UnionOptions(w.options, options)...,
+// 	)
+// }
