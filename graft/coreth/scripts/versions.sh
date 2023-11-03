b72e71c35ab4a5731b4de605bde96f69bdda9481
aaronbuchwald <aaron.buchwald56@gmail.com>
2023-11-03 12:04:45 -0400

    Start v0.12.8 release (#374)

--- graft/coreth/scripts/versions.sh
+++ graft/coreth/scripts/versions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.10.15-rc.1'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.10.15'}
