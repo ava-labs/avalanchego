2102b0e102b1de8348e4d36174c156eaee138dad
Dan Laine <daniel.laine@avalabs.org>
2023-11-03 15:47:58 -0400

    remove atomic root

--- vms/platformvm/sync/state_summary.go
+++ vms/platformvm/sync/state_summary.go
@@ -22,7 +22,6 @@ type SyncSummary struct {
 	BlockNumber uint64 `serialize:"true"`
 	BlockHash   ids.ID `serialize:"true"`
 	BlockRoot   ids.ID `serialize:"true"`
-	AtomicRoot  ids.ID `serialize:"true"`
 
 	// Invariant: non-nil.
 	summaryID ids.ID
@@ -50,12 +49,11 @@ func NewSyncSummaryFromBytes(summaryBytes []byte, acceptImpl func(SyncSummary) (
 	return summary, nil
 }
 
-func NewSyncSummary(blockHash ids.ID, blockNumber uint64, blockRoot ids.ID, atomicRoot ids.ID) (SyncSummary, error) {
+func NewSyncSummary(blockHash ids.ID, blockNumber uint64, blockRoot ids.ID) (SyncSummary, error) {
 	summary := SyncSummary{
 		BlockNumber: blockNumber,
 		BlockHash:   blockHash,
 		BlockRoot:   blockRoot,
-		AtomicRoot:  atomicRoot,
 	}
 	bytes, err := Codec.Marshal(Version, &summary)
 	if err != nil {
@@ -86,8 +84,8 @@ func (s SyncSummary) ID() ids.ID {
 
 func (s SyncSummary) String() string {
 	return fmt.Sprintf(
-		"SyncSummary(BlockHash=%s, BlockNumber=%d, BlockRoot=%s, AtomicRoot=%s)",
-		s.BlockHash, s.BlockNumber, s.BlockRoot, s.AtomicRoot,
+		"SyncSummary(BlockHash=%s, BlockNumber=%d, BlockRoot=%s)",
+		s.BlockHash, s.BlockNumber, s.BlockRoot,
 	)
 }
 
