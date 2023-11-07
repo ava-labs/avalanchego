4cad429a35c9e8bd74c51b4af69349b1f5fa1a3b
Dan Laine <daniel.laine@avalabs.org>
2023-11-07 15:41:56 -0500

    nit

--- config/config.go
+++ config/config.go
@@ -928,8 +928,8 @@ func getDatabaseConfig(v *viper.Viper, networkID uint32) (node.DatabaseConfig, e
 	}
 
 	return node.DatabaseConfig{
-		ReadOnly: v.GetBool(DBReadOnlyKey),
 		Name:     v.GetString(DBTypeKey),
+		ReadOnly: v.GetBool(DBReadOnlyKey),
 		Path: filepath.Join(
 			GetExpandedArg(v, DBPathKey),
 			constants.NetworkName(networkID),
