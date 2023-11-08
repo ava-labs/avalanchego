9ccb7ecf71cf22133272688de9ea2476ff2eba9e
Maru Newby <maru.newby@avalabs.org>
2023-11-08 17:04:03 +0100

    Remove unnecessary e2e.go file

--- tests/e2e/e2e.go
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2019-2023, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-package e2e
-
-// Dummy file to satisfy golang compilation requirement
