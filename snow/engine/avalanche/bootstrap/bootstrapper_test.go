92baae21ec5fdba8270c0ea16a04b3d472be1fae
Stephen Buttolph <stephen@avalabs.org>
2023-11-09 19:46:18 -0500

    cleanup tests

--- snow/engine/avalanche/bootstrap/bootstrapper_test.go
+++ snow/engine/avalanche/bootstrap/bootstrapper_test.go
@@ -12,6 +12,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"golang.org/x/exp/maps"
+
 	"github.com/ava-labs/avalanchego/database/memdb"
 	"github.com/ava-labs/avalanchego/database/prefixdb"
 	"github.com/ava-labs/avalanchego/ids"
@@ -619,464 +621,110 @@ func TestBootstrapperIncompleteAncestors(t *testing.T) {
 	require.Equal(choices.Accepted, vtx2.Status())
 }
 
-// func TestBootstrapperFinalized(t *testing.T) {
-// 	require := require.New(t)
+func TestBootstrapperUnexpectedVertex(t *testing.T) {
+	require := require.New(t)
 
-// 	config, peerID, sender, manager, vm := newConfig(t)
+	config, peerID, sender, manager, vm := newConfig(t)
 
-// 	vtxID0 := ids.Empty.Prefix(0)
-// 	vtxID1 := ids.Empty.Prefix(1)
+	vtxID0 := ids.Empty.Prefix(0)
+	vtxID1 := ids.Empty.Prefix(1)
 
-// 	vtxBytes0 := []byte{0}
-// 	vtxBytes1 := []byte{1}
+	vtxBytes0 := []byte{0}
+	vtxBytes1 := []byte{1}
 
-// 	vtx0 := &avalanche.TestVertex{
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID0,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		HeightV: 0,
-// 		BytesV:  vtxBytes0,
-// 	}
-// 	vtx1 := &avalanche.TestVertex{ // vtx1 is the stop vertex
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID1,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		ParentsV: []avalanche.Vertex{vtx0},
-// 		HeightV:  1,
-// 		BytesV:   vtxBytes1,
-// 	}
+	vtx0 := &avalanche.TestVertex{
+		TestDecidable: choices.TestDecidable{
+			IDV:     vtxID0,
+			StatusV: choices.Unknown,
+		},
+		HeightV: 0,
+		BytesV:  vtxBytes0,
+	}
+	vtx1 := &avalanche.TestVertex{ // vtx1 is the stop vertex
+		TestDecidable: choices.TestDecidable{
+			IDV:     vtxID1,
+			StatusV: choices.Unknown,
+		},
+		ParentsV: []avalanche.Vertex{vtx0},
+		HeightV:  1,
+		BytesV:   vtxBytes1,
+	}
 
-// 	bs, err := New(
-// 		config,
-// 		func(context.Context, uint32) error {
-// 			config.Ctx.State.Set(snow.EngineState{
-// 				Type:  p2p.EngineType_ENGINE_TYPE_AVALANCHE,
-// 				State: snow.NormalOp,
-// 			})
-// 			return nil
-// 		},
-// 	)
-// 	require.NoError(err)
+	config.StopVertexID = vtxID1
+	bs, err := New(
+		config,
+		func(context.Context, uint32) error {
+			config.Ctx.State.Set(snow.EngineState{
+				Type:  p2p.EngineType_ENGINE_TYPE_AVALANCHE,
+				State: snow.NormalOp,
+			})
+			return nil
+		},
+	)
+	require.NoError(err)
 
-// 	vm.CantSetState = false
-// 	require.NoError(bs.Start(context.Background(), 0))
+	parsedVtx0 := false
+	parsedVtx1 := false
+	manager.GetVtxF = func(_ context.Context, vtxID ids.ID) (avalanche.Vertex, error) {
+		switch vtxID {
+		case vtxID0:
+			if parsedVtx0 {
+				return vtx0, nil
+			}
+			return nil, errUnknownVertex
+		case vtxID1:
+			if parsedVtx1 {
+				return vtx1, nil
+			}
+			return nil, errUnknownVertex
+		default:
+			require.FailNow(errUnknownVertex.Error())
+			return nil, errUnknownVertex
+		}
+	}
+	manager.ParseVtxF = func(_ context.Context, vtxBytes []byte) (avalanche.Vertex, error) {
+		switch {
+		case bytes.Equal(vtxBytes, vtxBytes0):
+			vtx0.StatusV = choices.Processing
+			parsedVtx0 = true
+			return vtx0, nil
+		case bytes.Equal(vtxBytes, vtxBytes1):
+			vtx1.StatusV = choices.Processing
+			parsedVtx1 = true
+			return vtx1, nil
+		default:
+			require.FailNow(errUnknownVertex.Error())
+			return nil, errUnknownVertex
+		}
+	}
 
-// 	acceptedIDs := []ids.ID{vtxID1}
-// 	parsedVtx0 := false
-// 	parsedVtx1 := false
-// 	manager.GetVtxF = func(_ context.Context, vtxID ids.ID) (avalanche.Vertex, error) {
-// 		switch vtxID {
-// 		case vtxID0:
-// 			if parsedVtx0 {
-// 				return vtx0, nil
-// 			}
-// 			return nil, errUnknownVertex
-// 		case vtxID1:
-// 			if parsedVtx1 {
-// 				return vtx1, nil
-// 			}
-// 			return nil, errUnknownVertex
-// 		default:
-// 			require.FailNow(errUnknownVertex.Error())
-// 			return nil, errUnknownVertex
-// 		}
-// 	}
-// 	manager.ParseVtxF = func(_ context.Context, vtxBytes []byte) (avalanche.Vertex, error) {
-// 		switch {
-// 		case bytes.Equal(vtxBytes, vtxBytes0):
-// 			vtx0.StatusV = choices.Processing
-// 			parsedVtx0 = true
-// 			return vtx0, nil
-// 		case bytes.Equal(vtxBytes, vtxBytes1):
-// 			vtx1.StatusV = choices.Processing
-// 			parsedVtx1 = true
-// 			return vtx1, nil
-// 		default:
-// 			require.FailNow(errUnknownVertex.Error())
-// 			return nil, errUnknownVertex
-// 		}
-// 	}
+	requestIDs := map[ids.ID]uint32{}
+	sender.SendGetAncestorsF = func(_ context.Context, vdr ids.NodeID, reqID uint32, vtxID ids.ID) {
+		require.Equal(peerID, vdr)
+		requestIDs[vtxID] = reqID
+	}
 
-// 	requestIDs := map[ids.ID]uint32{}
-// 	sender.SendGetAncestorsF = func(_ context.Context, vdr ids.NodeID, reqID uint32, vtxID ids.ID) {
-// 		require.Equal(peerID, vdr)
+	vm.CantSetState = false
+	require.NoError(bs.Start(context.Background(), 0)) // should request vtx1
+	require.Contains(requestIDs, vtxID1)
 
-// 		requestIDs[vtxID] = reqID
-// 	}
+	reqID := requestIDs[vtxID1]
+	maps.Clear(requestIDs)
+	require.NoError(bs.Ancestors(context.Background(), peerID, reqID, [][]byte{vtxBytes0}))
+	require.Contains(requestIDs, vtxID1)
 
-// 	require.NoError(bs.ForceAccepted(context.Background(), acceptedIDs)) // should request vtx0 and vtx1
-// 	require.Contains(requestIDs, vtxID1)
+	manager.EdgeF = func(context.Context) []ids.ID {
+		require.Equal(choices.Accepted, vtx1.Status())
+		return []ids.ID{vtxID1}
+	}
 
-// 	reqID := requestIDs[vtxID1]
-// 	require.NoError(bs.Ancestors(context.Background(), peerID, reqID, [][]byte{vtxBytes1, vtxBytes0}))
-// 	require.Contains(requestIDs, vtxID0)
+	vm.LinearizeF = func(_ context.Context, stopVertexID ids.ID) error {
+		require.Equal(vtxID1, stopVertexID)
+		return nil
+	}
 
-// 	manager.StopVertexAcceptedF = func(context.Context) (bool, error) {
-// 		return vtx1.Status() == choices.Accepted, nil
-// 	}
-
-// 	manager.EdgeF = func(context.Context) []ids.ID {
-// 		require.Equal(choices.Accepted, vtx1.Status())
-// 		return []ids.ID{vtxID1}
-// 	}
-
-// 	vm.LinearizeF = func(_ context.Context, stopVertexID ids.ID) error {
-// 		require.Equal(vtxID1, stopVertexID)
-// 		return nil
-// 	}
-
-// 	reqID = requestIDs[vtxID0]
-// 	require.NoError(bs.GetAncestorsFailed(context.Background(), peerID, reqID))
-// 	require.Equal(snow.NormalOp, config.Ctx.State.Get().State)
-// 	require.Equal(choices.Accepted, vtx0.Status())
-// 	require.Equal(choices.Accepted, vtx1.Status())
-// }
-
-// // Test that Ancestors accepts the parents of the first vertex returned
-// func TestBootstrapperAcceptsAncestorsParents(t *testing.T) {
-// 	require := require.New(t)
-
-// 	config, peerID, sender, manager, vm := newConfig(t)
-
-// 	vtxID0 := ids.Empty.Prefix(0)
-// 	vtxID1 := ids.Empty.Prefix(1)
-// 	vtxID2 := ids.Empty.Prefix(2)
-
-// 	vtxBytes0 := []byte{0}
-// 	vtxBytes1 := []byte{1}
-// 	vtxBytes2 := []byte{2}
-
-// 	vtx0 := &avalanche.TestVertex{
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID0,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		HeightV: 0,
-// 		BytesV:  vtxBytes0,
-// 	}
-// 	vtx1 := &avalanche.TestVertex{
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID1,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		ParentsV: []avalanche.Vertex{vtx0},
-// 		HeightV:  1,
-// 		BytesV:   vtxBytes1,
-// 	}
-// 	vtx2 := &avalanche.TestVertex{ // vtx2 is the stop vertex
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID2,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		ParentsV: []avalanche.Vertex{vtx1},
-// 		HeightV:  2,
-// 		BytesV:   vtxBytes2,
-// 	}
-
-// 	bs, err := New(
-// 		config,
-// 		func(context.Context, uint32) error {
-// 			config.Ctx.State.Set(snow.EngineState{
-// 				Type:  p2p.EngineType_ENGINE_TYPE_AVALANCHE,
-// 				State: snow.NormalOp,
-// 			})
-// 			return nil
-// 		},
-// 	)
-// 	require.NoError(err)
-
-// 	vm.CantSetState = false
-// 	require.NoError(bs.Start(context.Background(), 0))
-
-// 	acceptedIDs := []ids.ID{vtxID2}
-// 	parsedVtx0 := false
-// 	parsedVtx1 := false
-// 	parsedVtx2 := false
-// 	manager.GetVtxF = func(_ context.Context, vtxID ids.ID) (avalanche.Vertex, error) {
-// 		switch vtxID {
-// 		case vtxID0:
-// 			if parsedVtx0 {
-// 				return vtx0, nil
-// 			}
-// 		case vtxID1:
-// 			if parsedVtx1 {
-// 				return vtx1, nil
-// 			}
-// 		case vtxID2:
-// 			if parsedVtx2 {
-// 				return vtx2, nil
-// 			}
-// 		default:
-// 			require.FailNow(errUnknownVertex.Error())
-// 			return nil, errUnknownVertex
-// 		}
-// 		return nil, errUnknownVertex
-// 	}
-// 	manager.ParseVtxF = func(_ context.Context, vtxBytes []byte) (avalanche.Vertex, error) {
-// 		switch {
-// 		case bytes.Equal(vtxBytes, vtxBytes0):
-// 			vtx0.StatusV = choices.Processing
-// 			parsedVtx0 = true
-// 			return vtx0, nil
-// 		case bytes.Equal(vtxBytes, vtxBytes1):
-// 			vtx1.StatusV = choices.Processing
-// 			parsedVtx1 = true
-// 			return vtx1, nil
-// 		case bytes.Equal(vtxBytes, vtxBytes2):
-// 			vtx2.StatusV = choices.Processing
-// 			parsedVtx2 = true
-// 			return vtx2, nil
-// 		default:
-// 			require.FailNow(errUnknownVertex.Error())
-// 			return nil, errUnknownVertex
-// 		}
-// 	}
-
-// 	requestIDs := map[ids.ID]uint32{}
-// 	sender.SendGetAncestorsF = func(_ context.Context, vdr ids.NodeID, reqID uint32, vtxID ids.ID) {
-// 		require.Equal(peerID, vdr)
-
-// 		requestIDs[vtxID] = reqID
-// 	}
-
-// 	require.NoError(bs.ForceAccepted(context.Background(), acceptedIDs)) // should request vtx2
-// 	require.Contains(requestIDs, vtxID2)
-
-// 	manager.StopVertexAcceptedF = func(context.Context) (bool, error) {
-// 		return vtx2.Status() == choices.Accepted, nil
-// 	}
-
-// 	manager.EdgeF = func(context.Context) []ids.ID {
-// 		require.Equal(choices.Accepted, vtx2.Status())
-// 		return []ids.ID{vtxID2}
-// 	}
-
-// 	vm.LinearizeF = func(_ context.Context, stopVertexID ids.ID) error {
-// 		require.Equal(vtxID2, stopVertexID)
-// 		return nil
-// 	}
-
-// 	reqID := requestIDs[vtxID2]
-// 	require.NoError(bs.Ancestors(context.Background(), peerID, reqID, [][]byte{vtxBytes2, vtxBytes1, vtxBytes0}))
-// 	require.Equal(snow.NormalOp, config.Ctx.State.Get().State)
-// 	require.Equal(choices.Accepted, vtx0.Status())
-// 	require.Equal(choices.Accepted, vtx1.Status())
-// 	require.Equal(choices.Accepted, vtx2.Status())
-// }
-
-// func TestRestartBootstrapping(t *testing.T) {
-// 	require := require.New(t)
-
-// 	config, peerID, sender, manager, vm := newConfig(t)
-
-// 	vtxID0 := ids.GenerateTestID()
-// 	vtxID1 := ids.GenerateTestID()
-// 	vtxID2 := ids.GenerateTestID()
-// 	vtxID3 := ids.GenerateTestID()
-// 	vtxID4 := ids.GenerateTestID()
-// 	vtxID5 := ids.GenerateTestID()
-
-// 	vtxBytes0 := []byte{0}
-// 	vtxBytes1 := []byte{1}
-// 	vtxBytes2 := []byte{2}
-// 	vtxBytes3 := []byte{3}
-// 	vtxBytes4 := []byte{4}
-// 	vtxBytes5 := []byte{5}
-
-// 	vtx0 := &avalanche.TestVertex{
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID0,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		HeightV: 0,
-// 		BytesV:  vtxBytes0,
-// 	}
-// 	vtx1 := &avalanche.TestVertex{
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID1,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		ParentsV: []avalanche.Vertex{vtx0},
-// 		HeightV:  1,
-// 		BytesV:   vtxBytes1,
-// 	}
-// 	vtx2 := &avalanche.TestVertex{
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID2,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		ParentsV: []avalanche.Vertex{vtx1},
-// 		HeightV:  2,
-// 		BytesV:   vtxBytes2,
-// 	}
-// 	vtx3 := &avalanche.TestVertex{
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID3,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		ParentsV: []avalanche.Vertex{vtx2},
-// 		HeightV:  3,
-// 		BytesV:   vtxBytes3,
-// 	}
-// 	vtx4 := &avalanche.TestVertex{
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID4,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		ParentsV: []avalanche.Vertex{vtx2},
-// 		HeightV:  3,
-// 		BytesV:   vtxBytes4,
-// 	}
-// 	vtx5 := &avalanche.TestVertex{ // vtx5 is the stop vertex
-// 		TestDecidable: choices.TestDecidable{
-// 			IDV:     vtxID5,
-// 			StatusV: choices.Unknown,
-// 		},
-// 		ParentsV: []avalanche.Vertex{vtx3, vtx4},
-// 		HeightV:  4,
-// 		BytesV:   vtxBytes5,
-// 	}
-
-// 	bsIntf, err := New(
-// 		config,
-// 		func(context.Context, uint32) error {
-// 			config.Ctx.State.Set(snow.EngineState{
-// 				Type:  p2p.EngineType_ENGINE_TYPE_AVALANCHE,
-// 				State: snow.NormalOp,
-// 			})
-// 			return nil
-// 		},
-// 	)
-// 	require.NoError(err)
-
-// 	bs := bsIntf.(*bootstrapper)
-
-// 	vm.CantSetState = false
-// 	require.NoError(bs.Start(context.Background(), 0))
-
-// 	parsedVtx0 := false
-// 	parsedVtx1 := false
-// 	parsedVtx2 := false
-// 	parsedVtx3 := false
-// 	parsedVtx4 := false
-// 	parsedVtx5 := false
-// 	manager.GetVtxF = func(_ context.Context, vtxID ids.ID) (avalanche.Vertex, error) {
-// 		switch vtxID {
-// 		case vtxID0:
-// 			if parsedVtx0 {
-// 				return vtx0, nil
-// 			}
-// 		case vtxID1:
-// 			if parsedVtx1 {
-// 				return vtx1, nil
-// 			}
-// 		case vtxID2:
-// 			if parsedVtx2 {
-// 				return vtx2, nil
-// 			}
-// 		case vtxID3:
-// 			if parsedVtx3 {
-// 				return vtx3, nil
-// 			}
-// 		case vtxID4:
-// 			if parsedVtx4 {
-// 				return vtx4, nil
-// 			}
-// 		case vtxID5:
-// 			if parsedVtx5 {
-// 				return vtx5, nil
-// 			}
-// 		default:
-// 			require.FailNow(errUnknownVertex.Error())
-// 			return nil, errUnknownVertex
-// 		}
-// 		return nil, errUnknownVertex
-// 	}
-// 	manager.ParseVtxF = func(_ context.Context, vtxBytes []byte) (avalanche.Vertex, error) {
-// 		switch {
-// 		case bytes.Equal(vtxBytes, vtxBytes0):
-// 			vtx0.StatusV = choices.Processing
-// 			parsedVtx0 = true
-// 			return vtx0, nil
-// 		case bytes.Equal(vtxBytes, vtxBytes1):
-// 			vtx1.StatusV = choices.Processing
-// 			parsedVtx1 = true
-// 			return vtx1, nil
-// 		case bytes.Equal(vtxBytes, vtxBytes2):
-// 			vtx2.StatusV = choices.Processing
-// 			parsedVtx2 = true
-// 			return vtx2, nil
-// 		case bytes.Equal(vtxBytes, vtxBytes3):
-// 			vtx3.StatusV = choices.Processing
-// 			parsedVtx3 = true
-// 			return vtx3, nil
-// 		case bytes.Equal(vtxBytes, vtxBytes4):
-// 			vtx4.StatusV = choices.Processing
-// 			parsedVtx4 = true
-// 			return vtx4, nil
-// 		case bytes.Equal(vtxBytes, vtxBytes5):
-// 			vtx5.StatusV = choices.Processing
-// 			parsedVtx5 = true
-// 			return vtx5, nil
-// 		default:
-// 			require.FailNow(errUnknownVertex.Error())
-// 			return nil, errUnknownVertex
-// 		}
-// 	}
-
-// 	requestIDs := map[ids.ID]uint32{}
-// 	sender.SendGetAncestorsF = func(_ context.Context, vdr ids.NodeID, reqID uint32, vtxID ids.ID) {
-// 		require.Equal(peerID, vdr)
-
-// 		requestIDs[vtxID] = reqID
-// 	}
-
-// 	require.NoError(bs.ForceAccepted(context.Background(), []ids.ID{vtxID3, vtxID4})) // should request vtx3 and vtx4
-// 	require.Contains(requestIDs, vtxID3)
-// 	require.Contains(requestIDs, vtxID4)
-
-// 	vtx3ReqID := requestIDs[vtxID3]
-// 	require.NoError(bs.Ancestors(context.Background(), peerID, vtx3ReqID, [][]byte{vtxBytes3, vtxBytes2}))
-// 	require.Contains(requestIDs, vtxID1)
-// 	require.True(bs.OutstandingRequests.RemoveAny(vtxID4))
-// 	require.True(bs.OutstandingRequests.RemoveAny(vtxID1))
-
-// 	bs.needToFetch.Clear()
-// 	requestIDs = map[ids.ID]uint32{}
-
-// 	require.NoError(bs.ForceAccepted(context.Background(), []ids.ID{vtxID5, vtxID3}))
-// 	require.Contains(requestIDs, vtxID1)
-// 	require.Contains(requestIDs, vtxID4)
-// 	require.Contains(requestIDs, vtxID5)
-// 	require.NotContains(requestIDs, vtxID3)
-
-// 	vtx5ReqID := requestIDs[vtxID5]
-// 	require.NoError(bs.Ancestors(context.Background(), peerID, vtx5ReqID, [][]byte{vtxBytes5, vtxBytes4, vtxBytes2, vtxBytes1}))
-// 	require.Contains(requestIDs, vtxID0)
-
-// 	manager.StopVertexAcceptedF = func(context.Context) (bool, error) {
-// 		return vtx5.Status() == choices.Accepted, nil
-// 	}
-
-// 	manager.EdgeF = func(context.Context) []ids.ID {
-// 		require.Equal(choices.Accepted, vtx5.Status())
-// 		return []ids.ID{vtxID5}
-// 	}
-
-// 	vm.LinearizeF = func(_ context.Context, stopVertexID ids.ID) error {
-// 		require.Equal(vtxID5, stopVertexID)
-// 		return nil
-// 	}
-
-// 	vtx1ReqID := requestIDs[vtxID1]
-// 	require.NoError(bs.Ancestors(context.Background(), peerID, vtx1ReqID, [][]byte{vtxBytes1, vtxBytes0}))
-// 	require.Equal(snow.NormalOp, config.Ctx.State.Get().State)
-// 	require.Equal(choices.Accepted, vtx0.Status())
-// 	require.Equal(choices.Accepted, vtx1.Status())
-// 	require.Equal(choices.Accepted, vtx2.Status())
-// 	require.Equal(choices.Accepted, vtx3.Status())
-// 	require.Equal(choices.Accepted, vtx4.Status())
-// 	require.Equal(choices.Accepted, vtx5.Status())
-// }
+	require.NoError(bs.Ancestors(context.Background(), peerID, reqID, [][]byte{vtxBytes1, vtxBytes0}))
+	require.Equal(choices.Accepted, vtx0.Status())
+	require.Equal(choices.Accepted, vtx1.Status())
+	require.Equal(snow.NormalOp, config.Ctx.State.Get().State)
+}
