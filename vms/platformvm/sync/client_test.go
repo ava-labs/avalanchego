26fba018c4fe4b58d0d7fcc41bd8c265a286a4ed
Dan Laine <daniel.laine@avalabs.org>
2023-11-14 12:21:31 -0500

    update TestNewClient

--- vms/platformvm/sync/client_test.go
+++ vms/platformvm/sync/client_test.go
@@ -14,17 +14,29 @@ import (
 
 func TestNewClient(t *testing.T) {
 	var (
-		require    = require.New(t)
-		metadataDB = memdb.New()
-		config     = ClientConfig{
-			ManagerConfig: xsync.ManagerConfig{},
+		require       = require.New(t)
+		metadataDB    = memdb.New()
+		managerConfig = xsync.ManagerConfig{
+			SimultaneousWorkLimit: 1,
+		}
+		onDoneCalled bool
+		onDone       = func(error) { onDoneCalled = true }
+		config       = ClientConfig{
+			ManagerConfig: managerConfig,
 			Enabled:       true,
-			OnDone:        func(error) {},
+			OnDone:        onDone,
 		}
 		client = NewClient(config, metadataDB)
 	)
 
 	require.NotNil(client)
 	require.NotNil(config, client.config)
+	require.Equal(config.ManagerConfig, managerConfig)
+	require.Equal(config.Enabled, client.config.Enabled)
 	require.Equal(metadataDB, client.metadataDB)
+
+	// Can't use reflect.Equal to test function equality
+	// so do this instead.
+	client.config.OnDone(nil)
+	require.True(onDoneCalled)
 }
