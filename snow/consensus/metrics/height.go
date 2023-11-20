6ba4e25f5fd74073d7e0a71ccf9885a1096466c4
Stephen Buttolph <stephen@avalabs.org>
2023-11-20 14:41:03 -0500

    Improve height monitoring

--- snow/consensus/metrics/height.go
+++ snow/consensus/metrics/height.go
@@ -3,29 +3,50 @@
 
 package metrics
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/ava-labs/avalanchego/utils"
+	"github.com/ava-labs/avalanchego/utils/math"
+)
 
 var _ Height = (*height)(nil)
 
-// Height reports the last accepted height
 type Height interface {
+	Verified(height uint64)
 	Accepted(height uint64)
 }
 
 type height struct {
-	// lastAcceptedHeight keeps track of the last accepted height
+	currentHighestVerifiedHeight uint64
+	highestVerifiedHeight        prometheus.Gauge
+
 	lastAcceptedHeight prometheus.Gauge
 }
 
 func NewHeight(namespace string, reg prometheus.Registerer) (Height, error) {
 	h := &height{
+		highestVerifiedHeight: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "highest_verified_height",
+			Help:      "Highest verified height",
+		}),
 		lastAcceptedHeight: prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: namespace,
 			Name:      "last_accepted_height",
 			Help:      "Last height accepted",
 		}),
 	}
-	return h, reg.Register(h.lastAcceptedHeight)
+	err := utils.Err(
+		reg.Register(h.lastAcceptedHeight),
+		reg.Register(h.highestVerifiedHeight),
+	)
+	return h, err
+}
+
+func (h *height) Verified(height uint64) {
+	h.currentHighestVerifiedHeight = math.Max(h.currentHighestVerifiedHeight, height)
+	h.highestVerifiedHeight.Set(float64(h.currentHighestVerifiedHeight))
 }
 
 func (h *height) Accepted(height uint64) {
