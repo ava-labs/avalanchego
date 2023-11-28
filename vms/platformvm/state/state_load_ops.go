434b696444d808addf80bdec999ec561e8829aee
Dan Laine <daniel.laine@avalabs.org>
2023-11-28 15:37:37 -0500

    reduce state.go diff

--- vms/platformvm/state/state_load_ops.go
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2019-2023, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-package state
-
-// var errNotYetImplemented = errors.New("NOT YET IMPLEMENTED")
