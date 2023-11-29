d469d8865c46741a5eacd2359413ea1f2a130ba2
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-11-28 16:25:44 -0800

    reduce diff

--- vms/platformvm/block/builder/builder.go
+++ vms/platformvm/block/builder/builder.go
@@ -89,7 +89,11 @@ func New(
 // This method removes the transactions from the returned
 // blocks from the mempool.
 func (b *builder) BuildBlock(context.Context) (snowman.Block, error) {
-	defer b.Mempool.RequestBuildBlock(false /*=emptyBlockPermitted*/)
+	b.Mempool.DisableAdding()
+	defer func() {
+		b.Mempool.EnableAdding()
+		b.Mempool.RequestBuildBlock(false /*=emptyBlockPermitted*/)
+	}()
 
 	ctx := b.txExecutorBackend.Ctx
 	ctx.Log.Debug("starting to attempt to build a block")
