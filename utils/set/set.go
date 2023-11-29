0da5bccfb04f98e1e1ac5079acea84056457fcbc
Dan Laine <daniel.laine@avalabs.org>
2023-11-29 16:32:16 -0500

    Remove method `CappedList` from `set.Set` (#2395)

--- utils/set/set.go
+++ utils/set/set.go
@@ -118,27 +118,6 @@ func (s Set[T]) List() []T {
 	return maps.Keys(s)
 }
 
-// CappedList returns a list of length at most [size].
-// Size should be >= 0. If size < 0, returns nil.
-func (s Set[T]) CappedList(size int) []T {
-	if size < 0 {
-		return nil
-	}
-	if l := s.Len(); l < size {
-		size = l
-	}
-	i := 0
-	elts := make([]T, size)
-	for elt := range s {
-		if i >= size {
-			break
-		}
-		elts[i] = elt
-		i++
-	}
-	return elts
-}
-
 // Equals returns true if the sets contain the same elements
 func (s Set[T]) Equals(other Set[T]) bool {
 	return maps.Equal(s, other)
