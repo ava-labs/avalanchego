06b1bdcd6a88ac6414974116abd935c56055ce83
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-11-30 13:53:11 -0500

    cleanup

--- vms/platformvm/block/builder/builder.go
+++ vms/platformvm/block/builder/builder.go
@@ -110,8 +110,7 @@ func (b *builder) BuildBlock(context.Context) (snowman.Block, error) {
 		return nil, fmt.Errorf("%w: %s", state.ErrMissingParentState, preferredID)
 	}
 
-	now := b.txExecutorBackend.Clk.Time()
-	timestamp := now
+	timestamp := b.txExecutorBackend.Clk.Time()
 	if parentTime := preferred.Timestamp(); parentTime.After(timestamp) {
 		timestamp = parentTime
 	}
@@ -162,42 +161,46 @@ func (b *builder) Shutdown() {
 }
 
 func (b *builder) ResetBlockTimer() {
-	ctx := b.txExecutorBackend.Ctx
+	// Next time the context lock is released, we can attempt to reset the block
+	// timer.
+	go func() {
+		ctx := b.txExecutorBackend.Ctx
 
-	// Grabbing the lock here enforces that this function is not called mid-way
-	// through modifying of the state.
-	ctx.Lock.Lock()
-	defer ctx.Lock.Unlock()
+		// Grabbing the lock here enforces that this function is not called mid-way
+		// through modifying of the state.
+		ctx.Lock.Lock()
+		defer ctx.Lock.Unlock()
 
-	preferredID := b.blkManager.Preferred()
-	preferredState, ok := b.blkManager.GetState(preferredID)
-	if !ok {
-		// The preferred block should always be a decision block
-		ctx.Log.Error("couldn't get preferred block state",
-			zap.Stringer("preferredID", preferredID),
-			zap.Stringer("lastAcceptedID", b.blkManager.LastAccepted()),
+		preferredID := b.blkManager.Preferred()
+		preferredState, ok := b.blkManager.GetState(preferredID)
+		if !ok {
+			// The preferred block should always be a decision block
+			ctx.Log.Error("couldn't get preferred block state",
+				zap.Stringer("preferredID", preferredID),
+				zap.Stringer("lastAcceptedID", b.blkManager.LastAccepted()),
+			)
+			return
+		}
+
+		nextStakerChangeTime, err := txexecutor.GetNextStakerChangeTime(preferredState)
+		if err != nil {
+			ctx.Log.Error("couldn't get next staker change time",
+				zap.Stringer("preferredID", preferredID),
+				zap.Stringer("lastAcceptedID", b.blkManager.LastAccepted()),
+				zap.Error(err),
+			)
+		}
+
+		now := b.txExecutorBackend.Clk.Time()
+		waitTime := nextStakerChangeTime.Sub(now)
+		ctx.Log.Debug("setting next scheduled event",
+			zap.Time("nextEventTime", nextStakerChangeTime),
+			zap.Duration("timeUntil", waitTime),
 		)
-		return
-	}
 
-	nextStakerChangeTime, err := txexecutor.GetNextStakerChangeTime(preferredState)
-	if err != nil {
-		ctx.Log.Error("couldn't get next staker change time",
-			zap.Stringer("preferredID", preferredID),
-			zap.Stringer("lastAcceptedID", b.blkManager.LastAccepted()),
-			zap.Error(err),
-		)
-	}
-
-	now := b.txExecutorBackend.Clk.Time()
-	waitTime := nextStakerChangeTime.Sub(now)
-	ctx.Log.Debug("setting next scheduled event",
-		zap.Time("nextEventTime", nextStakerChangeTime),
-		zap.Duration("timeUntil", waitTime),
-	)
-
-	// Wake up when it's time to add/remove the next validator
-	b.timer.SetTimeoutIn(waitTime)
+		// Wake up when it's time to add/remove the next validator
+		b.timer.SetTimeoutIn(waitTime)
+	}()
 }
 
 func (b *builder) maybeAdvanceTime() {
