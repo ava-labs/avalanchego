b4e264fc08653515f4b463a2735bcb95925327e3
David Boehm <david.boehm@avalabs.org>
2023-12-06 12:50:46 -0500

    Update db.go

--- x/merkledb/db.go
+++ x/merkledb/db.go
@@ -479,9 +479,14 @@ func (db *merkleDB) PrefetchPath(key []byte) error {
 func (db *merkleDB) prefetchPath(view *trieView, keyBytes []byte) error {
 	return view.visitPathToKey(ToKey(keyBytes), func(n *node) error {
 		if !n.hasValue() {
+			if _, ok := db.intermediateNodeDB.nodeCache.Get(n.key); ok {
+				return nil
+			}
 			return db.intermediateNodeDB.nodeCache.Put(n.key, n)
 		}
-
+		if _, ok := db.valueNodeDB.nodeCache.Get(n.key); ok {
+			return nil
+		}
 		db.valueNodeDB.nodeCache.Put(n.key, n)
 		return nil
 	})
