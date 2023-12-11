87fcfe06f225643f946fddc1245196cc8e7bf70b
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-12-11 12:14:51 -0500

    `vms/platformvm`: Add `TestBuildBlockShouldReward` test

--- vms/platformvm/block/builder/builder_test.go
+++ vms/platformvm/block/builder/builder_test.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow"
+	"github.com/ava-labs/avalanchego/snow/consensus/snowman"
 	"github.com/ava-labs/avalanchego/utils/constants"
 	"github.com/ava-labs/avalanchego/utils/crypto/secp256k1"
 	"github.com/ava-labs/avalanchego/utils/logging"
@@ -27,7 +28,6 @@ import (
 	"github.com/ava-labs/avalanchego/vms/platformvm/txs/mempool"
 
 	blockexecutor "github.com/ava-labs/avalanchego/vms/platformvm/block/executor"
-	txbuilder "github.com/ava-labs/avalanchego/vms/platformvm/txs/builder"
 	txexecutor "github.com/ava-labs/avalanchego/vms/platformvm/txs/executor"
 )
 
@@ -320,6 +320,114 @@ func TestGetNextStakerToReward(t *testing.T) {
 	}
 }
 
+func TestBuildBlockShouldReward(t *testing.T) {
+	require := require.New(t)
+
+	env := newEnvironment(t)
+	env.ctx.Lock.Lock()
+	defer func() {
+		require.NoError(shutdownEnvironment(env))
+		env.ctx.Lock.Unlock()
+	}()
+
+	var (
+		now    = env.backend.Clk.Time()
+		nodeID = ids.GenerateTestNodeID()
+
+		defaultValidatorStake = 100 * units.MilliAvax
+		validatorStartTime    = now.Add(2 * txexecutor.SyncBound)
+		validatorEndTime      = validatorStartTime.Add(360 * 24 * time.Hour)
+	)
+
+	// Create a valid [AddValidatorTx]
+	tx, err := env.txBuilder.NewAddValidatorTx(
+		defaultValidatorStake,
+		uint64(validatorStartTime.Unix()),
+		uint64(validatorEndTime.Unix()),
+		nodeID,
+		preFundedKeys[0].PublicKey().Address(),
+		reward.PercentDenominator,
+		[]*secp256k1.PrivateKey{preFundedKeys[0]},
+		preFundedKeys[0].PublicKey().Address(),
+	)
+	require.NoError(err)
+	txID := tx.ID()
+
+	// Issue the transaction
+	require.NoError(env.network.IssueTx(context.Background(), tx))
+	require.True(env.mempool.Has(txID))
+
+	// Build and accept a block with the tx
+	blk, err := env.Builder.BuildBlock(context.Background())
+	require.NoError(err)
+	require.Equal([]*txs.Tx{tx}, blk.(*blockexecutor.Block).Block.Txs())
+	require.NoError(blk.Verify(context.Background()))
+	require.NoError(blk.Accept(context.Background()))
+	require.True(env.blkManager.SetPreference(blk.ID()))
+
+	// Validator should now be pending
+	staker, err := env.state.GetPendingValidator(constants.PrimaryNetworkID, nodeID)
+	require.NoError(err)
+	require.Equal(txID, staker.TxID)
+
+	// Move it from pending to current
+	env.backend.Clk.Set(validatorStartTime)
+	blk, err = env.Builder.BuildBlock(context.Background())
+	require.NoError(err)
+	require.NoError(blk.Verify(context.Background()))
+	require.NoError(blk.Accept(context.Background()))
+	require.True(env.blkManager.SetPreference(blk.ID()))
+
+	staker, err = env.state.GetCurrentValidator(constants.PrimaryNetworkID, nodeID)
+	require.NoError(err)
+	require.Equal(txID, staker.TxID)
+
+	// Should be rewarded at the end of staking period
+	env.backend.Clk.Set(validatorEndTime)
+
+	for {
+		iter, err := env.state.GetCurrentStakerIterator()
+		require.NoError(err)
+		require.True(iter.Next())
+		staker := iter.Value()
+
+		// Check that the right block was built
+		blk, err := env.Builder.BuildBlock(context.Background())
+		require.NoError(err)
+		require.NoError(blk.Verify(context.Background()))
+		require.IsType(&block.BanffProposalBlock{}, blk.(*blockexecutor.Block).Block)
+
+		expectedTx, err := env.txBuilder.NewRewardValidatorTx(staker.TxID)
+		require.NoError(err)
+		require.Equal([]*txs.Tx{expectedTx}, blk.(*blockexecutor.Block).Block.Txs())
+
+		// Commit the [ProposalBlock] with a [CommitBlock]
+		proposalBlk, ok := blk.(snowman.OracleBlock)
+		require.True(ok)
+		options, err := proposalBlk.Options(context.Background())
+		require.NoError(err)
+
+		commit := options[0].(*blockexecutor.Block)
+		require.IsType(&block.BanffCommitBlock{}, commit.Block)
+
+		require.NoError(blk.Accept(context.Background()))
+		require.NoError(commit.Verify(context.Background()))
+		require.NoError(commit.Accept(context.Background()))
+		require.True(env.blkManager.SetPreference(commit.ID()))
+
+		// Stop rewarding once our staker is rewarded
+		if staker.TxID == txID {
+			break
+		}
+		iter.Release()
+	}
+
+	// Staking rewards should have been issued
+	rewardUTXOs, err := env.state.GetRewardUTXOs(txID)
+	require.NoError(err)
+	require.NotEmpty(rewardUTXOs)
+}
+
 func TestBuildBlock(t *testing.T) {
 	env := newEnvironment(t)
 	env.ctx.Lock.Lock()
@@ -333,7 +441,6 @@ func TestBuildBlock(t *testing.T) {
 		parentID        = ids.GenerateTestID()
 		height          = uint64(1337)
 		parentTimestamp = now.Add(-2 * time.Second)
-		stakerTxID      = ids.GenerateTestID()
 
 		defaultValidatorStake = 100 * units.MilliAvax
 		validatorStartTime    = now.Add(2 * txexecutor.SyncBound)
@@ -363,52 +470,6 @@ func TestBuildBlock(t *testing.T) {
 	}
 
 	tests := []test{
-		{
-			name: "should reward",
-			builderF: func(ctrl *gomock.Controller) *builder {
-				mempool := mempool.NewMockMempool(ctrl)
-
-				// The tx builder should be asked to build a reward tx
-				txBuilder := txbuilder.NewMockBuilder(ctrl)
-				txBuilder.EXPECT().NewRewardValidatorTx(stakerTxID).Return(tx, nil)
-
-				return &builder{
-					Mempool:   mempool,
-					txBuilder: txBuilder,
-				}
-			},
-			timestamp:        parentTimestamp,
-			forceAdvanceTime: false,
-			parentStateF: func(ctrl *gomock.Controller) state.Chain {
-				s := state.NewMockChain(ctrl)
-
-				// add current validator that ends at [parentTimestamp]
-				// i.e. it should be rewarded
-				currentStakerIter := state.NewMockStakerIterator(ctrl)
-				currentStakerIter.EXPECT().Next().Return(true)
-				currentStakerIter.EXPECT().Value().Return(&state.Staker{
-					TxID:     stakerTxID,
-					Priority: txs.PrimaryNetworkDelegatorCurrentPriority,
-					EndTime:  parentTimestamp,
-				})
-				currentStakerIter.EXPECT().Release()
-
-				s.EXPECT().GetCurrentStakerIterator().Return(currentStakerIter, nil)
-				return s
-			},
-			expectedBlkF: func(require *require.Assertions) block.Block {
-				expectedBlk, err := block.NewBanffProposalBlock(
-					parentTimestamp,
-					parentID,
-					height,
-					tx,
-					[]*txs.Tx{},
-				)
-				require.NoError(err)
-				return expectedBlk
-			},
-			expectedErr: nil,
-		},
 		{
 			name: "has decision txs",
 			builderF: func(ctrl *gomock.Controller) *builder {
