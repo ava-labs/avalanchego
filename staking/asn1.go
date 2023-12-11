d986723e0a7a72281b1316124a294137e3171880
Stephen Buttolph <stephen@avalabs.org>
2023-12-10 21:20:53 -0500

    nit

--- staking/asn1.go
+++ staking/asn1.go
@@ -31,17 +31,6 @@ var (
 	//		iso(1) member-body(2) us(840) ansi-X9-62(10045) keyType(2) 1 }
 	oidPublicKeyECDSA = asn1.ObjectIdentifier{1, 2, 840, 10045, 2, 1}
 
-	// Ref: https://github.com/golang/go/blob/go1.19.12/src/crypto/x509/x509.go#L468-L489
-	//
-	// RFC 5480, 2.1.1.1. Named Curve
-	//
-	//	secp256r1 OBJECT IDENTIFIER ::= {
-	//	  iso(1) member-body(2) us(840) ansi-X9-62(10045) curves(3)
-	//	  prime(1) 7 }
-	//
-	// NB: secp256r1 is equivalent to prime256v1
-	oidNamedCurveP256 = asn1.ObjectIdentifier{1, 2, 840, 10045, 3, 1, 7}
-
 	// Ref: https://github.com/golang/go/blob/go1.19.12/src/crypto/x509/x509.go#L326-L350
 	signatureAlgorithmVerificationDetails = map[x509.SignatureAlgorithm]x509.PublicKeyAlgorithm{
 		x509.SHA256WithRSA:   x509.RSA,
