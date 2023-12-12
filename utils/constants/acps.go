641854eaf85212b0b24a6d98f2db93e94e058c85
Stephen Buttolph <stephen@avalabs.org>
2023-12-12 16:29:21 -0500

    add acp-25

--- utils/constants/acps.go
+++ utils/constants/acps.go
@@ -12,6 +12,7 @@ import "github.com/ava-labs/avalanchego/utils/set"
 var CurrentACPs = set.Of[uint32](
 	23, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/23-p-chain-native-transfers.md
 	24, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/24-shanghai-eips.md
+	25, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/25-vm-application-errors.md
 	30, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/30-avalanche-warp-x-evm.md
 	31, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/31-enable-subnet-ownership-transfer.md
 	41, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/41-remove-pending-stakers.md
