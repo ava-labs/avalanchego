c12f9606c31116b8113d4b1937ecf60ac423e411
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-12-13 11:43:29 -0500

    nit

--- tests/e2e/p/permissionless_subnets.go
+++ tests/e2e/p/permissionless_subnets.go
@@ -134,13 +134,12 @@ var _ = e2e.DescribePChain("[Permissionless Subnets]", func() {
 				require.NoError(err)
 			})
 
-			validatorStartTime := time.Now().Add(time.Minute)
 			ginkgo.By("add permissionless validator", func() {
 				_, err := pWallet.IssueAddPermissionlessValidatorTx(
 					&txs.SubnetValidator{
 						Validator: txs.Validator{
 							NodeID: validatorID,
-							End:    uint64(validatorStartTime.Add(5 * time.Second).Unix()),
+							End:    uint64(time.Now().Add(time.Minute + 5*time.Second).Unix()),
 							Wght:   25 * units.MegaAvax,
 						},
 						Subnet: subnetID,
@@ -155,13 +154,12 @@ var _ = e2e.DescribePChain("[Permissionless Subnets]", func() {
 				require.NoError(err)
 			})
 
-			delegatorStartTime := validatorStartTime
 			ginkgo.By("add permissionless delegator", func() {
 				_, err := pWallet.IssueAddPermissionlessDelegatorTx(
 					&txs.SubnetValidator{
 						Validator: txs.Validator{
 							NodeID: validatorID,
-							End:    uint64(delegatorStartTime.Add(5 * time.Second).Unix()),
+							End:    uint64(time.Now().Add(time.Minute + 5*time.Second).Unix()),
 							Wght:   25 * units.MegaAvax,
 						},
 						Subnet: subnetID,
