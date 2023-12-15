d1690b9f6b7ebd7a4dee72ff79e6d0447a2da6e2
Stephen Buttolph <stephen@avalabs.org>
2023-12-15 12:02:49 -0500

    nit

--- vms/proposervm/vm.go
+++ vms/proposervm/vm.go
@@ -350,6 +350,14 @@ func (vm *VM) SetPreference(ctx context.Context, preferred ids.ID) error {
 		nextStartTime, err = vm.getPreDurangoSlotTime(ctx, blk, pChainHeight)
 	}
 	if err != nil {
+		vm.ctx.Log.Debug("failed to fetch the expected delay",
+			zap.Error(err),
+		)
+
+		// A nil error is returned here because it is possible that
+		// bootstrapping caused the last accepted block to move past the latest
+		// P-chain height. This will cause building blocks to return an error
+		// until the P-chain's height has advanced.
 		return nil
 	}
 	vm.Scheduler.SetBuildBlockTime(nextStartTime)
@@ -367,16 +375,8 @@ func (vm *VM) getPreDurangoSlotTime(
 	blk PostForkBlock,
 	pChainHeight uint64,
 ) (time.Time, error) {
-	parentTimestamp := blk.Timestamp()
-	minDelay, err := vm.Windower.Delay(ctx, blk.Height()+1, pChainHeight, vm.ctx.NodeID, proposer.MaxBuildWindows)
+	delay, err := vm.Windower.Delay(ctx, blk.Height()+1, pChainHeight, vm.ctx.NodeID, proposer.MaxBuildWindows)
 	if err != nil {
-		vm.ctx.Log.Debug("failed to fetch the expected delay",
-			zap.Error(err),
-		)
-		// A nil error is returned here because it is possible that
-		// bootstrapping caused the last accepted block to move past the latest
-		// P-chain height. This will cause building blocks to return an error
-		// until the P-chain's height has advanced.
 		return time.Time{}, err
 	}
 
@@ -386,10 +386,9 @@ func (vm *VM) getPreDurangoSlotTime(
 	// validators can specify. This delay may be an issue for high performance,
 	// custom VMs. Until the P-chain is modified to target a specific block
 	// time, ProposerMinBlockDelay can be configured in the subnet config.
-	minDelay = math.Max(minDelay, vm.MinBlkDelay)
-	nextStartTime := parentTimestamp.Add(minDelay)
-
-	return nextStartTime, nil
+	delay = math.Max(delay, vm.MinBlkDelay)
+	parentTimestamp := blk.Timestamp()
+	return parentTimestamp.Add(delay), nil
 }
 
 func (vm *VM) getPostDurangoSlotTime(
@@ -407,9 +406,7 @@ func (vm *VM) getPostDurangoSlotTime(
 		slot,
 	)
 	if err != nil {
-		vm.ctx.Log.Debug("failed to calculate min delay for proposer",
-			zap.Error(err),
-		)
+		return time.Time{}, err
 	}
 
 	// Note: The P-chain does not currently try to target any block time. It
