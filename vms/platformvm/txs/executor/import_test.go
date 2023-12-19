3d3cd0a67e223b73b7533632ed35dc83ee2a5a89
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-12-19 15:01:33 -0500

    more

--- vms/platformvm/txs/executor/import_test.go
+++ vms/platformvm/txs/executor/import_test.go
@@ -118,9 +118,9 @@ func TestNewImportTx(t *testing.T) {
 		},
 		{
 			description:   "attempting to import from C-chain",
-			sourceChainID: cChainID,
+			sourceChainID: env.ctx.CChainID,
 			sharedMemory: fundedSharedMemory(
-				cChainID,
+				env.ctx.CChainID,
 				map[ids.ID]uint64{
 					env.ctx.AVAXAssetID: env.config.TxFee,
 				},
