1d89d0d9e01cc36d67b7d69a97f4c23c58db1b72
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2023-12-19 15:07:13 -0500

    nit

--- snow/snowtest/snowtest.go
+++ snow/snowtest/snowtest.go
@@ -41,21 +41,26 @@ func ConsensusContext() *snow.ConsensusContext {
 	}
 }
 
+// Note: [ChainID] is not set in this function. It is expected to be set by the
+// consumer of this function to whichever chain this context exists within.
 func NewContext(tb testing.TB) *snow.Context {
 	require := require.New(tb)
 
 	ctx := snow.DefaultContextTest()
 	ctx.NetworkID = constants.UnitTestID
-	ctx.ChainID = ids.GenerateTestID()
-	ctx.XChainID = ctx.ChainID
+	ctx.XChainID = ids.GenerateTestID()
 	ctx.CChainID = ids.GenerateTestID()
 	ctx.AVAXAssetID = ids.GenerateTestID()
 
 	aliaser := ctx.BCLookup.(ids.Aliaser)
-	require.NoError(aliaser.Alias(ctx.XChainID, "X"))
-	require.NoError(aliaser.Alias(ctx.XChainID, ctx.XChainID.String()))
 	require.NoError(aliaser.Alias(constants.PlatformChainID, "P"))
 	require.NoError(aliaser.Alias(constants.PlatformChainID, constants.PlatformChainID.String()))
+
+	require.NoError(aliaser.Alias(ctx.XChainID, "X"))
+	require.NoError(aliaser.Alias(ctx.XChainID, ctx.XChainID.String()))
+
+	require.NoError(aliaser.Alias(ctx.CChainID, "P"))
+	require.NoError(aliaser.Alias(ctx.CChainID, ctx.CChainID.String()))
 
 	ctx.ValidatorState = &validators.TestState{
 		GetSubnetIDF: func(_ context.Context, chainID ids.ID) (ids.ID, error) {
