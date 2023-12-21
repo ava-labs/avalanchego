7518e5a188897df87c7491cfa5d2268e73b05ca0
Stephen Buttolph <stephen@avalabs.org>
2023-12-21 17:18:54 -0500

    nit

--- vms/avm/network/tx_verifier.go
+++ vms/avm/network/tx_verifier.go
@@ -9,27 +9,27 @@ import (
 	"github.com/ava-labs/avalanchego/vms/avm/txs"
 )
 
-var _ TxVerifier = (*lockedTxVerifier)(nil)
+var _ TxVerifier = (*LockedTxVerifier)(nil)
 
 type TxVerifier interface {
 	// VerifyTx verifies that the transaction should be issued into the mempool.
 	VerifyTx(tx *txs.Tx) error
 }
 
-type lockedTxVerifier struct {
+type LockedTxVerifier struct {
 	lock       sync.Locker
 	txVerifier TxVerifier
 }
 
-func (l *lockedTxVerifier) VerifyTx(tx *txs.Tx) error {
+func (l *LockedTxVerifier) VerifyTx(tx *txs.Tx) error {
 	l.lock.Lock()
 	defer l.lock.Unlock()
 
 	return l.txVerifier.VerifyTx(tx)
 }
 
-func NewLockedTxVerifier(lock sync.Locker, txVerifier TxVerifier) TxVerifier {
-	return &lockedTxVerifier{
+func NewLockedTxVerifier(lock sync.Locker, txVerifier TxVerifier) *LockedTxVerifier {
+	return &LockedTxVerifier{
 		lock:       lock,
 		txVerifier: txVerifier,
 	}
