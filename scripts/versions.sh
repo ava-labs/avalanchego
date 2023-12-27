a2d30768d7dcdab2c7e4e46feb7e907595aad496
Dan Laine <daniel.laine@avalabs.org>
2023-12-27 15:52:27 -0500

    use int64 timestamp (#433)
    * add gossip marshallers
    
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
    
    * nit
    
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
    
    * nit
    
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
    
    * fix build
    
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
    
    * use int64 timestamp
    
    * update avalanchego dep
    
    * update avalanchego dep
    
    ---------
    
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
    Co-authored-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
    Co-authored-by: Darioush Jalali <darioush.jalali@avalabs.org>

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.10.18-rc.8'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.10.18-rc.9'}
