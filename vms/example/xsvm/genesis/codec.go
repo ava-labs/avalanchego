196d1f2f1bb55eeab95be59113cd6c367125a1db
Stephen Buttolph <stephen@avalabs.org>
2023-12-29 13:02:45 -0500

    nit

--- vms/example/xsvm/genesis/codec.go
+++ vms/example/xsvm/genesis/codec.go
@@ -5,6 +5,6 @@ package genesis
 
 import "github.com/ava-labs/avalanchego/vms/example/xsvm/block"
 
-const Version = block.CodecVersion
+const CodecVersion = block.CodecVersion
 
 var Codec = block.Codec
