2db37df8a92c0eb6d2470aac442eae5569199074
Dan Laine <daniel.laine@avalabs.org>
2024-01-04 16:48:39 -0500

    re-add check

--- network/peer/peer.go
+++ network/peer/peer.go
@@ -868,7 +868,26 @@ func (p *peer) handleHandshake(msg *p2p.Handshake) {
 	myTime := p.Clock.Time()
 	myTimeUnix := uint64(myTime.Unix())
 	clockDifference := math.Abs(float64(msg.MyTime) - float64(myTimeUnix))
+
 	p.Metrics.ClockSkew.Observe(clockDifference)
+
+	if clockDifference > p.MaxClockDifference.Seconds() {
+		if _, ok := p.Beacons.GetValidator(constants.PrimaryNetworkID, p.id); ok {
+			p.Log.Warn("beacon reports out of sync time",
+				zap.Stringer("nodeID", p.id),
+				zap.Uint64("peerTime", msg.MyTime),
+				zap.Uint64("myTime", myTimeUnix),
+			)
+		} else {
+			p.Log.Debug("peer reports out of sync time",
+				zap.Stringer("nodeID", p.id),
+				zap.Uint64("peerTime", msg.MyTime),
+				zap.Uint64("myTime", myTimeUnix),
+			)
+		}
+		p.StartClose()
+		return
+	}
 
 	if msg.Client != nil {
 		p.version = &version.Application{
