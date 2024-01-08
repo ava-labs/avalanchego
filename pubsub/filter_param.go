bba8e75b5bec7c7a0c8d6b00930f9872131b301d
Stephen Buttolph <stephen@avalabs.org>
2024-01-07 22:24:14 -0500

    Optimize bloom filter (#2588)

--- pubsub/filter_param.go
+++ pubsub/filter_param.go
@@ -6,7 +6,7 @@ package pubsub
 import (
 	"sync"
 
-	"github.com/ava-labs/avalanchego/utils/bloom"
+	"github.com/ava-labs/avalanchego/pubsub/bloom"
 	"github.com/ava-labs/avalanchego/utils/set"
 )
 
