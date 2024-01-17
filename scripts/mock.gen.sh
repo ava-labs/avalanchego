0fcc746f0a6a0178ca5e9e5f8d1bcd83d1207d24
Dhruba Basu <7675102+dhrubabasu@users.noreply.github.com>
2024-01-17 11:13:01 -0500

    Update `go.uber.org/mock/gomock` to `v0.4.0` (#2618)

--- scripts/mock.gen.sh
+++ scripts/mock.gen.sh
@@ -11,7 +11,7 @@ if ! command -v mockgen &> /dev/null
 then
   echo "mockgen not found, installing..."
   # https://github.com/uber-go/mock
-  go install -v go.uber.org/mock/mockgen@v0.2.0
+  go install -v go.uber.org/mock/mockgen@v0.4.0
 fi
 
 source ./scripts/constants.sh
