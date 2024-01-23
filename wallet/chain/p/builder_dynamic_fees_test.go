4170845593ade3a9a4f417ed1891cdba8390e8e2
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2024-01-23 23:40:19 +0100

    wip: adding UTs for dynamic fees builder

--- wallet/chain/p/builder_dynamic_fees_test.go
+++ wallet/chain/p/builder_dynamic_fees_test.go
@@ -4,6 +4,7 @@
 package p
 
 import (
+	stdcontext "context"
 	"math"
 	"math/rand"
 	"testing"
@@ -16,15 +17,18 @@ import (
 	"github.com/ava-labs/avalanchego/utils/crypto/secp256k1"
 	"github.com/ava-labs/avalanchego/utils/set"
 	"github.com/ava-labs/avalanchego/utils/units"
+	"github.com/ava-labs/avalanchego/vms/components/avax"
 	"github.com/ava-labs/avalanchego/vms/platformvm/txs"
+	"github.com/ava-labs/avalanchego/vms/platformvm/txs/fees"
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 
-	"github.com/ava-labs/avalanchego/vms/components/avax"
 	commonfees "github.com/ava-labs/avalanchego/vms/components/fees"
 )
 
 var testKeys = secp256k1.TestKeys()
 
+// Create and sign the tx, then verify that utxos included
+// in the tx are exactly necessary to pay fees for it
 func TestCreateChainTx(t *testing.T) {
 	require := require.New(t)
 	ctrl := gomock.NewController(t)
@@ -64,42 +68,40 @@ func TestCreateChainTx(t *testing.T) {
 		}
 	)
 
-	be.EXPECT().GetTx(gomock.Any(), subnetID).Return(
-		&txs.Tx{
-			Unsigned: &txs.CreateSubnetTx{
-				Owner: &secp256k1fx.OutputOwners{},
-			},
+	subnetTx := &txs.Tx{
+		Unsigned: &txs.CreateSubnetTx{
+			Owner: &secp256k1fx.OutputOwners{},
 		},
-		nil,
-	)
+	}
+	be.EXPECT().GetTx(gomock.Any(), subnetID).Return(subnetTx, nil)
 
-	amount := 10 * units.Avax
-	avaxAssetID := ids.GenerateTestID()
-	be.EXPECT().AVAXAssetID().Return(avaxAssetID).AnyTimes()
-	be.EXPECT().NetworkID().Return(constants.MainnetID).AnyTimes()
-
-	be.EXPECT().UTXOs(gomock.Any(), constants.PlatformChainID).Return(
-		[]*avax.UTXO{
-			&avax.UTXO{
+	var (
+		amount      = 10 * units.Avax
+		avaxAssetID = ids.GenerateTestID()
+		utxos       = []*avax.UTXO{
+			{
 				UTXOID: avax.UTXOID{
 					TxID:        ids.GenerateTestID(),
-					OutputIndex: rand.Uint32(),
+					OutputIndex: rand.Uint32(), // #nosec G404
 				},
 				Asset: avax.Asset{ID: avaxAssetID},
 				Out: &secp256k1fx.TransferOutput{
-					Amt: uint64(amount),
+					Amt: amount,
 					OutputOwners: secp256k1fx.OutputOwners{
 						Locktime:  0,
-						Addrs:     []ids.ShortID{testKeys[0].PublicKey().Address()},
+						Addrs:     []ids.ShortID{utxoKey.PublicKey().Address()},
 						Threshold: 1,
 					},
 				},
 			},
-		},
-		nil,
+		}
 	)
 
-	_, err := b.NewCreateChainTx(
+	be.EXPECT().AVAXAssetID().Return(avaxAssetID).AnyTimes()
+	be.EXPECT().NetworkID().Return(constants.MainnetID).AnyTimes()
+	be.EXPECT().UTXOs(gomock.Any(), constants.PlatformChainID).Return(utxos, nil)
+
+	utx, err := b.NewCreateChainTx(
 		subnetID,
 		genesisBytes,
 		vmID,
@@ -110,5 +112,30 @@ func TestCreateChainTx(t *testing.T) {
 	)
 	require.NoError(err)
 
-	// TODO: sign and verify fees can be paid
+	var (
+		kc  = secp256k1fx.NewKeychain(utxoKey)
+		sbe = NewMockSignerBackend(ctrl)
+		s   = NewSigner(kc, sbe)
+	)
+
+	sbe.EXPECT().GetUTXO(gomock.Any(), gomock.Any(), gomock.Any()).Return(utxos[0], nil)
+	sbe.EXPECT().GetTx(gomock.Any(), subnetID).Return(subnetTx, nil)
+
+	tx, err := s.SignUnsigned(stdcontext.Background(), utx)
+	require.NoError(err)
+
+	fc := &fees.Calculator{
+		IsEForkActive:    true,
+		FeeManager:       commonfees.NewManager(testUnitFees),
+		ConsumedUnitsCap: testBlockMaxConsumedUnits,
+		Credentials:      tx.Creds,
+	}
+	require.NoError(utx.Visit(fc))
+	require.Equal(3128*units.MicroAvax, fc.Fee)
+
+	ins := utx.Ins
+	outs := utx.Outs
+	require.Len(ins, 1)
+	require.Len(outs, 1)
+	require.Equal(fc.Fee, utx.Ins[0].In.Amount()-utx.Outs[0].Out.Amount())
 }
