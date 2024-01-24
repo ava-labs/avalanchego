87952c27b65b435f3caef24e3c1dea5b532a6547
marun <maru.newby@avalabs.org>
2024-01-24 15:57:02 +0100

    `ci`: Add shellcheck step to lint job (#464)

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Ignore warnings about variables appearing unused since this file is not the consumer of the variables it defines.
+# shellcheck disable=SC2034
+
+set -euo pipefail
+
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.10.19-legacy-upgrade-times'}
