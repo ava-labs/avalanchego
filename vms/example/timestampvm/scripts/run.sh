cf6606dbbadb04e65cfd158d2dd54a0dce876892
Maru Newby <maru.newby@avalabs.org>
2024-02-01 17:51:30 +0100

    fixup: Update date in license headers

--- vms/example/timestampvm/scripts/run.sh
+++ vms/example/timestampvm/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# (c) 2019-2023, Ava Labs, Inc. All rights reserved.
+# (c) 2019-2024, Ava Labs, Inc. All rights reserved.
 # See the file LICENSE for licensing terms.
 
 set -e
