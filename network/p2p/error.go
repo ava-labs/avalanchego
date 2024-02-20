bef44da1b726e0d83cbf7dfe782e1e518c69f584
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-02-19 23:40:41 -0500

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- network/p2p/error.go
+++ network/p2p/error.go
@@ -9,21 +9,25 @@ var (
 	// ErrUnexpected should be used to indicate that a request failed due to a
 	// generic error
 	ErrUnexpected = &common.AppError{
-		Code: -1,
+		Code:    -1,
+		Message: "unexpected error",
 	}
 	// ErrUnregisteredHandler should be used to indicate that a request failed
 	// due to it not matching a registered handler
 	ErrUnregisteredHandler = &common.AppError{
-		Code: -2,
+		Code:    -2,
+		Message: "unregistered handler",
 	}
 	// ErrNotValidator should be used to indicate that a request failed due to
 	// the requesting peer not being a validator
 	ErrNotValidator = &common.AppError{
-		Code: -3,
+		Code:    -3,
+		Message: "not a validator",
 	}
 	// ErrThrottled should be used to indicate that a request failed due to the
 	// requesting peer exceeding a rate limit
 	ErrThrottled = &common.AppError{
-		Code: -4,
+		Code:    -4,
+		Message: "throttled",
 	}
 )
