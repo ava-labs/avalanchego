eaf3d637028b96f399ddf60cc22c95584fd5a56b
bianyuanop <chen.me.nan@gmail.com>
2024-02-21 18:49:37 -0500

    remote  from noOpTracer

--- trace/noop.go
+++ trace/noop.go
@@ -4,26 +4,14 @@
 package trace
 
 import (
-	"context"
-
-	"go.opentelemetry.io/otel/trace"
 	"go.opentelemetry.io/otel/trace/noop"
-
-	"github.com/ava-labs/avalanchego/utils/constants"
 )
 
-var Noop Tracer = noOpTracer{
-	t: noop.NewTracerProvider().Tracer(constants.AppName),
-}
+var Noop Tracer = noOpTracer{}
 
 // noOpTracer is an implementation of trace.Tracer that does nothing.
 type noOpTracer struct {
 	noop.Tracer
-	t trace.Tracer
-}
-
-func (n noOpTracer) Start(ctx context.Context, spanName string, opts ...trace.SpanStartOption) (context.Context, trace.Span) {
-	return n.t.Start(ctx, spanName, opts...)
 }
 
 func (noOpTracer) Close() error {
