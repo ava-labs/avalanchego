9e40b645a0dd2dc5bb7d402dd092312840ff59c8
Stephen Buttolph <stephen@avalabs.org>
2024-02-26 19:30:30 -0500

    push gossip with partial sync

--- vms/platformvm/network/network.go
+++ vms/platformvm/network/network.go
@@ -172,12 +172,6 @@ func New(
 }
 
 func (n *network) PushGossip(ctx context.Context) {
-	// If the node is running partial sync, we should not perform any pull
-	// gossip.
-	if n.partialSyncPrimaryNetwork {
-		return
-	}
-
 	gossip.Every(ctx, n.log, n.txPushGossiper, n.txPushGossipFrequency)
 }
 
