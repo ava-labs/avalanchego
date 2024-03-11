3175c7d1e60b2f8244663feab5dcedb233e539d6
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2024-03-11 15:00:05 +0100

    exponential fees update stability

--- vms/components/fees/manager_test.go
+++ vms/components/fees/manager_test.go
@@ -155,3 +155,65 @@ func TestComputeNextEdgeCases(t *testing.T) {
 	// next cumulated units are zeroed
 	require.Equal(Dimensions{}, next.cumulatedUnits)
 }
+
+func TestComputeNextStability(t *testing.T) {
+	// The advantage of using an exponential fee update scheme
+	// (vs e.g. the EIP-1559 scheme we use in the C-chain) is that
+	// it is more stable against dithering.
+	// We prove here that if consumed used oscillate around the target
+	// unit fees are unchanged.
+
+	require := require.New(t)
+
+	var (
+		initialUnitFees  = Dimensions{10, 100, 1_000, 1_000_000_000}
+		consumedUnits1   = Dimensions{24, 45, 70, 500}
+		consumedUnits2   = Dimensions{26, 55, 130, 1500}
+		targetComplexity = Dimensions{25, 50, 100, 1000}
+
+		lastBlkTime = time.Now().Truncate(time.Second)
+		currBlkTime = lastBlkTime.Add(time.Second)
+
+		updateCoefficient = Dimensions{1, 2, 5, 10}
+		minUnitFees       = Dimensions{0, 0, 0, 0}
+	)
+
+	// step1: cumulated units are below target. Unit fees must decrease
+	m1 := &Manager{
+		unitFees:       initialUnitFees,
+		windows:        [FeeDimensions]Window{},
+		cumulatedUnits: consumedUnits1,
+	}
+	next1 := m1.ComputeNext(
+		lastBlkTime.Unix(),
+		currBlkTime.Unix(),
+		targetComplexity,
+		updateCoefficient,
+		minUnitFees,
+	)
+
+	require.Less(next1.unitFees[Bandwidth], initialUnitFees[Bandwidth])
+	require.Less(next1.unitFees[UTXORead], initialUnitFees[UTXORead])
+	require.Less(next1.unitFees[UTXOWrite], initialUnitFees[UTXOWrite])
+	require.Less(next1.unitFees[Compute], initialUnitFees[Compute])
+
+	// step2: cumulated units go slight above target, so that average consumed units are at target.
+	// Unit fees go back to the original value
+	m2 := &Manager{
+		unitFees:       next1.unitFees,
+		windows:        [FeeDimensions]Window{},
+		cumulatedUnits: consumedUnits2,
+	}
+	next2 := m2.ComputeNext(
+		lastBlkTime.Unix(),
+		currBlkTime.Unix(),
+		targetComplexity,
+		updateCoefficient,
+		minUnitFees,
+	)
+
+	require.Equal(initialUnitFees[Bandwidth], next2.unitFees[Bandwidth])
+	require.Equal(initialUnitFees[UTXORead], next2.unitFees[UTXORead])
+	require.Equal(initialUnitFees[UTXOWrite], next2.unitFees[UTXOWrite])
+	require.Equal(initialUnitFees[Compute], next2.unitFees[Compute])
+}
