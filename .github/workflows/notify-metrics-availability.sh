d78c0cc81f4e3933e309a27539e0456eee08c476
Maru Newby <maru.newby@avalabs.org>
2024-03-12 12:29:24 -0700

    fixup: Fix shellcheck error

--- .github/workflows/notify-metrics-availability.sh
+++ .github/workflows/notify-metrics-availability.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Timestamps are in seconds
 from_timestamp="$(date '+%s')"
 monitoring_period=900 # 15 minutes
-to_timestamp="$((${from_timestamp} + ${monitoring_period}))"
+to_timestamp="$((from_timestamp + monitoring_period))"
 
 # Grafana expects microseconds, so pad timestamps with 3 zeros
 metrics_url="${PROMETHEUS_URL}&var-filter=gh_job_id%7C%3D%7C${GH_JOB_ID}&from=${from_timestamp}000&to=${to_timestamp}000"
