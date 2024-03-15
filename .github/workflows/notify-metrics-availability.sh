bbeb4b32f4bca9748e3eaf2cebcb4a6e2c3780ec
Maru Newby <maru.newby@avalabs.org>
2024-03-14 22:56:27 -0700

    fixup: s/grafana_url/prometheus_url/

--- .github/workflows/notify-metrics-availability.sh
+++ .github/workflows/notify-metrics-availability.sh
@@ -8,7 +8,7 @@ monitoring_period=900 # 15 minutes
 to_timestamp="$((from_timestamp + monitoring_period))"
 
 # Grafana expects microseconds, so pad timestamps with 3 zeros
-metrics_url="${PROMETHEUS_URL}&var-filter=gh_job_id%7C%3D%7C${GH_JOB_ID}&from=${from_timestamp}000&to=${to_timestamp}000"
+metrics_url="${GRAFANA_URL}&var-filter=gh_job_id%7C%3D%7C${GH_JOB_ID}&from=${from_timestamp}000&to=${to_timestamp}000"
 
 # Optionally ensure that the link displays metrics only for the shared
 # network rather than mixing it with the results for private networks.
