d54484aec2abfa8122dd4a60a28096577f3a731f
Darioush Jalali <darioush.jalali@avalabs.org>
2024-03-18 13:33:32 -0700

    start v0.13.2 release cycle (#517)

--- graft/coreth/plugin/evm/version.go
+++ graft/coreth/plugin/evm/version.go
@@ -11,7 +11,7 @@ var (
 	// GitCommit is set by the build script
 	GitCommit string
 	// Version is the version of Coreth
-	Version string = "v0.13.1"
+	Version string = "v0.13.2"
 )
 
 func init() {
