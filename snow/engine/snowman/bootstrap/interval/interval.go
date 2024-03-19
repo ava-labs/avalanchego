be64296df381b980399f935816f91934a30aed6d
Stephen Buttolph <stephen@avalabs.org>
2024-03-19 14:35:30 -0400

    Add interval tests

--- snow/engine/snowman/bootstrap/interval/interval.go
+++ snow/engine/snowman/bootstrap/interval/interval.go
@@ -3,6 +3,8 @@
 
 package interval
 
+import "math"
+
 type Interval struct {
 	LowerBound uint64
 	UpperBound uint64
@@ -13,15 +15,21 @@ func (i *Interval) Less(other *Interval) bool {
 }
 
 func (i *Interval) Contains(height uint64) bool {
-	return i != nil && i.LowerBound <= height && height <= i.UpperBound
-}
-
-// AdjacentToUpperBound returns true if height is 1 greater than upperBound.
-func (i *Interval) AdjacentToUpperBound(height uint64) bool {
-	return i != nil && i.UpperBound+1 == height
+	return i != nil &&
+		i.LowerBound <= height &&
+		height <= i.UpperBound
 }
 
 // AdjacentToLowerBound returns true if height is 1 less than lowerBound.
 func (i *Interval) AdjacentToLowerBound(height uint64) bool {
-	return i != nil && height+1 == i.LowerBound
+	return i != nil &&
+		height < math.MaxUint64 &&
+		height+1 == i.LowerBound
+}
+
+// AdjacentToUpperBound returns true if height is 1 greater than upperBound.
+func (i *Interval) AdjacentToUpperBound(height uint64) bool {
+	return i != nil &&
+		i.UpperBound < math.MaxUint64 &&
+		i.UpperBound+1 == height
 }
