b2d11ce56cb67c637d025c99283bd510b786f5ec
Stephen Buttolph <stephen@avalabs.org>
2024-03-18 21:17:41 -0400

    nit

--- vms/platformvm/block/executor/helpers_test.go
+++ vms/platformvm/block/executor/helpers_test.go
@@ -169,7 +169,6 @@ func newEnvironment(t *testing.T, ctrl *gomock.Controller, f fork) *environment 
 		res.uptimes = uptime.NewManager(res.mockedState, res.clk)
 		res.utxosVerifier = utxo.NewVerifier(res.ctx, res.clk, res.fx)
 
-		res.mockedState.EXPECT().GetTimestamp().Return(time.Time{}).Times(2) // to initialize createSubnet/BlockchainTx fee
 		res.txBuilder = p_tx_builder.New(
 			res.ctx,
 			res.config,
