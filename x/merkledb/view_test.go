4dec1abab4813fa93445f1c4c1d2cf455c8dd92b
Stephen Buttolph <stephen@avalabs.org>
2024-03-31 18:36:16 -0400

    add tests

--- x/merkledb/view_test.go
+++ x/merkledb/view_test.go
@@ -14,56 +14,80 @@ import (
 	"github.com/ava-labs/avalanchego/utils/hashing"
 )
 
-func Benchmark_HashChangedNodes(b *testing.B) {
-	tests := []struct {
-		name    string
-		numKeys uint64
-	}{
-		{
-			name:    "1",
-			numKeys: 1,
-		},
-		{
-			name:    "10",
-			numKeys: 10,
-		},
-		{
-			name:    "100",
-			numKeys: 100,
-		},
-		{
-			name:    "1000",
-			numKeys: 1000,
-		},
-		{
-			name:    "10000",
-			numKeys: 10000,
-		},
-		{
-			name:    "100000",
-			numKeys: 100000,
-		},
+var hashChangedNodesTests = []struct {
+	name             string
+	numKeys          uint64
+	expectedRootHash string
+}{
+	{
+		name:             "1",
+		numKeys:          1,
+		expectedRootHash: "2A4DRkSWbTvSxgA1UMGp1Mpt1yzMFaeMMiDnrijVGJXPcRYiD4",
+	},
+	{
+		name:             "10",
+		numKeys:          10,
+		expectedRootHash: "2PGy7QvbYwVwn5QmLgj4KBgV2BisanZE8Nue2SxK9ffybb4mAn",
+	},
+	{
+		name:             "100",
+		numKeys:          100,
+		expectedRootHash: "LCeS4DWh6TpNKWH4ke9a2piSiwwLbmxGUj8XuaWx1XDGeCMAv",
+	},
+	{
+		name:             "1000",
+		numKeys:          1000,
+		expectedRootHash: "2S6f84wdRHmnx51mj35DF2owzf8wio5pzNJXfEWfFYFNxUB64T",
+	},
+	{
+		name:             "10000",
+		numKeys:          10000,
+		expectedRootHash: "wF6UnhaDoA9fAqiXAcx27xCYBK2aspDBEXkicmC7rs8EzLCD8",
+	},
+	{
+		name:             "100000",
+		numKeys:          100000,
+		expectedRootHash: "2Dy3RWZeNDUnUvzXpruB5xdp1V7xxb14M53ywdZVACDkdM66M1",
+	},
+}
+
+func makeViewForHashChangedNodes(t require.TestingT, numKeys uint64) *view {
+	db, err := getBasicDB()
+	require.NoError(t, err)
+
+	ops := make([]database.BatchOp, 0, numKeys)
+	for i := uint64(0); i < numKeys; i++ {
+		k := binary.AppendUvarint(nil, i)
+		ops = append(ops, database.BatchOp{
+			Key:   k,
+			Value: hashing.ComputeHash256(k),
+		})
 	}
-	for _, test := range tests {
-		db, err := getBasicDB()
-		require.NoError(b, err)
 
-		ops := make([]database.BatchOp, 0, test.numKeys)
-		for i := uint64(0); i < test.numKeys; i++ {
-			k := binary.AppendUvarint(nil, i)
-			ops = append(ops, database.BatchOp{
-				Key:   k,
-				Value: hashing.ComputeHash256(k),
-			})
-		}
+	ctx := context.Background()
+	viewIntf, err := db.NewView(ctx, ViewChanges{BatchOps: ops})
+	require.NoError(t, err)
 
+	view := viewIntf.(*view)
+	require.NoError(t, view.calculateNodeChanges(ctx))
+	return view
+}
+
+func Test_HashChangedNodes(t *testing.T) {
+	for _, test := range hashChangedNodesTests {
+		t.Run(test.name, func(t *testing.T) {
+			view := makeViewForHashChangedNodes(t, test.numKeys)
+			ctx := context.Background()
+			view.hashChangedNodes(ctx)
+			require.Equal(t, test.expectedRootHash, view.changes.rootID.String())
+		})
+	}
+}
+
+func Benchmark_HashChangedNodes(b *testing.B) {
+	for _, test := range hashChangedNodesTests {
+		view := makeViewForHashChangedNodes(b, test.numKeys)
 		ctx := context.Background()
-		viewIntf, err := db.NewView(ctx, ViewChanges{BatchOps: ops})
-		require.NoError(b, err)
-
-		view := viewIntf.(*view)
-		require.NoError(b, view.calculateNodeChanges(ctx))
-
 		b.Run(test.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				view.hashChangedNodes(ctx)
