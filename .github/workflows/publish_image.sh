4351372247dd9a5412f1a4364d4f958b836fc2fd
Maru Newby <maru.newby@avalabs.org>
2024-04-04 18:53:42 +0200

    Enable multi-arch docker images

--- .github/workflows/publish_image.sh
+++ .github/workflows/publish_image.sh
@@ -16,14 +16,42 @@ source "$AVALANCHE_PATH"/scripts/constants.sh
 # Build current avalanchego
 source "$AVALANCHE_PATH"/scripts/build_image.sh
 
-if [[ $current_branch == "master" ]]; then
-  echo "Tagging current avalanchego image as $avalanchego_dockerhub_repo:latest"
-  docker tag "$avalanchego_dockerhub_repo:$current_branch" "$avalanchego_dockerhub_repo:latest"
-fi
-
-echo "Pushing: $avalanchego_dockerhub_repo:$current_branch"
-
 echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+if [[ $current_branch == "master" ]]; then
+  echo "Tagging current avalanchego images as $avalanchego_dockerhub_repo:latest-{amd64,arm64}"
+  docker tag "$avalanchego_dockerhub_repo:$COMMIT_HASH-amd64" "$avalanchego_dockerhub_repo:latest-amd64"
+  docker tag "$avalanchego_dockerhub_repo:$COMMIT_HASH-arm64" "$avalanchego_dockerhub_repo:latest-arm64"
+fi
 
 ## pushing image with tags
 docker image push -a "$avalanchego_dockerhub_repo"
+
+create_multi_arch_image() {
+  local images=$1
+  local tag=$2
+  local repo="$avalanchego_dockerhub_repo"
+
+  echo "Creating multi-arch Docker Image $repo:$tag"
+
+  docker manifest create "$repo:$tag" ${images[@]}
+
+  # Annotate the manifest with the OS and Architecture for each image
+  for image in "${images[@]}"; do
+    local arch=$(echo $image | rev | cut -d- -f1 | rev)
+    docker manifest annotate "$repo:$tag" "$image" --os linux --arch $arch
+  done
+
+  docker manifest push "$repo:$tag"
+}
+
+# Create multi-arch images
+IMAGES=("$avalanchego_dockerhub_repo:$COMMIT_HASH-amd64" "$avalanchego_dockerhub_repo:$COMMIT_HASH-arm64"
+create_multi_arch_image "$IMAGES" "$COMMIT_HASH"
+create_multi_arch_image "$IMAGES" "$current_branch"
+if [[ $current_branch == "master" ]]; then
+  create_multi_arch_image "$IMAGES" "latest"
+fi
+RACE_DETECTION_IMAGES=("$avalanchego_dockerhub_repo:$COMMIT_HASH-race-amd64" "$avalanchego_dockerhub_repo:$COMMIT_HASH-race-arm64")
+create_multi_arch_image "$RACE_DETECTION_IMAGES" "$COMMIT_HASH-race"
+create_multi_arch_image "$RACE_DETECTION_IMAGES" "$current_branch-race"
--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,6 +2,15 @@
 
 set -euo pipefail
 
+# Cross-platform builds require Docker Buildx and QEMU. buildx should
+# be enabled by default as of this writing, and qemu can be installed
+# on ubuntu as follows:
+#
+#  sudo apt-get install qemu qemu-user-static
+#
+#
+# Reference: https://docs.docker.com/buildx/working-with-buildx/
+
 # Directory above this script
 AVALANCHE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
 
@@ -15,12 +24,22 @@ fi
 
 # WARNING: this will use the most recent commit even if there are un-committed changes present
 full_commit_hash="$(git --git-dir="$AVALANCHE_PATH/.git" rev-parse HEAD)"
-commit_hash="${full_commit_hash::8}"
+# Export to ensure the variable is available to the caller
+export COMMIT_HASH="${full_commit_hash::8}"
 
-echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$commit_hash , $avalanchego_dockerhub_repo:$current_branch"
-docker build -t "$avalanchego_dockerhub_repo:$commit_hash" \
-        -t "$avalanchego_dockerhub_repo:$current_branch" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile"
+# Build images for supported architectures
+TARGET_ARCHITECTURES=("amd64" "arm64")
+for TARGET_ARCH in "${TARGET_ARCHITECTURES[@]}"; do
+  # Only linux is supported
+  PLATFORM="linux/$TARGET_ARCH"
 
-echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$commit_hash-race , $avalanchego_dockerhub_repo:$current_branch-race"
-docker build --build-arg="RACE_FLAG=-r" -t "$avalanchego_dockerhub_repo:$commit_hash-race" \
-        -t "$avalanchego_dockerhub_repo:$current_branch-race" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile"
+  echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$COMMIT_HASH-$TARGET_ARCH , $avalanchego_dockerhub_repo:$current_branch-$TARGET_ARCH"
+  docker buildx build --platform="$PLATFORM" \
+         -t "$avalanchego_dockerhub_repo:$COMMIT_HASH-$TARGET_ARCH" -t "$avalanchego_dockerhub_repo:$current_branch-$TARGET_ARCH" \
+         "$AVALANCHE_PATH" --load -f "$AVALANCHE_PATH/Dockerfile"
+
+  echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$COMMIT_HASH-race-$TARGET_ARCH , $avalanchego_dockerhub_repo:$current_branch-race-$TARGET_ARCH"
+  docker buildx build --platform="$PLATFORM" --build-arg="RACE_FLAG=-r" \
+         -t "$avalanchego_dockerhub_repo:$COMMIT_HASH-race-$TARGET_ARCH" -t "$avalanchego_dockerhub_repo:$current_branch-race-$TARGET_ARCH" \
+         "$AVALANCHE_PATH" --load -f "$AVALANCHE_PATH/Dockerfile"
+done
