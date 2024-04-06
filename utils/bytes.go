54896fae1cb34bbb2ac8b885024ef8fba20f8335
Stephen Buttolph <stephen@avalabs.org>
2024-04-06 17:57:11 -0400

    cleanup

--- utils/bytes.go
+++ utils/bytes.go
@@ -23,6 +23,7 @@ const intSize = 32 << (^uint(0) >> 63) // 32 or 64
 // BytesPool tracks buckets of available buffers to be allocated. Each bucket
 // allocates buffers of the following length:
 //
+// 0
 // 1
 // 3
 // 7
@@ -35,44 +36,48 @@ const intSize = 32 << (^uint(0) >> 63) // 32 or 64
 //
 // In order to allocate a buffer of length 19 (for example), we calculate the
 // number of bits required to represent 19 (5). And therefore allocate a slice
-// from bucket 4, which has length 31. This is the bucket which produces the
+// from bucket 5, which has length 31. This is the bucket which produces the
 // smallest slices that are at least length 19.
 //
 // When replacing a buffer of length 19, we calculate the number of bits
-// required to represent 20 (5). And therefore place the slice into bucket 3,
+// required to represent 20 (5). And therefore place the slice into bucket 4,
 // which has length 15. This is the bucket which produces the largest slices
-// that are at most length 19.
-type BytesPool [intSize - 1]*sync.Pool
+// that a length 19 slice can be used for.
+type BytesPool [intSize]*sync.Pool
 
 func NewBytesPool() *BytesPool {
 	var p BytesPool
 	for i := range p {
-		size := uint64(1)<<(i+1) - 1
+		// uint is used here to avoid overflowing int during the shift
+		size := uint(1)<<i - 1
 		p[i] = &sync.Pool{
 			New: func() interface{} {
-				return make([]byte, size)
+				// Sync pool needs to return pointer-like values to avoid memory
+				// allocations.
+				b := make([]byte, size)
+				return &b
 			},
 		}
 	}
 	return &p
 }
 
-func (p *BytesPool) Get(length int) []byte {
-	if length <= 0 {
-		return nil
-	}
-
-	index := bits.Len(uint(length)) - 1 // Round up
-	bytes := p[index].Get().([]byte)
-	return bytes[:length]
+// Get returns a non-nil pointer to a slice with the requested length.
+//
+// It is not guaranteed for the returned bytes to have been zeroed.
+func (p *BytesPool) Get(length int) *[]byte {
+	index := bits.Len(uint(length)) // Round up
+	bytes := p[index].Get().(*[]byte)
+	*bytes = (*bytes)[:length] // Set the length to be the expected value
+	return bytes
 }
 
-func (p *BytesPool) Put(bytes []byte) {
-	size := cap(bytes)
-	if size == 0 {
-		return
-	}
-
-	index := bits.Len(uint(size+1)) - 2 // Round down
+// Put takes ownership of a non-nil pointer to a slice of bytes.
+//
+// Note: this function takes ownership of the underlying array. So, the length
+// of the provided slice is ignored and only its capacity is used.
+func (p *BytesPool) Put(bytes *[]byte) {
+	size := cap(*bytes)
+	index := bits.Len(uint(size+1)) - 1 // Round down
 	p[index].Put(bytes)
 }
--- utils/bytes_test.go
+++ utils/bytes_test.go
@@ -4,6 +4,10 @@
 package utils
 
 import (
+	"math"
+	"math/rand"
+	"strconv"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,14 +17,172 @@ func TestBytesPool(t *testing.T) {
 	require := require.New(t)
 
 	p := NewBytesPool()
-	for i := 127; i < 128; i++ {
-		p.Put(make([]byte, i))
-	}
-	// for j := 0; j < 256; j++ {
-	for i := 127; i < 128; i++ {
+	for i := 0; i < 128; i++ {
 		bytes := p.Get(i)
-		require.Len(bytes, i)
+		require.Len(*bytes, i)
 		p.Put(bytes)
 	}
-	// }
+}
+
+func TestBytesPoolMaxInt(t *testing.T) {
+	require := require.New(t)
+
+	p := NewBytesPool()
+	bytes := p.Get(math.MaxInt32)
+	require.Len(*bytes, math.MaxInt32)
+	p.Put(bytes)
+}
+
+func getBufferFromPool(bufferPool *sync.Pool, size int) *[]byte {
+	buffer := bufferPool.Get().(*[]byte)
+	if cap(*buffer) >= size {
+		// The [] byte we got from the pool is big enough to hold the prefixed key
+		*buffer = (*buffer)[:size]
+	} else {
+		// The []byte from the pool wasn't big enough.
+		// Put it back and allocate a new, bigger one
+		bufferPool.Put(buffer)
+		b := make([]byte, size)
+		buffer = &b
+	}
+	return buffer
+}
+
+func BenchmarkOldBytesPool_Constant(b *testing.B) {
+	sizes := []int{
+		0,
+		8,
+		16,
+		32,
+		64,
+		256,
+		2048,
+	}
+	for _, size := range sizes {
+		b.Run(strconv.Itoa(size), func(b *testing.B) {
+			p := &sync.Pool{
+				New: func() interface{} {
+					b := make([]byte, 0, 256)
+					return &b
+				},
+			}
+			for i := 0; i < b.N; i++ {
+				p.Put(getBufferFromPool(p, size))
+			}
+		})
+	}
+}
+
+func BenchmarkOldBytesPool_Decending(b *testing.B) {
+	p := &sync.Pool{
+		New: func() interface{} {
+			b := make([]byte, 0, 256)
+			return &b
+		},
+	}
+	for i := b.N; i > 0; i-- {
+		p.Put(getBufferFromPool(p, i))
+	}
+}
+
+func BenchmarkOldBytesPool_Ascending(b *testing.B) {
+	p := &sync.Pool{
+		New: func() interface{} {
+			b := make([]byte, 0, 256)
+			return &b
+		},
+	}
+	for i := 0; i < b.N; i++ {
+		p.Put(getBufferFromPool(p, i))
+	}
+}
+
+func BenchmarkOldBytesPool_Random(b *testing.B) {
+	p := &sync.Pool{
+		New: func() interface{} {
+			b := make([]byte, 0, 256)
+			return &b
+		},
+	}
+	for i := 0; i < b.N; i++ {
+		p.Put(getBufferFromPool(p, rand.Intn(100_000))) //#nosec G404
+	}
+}
+
+func BenchmarkBytesPool_Constant(b *testing.B) {
+	sizes := []int{
+		0,
+		8,
+		16,
+		32,
+		64,
+		256,
+		2048,
+	}
+	for _, size := range sizes {
+		b.Run(strconv.Itoa(size), func(b *testing.B) {
+			p := NewBytesPool()
+			for i := 0; i < b.N; i++ {
+				p.Put(p.Get(size))
+			}
+		})
+	}
+}
+
+func BenchmarkBytesPool_Decending(b *testing.B) {
+	p := NewBytesPool()
+	for i := b.N; i > 0; i-- {
+		p.Put(p.Get(i))
+	}
+}
+
+func BenchmarkBytesPool_Ascending(b *testing.B) {
+	p := NewBytesPool()
+	for i := 0; i < b.N; i++ {
+		p.Put(p.Get(i))
+	}
+}
+
+func BenchmarkBytesPool_Random(b *testing.B) {
+	p := NewBytesPool()
+	for i := 0; i < b.N; i++ {
+		p.Put(p.Get(rand.Intn(100_000))) //#nosec G404
+	}
+}
+
+func BenchmarkAllocBytes_Constant(b *testing.B) {
+	sizes := []int{
+		0,
+		8,
+		16,
+		32,
+		64,
+		256,
+		2048,
+	}
+	for _, size := range sizes {
+		b.Run(strconv.Itoa(size), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = make([]byte, size)
+			}
+		})
+	}
+}
+
+func BenchmarkAllocBytes_Decending(b *testing.B) {
+	for i := b.N; i > 0; i-- {
+		_ = make([]byte, i)
+	}
+}
+
+func BenchmarkAllocBytes_Ascending(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = make([]byte, i)
+	}
+}
+
+func BenchmarkAllocBytes_Random(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = make([]byte, rand.Intn(100_000)) //#nosec G404
+	}
 }
