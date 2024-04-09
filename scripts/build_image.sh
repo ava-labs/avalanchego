b83d47f8c1cfd5b9783affe045b70b1aecb0c8ac
Maru Newby <maru.newby@avalabs.org>
2024-04-09 15:38:50 +0200

    fixup: Restore single-arch local builds and add test job

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,12 +2,16 @@
 
 set -euo pipefail
 
-# Cross-platform builds require Docker Buildx and QEMU. buildx should
-# be enabled by default as of this writing, and qemu can be installed
-# on ubuntu as follows:
+# e.g.,
+# ./scripts/build_image.sh                                              # Build local single-arch image
+# DOCKER_REPO=avaplatform/avalanchego ./scripts/build_image.sh          # Build and push multi-arch image to docker hub
+# DOCKER_REPO=localhost:5001/avalanchego ./scripts/build_image.sh       # Build and push multi-arch image to private registry
+
+# Multi-arch builds require Docker Buildx and QEMU. buildx should be
+# enabled by default as of this writing, and qemu can be installed on
+# ubuntu as follows:
 #
 #  sudo apt-get install qemu qemu-user-static
-#
 #
 # Reference: https://docs.docker.com/buildx/working-with-buildx/
 
@@ -22,24 +26,28 @@ if [[ $current_branch == *"-race" ]]; then
   exit 1
 fi
 
-# WARNING: this will use the most recent commit even if there are un-committed changes present
-full_commit_hash="$(git --git-dir="$AVALANCHE_PATH/.git" rev-parse HEAD)"
-# Export to ensure the variable is available to the caller
-export COMMIT_HASH="${full_commit_hash::8}"
+DOCKER_CMD="docker build" # Default to a local single-arch image
+if [[ "${avalanchego_dockerhub_repo}" == *"/"* ]]; then
+  # Assume that the image name containing a '/' (indicating use of a registry) requires pushing a multi-arch image to a registry
+  PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64}"
+  DOCKER_CMD="docker buildx build --push --platform=${PLATFORMS}"
 
-# Build images for supported architectures
-TARGET_ARCHITECTURES=("amd64" "arm64")
-for TARGET_ARCH in "${TARGET_ARCHITECTURES[@]}"; do
-  # Only linux is supported
-  PLATFORM="linux/$TARGET_ARCH"
+  if [[ -n "${DOCKER_USERNAME:-}" ]]; then
+    # Assume the registry requires authentication
+    echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
+  fi
+fi
 
-  echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$COMMIT_HASH-$TARGET_ARCH , $avalanchego_dockerhub_repo:$current_branch-$TARGET_ARCH"
-  docker buildx build --platform="$PLATFORM" \
-         -t "$avalanchego_dockerhub_repo:$COMMIT_HASH-$TARGET_ARCH" -t "$avalanchego_dockerhub_repo:$current_branch-$TARGET_ARCH" \
-         "$AVALANCHE_PATH" --load -f "$AVALANCHE_PATH/Dockerfile"
+echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$commit_hash, $avalanchego_dockerhub_repo:$current_branch"
+${DOCKER_CMD} -t "$avalanchego_dockerhub_repo:$commit_hash" \
+        -t "$avalanchego_dockerhub_repo:$current_branch" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile"
 
-  echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$COMMIT_HASH-race-$TARGET_ARCH , $avalanchego_dockerhub_repo:$current_branch-race-$TARGET_ARCH"
-  docker buildx build --platform="$PLATFORM" --build-arg="RACE_FLAG=-r" \
-         -t "$avalanchego_dockerhub_repo:$COMMIT_HASH-race-$TARGET_ARCH" -t "$avalanchego_dockerhub_repo:$current_branch-race-$TARGET_ARCH" \
-         "$AVALANCHE_PATH" --load -f "$AVALANCHE_PATH/Dockerfile"
-done
+echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$commit_hash-race , $avalanchego_dockerhub_repo:$current_branch-race"
+${DOCKER_CMD} --build-arg="RACE_FLAG=-r" -t "$avalanchego_dockerhub_repo:$commit_hash-race" \
+        -t "$avalanchego_dockerhub_repo:$current_branch-race" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile"
+
+# Only tag the latest image for the master branch when images are pushed to a registry
+if [[ "${avalanchego_dockerhub_repo}" == *"/"* && $current_branch == "master" ]]; then
+  echo "Tagging current avalanchego images as $avalanchego_dockerhub_repo:latest"
+  docker buildx imagetools create -t "$avalanchego_dockerhub_repo:latest" "$avalanchego_dockerhub_repo:$commit_hash"
+fi
