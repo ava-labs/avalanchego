b6ce8484775f5994d344c570842b0fcf0f5aa454
Maru Newby <maru.newby@avalabs.org>
2024-04-10 12:22:46 +0200

    fixup: Cleanup up name of image variables for consistency

--- scripts/tests.build_image.sh
+++ scripts/tests.build_image.sh
@@ -10,9 +10,9 @@ AVALANCHE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
 source "$AVALANCHE_PATH"/scripts/constants.sh
 
 build_and_test() {
-  local repo=$1
+  local image_name=$1
 
-  DOCKER_REPO="$repo" ./scripts/build_image.sh
+  DOCKER_IMAGE="$image_name" ./scripts/build_image.sh
 
   echo "listing images"
   docker images
@@ -20,7 +20,7 @@ build_and_test() {
   local host_arch
   host_arch="$(go env GOARCH)"
 
-  if [[ "$repo" == *"/"* ]]; then
+  if [[ "$image_name" == *"/"* ]]; then
     # Test all arches if testing a multi-arch image
     local arches=("amd64" "arm64")
   else
@@ -29,27 +29,27 @@ build_and_test() {
   fi
 
   # Check all of the images expected to have been built
-  local images=(
-    "$repo:$commit_hash"
-    "$repo:$current_branch"
-    "$repo:$commit_hash-race"
-    "$repo:$current_branch-race"
+  local target_images=(
+    "$image_name:$commit_hash"
+    "$image_name:$current_branch"
+    "$image_name:$commit_hash-race"
+    "$image_name:$current_branch-race"
   )
 
   for arch in "${arches[@]}"; do
-    for image in "${images[@]}"; do
+    for target_image in "${target_images[@]}"; do
       if [[ "$host_arch" == "amd64" && "$arch" == "arm64" && "$image" =~ "-race" ]]; then
         # Error reported when trying to sanity check this configuration:
         #
         #   FATAL: ThreadSanitizer: unsupported VMA range
         #   FATAL: Found 39 - Supported 48
         #
-        echo "skipping sanity check for $image"
+        echo "skipping sanity check for $target_image"
         echo "image is for arm64 and binary is compiled with race detection"
         echo "amd64 github workers are known to run kernels incompatible with these images"
       else
         echo "checking sanity of image $image for $arch by running 'avalanchego --version'"
-        docker run  -t --rm --platform "linux/$arch" "$image" /avalanchego/build/avalanchego --version
+        docker run  -t --rm --platform "linux/$arch" "$target_image" /avalanchego/build/avalanchego --version
       fi
     done
   done
