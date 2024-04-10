26352bb1c9975b274f365636060a95df328ba91a
Maru Newby <maru.newby@avalabs.org>
2024-04-10 12:22:46 +0200

    fixup: Cleanup up name of image variables for consistency

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 # e.g.,
 # ./scripts/build_image.sh                                          # Build local single-arch image
-# DOCKER_REPO=myavalanchego ./scripts/build_image.sh                # Build local single arch image with a custom image name
-# DOCKER_REPO=avaplatform/avalanchego ./scripts/build_image.sh      # Build and push multi-arch image to docker hub
-# DOCKER_REPO=localhost:5001/avalanchego ./scripts/build_image.sh   # Build and push multi-arch image to private registry
-# DOCKER_REPO=localhost:5001/myavalanchego ./scripts/build_image.sh # Build and push multi-arch image to private registry with a custom image name
+# DOCKER_IMAGE=myavalanchego ./scripts/build_image.sh                # Build local single arch image with a custom image name
+# DOCKER_IMAGE=avaplatform/avalanchego ./scripts/build_image.sh      # Build and push multi-arch image to docker hub
+# DOCKER_IMAGE=localhost:5001/avalanchego ./scripts/build_image.sh   # Build and push multi-arch image to private registry
+# DOCKER_IMAGE=localhost:5001/myavalanchego ./scripts/build_image.sh # Build and push multi-arch image to private registry with a custom image name
 
 # Multi-arch builds require Docker Buildx and QEMU. buildx should be enabled by
 # default as of this writing, and qemu can be installed on ubuntu as follows:
@@ -35,9 +35,7 @@ fi
 # The published name should be 'avaplatform/avalanchego', but to avoid unintentional
 # pushes it is defaulted to 'avalanchego' (without the registry name) which is a
 # local-only name.
-#
-# TODO(marun) Rename both avalanchego_dockerhub_repo and DOCKER_REPO to IMAGE_NAME for clarity
-avalanchego_dockerhub_repo=${DOCKER_REPO:-"avalanchego"}
+DOCKER_IMAGE=${DOCKER_IMAGE:-"avalanchego"}
 
 DOCKER_CMD="docker buildx build"
 
@@ -46,7 +44,7 @@ DOCKER_CMD="docker buildx build"
 # registry.
 #
 # If an image name does not include a slash, only a single-arch image can be built.
-if [[ "${avalanchego_dockerhub_repo}" == *"/"* ]]; then
+if [[ "${DOCKER_IMAGE}" == *"/"* ]]; then
   PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64}"
   DOCKER_CMD="${DOCKER_CMD} --push --platform=${PLATFORMS}"
 
@@ -61,16 +59,16 @@ else
   DOCKER_CMD="${DOCKER_CMD} --load"
 fi
 
-echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$commit_hash , $avalanchego_dockerhub_repo:$current_branch"
-${DOCKER_CMD} -t "$avalanchego_dockerhub_repo:$commit_hash" \
-        -t "$avalanchego_dockerhub_repo:$current_branch" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile"
+echo "Building Docker Image with tags: $DOCKER_IMAGE:$commit_hash , $DOCKER_IMAGE:$current_branch"
+${DOCKER_CMD} -t "$DOCKER_IMAGE:$commit_hash" -t "$DOCKER_IMAGE:$current_branch" \
+              "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile"
 
-echo "Building Docker Image with tags: $avalanchego_dockerhub_repo:$commit_hash-race , $avalanchego_dockerhub_repo:$current_branch-race"
-${DOCKER_CMD} --build-arg="RACE_FLAG=-r" -t "$avalanchego_dockerhub_repo:$commit_hash-race" \
-        -t "$avalanchego_dockerhub_repo:$current_branch-race" "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile"
+echo "Building Docker Image with tags: $DOCKER_IMAGE:$commit_hash-race , $DOCKER_IMAGE:$current_branch-race"
+${DOCKER_CMD} --build-arg="RACE_FLAG=-r" -t "$DOCKER_IMAGE:$commit_hash-race" -t "$DOCKER_IMAGE:$current_branch-race" \
+              "$AVALANCHE_PATH" -f "$AVALANCHE_PATH/Dockerfile"
 
 # Only tag the latest image for the master branch when images are pushed to a registry
-if [[ "${avalanchego_dockerhub_repo}" == *"/"* && $current_branch == "master" ]]; then
-  echo "Tagging current avalanchego images as $avalanchego_dockerhub_repo:latest"
-  docker buildx imagetools create -t "$avalanchego_dockerhub_repo:latest" "$avalanchego_dockerhub_repo:$commit_hash"
+if [[ "${DOCKER_IMAGE}" == *"/"* && $current_branch == "master" ]]; then
+  echo "Tagging current avalanchego images as $DOCKER_IMAGE:latest"
+  docker buildx imagetools create -t "$DOCKER_IMAGE:latest" "$DOCKER_IMAGE:$commit_hash"
 fi
