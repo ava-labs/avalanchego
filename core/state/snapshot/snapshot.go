cd53e861ede055cb0f701f73eea71c96599df6f5
Darioush Jalali <darioush.jalali@avalabs.org>
2024-04-18 11:45:50 -0700

    snapshot: fix race in abort/generate (#1155)

--- core/state/snapshot/snapshot.go
+++ core/state/snapshot/snapshot.go
@@ -568,6 +568,9 @@ func (t *Tree) AbortGeneration() {
 // for it to shutdown before returning (if it is running). This call should not
 // be made concurrently.
 func (dl *diskLayer) abortGeneration() bool {
+	dl.lock.Lock()
+	defer dl.lock.Unlock()
+
 	// Store ideal time for abort to get better estimate of load
 	//
 	// Note that we set this time regardless if abortion was skipped otherwise we
