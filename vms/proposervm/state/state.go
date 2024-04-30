34af3c2b5f8a9ec10dd81b9341d5b4ffa52fa7b1
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-04-29 23:51:51 -0400

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- vms/proposervm/state/state.go
+++ vms/proposervm/state/state.go
@@ -4,6 +4,8 @@
 package state
 
 import (
+	"fmt"
+
 	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/ava-labs/avalanchego/database"
@@ -43,7 +45,7 @@ func New(db *versiondb.Database) (State, error) {
 		HeightIndex: NewHeightIndex(heightDB, db),
 	}
 
-	return s, s.pruneVerifiedBlocks(db)
+	return s, s.pruneProcessingBlocks(db)
 }
 
 func NewMetered(db *versiondb.Database, namespace string, metrics prometheus.Registerer) (State, error) {
@@ -62,48 +64,52 @@ func NewMetered(db *versiondb.Database, namespace string, metrics prometheus.Reg
 		HeightIndex: NewHeightIndex(heightDB, db),
 	}
 
-	return s, s.pruneVerifiedBlocks(db)
+	return s, s.pruneProcessingBlocks(db)
 }
 
-func (s *state) pruneVerifiedBlocks(db *versiondb.Database) error {
+func (s *state) pruneProcessingBlocks(db *versiondb.Database) error {
 	preferredID, err := s.chainState.GetPreference()
 	switch {
 	case err == database.ErrNotFound:
 		return nil
 	case err != nil:
-		return err
+		return fmt.Errorf("failed to get preference: %w", err)
 	}
 
-	// Prune all verified blocks that are not in the preferred chain
+	// Prune all processing blocks that are not in the preferred chain
 	preferredBlkIDs := set.Set[ids.ID]{}
 	preferredBlk, status, err := s.BlockState.GetBlock(preferredID)
 	if err != nil {
-		return nil
+		return fmt.Errorf("failed to get block: %w", err)
 	}
 
 	for status == choices.Processing {
 		preferredBlkIDs.Add(preferredBlk.ID())
 		preferredBlk, status, err = s.BlockState.GetBlock(preferredBlk.ParentID())
 		if err != nil {
-			return nil
+			return fmt.Errorf("failed to get block: %w", err)
 		}
 	}
 
-	iter := s.chainState.db.NewIteratorWithPrefix([]byte{verifiedByte})
+	iter := s.chainState.processingBlockDB.NewIterator()
 	defer iter.Release()
 
 	for iter.Next() {
-		blkID := ids.ID(iter.Key()[1:]) // Drop the one byte prefix
+		blkID, err := ids.ToID(iter.Key())
+		if err != nil {
+			return fmt.Errorf("failed to parse processing block id: %w", err)
+		}
+
 		if preferredBlkIDs.Contains(blkID) {
 			continue
 		}
 
 		if err := s.chainState.DeleteProcessingBlock(blkID); err != nil {
-			return nil
+			return fmt.Errorf("failed to delete processing block from index: %w", err)
 		}
 
 		if err := s.BlockState.DeleteBlock(blkID); err != nil {
-			return nil
+			return fmt.Errorf("failed to delete processing block: %w", err)
 		}
 	}
 
