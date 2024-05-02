fdd33f12ac93a4714f80dacf93a4834f8c8e33ab
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-05-02 13:31:13 -0400

    clean diff
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- vms/proposervm/batched_vm.go
+++ vms/proposervm/batched_vm.go
@@ -143,7 +143,6 @@ func (vm *VM) BatchedParseBlock(ctx context.Context, blks [][]byte) ([]snowman.B
 				SignedBlock: statelessSignedBlock,
 				postForkCommonComponents: postForkCommonComponents{
 					vm:       vm,
-					outerBlk: statelessSignedBlock,
 					innerBlk: innerBlks[innerBlocksIndex],
 					status:   status,
 				},
@@ -153,7 +152,6 @@ func (vm *VM) BatchedParseBlock(ctx context.Context, blks [][]byte) ([]snowman.B
 				Block: statelessBlk,
 				postForkCommonComponents: postForkCommonComponents{
 					vm:       vm,
-					outerBlk: statelessSignedBlock,
 					innerBlk: innerBlks[innerBlocksIndex],
 					status:   status,
 				},
