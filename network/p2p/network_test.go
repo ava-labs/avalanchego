f73e0fb8fd00c539ff64286b0b39b28af8cb90fb
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-05-13 17:26:48 -0400

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- network/p2p/network_test.go
+++ network/p2p/network_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/ava-labs/avalanchego/snow/engine/common"
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/utils/set"
+	"github.com/ava-labs/avalanchego/version"
 )
 
 const (
@@ -22,10 +23,14 @@ const (
 	handlerPrefix = byte(handlerID)
 )
 
-var errFoo = &common.AppError{
-	Code:    123,
-	Message: "foo",
-}
+var (
+	errFoo = &common.AppError{
+		Code:    123,
+		Message: "foo",
+	}
+
+	_ SamplingFilter = (*testFilter)(nil)
+)
 
 func TestMessageRouting(t *testing.T) {
 	require := require.New(t)
@@ -478,6 +483,93 @@ func TestAppRequestDuplicateRequestIDs(t *testing.T) {
 	require.ErrorIs(err, ErrRequestPending)
 }
 
+// AppRequestAny should send a request to a random peer that matches the
+// configured SamplingFilter
+func TestClientAppRequestAny(t *testing.T) {
+	peerID := ids.GenerateTestNodeID()
+
+	tests := []struct {
+		name          string
+		connected     []ids.NodeID
+		filters       []SamplingFilter
+		expectedPeers []ids.NodeID
+		expected      error
+	}{
+		{
+			name:     "no peers - no peers connected",
+			expected: ErrNoPeers,
+		},
+		{
+			name:      "no peers - no peers matching filter",
+			connected: []ids.NodeID{peerID},
+			filters: []SamplingFilter{
+				testFilter{},
+			},
+			expected: ErrNoPeers,
+		},
+		{
+			name:          "has peers",
+			connected:     []ids.NodeID{peerID},
+			expectedPeers: []ids.NodeID{peerID},
+		},
+		{
+			name:      "has peers in filter",
+			connected: []ids.NodeID{peerID},
+			filters: []SamplingFilter{
+				testFilter{
+					nodeIDs: set.Of(peerID),
+				},
+			},
+			expectedPeers: []ids.NodeID{peerID},
+		},
+		{
+			name:      "has peers in multiple filters",
+			connected: []ids.NodeID{peerID},
+			filters: []SamplingFilter{
+				testFilter{
+					nodeIDs: set.Of(peerID),
+				},
+				testFilter{
+					nodeIDs: set.Of(peerID),
+				},
+			},
+			expectedPeers: []ids.NodeID{peerID},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+
+			sent := set.Set[ids.NodeID]{}
+			sender := &common.SenderTest{
+				SendAppRequestF: func(_ context.Context, nodeIDs set.Set[ids.NodeID], _ uint32, _ []byte) error {
+					sent = nodeIDs
+					return nil
+				},
+			}
+
+			n, err := NewNetwork(logging.NoLog{}, sender, prometheus.NewRegistry(), "")
+			require.NoError(err)
+			for _, peer := range tt.connected {
+				require.NoError(n.Connected(context.Background(), peer, &version.Application{}))
+			}
+
+			client := n.NewClient(1, WithSamplingFilters(tt.filters...))
+
+			err = client.AppRequestAny(context.Background(), []byte("foobar"), nil)
+			require.ErrorIs(err, tt.expected)
+			require.Subset(tt.expectedPeers, sent.List())
+
+			if len(tt.expectedPeers) > 0 {
+				require.Len(sent, 1)
+			} else {
+				require.Empty(sent)
+			}
+		})
+	}
+}
+
 // Tests that a given protocol can have more than one client
 func TestMultipleClients(t *testing.T) {
 	require := require.New(t)
@@ -486,4 +578,12 @@ func TestMultipleClients(t *testing.T) {
 	require.NoError(err)
 	_ = n.NewClient(0)
 	_ = n.NewClient(0)
+}
+
+type testFilter struct {
+	nodeIDs set.Set[ids.NodeID]
+}
+
+func (t testFilter) Filter(_ context.Context, nodeID ids.NodeID) bool {
+	return t.nodeIDs.Contains(nodeID)
 }
