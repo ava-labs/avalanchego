a4f9715825fa4b5482ec03408625e1382cdcf5b7
Maru Newby <maru.newby@avalabs.org>
2024-05-13 10:55:47 -0700

    fixup: bump the funding transfer further

--- tests/e2e/x/transfer/virtuous.go
+++ tests/e2e/x/transfer/virtuous.go
@@ -173,7 +173,7 @@ var _ = e2e.DescribeXChainSerial("[Virtuous Transfer Tx AVAX]", func() {
 				// Special-case the transfer amount from the funded key to ensure
 				// that new keys have enough funds to perform their own transfers.
 				if shortAddrs[fromIdx] == env.PreFundedKey.Address() {
-					amountToTransfer = units.Avax
+					amountToTransfer = 10 * units.Avax
 				}
 
 				senderNewBal := senderOrigBal - amountToTransfer - xContext.BaseTxFee
