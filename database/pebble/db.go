c68a07f7dc4008fb31e2640df5b04184da536eb1
Darioush Jalali <darioush.jalali@avalabs.org>
2024-05-15 10:38:14 -0700

    bump pebble and coreth rc

--- database/pebble/db.go
+++ database/pebble/db.go
@@ -88,7 +88,7 @@ func New(file string, configBytes []byte, log logging.Logger, _ string, _ promet
 		Comparer:                    pebble.DefaultComparer,
 		WALBytesPerSync:             cfg.WALBytesPerSync,
 		MemTableStopWritesThreshold: cfg.MemTableStopWritesThreshold,
-		MemTableSize:                cfg.MemTableSize,
+		MemTableSize:                uint64(cfg.MemTableSize),
 		MaxOpenFiles:                cfg.MaxOpenFiles,
 		MaxConcurrentCompactions:    func() int { return cfg.MaxConcurrentCompactions },
 	}
@@ -203,7 +203,10 @@ func (db *Database) Compact(start []byte, end []byte) error {
 		// The database.Database spec treats a nil [limit] as a key after all keys
 		// but pebble treats a nil [limit] as a key before all keys in Compact.
 		// Use the greatest key in the database as the [limit] to get the desired behavior.
-		it := db.pebbleDB.NewIter(&pebble.IterOptions{})
+		it, err := db.pebbleDB.NewIter(&pebble.IterOptions{})
+		if err != nil {
+			return updateError(err)
+		}
 
 		if !it.Last() {
 			// The database is empty.
@@ -248,9 +251,18 @@ func (db *Database) NewIteratorWithStartAndPrefix(start, prefix []byte) database
 		}
 	}
 
+	it, err := db.pebbleDB.NewIter(keyRange(start, prefix))
+	if err != nil {
+		return &iter{
+			db:     db,
+			closed: true,
+			err:    updateError(err),
+		}
+	}
+
 	iter := &iter{
 		db:   db,
-		iter: db.pebbleDB.NewIter(keyRange(start, prefix)),
+		iter: it,
 	}
 	db.openIterators.Add(iter)
 	return iter
