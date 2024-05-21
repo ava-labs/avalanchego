6a894d0e2e762e39970d6094f8139bb25449ce93
Dhruba Basu <7675102+dhrubabasu@users.noreply.github.com>
2024-05-21 14:43:41 -0400

    Prevent unnecessary bandwidth from activated ACPs (#3031)

--- utils/constants/acps.go
+++ utils/constants/acps.go
@@ -6,11 +6,10 @@ package constants
 import "github.com/ava-labs/avalanchego/utils/set"
 
 var (
-	// CurrentACPs is the set of ACPs that are currently, at the time of
-	// release, marked as implementable and not activated.
+	// ActivatedACPs is the set of ACPs that are activated.
 	//
 	// See: https://github.com/orgs/avalanche-foundation/projects/1
-	CurrentACPs = set.Of[uint32](
+	ActivatedACPs = set.Of[uint32](
 		23, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/23-p-chain-native-transfers/README.md
 		24, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/24-shanghai-eips/README.md
 		25, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/25-vm-application-errors/README.md
@@ -20,14 +19,12 @@ var (
 		62, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/62-disable-addvalidatortx-and-adddelegatortx/README.md
 	)
 
-	// ScheduledACPs are the ACPs incuded into the next upgrade.
-	ScheduledACPs = set.Of[uint32](
-		23, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/23-p-chain-native-transfers/README.md
-		24, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/24-shanghai-eips/README.md
-		25, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/25-vm-application-errors/README.md
-		30, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/30-avalanche-warp-x-evm/README.md
-		31, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/31-enable-subnet-ownership-transfer/README.md
-		41, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/41-remove-pending-stakers/README.md
-		62, // https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/62-disable-addvalidatortx-and-adddelegatortx/README.md
-	)
+	// CurrentACPs is the set of ACPs that are currently, at the time of
+	// release, marked as implementable and not activated.
+	//
+	// See: https://github.com/orgs/avalanche-foundation/projects/1
+	CurrentACPs = set.Of[uint32]()
+
+	// ScheduledACPs are the ACPs included into the next upgrade.
+	ScheduledACPs = set.Of[uint32]()
 )
