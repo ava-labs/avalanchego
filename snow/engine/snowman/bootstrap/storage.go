37766e8aad40a4480091902ce9f4b7c95f7cf5d8
Stephen Buttolph <stephen@avalabs.org>
2024-05-22 00:10:35 -0400

    less

--- snow/engine/snowman/bootstrap/storage.go
+++ snow/engine/snowman/bootstrap/storage.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	batchWritePeriod      = 64
-	iteratorReleasePeriod = 25_000
+	iteratorReleasePeriod = 50_000
 	logPeriod             = 5 * time.Second
 	minBlocksToCompact    = 5000
 )
