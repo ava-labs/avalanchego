53ae50c0702145c3655c7e8ed56bc51a749700f1
Aaron Buchwald <aaron.buchwald56@gmail.com>
2024-06-06 11:31:27 -0400

    Add test coverage for multiple termination conditions over unary snowflake

--- snow/consensus/snowball/unary_snowflake_test.go
+++ snow/consensus/snowball/unary_snowflake_test.go
@@ -9,10 +9,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func UnarySnowflakeStateTest(t *testing.T, sf *unarySnowflake, expectedConfidence []int, expectedFinalized bool) {
+func UnarySnowflakeStateTest(t *testing.T, sf *unarySnowflake, expectedConfidences []int, expectedFinalized bool) {
 	require := require.New(t)
 
-	require.Equal(expectedConfidence, sf.confidence)
+	require.Equal(expectedConfidences, sf.confidence)
 	require.Equal(expectedFinalized, sf.Finalized())
 }
 
@@ -66,4 +66,106 @@ func TestUnarySnowflake(t *testing.T) {
 
 	sf.RecordPoll(alphaConfidence)
 	UnarySnowflakeStateTest(t, &sf, []int{1}, true)
+}
+
+func TestUnarySnowflakeErrorDriven(t *testing.T) {
+	alphaPreference := 3
+	terminationConditions := []terminationCondition{
+		{
+			alphaConfidence: 3,
+			beta:            3,
+		},
+		{
+			alphaConfidence: 4,
+			beta:            2,
+		},
+		{
+			alphaConfidence: 5,
+			beta:            1,
+		},
+	}
+
+	for i, terminationCondition := range terminationConditions {
+		sf := newUnarySnowflake(alphaPreference, terminationConditions)
+
+		for poll := 1; poll <= terminationCondition.beta; poll++ {
+			sf.RecordPoll(terminationCondition.alphaConfidence)
+
+			expectedConfidences := make([]int, len(terminationConditions))
+			for j := 0; j < i+1; j++ {
+				expectedConfidences[j] = poll
+			}
+			UnarySnowflakeStateTest(t, &sf, expectedConfidences, poll >= terminationCondition.beta)
+		}
+	}
+}
+
+func TestUnarySnowflakeErrorDrivenReset(t *testing.T) {
+	alphaPreference := 3
+	terminationConditions := []terminationCondition{
+		{
+			alphaConfidence: 3,
+			beta:            4,
+		},
+		{
+			alphaConfidence: 4,
+			beta:            3,
+		},
+		{
+			alphaConfidence: 5,
+			beta:            2,
+		},
+	}
+
+	for i, terminationCondition := range terminationConditions {
+		sf := newUnarySnowflake(alphaPreference, terminationConditions)
+
+		// Accumulate confidence up to 1 less than beta, reset, and confirm
+		// expected behavior from fresh state.
+		for poll := 0; poll < terminationCondition.beta-1; poll++ {
+			sf.RecordPoll(terminationCondition.alphaConfidence)
+		}
+		sf.RecordUnsuccessfulPoll()
+		zeroConfidence := make([]int, len(terminationConditions))
+		UnarySnowflakeStateTest(t, &sf, zeroConfidence, false)
+
+		for poll := 0; poll < terminationCondition.beta; poll++ {
+			sf.RecordPoll(terminationCondition.alphaConfidence)
+
+			expectedConfidences := make([]int, len(terminationConditions))
+			for j := 0; j < i+1; j++ {
+				expectedConfidences[j] = poll + 1
+			}
+			UnarySnowflakeStateTest(t, &sf, expectedConfidences, poll+1 >= terminationCondition.beta)
+		}
+	}
+}
+
+func TestUnarySnowflakeErrorDrivenResetHighestAlphaConfidence(t *testing.T) {
+	alphaPreference := 3
+	terminationConditions := []terminationCondition{
+		{
+			alphaConfidence: 3,
+			beta:            4,
+		},
+		{
+			alphaConfidence: 4,
+			beta:            3,
+		},
+		{
+			alphaConfidence: 5,
+			beta:            2,
+		},
+	}
+
+	sf := newUnarySnowflake(alphaPreference, terminationConditions)
+
+	sf.RecordPoll(5)
+	UnarySnowflakeStateTest(t, &sf, []int{1, 1, 1}, false)
+	sf.RecordPoll(4)
+	UnarySnowflakeStateTest(t, &sf, []int{2, 2, 0}, false)
+	sf.RecordPoll(3)
+	UnarySnowflakeStateTest(t, &sf, []int{3, 0, 0}, false)
+	sf.RecordPoll(5)
+	UnarySnowflakeStateTest(t, &sf, []int{4, 0, 0}, true)
 }
