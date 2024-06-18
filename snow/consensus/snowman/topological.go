2e72c7c29c498e7ffa5bb31ff18495e14e2cdfb7
Stephen Buttolph <stephen@avalabs.org>
2024-06-18 11:42:55 -0400

    Remove .Status() from .Accepted() (#3124)

--- snow/consensus/snowman/topological.go
+++ snow/consensus/snowman/topological.go
@@ -126,7 +126,7 @@ func (ts *Topological) Initialize(
 	ts.lastAcceptedID = lastAcceptedID
 	ts.lastAcceptedHeight = lastAcceptedHeight
 	ts.blocks = map[ids.ID]*snowmanBlock{
-		lastAcceptedID: {params: ts.params},
+		lastAcceptedID: {t: ts},
 	}
 	ts.preferredHeights = make(map[uint64]ids.ID)
 	ts.preference = lastAcceptedID
@@ -162,8 +162,8 @@ func (ts *Topological) Add(blk Block) error {
 	// add the block as a child of its parent, and add the block to the tree
 	parentNode.AddChild(blk)
 	ts.blocks[blkID] = &snowmanBlock{
-		params: ts.params,
-		blk:    blk,
+		t:   ts,
+		blk: blk,
 	}
 
 	// If we are extending the preference, this is the new preference
@@ -278,7 +278,12 @@ func (ts *Topological) RecordPoll(ctx context.Context, voteBag bag.Bag[ids.ID]) 
 
 	// Runtime = |live set| ; Space = Constant
 	// Traverse from the preferred ID to the last accepted ancestor.
-	for block := startBlock; !block.Accepted(); {
+	//
+	// It is guaranteed that the first decided block we encounter is the last
+	// accepted block because the startBlock is the preferred block. The
+	// preferred block is guaranteed to either be the last accepted block or
+	// extend the accepted chain.
+	for block := startBlock; !block.Decided(); {
 		blkID := block.blk.ID()
 		ts.preferredIDs.Add(blkID)
 		ts.preferredHeights[block.blk.Height()] = blkID
@@ -349,7 +354,7 @@ func (ts *Topological) calculateInDegree(votes bag.Bag[ids.ID]) {
 		}
 
 		// If the vote is for the last accepted block, the vote is dropped
-		if votedBlock.Accepted() {
+		if votedBlock.Decided() {
 			continue
 		}
 
@@ -373,7 +378,7 @@ func (ts *Topological) calculateInDegree(votes bag.Bag[ids.ID]) {
 
 		// iterate through all the block's ancestors and set up the inDegrees of
 		// the blocks
-		for n := ts.blocks[parentID]; !n.Accepted(); n = ts.blocks[parentID] {
+		for n := ts.blocks[parentID]; !n.Decided(); n = ts.blocks[parentID] {
 			parentID = n.blk.Parent()
 
 			// Increase the inDegree by one
@@ -417,7 +422,7 @@ func (ts *Topological) pushVotes() []votes {
 
 		// If the block is accepted, then we don't need to push votes to the
 		// parent block
-		if block.Accepted() {
+		if block.Decided() {
 			continue
 		}
 
