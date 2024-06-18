71728019e6993a8b8f7c7f3ed03e83dbf67bd7c9
Maru Newby <maru.newby@avalabs.org>
2024-06-18 23:37:40 +0200

    Emit version in JSON format for --json-version

--- version/string.go
+++ version/string.go
@@ -4,8 +4,10 @@
 package version
 
 import (
+	"encoding/json"
 	"fmt"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
@@ -13,20 +15,28 @@ var (
 	// String is displayed when CLI arg --version is used
 	String string
 
+	// String is displayed when CLI arg --json-version is used
+	JSONString string
+
 	// GitCommit is set in the build script at compile time
 	GitCommit string
 )
 
 func init() {
+	flags := map[string]string{}
 	format := "%s [database=%s, rpcchainvm=%d"
 	args := []interface{}{
 		CurrentApp,
 		CurrentDatabase,
 		RPCChainVMProtocol,
 	}
+	flags["app"] = CurrentApp.String()
+	flags["database"] = CurrentDatabase.String()
+	flags["rpcchainvm"] = strconv.FormatUint(uint64(RPCChainVMProtocol), 10)
 	if GitCommit != "" {
 		format += ", commit=%s"
 		args = append(args, GitCommit)
+		flags["commit"] = GitCommit
 	}
 
 	// add golang version
@@ -34,7 +44,14 @@ func init() {
 	goVersionNumber := strings.TrimPrefix(goVersion, "go")
 	format += ", go=%s"
 	args = append(args, goVersionNumber)
+	flags["go"] = goVersionNumber
 
 	format += "]\n"
 	String = fmt.Sprintf(format, args...)
+
+	jsonBytes, err := json.MarshalIndent(flags, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	JSONString = string(jsonBytes) + "\n"
 }
