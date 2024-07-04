02b44c07b1cb03cc94dbb5b8eff75c8d497b373e
Arran Schlosberg <me@arranschlosberg.com>
2024-07-04 18:49:07 +0100

    fix: exclude files needed for compiling AvalancheGo binary

--- snow/engine/avalanche/vertex/test_parser.go
+++ snow/engine/avalanche/vertex/test_parser.go
@@ -1,7 +1,8 @@
 // Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-//go:build test
+// TODO: https://github.com/ava-labs/avalanchego/issues/3174
+//go:build test || !test
 
 package vertex
 
