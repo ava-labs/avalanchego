e410926e7b0c91c3c17b48a2fdcef21516df1116
Arran Schlosberg <me@arranschlosberg.com>
2024-07-05 09:46:17 +0100

    feat(linter): detect files importing `testify/*`

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -88,19 +88,25 @@ function test_interface_compliance_nil {
 
 function test_import_testing_only_in_tests {
   ROOT=$( git rev-parse --show-toplevel )
-  NON_TEST_GO_FILES=$( find "${ROOT}" -iname '*.go' ! -iname '*_test.go' | sort );
+  NON_TEST_GO_FILES=$( find "${ROOT}" -iname '*.go' ! -iname '*_test.go');
+
   IMPORT_TESTING=$( echo "${NON_TEST_GO_FILES}" | xargs grep -lP '^\s*(import\s+)?"testing"');
+  IMPORT_TESTIFY=$( echo "${NON_TEST_GO_FILES}" | xargs grep -l '"github.com/stretchr/testify');
+  # TODO(arr4n): send a PR to add support for build tags in `mockgen` and then enable this.
+  # IMPORT_GOMOCK=$( echo "${NON_TEST_GO_FILES}" | xargs grep -l '"go.uber.org/mock');
+  HAVE_TEST_LOGIC=$( printf "${IMPORT_TESTING}\n${IMPORT_TESTIFY}" );
+
   TAGGED_AS_TEST=$( echo "${NON_TEST_GO_FILES}" | xargs grep -lP '^\/\/go:build\s+(.+(,|\s+))?test[,\s]?');
 
-  # -3 suppresses files that import "testing" and have the "test" build tag
-  # -2 suppresses files that are tagged despite not importing "testing"
-  UNTAGGED=$( comm -23 <( echo "${IMPORT_TESTING}" ) <( echo "${TAGGED_AS_TEST}" ) );
+  # -3 suppresses files that have test logic and have the "test" build tag
+  # -2 suppresses files that are tagged despite not having detectable test logic
+  UNTAGGED=$( comm -23 <( echo "${HAVE_TEST_LOGIC}" | sort -u ) <( echo "${TAGGED_AS_TEST}" | sort -u ) );
   if [ -z "${UNTAGGED}" ];
   then
     return 0;
   fi
 
-  echo "Non-test Go files importing \"testing\" MUST have '//go:build test' tag:";
+  echo "Non-test Go files importing test-only packages MUST have '//go:build test' tag:";
   echo "${UNTAGGED}";
   return 1;
 }
