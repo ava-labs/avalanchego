37839ec897bd9302a09315cf181cf31556151423
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2024-07-16 13:18:47 -0400

    rollback correctly.

--- scripts/protobuf_codegen.sh
+++ scripts/protobuf_codegen.sh
@@ -8,14 +8,14 @@ if ! [[ "$0" =~ scripts/protobuf_codegen.sh ]]; then
 fi
 
 ## ensure the correct version of "buf" is installed
-BUF_VERSION='1.34.0'
+BUF_VERSION='1.31.0'
 if [[ $(buf --version | cut -f2 -d' ') != "${BUF_VERSION}" ]]; then
   echo "could not find buf ${BUF_VERSION}, is it installed + in PATH?"
   exit 255
 fi
 
 ## install "protoc-gen-go"
-PROTOC_GEN_GO_VERSION='v1.31.0'
+PROTOC_GEN_GO_VERSION='v1.33.0'
 go install -v google.golang.org/protobuf/cmd/protoc-gen-go@${PROTOC_GEN_GO_VERSION}
 if [[ $(protoc-gen-go --version | cut -f2 -d' ') != "${PROTOC_GEN_GO_VERSION}" ]]; then
   # e.g., protoc-gen-go v1.28.1
