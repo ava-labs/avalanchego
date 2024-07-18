5509e9cdb5fe8b07b18933399359e9658ecd9d34
Stephen Buttolph <stephen@avalabs.org>
2024-07-18 11:35:50 -0400

    Implement ACP-103 fee calculations

--- vms/components/fee/gas.go
+++ vms/components/fee/gas.go
@@ -6,8 +6,12 @@ package fee
 import (
 	"math"
 
+	"github.com/holiman/uint256"
+
 	safemath "github.com/ava-labs/avalanchego/utils/math"
 )
+
+var maxUint64 = new(uint256.Int).SetUint64(math.MaxUint64)
 
 type (
 	Gas      uint64
@@ -38,30 +42,61 @@ func (g Gas) SubPerSecond(gasPerSecond Gas, seconds uint64) Gas {
 	return Gas(totalGas)
 }
 
-// MulExp returns an approximation of g*e^(excess / gasConversionConstant)
+// MulExp returns an approximation of g*e^(excess / excessConversionConstant)
+//
+// This implements the EIP-4844 fake exponential formula:
+//
+//	def fake_exponential(factor: int, numerator: int, denominator: int) -> int:
+//		i = 1
+//		output = 0
+//		numerator_accum = factor * denominator
+//		while numerator_accum > 0:
+//			output += numerator_accum
+//			numerator_accum = (numerator_accum * numerator) // (denominator * i)
+//			i += 1
+//		return output // denominator
+//
+// This implementation is optimized with the knowledge that any value greater
+// than MaxUint64 gets returned as MaxUint64. This means that every intermediate
+// value is guaranteed to be at most MaxUint192. So, we can safely use
+// uint256.Int.
+//
+// This function does not perform any memory allocations.
+//
+//nolint:dupword // This comment is copied from the EIP-4844 specification
 func (g GasPrice) MulExp(
 	excess Gas,
-	gasConversionConstant GasPrice,
+	excessConversionConstant Gas,
 ) GasPrice {
 	var (
-		iteration      GasPrice = 1
-		output         GasPrice
-		numeratorAccum = g * gasConversionConstant
-	)
-	for numeratorAccum > 0 {
-		output += numeratorAccum
-		numeratorAccum = (numeratorAccum * GasPrice(excess)) / (gasConversionConstant * iteration)
-		iteration++
-	}
-	return output / gasConversionConstant
-}
+		numerator   uint256.Int
+		denominator uint256.Int
 
-// def fake_exponential(factor: int, numerator: int, denominator: int) -> int:
-//     i = 1
-//     output = 0
-//     numerator_accum = factor * denominator
-//     while numerator_accum > 0:
-//         output += numerator_accum
-//         numerator_accum = (numerator_accum * numerator) // (denominator * i)
-//         i += 1
-//     return output // denominator
+		i              uint256.Int
+		output         uint256.Int
+		numeratorAccum uint256.Int
+
+		maxOutput uint256.Int // range is [0, MaxUint128]
+	)
+	numerator.SetUint64(uint64(excess))                     // range is [0, MaxUint64]
+	denominator.SetUint64(uint64(excessConversionConstant)) // range is [0, MaxUint64]
+
+	i.SetOne()
+	numeratorAccum.SetUint64(uint64(g))               // range is [0, MaxUint64]
+	numeratorAccum.Mul(&numeratorAccum, &denominator) // range is [0, MaxUint128]
+
+	maxOutput.Mul(&denominator, maxUint64) // range is [0, MaxUint128]
+	for numeratorAccum.Sign() > 0 {
+		output.Add(&output, &numeratorAccum) // range is [0, MaxUint192+MaxUint128]
+		if output.Cmp(&maxOutput) >= 0 {
+			return math.MaxUint64
+		}
+		// maxOutput < MaxUint128 so numeratorAccum < MaxUint128.
+		numeratorAccum.Mul(&numeratorAccum, &numerator) // range is [0, MaxUint192]
+		numeratorAccum.Div(&numeratorAccum, &denominator)
+		numeratorAccum.Div(&numeratorAccum, &i)
+
+		i.AddUint64(&i, 1)
+	}
+	return GasPrice(output.Div(&output, &denominator).Uint64())
+}
--- vms/components/fee/state.go
+++ vms/components/fee/state.go
@@ -3,17 +3,51 @@
 
 package fee
 
+import (
+	"errors"
+	"math"
+
+	safemath "github.com/ava-labs/avalanchego/utils/math"
+)
+
+var ErrInsufficientCapacity = errors.New("insufficient capacity")
+
 type State struct {
 	Capacity Gas
 	Excess   Gas
 }
 
-func (s State) AdvanceTime(c Config, duration uint64) State {
+func (s State) AdvanceTime(
+	maxGasCapacity Gas,
+	maxGasPerSecond Gas,
+	targetGasPerSecond Gas,
+	duration uint64,
+) State {
 	return State{
 		Capacity: min(
-			s.Capacity.AddPerSecond(c.MaxGasPerSecond, duration),
-			c.MaxGasCapacity,
+			s.Capacity.AddPerSecond(maxGasPerSecond, duration),
+			maxGasCapacity,
 		),
-		Excess: s.Excess.SubPerSecond(c.TargetGasPerSecond, duration),
+		Excess: s.Excess.SubPerSecond(targetGasPerSecond, duration),
 	}
+}
+
+func (s State) ConsumeGas(gas Gas) (State, error) {
+	newCapacity, err := safemath.Sub(uint64(s.Capacity), uint64(gas))
+	if err != nil {
+		return State{}, ErrInsufficientCapacity
+	}
+
+	newExcess, err := safemath.Add64(uint64(s.Excess), uint64(gas))
+	if err != nil {
+		return State{
+			Capacity: Gas(newCapacity),
+			Excess:   math.MaxUint64,
+		}, nil
+	}
+
+	return State{
+		Capacity: Gas(newCapacity),
+		Excess:   Gas(newExcess),
+	}, nil
 }
