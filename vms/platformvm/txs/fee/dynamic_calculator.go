060e3c4b685d3520b0e0af0711789354d0b00149
Stephen Buttolph <stephen@avalabs.org>
2024-07-21 14:42:54 -0400

    implement dynamic fee calculator

--- vms/platformvm/txs/fee/dynamic_calculator.go
+++ vms/platformvm/txs/fee/dynamic_calculator.go
@@ -4,140 +4,36 @@
 package fee
 
 import (
-	"time"
-
-	"github.com/ava-labs/avalanchego/utils/constants"
+	"github.com/ava-labs/avalanchego/utils/math"
 	"github.com/ava-labs/avalanchego/vms/components/fee"
 	"github.com/ava-labs/avalanchego/vms/platformvm/txs"
-	"github.com/ava-labs/avalanchego/vms/platformvm/upgrade"
 )
 
-var (
-	_ Calculator  = (*staticCalculator)(nil)
-	_ txs.Visitor = (*staticVisitor)(nil)
-)
+var _ Calculator = (*dynamicCalculator)(nil)
 
-func NewDynamicCalculator(config fee.Config) Calculator {
-	return &staticCalculator{
-		upgrades:  upgradeTimes,
-		staticCfg: config,
-		time:      chainTime,
+func NewDynamicCalculator(
+	config fee.Config,
+	excess fee.Gas,
+) Calculator {
+	return &dynamicCalculator{
+		config: config,
+		price:  config.MinGasPrice.MulExp(excess, config.ExcessConversionConstant),
 	}
 }
 
-type staticCalculator struct {
-	staticCfg StaticConfig
-	upgrades  upgrade.Config
-	time      time.Time
+type dynamicCalculator struct {
+	config fee.Config
+	price  fee.GasPrice
 }
 
-func (c *staticCalculator) CalculateFee(tx txs.UnsignedTx) (uint64, error) {
-	v := staticVisitor{
-		staticCfg: c.staticCfg,
-		upgrades:  c.upgrades,
-		time:      c.time,
+func (c *dynamicCalculator) CalculateFee(tx txs.UnsignedTx) (uint64, error) {
+	complexity, err := TxComplexity(tx)
+	if err != nil {
+		return 0, err
 	}
-	err := tx.Visit(&v)
-	return v.fee, err
-}
-
-type staticVisitor struct {
-	// inputs
-	staticCfg StaticConfig
-	upgrades  upgrade.Config
-	time      time.Time
-
-	// outputs
-	fee uint64
-}
-
-func (c *staticVisitor) AddValidatorTx(*txs.AddValidatorTx) error {
-	c.fee = c.staticCfg.AddPrimaryNetworkValidatorFee
-	return nil
-}
-
-func (c *staticVisitor) AddSubnetValidatorTx(*txs.AddSubnetValidatorTx) error {
-	c.fee = c.staticCfg.AddSubnetValidatorFee
-	return nil
-}
-
-func (c *staticVisitor) AddDelegatorTx(*txs.AddDelegatorTx) error {
-	c.fee = c.staticCfg.AddPrimaryNetworkDelegatorFee
-	return nil
-}
-
-func (c *staticVisitor) CreateChainTx(*txs.CreateChainTx) error {
-	if c.upgrades.IsApricotPhase3Activated(c.time) {
-		c.fee = c.staticCfg.CreateBlockchainTxFee
-	} else {
-		c.fee = c.staticCfg.CreateAssetTxFee
+	gas, err := complexity.ToGas(c.config.Weights)
+	if err != nil {
+		return 0, err
 	}
-	return nil
-}
-
-func (c *staticVisitor) CreateSubnetTx(*txs.CreateSubnetTx) error {
-	if c.upgrades.IsApricotPhase3Activated(c.time) {
-		c.fee = c.staticCfg.CreateSubnetTxFee
-	} else {
-		c.fee = c.staticCfg.CreateAssetTxFee
-	}
-	return nil
-}
-
-func (c *staticVisitor) AdvanceTimeTx(*txs.AdvanceTimeTx) error {
-	c.fee = 0 // no fees
-	return nil
-}
-
-func (c *staticVisitor) RewardValidatorTx(*txs.RewardValidatorTx) error {
-	c.fee = 0 // no fees
-	return nil
-}
-
-func (c *staticVisitor) RemoveSubnetValidatorTx(*txs.RemoveSubnetValidatorTx) error {
-	c.fee = c.staticCfg.TxFee
-	return nil
-}
-
-func (c *staticVisitor) TransformSubnetTx(*txs.TransformSubnetTx) error {
-	c.fee = c.staticCfg.TransformSubnetTxFee
-	return nil
-}
-
-func (c *staticVisitor) TransferSubnetOwnershipTx(*txs.TransferSubnetOwnershipTx) error {
-	c.fee = c.staticCfg.TxFee
-	return nil
-}
-
-func (c *staticVisitor) AddPermissionlessValidatorTx(tx *txs.AddPermissionlessValidatorTx) error {
-	if tx.Subnet != constants.PrimaryNetworkID {
-		c.fee = c.staticCfg.AddSubnetValidatorFee
-	} else {
-		c.fee = c.staticCfg.AddPrimaryNetworkValidatorFee
-	}
-	return nil
-}
-
-func (c *staticVisitor) AddPermissionlessDelegatorTx(tx *txs.AddPermissionlessDelegatorTx) error {
-	if tx.Subnet != constants.PrimaryNetworkID {
-		c.fee = c.staticCfg.AddSubnetDelegatorFee
-	} else {
-		c.fee = c.staticCfg.AddPrimaryNetworkDelegatorFee
-	}
-	return nil
-}
-
-func (c *staticVisitor) BaseTx(*txs.BaseTx) error {
-	c.fee = c.staticCfg.TxFee
-	return nil
-}
-
-func (c *staticVisitor) ImportTx(*txs.ImportTx) error {
-	c.fee = c.staticCfg.TxFee
-	return nil
-}
-
-func (c *staticVisitor) ExportTx(*txs.ExportTx) error {
-	c.fee = c.staticCfg.TxFee
-	return nil
+	return math.Mul(uint64(c.price), uint64(gas))
 }
