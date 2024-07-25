b16ef2d13dbbf9e1ea4b8a3913b3039f066e30a1
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2024-07-25 09:44:21 +0200

    fixed benchmarks meaning

--- vms/components/fee/dimensions_test.go
+++ vms/components/fee/dimensions_test.go
@@ -428,8 +428,6 @@ func Test_Dimensions_ToGas(t *testing.T) {
 }
 
 func Benchmark_Dimensions_Add(b *testing.B) {
-	require := require.New(b)
-
 	lhs := Dimensions{600, 10, 10, 1000}
 	rhs := []Dimensions{
 		{1, 1, 1, 1},
@@ -443,14 +441,11 @@ func Benchmark_Dimensions_Add(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, err := lhs.Add(rhs...)
-		require.NoError(err)
+		_, _ = lhs.Add(rhs...)
 	}
 }
 
 func Benchmark_Dimensions_Sub(b *testing.B) {
-	require := require.New(b)
-
 	lhs := Dimensions{10_000, 10_000, 10_000, 100_000}
 	rhs := []Dimensions{
 		{1, 1, 1, 1},
@@ -463,7 +458,6 @@ func Benchmark_Dimensions_Sub(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, err := lhs.Sub(rhs...)
-		require.NoError(err)
+		_, _ = lhs.Sub(rhs...)
 	}
 }
