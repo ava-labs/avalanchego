320478ff5a0206436ab18fcc7587b4b0d97eb36f
Maru Newby <maru.newby@avalabs.org>
2024-07-24 20:19:42 -0700

    fixup: Ensure bootstrap tester image build is multiplatform
    This supports building an image on a mac that will work in an amd64
    eks cluster.

--- scripts/build_bootstrap_tester_image.sh
+++ scripts/build_bootstrap_tester_image.sh
@@ -18,10 +18,20 @@ fi
 # Build the avalanchego image
 DOCKER_CMD="docker buildx build"
 
+# TODO(marun) Figure out how best to support this in CI and locally
+PLATFORMS="${PLATFORMS:-}"
+
+# Build the avalanchego image local-only
+PLATFORMS="${PLATFORMS}" IMAGE_PREFIX= ./scripts/build_image.sh
+AVALANCHEGO_NODE_IMAGE="avalanchego:${IMAGE_TAG}"
+
 # Specifying an image prefix will ensure the image is pushed after build
 IMAGE_PREFIX="${IMAGE_PREFIX:-}"
 if [[ -n "${IMAGE_PREFIX}" ]]; then
   IMAGE_NAME="${IMAGE_PREFIX}/${IMAGE_NAME}"
+  if [[ -n "${PLATFORMS}" ]]; then
+    DOCKER_CMD="${DOCKER_CMD} --platform=${PLATFORMS}"
+  fi
   DOCKER_CMD="${DOCKER_CMD} --push"
 
   # Tag the image as latest for the master branch
@@ -33,18 +43,13 @@ if [[ -n "${IMAGE_PREFIX}" ]]; then
   if [[ -n "${DOCKER_USERNAME:-}" ]]; then
     echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
   fi
-
-  # The avalanchego image will have already have been built
-  AVALANCHEGO_NODE_IMAGE="${IMAGE_PREFIX}/avalanchego:${IMAGE_TAG}"
-else
-  # Build the avalanchego image locally
-  ./scripts/build_image.sh
-  AVALANCHEGO_NODE_IMAGE="avalanchego:${IMAGE_TAG}"
 fi
 
 # The dockerfiles don't specify the golang version to minimize the changes required to bump
 # the version. Instead, the golang version is provided as an argument.
 GO_VERSION="$(go list -m -f '{{.GoVersion}}')"
+
+PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64}"
 
 # Build the image for the bootstrap tester
 ${DOCKER_CMD} -t "${IMAGE_NAME}:${IMAGE_TAG}" \
