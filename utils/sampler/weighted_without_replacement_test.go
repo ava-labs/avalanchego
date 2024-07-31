1b98b61d4d7a8ea7427b877c00676bf0fc35cebc
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2024-07-30 20:50:41 -0400

    nit

--- utils/sampler/weighted_without_replacement_test.go
+++ utils/sampler/weighted_without_replacement_test.go
@@ -14,72 +14,72 @@ import (
 )
 
 func TestWeightedWithoutReplacementInitializeOverflow(t *testing.T) {
-	sampler := NewWeightedWithoutReplacement()
-	err := sampler.Initialize([]uint64{1, math.MaxUint64})
+	s := NewWeightedWithoutReplacement()
+	err := s.Initialize([]uint64{1, math.MaxUint64})
 	require.ErrorIs(t, err, safemath.ErrOverflow)
 }
 
 func TestWeightedWithoutReplacementOutOfRange(t *testing.T) {
 	require := require.New(t)
-	sampler := NewWeightedWithoutReplacement()
+	s := NewWeightedWithoutReplacement()
 
-	require.NoError(sampler.Initialize([]uint64{1}))
+	require.NoError(s.Initialize([]uint64{1}))
 
-	_, ok := sampler.Sample(2)
+	_, ok := s.Sample(2)
 	require.False(ok)
 }
 
 func TestWeightedWithoutReplacementEmptyWithoutWeight(t *testing.T) {
 	require := require.New(t)
-	sampler := NewWeightedWithoutReplacement()
+	s := NewWeightedWithoutReplacement()
 
-	require.NoError(sampler.Initialize(nil))
+	require.NoError(s.Initialize(nil))
 
-	indices, ok := sampler.Sample(0)
+	indices, ok := s.Sample(0)
 	require.True(ok)
 	require.Empty(indices)
 }
 
 func TestWeightedWithoutReplacementEmpty(t *testing.T) {
 	require := require.New(t)
-	sampler := NewWeightedWithoutReplacement()
+	s := NewWeightedWithoutReplacement()
 
-	require.NoError(sampler.Initialize([]uint64{1}))
+	require.NoError(s.Initialize([]uint64{1}))
 
-	indices, ok := sampler.Sample(0)
+	indices, ok := s.Sample(0)
 	require.True(ok)
 	require.Empty(indices)
 }
 
 func TestWeightedWithoutReplacementSingleton(t *testing.T) {
 	require := require.New(t)
-	sampler := NewWeightedWithoutReplacement()
+	s := NewWeightedWithoutReplacement()
 
-	require.NoError(sampler.Initialize([]uint64{1}))
+	require.NoError(s.Initialize([]uint64{1}))
 
-	indices, ok := sampler.Sample(1)
+	indices, ok := s.Sample(1)
 	require.True(ok)
 	require.Equal([]int{0}, indices)
 }
 
 func TestWeightedWithoutReplacementWithZero(t *testing.T) {
 	require := require.New(t)
-	sampler := NewWeightedWithoutReplacement()
+	s := NewWeightedWithoutReplacement()
 
-	require.NoError(sampler.Initialize([]uint64{0, 1}))
+	require.NoError(s.Initialize([]uint64{0, 1}))
 
-	indices, ok := sampler.Sample(1)
+	indices, ok := s.Sample(1)
 	require.True(ok)
 	require.Equal([]int{1}, indices)
 }
 
 func TestWeightedWithoutReplacementDistribution(t *testing.T) {
 	require := require.New(t)
-	sampler := NewWeightedWithoutReplacement()
+	s := NewWeightedWithoutReplacement()
 
-	require.NoError(sampler.Initialize([]uint64{1, 1, 2}))
+	require.NoError(s.Initialize([]uint64{1, 1, 2}))
 
-	indices, ok := sampler.Sample(4)
+	indices, ok := s.Sample(4)
 	require.True(ok)
 
 	slices.Sort(indices)
