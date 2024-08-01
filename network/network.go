d00f709c785c9d31375eda7ee8ef4d55f5ec0c08
Ian Suvak <ian.suvak@avalabs.org>
2024-08-01 14:39:22 -0400

    Remove trackedSubnet check for explicitly named peers in network.Send()

--- network/network.go
+++ network/network.go
@@ -677,10 +677,10 @@ func (n *network) track(ip *ips.ClaimedIPPort) error {
 //
 //   - [nodeIDs] the IDs of the peers that should be returned if they are
 //     connected.
-//   - [subnetID] the subnetID whose membership should be considered if
-//     [validatorOnly] is set to true.
-//   - [validatorOnly] is the flag to drop any nodes from [nodeIDs] that are not
-//     validators in [subnetID].
+//   - [subnetID] the subnetID whose membership should be considered to
+//     determine if the node is a validator.
+//   - [allower] interface that determines if a node is allowed to connect to
+//     the subnet based on it's validator status.
 func (n *network) getPeers(
 	nodeIDs set.Set[ids.NodeID],
 	subnetID ids.ID,
@@ -694,10 +694,6 @@ func (n *network) getPeers(
 	for nodeID := range nodeIDs {
 		peer, ok := n.connectedPeers.GetByID(nodeID)
 		if !ok {
-			continue
-		}
-
-		if trackedSubnets := peer.TrackedSubnets(); !trackedSubnets.Contains(subnetID) {
 			continue
 		}
 
