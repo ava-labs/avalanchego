11b59512fa8d8ec66d2a01ad3fae8116075b41fd
Stephen Buttolph <stephen@avalabs.org>
2024-08-05 15:19:27 -0400

    reduce diff

--- vms/components/fee/gas_test.go
+++ vms/components/fee/gas_test.go
@@ -9,8 +9,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	safemath "github.com/ava-labs/avalanchego/utils/math"
 )
 
 var gasPriceMulExpTests = []struct {
@@ -82,52 +80,16 @@ var gasPriceMulExpTests = []struct {
 }
 
 func Test_Gas_Cost(t *testing.T) {
-	tests := []struct {
-		gas          Gas
-		price        GasPrice
-		expectedCost uint64
-		expectedErr  error
-	}{
-		{
-			gas:          0,
-			price:        100,
-			expectedCost: 0,
-			expectedErr:  nil,
-		},
-		{
-			gas:          100,
-			price:        0,
-			expectedCost: 0,
-			expectedErr:  nil,
-		},
-		{
-			gas:          1,
-			price:        100,
-			expectedCost: 100,
-			expectedErr:  nil,
-		},
-		{
-			gas:          100,
-			price:        100,
-			expectedCost: 10000,
-			expectedErr:  nil,
-		},
-		{
-			gas:          2,
-			price:        math.MaxUint64,
-			expectedCost: 0,
-			expectedErr:  safemath.ErrOverflow,
-		},
-	}
-	for _, test := range tests {
-		t.Run(fmt.Sprintf("%d*%d", test.gas, test.price), func(t *testing.T) {
-			require := require.New(t)
+	require := require.New(t)
 
-			actual, err := test.gas.Cost(test.price)
-			require.Equal(test.expectedCost, actual)
-			require.ErrorIs(err, test.expectedErr)
-		})
-	}
+	const (
+		gas      Gas      = 40
+		price    GasPrice = 100
+		expected uint64   = 4000
+	)
+	actual, err := gas.Cost(price)
+	require.NoError(err)
+	require.Equal(expected, actual)
 }
 
 func Test_Gas_AddPerSecond(t *testing.T) {
