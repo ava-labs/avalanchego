64340f499a3cd5e66f094d9adf3dce05b0762607
Maru Newby <maru.newby@avalabs.org>
2024-08-07 23:21:12 -0700

    fixup: Cleanup panic handling

--- tests/other_test_context.go
+++ tests/other_test_context.go
@@ -49,30 +49,33 @@ func (tc *OtherTestContext) Cleanup() {
 	}
 	tc.cleanupCalled = true
 
-	// Only exit non-zero if a panic caused cleanup or cleanup caused a panic
-	exitNonZero := false
-
+	var panicData any
 	if r := recover(); r != nil {
-		exitNonZero = true
-		if r.(string) != failNowMessage {
-			// Only print panic messages not originating from this context
-			fmt.Println(r)
+		errorString, ok := r.(string)
+		if !ok || errorString != failNowMessage {
+			// Retain the panic data to raise after cleanup
+			panicData = r
 		}
 	}
 
+	// Only exit non-zero if a cleanup caused a panic
+	exitNonZero := false
 	for _, cleanupFunc := range tc.cleanupFuncs {
 		func() {
 			// Ensure a failed cleanup doesn't prevent subsequent cleanup functions from running
 			defer func() {
 				if r := recover(); r != nil {
 					exitNonZero = true
-					fmt.Println("Recovered from in panic during cleanup:", r)
+					fmt.Println("Recovered from panic during cleanup:", r)
 				}
 			}()
 			cleanupFunc()
 		}()
 	}
 
+	if panicData != nil {
+		panic(panicData)
+	}
 	if exitNonZero {
 		os.Exit(1)
 	}
