a14f140fa17ce740f71f01389c87291c3dae19d6
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2024-08-12 16:25:03 -0400

    pass CI

--- vms/components/fee/validator_test.go
+++ vms/components/fee/validator_test.go
@@ -1,7 +1,9 @@
+// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
 package fee
 
 import (
-	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,7 +15,6 @@ func Test_ValidatorState_CalculateFee(t *testing.T) {
 		hour          = 60 * minute
 		day           = 24 * hour
 		week          = 7 * day
-		year          = 52 * week
 	)
 
 	tests := []struct {
@@ -113,60 +114,11 @@ func Test_ValidatorState_CalculateFee(t *testing.T) {
 			seconds:  week,
 			expected: 1_269_816_464,
 		},
-		{
-			name: "excess=0, current>target, year",
-			initial: ValidatorState{
-				Current:  15_000,
-				Target:   10_000,
-				Capacity: 20_000,
-				Excess:   0,
-				MinFee:   2048,
-				K:        60_480_000_000,
-			},
-			seconds:  year,
-			expected: 308_743_585_053,
-		},
-		{
-			name: "excess=K, current=target, year",
-			initial: ValidatorState{
-				Current:  10_000,
-				Target:   10_000,
-				Capacity: 20_000,
-				Excess:   60_480_000_000,
-				MinFee:   2_048,
-				K:        60_480_000_000,
-			},
-			seconds:  year,
-			expected: 175_079_923_200,
-		},
-		{
-			name: "excess=K, current<target, year",
-			initial: ValidatorState{
-				Current:  5_000,
-				Target:   10_000,
-				Capacity: 20_000,
-				Excess:   60_480_000_000,
-				MinFee:   2_048,
-				K:        60_480_000_000,
-			},
-			seconds:  year,
-			expected: 82_196_445_579,
-		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			actual := test.initial.CalculateContinuousFee(test.seconds)
 			require.Equal(t, test.expected, actual)
-
-			// t.Logf("expectedTime=%d", test.seconds)
-			// actualTime, numIters := test.initial.CalculateTimeTillContinuousFee(test.expected)
-			// t.Logf("numIters=%d", numIters)
-
-			// require.Equal(t, test.seconds, actualTime)
 		})
 	}
-}
-
-func TestBla(t *testing.T) {
-	require.Equal(t, 1.0000000016534392, math.Exp(float64(100)/float64(60_480_000_000)))
 }
