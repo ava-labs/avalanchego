bbd83663ce5456c1ff3376cd4bd3bedf78e1c90a
Felipe Madero <felipe.madero@gmail.com>
2024-08-12 16:48:21 -0300

    address PR comments

--- tests/e2e/p/owner_retrieval.go
+++ tests/e2e/p/owner_retrieval.go
@@ -17,115 +17,107 @@ import (
 	ginkgo "github.com/onsi/ginkgo/v2"
 )
 
-var _ = e2e.DescribePChain("[Retrieving Owner from Subnet Txs]", func() {
+var _ = e2e.DescribePChain("[P-Chain Wallet]", func() {
 	tc := e2e.NewTestContext()
-	require := require.New(ginkgo.GinkgoT())
+	require := require.New(tc)
 
-	ginkgo.It("owner retrieval",
-		func() {
-			env := e2e.GetEnv(tc)
+	ginkgo.It("should support retrieving subnet owners", func() {
+		env := e2e.GetEnv(tc)
 
-			nodeURI := env.GetRandomNodeURI()
-			pChainClient := platformvm.NewClient(nodeURI.URI)
+		nodeURI := env.GetRandomNodeURI()
+		pChainClient := platformvm.NewClient(nodeURI.URI)
 
-			keychain := env.NewKeychain(1)
-			baseWallet := e2e.NewWallet(tc, keychain, nodeURI)
-			pWallet := baseWallet.P()
+		keychain := env.NewKeychain(1)
+		baseWallet := e2e.NewWallet(tc, keychain, nodeURI)
+		pWallet := baseWallet.P()
 
-			owner := &secp256k1fx.OutputOwners{
-				Threshold: 1,
-				Addrs: []ids.ShortID{
-					keychain.Keys[0].Address(),
-				},
+		owner := &secp256k1fx.OutputOwners{
+			Threshold: 1,
+			Addrs: []ids.ShortID{
+				keychain.Keys[0].Address(),
+			},
+		}
+
+		tc.By("creating a permissioned subnet")
+		subnetTx, err := pWallet.IssueCreateSubnetTx(
+			owner,
+			tc.WithDefaultContext(),
+		)
+		subnetID := subnetTx.ID()
+		require.NoError(err)
+		require.NotEqual(subnetID, constants.PrimaryNetworkID)
+
+		tc.By("verifying owner", func() {
+			pChainTxs := map[ids.ID]*txs.Tx{
+				subnetID: subnetTx,
 			}
-
-			var subnetID ids.ID
-			var subnetTx *txs.Tx
-			ginkgo.By("create a permissioned subnet", func() {
-				var err error
-				subnetTx, err = pWallet.IssueCreateSubnetTx(
-					owner,
-					tc.WithDefaultContext(),
-				)
-				subnetID = subnetTx.ID()
-				require.NoError(err)
-				require.NotEqual(subnetID, constants.PrimaryNetworkID)
-			})
-
-			ginkgo.By("verify owner", func() {
-				pChainTxs := map[ids.ID]*txs.Tx{
-					subnetID: subnetTx,
-				}
-				subnetOwners, err := primary.ExtractTxSubnetOwners(
-					tc.DefaultContext(),
-					pChainClient,
-					pChainTxs,
-				)
-				require.NoError(err)
-				subnetOwnerI, found := subnetOwners[subnetID]
-				require.True(found)
-				subnetOwner, ok := subnetOwnerI.(*secp256k1fx.OutputOwners)
-				require.True(ok)
-				require.Equal(owner.Locktime, subnetOwner.Locktime)
-				require.Equal(owner.Threshold, subnetOwner.Threshold)
-				require.Equal(owner.Addrs, subnetOwner.Addrs)
-			})
-
-			newKeychain := env.NewKeychain(1)
-			newOwner := &secp256k1fx.OutputOwners{
-				Threshold: 1,
-				Addrs: []ids.ShortID{
-					newKeychain.Keys[0].Address(),
-				},
-			}
-
-			var transferSubnetOwnershipTx *txs.Tx
-			ginkgo.By("change subnet owner", func() {
-				var err error
-				transferSubnetOwnershipTx, err = pWallet.IssueTransferSubnetOwnershipTx(
-					subnetID,
-					newOwner,
-					tc.WithDefaultContext(),
-				)
-				require.NoError(err)
-			})
-
-			ginkgo.By("verify new owner from CreateSubnetTx", func() {
-				pChainTxs := map[ids.ID]*txs.Tx{
-					subnetID: subnetTx,
-				}
-				subnetOwners, err := primary.ExtractTxSubnetOwners(
-					tc.DefaultContext(),
-					pChainClient,
-					pChainTxs,
-				)
-				require.NoError(err)
-				subnetOwnerI, found := subnetOwners[subnetID]
-				require.True(found)
-				subnetOwner, ok := subnetOwnerI.(*secp256k1fx.OutputOwners)
-				require.True(ok)
-				require.Equal(newOwner.Locktime, subnetOwner.Locktime)
-				require.Equal(newOwner.Threshold, subnetOwner.Threshold)
-				require.Equal(newOwner.Addrs, subnetOwner.Addrs)
-			})
-
-			ginkgo.By("verify new owner from TransferSubnetOwnershipTx", func() {
-				pChainTxs := map[ids.ID]*txs.Tx{
-					subnetID: transferSubnetOwnershipTx,
-				}
-				subnetOwners, err := primary.ExtractTxSubnetOwners(
-					tc.DefaultContext(),
-					pChainClient,
-					pChainTxs,
-				)
-				require.NoError(err)
-				subnetOwnerI, found := subnetOwners[subnetID]
-				require.True(found)
-				subnetOwner, ok := subnetOwnerI.(*secp256k1fx.OutputOwners)
-				require.True(ok)
-				require.Equal(newOwner.Locktime, subnetOwner.Locktime)
-				require.Equal(newOwner.Threshold, subnetOwner.Threshold)
-				require.Equal(newOwner.Addrs, subnetOwner.Addrs)
-			})
+			subnetOwners, err := primary.ExtractTxSubnetOwners(
+				tc.DefaultContext(),
+				pChainClient,
+				pChainTxs,
+			)
+			require.NoError(err)
+			subnetOwnerI, found := subnetOwners[subnetID]
+			require.True(found)
+			subnetOwner, ok := subnetOwnerI.(*secp256k1fx.OutputOwners)
+			require.True(ok)
+			require.Equal(owner.Locktime, subnetOwner.Locktime)
+			require.Equal(owner.Threshold, subnetOwner.Threshold)
+			require.Equal(owner.Addrs, subnetOwner.Addrs)
 		})
+
+		newKeychain := env.NewKeychain(1)
+		newOwner := &secp256k1fx.OutputOwners{
+			Threshold: 1,
+			Addrs: []ids.ShortID{
+				newKeychain.Keys[0].Address(),
+			},
+		}
+
+		tc.By("changing subnet owner")
+		transferSubnetOwnershipTx, err := pWallet.IssueTransferSubnetOwnershipTx(
+			subnetID,
+			newOwner,
+			tc.WithDefaultContext(),
+		)
+		require.NoError(err)
+
+		tc.By("verifying new owner from CreateSubnetTx", func() {
+			pChainTxs := map[ids.ID]*txs.Tx{
+				subnetID: subnetTx,
+			}
+			subnetOwners, err := primary.ExtractTxSubnetOwners(
+				tc.DefaultContext(),
+				pChainClient,
+				pChainTxs,
+			)
+			require.NoError(err)
+			subnetOwnerI, found := subnetOwners[subnetID]
+			require.True(found)
+			subnetOwner, ok := subnetOwnerI.(*secp256k1fx.OutputOwners)
+			require.True(ok)
+			require.Equal(newOwner.Locktime, subnetOwner.Locktime)
+			require.Equal(newOwner.Threshold, subnetOwner.Threshold)
+			require.Equal(newOwner.Addrs, subnetOwner.Addrs)
+		})
+
+		tc.By("verifying new owner from TransferSubnetOwnershipTx", func() {
+			pChainTxs := map[ids.ID]*txs.Tx{
+				subnetID: transferSubnetOwnershipTx,
+			}
+			subnetOwners, err := primary.ExtractTxSubnetOwners(
+				tc.DefaultContext(),
+				pChainClient,
+				pChainTxs,
+			)
+			require.NoError(err)
+			subnetOwnerI, found := subnetOwners[subnetID]
+			require.True(found)
+			subnetOwner, ok := subnetOwnerI.(*secp256k1fx.OutputOwners)
+			require.True(ok)
+			require.Equal(newOwner.Locktime, subnetOwner.Locktime)
+			require.Equal(newOwner.Threshold, subnetOwner.Threshold)
+			require.Equal(newOwner.Addrs, subnetOwner.Addrs)
+		})
+	})
 })
