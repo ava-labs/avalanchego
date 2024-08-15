35b58d0d2766ce724b3913da2b921a22b923a8a1
Stephen Buttolph <stephen@avalabs.org>
2024-08-14 23:16:05 -0400

    Fix e2e tests to support dynamic fees (#3296)

--- tests/e2e/p/staking_rewards.go
+++ tests/e2e/p/staking_rewards.go
@@ -94,14 +94,8 @@ var _ = ginkgo.Describe("[Staking Rewards]", func() {
 				deltaDelegationRewardKey,
 			}
 
-			keychain = secp256k1fx.NewKeychain(rewardKeys...)
-		)
-
-		fundedKey := env.AllocatePreFundedKey()
-		keychain.Add(fundedKey)
-
-		var (
-			nodeURI = tmpnet.NodeURI{
+			keychain = env.NewKeychain(1)
+			nodeURI  = tmpnet.NodeURI{
 				NodeID: alphaNodeID,
 				URI:    alphaNode.URI,
 			}
