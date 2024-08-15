6d897324b6c63a2f84bc292f10a7f21991892275
Stephen Buttolph <stephen@avalabs.org>
2024-08-14 22:38:32 -0400

    Refactor e2e tests for P-chain tests (#3295)

--- tests/e2e/p/staking_rewards.go
+++ tests/e2e/p/staking_rewards.go
@@ -14,6 +14,7 @@ import (
 	"github.com/ava-labs/avalanchego/api/info"
 	"github.com/ava-labs/avalanchego/config"
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/tests"
 	"github.com/ava-labs/avalanchego/tests/fixture/e2e"
 	"github.com/ava-labs/avalanchego/tests/fixture/tmpnet"
 	"github.com/ava-labs/avalanchego/utils/constants"
@@ -27,19 +28,26 @@ import (
 	ginkgo "github.com/onsi/ginkgo/v2"
 )
 
-const (
-	targetDelegationPeriod = 15 * time.Second
-	targetValidationPeriod = 30 * time.Second
-)
-
 var _ = ginkgo.Describe("[Staking Rewards]", func() {
-	tc := e2e.NewTestContext()
-	require := require.New(tc)
+	var (
+		tc      = e2e.NewTestContext()
+		require = require.New(tc)
+	)
 
 	ginkgo.It("should ensure that validator node uptime determines whether a staking reward is issued", func() {
-		env := e2e.GetEnv(tc)
+		const (
+			targetDelegationPeriod = 15 * time.Second
+			targetValidationPeriod = 30 * time.Second
 
-		network := env.GetNetwork()
+			delegationPercent = 0.10 // 10%
+			delegationShare   = reward.PercentDenominator * delegationPercent
+			weight            = 2_000 * units.Avax
+		)
+
+		var (
+			env     = e2e.GetEnv(tc)
+			network = env.GetNetwork()
+		)
 
 		tc.By("checking that the network has a compatible minimum stake duration", func() {
 			minStakeDuration := cast.ToDuration(network.DefaultFlags[config.MinStakeDurationKey])
@@ -68,68 +76,56 @@ var _ = ginkgo.Describe("[Staking Rewards]", func() {
 		betaNodeID, betaPOP, err := betaInfoClient.GetNodeID(tc.DefaultContext())
 		require.NoError(err)
 
-		tc.By("generating reward keys")
-
-		alphaValidationRewardKey, err := secp256k1.NewPrivateKey()
-		require.NoError(err)
-		alphaDelegationRewardKey, err := secp256k1.NewPrivateKey()
-		require.NoError(err)
-
-		betaValidationRewardKey, err := secp256k1.NewPrivateKey()
-		require.NoError(err)
-		betaDelegationRewardKey, err := secp256k1.NewPrivateKey()
-		require.NoError(err)
-
-		gammaDelegationRewardKey, err := secp256k1.NewPrivateKey()
-		require.NoError(err)
-
-		deltaDelegationRewardKey, err := secp256k1.NewPrivateKey()
-		require.NoError(err)
-
-		rewardKeys := []*secp256k1.PrivateKey{
-			alphaValidationRewardKey,
-			alphaDelegationRewardKey,
-			betaValidationRewardKey,
-			betaDelegationRewardKey,
-			gammaDelegationRewardKey,
-			deltaDelegationRewardKey,
-		}
-
 		tc.By("creating keychain and P-Chain wallet")
-		keychain := secp256k1fx.NewKeychain(rewardKeys...)
-		fundedKey := env.AllocatePreFundedKey()
-		keychain.Add(fundedKey)
-		nodeURI := tmpnet.NodeURI{
-			NodeID: alphaNodeID,
-			URI:    alphaNode.URI,
-		}
-		baseWallet := e2e.NewWallet(tc, keychain, nodeURI)
-		pWallet := baseWallet.P()
+		var (
+			alphaValidationRewardKey = e2e.NewPrivateKey(tc)
+			alphaDelegationRewardKey = e2e.NewPrivateKey(tc)
+			betaValidationRewardKey  = e2e.NewPrivateKey(tc)
+			betaDelegationRewardKey  = e2e.NewPrivateKey(tc)
+			gammaDelegationRewardKey = e2e.NewPrivateKey(tc)
+			deltaDelegationRewardKey = e2e.NewPrivateKey(tc)
 
-		pBuilder := pWallet.Builder()
-		pContext := pBuilder.Context()
+			rewardKeys = []*secp256k1.PrivateKey{
+				alphaValidationRewardKey,
+				alphaDelegationRewardKey,
+				betaValidationRewardKey,
+				betaDelegationRewardKey,
+				gammaDelegationRewardKey,
+				deltaDelegationRewardKey,
+			}
 
-		const (
-			delegationPercent = 0.10 // 10%
-			delegationShare   = reward.PercentDenominator * delegationPercent
-			weight            = 2_000 * units.Avax
+			keychain = secp256k1fx.NewKeychain(rewardKeys...)
 		)
 
-		pvmClient := platformvm.NewClient(alphaNode.URI)
+		fundedKey := env.AllocatePreFundedKey()
+		keychain.Add(fundedKey)
 
-		tc.By("retrieving supply before inserting validators")
-		supplyAtValidatorsStart, _, err := pvmClient.GetCurrentSupply(tc.DefaultContext(), constants.PrimaryNetworkID)
+		var (
+			nodeURI = tmpnet.NodeURI{
+				NodeID: alphaNodeID,
+				URI:    alphaNode.URI,
+			}
+			baseWallet = e2e.NewWallet(tc, keychain, nodeURI)
+			pWallet    = baseWallet.P()
+			pBuilder   = pWallet.Builder()
+			pContext   = pBuilder.Context()
+
+			pvmClient = platformvm.NewClient(alphaNode.URI)
+		)
+
+		tc.By("retrieving supply before adding alpha node as a validator")
+		supplyAtAlphaNodeStart, _, err := pvmClient.GetCurrentSupply(tc.DefaultContext(), constants.PrimaryNetworkID)
 		require.NoError(err)
 
-		alphaValidatorsEndTime := time.Now().Add(targetValidationPeriod)
-		tc.Outf("alpha node validation period ending at: %v\n", alphaValidatorsEndTime)
-
 		tc.By("adding alpha node as a validator", func() {
+			endTime := time.Now().Add(targetValidationPeriod)
+			tc.Outf("validation period ending at: %v\n", endTime)
+
 			_, err := pWallet.IssueAddPermissionlessValidatorTx(
 				&txs.SubnetValidator{
 					Validator: txs.Validator{
 						NodeID: alphaNodeID,
-						End:    uint64(alphaValidatorsEndTime.Unix()),
+						End:    uint64(endTime.Unix()),
 						Wght:   weight,
 					},
 					Subnet: constants.PrimaryNetworkID,
@@ -179,19 +175,19 @@ var _ = ginkgo.Describe("[Staking Rewards]", func() {
 			require.NoError(err)
 		})
 
-		tc.By("retrieving supply before inserting delegators")
-		supplyAtDelegatorsStart, _, err := pvmClient.GetCurrentSupply(tc.DefaultContext(), constants.PrimaryNetworkID)
+		tc.By("retrieving supply before adding gamma as a delegator")
+		supplyAtGammaDelegatorStart, _, err := pvmClient.GetCurrentSupply(tc.DefaultContext(), constants.PrimaryNetworkID)
 		require.NoError(err)
 
-		gammaDelegatorEndTime := time.Now().Add(targetDelegationPeriod)
-		tc.Outf("gamma delegation period ending at: %v\n", gammaDelegatorEndTime)
+		tc.By("adding gamma as a delegator to the alpha node", func() {
+			endTime := time.Now().Add(targetDelegationPeriod)
+			tc.Outf("delegation period ending at: %v\n", endTime)
 
-		tc.By("adding gamma as delegator to the alpha node", func() {
 			_, err := pWallet.IssueAddPermissionlessDelegatorTx(
 				&txs.SubnetValidator{
 					Validator: txs.Validator{
 						NodeID: alphaNodeID,
-						End:    uint64(gammaDelegatorEndTime.Unix()),
+						End:    uint64(endTime.Unix()),
 						Wght:   weight,
 					},
 					Subnet: constants.PrimaryNetworkID,
@@ -206,15 +202,15 @@ var _ = ginkgo.Describe("[Staking Rewards]", func() {
 			require.NoError(err)
 		})
 
-		deltaDelegatorEndTime := time.Now().Add(targetDelegationPeriod)
-		tc.Outf("delta delegation period ending at: %v\n", deltaDelegatorEndTime)
-
 		tc.By("adding delta as delegator to the beta node", func() {
+			endTime := time.Now().Add(targetDelegationPeriod)
+			tc.Outf("delegation period ending at: %v\n", endTime)
+
 			_, err := pWallet.IssueAddPermissionlessDelegatorTx(
 				&txs.SubnetValidator{
 					Validator: txs.Validator{
 						NodeID: betaNodeID,
-						End:    uint64(deltaDelegatorEndTime.Unix()),
+						End:    uint64(endTime.Unix()),
 						Wght:   weight,
 					},
 					Subnet: constants.PrimaryNetworkID,
@@ -229,84 +225,104 @@ var _ = ginkgo.Describe("[Staking Rewards]", func() {
 			require.NoError(err)
 		})
 
-		tc.By("stopping beta node to prevent it and its delegator from receiving a validation reward")
-		require.NoError(betaNode.Stop(tc.DefaultContext()))
+		tc.By("stopping beta node to prevent it and its delegator from receiving a validation reward", func() {
+			require.NoError(betaNode.Stop(tc.DefaultContext()))
+		})
 
 		tc.By("retrieving staking periods from the chain")
-		data, err := pvmClient.GetCurrentValidators(tc.DefaultContext(), constants.PlatformChainID, []ids.NodeID{alphaNodeID})
+		currentValidators, err := pvmClient.GetCurrentValidators(tc.DefaultContext(), constants.PlatformChainID, []ids.NodeID{alphaNodeID})
 		require.NoError(err)
-		require.Len(data, 1)
-		actualAlphaValidationPeriod := time.Duration(data[0].EndTime-data[0].StartTime) * time.Second
-		delegatorData := data[0].Delegators[0]
-		actualGammaDelegationPeriod := time.Duration(delegatorData.EndTime-delegatorData.StartTime) * time.Second
+		require.Len(currentValidators, 1)
+		alphaValidator := currentValidators[0]
+		require.Len(alphaValidator.Delegators, 1)
+		gammaDelegator := alphaValidator.Delegators[0]
+		actualAlphaValidationPeriod := time.Duration(alphaValidator.EndTime-alphaValidator.StartTime) * time.Second
+		actualGammaDelegationPeriod := time.Duration(gammaDelegator.EndTime-gammaDelegator.StartTime) * time.Second
 
-		tc.By("waiting until all validation periods are over")
-		// The beta validator was the last added and so has the latest end time. The
-		// delegation periods are shorter than the validation periods.
-		time.Sleep(time.Until(betaValidatorEndTime))
+		tc.By("waiting until the alpha and beta nodes are no longer validators", func() {
+			// The beta validator was the last added and so has the latest end
+			// time. The delegation periods are shorter than the validation
+			// periods.
+			time.Sleep(time.Until(betaValidatorEndTime))
 
-		tc.By("waiting until the alpha and beta nodes are no longer validators")
-		tc.Eventually(func() bool {
-			validators, err := pvmClient.GetCurrentValidators(tc.DefaultContext(), constants.PrimaryNetworkID, nil)
-			require.NoError(err)
-			for _, validator := range validators {
-				if validator.NodeID == alphaNodeID || validator.NodeID == betaNodeID {
-					return false
+			// To avoid racy behavior, we confirm that the nodes are no longer
+			// validators before advancing.
+			tc.Eventually(func() bool {
+				validators, err := pvmClient.GetCurrentValidators(tc.DefaultContext(), constants.PrimaryNetworkID, nil)
+				require.NoError(err)
+				for _, validator := range validators {
+					if validator.NodeID == alphaNodeID || validator.NodeID == betaNodeID {
+						return false
+					}
 				}
+				return true
+			}, e2e.DefaultTimeout, e2e.DefaultPollingInterval, "nodes failed to stop validating before timeout")
+		})
+
+		tc.By("checking expected rewards against actual rewards", func() {
+			var (
+				adminClient  = admin.NewClient(nodeURI.URI)
+				rewardConfig = getRewardConfig(tc, adminClient)
+				calculator   = reward.NewCalculator(rewardConfig)
+
+				expectedValidationReward = calculator.Calculate(actualAlphaValidationPeriod, weight, supplyAtAlphaNodeStart)
+
+				potentialDelegationReward                      = calculator.Calculate(actualGammaDelegationPeriod, weight, supplyAtGammaDelegatorStart)
+				expectedDelegationFee, expectedDelegatorReward = reward.Split(potentialDelegationReward, delegationShare)
+			)
+
+			rewardBalances := make(map[ids.ShortID]uint64, len(rewardKeys))
+			for _, rewardKey := range rewardKeys {
+				var (
+					keychain   = secp256k1fx.NewKeychain(rewardKey)
+					baseWallet = e2e.NewWallet(tc, keychain, nodeURI)
+					pWallet    = baseWallet.P()
+					pBuilder   = pWallet.Builder()
+				)
+
+				balances, err := pBuilder.GetBalance()
+				require.NoError(err)
+				rewardBalances[rewardKey.Address()] = balances[pContext.AVAXAssetID]
 			}
-			return true
-		}, e2e.DefaultTimeout, e2e.DefaultPollingInterval, "nodes failed to stop validating before timeout ")
 
-		tc.By("retrieving reward configuration for the network")
-		// TODO(marun) Enable GetConfig to return *node.Config
-		// directly. Currently, due to a circular dependency issue, a
-		// map-based equivalent is used for which manual unmarshaling
-		// is required.
-		adminClient := admin.NewClient(env.GetRandomNodeURI().URI)
-		rawNodeConfigMap, err := adminClient.GetConfig(tc.DefaultContext())
-		require.NoError(err)
-		nodeConfigMap, ok := rawNodeConfigMap.(map[string]interface{})
-		require.True(ok)
-		stakingConfigMap, ok := nodeConfigMap["stakingConfig"].(map[string]interface{})
-		require.True(ok)
-		rawRewardConfig := stakingConfigMap["rewardConfig"]
-		rewardConfig := reward.Config{}
-		require.NoError(mapstructure.Decode(rawRewardConfig, &rewardConfig))
+			require.Equal(
+				map[ids.ShortID]uint64{
+					alphaValidationRewardKey.Address(): expectedValidationReward,
+					alphaDelegationRewardKey.Address(): expectedDelegationFee,
+					betaValidationRewardKey.Address():  0, // Validator didn't meet uptime requirement
+					betaDelegationRewardKey.Address():  0, // Validator didn't meet uptime requirement
+					gammaDelegationRewardKey.Address(): expectedDelegatorReward,
+					deltaDelegationRewardKey.Address(): 0, // Validator didn't meet uptime requirement
+				},
+				rewardBalances,
+			)
+		})
 
-		tc.By("retrieving reward address balances")
-		rewardBalances := make(map[ids.ShortID]uint64, len(rewardKeys))
-		for _, rewardKey := range rewardKeys {
-			keychain := secp256k1fx.NewKeychain(rewardKey)
-			baseWallet := e2e.NewWallet(tc, keychain, nodeURI)
-			pWallet := baseWallet.P()
-			balances, err := pWallet.Builder().GetBalance()
-			require.NoError(err)
-			rewardBalances[rewardKey.Address()] = balances[pContext.AVAXAssetID]
-		}
-		require.Len(rewardBalances, len(rewardKeys))
-
-		tc.By("determining expected validation and delegation rewards")
-		calculator := reward.NewCalculator(rewardConfig)
-		expectedValidationReward := calculator.Calculate(actualAlphaValidationPeriod, weight, supplyAtValidatorsStart)
-		potentialDelegationReward := calculator.Calculate(actualGammaDelegationPeriod, weight, supplyAtDelegatorsStart)
-		expectedDelegationFee, expectedDelegatorReward := reward.Split(potentialDelegationReward, delegationShare)
-
-		tc.By("checking expected rewards against actual rewards")
-		expectedRewardBalances := map[ids.ShortID]uint64{
-			alphaValidationRewardKey.Address(): expectedValidationReward,
-			alphaDelegationRewardKey.Address(): expectedDelegationFee,
-			betaValidationRewardKey.Address():  0, // Validator didn't meet uptime requirement
-			betaDelegationRewardKey.Address():  0, // Validator didn't meet uptime requirement
-			gammaDelegationRewardKey.Address(): expectedDelegatorReward,
-			deltaDelegationRewardKey.Address(): 0, // Validator didn't meet uptime requirement
-		}
-		for address := range expectedRewardBalances {
-			require.Equal(expectedRewardBalances[address], rewardBalances[address])
-		}
-
-		tc.By("stopping alpha to free up resources for a bootstrap check")
-		require.NoError(alphaNode.Stop(tc.DefaultContext()))
+		tc.By("stopping alpha to free up resources for a bootstrap check", func() {
+			require.NoError(alphaNode.Stop(tc.DefaultContext()))
+		})
 
 		_ = e2e.CheckBootstrapIsPossible(tc, network)
 	})
 })
+
+// TODO(marun) Enable GetConfig to return *node.Config directly. Currently, due
+// to a circular dependency issue, a map-based equivalent is used for which
+// manual unmarshaling is required.
+func getRewardConfig(tc tests.TestContext, client admin.Client) reward.Config {
+	require := require.New(tc)
+
+	rawNodeConfigMap, err := client.GetConfig(tc.DefaultContext())
+	require.NoError(err)
+	nodeConfigMap, ok := rawNodeConfigMap.(map[string]interface{})
+	require.True(ok)
+	stakingConfigMap, ok := nodeConfigMap["stakingConfig"].(map[string]interface{})
+	require.True(ok)
+
+	var rewardConfig reward.Config
+	require.NoError(mapstructure.Decode(
+		stakingConfigMap["rewardConfig"],
+		&rewardConfig,
+	))
+	return rewardConfig
+}
--- tests/e2e/p/workflow.go
+++ tests/e2e/p/workflow.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/ava-labs/avalanchego/api/info"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/tests/fixture/e2e"
 	"github.com/ava-labs/avalanchego/utils"
@@ -25,177 +24,160 @@ import (
 )
 
 // PChainWorkflow is an integration test for normal P-Chain operations
-// - Issues an Add Validator and an Add Delegator using the funding address
-// - Exports AVAX from the P-Chain funding address to the X-Chain created address
-// - Exports AVAX from the X-Chain created address to the P-Chain created address
-// - Checks the expected value of the funding address
+// - Issues an AddPermissionlessValidatorTx
+// - Issues an AddPermissionlessDelegatorTx
+// - Issues an ExportTx on the P-chain and verifies the expected balances
+// - Issues an ImportTx on the X-chain and verifies the expected balances
 
 var _ = e2e.DescribePChain("[Workflow]", func() {
-	tc := e2e.NewTestContext()
-	require := require.New(tc)
+	var (
+		tc      = e2e.NewTestContext()
+		require = require.New(tc)
+	)
 
-	ginkgo.It("P-chain main operations",
-		func() {
-			env := e2e.GetEnv(tc)
-
-			nodeURI := env.GetRandomNodeURI()
-			keychain := env.NewKeychain(2)
-			baseWallet := e2e.NewWallet(tc, keychain, nodeURI)
-
-			pWallet := baseWallet.P()
-			pBuilder := pWallet.Builder()
-			pContext := pBuilder.Context()
-			avaxAssetID := pContext.AVAXAssetID
-			xWallet := baseWallet.X()
-			xBuilder := xWallet.Builder()
-			xContext := xBuilder.Context()
-			pChainClient := platformvm.NewClient(nodeURI.URI)
-
-			tc.Outf("{{blue}} fetching minimal stake amounts {{/}}\n")
-			minValStake, minDelStake, err := pChainClient.GetMinStake(tc.DefaultContext(), constants.PlatformChainID)
-			require.NoError(err)
-			tc.Outf("{{green}} minimal validator stake: %d {{/}}\n", minValStake)
-			tc.Outf("{{green}} minimal delegator stake: %d {{/}}\n", minDelStake)
-
-			tc.Outf("{{blue}} fetching tx fee {{/}}\n")
-			infoClient := info.NewClient(nodeURI.URI)
-			fees, err := infoClient.GetTxFee(tc.DefaultContext())
-			require.NoError(err)
-			txFees := uint64(fees.TxFee)
-			tc.Outf("{{green}} txFee: %d {{/}}\n", txFees)
-
+	ginkgo.It("P-chain main operations", func() {
+		const (
 			// amount to transfer from P to X chain
-			toTransfer := 1 * units.Avax
+			toTransfer                 = 1 * units.Avax
+			delegationFeeShares uint32 = 20000 // TODO: retrieve programmatically
+		)
 
-			pShortAddr := keychain.Keys[0].Address()
-			xTargetAddr := keychain.Keys[1].Address()
-			tc.By("check selected keys have sufficient funds", func() {
-				pBalances, err := pWallet.Builder().GetBalance()
-				pBalance := pBalances[avaxAssetID]
-				minBalance := minValStake + txFees + minDelStake + txFees + toTransfer + txFees
-				require.NoError(err)
-				require.GreaterOrEqual(pBalance, minBalance)
-			})
+		var (
+			env = e2e.GetEnv(tc)
 
-			// Use a random node ID to ensure that repeated test runs
-			// will succeed against a network that persists across runs.
-			validatorID, err := ids.ToNodeID(utils.RandomBytes(ids.NodeIDLen))
-			require.NoError(err)
+			nodeURI  = env.GetRandomNodeURI()
+			keychain = env.NewKeychain(2)
 
-			vdr := &txs.SubnetValidator{
-				Validator: txs.Validator{
-					NodeID: validatorID,
-					End:    uint64(time.Now().Add(72 * time.Hour).Unix()),
-					Wght:   minValStake,
-				},
-				Subnet: constants.PrimaryNetworkID,
-			}
-			rewardOwner := &secp256k1fx.OutputOwners{
+			rewardAddr  = keychain.Keys[0].Address()
+			rewardOwner = &secp256k1fx.OutputOwners{
 				Threshold: 1,
-				Addrs:     []ids.ShortID{pShortAddr},
+				Addrs:     []ids.ShortID{rewardAddr},
 			}
-			shares := uint32(20000) // TODO: retrieve programmatically
 
+			transferAddr  = keychain.Keys[1].Address()
+			transferOwner = secp256k1fx.OutputOwners{
+				Threshold: 1,
+				Addrs:     []ids.ShortID{transferAddr},
+			}
+
+			baseWallet = e2e.NewWallet(tc, keychain, nodeURI)
+
+			pWallet  = baseWallet.P()
+			pBuilder = pWallet.Builder()
+			pContext = pBuilder.Context()
+
+			xWallet  = baseWallet.X()
+			xBuilder = xWallet.Builder()
+			xContext = xBuilder.Context()
+
+			avaxAssetID = pContext.AVAXAssetID
+		)
+
+		tc.Outf("{{blue}} fetching minimal stake amounts {{/}}\n")
+		pChainClient := platformvm.NewClient(nodeURI.URI)
+		minValStake, minDelStake, err := pChainClient.GetMinStake(
+			tc.DefaultContext(),
+			constants.PlatformChainID,
+		)
+		require.NoError(err)
+		tc.Outf("{{green}} minimal validator stake: %d {{/}}\n", minValStake)
+		tc.Outf("{{green}} minimal delegator stake: %d {{/}}\n", minDelStake)
+
+		// Use a random node ID to ensure that repeated test runs will succeed
+		// against a network that persists across runs.
+		validatorID, err := ids.ToNodeID(utils.RandomBytes(ids.NodeIDLen))
+		require.NoError(err)
+
+		vdr := &txs.SubnetValidator{
+			Validator: txs.Validator{
+				NodeID: validatorID,
+				End:    uint64(time.Now().Add(72 * time.Hour).Unix()),
+				Wght:   minValStake,
+			},
+			Subnet: constants.PrimaryNetworkID,
+		}
+
+		tc.By("issuing an AddPermissionlessValidatorTx", func() {
 			sk, err := bls.NewSecretKey()
 			require.NoError(err)
 			pop := signer.NewProofOfPossession(sk)
 
-			tc.By("issue add validator tx", func() {
-				_, err := pWallet.IssueAddPermissionlessValidatorTx(
-					vdr,
-					pop,
-					avaxAssetID,
-					rewardOwner,
-					rewardOwner,
-					shares,
-					tc.WithDefaultContext(),
-				)
-				require.NoError(err)
-			})
-
-			tc.By("issue add delegator tx", func() {
-				_, err := pWallet.IssueAddPermissionlessDelegatorTx(
-					vdr,
-					avaxAssetID,
-					rewardOwner,
-					tc.WithDefaultContext(),
-				)
-				require.NoError(err)
-			})
-
-			// retrieve initial balances
-			pBalances, err := pWallet.Builder().GetBalance()
+			_, err = pWallet.IssueAddPermissionlessValidatorTx(
+				vdr,
+				pop,
+				avaxAssetID,
+				rewardOwner,
+				rewardOwner,
+				delegationFeeShares,
+				tc.WithDefaultContext(),
+			)
 			require.NoError(err)
-			pStartBalance := pBalances[avaxAssetID]
-			tc.Outf("{{blue}} P-chain balance before P->X export: %d {{/}}\n", pStartBalance)
+		})
 
-			xBalances, err := xWallet.Builder().GetFTBalance()
+		tc.By("issuing an AddPermissionlessDelegatorTx", func() {
+			_, err := pWallet.IssueAddPermissionlessDelegatorTx(
+				vdr,
+				avaxAssetID,
+				rewardOwner,
+				tc.WithDefaultContext(),
+			)
 			require.NoError(err)
-			xStartBalance := xBalances[avaxAssetID]
-			tc.Outf("{{blue}} X-chain balance before P->X export: %d {{/}}\n", xStartBalance)
+		})
 
-			outputOwner := secp256k1fx.OutputOwners{
-				Threshold: 1,
-				Addrs: []ids.ShortID{
-					xTargetAddr,
-				},
-			}
-			output := &secp256k1fx.TransferOutput{
-				Amt:          toTransfer,
-				OutputOwners: outputOwner,
-			}
+		tc.By("issuing an ExportTx on the P-chain", func() {
+			balances, err := pBuilder.GetBalance()
+			require.NoError(err)
 
-			tc.By("export avax from P to X chain", func() {
-				_, err := pWallet.IssueExportTx(
-					xContext.BlockchainID,
-					[]*avax.TransferableOutput{
-						{
-							Asset: avax.Asset{
-								ID: avaxAssetID,
-							},
-							Out: output,
+			initialAVAXBalance := balances[avaxAssetID]
+			tc.Outf("{{blue}} P-chain balance before P->X export: %d {{/}}\n", initialAVAXBalance)
+
+			_, err = pWallet.IssueExportTx(
+				xContext.BlockchainID,
+				[]*avax.TransferableOutput{
+					{
+						Asset: avax.Asset{
+							ID: avaxAssetID,
+						},
+						Out: &secp256k1fx.TransferOutput{
+							Amt:          toTransfer,
+							OutputOwners: transferOwner,
 						},
 					},
-					tc.WithDefaultContext(),
-				)
-				require.NoError(err)
-			})
-
-			// check balances post export
-			pBalances, err = pWallet.Builder().GetBalance()
+				},
+				tc.WithDefaultContext(),
+			)
 			require.NoError(err)
-			pPreImportBalance := pBalances[avaxAssetID]
-			tc.Outf("{{blue}} P-chain balance after P->X export: %d {{/}}\n", pPreImportBalance)
 
-			xBalances, err = xWallet.Builder().GetFTBalance()
+			balances, err = pBuilder.GetBalance()
 			require.NoError(err)
-			xPreImportBalance := xBalances[avaxAssetID]
-			tc.Outf("{{blue}} X-chain balance after P->X export: %d {{/}}\n", xPreImportBalance)
 
-			require.Equal(xPreImportBalance, xStartBalance) // import not performed yet
-			require.Equal(pPreImportBalance, pStartBalance-toTransfer-txFees)
+			finalAVAXBalance := balances[avaxAssetID]
+			tc.Outf("{{blue}} P-chain balance after P->X export: %d {{/}}\n", finalAVAXBalance)
 
-			tc.By("import avax from P into X chain", func() {
-				_, err := xWallet.IssueImportTx(
-					constants.PlatformChainID,
-					&outputOwner,
-					tc.WithDefaultContext(),
-				)
-				require.NoError(err)
-			})
-
-			// check balances post import
-			pBalances, err = pWallet.Builder().GetBalance()
-			require.NoError(err)
-			pFinalBalance := pBalances[avaxAssetID]
-			tc.Outf("{{blue}} P-chain balance after P->X import: %d {{/}}\n", pFinalBalance)
-
-			xBalances, err = xWallet.Builder().GetFTBalance()
-			require.NoError(err)
-			xFinalBalance := xBalances[avaxAssetID]
-			tc.Outf("{{blue}} X-chain balance after P->X import: %d {{/}}\n", xFinalBalance)
-
-			require.Equal(xFinalBalance, xPreImportBalance+toTransfer-txFees) // import not performed yet
-			require.Equal(pFinalBalance, pPreImportBalance)
+			require.Equal(initialAVAXBalance-toTransfer-pContext.StaticFeeConfig.TxFee, finalAVAXBalance)
 		})
+
+		tc.By("issuing an ImportTx on the X-Chain", func() {
+			balances, err := xBuilder.GetFTBalance()
+			require.NoError(err)
+
+			initialAVAXBalance := balances[avaxAssetID]
+			tc.Outf("{{blue}} X-chain balance before P->X import: %d {{/}}\n", initialAVAXBalance)
+
+			_, err = xWallet.IssueImportTx(
+				constants.PlatformChainID,
+				&transferOwner,
+				tc.WithDefaultContext(),
+			)
+			require.NoError(err)
+
+			balances, err = xBuilder.GetFTBalance()
+			require.NoError(err)
+
+			finalAVAXBalance := balances[avaxAssetID]
+			tc.Outf("{{blue}} X-chain balance after P->X import: %d {{/}}\n", finalAVAXBalance)
+
+			require.Equal(initialAVAXBalance+toTransfer-xContext.BaseTxFee, finalAVAXBalance)
+		})
+	})
 })
