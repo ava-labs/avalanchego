18633729a0cde7d695616e14b77873957a2b59c2
Darioush Jalali <darioush.jalali@avalabs.org>
2024-08-16 13:27:46 -0700

    nit: reduce diff with upstream (#1290)
    * nit: reduce diff with upstream
    
    * reduce diff

--- internal/ethapi/transaction_args.go
+++ internal/ethapi/transaction_args.go
@@ -191,13 +191,12 @@ func (args *TransactionArgs) setFeeDefaults(ctx context.Context, b feeBackend) e
 		if args.MaxFeePerGas != nil || args.MaxPriorityFeePerGas != nil {
 			return errors.New("maxFeePerGas and maxPriorityFeePerGas are not valid before London is active")
 		}
-		if args.GasPrice == nil {
-			price, err := b.SuggestGasTipCap(ctx)
-			if err != nil {
-				return err
-			}
-			args.GasPrice = (*hexutil.Big)(price)
+		// London not active, set gas price.
+		price, err := b.SuggestGasTipCap(ctx)
+		if err != nil {
+			return err
 		}
+		args.GasPrice = (*hexutil.Big)(price)
 	}
 	return nil
 }
@@ -217,11 +216,11 @@ func (args *TransactionArgs) setSubnetEVMFeeDefault(ctx context.Context, head *t
 		// Set the max fee to be 2 times larger than the previous block's base fee.
 		// The additional slack allows the tx to not become invalidated if the base
 		// fee is rising.
-		gasFeeCap := new(big.Int).Add(
-			(*big.Int)(args.MaxPriorityFeePerGas),
+		val := new(big.Int).Add(
+			args.MaxPriorityFeePerGas.ToInt(),
 			new(big.Int).Mul(head.BaseFee, big.NewInt(2)),
 		)
-		args.MaxFeePerGas = (*hexutil.Big)(gasFeeCap)
+		args.MaxFeePerGas = (*hexutil.Big)(val)
 	}
 	// Both EIP-1559 fee parameters are now set; sanity check them.
 	if args.MaxFeePerGas.ToInt().Cmp(args.MaxPriorityFeePerGas.ToInt()) < 0 {
