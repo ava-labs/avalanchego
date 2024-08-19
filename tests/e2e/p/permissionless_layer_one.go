a8524ba61943223459aad2054b0709cac46f82df
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2024-08-19 13:52:13 -0400

    cleanup

--- tests/e2e/p/permissionless_layer_one.go
+++ tests/e2e/p/permissionless_layer_one.go
@@ -11,11 +11,9 @@ import (
 	"github.com/ava-labs/avalanchego/api/info"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/tests/fixture/e2e"
-	"github.com/ava-labs/avalanchego/tests/fixture/tmpnet"
 	"github.com/ava-labs/avalanchego/utils/constants"
 	"github.com/ava-labs/avalanchego/vms/platformvm"
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
-	"github.com/ava-labs/avalanchego/wallet/chain/p"
 
 	ginkgo "github.com/onsi/ginkgo/v2"
 )
@@ -24,13 +22,9 @@ var _ = e2e.DescribePChain("[Permissionless L1]", func() {
 	tc := e2e.NewTestContext()
 	require := require.New(tc)
 
-	var (
-		env     *e2e.TestEnvironment
-		nodeURI tmpnet.NodeURI
-	)
 	ginkgo.It("skips test if Etna is not activated", func() {
-		env = e2e.GetEnv(tc)
-		nodeURI = env.GetRandomNodeURI()
+		env := e2e.GetEnv(tc)
+		nodeURI := env.GetRandomNodeURI()
 		infoClient := info.NewClient(nodeURI.URI)
 
 		tc.By("fetching upgrade config")
@@ -44,18 +38,15 @@ var _ = e2e.DescribePChain("[Permissionless L1]", func() {
 		}
 	})
 
-	var (
-		subnetID ids.ID
-		pWallet  p.Wallet
-		pClient  platformvm.Client
-		keychain *secp256k1fx.Keychain
-	)
-	ginkgo.It("creates a Permissioned Subnet", func() {
-		keychain = env.NewKeychain(1)
+	ginkgo.It("creates a Permissionless L1", func() {
+		env := e2e.GetEnv(tc)
+		nodeURI := env.GetRandomNodeURI()
+
+		keychain := env.NewKeychain(1)
 		baseWallet := e2e.NewWallet(tc, keychain, nodeURI)
 
-		pWallet = baseWallet.P()
-		pClient = platformvm.NewClient(nodeURI.URI)
+		pWallet := baseWallet.P()
+		pClient := platformvm.NewClient(nodeURI.URI)
 
 		owner := &secp256k1fx.OutputOwners{
 			Threshold: 1,
@@ -71,8 +62,8 @@ var _ = e2e.DescribePChain("[Permissionless L1]", func() {
 		)
 		require.NoError(err)
 
-		tc.By("verifying the Subnet was successfully created")
-		subnetID = subnetTx.ID()
+		tc.By("verifying a Permissioned Subnet was successfully created")
+		subnetID := subnetTx.ID()
 		require.NotEqual(subnetID, constants.PrimaryNetworkID)
 
 		res, err := pClient.GetSubnet(tc.DefaultContext(), subnetID)
@@ -85,14 +76,12 @@ var _ = e2e.DescribePChain("[Permissionless L1]", func() {
 			},
 			Threshold: 1,
 		}, res)
-	})
 
-	ginkgo.It("converts the Permissioned Subnet to a Permissionless L1", func() {
 		chainID := ids.GenerateTestID()
 		address := []byte{'a', 'd', 'd', 'r', 'e', 's', 's'}
 
 		tc.By("issuing a ConvertSubnetTx")
-		_, err := pWallet.IssueConvertSubnetTx(
+		_, err = pWallet.IssueConvertSubnetTx(
 			subnetID,
 			chainID,
 			address,
@@ -100,8 +89,8 @@ var _ = e2e.DescribePChain("[Permissionless L1]", func() {
 		)
 		require.NoError(err)
 
-		tc.By("verifying the Subnet is now a Permissionless L1")
-		res, err := pClient.GetSubnet(tc.DefaultContext(), subnetID)
+		tc.By("verifying the Permissioned Subnet was converted to a Permissionless L1")
+		res, err = pClient.GetSubnet(tc.DefaultContext(), subnetID)
 		require.NoError(err)
 
 		require.Equal(platformvm.GetSubnetClientResponse{
