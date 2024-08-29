abf90349f0ed791e65acc36cb0cc2106ae0dcad7
Stephen Buttolph <stephen@avalabs.org>
2024-08-29 19:04:54 -0400

    Add blake3 benchmark

--- x/merkledb/hashing.go
+++ x/merkledb/hashing.go
@@ -8,6 +8,8 @@ import (
 	"encoding/binary"
 	"slices"
 
+	"github.com/zeebo/blake3"
+
 	"github.com/ava-labs/avalanchego/ids"
 )
 
@@ -16,6 +18,7 @@ const HashLength = 32
 
 var (
 	SHA256Hasher Hasher = &sha256Hasher{}
+	BLAKE3Hasher Hasher = &blake3Hasher{}
 
 	// If a Hasher isn't specified, this package defaults to using the
 	// [SHA256Hasher].
@@ -89,11 +92,70 @@ func (*sha256Hasher) HashNode(n *node) ids.ID {
 // This method is performance critical. It is not expected to perform any memory
 // allocations.
 func (*sha256Hasher) HashValue(value []byte) ids.ID {
-	sha := sha256.New()
-	// sha.Write always returns nil, so we ignore its return values.
-	_, _ = sha.Write(value)
+	return sha256.Sum256(value)
+}
 
-	var hash ids.ID
-	sha.Sum(hash[:0])
+// TODO: If the hasher were not used concurrently, we could reset the internal
+// state for a significant performance improvement.
+type blake3Hasher struct{}
+
+// This method is performance critical. It is not expected to perform any memory
+// allocations.
+func (*blake3Hasher) HashNode(n *node) ids.ID {
+	var (
+		// blake.Write always returns nil, so we ignore its return values.
+		blake = blake3.New()
+		hash  ids.ID
+		// The hash length is larger than the maximum Uvarint length. This
+		// ensures binary.AppendUvarint doesn't perform any memory allocations.
+		emptyHashBuffer = hash[:0]
+	)
+
+	// By directly calling sha.Write rather than passing sha around as an
+	// io.Writer, the compiler can perform sufficient escape analysis to avoid
+	// allocating buffers on the heap.
+	numChildren := len(n.children)
+	_, _ = blake.Write(binary.AppendUvarint(emptyHashBuffer, uint64(numChildren)))
+
+	// Avoid allocating keys entirely if the node doesn't have any children.
+	if numChildren != 0 {
+		// By allocating BranchFactorLargest rather than [numChildren], this
+		// slice is allocated on the stack rather than the heap.
+		// BranchFactorLargest is at least [numChildren] which avoids memory
+		// allocations.
+		keys := make([]byte, numChildren, BranchFactorLargest)
+		i := 0
+		for k := range n.children {
+			keys[i] = k
+			i++
+		}
+
+		// Ensure that the order of entries is correct.
+		slices.Sort(keys)
+		for _, index := range keys {
+			entry := n.children[index]
+			_, _ = blake.Write(binary.AppendUvarint(emptyHashBuffer, uint64(index)))
+			_, _ = blake.Write(entry.id[:])
+		}
+	}
+
+	if n.valueDigest.HasValue() {
+		_, _ = blake.Write(trueBytes)
+		value := n.valueDigest.Value()
+		_, _ = blake.Write(binary.AppendUvarint(emptyHashBuffer, uint64(len(value))))
+		_, _ = blake.Write(value)
+	} else {
+		_, _ = blake.Write(falseBytes)
+	}
+
+	_, _ = blake.Write(binary.AppendUvarint(emptyHashBuffer, uint64(n.key.length)))
+	_, _ = blake.Write(n.key.Bytes())
+	blake.Sum(emptyHashBuffer)
 	return hash
+}
+
+// This method is performance critical. It is not expected to perform any memory
+// allocations.
+func (*blake3Hasher) HashValue(value []byte) ids.ID {
+	return blake3.Sum256(value)
 }
