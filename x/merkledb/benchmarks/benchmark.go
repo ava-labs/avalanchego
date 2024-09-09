30f05e74c41a922d55c10f183987f7c6b9729ce0
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2024-09-09 16:28:45 -0400

    update

--- x/merkledb/benchmarks/benchmark.go
+++ x/merkledb/benchmarks/benchmark.go
@@ -25,10 +25,10 @@ import (
 )
 
 const (
-	defaultDatabaseEntries    = 2000000
-	databaseCreationBatchSize = 1000000
-	databaseRunningBatchSize  = 2500
-	databaseRunningUpdateSize = 5000
+	defaultDatabaseEntries    = 1000000000 // 1B
+	databaseCreationBatchSize = 10000      // 10k
+	databaseRunningBatchSize  = 2500       // 2.5k
+	databaseRunningUpdateSize = 5000       // 5k
 	defaultMetricsPort        = 3000
 )
 
@@ -36,17 +36,59 @@ var (
 	databaseEntries = pflag.Uint64("n", defaultDatabaseEntries, "number of database entries")
 	httpMetricPort  = pflag.Uint64("p", defaultMetricsPort, "default metrics port")
 	verbose         = pflag.Bool("v", false, "verbose")
+
+	insertsCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "merkledb_bench",
+		Name:      "insert_counter",
+		Help:      "Total number of inserts",
+	})
+	deletesCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "merkledb_bench",
+		Name:      "deletes_counter",
+		Help:      "Total number of deletes",
+	})
+	updatesCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "merkledb_bench",
+		Name:      "updates_counter",
+		Help:      "Total number of updates",
+	})
+	batchCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "merkledb_bench",
+		Name:      "batch",
+		Help:      "Total number of batches written",
+	})
+	deleteRate = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "merkledb_bench",
+		Name:      "entry_delete_rate",
+		Help:      "The rate at which elements are deleted",
+	})
+	updateRate = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "merkledb_bench",
+		Name:      "entry_update_rate",
+		Help:      "The rate at which elements are updated",
+	})
+	insertRate = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "merkledb_bench",
+		Name:      "entry_insert_rate",
+		Help:      "The rate at which elements are inserted",
+	})
+	batchWriteRate = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "merkledb_bench",
+		Name:      "batch_write_rate",
+		Help:      "The rate at which the batch was written",
+	})
+	promRegistry = prometheus.NewRegistry()
 )
 
 func getMerkleDBConfig(promRegistry prometheus.Registerer) merkledb.Config {
-	const defaultHistoryLength = 300
+	const defaultHistoryLength = 120
 	return merkledb.Config{
 		BranchFactor:                merkledb.BranchFactor16,
 		Hasher:                      merkledb.DefaultHasher,
 		RootGenConcurrency:          0,
 		HistoryLength:               defaultHistoryLength,
 		ValueNodeCacheSize:          units.MiB,
-		IntermediateNodeCacheSize:   units.MiB,
+		IntermediateNodeCacheSize:   1024 * units.MiB,
 		IntermediateWriteBufferSize: units.KiB,
 		IntermediateWriteBatchSize:  256 * units.KiB,
 		Reg:                         promRegistry,
@@ -89,8 +131,6 @@ func createGoldenDatabase() error {
 		return err
 	}
 
-	promRegistry := prometheus.NewRegistry()
-
 	levelDB, err := leveldb.New(
 		getGoldenStagingDatabaseDirectory(),
 		nil,
@@ -108,7 +148,7 @@ func createGoldenDatabase() error {
 		return err
 	}
 
-	fmt.Printf("Initializing database.")
+	fmt.Print("Initializing database.")
 	ticksCh := make(chan interface{})
 	go func() {
 		t := time.NewTicker(time.Second)
@@ -118,12 +158,13 @@ func createGoldenDatabase() error {
 			case <-ticksCh:
 				return
 			case <-t.C:
-				fmt.Printf(".")
+				fmt.Print(".")
 			}
 		}
 	}()
 
 	startInsertTime := time.Now()
+	startInsertBatchTime := startInsertTime
 	currentBatch := mdb.NewBatch()
 	for entryIdx := uint64(0); entryIdx < *databaseEntries; entryIdx++ {
 		entryHash := calculateIndexEncoding(entryIdx)
@@ -140,6 +181,11 @@ func createGoldenDatabase() error {
 				return err
 			}
 			currentBatch.Reset()
+
+			addDuration := time.Since(startInsertBatchTime)
+			insertRate.Set(float64(databaseCreationBatchSize) * float64(time.Second) / float64(addDuration))
+			insertsCounter.Add(databaseCreationBatchSize)
+			startInsertBatchTime = time.Now()
 		}
 	}
 	if (*databaseEntries)%databaseCreationBatchSize != 0 {
@@ -151,7 +197,7 @@ func createGoldenDatabase() error {
 	}
 	close(ticksCh)
 
-	fmt.Printf(" done!\n")
+	fmt.Print(" done!\n")
 	rootID, err := mdb.GetMerkleRoot(context.Background())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "unable to get root ID : %v\n", err)
@@ -202,61 +248,6 @@ func resetRunningDatabaseDirectory() error {
 }
 
 func runBenchmark() error {
-	promRegistry := prometheus.NewRegistry()
-	if err := prometheus.Register(promRegistry); err != nil {
-		return err
-	}
-
-	writesCounter := prometheus.NewCounter(prometheus.CounterOpts{
-		Namespace: "merkledb_bench",
-		Name:      "writes",
-		Help:      "Total number of keys written",
-	})
-	batchCounter := prometheus.NewCounter(prometheus.CounterOpts{
-		Namespace: "merkledb_bench",
-		Name:      "batch",
-		Help:      "Total number of batches written",
-	})
-	deleteRate := prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: "merkledb_bench",
-		Name:      "entry_delete_rate",
-		Help:      "The rate at which elements are deleted",
-	})
-	updateRate := prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: "merkledb_bench",
-		Name:      "entry_update_rate",
-		Help:      "The rate at which elements are updated",
-	})
-	insertRate := prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: "merkledb_bench",
-		Name:      "entry_insert_rate",
-		Help:      "The rate at which elements are inserted",
-	})
-	batchWriteRate := prometheus.NewGauge(prometheus.GaugeOpts{
-		Namespace: "merkledb_bench",
-		Name:      "batch_write_rate",
-		Help:      "The rate at which the batch was written",
-	})
-	promRegistry.MustRegister(writesCounter)
-	promRegistry.MustRegister(batchCounter)
-	promRegistry.MustRegister(deleteRate)
-	promRegistry.MustRegister(updateRate)
-	promRegistry.MustRegister(insertRate)
-	promRegistry.MustRegister(batchWriteRate)
-
-	http.Handle("/metrics", promhttp.Handler())
-
-	server := &http.Server{
-		Addr:              fmt.Sprintf(":%d", *httpMetricPort),
-		ReadHeaderTimeout: 3 * time.Second,
-	}
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			fmt.Fprintf(os.Stderr, "unable to listen and serve : %v\n", err)
-		}
-	}()
-
 	levelDB, err := leveldb.New(
 		getRunningDatabaseDirectory(),
 		nil,
@@ -296,6 +287,7 @@ func runBenchmark() error {
 		}
 		deleteDuration = time.Since(startDeleteTime)
 		deleteRate.Set(float64(databaseRunningBatchSize) * float64(time.Second) / float64(deleteDuration))
+		deletesCounter.Add(databaseRunningBatchSize)
 
 		// add 2.5k past end.
 		startInsertTime := time.Now()
@@ -309,6 +301,7 @@ func runBenchmark() error {
 		}
 		addDuration = time.Since(startInsertTime)
 		insertRate.Set(float64(databaseRunningBatchSize) * float64(time.Second) / float64(addDuration))
+		insertsCounter.Add(databaseRunningBatchSize)
 
 		// update middle 5k entries
 		startUpdateTime := time.Now()
@@ -323,6 +316,7 @@ func runBenchmark() error {
 		}
 		updateDuration = time.Since(startUpdateTime)
 		updateRate.Set(float64(databaseRunningUpdateSize) * float64(time.Second) / float64(updateDuration))
+		updatesCounter.Add(databaseRunningUpdateSize)
 
 		batchWriteStartTime := time.Now()
 		err = batch.Write()
@@ -343,15 +337,48 @@ func runBenchmark() error {
 				time.Second/addDuration,
 				time.Second/batchDuration)
 		}
-		writesCounter.Add(databaseRunningBatchSize + databaseRunningBatchSize + databaseRunningUpdateSize)
+
 		batchCounter.Inc()
 	}
 
 	// return nil
 }
 
+func setupMetrics() error {
+	if err := prometheus.Register(promRegistry); err != nil {
+		return err
+	}
+	promRegistry.MustRegister(insertsCounter)
+	promRegistry.MustRegister(deletesCounter)
+	promRegistry.MustRegister(updatesCounter)
+	promRegistry.MustRegister(batchCounter)
+	promRegistry.MustRegister(deleteRate)
+	promRegistry.MustRegister(updateRate)
+	promRegistry.MustRegister(insertRate)
+	promRegistry.MustRegister(batchWriteRate)
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", *httpMetricPort),
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			fmt.Fprintf(os.Stderr, "unable to listen and serve : %v\n", err)
+		}
+	}()
+	return nil
+}
+
 func main() {
 	pflag.Parse()
+
+	if setupMetrics() != nil {
+		os.Exit(1)
+		return
+	}
 
 	goldenDir := getGoldenDatabaseDirectory()
 	if _, err := os.Stat(goldenDir); os.IsNotExist(err) {
