2014fdc6e51bea3068f08374f59ceca0b287b4a2
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2024-09-16 17:19:51 -0400

    add a benchmark to compare with eth's merkle trie.

---
+++ x/merkledb/benchmarks_eth/benchmark.go
@@ -0,0 +1,456 @@
+// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"net/http"
+	"os"
+	"path"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spf13/pflag"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/trie"
+	"github.com/ethereum/go-ethereum/trie/trienode"
+	"github.com/ethereum/go-ethereum/triedb"
+)
+
+const (
+	defaultDatabaseEntries    = 1000000000 // 1B
+	databaseCreationBatchSize = 10000      // 10k
+	databaseRunningBatchSize  = 2500       // 2.5k
+	databaseRunningUpdateSize = 5000       // 5k
+	defaultMetricsPort        = 3000
+)
+
+var (
+	databaseEntries = pflag.Uint64("n", defaultDatabaseEntries, "number of database entries")
+	httpMetricPort  = pflag.Uint64("p", defaultMetricsPort, "default metrics port")
+	verbose         = pflag.Bool("v", false, "verbose")
+
+	insertsCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "merkledb_bench",
+		Name:      "insert_counter",
+		Help:      "Total number of inserts",
+	})
+	deletesCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "merkledb_bench",
+		Name:      "deletes_counter",
+		Help:      "Total number of deletes",
+	})
+	updatesCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "merkledb_bench",
+		Name:      "updates_counter",
+		Help:      "Total number of updates",
+	})
+	batchCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "merkledb_bench",
+		Name:      "batch",
+		Help:      "Total number of batches written",
+	})
+	deleteRate = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "merkledb_bench",
+		Name:      "entry_delete_rate",
+		Help:      "The rate at which elements are deleted",
+	})
+	updateRate = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "merkledb_bench",
+		Name:      "entry_update_rate",
+		Help:      "The rate at which elements are updated",
+	})
+	insertRate = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "merkledb_bench",
+		Name:      "entry_insert_rate",
+		Help:      "The rate at which elements are inserted",
+	})
+	batchWriteRate = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "merkledb_bench",
+		Name:      "batch_write_rate",
+		Help:      "The rate at which the batch was written",
+	})
+	promRegistry = prometheus.NewRegistry()
+)
+
+/*
+func getMerkleDBConfig(promRegistry prometheus.Registerer) merkledb.Config {
+	const defaultHistoryLength = 120
+	return merkledb.Config{
+		BranchFactor:                merkledb.BranchFactor16,
+		Hasher:                      merkledb.DefaultHasher,
+		RootGenConcurrency:          0,
+		HistoryLength:               defaultHistoryLength,
+		ValueNodeCacheSize:          units.MiB,
+		IntermediateNodeCacheSize:   1024 * units.MiB,
+		IntermediateWriteBufferSize: units.KiB,
+		IntermediateWriteBatchSize:  256 * units.KiB,
+		Reg:                         promRegistry,
+		TraceLevel:                  merkledb.NoTrace,
+		Tracer:                      trace.Noop,
+	}
+}*/
+
+func getGoldenStagingDatabaseDirectory() string {
+	wd, _ := os.Getwd()
+	return path.Join(wd, fmt.Sprintf("db-bench-test-golden-staging-%d", *databaseEntries))
+}
+
+func getGoldenDatabaseDirectory() string {
+	wd, _ := os.Getwd()
+	return path.Join(wd, fmt.Sprintf("db-bench-test-golden-%d", *databaseEntries))
+}
+
+func getRunningDatabaseDirectory() string {
+	wd, _ := os.Getwd()
+	return path.Join(wd, fmt.Sprintf("db-bench-test-running-%d", *databaseEntries))
+}
+
+func calculateIndexEncoding(idx uint64) []byte {
+	var entryEncoding [8]byte
+	binary.NativeEndian.PutUint64(entryEncoding[:], idx)
+	entryHash := sha256.Sum256(entryEncoding[:])
+	return entryHash[:]
+}
+
+func createGoldenDatabase() error {
+	stagingDirectory := getGoldenStagingDatabaseDirectory()
+	err := os.RemoveAll(stagingDirectory)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to remove running directory : %v\n", err)
+		return err
+	}
+	err = os.Mkdir(stagingDirectory, 0o777)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to create golden staging directory : %v\n", err)
+		return err
+	}
+
+	ldb, err := rawdb.NewLevelDBDatabase(stagingDirectory, 500, 200, "metrics_prefix", false)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to create level db database : %v\n", err)
+		return err
+	}
+
+	trieDb := triedb.NewDatabase(ldb, &triedb.Config{
+		Preimages: false,
+		IsVerkle:  false,
+		HashDB:    nil,
+		PathDB:    nil,
+	})
+	tdb := trie.NewEmpty(trieDb)
+
+	fmt.Print("Initializing database.")
+	ticksCh := make(chan interface{})
+	go func() {
+		t := time.NewTicker(time.Second)
+		defer t.Stop()
+		for {
+			select {
+			case <-ticksCh:
+				return
+			case <-t.C:
+				fmt.Print(".")
+			}
+		}
+	}()
+
+	var root common.Hash
+	writeBatch := func() error {
+		var nodes *trienode.NodeSet
+		root, nodes = tdb.Commit(false)
+		err = trieDb.Update(root, types.EmptyRootHash, 0 /*block*/, trienode.NewWithNodeSet(nodes), nil /*states*/)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to update trie : %v\n", err)
+			return err
+		}
+		err = trieDb.Commit(root, false)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to commit trie : %v\n", err)
+			return err
+		}
+		tdb, err = trie.New(trie.TrieID(root), trieDb)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to create new trie : %v\n", err)
+			return err
+		}
+		return nil
+	}
+
+	startInsertTime := time.Now()
+	startInsertBatchTime := startInsertTime
+	for entryIdx := uint64(0); entryIdx < *databaseEntries; entryIdx++ {
+		entryHash := calculateIndexEncoding(entryIdx)
+		tdb.Update(entryHash, entryHash)
+
+		if entryIdx%databaseCreationBatchSize == (databaseCreationBatchSize - 1) {
+			addDuration := time.Since(startInsertBatchTime)
+			insertRate.Set(float64(databaseCreationBatchSize) * float64(time.Second) / float64(addDuration))
+			insertsCounter.Add(databaseCreationBatchSize)
+
+			batchWriteStartTime := time.Now()
+
+			err = writeBatch()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "unable to write value in database : %v\n", err)
+				return err
+			}
+
+			batchWriteDuration := time.Since(batchWriteStartTime)
+			batchWriteRate.Set(float64(time.Second) / float64(batchWriteDuration))
+			batchCounter.Inc()
+
+			startInsertBatchTime = time.Now()
+		}
+	}
+	if (*databaseEntries)%databaseCreationBatchSize != 0 {
+		err = writeBatch()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to write value in database : %v\n", err)
+			return err
+		}
+	}
+	close(ticksCh)
+	fmt.Print(" done!\n")
+
+	fmt.Printf("Generated and inserted %d batches of size %d in %v\n",
+		(*databaseEntries)/databaseCreationBatchSize, databaseCreationBatchSize, time.Since(startInsertTime))
+	err = trieDb.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to close trie database : %v\n", err)
+		return err
+	}
+	err = ldb.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to close levelDB database : %v\n", err)
+		return err
+	}
+
+	err = os.WriteFile(path.Join(stagingDirectory, "root.txt"), root.Bytes(), 0o644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to save root : %v\n", err)
+		return err
+	}
+
+	err = os.Rename(getGoldenStagingDatabaseDirectory(), getGoldenDatabaseDirectory())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to rename golden staging directory : %v\n", err)
+		return err
+	}
+	fmt.Printf("Completed initialization with hash of %v\n", root.Hex())
+	return nil
+}
+
+func resetRunningDatabaseDirectory() error {
+	runningDir := getRunningDatabaseDirectory()
+	if _, err := os.Stat(runningDir); err == nil {
+		err := os.RemoveAll(runningDir)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to remove running directory : %v\n", err)
+			return err
+		}
+	}
+	err := os.Mkdir(runningDir, 0o777)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to create running directory : %v\n", err)
+		return err
+	}
+	err = CopyDirectory(getGoldenDatabaseDirectory(), runningDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to duplicate golden directory : %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func runBenchmark() error {
+	rootBytes, err := os.ReadFile(path.Join(getRunningDatabaseDirectory(), "root.txt"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to read root : %v\n", err)
+		return err
+	}
+
+	ldb, err := rawdb.Open(rawdb.OpenOptions{
+		Type:              "leveldb",
+		Directory:         getRunningDatabaseDirectory(),
+		AncientsDirectory: "",
+		Namespace:         "metrics_prefix",
+		Cache:             500,
+		Handles:           200,
+		ReadOnly:          false,
+
+		Ephemeral: false,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to create level db database : %v\n", err)
+		return err
+	}
+
+	trieDb := triedb.NewDatabase(ldb, &triedb.Config{
+		Preimages: false,
+		IsVerkle:  false,
+		HashDB:    nil,
+		PathDB:    nil,
+	})
+
+	root := common.BytesToHash(rootBytes)
+	tdb, err := trie.New(trie.TrieID(root), trieDb)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to create trie database : %v\n", err)
+		return err
+	}
+
+	writeBatch := func() error {
+		var nodes *trienode.NodeSet
+		root, nodes = tdb.Commit(false)
+		err = trieDb.Update(root, types.EmptyRootHash, 0 /*block*/, trienode.NewWithNodeSet(nodes), nil /*states*/)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to update trie : %v\n", err)
+			return err
+		}
+		err = trieDb.Commit(root, false)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to commit trie : %v\n", err)
+			return err
+		}
+		tdb, err = trie.New(trie.TrieID(root), trieDb)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to create new trie : %v\n", err)
+			return err
+		}
+		return nil
+	}
+
+	low := uint64(0)
+	var deleteDuration, addDuration, updateDuration, batchDuration time.Duration
+	for {
+		startBatchTime := time.Now()
+
+		// delete first 2.5k keys from the beginning
+		startDeleteTime := time.Now()
+		for keyToDeleteIdx := low; keyToDeleteIdx < low+databaseRunningBatchSize; keyToDeleteIdx++ {
+			entryHash := calculateIndexEncoding(keyToDeleteIdx)
+			err = tdb.Delete(entryHash)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "unable to delete merkleDB entry : %v\n", err)
+				return err
+			}
+		}
+		deleteDuration = time.Since(startDeleteTime)
+		deleteRate.Set(float64(databaseRunningBatchSize) * float64(time.Second) / float64(deleteDuration))
+		deletesCounter.Add(databaseRunningBatchSize)
+
+		// add 2.5k past end.
+		startInsertTime := time.Now()
+		for keyToAddIdx := low + (*databaseEntries); keyToAddIdx < low+(*databaseEntries)+databaseRunningBatchSize; keyToAddIdx++ {
+			entryHash := calculateIndexEncoding(keyToAddIdx)
+			err = tdb.Update(entryHash, entryHash)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "unable to insert merkleDB entry : %v\n", err)
+				return err
+			}
+		}
+		addDuration = time.Since(startInsertTime)
+		insertRate.Set(float64(databaseRunningBatchSize) * float64(time.Second) / float64(addDuration))
+		insertsCounter.Add(databaseRunningBatchSize)
+
+		// update middle 5k entries
+		startUpdateTime := time.Now()
+		updateEntryValue := calculateIndexEncoding(low)
+		for keyToUpdateIdx := low + ((*databaseEntries) / 2); keyToUpdateIdx < low+((*databaseEntries)/2)+databaseRunningUpdateSize; keyToUpdateIdx++ {
+			updateEntryKey := calculateIndexEncoding(keyToUpdateIdx)
+			err = tdb.Update(updateEntryKey, updateEntryValue)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "unable to update merkleDB entry : %v\n", err)
+				return err
+			}
+		}
+		updateDuration = time.Since(startUpdateTime)
+		updateRate.Set(float64(databaseRunningUpdateSize) * float64(time.Second) / float64(updateDuration))
+		updatesCounter.Add(databaseRunningUpdateSize)
+
+		batchWriteStartTime := time.Now()
+		err = writeBatch()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to write batch : %v\n", err)
+			return err
+		}
+		batchDuration = time.Since(startBatchTime)
+		batchWriteDuration := time.Since(batchWriteStartTime)
+		batchWriteRate.Set(float64(time.Second) / float64(batchWriteDuration))
+
+		if *verbose {
+			fmt.Printf("delete rate [%d]	update rate [%d]	insert rate [%d]	batch rate [%d]\n",
+				time.Second/deleteDuration,
+				time.Second/updateDuration,
+				time.Second/addDuration,
+				time.Second/batchDuration)
+		}
+
+		batchCounter.Inc()
+		low += databaseRunningBatchSize
+	}
+
+	// return nil
+}
+
+func setupMetrics() error {
+	if err := prometheus.Register(promRegistry); err != nil {
+		return err
+	}
+	promRegistry.MustRegister(insertsCounter)
+	promRegistry.MustRegister(deletesCounter)
+	promRegistry.MustRegister(updatesCounter)
+	promRegistry.MustRegister(batchCounter)
+	promRegistry.MustRegister(deleteRate)
+	promRegistry.MustRegister(updateRate)
+	promRegistry.MustRegister(insertRate)
+	promRegistry.MustRegister(batchWriteRate)
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", *httpMetricPort),
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			fmt.Fprintf(os.Stderr, "unable to listen and serve : %v\n", err)
+		}
+	}()
+	return nil
+}
+
+func main() {
+	pflag.Parse()
+
+	if setupMetrics() != nil {
+		os.Exit(1)
+		return
+	}
+
+	goldenDir := getGoldenDatabaseDirectory()
+	if _, err := os.Stat(goldenDir); os.IsNotExist(err) {
+		// create golden image.
+		if createGoldenDatabase() != nil {
+			os.Exit(1)
+			return
+		}
+	}
+	if resetRunningDatabaseDirectory() != nil {
+		os.Exit(1)
+		return
+	}
+	if runBenchmark() != nil {
+		os.Exit(1)
+		return
+	}
+}
---
+++ x/merkledb/benchmarks_eth/go.mod
@@ -0,0 +1,66 @@
+module github.com/ava-labs/avalanchego/x/merkledb/benchmarks_eth
+
+go 1.22.3
+
+require (
+	github.com/prometheus/client_golang v1.20.3
+	github.com/spf13/pflag v1.0.5
+)
+
+require (
+	github.com/DataDog/zstd v1.5.2 // indirect
+	github.com/VictoriaMetrics/fastcache v1.12.2 // indirect
+	github.com/bits-and-blooms/bitset v1.10.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.4 // indirect
+	github.com/cockroachdb/errors v1.11.3 // indirect
+	github.com/cockroachdb/fifo v0.0.0-20240606204812-0bbfbd93a7ce // indirect
+	github.com/cockroachdb/logtags v0.0.0-20230118201751-21c54148d20b // indirect
+	github.com/cockroachdb/pebble v1.1.1 // indirect
+	github.com/cockroachdb/redact v1.1.5 // indirect
+	github.com/cockroachdb/tokenbucket v0.0.0-20230807174530-cc333fc44b06 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240223125850-b1e8a79f509c // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/ethereum/go-verkle v0.1.1-0.20240306133620-7d920df305f0 // indirect
+	github.com/getsentry/sentry-go v0.27.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/holiman/uint256 v1.3.1 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/supranational/blst v0.3.11 // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20220614013038-64ee5596c38a // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/ethereum/go-ethereum v1.14.8
+	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
