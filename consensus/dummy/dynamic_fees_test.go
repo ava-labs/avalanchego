2fec97b5e1a7f7a7686ea4bbfb8639e0ed889d61
Ceyhun Onur <ceyhun.onur@avalabs.org>
2024-09-16 22:28:56 -0400

    fix modifying common.Big1 (#649)
    * add regression test
    
    * apply fix
    
    * use cmp
    
    * reorder equal params
    
    * improve readability by removing extra var (#650)

--- consensus/dummy/dynamic_fees_test.go
+++ consensus/dummy/dynamic_fees_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/ava-labs/coreth/core/types"
 	"github.com/ava-labs/coreth/params"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/stretchr/testify/assert"
@@ -563,4 +564,21 @@ func TestDynamicFeesEtna(t *testing.T) {
 	// After some time has passed in the Etna phase, the base fee should drop
 	// lower than the prior base fee minimum.
 	require.Less(nextBaseFee.Int64(), params.ApricotPhase4MinBaseFee)
+}
+
+func TestCalcBaseFeeRegression(t *testing.T) {
+	parentTimestamp := uint64(1)
+	timestamp := parentTimestamp + params.RollupWindow + 1000
+
+	parentHeader := &types.Header{
+		Time:    parentTimestamp,
+		GasUsed: 14_999_999,
+		Number:  big.NewInt(1),
+		BaseFee: big.NewInt(1),
+		Extra:   make([]byte, params.DynamicFeeExtraDataSize),
+	}
+
+	_, _, err := CalcBaseFee(params.TestChainConfig, parentHeader, timestamp)
+	require.NoError(t, err)
+	require.Equalf(t, 0, common.Big1.Cmp(big.NewInt(1)), "big1 should be 1, got %s", common.Big1)
 }
