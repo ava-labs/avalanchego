afc94e51a8f96d2a6d8448a9de77dbf8c211c4a3
Darioush Jalali <darioush.jalali@avalabs.org>
2024-09-18 18:28:31 -0400

    prepare v0.13.8 (#654)

--- plugin/evm/version.go
+++ plugin/evm/version.go
@@ -11,7 +11,7 @@ var (
 	// GitCommit is set by the build script
 	GitCommit string
 	// Version is the version of Coreth
-	Version string = "v0.13.7"
+	Version string = "v0.13.8"
 )
 
 func init() {
