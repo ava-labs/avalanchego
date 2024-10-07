38a093c1ff1eec2facf7adbb60acba76ea37d597
Stephen Buttolph <stephen@avalabs.org>
2024-10-07 17:03:02 -0400

    nit

--- vms/example/xsvm/warp.go
+++ vms/example/xsvm/warp.go
@@ -6,9 +6,12 @@ package xsvm
 import (
 	"context"
 
+	"github.com/ava-labs/avalanchego/network/p2p/acp118"
 	"github.com/ava-labs/avalanchego/snow/engine/common"
 	"github.com/ava-labs/avalanchego/vms/platformvm/warp"
 )
+
+var _ acp118.Verifier = (*acp118Verifier)(nil)
 
 // acp118Verifier allows signing all warp messages
 type acp118Verifier struct{}
