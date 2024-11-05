4e6079dc3c423617e7dae651cadc7835b2f9290a
Stephen Buttolph <stephen@avalabs.org>
2024-11-05 12:47:49 -0500

    Add tests for fractional seconds

--- vms/platformvm/state/chain_time_helpers_test.go
+++ vms/platformvm/state/chain_time_helpers_test.go
@@ -16,10 +16,13 @@ import (
 	"github.com/ava-labs/avalanchego/utils/constants"
 	"github.com/ava-labs/avalanchego/utils/timer/mockable"
 	"github.com/ava-labs/avalanchego/utils/units"
+	"github.com/ava-labs/avalanchego/vms/components/gas"
 	"github.com/ava-labs/avalanchego/vms/platformvm/config"
 	"github.com/ava-labs/avalanchego/vms/platformvm/genesis/genesistest"
 	"github.com/ava-labs/avalanchego/vms/platformvm/txs"
-	"github.com/ava-labs/avalanchego/vms/platformvm/txs/fee"
+
+	txfee "github.com/ava-labs/avalanchego/vms/platformvm/txs/fee"
+	validatorfee "github.com/ava-labs/avalanchego/vms/platformvm/validators/fee"
 )
 
 func TestNextBlockTime(t *testing.T) {
@@ -83,6 +86,13 @@ func TestNextBlockTime(t *testing.T) {
 }
 
 func TestGetNextStakerChangeTime(t *testing.T) {
+	config := validatorfee.Config{
+		Capacity:                 genesis.LocalParams.ValidatorFeeConfig.Capacity,
+		Target:                   genesis.LocalParams.ValidatorFeeConfig.Target,
+		MinPrice:                 gas.Price(2 * units.NanoAvax), // Increase minimum price to test fractional seconds
+		ExcessConversionConstant: genesis.LocalParams.ValidatorFeeConfig.ExcessConversionConstant,
+	}
+
 	tests := []struct {
 		name     string
 		pending  []*Staker
@@ -114,14 +124,42 @@ func TestGetNextStakerChangeTime(t *testing.T) {
 			expected: genesistest.DefaultValidatorStartTime.Add(time.Second),
 		},
 		{
-			name: "current and subnet only validator with low balance",
+			name: "subnet only validator with less than 1 second of fees",
 			sovs: []SubnetOnlyValidator{
 				{
 					ValidationID:      ids.GenerateTestID(),
 					SubnetID:          ids.GenerateTestID(),
 					NodeID:            ids.GenerateTestNodeID(),
 					Weight:            1,
-					EndAccumulatedFee: 1, // This validator should be evicted in 1 second.
+					EndAccumulatedFee: 1, // This validator should be evicted in .5 seconds, which is rounded to 0.
+				},
+			},
+			maxTime:  mockable.MaxTime,
+			expected: genesistest.DefaultValidatorStartTime,
+		},
+		{
+			name: "subnet only validator with 1 second of fees",
+			sovs: []SubnetOnlyValidator{
+				{
+					ValidationID:      ids.GenerateTestID(),
+					SubnetID:          ids.GenerateTestID(),
+					NodeID:            ids.GenerateTestNodeID(),
+					Weight:            1,
+					EndAccumulatedFee: 2, // This validator should be evicted in 1 second.
+				},
+			},
+			maxTime:  mockable.MaxTime,
+			expected: genesistest.DefaultValidatorStartTime.Add(time.Second),
+		},
+		{
+			name: "subnet only validator with less than 2 seconds of fees",
+			sovs: []SubnetOnlyValidator{
+				{
+					ValidationID:      ids.GenerateTestID(),
+					SubnetID:          ids.GenerateTestID(),
+					NodeID:            ids.GenerateTestNodeID(),
+					Weight:            1,
+					EndAccumulatedFee: 3, // This validator should be evicted in 1.5 seconds, which is rounded to 1.
 				},
 			},
 			maxTime:  mockable.MaxTime,
@@ -161,7 +199,7 @@ func TestGetNextStakerChangeTime(t *testing.T) {
 			}
 
 			actual, err := GetNextStakerChangeTime(
-				genesis.LocalParams.ValidatorFeeConfig,
+				config,
 				s,
 				test.maxTime,
 			)
@@ -184,19 +222,19 @@ func TestPickFeeCalculator(t *testing.T) {
 
 	tests := []struct {
 		fork     upgradetest.Fork
-		expected fee.Calculator
+		expected txfee.Calculator
 	}{
 		{
 			fork:     upgradetest.ApricotPhase2,
-			expected: fee.NewStaticCalculator(apricotPhase2StaticFeeConfig),
+			expected: txfee.NewStaticCalculator(apricotPhase2StaticFeeConfig),
 		},
 		{
 			fork:     upgradetest.ApricotPhase3,
-			expected: fee.NewStaticCalculator(staticFeeConfig),
+			expected: txfee.NewStaticCalculator(staticFeeConfig),
 		},
 		{
 			fork: upgradetest.Etna,
-			expected: fee.NewDynamicCalculator(
+			expected: txfee.NewDynamicCalculator(
 				dynamicFeeConfig.Weights,
 				dynamicFeeConfig.MinPrice,
 			),
