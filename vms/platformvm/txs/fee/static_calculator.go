687398e38ab941f5aa5e07d9c74b2b0c97313ff7
Stephen Buttolph <stephen@avalabs.org>
2024-11-07 12:05:16 -0500

    reduce diff

--- vms/platformvm/txs/fee/static_calculator.go
+++ vms/platformvm/txs/fee/static_calculator.go
@@ -76,16 +76,6 @@ func (c *staticVisitor) CreateSubnetTx(*txs.CreateSubnetTx) error {
 	return nil
 }
 
-func (c *staticVisitor) ImportTx(*txs.ImportTx) error {
-	c.fee = c.config.TxFee
-	return nil
-}
-
-func (c *staticVisitor) ExportTx(*txs.ExportTx) error {
-	c.fee = c.config.TxFee
-	return nil
-}
-
 func (c *staticVisitor) RemoveSubnetValidatorTx(*txs.RemoveSubnetValidatorTx) error {
 	c.fee = c.config.TxFee
 	return nil
@@ -93,6 +83,11 @@ func (c *staticVisitor) RemoveSubnetValidatorTx(*txs.RemoveSubnetValidatorTx) er
 
 func (c *staticVisitor) TransformSubnetTx(*txs.TransformSubnetTx) error {
 	c.fee = c.config.TransformSubnetTxFee
+	return nil
+}
+
+func (c *staticVisitor) TransferSubnetOwnershipTx(*txs.TransferSubnetOwnershipTx) error {
+	c.fee = c.config.TxFee
 	return nil
 }
 
@@ -114,12 +109,17 @@ func (c *staticVisitor) AddPermissionlessDelegatorTx(tx *txs.AddPermissionlessDe
 	return nil
 }
 
-func (c *staticVisitor) TransferSubnetOwnershipTx(*txs.TransferSubnetOwnershipTx) error {
+func (c *staticVisitor) BaseTx(*txs.BaseTx) error {
 	c.fee = c.config.TxFee
 	return nil
 }
 
-func (c *staticVisitor) BaseTx(*txs.BaseTx) error {
+func (c *staticVisitor) ImportTx(*txs.ImportTx) error {
+	c.fee = c.config.TxFee
+	return nil
+}
+
+func (c *staticVisitor) ExportTx(*txs.ExportTx) error {
 	c.fee = c.config.TxFee
 	return nil
 }
