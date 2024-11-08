a35c877873900a37876b0e9f003e67457f44471c
Stephen Buttolph <stephen@avalabs.org>
2024-11-07 20:27:08 -0500

    nit

--- tests/e2e/p/l1.go
+++ tests/e2e/p/l1.go
@@ -328,18 +328,15 @@ var _ = e2e.DescribePChain("[L1]", func() {
 			require.True(ok)
 
 			tc.By("creating the signed warp message to register the validator")
-			signers := set.NewBits(0) // [signers] has weight from the genesis peer
-
-			var sigBytes [bls.SignatureLen]byte
-			copy(sigBytes[:], bls.SignatureToBytes(registerSubnetValidatorSignature))
 			registerSubnetValidator, err := warp.NewMessage(
 				unsignedRegisterSubnetValidator,
 				&warp.BitSetSignature{
-					Signers:   signers.Bytes(),
-					Signature: sigBytes,
+					Signers: set.NewBits(0).Bytes(), // [signers] has weight from the genesis peer
+					Signature: ([bls.SignatureLen]byte)(
+						bls.SignatureToBytes(registerSubnetValidatorSignature),
+					),
 				},
 			)
-			require.NoError(err)
 
 			tc.By("issuing a RegisterSubnetValidatorTx", func() {
 				_, err := pWallet.IssueRegisterSubnetValidatorTx(
