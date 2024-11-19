ce284e6c25928795c6dc0d6f9bca01dab7108863
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2024-11-19 02:58:48 -0500

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- network/p2p/acp118/aggregator.go
+++ network/p2p/acp118/aggregator.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/ava-labs/avalanchego/utils/math"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
 
@@ -18,6 +17,7 @@ import (
 	"github.com/ava-labs/avalanchego/proto/pb/sdk"
 	"github.com/ava-labs/avalanchego/utils/crypto/bls"
 	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/ava-labs/avalanchego/utils/math"
 	"github.com/ava-labs/avalanchego/utils/set"
 	"github.com/ava-labs/avalanchego/vms/platformvm/warp"
 )
