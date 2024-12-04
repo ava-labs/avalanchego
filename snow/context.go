0c3771247b374a0e21ea68c4c2761bdedde8d2a4
Richard Pringle <richard.pringle@avalabs.org>
2024-12-04 17:38:30 -0500

    Revert "Move test function for coreth to avalanchego"
    This reverts commit 3f46a5a4a08483259517447ddd351fc25218cb66.

--- snow/context.go
+++ snow/context.go
@@ -4,8 +4,6 @@
 package snow
 
 import (
-	"context"
-	"errors"
 	"sync"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -15,10 +13,8 @@ import (
 	"github.com/ava-labs/avalanchego/chains/atomic"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow/validators"
-	"github.com/ava-labs/avalanchego/snow/validators/validatorstest"
 	"github.com/ava-labs/avalanchego/upgrade"
 	"github.com/ava-labs/avalanchego/utils"
-	"github.com/ava-labs/avalanchego/utils/constants"
 	"github.com/ava-labs/avalanchego/utils/crypto/bls"
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/vms/platformvm/warp"
@@ -98,60 +94,4 @@ type ConsensusContext struct {
 
 	// True iff this chain is currently state-syncing
 	StateSyncing utils.Atomic[bool]
-}
-
-func NewTestContext() *Context {
-	var (
-		testCChainID = ids.ID{'c', 'c', 'h', 'a', 'i', 'n', 't', 'e', 's', 't'}
-		testXChainID = ids.ID{'t', 'e', 's', 't', 'x'}
-		testChainID  = ids.ID{'t', 'e', 's', 't', 'c', 'h', 'a', 'i', 'n'}
-	)
-
-	signer, err := bls.NewSigner()
-	if err != nil {
-		panic(err)
-	}
-	pk := signer.PublicKey()
-	networkID := constants.UnitTestID
-	chainID := testChainID
-
-	validatorState := &validatorstest.State{
-		GetCurrentHeightF: func(context.Context) (uint64, error) {
-			return 0, nil
-		},
-		GetSubnetIDF: func(_ context.Context, chainID ids.ID) (ids.ID, error) {
-			subnetID, ok := map[ids.ID]ids.ID{
-				constants.PlatformChainID: constants.PrimaryNetworkID,
-				testXChainID:              constants.PrimaryNetworkID,
-				testCChainID:              constants.PrimaryNetworkID,
-			}[chainID]
-			if !ok {
-				return ids.Empty, errors.New("unknown chain")
-			}
-			return subnetID, nil
-		},
-		GetValidatorSetF: func(context.Context, uint64, ids.ID) (map[ids.NodeID]*validators.GetValidatorOutput, error) {
-			return map[ids.NodeID]*validators.GetValidatorOutput{}, nil
-		},
-		GetCurrentValidatorSetF: func(context.Context, ids.ID) (map[ids.ID]*validators.GetCurrentValidatorOutput, uint64, error) {
-			return map[ids.ID]*validators.GetCurrentValidatorOutput{}, 0, nil
-		},
-	}
-
-	ctx := &Context{
-		NetworkID:      networkID,
-		SubnetID:       ids.Empty,
-		ChainID:        chainID,
-		NodeID:         ids.GenerateTestNodeID(),
-		XChainID:       testXChainID,
-		CChainID:       testCChainID,
-		PublicKey:      pk,
-		WarpSigner:     warp.NewSigner(signer, networkID, chainID),
-		Log:            logging.NoLog{},
-		BCLookup:       ids.NewAliaser(),
-		Metrics:        metrics.NewPrefixGatherer(),
-		ChainDataDir:   "",
-		ValidatorState: validatorState,
-	}
-	return ctx
 }
