71d76f641ff98cdb5ca205fd887e81b1b51e03e1
Ceyhun Onur <ceyhun.onur@avalabs.org>
2024-12-11 17:35:11 +0300

    lint

--- wallet/chain/c/wallet_with_options.go
+++ wallet/chain/c/wallet_with_options.go
@@ -4,10 +4,11 @@
 package c
 
 import (
+	"github.com/ava-labs/coreth/plugin/evm/atomic"
+
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 	"github.com/ava-labs/avalanchego/wallet/subnet/primary/common"
-	"github.com/ava-labs/coreth/plugin/evm/atomic"
 
 	ethcommon "github.com/ethereum/go-ethereum/common"
 )
