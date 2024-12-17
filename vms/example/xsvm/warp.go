39e29e3a928517361dc352736461ace5212ac338
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2024-12-17 15:25:16 -0500

    update interface

--- vms/example/xsvm/warp.go
+++ vms/example/xsvm/warp.go
@@ -6,6 +6,7 @@ package xsvm
 import (
 	"context"
 
+	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/network/p2p/acp118"
 	"github.com/ava-labs/avalanchego/snow/engine/common"
 	"github.com/ava-labs/avalanchego/vms/platformvm/warp"
@@ -16,6 +17,6 @@ var _ acp118.Verifier = (*acp118Verifier)(nil)
 // acp118Verifier allows signing all warp messages
 type acp118Verifier struct{}
 
-func (acp118Verifier) Verify(context.Context, *warp.UnsignedMessage, []byte) *common.AppError {
+func (acp118Verifier) Verify(context.Context, *warp.UnsignedMessage, []byte, ids.NodeID) *common.AppError {
 	return nil
 }
--- vms/platformvm/network/warp.go
+++ vms/platformvm/network/warp.go
@@ -56,6 +56,7 @@ func (s signatureRequestVerifier) Verify(
 	_ context.Context,
 	unsignedMessage *warp.UnsignedMessage,
 	justification []byte,
+	_ ids.NodeID,
 ) *common.AppError {
 	msg, err := payload.ParseAddressedCall(unsignedMessage.Payload)
 	if err != nil {
