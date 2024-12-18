a64c770f7dbf9edd89048dc96f7630c5b19382ac
Richard Pringle <richard.pringle@avalabs.org>
2024-12-18 18:43:59 -0500

    fixup! Prepare bls-tests for multiple signer-types

--- utils/crypto/bls/test/bls_test.go
+++ utils/crypto/bls/test/bls_test.go
@@ -487,6 +487,7 @@ func TestVerifyProofOfPossession(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			require := require.New(t)
+
 			for _, newSigner := range tt.signers {
 				signer, err := newSigner()
 				require.NoError(err)
