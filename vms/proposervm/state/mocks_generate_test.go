7d1f54f1255baca45fcc60eb5ea3e38e6b973572
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2024-12-31 13:35:58 +0100

    remove unused generated mocks

--- vms/proposervm/state/mocks_generate_test.go
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-package state
-
-//go:generate go run go.uber.org/mock/mockgen@v0.4 -package=${GOPACKAGE}mock -destination=${GOPACKAGE}mock/state.go -mock_names=State=State . State
