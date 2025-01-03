c78fcc3cb79e61c73d1a56da1b44a037c47db060
Maru Newby <maru.newby@avalabs.org>
2025-01-02 18:13:10 -0800

    Ensure the xsvm image is pushed

--- scripts/build_xsvm_image.sh
+++ scripts/build_xsvm_image.sh
@@ -16,6 +16,7 @@ DOCKER_IMAGE="localhost:5001/avalanchego"
 # Build the avalancehgo node image
 FORCE_TAG_LATEST=1 SKIP_BUILD_RACE=1 DOCKER_IMAGE="${DOCKER_IMAGE}" ./scripts/build_image.sh
 
+# TODO(marun) conditionally push the image to the registry
 GO_VERSION="$(go list -m -f '{{.GoVersion}}')"
 docker buildx build --build-arg GO_VERSION="${GO_VERSION}" --build-arg AVALANCHEGO_NODE_IMAGE="${DOCKER_IMAGE}" \
-       -t "${DOCKER_IMAGE}-xsvm" -f "${AVALANCHE_PATH}/vms/example/xsvm/Dockerfile" .
+       --push -t "${DOCKER_IMAGE}-xsvm" -f "${AVALANCHE_PATH}/vms/example/xsvm/Dockerfile" .
