93ce87ba21b297203966e3d699d356e4100b1612
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-01-06 16:34:30 +0300

    bump avago 902377d447ba (#732)

--- graft/coreth/scripts/versions.sh
+++ graft/coreth/scripts/versions.sh
@@ -6,4 +6,4 @@
 set -euo pipefail
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'fade5be3051d'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'902377d447ba'}
