8fd2d7b4e7010c127fe4d2549e2194c246bc40b0
Quentin McGaw <quentin.mcgaw@gmail.com>
2025-01-06 10:17:58 +0100

    chore(all): mocks generation improved (#3628)

---
+++ vms/components/avax/mocks_generate_test.go
@@ -0,0 +1,6 @@
+// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package avax
+
+//go:generate go run go.uber.org/mock/mockgen -package=${GOPACKAGE}mock -destination=${GOPACKAGE}mock/transferable_in.go -mock_names=TransferableIn=TransferableIn . TransferableIn
