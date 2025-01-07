ff79653e5683002b199b15da177fd3b32fde2cd9
Quentin McGaw <quentin.mcgaw@gmail.com>
2025-01-07 13:30:33 +0100

    chore(scripts): simplify mocks generation (#721)
    * header.yml is missing so removing ineffective go-license
    
    * Remove unneeded source constants
    
    * Remove unneeded source versions
    
    * Use go generate commands to generate mocks
    
    * Remove the need to install mockgen in the script
    
    * Add mock-up-to-date step to test CI workflow
    
    * Remove all mocks before generating them in CI
    
    * Add generate command for non-tracked mock
    (Note using source mode was compulsory due to CGO)
    
    * Use bash shell for mock check step
    
    * Remove script mock.gen.sh
    
    * docs(contributing): add mocks section
    
    * mockgen version used is the one defined in go.mod
    - requires a `tools.go` blank importing golang.org/x/tools/imports in order to have the `golang.org/x/tools` dependency satisfied for mockgen v0.4
    - less future deltas when upgrading mockgen
    
    * ci: use more precise go generate regex
    
    * Add `git add --intent-to-add --all` to detect untracked files
    
    * Add copyright notices

---
+++ graft/coreth/precompile/contract/mocks_generate_test.go
@@ -0,0 +1,6 @@
+// Copyright (C) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package contract
+
+//go:generate go run go.uber.org/mock/mockgen -package=$GOPACKAGE -destination=mocks.go . BlockContext,AccessibleState,StateDB
