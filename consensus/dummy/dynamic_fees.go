fca99effa9abd0057c40fccb5b5f4e31d86b9737
Stephen Buttolph <stephen@avalabs.org>
2025-02-13 12:50:19 -0500

    Remove `header.Extra` from `EstimateNextBaseFee` (#793)

--- consensus/dummy/dynamic_fees.go
+++ consensus/dummy/dynamic_fees.go
@@ -169,11 +169,12 @@ func CalcBaseFee(config *params.ChainConfig, parent *types.Header, timestamp uin
 // If [timestamp] is less than the timestamp of [parent], then it uses the same timestamp as parent.
 // Warning: This function should only be used in estimation and should not be used when calculating the canonical
 // base fee for a subsequent block.
-func EstimateNextBaseFee(config *params.ChainConfig, parent *types.Header, timestamp uint64) ([]byte, *big.Int, error) {
+func EstimateNextBaseFee(config *params.ChainConfig, parent *types.Header, timestamp uint64) (*big.Int, error) {
 	if timestamp < parent.Time {
 		timestamp = parent.Time
 	}
-	return CalcBaseFee(config, parent, timestamp)
+	_, baseFee, err := CalcBaseFee(config, parent, timestamp)
+	return baseFee, err
 }
 
 // selectBigWithinBounds returns [value] if it is within the bounds:
