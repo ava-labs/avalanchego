0e7a1ac07031a722e9f5d0b8ee9b2431fe7bb883
Stephen Buttolph <stephen@avalabs.org>
2025-02-18 18:41:22 -0500

    Move AP4 params (#809)

--- graft/coreth/plugin/evm/ap4/cost.go
+++ graft/coreth/plugin/evm/ap4/cost.go
@@ -9,25 +9,47 @@ import (
 	"math"
 
 	safemath "github.com/ava-labs/avalanchego/utils/math"
+	"github.com/ava-labs/coreth/params"
 )
 
 const (
+	// MinBlockGasCost is the minimum block gas cost.
 	MinBlockGasCost = 0
+	// MaxBlockGasCost is the maximum block gas cost. If the block gas cost
+	// would exceed this value, the block gas cost is set to this value.
 	MaxBlockGasCost = 1_000_000
-	TargetBlockRate = 2 // in seconds
+	// TargetBlockRate is the target amount of time in seconds between blocks.
+	// If blocks are produced faster than this rate, the block gas cost is
+	// increased. If blocks are produced slower than this rate, the block gas
+	// cost is decreased.
+	TargetBlockRate = 2
 
 	// BlockGasCostStep is the rate at which the block gas cost changes per
 	// second.
 	//
 	// This value was modified by the Apricot Phase 5 upgrade.
 	BlockGasCostStep = 50_000
+
+	// MinBaseFee is the minimum base fee that is allowed after Apricot Phase 3
+	// upgrade.
+	//
+	// This value modifies the previously used `ap3.MinBaseFee`.
+	//
+	// This value was modified in Etna.
+	MinBaseFee = 25 * params.GWei
+
+	// MaxBaseFee is the maximum base fee that is allowed after Apricot Phase 3
+	// upgrade.
+	//
+	// This value modifies the previously used `ap3.MaxBaseFee`.
+	MaxBaseFee = 1_000 * params.GWei
 )
 
 // BlockGasCost calculates the required block gas cost.
 //
-// cost = parentCost + step * (TargetBlockRate - timeElapsed)
+// cost = parentCost + step * ([TargetBlockRate] - timeElapsed)
 //
-// The returned cost is clamped to [MinBlockGasCost, MaxBlockGasCost].
+// The returned cost is clamped to [[MinBlockGasCost], [MaxBlockGasCost]].
 func BlockGasCost(
 	parentCost uint64,
 	step uint64,
