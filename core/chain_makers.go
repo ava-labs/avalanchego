2dd2fb5ae4dc958cc60f4ec035fb6c7056fc0ab5
Stephen Buttolph <stephen@avalabs.org>
2025-02-21 13:23:06 -0500

    Move `header.ExtraPrefix` calculation (#825)

--- core/chain_makers.go
+++ core/chain_makers.go
@@ -384,10 +384,6 @@ func (cm *chainMaker) makeHeader(parent *types.Block, gap uint64, state *state.S
 		gasLimit = CalcGasLimit(parent.GasUsed(), parent.GasLimit(), parent.GasLimit(), parent.GasLimit())
 	}
 
-	extra, err := header.ExtraPrefix(cm.config, parent.Header(), time)
-	if err != nil {
-		panic(err)
-	}
 	baseFee, err := header.BaseFee(cm.config, parent.Header(), time)
 	if err != nil {
 		panic(err)
@@ -401,7 +397,6 @@ func (cm *chainMaker) makeHeader(parent *types.Block, gap uint64, state *state.S
 		GasLimit:   gasLimit,
 		Number:     new(big.Int).Add(parent.Number(), common.Big1),
 		Time:       time,
-		Extra:      extra,
 		BaseFee:    baseFee,
 	}
 
