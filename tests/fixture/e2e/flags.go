7c94a7af06d1493ca1aeded42a4197980b60d979
Darioush Jalali <darioush.jalali@avalabs.org>
2025-02-24 13:53:52 -0800

    all changes from tmpnet-check-metrics-collection

--- tests/fixture/e2e/flags.go
+++ tests/fixture/e2e/flags.go
@@ -10,23 +10,21 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/spf13/cast"
+
 	"github.com/ava-labs/avalanchego/tests/fixture/tmpnet"
 )
 
-// Ensure that this value takes into account the scrape_interval
-// defined in scripts/run_prometheus.sh.
-const networkShutdownDelay = 12 * time.Second
-
 type FlagVars struct {
-	avalancheGoExecPath  string
-	pluginDir            string
-	networkDir           string
-	reuseNetwork         bool
-	delayNetworkShutdown bool
-	startNetwork         bool
-	stopNetwork          bool
-	restartNetwork       bool
-	nodeCount            int
+	avalancheGoExecPath string
+	pluginDir           string
+	networkDir          string
+	reuseNetwork        bool
+	startCollectors     bool
+	startNetwork        bool
+	stopNetwork         bool
+	restartNetwork      bool
+	nodeCount           int
 }
 
 func (v *FlagVars) AvalancheGoExecPath() (string, error) {
@@ -74,10 +72,14 @@ func (v *FlagVars) RestartNetwork() bool {
 	return v.restartNetwork
 }
 
+func (v *FlagVars) StartCollectors() bool {
+	return v.startCollectors
+}
+
 func (v *FlagVars) NetworkShutdownDelay() time.Duration {
-	if v.delayNetworkShutdown {
+	if v.startCollectors {
 		// Only return a non-zero value if the delay is enabled.
-		return networkShutdownDelay
+		return tmpnet.NetworkShutdownDelay
 	}
 	return 0
 }
@@ -94,14 +96,6 @@ func (v *FlagVars) NodeCount() int {
 	return v.nodeCount
 }
 
-func GetEnvWithDefault(envVar, defaultVal string) string {
-	val := os.Getenv(envVar)
-	if len(val) == 0 {
-		return defaultVal
-	}
-	return val
-}
-
 func RegisterFlags() *FlagVars {
 	vars := FlagVars{}
 	flag.StringVar(
@@ -116,7 +110,7 @@ func RegisterFlags() *FlagVars {
 	flag.StringVar(
 		&vars.pluginDir,
 		"plugin-dir",
-		GetEnvWithDefault(tmpnet.AvalancheGoPluginDirEnvName, os.ExpandEnv("$HOME/.avalanchego/plugins")),
+		tmpnet.GetEnvWithDefault(tmpnet.AvalancheGoPluginDirEnvName, os.ExpandEnv("$HOME/.avalanchego/plugins")),
 		fmt.Sprintf(
 			"[optional] the dir containing VM plugins. Also possible to configure via the %s env variable.",
 			tmpnet.AvalancheGoPluginDirEnvName,
@@ -140,12 +134,7 @@ func RegisterFlags() *FlagVars {
 		false,
 		"[optional] restart an existing network previously started with --reuse-network. Useful for ensuring a network is running with the current state of binaries on disk. Ignored if a network is not already running or --stop-network is provided.",
 	)
-	flag.BoolVar(
-		&vars.delayNetworkShutdown,
-		"delay-network-shutdown",
-		false,
-		"[optional] whether to delay network shutdown to allow a final metrics scrape.",
-	)
+	SetStartCollectorsFlag(&vars.startCollectors)
 	flag.BoolVar(
 		&vars.startNetwork,
 		"start-network",
@@ -166,4 +155,14 @@ func RegisterFlags() *FlagVars {
 	)
 
 	return &vars
+}
+
+// Enable reuse by the upgrade job
+func SetStartCollectorsFlag(p *bool) {
+	flag.BoolVar(
+		p,
+		"start-collectors",
+		cast.ToBool(tmpnet.GetEnvWithDefault("TMPNET_START_COLLECTORS", "false")),
+		"[optional] whether to start collectors of logs and metrics from nodes of the temporary network.",
+	)
 }
