2e9515d78f43da89fa991004525b9549032e1578
aaronbuchwald <aaron.buchwald56@gmail.com>
2025-03-04 08:25:16 -0500

    Update scripts/lint.sh
    Co-authored-by: Razvan <36448915+rrazvan1@users.noreply.github.com>
    Signed-off-by: aaronbuchwald <aaron.buchwald56@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -43,7 +43,7 @@ _addlicense_flags=${ADDLICENSE_FLAGS:-"--verify --debug"}
 function test_license_header {
   go install -v github.com/palantir/go-license@v1.25.0
   local files=()
-  while IFS= read -r line; do files+=("$line"); done < <(find . -type f -name '*.go' ! -name '*.pb.go' ! -name 'mock_*.go' ! -name 'mocks_*.go' ! -path './**/*mock/*.go' ! -path '*.canoto.go')
+  while IFS= read -r line; do files+=("$line"); done < <(find . -type f -name '*.go' ! -name '*.pb.go' ! -name 'mock_*.go' ! -name 'mocks_*.go' ! -path './**/*mock/*.go' ! -name '*.canoto.go')
 
   # shellcheck disable=SC2086
   go-license \
