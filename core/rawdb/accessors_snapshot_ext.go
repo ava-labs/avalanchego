2768dd5929b97a1cbcb42b4d4b198da1c02f861c
Quentin McGaw <quentin.mcgaw@avalabs.org>
2025-03-05 17:37:31 +0100

    chore(core/rawdb): use libevm core/rawdb
    - Move our own code to `_ext.go` files
    - Import symbols from libevm in imports.go
    
    See original PR https://github.com/ava-labs/coreth/pull/772
    
    Co-authored-by: Arran Schlosberg <519948+ARR4N@users.noreply.github.com>

---
+++ core/rawdb/accessors_snapshot_ext.go
@@ -0,0 +1,45 @@
+// (c) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package rawdb
+
+import (
+	"github.com/ava-labs/libevm/common"
+	"github.com/ava-labs/libevm/ethdb"
+	"github.com/ava-labs/libevm/log"
+)
+
+// ReadSnapshotBlockHash retrieves the hash of the block whose state is contained in
+// the persisted snapshot.
+func ReadSnapshotBlockHash(db ethdb.KeyValueReader) common.Hash {
+	data, _ := db.Get(snapshotBlockHashKey)
+	if len(data) != common.HashLength {
+		return common.Hash{}
+	}
+	return common.BytesToHash(data)
+}
+
+// WriteSnapshotBlockHash stores the root of the block whose state is contained in
+// the persisted snapshot.
+func WriteSnapshotBlockHash(db ethdb.KeyValueWriter, blockHash common.Hash) {
+	if err := db.Put(snapshotBlockHashKey, blockHash[:]); err != nil {
+		log.Crit("Failed to store snapshot block hash", "err", err)
+	}
+}
+
+// DeleteSnapshotBlockHash deletes the hash of the block whose state is contained in
+// the persisted snapshot. Since snapshots are not immutable, this  method can
+// be used during updates, so a crash or failure will mark the entire snapshot
+// invalid.
+func DeleteSnapshotBlockHash(db ethdb.KeyValueWriter) {
+	if err := db.Delete(snapshotBlockHashKey); err != nil {
+		log.Crit("Failed to remove snapshot block hash", "err", err)
+	}
+}
+
+// IterateAccountSnapshots returns an iterator for walking all of the accounts in the snapshot
+func IterateAccountSnapshots(db ethdb.Iteratee) ethdb.Iterator {
+	it := db.NewIterator(SnapshotAccountPrefix, nil)
+	keyLen := len(SnapshotAccountPrefix) + common.HashLength
+	return NewKeyLengthIterator(it, keyLen)
+}
