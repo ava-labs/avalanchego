200f5b6f74faf85031dd748eb3e62ba592a98128
Aaron Buchwald <aaron.buchwald56@gmail.com>
2025-03-06 16:34:39 -0500

    gofumpt tools.go

--- tools.go
+++ tools.go
@@ -5,5 +5,5 @@ package avalanchego
 
 import (
 	_ "github.com/StephenButtolph/canoto/generate"
-	_ "golang.org/x/mod/semver"                    // golang.org/x/mod to satisfy requirement for go.uber.org/mock/mockgen@v0.5
+	_ "golang.org/x/mod/semver" // golang.org/x/mod to satisfy requirement for go.uber.org/mock/mockgen@v0.5
 )
