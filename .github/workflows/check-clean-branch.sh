cb1eda9ac4b584b80e567e4cff45d598c33b7520
Stephen Buttolph <stephen@avalabs.org>
2025-03-13 15:02:01 -0400

    nit

--- .github/workflows/check-clean-branch.sh
+++ .github/workflows/check-clean-branch.sh
@@ -6,7 +6,7 @@ git add --all
 git update-index --really-refresh >> /dev/null
 
 # Show the status of the working tree.
-git status
+git status --short
 
 # Exits if any uncommitted changes are found.
 git diff-index --quiet HEAD
