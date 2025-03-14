f5364a12931cde4d8fd3ebf6504440c491a48ee5
Arran Schlosberg <519948+ARR4N@users.noreply.github.com>
2025-03-14 16:48:57 +0000

    chore: disallow direct `libevm/params` import
    Until a complete transition to a `libevm` package, it shouldn't be allowed to be imported directly.
    
    Remove the package from the allowed list and move a `types` test into `package types_test` to avoid a circular dependency.
    
    See original PR https://github.com/ava-labs/coreth/pull/874

--- core/types/block_test.go
+++ core/types/block_test.go
@@ -24,7 +24,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package types
+package types_test
 
 import (
 	"bytes"
@@ -35,9 +35,11 @@ import (
 	"github.com/ava-labs/libevm/common"
 	"github.com/ava-labs/libevm/common/math"
 	"github.com/ava-labs/libevm/crypto"
-	"github.com/ava-labs/libevm/params"
 	"github.com/ava-labs/libevm/rlp"
 	"github.com/ava-labs/subnet-evm/internal/blocktest"
+	"github.com/ava-labs/subnet-evm/params"
+
+	. "github.com/ava-labs/subnet-evm/core/types"
 )
 
 func TestBlockEncoding(t *testing.T) {
