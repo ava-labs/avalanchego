67bc5966cc62d7cf03e2a90f84416b9ce1400279
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-03-20 12:10:32 +0100

    wallet/subnet/primary: define local `ethClient` to drop dependency on coreth/ethclient

---
+++ wallet/subnet/primary/ethclient.go
@@ -0,0 +1,71 @@
+// Copyright (C) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package primary
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+
+	"github.com/ava-labs/libevm/common"
+	"github.com/ava-labs/libevm/common/hexutil"
+	"github.com/ava-labs/libevm/rpc"
+)
+
+type ethClient struct {
+	c *rpc.Client
+}
+
+func newEthClient(ctx context.Context, rawurl string) (*ethClient, error) {
+	c, err := rpc.DialContext(ctx, rawurl)
+	if err != nil {
+		return nil, fmt.Errorf("dialing %s: %w", rawurl, err)
+	}
+	return &ethClient{
+		c: c,
+	}, nil
+}
+
+// BalanceAt returns the wei balance of the given account.
+// The block number can be nil, in which case the balance is taken from the latest known block.
+func (ec *ethClient) BalanceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (*big.Int, error) {
+	var result hexutil.Big
+	err := ec.c.CallContext(ctx, &result, "eth_getBalance", account, toBlockNumArg(blockNumber))
+	return (*big.Int)(&result), err
+}
+
+// NonceAt returns the account nonce of the given account.
+// The block number can be nil, in which case the nonce is taken from the latest known block.
+func (ec *ethClient) NonceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (uint64, error) {
+	var result hexutil.Uint64
+	err := ec.c.CallContext(ctx, &result, "eth_getTransactionCount", account, toBlockNumArg(blockNumber))
+	return uint64(result), err
+}
+
+// EstimateBaseFee tries to estimate the base fee for the next block if it were created
+// immediately. There is no guarantee that this will be the base fee used in the next block
+// or that the next base fee will be higher or lower than the returned value.
+func (ec *ethClient) EstimateBaseFee(ctx context.Context) (*big.Int, error) {
+	var hex hexutil.Big
+	err := ec.c.CallContext(ctx, &hex, "eth_baseFee")
+	if err != nil {
+		return nil, err
+	}
+	return (*big.Int)(&hex), nil
+}
+
+func toBlockNumArg(number *big.Int) string {
+	if number == nil {
+		return "latest"
+	}
+	if number.Sign() >= 0 {
+		return hexutil.EncodeBig(number)
+	}
+	// It's negative.
+	if number.IsInt64() {
+		return rpc.BlockNumber(number.Int64()).String()
+	}
+	// It's negative and large, which is invalid.
+	return fmt.Sprintf("<invalid %d>", number)
+}
