9646fddeb4663be935d6b6261d8d2f8808f3ec7a
Quentin McGaw <quentin.mcgaw@avalabs.org>
2025-03-26 09:06:12 +0100

    chore(deps): use libevm avalanchego based on `v1.12.3-name-fortuna` (#887)
    - Previous avalanchego was based on top of v1.12.3, although master branch is based off v1.12.3-name-fortuna
    - Current avalanchego is now based on top of v1.12.3-name-fortuna.0
    - Fix versions.sh to detect avalanchego commit hashes for names with more dashes

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -18,7 +18,7 @@ if [[ -z ${AVALANCHE_VERSION:-} ]]; then
   # If not, the value is assumed to represent a tag
   if [[ "${AVALANCHE_VERSION}" =~ ^v.*[0-9]{14}-[0-9a-f]{12}$ ]]; then
     # Extract module hash from version
-    MODULE_HASH="$(echo "${AVALANCHE_VERSION}" | cut -d'-' -f3)"
+    MODULE_HASH="$(echo "${AVALANCHE_VERSION}" | grep -Eo '[0-9a-f]{12}$')"
 
     # The first 8 chars of the hash is used as the tag of avalanchego images
     AVALANCHE_VERSION="${MODULE_HASH::8}"
