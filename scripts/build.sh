dc5f943cd0f8e009a8a74bf6d9ffd003a21d569a
Maru Newby <maru.newby@avalabs.org>
2025-04-06 09:02:36 -0700

    [tooling] Simplify avalanchego build script
    Previously the `scripts/build.sh` script delegated responsibility for
    actually building avalanchego to
    `scripts/build_avalanche.sh`. This change incorporates
    build_avalanchego.sh into build.sh to simplify maintenance.

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,31 +16,25 @@ print_usage() {
 race=''
 while getopts 'r' flag; do
   case "${flag}" in
-    r) race='-r' ;;
+    r)
+      echo "Building with race detection enabled"
+      race='-race'
+      ;;
     *) print_usage
       exit 1 ;;
   esac
 done
 
-# Avalanchego root folder
 AVALANCHE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
-# Load the constants
+# Configure the build environment
 source "$AVALANCHE_PATH"/scripts/constants.sh
+# Determine the git commit hash to use for the build
+source "$AVALANCHE_PATH"/scripts/git_commit.sh
 
-# Download dependencies
 echo "Downloading dependencies..."
 go mod download
 
-build_args="$race"
-
-# Build avalanchego
-"$AVALANCHE_PATH"/scripts/build_avalanche.sh $build_args
-
-# Exit build successfully if the AvalancheGo binary is created successfully
-if [[ -f "$avalanchego_path" ]]; then
-        echo "Build Successful"
-        exit 0
-else
-        echo "Build failure" >&2
-        exit 1
-fi
+echo "Building AvalancheGo with [$(go version)]..."
+go build $race -o "$avalanchego_path" \
+   -ldflags "-X github.com/ava-labs/avalanchego/version.GitCommit=$git_commit $static_ld_flags" \
+   "$AVALANCHE_PATH/main/"*.go
