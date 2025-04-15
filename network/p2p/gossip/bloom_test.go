8b2e2e7916e06fa0c158efc2bc653d13d50dc5cf
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2025-04-15 13:23:10 -0400

    restore original test files.

---
+++ network/p2p/gossip/bloom_test.go
@@ -0,0 +1,108 @@
+// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package gossip
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ava-labs/avalanchego/ids"
+)
+
+func TestBloomFilterRefresh(t *testing.T) {
+	tests := []struct {
+		name                           string
+		minTargetElements              int
+		targetFalsePositiveProbability float64
+		resetFalsePositiveProbability  float64
+		resetCount                     uint64
+		add                            []*testTx
+		expected                       []*testTx
+	}{
+		{
+			name:                           "no refresh",
+			minTargetElements:              1,
+			targetFalsePositiveProbability: 0.01,
+			resetFalsePositiveProbability:  1,
+			resetCount:                     0, // maxCount = 9223372036854775807
+			add: []*testTx{
+				{id: ids.ID{0}},
+				{id: ids.ID{1}},
+				{id: ids.ID{2}},
+			},
+			expected: []*testTx{
+				{id: ids.ID{0}},
+				{id: ids.ID{1}},
+				{id: ids.ID{2}},
+			},
+		},
+		{
+			name:                           "refresh",
+			minTargetElements:              1,
+			targetFalsePositiveProbability: 0.01,
+			resetFalsePositiveProbability:  0.0000000000000001, // maxCount = 1
+			resetCount:                     1,
+			add: []*testTx{
+				{id: ids.ID{0}},
+				{id: ids.ID{1}},
+				{id: ids.ID{2}},
+			},
+			expected: []*testTx{
+				{id: ids.ID{2}},
+			},
+		},
+		{
+			name:                           "multiple refresh",
+			minTargetElements:              1,
+			targetFalsePositiveProbability: 0.01,
+			resetFalsePositiveProbability:  0.0000000000000001, // maxCount = 1
+			resetCount:                     2,
+			add: []*testTx{
+				{id: ids.ID{0}},
+				{id: ids.ID{1}},
+				{id: ids.ID{2}},
+				{id: ids.ID{3}},
+				{id: ids.ID{4}},
+			},
+			expected: []*testTx{
+				{id: ids.ID{4}},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+			bloom, err := NewBloomFilter(prometheus.NewRegistry(), "", tt.minTargetElements, tt.targetFalsePositiveProbability, tt.resetFalsePositiveProbability)
+			require.NoError(err)
+
+			var resetCount uint64
+			for _, item := range tt.add {
+				bloomBytes, saltBytes := bloom.Marshal()
+				initialBloomBytes := slices.Clone(bloomBytes)
+				initialSaltBytes := slices.Clone(saltBytes)
+
+				reset, err := ResetBloomFilterIfNeeded(bloom, len(tt.add))
+				require.NoError(err)
+				if reset {
+					resetCount++
+				}
+				bloom.Add(item)
+
+				require.Equal(initialBloomBytes, bloomBytes)
+				require.Equal(initialSaltBytes, saltBytes)
+			}
+
+			require.Equal(tt.resetCount, resetCount)
+			require.Equal(float64(tt.resetCount+1), testutil.ToFloat64(bloom.metrics.ResetCount))
+			for _, expected := range tt.expected {
+				require.True(bloom.Has(expected))
+			}
+		})
+	}
+}
---
+++ utils/sampler/uniform_test.go
@@ -0,0 +1,100 @@
+// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package sampler
+
+import (
+	"math"
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUniformInitializeMaxUint64(t *testing.T) {
+	s := NewUniform()
+	s.Initialize(math.MaxUint64)
+
+	for {
+		val, hasNext := s.Next()
+		require.True(t, hasNext)
+
+		if val > math.MaxInt64 {
+			break
+		}
+	}
+}
+
+func TestUniformOutOfRange(t *testing.T) {
+	s := NewUniform()
+	s.Initialize(0)
+
+	_, ok := s.Sample(1)
+	require.False(t, ok)
+}
+
+func TestUniformEmpty(t *testing.T) {
+	require := require.New(t)
+	s := NewUniform()
+
+	s.Initialize(1)
+
+	val, ok := s.Sample(0)
+	require.True(ok)
+	require.Empty(val)
+}
+
+func TestUniformSingleton(t *testing.T) {
+	require := require.New(t)
+	s := NewUniform()
+
+	s.Initialize(1)
+
+	val, ok := s.Sample(1)
+	require.True(ok)
+	require.Equal([]uint64{0}, val)
+}
+
+func TestUniformDistribution(t *testing.T) {
+	require := require.New(t)
+	s := NewUniform()
+
+	s.Initialize(3)
+
+	val, ok := s.Sample(3)
+	require.True(ok)
+
+	slices.Sort(val)
+	require.Equal([]uint64{0, 1, 2}, val)
+}
+
+func TestUniformOverSample(t *testing.T) {
+	s := NewUniform()
+	s.Initialize(3)
+
+	_, ok := s.Sample(4)
+	require.False(t, ok)
+}
+
+func TestUniformLazilySample(t *testing.T) {
+	require := require.New(t)
+	s := NewUniform()
+
+	s.Initialize(3)
+
+	for j := 0; j < 2; j++ {
+		sampled := map[uint64]bool{}
+		for i := 0; i < 3; i++ {
+			val, hasNext := s.Next()
+			require.True(hasNext)
+			require.False(sampled[val])
+
+			sampled[val] = true
+		}
+
+		_, hasNext := s.Next()
+		require.False(hasNext)
+
+		s.Reset()
+	}
+}
