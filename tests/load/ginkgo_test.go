f6e300c61f4c941a1667fdb617f6d6bcb7c43dab
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-04-28 12:19:24 +0200

    Ginkgo setup

---
+++ tests/load/ginkgo_test.go
@@ -0,0 +1,126 @@
+// Copyright (C) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package load
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"testing"
+
+	"github.com/ava-labs/libevm/log"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ava-labs/avalanchego/tests/fixture/e2e"
+	"github.com/ava-labs/avalanchego/tests/fixture/tmpnet"
+)
+
+// Run this using the command:
+// ./bin/ginkgo -v ./tests/load -- --avalanchego-path=$PWD/build/avalanchego
+func TestLoad(t *testing.T) {
+	ginkgo.RunSpecs(t, "load tests")
+}
+
+var flagVars *e2e.FlagVars
+
+func init() {
+	flagVars = e2e.RegisterFlagsWithDefaultOwner("avalanchego-load")
+}
+
+var _ = ginkgo.SynchronizedBeforeSuite(func() []byte {
+	// Run only once in the first ginkgo process
+
+	tc := e2e.NewTestContext()
+	nodes := tmpnet.NewNodesOrPanic(tmpnet.DefaultNodeCount)
+	network := &tmpnet.Network{
+		Owner: "avalanchego-load-test",
+		Nodes: nodes,
+	}
+
+	env := e2e.NewTestEnvironment(
+		tc,
+		flagVars,
+		network,
+	)
+
+	return env.Marshal()
+}, func(envBytes []byte) {
+	// Run in every ginkgo process
+
+	// Initialize the local test environment from the global state
+	e2e.InitSharedTestEnvironment(e2e.NewTestContext(), envBytes)
+})
+
+var _ = ginkgo.Describe("[Load Simulator]", ginkgo.Ordered, func() {
+	var privateNetwork *tmpnet.Network
+
+	ginkgo.BeforeAll(func() {
+		tc := e2e.NewTestContext()
+		env := e2e.GetEnv(tc)
+		privateNetwork = tmpnet.NewDefaultNetwork("avalanchego-load-test")
+		privateNetwork.DefaultFlags = tmpnet.FlagsMap{}
+		publicNetwork := env.GetNetwork()
+		privateNetwork.DefaultFlags.SetDefaults(publicNetwork.DefaultFlags)
+		privateNetwork.Nodes = make([]*tmpnet.Node, tmpnet.DefaultNodeCount)
+		for i := range privateNetwork.Nodes {
+			node := tmpnet.NewNode()
+			err := node.EnsureKeys()
+			require.NoError(tc, err, "ensuring keys for node %s", node.NodeID)
+			privateNetwork.Nodes[i] = node
+		}
+		env.StartPrivateNetwork(privateNetwork)
+	})
+
+	ginkgo.It("C-Chain", func() {
+		nodes := privateNetwork.Nodes
+		preFundedKey := privateNetwork.PreFundedKeys[0].ToECDSA()
+		const blockchainID = "C"
+		endpoints, err := tmpnet.GetNodeWebsocketURIs(nodes, blockchainID)
+		require.NoError(ginkgo.GinkgoT(), err, "getting node websocket URIs")
+		config := Config{
+			Endpoints:   endpoints,
+			MaxFeeCap:   50,
+			MaxTipCap:   1,
+			Agents:      1,
+			TxsPerAgent: 100,
+		}
+		err = run(preFundedKey, config)
+		if err != nil {
+			log.Error(err.Error())
+			os.Exit(1)
+		}
+	})
+})
+
+func run(preFundedKey *ecdsa.PrivateKey, config Config) error {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+
+	logger := log.NewLogger(log.NewTerminalHandlerWithLevel(os.Stderr, log.LevelInfo, true))
+	log.SetDefault(logger)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	runError := make(chan error)
+	go func() {
+		runError <- Execute(ctx, preFundedKey, config)
+	}()
+
+	select {
+	case signal := <-signalCh:
+		cancel()
+		<-runError
+		return fmt.Errorf("received OS signal %s", signal)
+	case err := <-runError:
+		if err != nil {
+			return fmt.Errorf("execution failed: %w", err)
+		}
+		return nil
+	}
+}
