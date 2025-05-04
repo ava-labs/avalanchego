533decf6ece61dfbae5e752b07271dd47f559f6d
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-05-04 19:29:38 +0300

    update imports test

--- tests/fixture/imports_test.go
+++ tests/fixture/imports_test.go
@@ -61,7 +61,7 @@ func TestMustNotImport(t *testing.T) {
 		// Importing these packages configures libevm globally and it is not
 		// possible to do so for both coreth and subnet-evm, where the client may
 		// wish to connect to multiple chains.
-		"tests/fixture/...": {"core", "core/vm", "params"},
+		"tests/fixture/...": {"plugin/evm/customtypes", "params"},
 	}
 
 	for packageName, forbiddenImports := range mustNotImport {
