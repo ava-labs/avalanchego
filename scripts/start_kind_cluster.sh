dea4e91c65aecb35b705af64484b25bebf1a3dc6
Maru Newby <maru.newby@avalabs.org>
2025-05-08 01:04:02 +0000

    fixup: Respond to review feedback

--- scripts/start_kind_cluster.sh
+++ scripts/start_kind_cluster.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 # Enable reuse of the arguments to ginkgo relevant to starting a cluster
 START_CLUSTER_ARGS=()
 for arg in "$@"; do
-  if [[ "${arg}" =~ "--kubeconfig" || "${arg}" =~ "--kubeconfig-context" ]]; then
+  if [[ "${arg}" =~ "--kubeconfig=" || "${arg}" =~ "--kubeconfig-context=" ]]; then
     START_CLUSTER_ARGS+=("${arg}")
   fi
 done
