5aeb7763ca63fe4254fbdf1423b675ae9ba8823c
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-05-21 14:57:50 +0200

    Import libevm instead of coreth for params package

--- tests/load/c/issuers/simple.go
+++ tests/load/c/issuers/simple.go
@@ -10,9 +10,9 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ava-labs/coreth/params"
 	"github.com/ava-labs/libevm/common"
 	"github.com/ava-labs/libevm/core/types"
+	"github.com/ava-labs/libevm/params"
 
 	ethcrypto "github.com/ava-labs/libevm/crypto"
 )
