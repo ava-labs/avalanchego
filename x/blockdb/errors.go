cf3547325e1afb6be6bef3af464703cb623bf18c
Draco <draco@dracoli.com>
2025-06-09 14:41:19 -0400

    refactor: rename store to database

--- x/blockdb/errors.go
+++ x/blockdb/errors.go
@@ -8,7 +8,7 @@ var (
 	ErrBlockEmpty                = fmt.Errorf("blockdb: block is empty")
 	ErrBlockSizeMismatch         = fmt.Errorf("blockdb: block size in index file does not match data header")
 	ErrChecksumMismatch          = fmt.Errorf("blockdb: checksum mismatch")
-	ErrStoreClosed               = fmt.Errorf("blockdb: store is closed")
+	ErrDatabaseClosed            = fmt.Errorf("blockdb: database is closed")
 	ErrInvalidCheckpointInterval = fmt.Errorf("blockdb: invalid checkpoint interval")
 	ErrCorrupted                 = fmt.Errorf("blockdb: unrecoverable corruption detected")
 	ErrBlockTooLarge             = fmt.Errorf("blockdb: block size exceeds maximum allowed size of %d bytes", MaxBlockDataSize)
