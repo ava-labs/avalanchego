cfbb9534664adaf5bb4761643c0eeca322ccde27
samliok <samliok@stanford.edu>
2025-06-10 15:13:57 -0500

    simplex block

---
+++ simplex/block.go
@@ -0,0 +1,96 @@
+package simplex
+
+import (
+	"context"
+	"crypto/sha256"
+	"fmt"
+
+	"sync"
+
+	"github.com/ava-labs/avalanchego/snow/engine/snowman/block"
+	"github.com/ava-labs/simplex"
+)
+
+// type Block struct {
+// 	verifiedBlock VerifiedBlock
+// }
+
+// func (b *Block) BlockHeader() simplex.BlockHeader {
+// 	return b.verifiedBlock.BlockHeader()
+// }
+
+// func (b *Block) Verify(ctx context.Context) (simplex.VerifiedBlock, error) {
+// 	// TODO: Implement the logic to verify the block.
+// 	return &b.verifiedBlock, nil
+// }
+
+type VerifiedBlock struct {
+	computeDigestOnce sync.Once
+	digest            simplex.Digest // cached, not serialized
+
+	metadata   simplex.ProtocolMetadata
+	innerBlock []byte
+}
+
+// BlockHeader returns the block header for the verified block.
+func (v *VerifiedBlock) BlockHeader() simplex.BlockHeader {
+	v.computeDigestOnce.Do(v.computeDigest)
+	return simplex.BlockHeader{
+		ProtocolMetadata: v.metadata,
+		Digest:           v.digest,
+	}
+}
+
+func VerifiedBlockFromBytes(buff []byte) (*VerifiedBlock, error) {
+	if len(buff) < simplex.ProtocolMetadataLen {
+		return nil, fmt.Errorf("buff too small, expected at least 57 bytes, got %d", len(buff))
+	}
+
+	md, err := simplex.ProtocolMetadataFromBytes(buff[:simplex.ProtocolMetadataLen])
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse metadata: %w", err)
+	}
+
+	v := &VerifiedBlock{
+		metadata: md,
+		innerBlock: buff[simplex.ProtocolMetadataLen:],
+	}
+	
+	return v, nil
+}
+
+func (v *VerifiedBlock) Bytes() []byte {
+	mdBytes := v.metadata.Bytes()
+	buff := make([]byte, len(mdBytes)+len(v.innerBlock))
+	copy(buff, mdBytes)
+	copy(buff[len(mdBytes):], v.innerBlock)
+	return buff
+}
+
+// computeDigest computes the digest of the block.
+func (v *VerifiedBlock) computeDigest() {
+	mdBytes := v.metadata.Bytes()
+	h := sha256.New()
+	h.Write(v.innerBlock)
+	h.Write(mdBytes)
+	digest := h.Sum(nil)
+	v.digest = simplex.Digest(digest[:])
+}
+
+type blockDeserializer struct {
+	vm block.ChainVM
+}
+
+func (b *blockDeserializer) DeserializeBlock(bytes []byte) (simplex.VerifiedBlock, error) {
+	vb, err := VerifiedBlockFromBytes(bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to deserialize verified block: %w", err)
+	}
+
+	_, err = b.vm.ParseBlock(context.Background(), vb.innerBlock)
+	if err != nil {
+		return nil, err
+	}
+
+	return vb, nil
+}
