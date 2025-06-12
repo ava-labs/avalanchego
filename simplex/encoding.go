7355fe4234c3563850b56585c6e1d3cee35a3146
samliok <samliok@stanford.edu>
2025-06-12 14:10:42 -0500

    canoto

--- simplex/encoding.go
+++ simplex/encoding.go
@@ -2,10 +2,10 @@
 
 package simplex
 
-// // CanotoVerifiedBlock is the Canoto representation of a verified block
-// type CanotoVerifiedBlock struct {
-// 	Metadata   []byte  `canoto:"bytes,1"`
-// 	InnerBlock []byte  `canoto:"bytes,2"`
+// CanotoVerifiedBlock is the Canoto representation of a verified block
+type CanotoVerifiedBlock struct {
+	Metadata   []byte  `canoto:"bytes,1"`
+	InnerBlock []byte  `canoto:"bytes,2"`
 
-// 	canotoData canotoData_CanotoVerifiedBlock
-// }
+	canotoData canotoData_CanotoVerifiedBlock
+}
