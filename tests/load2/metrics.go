3849da104574ef39b89315b97dc8535b619e5cc4
Rodrigo Villar <rodrigo.villar@avalabs.org>
2025-06-24 14:27:17 -0400

    refactor: move metrics to separate file

---
+++ tests/load2/metrics.go
@@ -0,0 +1,72 @@
+// Copyright (C) 2019-2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package load2
+
+import (
+	"errors"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type Metrics struct {
+	txsIssuedCounter      prometheus.Counter
+	txsAcceptedCounter    prometheus.Counter
+	txIssuanceLatency     prometheus.Histogram
+	txConfirmationLatency prometheus.Histogram
+	txTotalLatency        prometheus.Histogram
+}
+
+func NewMetrics(namespace string, registry *prometheus.Registry) (*Metrics, error) {
+	m := &Metrics{
+		txsIssuedCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "txs_issued",
+			Help:      "Number of transactions issued",
+		}),
+		txsAcceptedCounter: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "txs_confirmed",
+			Help:      "Number of transactions confirmed",
+		}),
+		txIssuanceLatency: prometheus.NewHistogram(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Name:      "tx_issuance_latency",
+			Help:      "Issuance latency of transactions",
+		}),
+		txConfirmationLatency: prometheus.NewHistogram(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Name:      "tx_confirmation_latency",
+			Help:      "Confirmation latency of transactions",
+		}),
+		txTotalLatency: prometheus.NewHistogram(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Name:      "tx_total_latency",
+			Help:      "Total latency of transactions",
+		}),
+	}
+
+	if err := errors.Join(
+		registry.Register(m.txsIssuedCounter),
+		registry.Register(m.txsAcceptedCounter),
+		registry.Register(m.txIssuanceLatency),
+		registry.Register(m.txConfirmationLatency),
+		registry.Register(m.txTotalLatency),
+	); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (m *Metrics) Issue(d time.Duration) {
+	m.txsIssuedCounter.Inc()
+	m.txIssuanceLatency.Observe(float64(d.Milliseconds()))
+}
+
+func (m *Metrics) Accept(confirmationDuration time.Duration, totalDuration time.Duration) {
+	m.txsAcceptedCounter.Inc()
+	m.txTotalLatency.Observe(float64(totalDuration.Milliseconds()))
+	m.txConfirmationLatency.Observe(float64(confirmationDuration.Milliseconds()))
+}
