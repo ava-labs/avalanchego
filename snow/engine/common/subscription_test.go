9f0e78e3160c74b47600cd7b98718863bd6fc7dc
Stephen Buttolph <stephen@avalabs.org>
2025-06-27 13:45:14 -0400

    Remove unused subscription proxy

--- snow/engine/common/subscription_test.go
+++ snow/engine/common/subscription_test.go
@@ -10,8 +10,6 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/ava-labs/avalanchego/utils/logging"
 )
 
 func TestSimpleSubscriber(t *testing.T) {
@@ -60,78 +58,4 @@ func TestSimpleSubscriberClose(t *testing.T) {
 	}()
 
 	subscriber.Close()
-}
-
-func TestSubscriptionProxy(t *testing.T) {
-	for _, testCase := range []struct {
-		name          string
-		f             func(*SubscriptionProxy, context.CancelFunc, chan Message)
-		expectedEvent Message
-	}{
-		{
-			name: "Natural notification",
-			f: func(_ *SubscriptionProxy, _ context.CancelFunc, msgs chan Message) {
-				msgs <- PendingTxs
-			},
-			expectedEvent: PendingTxs,
-		},
-		{
-			name: "Inject notification after delay",
-			f: func(sp *SubscriptionProxy, _ context.CancelFunc, _ chan Message) {
-				sp.Publish(StateSyncDone)
-			},
-			expectedEvent: StateSyncDone,
-		},
-		{
-			name: "Cancel wait",
-			f: func(_ *SubscriptionProxy, cancel context.CancelFunc, _ chan Message) {
-				cancel()
-			},
-		},
-		{
-			name: "Close",
-			f: func(sp *SubscriptionProxy, _ context.CancelFunc, _ chan Message) {
-				sp.Close()
-			},
-		},
-	} {
-		t.Run(testCase.name, func(t *testing.T) {
-			msgs := make(chan Message)
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			subscription := func(ctx context.Context) (Message, error) {
-				select {
-				case msg := <-msgs:
-					return msg, nil
-				case <-ctx.Done():
-					return Message(0), nil
-				}
-			}
-
-			sp := NewSubscriptionProxy(subscription, &logging.NoLog{})
-
-			start := time.Now()
-
-			var wg sync.WaitGroup
-			wg.Add(2)
-
-			go func() {
-				defer wg.Done()
-				time.Sleep(time.Millisecond * 10)
-				testCase.f(sp, cancel, msgs)
-			}()
-
-			go func() {
-				defer wg.Done()
-				<-sp.Forward(ctx)
-				require.Greater(t, time.Since(start), time.Millisecond*10)
-			}()
-
-			msg, _ := sp.WaitForEvent(ctx)
-			require.Greater(t, time.Since(start), time.Millisecond*10)
-			require.Equal(t, testCase.expectedEvent, msg)
-			wg.Wait()
-		})
-	}
 }
