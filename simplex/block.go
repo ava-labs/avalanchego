5dc850842253f68722387020ef8cab38d4286e73
samliok <samliok@stanford.edu>
2025-07-03 17:45:19 -0400

    add verification + tracking logic

--- simplex/block.go
+++ simplex/block.go
@@ -7,19 +7,25 @@ package simplex
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"sync"
 
 	"github.com/ava-labs/simplex"
 
 	"github.com/ava-labs/avalanchego/snow/consensus/snowman"
 	"github.com/ava-labs/avalanchego/snow/engine/snowman/block"
 	"github.com/ava-labs/avalanchego/utils/hashing"
+	"github.com/ava-labs/avalanchego/vms/proposervm/tree"
 )
 
 var (
 	_ simplex.BlockDeserializer = (*blockDeserializer)(nil)
 	_ simplex.Block             = (*Block)(nil)
 	_ simplex.VerifiedBlock     = (*Block)(nil)
+
+	errDigestNotFound = errors.New("digest not found in block tracker")
+	errMismatchedPrevDigest = errors.New("prev digest does not match block parent")
 )
 
 type Block struct {
@@ -30,6 +36,8 @@ type Block struct {
 
 	// the parsed block
 	vmBlock snowman.Block
+
+	blockTracker *blockTracker
 }
 
 // CanotoSimplexBlock is the Canoto representation of a block
@@ -58,9 +66,51 @@ func (b *Block) Bytes() ([]byte, error) {
 	return cBlock.MarshalCanoto(), nil
 }
 
+// Verify calls the Verify method on the underlying vmBlock or returns an error if the block is not able to be verified.
+// We ensure `b.vmBlock.parent` references the same block as `b.metadata.prev.vmBlock` to keep the two hash chains in sync.
 func (b *Block) Verify(ctx context.Context) (simplex.VerifiedBlock, error) {
-	// TODO: track blocks that have been verified to ensure they are either rejected or accepted
-	return b, b.vmBlock.Verify(ctx)
+	if b.metadata.Seq != 0 {
+		err := b.verifyParentMatchesPrevBlock(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err := b.vmBlock.Verify(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to verify block: %w", err)
+	}
+
+	// once verified, the block tracker will eventually either accept or reject the block
+	b.blockTracker.trackBlock(b)
+
+	return b, nil
+}
+
+// verifyPrevBlock verifies that the previous block referenced in the current block's metadata
+// matches the parent of the current block's vmBlock.
+func (b *Block) verifyParentMatchesPrevBlock(ctx context.Context) error {
+	prevBlock := b.blockTracker.getBlock(b.metadata.Prev)
+
+	if prevBlock!= nil && b.vmBlock.Parent() != prevBlock.vmBlock.ID() {
+		return fmt.Errorf("%w: parentID %s, prevID %s", errMismatchedPrevDigest, b.vmBlock.Parent(), prevBlock.vmBlock.ID())
+	}
+
+	// if we do not have it in the map, it's possible for it to be the last accepted block
+	lastID, err := b.blockTracker.vm.LastAccepted(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get last accepted block: %w", err)
+	}
+
+	if lastID != b.vmBlock.Parent() {
+		return fmt.Errorf("%w: last accepted block %s, parentID %s", errDigestNotFound, lastID, b.vmBlock.Parent())
+	}
+
+	return nil
+}
+
+func computeDigest(bytes []byte) simplex.Digest {
+	return hashing.ComputeHash256Array(bytes)
 }
 
 type blockDeserializer struct {
@@ -84,11 +134,72 @@ func (d *blockDeserializer) DeserializeBlock(bytes []byte) (simplex.Block, error
 		return nil, err
 	}
 
-	digest := hashing.ComputeHash256Array(bytes)
-
 	return &Block{
 		metadata: *md,
 		vmBlock:  vmblock,
-		digest:   digest,
+		digest:   computeDigest(bytes),
 	}, nil
 }
+
+// blockTracker is used to ensure that blocks are properly rejected, if competing blocks are accepted.
+type blockTracker struct {
+	lock sync.Mutex
+
+	// tracks the simplex digests to the blocks that have been verified
+	simplexDigestsToBlock map[simplex.Digest]*Block
+
+	vm block.ChainVM
+
+	// handles block acceptance and rejection of inner blocks
+	tree tree.Tree
+}
+
+func newBlockTracker(vm block.ChainVM) *blockTracker {
+	return &blockTracker{
+		tree: 				tree.New(),
+		vm: 				vm,
+		simplexDigestsToBlock: make(map[simplex.Digest]*Block),
+	}
+}
+
+func (bt *blockTracker) getBlock(digest simplex.Digest) *Block {
+	bt.lock.Lock()
+	defer bt.lock.Unlock()
+
+	return bt.simplexDigestsToBlock[digest]
+}
+
+func (bt *blockTracker) trackBlock(b *Block) {
+	bt.lock.Lock()
+	defer bt.lock.Unlock()
+
+	// add the block to the block tracker
+	bt.simplexDigestsToBlock[b.digest] = b
+	bt.tree.Add(b.vmBlock)
+}
+
+// indexBlock indexes the block in the block tracker and sets up the onIndex callback.
+func (bt *blockTracker) indexBlock(ctx context.Context, digest simplex.Digest) error {
+	// check if we have already verified this block
+	bt.lock.Lock()
+	defer bt.lock.Unlock()
+
+	bd, exists := bt.simplexDigestsToBlock[digest]
+	if !exists {
+		return fmt.Errorf("%w: %s", errDigestNotFound, digest)
+	}
+
+	// delete all digests from the map with a lower seq as this block's seq
+	// we keep the seq in the map so we can reference it in verify
+	for d, block := range bt.simplexDigestsToBlock {
+		if block.metadata.Seq < bd.metadata.Seq {
+			// remove the block from the map
+			delete(bt.simplexDigestsToBlock, d)
+		}
+	}
+
+	// notify the VM that we are accepting this block, and reject all competing blocks
+	// one thing the tree doesnt do is reject uncles
+	return bt.tree.Accept(ctx, bd.vmBlock)
+}
+
