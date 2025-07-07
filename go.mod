507e6bbb5e7d166749fa0b5633e26fd36b753064
yacovm <yacovm@users.noreply.github.com>
2025-07-07 22:19:33 +0200

    Replace toEngine channel with a dedicated subscription API (#3999)
    Signed-off-by: Yacov Manevich <yacov.manevich@avalabs.org>
    Co-authored-by: Stephen Buttolph <stephen@avalabs.org>

--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/DataDog/zstd v1.5.2
 	github.com/StephenButtolph/canoto v0.17.1
 	github.com/antithesishq/antithesis-sdk-go v0.3.8
-	github.com/ava-labs/coreth v0.15.2-rc.0.0.20250620163936-0058a7ec6d9d
+	github.com/ava-labs/coreth v0.15.3-0.20250707191145-055af77d543b
 	github.com/ava-labs/ledger-avalanche/go v0.0.0-20241009183145-e6f90a8a1a60
 	github.com/ava-labs/libevm v1.13.14-0.3.0.rc.1
 	github.com/btcsuite/btcd/btcutil v1.1.3
--- vms/avm/vm_test.go
+++ vms/avm/vm_test.go
@@ -38,14 +38,13 @@ func TestInvalidGenesis(t *testing.T) {
 
 	err := vm.Initialize(
 		context.Background(),
-		ctx,                          // context
-		memdb.New(),                  // database
-		nil,                          // genesisState
-		nil,                          // upgradeBytes
-		nil,                          // configBytes
-		make(chan common.Message, 1), // engineMessenger
-		nil,                          // fxs
-		nil,                          // AppSender
+		ctx,         // context
+		memdb.New(), // database
+		nil,         // genesisState
+		nil,         // upgradeBytes
+		nil,         // configBytes
+		nil,         // fxs
+		nil,         // AppSender
 	)
 	require.ErrorIs(err, codec.ErrCantUnpackVersion)
 }
@@ -64,12 +63,11 @@ func TestInvalidFx(t *testing.T) {
 	genesisBytes := newGenesisBytesTest(t)
 	err := vm.Initialize(
 		context.Background(),
-		ctx,                          // context
-		memdb.New(),                  // database
-		genesisBytes,                 // genesisState
-		nil,                          // upgradeBytes
-		nil,                          // configBytes
-		make(chan common.Message, 1), // engineMessenger
+		ctx,          // context
+		memdb.New(),  // database
+		genesisBytes, // genesisState
+		nil,          // upgradeBytes
+		nil,          // configBytes
 		[]*common.Fx{ // fxs
 			nil,
 		},
@@ -92,12 +90,11 @@ func TestFxInitializationFailure(t *testing.T) {
 	genesisBytes := newGenesisBytesTest(t)
 	err := vm.Initialize(
 		context.Background(),
-		ctx,                          // context
-		memdb.New(),                  // database
-		genesisBytes,                 // genesisState
-		nil,                          // upgradeBytes
-		nil,                          // configBytes
-		make(chan common.Message, 1), // engineMessenger
+		ctx,          // context
+		memdb.New(),  // database
+		genesisBytes, // genesisState
+		nil,          // upgradeBytes
+		nil,          // configBytes
 		[]*common.Fx{{ // fxs
 			ID: ids.Empty,
 			Fx: &FxTest{
@@ -120,7 +117,7 @@ func TestIssueTx(t *testing.T) {
 	env.vm.ctx.Lock.Unlock()
 
 	tx := newTx(t, env.genesisBytes, env.vm.ctx.ChainID, env.vm.parser, "AVAX")
-	issueAndAccept(require, env.vm, env.issuer, tx)
+	issueAndAccept(require, env.vm, tx)
 }
 
 // Test issuing a transaction that creates an NFT family
@@ -159,7 +156,7 @@ func TestIssueNFT(t *testing.T) {
 		key.Address(),
 	)
 	require.NoError(err)
-	issueAndAccept(require, env.vm, env.issuer, createAssetTx)
+	issueAndAccept(require, env.vm, createAssetTx)
 
 	// Mint the NFT
 	mintNFTTx, err := env.txBuilder.MintNFT(
@@ -173,7 +170,7 @@ func TestIssueNFT(t *testing.T) {
 		key.Address(),
 	)
 	require.NoError(err)
-	issueAndAccept(require, env.vm, env.issuer, mintNFTTx)
+	issueAndAccept(require, env.vm, mintNFTTx)
 
 	// Move the NFT
 	utxos, err := avax.GetAllUTXOs(env.vm.state, kc.Addresses())
@@ -193,7 +190,7 @@ func TestIssueNFT(t *testing.T) {
 		key.Address(),
 	)
 	require.NoError(err)
-	issueAndAccept(require, env.vm, env.issuer, transferNFTTx)
+	issueAndAccept(require, env.vm, transferNFTTx)
 }
 
 // Test issuing a transaction that creates an Property family
@@ -235,7 +232,7 @@ func TestIssueProperty(t *testing.T) {
 		key.Address(),
 	)
 	require.NoError(err)
-	issueAndAccept(require, env.vm, env.issuer, createAssetTx)
+	issueAndAccept(require, env.vm, createAssetTx)
 
 	// mint the property
 	mintPropertyOp := &txs.Operation{
@@ -264,7 +261,7 @@ func TestIssueProperty(t *testing.T) {
 		key.Address(),
 	)
 	require.NoError(err)
-	issueAndAccept(require, env.vm, env.issuer, mintPropertyTx)
+	issueAndAccept(require, env.vm, mintPropertyTx)
 
 	// burn the property
 	burnPropertyOp := &txs.Operation{
@@ -282,7 +279,7 @@ func TestIssueProperty(t *testing.T) {
 		key.Address(),
 	)
 	require.NoError(err)
-	issueAndAccept(require, env.vm, env.issuer, burnPropertyTx)
+	issueAndAccept(require, env.vm, burnPropertyTx)
 }
 
 func TestIssueTxWithFeeAsset(t *testing.T) {
@@ -296,7 +293,7 @@ func TestIssueTxWithFeeAsset(t *testing.T) {
 
 	// send first asset
 	tx := newTx(t, env.genesisBytes, env.vm.ctx.ChainID, env.vm.parser, feeAssetName)
-	issueAndAccept(require, env.vm, env.issuer, tx)
+	issueAndAccept(require, env.vm, tx)
 }
 
 func TestIssueTxWithAnotherAsset(t *testing.T) {
@@ -345,7 +342,7 @@ func TestIssueTxWithAnotherAsset(t *testing.T) {
 		key.Address(),
 	)
 	require.NoError(err)
-	issueAndAccept(require, env.vm, env.issuer, tx)
+	issueAndAccept(require, env.vm, tx)
 }
 
 func TestVMFormat(t *testing.T) {
@@ -513,7 +510,7 @@ func TestIssueImportTx(t *testing.T) {
 
 	env.vm.ctx.Lock.Unlock()
 
-	issueAndAccept(require, env.vm, env.issuer, tx)
+	issueAndAccept(require, env.vm, tx)
 
 	env.vm.ctx.Lock.Lock()
 
@@ -636,7 +633,7 @@ func TestIssueExportTx(t *testing.T) {
 
 	env.vm.ctx.Lock.Unlock()
 
-	issueAndAccept(require, env.vm, env.issuer, tx)
+	issueAndAccept(require, env.vm, tx)
 
 	env.vm.ctx.Lock.Lock()
 
@@ -699,7 +696,7 @@ func TestClearForceAcceptedExportTx(t *testing.T) {
 
 	env.vm.ctx.Lock.Unlock()
 
-	issueAndAccept(require, env.vm, env.issuer, tx)
+	issueAndAccept(require, env.vm, tx)
 
 	env.vm.ctx.Lock.Lock()
 
