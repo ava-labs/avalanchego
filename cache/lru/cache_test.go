4566e4a78e46de17b9101fda068a7d3b60ccb61d
Draco <draco@dracoli.com>
2025-07-15 11:03:30 -0400

    feat: add eviction callback in LRU cache

--- cache/lru/cache_test.go
+++ cache/lru/cache_test.go
@@ -6,6 +6,8 @@ package lru
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/ava-labs/avalanchego/cache/cachetest"
 	"github.com/ava-labs/avalanchego/ids"
 )
@@ -18,4 +20,31 @@ func TestCache(t *testing.T) {
 func TestCacheEviction(t *testing.T) {
 	c := NewCache[ids.ID, int64](2)
 	cachetest.Eviction(t, c)
+}
+
+func TestCacheFlushWithOnEvict(t *testing.T) {
+	c := NewCache[ids.ID, int64](2)
+
+	// Track which elements were evicted
+	evicted := make(map[ids.ID]int64)
+	c.SetOnEvict(func(key ids.ID, value int64) {
+		evicted[key] = value
+	})
+
+	cachetest.Eviction(t, c)
+	require.Zero(t, c.Len())
+	require.Len(t, evicted, 3)
+}
+
+func TestCachePutWithOnEvict(t *testing.T) {
+	c := NewCache[ids.ID, int64](1)
+
+	evicted := make(map[ids.ID]int64)
+	c.SetOnEvict(func(key ids.ID, value int64) {
+		evicted[key] = value
+	})
+
+	cachetest.Basic(t, c)
+	require.Len(t, evicted, 1)
+	require.Equal(t, evicted[ids.ID{1}], int64(1))
 }
