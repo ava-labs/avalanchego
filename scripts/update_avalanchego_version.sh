ab3d4656a271c8bfc5661abb28ebeb5903f402be
Austin Larson <78000745+alarso16@users.noreply.github.com>
2025-07-24 11:43:40 -0400

    ci: Set GitHub token to remove flake (#1066)

--- scripts/update_avalanchego_version.sh
+++ scripts/update_avalanchego_version.sh
@@ -24,6 +24,8 @@ CURL_ARGS=(curl -s)
 if [[ -n "${GITHUB_TOKEN:-}" ]]; then
   # Using an auth token avoids being rate limited when run in CI
   CURL_ARGS+=(-H "Authorization: token ${GITHUB_TOKEN}")
+else
+  echo "No GITHUB_TOKEN found, using unauthenticated requests"
 fi
 
 GIT_COMMIT=$("${CURL_ARGS[@]}" "https://api.github.com/repos/ava-labs/avalanchego/commits/${AVALANCHE_VERSION}")
