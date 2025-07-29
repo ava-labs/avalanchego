e953ba2c06a05bba52c5d8f149c6e8ddfb3dd92b
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2025-07-29 17:19:35 -0400

    nit
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- tests/e2e/api/api.go
+++ tests/e2e/api/api.go
@@ -5,45 +5,35 @@ package api
 
 import (
 	"net/http"
-	"time"
-
 	"connectrpc.com/connect"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ava-labs/avalanchego/api/connectclient"
+	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/tests/fixture/e2e"
-	"github.com/ava-labs/avalanchego/tests/fixture/tmpnet"
+	"github.com/ava-labs/avalanchego/utils/constants"
 
 	infopb "github.com/ava-labs/avalanchego/connectproto/pb/info"
 	connectinfopb "github.com/ava-labs/avalanchego/connectproto/pb/info/infoconnect"
-	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/utils/constants"
+	"github.com/ava-labs/avalanchego/tests/fixture/tmpnet"
 )
 
-var _ = ginkgo.Describe("[Info Service]", func() {
+var _ = ginkgo.Describe("[Info Service]", ginkgo.Ordered, func() {
+	tc := e2e.NewTestContext()
+	require := require.New(tc)
+
 	var (
-		tc      = e2e.NewTestContext()
-		env     = e2e.GetEnv(tc)
-		require = require.New(tc)
-		client  connectinfopb.InfoServiceClient
-		ctx     = tc.DefaultContext()
+		network *tmpnet.Network
+		node    *tmpnet.Node
 		nodeID ids.NodeID
+		client  connectinfopb.InfoServiceClient
 	)
 
 	ginkgo.BeforeAll(func() {
-		rootDir := env.RootNetworkDir
-		network := tmpnet.NewDefaultNetwork("info-service-test")
-		require.NoError(tmpnet.BootstrapNewNetwork(ctx, tc.Log(), network, rootDir))
-
-		var err error
-		nodeID, err = ids.NodeIDFromString(network.GetAvailableNodeIDs()[0])
-		require.NoError(err)
-		node, err := network.GetNode(nodeID)
-		require.NoError(err)
-
-		e2e.WaitForHealthy(tc, node)
-
+		network = e2e.GetEnv(tc).GetNetwork()
+		node = network.Nodes[0]
+		nodeID = node.NodeID
 		uri := node.GetAccessibleURI()
 		client = connectinfopb.NewInfoServiceClient(
 			http.DefaultClient,
@@ -54,16 +44,19 @@ var _ = ginkgo.Describe("[Info Service]", func() {
 		)
 	})
 
+
 	ginkgo.It("serves GetNodeVersion", func() {
+		tc.Log().Info("calling GetNodeVersion")
 		req := connect.NewRequest(&infopb.GetNodeVersionRequest{})
-		resp, err := client.GetNodeVersion(ctx, req)
+		resp, err := client.GetNodeVersion(tc.DefaultContext(), req)
 		require.NoError(err)
 		require.NotEmpty(resp.Msg.Version)
 	})
 
 	ginkgo.It("serves GetNodeID", func() {
+		tc.Log().Info("calling GetNodeID")
 		req := connect.NewRequest(&infopb.GetNodeIDRequest{})
-		resp, err := client.GetNodeID(ctx, req)
+		resp, err := client.GetNodeID(tc.DefaultContext(), req)
 		require.NoError(err)
 
 		gotNodeID, err := ids.NodeIDFromString(resp.Msg.NodeId)
@@ -72,60 +65,67 @@ var _ = ginkgo.Describe("[Info Service]", func() {
 	})
 
 	ginkgo.It("serves GetNetworkID", func() {
+		tc.Log().Info("calling GetNetworkID")
 		req := connect.NewRequest(&infopb.GetNetworkIDRequest{})
-		resp, err := client.GetNetworkID(ctx, req)
+		resp, err := client.GetNetworkID(tc.DefaultContext(), req)
 		require.NoError(err)
 		require.Equal(constants.LocalID, resp.Msg.NetworkId)
 	})
 
 	ginkgo.It("serves GetNetworkName", func() {
+		tc.Log().Info("calling GetNetworkName")
 		req := connect.NewRequest(&infopb.GetNetworkNameRequest{})
-		resp, err := client.GetNetworkName(ctx, req)
+		resp, err := client.GetNetworkName(tc.DefaultContext(), req)
 		require.NoError(err)
 		require.Equal("local", resp.Msg.NetworkName)
 	})
 
 	ginkgo.It("serves GetNodeIP", func() {
+		tc.Log().Info("calling GetNodeIP")
 		req := connect.NewRequest(&infopb.GetNodeIPRequest{})
-		resp, err := client.GetNodeIP(ctx, req)
+		resp, err := client.GetNodeIP(tc.DefaultContext(), req)
 		require.NoError(err)
 		require.Equal("127.0.0.1", resp.Msg.Ip)
 	})
 
-	ginkgo.It("serves GetBlockchainID", func() {
+	ginkgo.It("serves GetChainID", func() {
+		tc.Log().Info("calling GetChainID")
 		req := connect.NewRequest(&infopb.GetChainIDRequest{Alias: "X"})
-		resp, err := client.GetChainID(ctx, req)
+		resp, err := client.GetChainID(tc.DefaultContext(), req)
 		require.NoError(err)
 		// TODO
 		require.NotEmpty(resp.Msg.ChainId)
 	})
 
 	ginkgo.It("serves GetPeers", func() {
+		tc.Log().Info("calling GetPeers")
 		req := connect.NewRequest(&infopb.GetPeersRequest{})
-		resp, err := client.GetPeers(ctx, req)
+		resp, err := client.GetPeers(tc.DefaultContext(), req)
 		require.NoError(err)
 		// TODO
 		require.NotEmpty(resp.Msg.Peers)
 	})
 
 	ginkgo.It("serves GetBootstrapped", func() {
-		tc.Eventually(func() bool {
-			req := connect.NewRequest(&infopb.GetBootstrappedRequest{Chain: "X"})
-			resp, err := client.GetBootstrapped(ctx, req)
-			return err == nil && resp.Msg.Bootstrapped
-		}, 60*time.Second, 2*time.Second, "node should eventually bootstrap")
+		tc.Log().Info("calling GetBootstrapped")
+		req := connect.NewRequest(&infopb.GetBootstrappedRequest{Chain: "X"})
+		//TODO
+		_, err := client.GetBootstrapped(tc.DefaultContext(), req)
+		require.NoError(err)
 	})
 
 	ginkgo.It("serves GetUpgrades", func() {
+		tc.Log().Info("calling GetUpgrades")
 		req := connect.NewRequest(&infopb.GetUpgradesRequest{})
-		resp, err := client.GetUpgrades(ctx, req)
+		resp, err := client.GetUpgrades(tc.DefaultContext(), req)
 		require.NoError(err)
 		require.NotNil(resp.Msg)
 	})
 
 	ginkgo.It("serves GetUptime", func() {
+		tc.Log().Info("calling GetUptime")
 		req := connect.NewRequest(&infopb.GetUptimeRequest{})
-		resp, err := client.GetUptime(ctx, req)
+		resp, err := client.GetUptime(tc.DefaultContext(), req)
 		require.NoError(err)
 		require.NotNil(resp.Msg)
 	})
@@ -133,8 +133,9 @@ var _ = ginkgo.Describe("[Info Service]", func() {
 	// TODO acps test
 
 	ginkgo.It("serves GetVMs", func() {
+		tc.Log().Info("calling GetVMs")
 		req := connect.NewRequest(&infopb.GetVMsRequest{})
-		resp, err := client.GetVMs(ctx, req)
+		resp, err := client.GetVMs(tc.DefaultContext(), req)
 		require.NoError(err)
 		found := false
 		for _, v := range resp.Msg.Vms {
