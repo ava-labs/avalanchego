1f94a02e9209efe5492e8785deed3707fefded21
github-action-benchmark <github@users.noreply.github.com>
2025-07-30 09:17:34 +0000

    add Benchmark (go) benchmark result for 45df9e6926b93aacd11589a7b90a76078ae956d9

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1753867054512,
+  "repoUrl": "https://github.com/ava-labs/avalanchego",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Jonathan Oppenheimer",
+            "username": "JonathanOppenheimer",
+            "email": "jonathan.oppenheimer@avalabs.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "45df9e6926b93aacd11589a7b90a76078ae956d9",
+          "message": "chore: Update header year to 2025 (#4140)",
+          "timestamp": "2025-07-29T19:44:08Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/45df9e6926b93aacd11589a7b90a76078ae956d9"
+        },
+        "date": 1753867053438,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[101,250000]",
+            "value": 89.69,
+            "unit": "mgas/s",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
