3c3007fc08b8073776a16df056adc0778312bf1b
github-action-benchmark <github@users.noreply.github.com>
2025-08-01 09:17:03 +0000

    add Benchmark (go) benchmark result for 110807a9dec9712072ab430bfb05b7fb5835c7c8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753953421824,
+  "lastUpdate": 1754039823977,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -54,6 +54,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[101,250000]",
             "value": 85.97,
+            "unit": "mgas/s",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "rodrigo",
+            "username": "RodrigoVillar",
+            "email": "77309055+RodrigoVillar@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "110807a9dec9712072ab430bfb05b7fb5835c7c8",
+          "message": "docs: load (#4132)",
+          "timestamp": "2025-08-01T02:06:40Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/110807a9dec9712072ab430bfb05b7fb5835c7c8"
+        },
+        "date": 1754039823515,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[101,250000]",
+            "value": 89.46,
             "unit": "mgas/s",
             "extra": "1 times\n4 procs"
           }
