ebc97d0fab4d4bc82be65037ea8635c0a95ecfb7
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-08-13 14:20:38 -0400

    refactor tests

--- vms/evm/predicate/predicate_test.go
+++ vms/evm/predicate/predicate_test.go
@@ -5,13 +5,12 @@ package predicate
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestPackUnpack(t *testing.T) {
+func TestNew(t *testing.T) {
 	tests := []struct {
 		name  string
 		input []byte
@@ -42,6 +41,11 @@ func TestPackUnpack(t *testing.T) {
 			input: bytes.Repeat([]byte{0xcc}, 63),
 			want:  append(bytes.Repeat([]byte{0xcc}, 63), 0xff),
 		},
+		{
+			name:  "exactly 65 bytes",
+			input: bytes.Repeat([]byte{0xdd}, 65),
+			want:  append(bytes.Repeat([]byte{0xdd}, 65), 0xff),
+		},
 	}
 
 	for _, tt := range tests {
@@ -49,10 +53,6 @@ func TestPackUnpack(t *testing.T) {
 			require := require.New(t)
 
 			packed := New(tt.input)
-			unpacked, err := Unpack(packed)
-
-			require.NoError(err)
-			require.Equal(tt.input, unpacked)
 
 			// Verify the packed result has the correct structure
 			require.Equal(tt.want, []byte(packed)[:len(tt.want)])
@@ -61,27 +61,6 @@ func TestPackUnpack(t *testing.T) {
 			for i := len(tt.want); i < len(packed); i++ {
 				require.Equal(byte(0), packed[i])
 			}
-		})
-	}
-}
-
-func TestPackUnpackPadding(t *testing.T) {
-	require := require.New(t)
-
-	// Test that padding is correctly applied to reach multiple of 32
-	testCases := []int{0, 1, 31, 32, 33, 63, 64, 65}
-
-	for _, length := range testCases {
-		t.Run(fmt.Sprintf("length_%d", length), func(_ *testing.T) {
-			input := make([]byte, length)
-			for i := range input {
-				input[i] = byte(i + 1)
-			}
-			packed := New(input)
-			unpacked, err := Unpack(packed)
-
-			require.NoError(err)
-			require.Equal(input, unpacked)
 
 			// Verify packed length is a multiple of 32
 			require.Equal(0, len(packed)%32)
@@ -89,40 +68,79 @@ func TestPackUnpackPadding(t *testing.T) {
 	}
 }
 
-func TestUnpackInvalid(t *testing.T) {
+func TestUnpack(t *testing.T) {
 	tests := []struct {
 		name    string
 		input   Predicate
+		want    []byte
 		wantErr error
 	}{
+		// Valid test cases
 		{
 			name:    "empty input",
 			input:   Predicate{},
+			want:    nil,
 			wantErr: errEmptyPredicate,
 		},
 		{
+			name:    "single byte",
+			input:   New([]byte{0x42}),
+			want:    []byte{0x42},
+			wantErr: nil,
+		},
+		{
+			name:    "exactly 31 bytes",
+			input:   New(bytes.Repeat([]byte{0xaa}, 31)),
+			want:    bytes.Repeat([]byte{0xaa}, 31),
+			wantErr: nil,
+		},
+		{
+			name:    "exactly 32 bytes",
+			input:   New(bytes.Repeat([]byte{0xbb}, 32)),
+			want:    bytes.Repeat([]byte{0xbb}, 32),
+			wantErr: nil,
+		},
+		{
+			name:    "exactly 63 bytes",
+			input:   New(bytes.Repeat([]byte{0xcc}, 63)),
+			want:    bytes.Repeat([]byte{0xcc}, 63),
+			wantErr: nil,
+		},
+		{
+			name:    "exactly 65 bytes",
+			input:   New(bytes.Repeat([]byte{0xdd}, 65)),
+			want:    bytes.Repeat([]byte{0xdd}, 65),
+			wantErr: nil,
+		},
+		// Invalid test cases
+		{
 			name:    "all zeros",
 			input:   Predicate(bytes.Repeat([]byte{0}, 32)),
+			want:    nil,
 			wantErr: errAllZeroBytes,
 		},
 		{
 			name:    "missing delimiter",
 			input:   Predicate(bytes.Repeat([]byte{0x42}, 32)),
+			want:    nil,
 			wantErr: errWrongEndDelimiter,
 		},
 		{
 			name:    "wrong delimiter",
 			input:   Predicate(append(bytes.Repeat([]byte{0x42}, 31), 0x00)),
+			want:    nil,
 			wantErr: errWrongEndDelimiter,
 		},
 		{
 			name:    "excess padding",
 			input:   Predicate(append(append([]byte{0x42, 0xff}, bytes.Repeat([]byte{0}, 30)...), 0x01)),
+			want:    nil,
 			wantErr: errExcessPadding,
 		},
 		{
 			name:    "non-zero padding",
 			input:   Predicate(append(append([]byte{0x42, 0xff}, bytes.Repeat([]byte{0}, 29)...), 0x01, 0x00)),
+			want:    nil,
 			wantErr: errExcessPadding,
 		},
 	}
@@ -131,8 +149,15 @@ func TestUnpackInvalid(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			require := require.New(t)
 
-			_, err := Unpack(tt.input)
-			require.ErrorIs(err, tt.wantErr)
+			unpacked, err := Unpack(tt.input)
+
+			if tt.wantErr != nil {
+				require.ErrorIs(err, tt.wantErr)
+				require.Nil(unpacked)
+			} else {
+				require.NoError(err)
+				require.Equal(tt.want, unpacked)
+			}
 		})
 	}
 }
