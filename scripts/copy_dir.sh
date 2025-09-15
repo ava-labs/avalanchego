eabe3e0d57368b7d33ef2bdba633de80fa990ef9
Elvis S <elvissabanovic3@gmail.com>
2025-09-15 19:59:23 +0400

    ci: add scripts and Taskfile

---
+++ scripts/copy_dir.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Usage: ./scripts/copy_dir.sh source_directory destination_directory
+# Sources can be S3 URLs (s3://bucket/path) or a local file path
+# Assumes s5cmd has been installed and is available in the PATH.
+# s5cmd is included in the nix dev shell.
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <source_directory> <destination_directory>"
+    echo "Import from S3 Example: $0 's3://bucket1/path1' /dest/dir"
+    echo "Export to S3 Example: $0 '/local/path1' 's3://bucket2/path2'"
+    echo "Local Example: $0 '/local/path1' /dest/dir"
+    exit 1
+fi
+
+SRC="$1"
+DST="$2"
+
+# Function to copy from a single source to destination
+function copy_source() {
+    local source="$1"
+    local dest="$2"
+    
+    # Check if source starts with s3://
+    if [[ "$source" == s3://* || "$dest" == s3://* ]]; then
+        # Use s5cmd to copy from S3
+        echo "Copying from S3: $source to $dest"
+        time s5cmd cp --show-progress "$source" "$dest"
+    else
+        # Use cp for local filesystem with recursive support
+        
+        # Ensure destination directory exists
+        mkdir -p "$dest"
+
+        if [ -d "$source" ]; then
+            time cp -r "$source"/* "$dest/"
+        elif [ -f "$source" ]; then
+            time cp "$source" "$dest/"
+        else
+            echo "Warning: Source not found: $source"
+            return 1
+        fi
+    fi
+}
+
+# Function to check the destination directory does not exist to avoid
+# overwrites
+function check_dst_not_exists() {
+    local dst="$1"
+
+    if [[ "$dst" == s3://* ]]; then
+        # Validate the S3 path format as s3://<bucket-name>/<directory-name>/
+        echo "Checking S3 path format: $dst"
+        if ! [[ "$dst" =~ ^s3://[^/]+/([^/]+/)$ ]]; then
+          echo "Error: Invalid S3 path format."
+          echo "Expected format: s3://<bucket-name>/<directory-name>/"
+          exit 1
+        fi
+
+        # Note: S3 tooling does not provide a native way to check for an empty
+        # directory. To avoid accidental overwrites, we use a best-effort, brittle
+        # workaround that relies on the expected status code and error message
+        # from s5cmd ls.
+        # If the error message changes, this script would be expected to fail
+        # by misreporting non-existent directories as existing, which means
+        # a change in beahvior would cause the script to fail to copy rather
+        # than allow accidental overwrites.
+        echo "Checking if S3 path exists: $dst"
+        if ! OUTPUT=$(s5cmd ls "$dst" 2>&1); then
+            # If the command fails, check for the expected error message.
+            if [[ "$OUTPUT" == *"no object found"* ]]; then
+                echo "Verified S3 destination: '$dst' is empty"
+            else
+                echo "Error: failed to check for contents of $dst"
+                echo "$OUTPUT"
+                exit 1
+            fi
+        else
+            # Success indicates a non-empty destination, so we exit with an error.
+            echo "Cannot copy to non-empty destination: '$dst':"
+            echo "$OUTPUT"
+            exit 1
+        fi
+    else
+        echo "Checking if local path exists: $dst"
+        if [[ -e "$dst" ]]; then
+            echo "Local destination directory '$dst' already exists. Exiting."
+            exit 1
+        fi
+    fi
+}
+
+check_dst_not_exists "$DST"
+
+copy_source "$SRC" "$DST"
--- scripts/tests.load.kube.kind.sh
+++ scripts/tests.load.kube.kind.sh
@@ -2,25 +2,22 @@
 
 set -euo pipefail
 
-# Run e2e tests against nodes deployed to a kind cluster.
+# Run load test against nodes deployed to a kind cluster
 
-# TODO(marun) Support testing against a remote cluster
-
-if ! [[ "$0" =~ scripts/tests.e2e.kube.sh ]]; then
-  echo "must be run from repository root"
-  exit 255
+if ! [[ "$0" =~ scripts/tests.load.kube.kind.sh ]]; then
+    echo "must be run from repository root"
+    exit 255
 fi
 
 # This script will use kubeconfig arguments if supplied
 ./scripts/start_kind_cluster.sh "$@"
 
-# Use an image that will be pushed to the local registry that the kind cluster is configured to use.
+# Build AvalancheGo image
 AVALANCHEGO_IMAGE="localhost:5001/avalanchego"
-XSVM_IMAGE="${AVALANCHEGO_IMAGE}-xsvm"
 if [[ -n "${SKIP_BUILD_IMAGE:-}" ]]; then
-  echo "Skipping build of xsvm image due to SKIP_BUILD_IMAGE=${SKIP_BUILD_IMAGE}"
+  echo "Skipping build of avalanchego image due to SKIP_BUILD_IMAGE=${SKIP_BUILD_IMAGE}"
 else
-  XSVM_IMAGE="${XSVM_IMAGE}" AVALANCHEGO_IMAGE="${AVALANCHEGO_IMAGE}" bash -x ./scripts/build_xsvm_image.sh
+  DOCKER_IMAGE="$AVALANCHEGO_IMAGE" FORCE_TAG_LATEST=1 ./scripts/build_image.sh
 fi
 
 # Determine kubeconfig context to use
@@ -36,4 +33,4 @@ else
     echo "Defaulting to limited-permission context 'kind-kind-tmpnet' to test RBAC Role permissions"
 fi
 
-bash -x ./scripts/tests.e2e.sh --runtime=kube --kube-image="${XSVM_IMAGE}" "$KUBECONFIG_CONTEXT" "$@"
+go run ./tests/load/main --runtime=kube --kube-image="$AVALANCHEGO_IMAGE" "$KUBECONFIG_CONTEXT" "$@"
