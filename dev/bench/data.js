3f558d87ebd7cc0f6df2aeada925d17e0b12a68e
github-action-benchmark <github@users.noreply.github.com>
2025-09-23 09:11:06 +0000

    add Benchmark (go) benchmark result for 0ee1469f13281d3ec79609ef0b9547108ebb65fc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,7 +1,36 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758532513512,
+  "lastUpdate": 1758618666534,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
-    "Benchmark": []
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "aaronbuchwald",
+            "username": "aaronbuchwald",
+            "email": "aaron.buchwald56@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "0ee1469f13281d3ec79609ef0b9547108ebb65fc",
+          "message": "Move task args to cli instead of run_env (#4309)\n\nSigned-off-by: aaronbuchwald <aaron.buchwald56@gmail.com>",
+          "timestamp": "2025-09-22T20:12:48Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/0ee1469f13281d3ec79609ef0b9547108ebb65fc"
+        },
+        "date": 1758618665368,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[101,250000]-Config-default-Runner-blacksmith-4vcpu-ubuntu-2404",
+            "value": 136.3,
+            "unit": "mgas/s",
+            "extra": "1 times\n6 procs"
+          }
+        ]
+      }
+    ]
   }
 }
