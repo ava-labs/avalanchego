f0012a4352b41031835eb44ae9ba71410263b56f
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-09-23 13:53:19 -0400

    don't panic!

--- plugin/evm/vmtest/test_syncervm.go
+++ plugin/evm/vmtest/test_syncervm.go
@@ -162,7 +162,11 @@ func StateSyncToggleEnabledToDisabledTest(t *testing.T, testSetup *SyncTestSetup
 		if !hasItem {
 			t.Fatal("expected nodeSet to contain at least 1 nodeID")
 		}
-		require.NoError(t, testSyncVMSetup.serverVM.VM.AppRequest(ctx, nodeID, requestID, time.Now().Add(1*time.Second), request))
+		go func() {
+			if err := testSyncVMSetup.serverVM.VM.AppRequest(ctx, nodeID, requestID, time.Now().Add(1*time.Second), request); err != nil {
+				t.Errorf("AppRequest failed: %v", err)
+			}
+		}()
 
 		return nil
 	}
@@ -248,9 +252,13 @@ func StateSyncToggleEnabledToDisabledTest(t *testing.T, testSetup *SyncTestSetup
 	// override [serverVM]'s SendAppResponse function to trigger AppResponse on [syncerVM]
 	testSyncVMSetup.serverVM.AppSender.SendAppResponseF = func(ctx context.Context, nodeID ids.NodeID, requestID uint32, response []byte) error {
 		if test.responseIntercept == nil {
-			require.NoError(t, syncReEnabledVM.AppResponse(ctx, nodeID, requestID, response))
+			go func() {
+				if err := syncReEnabledVM.AppResponse(ctx, nodeID, requestID, response); err != nil {
+					t.Errorf("AppResponse failed: %v", err)
+				}
+			}()
 		} else {
-			test.responseIntercept(syncReEnabledVM, nodeID, requestID, response)
+			go test.responseIntercept(syncReEnabledVM, nodeID, requestID, response)
 		}
 
 		return nil
@@ -389,9 +397,13 @@ func initSyncServerAndClientVMs(t *testing.T, test SyncTestParams, numBlocks int
 	// override [serverVM]'s SendAppResponse function to trigger AppResponse on [syncerVM]
 	serverTest.AppSender.SendAppResponseF = func(ctx context.Context, nodeID ids.NodeID, requestID uint32, response []byte) error {
 		if test.responseIntercept == nil {
-			require.NoError(syncerVM.AppResponse(ctx, nodeID, requestID, response))
+			go func() {
+				if err := syncerVM.AppResponse(ctx, nodeID, requestID, response); err != nil {
+					t.Errorf("AppResponse failed: %v", err)
+				}
+			}()
 		} else {
-			test.responseIntercept(syncerVM, nodeID, requestID, response)
+			go test.responseIntercept(syncerVM, nodeID, requestID, response)
 		}
 
 		return nil
