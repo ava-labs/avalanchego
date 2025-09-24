76d429568c329f09b30f32ac3c076c01b2cb0f69
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-09-24 16:22:27 -0400

    cleanup comments

--- vms/evm/database/wrapped_database.go
+++ vms/evm/database/wrapped_database.go
@@ -21,16 +21,12 @@ var (
 
 func WrapDatabase(db database.Database) ethdb.KeyValueStore { return ethDBWrapper{db} }
 
-// ethDbWrapper implements ethdb.Database
 type ethDBWrapper struct{ database.Database }
 
-// Stat implements ethdb.Database
 func (ethDBWrapper) Stat(string) (string, error) { return "", ErrStatNotSupported }
 
-// NewBatch implements ethdb.Database
 func (db ethDBWrapper) NewBatch() ethdb.Batch { return ethBatchWrapper{db.Database.NewBatch()} }
 
-// NewBatchWithSize implements ethdb.Database
 // TODO: propagate size through avalanchego Database interface
 func (db ethDBWrapper) NewBatchWithSize(int) ethdb.Batch {
 	return ethBatchWrapper{db.Database.NewBatch()}
@@ -40,9 +36,7 @@ func (ethDBWrapper) NewSnapshot() (ethdb.Snapshot, error) {
 	return nil, ErrSnapshotNotSupported
 }
 
-// NewIterator implements ethdb.Database
-//
-// Note: This method assumes that the prefix is NOT part of the start, so there's
+// This method assumes that the prefix is NOT part of the start, so there's
 // no need for the caller to prepend the prefix to the start.
 func (db ethDBWrapper) NewIterator(prefix []byte, start []byte) ethdb.Iterator {
 	// avalanchego's database implementation assumes that the prefix is part of the
@@ -56,16 +50,12 @@ func (db ethDBWrapper) NewIterator(prefix []byte, start []byte) ethdb.Iterator {
 	return db.Database.NewIteratorWithStartAndPrefix(start, prefix)
 }
 
-// NewIteratorWithStart implements ethdb.Database
 func (db ethDBWrapper) NewIteratorWithStart(start []byte) ethdb.Iterator {
 	return db.Database.NewIteratorWithStart(start)
 }
 
-// ethBatchWrapper implements ethdb.Batch
 type ethBatchWrapper struct{ database.Batch }
 
-// ValueSize implements ethdb.Batch
 func (batch ethBatchWrapper) ValueSize() int { return batch.Batch.Size() }
 
-// Replay implements ethdb.Batch
 func (batch ethBatchWrapper) Replay(w ethdb.KeyValueWriter) error { return batch.Batch.Replay(w) }
