f52b2d4d93375c6eee94e207fba906770b0715fb
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-09-24 15:57:49 -0400

    partial lint - rest is on commented out tests

--- vms/evm/uptimetracker/uptime_tracker_test.go
+++ vms/evm/uptimetracker/uptime_tracker_test.go
@@ -11,12 +11,12 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/ava-labs/avalanchego/codec"
 	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/database/memdb"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow/validators"
 	"github.com/ava-labs/avalanchego/utils/timer/mockable"
-	"github.com/ava-labs/avalanchego/codec"
 	"github.com/ava-labs/avalanchego/utils/wrappers"
 )
 
