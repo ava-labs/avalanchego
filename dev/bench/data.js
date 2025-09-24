77c11916c6010386eb3a2495e9d2d4164b2b0005
github-action-benchmark <github@users.noreply.github.com>
2025-09-24 00:59:32 +0000

    add Benchmark (go) benchmark result for 454774549a6c01eb4767a5f895824b81a3ccf992

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,7 +1,36 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758618760778,
+  "lastUpdate": 1758675572657,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
-    "Benchmark": []
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Stephen Buttolph",
+            "username": "StephenButtolph",
+            "email": "stephen@avalabs.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "454774549a6c01eb4767a5f895824b81a3ccf992",
+          "message": "Add test-unit-fast task (#4323)",
+          "timestamp": "2025-09-23T17:21:42Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/454774549a6c01eb4767a5f895824b81a3ccf992"
+        },
+        "date": 1758675571217,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast",
+            "value": 44.61,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      }
+    ]
   }
 }
