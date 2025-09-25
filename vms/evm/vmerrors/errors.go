12f1bc555e287ad6a9009a35fade90b3fa1ea8e5
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-09-25 16:29:18 -0400

    add errors

---
+++ vms/evm/vmerrors/errors.go
@@ -0,0 +1,14 @@
+// Copyright (C) 2019-2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package vmerrors
+
+import "errors"
+
+var (
+	ErrGenerateBlockFailed         = errors.New("failed to generate block")
+	ErrBlockVerificationFailed     = errors.New("failed to verify block")
+	ErrWrapBlockFailed             = errors.New("failed to wrap block")
+	ErrInvalidCoinbase             = errors.New("invalid coinbase")
+	ErrSenderAddressNotAllowListed = errors.New("cannot issue transaction from non-allow listed address")
+)
