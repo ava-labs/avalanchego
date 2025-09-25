eb5ffc5759393ab307a11bf6b03ae23341f01b51
github-action-benchmark <github@users.noreply.github.com>
2025-09-25 23:43:39 +0000

    add Benchmark (go) benchmark result for d0bcd73114e3d31dadbb12a245688f35e0dfd255

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758834824763,
+  "lastUpdate": 1758843819539,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -54,6 +54,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd",
             "value": 73.92,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stephen Buttolph",
+            "username": "StephenButtolph",
+            "email": "stephen@avalabs.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "d0bcd73114e3d31dadbb12a245688f35e0dfd255",
+          "message": "Replace `mockable.MaxTime` with `upgrade.UnscheduledActivationTime` (#4322)",
+          "timestamp": "2025-09-25T20:46:19Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/d0bcd73114e3d31dadbb12a245688f35e0dfd255"
+        },
+        "date": 1758843817491,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast",
+            "value": 43.43,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
           }
