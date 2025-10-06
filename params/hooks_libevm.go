ac598a8256f6e1015f75bc695588b07ebedb334e
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-10-06 15:30:23 -0400

    sync: coreth PR #1044: register secp256r1 precompile (#1758)
    Signed-off-by: Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
    Signed-off-by: Jonathan Oppenheimer <147infiniti@gmail.com>
    Co-authored-by: Ceyhun Onur <ceyhun.onur@avalabs.org>
    Co-authored-by: Stephen Buttolph <stephen@avalabs.org>

--- params/hooks_libevm.go
+++ params/hooks_libevm.go
@@ -5,7 +5,9 @@ package params
 
 import (
 	"fmt"
+	"maps"
 	"math/big"
+	"slices"
 
 	"github.com/ava-labs/avalanchego/snow"
 	"github.com/ava-labs/avalanchego/utils/set"
@@ -28,6 +30,9 @@ import (
 // invalidateDelegateTime is the Unix timestamp for August 2nd, 2025, midnight Eastern Time
 // (August 2nd, 2025, 04:00 UTC)
 const invalidateDelegateUnix = 1754107200
+
+// P256VerifyAddress is the address of the p256 signature verification precompile
+var P256VerifyAddress = common.BytesToAddress([]byte{0x1, 0x00})
 
 type RulesExtra extras.Rules
 
@@ -56,20 +61,36 @@ func (RulesExtra) CanExecuteTransaction(common.Address, *common.Address, libevm.
 	return nil
 }
 
-func (RulesExtra) ActivePrecompiles(existing []common.Address) []common.Address {
-	return existing
-}
-
 // MinimumGasConsumption is a no-op.
 func (RulesExtra) MinimumGasConsumption(x uint64) uint64 {
 	return (ethparams.NOOPHooks{}).MinimumGasConsumption(x)
 }
 
+var PrecompiledContractsGranite = map[common.Address]vm.PrecompiledContract{
+	P256VerifyAddress: &vm.P256Verify{},
+}
+
+func (r RulesExtra) ActivePrecompiles(existing []common.Address) []common.Address {
+	var addresses []common.Address
+	addresses = slices.AppendSeq(addresses, maps.Keys(r.currentPrecompiles()))
+	addresses = append(addresses, existing...)
+	return addresses
+}
+
+func (r RulesExtra) currentPrecompiles() map[common.Address]vm.PrecompiledContract {
+	switch {
+	case r.IsGranite:
+		return PrecompiledContractsGranite
+	}
+	return nil
+}
+
 // precompileOverrideBuiltin specifies precompiles that were activated prior to the
 // dynamic precompile activation registry.
 // These were only active historically and are not active in the current network.
-func (RulesExtra) precompileOverrideBuiltin(common.Address) (libevm.PrecompiledContract, bool) {
-	return nil, false
+func (r RulesExtra) precompileOverrideBuiltin(addr common.Address) (libevm.PrecompiledContract, bool) {
+	precompile, ok := r.currentPrecompiles()[addr]
+	return precompile, ok
 }
 
 func makePrecompile(contract contract.StatefulPrecompiledContract) libevm.PrecompiledContract {
@@ -109,6 +130,7 @@ func (r RulesExtra) PrecompileOverride(addr common.Address) (libevm.PrecompiledC
 	if p, ok := r.precompileOverrideBuiltin(addr); ok {
 		return p, true
 	}
+
 	if _, ok := r.Precompiles[addr]; !ok {
 		return nil, false
 	}
