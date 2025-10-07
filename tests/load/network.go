041070d9339ec6203efea5aed8b3a2cd834e9cfa
Rodrigo Villar <rodrigo.villar@avalabs.org>
2025-10-07 08:48:43 -0400

    feat(load): add devnet support

---
+++ tests/load/network.go
@@ -0,0 +1,93 @@
+// Copyright (C) 2019-2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package load
+
+import (
+	"os"
+
+	"github.com/ava-labs/libevm/ethclient"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ava-labs/avalanchego/tests"
+	"github.com/ava-labs/avalanchego/tests/fixture/tmpnet"
+	"github.com/ava-labs/avalanchego/utils/crypto/secp256k1"
+)
+
+// DevnetConfig holds the configuration for connecting to an existing devnet.
+// It specifies the private keys to use for load generation and the WebSocket
+// URIs of the nodes to connect to.
+type DevnetConfig struct {
+	PrivateKeys []string `json:"private-keys"`
+	NodeWsURIs  []string `json:"node-ws-uris"`
+}
+
+// ParseKeys converts the string representation of private keys into their
+// concrete types (*secp256k1.PrivateKeys). Returns an error if any key fails to parse.
+func (d DevnetConfig) ParseKeys() ([]*secp256k1.PrivateKey, error) {
+	keys := make([]*secp256k1.PrivateKey, len(d.PrivateKeys))
+	for i, pk := range d.PrivateKeys {
+		key, err := secp256k1.ToPrivateKey([]byte(pk))
+		if err != nil {
+			return nil, err
+		}
+		keys[i] = key
+	}
+
+	return keys, nil
+}
+
+// ConnectNetwork connects to an existing network and returns a list of workers
+// that can interact with the network.
+//
+// The function also configures Prometheus monitoring for any client-side
+// metrics defined in metricsServer and registers a cleanup function to remove the
+// monitoring configuration file when the test completes.
+func ConnectNetwork(tc tests.TestContext, metricsServer *tests.PrometheusServer, devnetConfig DevnetConfig) []Worker {
+	require := require.New(tc)
+
+	keys, err := devnetConfig.ParseKeys()
+	require.NoError(err)
+
+	wsURIs := devnetConfig.NodeWsURIs
+	workers := make([]Worker, len(keys))
+	for i, key := range keys {
+		wsURI := wsURIs[i%len(wsURIs)]
+		client, err := ethclient.Dial(wsURI)
+		require.NoError(err)
+
+		nonce, err := client.NonceAt(tc.DefaultContext(), key.EthAddress(), nil)
+		require.NoError(err)
+
+		workers[i] = Worker{
+			PrivKey: key.ToECDSA(),
+			Client:  client,
+			Nonce:   nonce,
+		}
+	}
+
+	// XXX: we could probably remove this metrics code from ConnectNetwork
+	networkUUID := uuid.NewString()
+	labels := map[string]string{
+		"job":               "load-test",
+		"is_ephemeral_node": "false",
+		"chain":             "C",
+		"networkUUID":       networkUUID,
+	}
+
+	monitoringConfigFilePath, err := tmpnet.WritePrometheusSDConfig("load-test", tmpnet.SDConfig{
+		Targets: []string{metricsServer.Address()},
+		Labels:  labels,
+	}, false)
+	require.NoError(err, "failed to generate monitoring config file")
+
+	tc.DeferCleanup(func() {
+		require.NoError(
+			os.Remove(monitoringConfigFilePath),
+			"failed †o remove monitoring config file",
+		)
+	})
+
+	return workers
+}
