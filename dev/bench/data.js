972e759948f3c2cf067e922102c5a854397c8c64
github-action-benchmark <github@users.noreply.github.com>
2025-10-15 12:37:46 +0000

    add Benchmark (go) benchmark result for edf67505fee7c95837c2220467de86fea0efc860

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760454897017,
+  "lastUpdate": 1760531866767,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -1982,6 +1982,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast",
             "value": 43.34,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ian Suvak",
+            "username": "iansuvak",
+            "email": "ian.suvak@avalabs.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "edf67505fee7c95837c2220467de86fea0efc860",
+          "message": "Add network config to connect to all validators (#4164)\n\nSigned-off-by: Ian Suvak <ian.suvak@avalabs.org>\nCo-authored-by: Stephen Buttolph <stephen@avalabs.org>",
+          "timestamp": "2025-10-14T16:05:33Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/edf67505fee7c95837c2220467de86fea0efc860"
+        },
+        "date": 1760531866256,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd",
+            "value": 74.79,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
           }
