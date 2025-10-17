fdcdb92db593ac88432f26ac93cdd3747c70a568
maru <maru.newby@avalabs.org>
2025-10-17 17:42:52 +0000

    Increase the detail of the status command

--- tests/fixture/polyrepo/core/status.go
+++ tests/fixture/polyrepo/core/status.go
@@ -11,13 +11,15 @@ import (
 
 // RepoStatus represents the status of a repository
 type RepoStatus struct {
-	Name        string
-	Path        string
-	Exists      bool
-	CurrentRef  string
-	IsDirty     bool
-	HasReplace  bool
-	ReplacePath string
+	Name         string
+	Path         string
+	Exists       bool
+	CurrentRef   string
+	CommitSHA    string   // 8-digit short SHA of current commit
+	Tags         []string // tags pointing to current commit
+	Branches     []string // branches pointing to current commit
+	IsDirty      bool
+	Replacements map[string]string // module path -> replacement path/version
 }
 
 // GetRepoStatus returns the status of a repository
@@ -38,9 +40,10 @@ func GetRepoStatus(log logging.Logger, repoName, baseDir, goModPath string, isPr
 	}
 
 	status := &RepoStatus{
-		Name:   repoName,
-		Path:   repoPath,
-		Exists: false,
+		Name:         repoName,
+		Path:         repoPath,
+		Exists:       false,
+		Replacements: make(map[string]string),
 	}
 
 	// Check if repo exists
@@ -54,19 +57,42 @@ func GetRepoStatus(log logging.Logger, repoName, baseDir, goModPath string, isPr
 		if err == nil {
 			status.CurrentRef = currentRef
 		}
+
+		// Get commit SHA
+		commitSHA, err := GetCommitSHA(log, status.Path)
+		if err == nil {
+			status.CommitSHA = commitSHA
+
+			// Get tags for this commit
+			tags, err := GetTagsForCommit(log, status.Path, commitSHA)
+			if err == nil {
+				status.Tags = tags
+			}
+
+			// Get branches for this commit
+			branches, err := GetBranchesForCommit(log, status.Path, commitSHA)
+			if err == nil {
+				status.Branches = branches
+			}
+		}
 	}
 
-	// Check for replace directive in go.mod
-	if goModPath != "" {
-		modFile, err := ReadGoMod(log, goModPath)
+	// Get replace directives from THIS repo's go.mod (if it exists)
+	var repoGoModPath string
+	if isPrimary {
+		// For primary repo, use the provided goModPath
+		repoGoModPath = goModPath
+	} else if config.GoModPath != "" {
+		// For synced repos, construct path to their go.mod
+		// Use config.GoModPath which is relative to repo root
+		// e.g., "go.mod" for most repos, "ffi/go.mod" for firewood
+		repoGoModPath = GetRepoClonePath(repoName, baseDir) + "/" + config.GoModPath
+	}
+
+	if repoGoModPath != "" {
+		replacements, err := GetAllReplaceDirectives(log, repoGoModPath)
 		if err == nil {
-			for _, replace := range modFile.Replace {
-				if replace.Old.Path == config.GoModule {
-					status.HasReplace = true
-					status.ReplacePath = replace.New.Path
-					break
-				}
-			}
+			status.Replacements = replacements
 		}
 	}
 
@@ -80,8 +106,45 @@ func FormatRepoStatus(status *RepoStatus) string {
 	}
 
 	result := fmt.Sprintf("%s: %s", status.Name, status.Path)
-	if status.HasReplace {
-		result += fmt.Sprintf(" (replace: %s)", status.ReplacePath)
+	if status.CommitSHA != "" {
+		result += fmt.Sprintf(" (%s)", status.CommitSHA)
+	}
+
+	// Add branches
+	if len(status.Branches) > 0 {
+		result += "\n    branches: " + joinStrings(status.Branches)
+	} else {
+		result += "\n    branches: none"
+	}
+
+	// Add tags
+	if len(status.Tags) > 0 {
+		result += "\n    tags: " + joinStrings(status.Tags)
+	} else {
+		result += "\n    tags: none"
+	}
+
+	// Add replacements
+	if len(status.Replacements) > 0 {
+		result += "\n    replacements:"
+		for module, path := range status.Replacements {
+			result += fmt.Sprintf("\n      %s: %s", module, path)
+		}
+	} else {
+		result += "\n    replacements: none"
+	}
+
+	return result
+}
+
+// joinStrings joins a slice of strings with ", "
+func joinStrings(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	result := strs[0]
+	for i := 1; i < len(strs); i++ {
+		result += ", " + strs[i]
 	}
 	return result
 }
