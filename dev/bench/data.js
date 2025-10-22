77bd97325005385a37d450d0e9a2c76cb4745e9e
github-action-benchmark <github@users.noreply.github.com>
2025-10-22 12:33:01 +0000

    add Benchmark (go) benchmark result for f369e5df25d31cc684028bb11ce783c3f17085b5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761059582996,
+  "lastUpdate": 1761136381212,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -2318,6 +2318,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast",
             "value": 43.43,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Tsvetan Dimitrov",
+            "username": "powerslider",
+            "email": "tsvetan.dimitrov23@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "f369e5df25d31cc684028bb11ce783c3f17085b5",
+          "message": "feat(sync/customrawdb): migrate customrawdb package from coreth (#4387)\n\nSigned-off-by: Tsvetan Dimitrov (tsvetan.dimitrov23@gmail.com)",
+          "timestamp": "2025-10-20T23:30:51Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/f369e5df25d31cc684028bb11ce783c3f17085b5"
+        },
+        "date": 1761136380353,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd",
+            "value": 76.69,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
           }
