6d82c5c61a288c33d0e0125dd290ac0272bb8517
github-action-benchmark <github@users.noreply.github.com>
2025-10-29 12:40:09 +0000

    add Benchmark (go) benchmark result for edff59906ed4d6334d9e591dcb9c85a144520600

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761673328391,
+  "lastUpdate": 1761741609394,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -2712,6 +2712,34 @@ window.BENCHMARK_DATA = {
             "value": 87.72,
             "unit": "mgas/s",
             "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jonathan Oppenheimer",
+            "username": "JonathanOppenheimer",
+            "email": "jonathan.oppenheimer@avalabs.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "edff59906ed4d6334d9e591dcb9c85a144520600",
+          "message": "Export uptimetracker error (#4436)",
+          "timestamp": "2025-10-28T17:37:23Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/edff59906ed4d6334d9e591dcb9c85a144520600"
+        },
+        "date": 1761741608925,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd",
+            "value": 72.72,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
           }
         ]
       }
