2849f2ce4aed7e2033bd6f3946679081538c0144
github-action-benchmark <github@users.noreply.github.com>
2025-11-06 15:29:29 +0000

    add Benchmark (go) benchmark result for eb2358a08b7a3927ca29684c09071a7c7c63a537

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762432926507,
+  "lastUpdate": 1762442969194,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -1258,6 +1258,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd",
             "value": 72.35,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stephen Buttolph",
+            "username": "StephenButtolph",
+            "email": "stephen@avalabs.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "eb2358a08b7a3927ca29684c09071a7c7c63a537",
+          "message": "Add UpgradeTime to p2p handshake (#4468)\n\nSigned-off-by: Stephen Buttolph <stephen@avalabs.org>\nCo-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>",
+          "timestamp": "2025-11-06T14:37:48Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/eb2358a08b7a3927ca29684c09071a7c7c63a537"
+        },
+        "date": 1762442967167,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast",
+            "value": 41.45,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
           }
