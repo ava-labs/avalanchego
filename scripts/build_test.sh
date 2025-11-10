4af2868bb7481eb98586d82330a65f855904960a
maru <maru.newby@avalabs.org>
2025-11-10 18:02:38 +0000

    Add coreth-specific unit test task and job to allow for longer timeout

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -19,13 +19,15 @@ if [[ -n "${NO_RACE:-}" ]]; then
     race=""
 fi
 
-EXCLUDED_TARGETS="| grep -v /mocks | grep -v proto | grep -v tests/e2e | grep -v tests/load/c | grep -v tests/upgrade | grep -v tests/fixture/bootstrapmonitor/e2e | grep -v tests/reexecute"
-
-if [[ "$(go env GOOS)" == "windows" ]]; then
-  # Test discovery for the antithesis test setups is broken due to
-  # their dependence on the linux-only Antithesis SDK.
-  EXCLUDED_TARGETS="${EXCLUDED_TARGETS} | grep -v tests/antithesis"
-fi
+EXCLUDED_TARGETS="|\
+ grep -v /mocks |\
+ grep -v proto |\
+ grep -v tests/e2e |\
+ grep -v tests/load/c |\
+ grep -v tests/upgrade |\
+ grep -v tests/fixture/bootstrapmonitor/e2e |\
+ grep -v tests/reexecute |\
+ grep -v graft/coreth"
 
 TEST_TARGETS="$(eval "go list ./... ${EXCLUDED_TARGETS}")"
 
