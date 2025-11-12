fc9f411fa2a045122cfcccbb10808e25bcd55f89
github-action-benchmark <github@users.noreply.github.com>
2025-11-12 15:17:03 +0000

    add Benchmark (go) benchmark result for 29b4e6bc541bcd913da6c51c1c5403fff2e377a4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762951204097,
+  "lastUpdate": 1762960623384,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -1594,6 +1594,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd",
             "value": 73.19,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stephen Buttolph",
+            "username": "StephenButtolph",
+            "email": "stephen@avalabs.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "29b4e6bc541bcd913da6c51c1c5403fff2e377a4",
+          "message": "Alert upon an unknown future upgrade (#4474)",
+          "timestamp": "2025-11-11T16:51:33Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/29b4e6bc541bcd913da6c51c1c5403fff2e377a4"
+        },
+        "date": 1762960621386,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast",
+            "value": 42.97,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
           }
