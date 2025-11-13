8a4847501534510c3abf6f8086025d9f2a093d23
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-11-13 16:48:01 -0500

    Move time ordering test to upgrade_test.go

--- upgrade/upgrade_test.go
+++ upgrade/upgrade_test.go
@@ -4,6 +4,7 @@
 package upgrade
 
 import (
+	"reflect"
 	"testing"
 	"time"
 
@@ -36,7 +37,6 @@ func TestValidDefaultUpgrades(t *testing.T) {
 }
 
 func TestInvalidUpgrade(t *testing.T) {
-	require := require.New(t)
 	firstUpgradeTime := time.Now()
 	invalidSecondUpgradeTime := firstUpgradeTime.Add(-1 * time.Second)
 	upgrade := Config{
@@ -44,5 +44,40 @@ func TestInvalidUpgrade(t *testing.T) {
 		ApricotPhase2Time: invalidSecondUpgradeTime,
 	}
 	err := upgrade.Validate()
-	require.ErrorIs(err, ErrInvalidUpgradeTimes)
+	require.ErrorIs(t, err, ErrInvalidUpgradeTimes)
+}
+
+func TestValidUpgradeTimeOrdering(t *testing.T) {
+	configType := reflect.TypeOf(Config{})
+	var timeFields []string
+
+	for i := 0; i < configType.NumField(); i++ {
+		field := configType.Field(i)
+		if field.Type == reflect.TypeOf(time.Time{}) {
+			timeFields = append(timeFields, field.Name)
+		}
+	}
+
+	// Test that Validate() doesn't return an error when all times are equal
+	testConfig := Config{}
+	configValue := reflect.ValueOf(&testConfig).Elem()
+
+	testTime := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
+	for _, fieldName := range timeFields {
+		field := configValue.FieldByName(fieldName)
+		require.True(t, field.IsValid(), "Field %s should exist", fieldName)
+		field.Set(reflect.ValueOf(testTime))
+	}
+
+	// If all times are equal, Validate should pass
+	require.NoError(t, testConfig.Validate(), "Validate should pass when all upgrade times are equal")
+
+	// Now test that Validate catches out-of-order times
+	// Set the last time field to before the first
+	if len(timeFields) >= 2 {
+		lastField := configValue.FieldByName(timeFields[len(timeFields)-1])
+		lastField.Set(reflect.ValueOf(testTime.Add(-time.Hour)))
+		err := testConfig.Validate()
+		require.ErrorIs(t, err, ErrInvalidUpgradeTimes, "Validate should fail when upgrade times are out of order")
+	}
 }
--- upgrade/upgradetest/upgrade_consistency_test.go
+++ upgrade/upgradetest/upgrade_consistency_test.go
@@ -18,41 +18,6 @@ import (
 // TestUpgradeConsistency ensures that when a new upgrade is added, all
 // necessary locations are updated.
 func TestUpgradeConsistency(t *testing.T) {
-	configType := reflect.TypeOf(upgrade.Config{})
-	var timeFields []string
-
-	for i := 0; i < configType.NumField(); i++ {
-		field := configType.Field(i)
-		if field.Type == reflect.TypeOf(time.Time{}) {
-			timeFields = append(timeFields, field.Name)
-		}
-	}
-
-	t.Run("all time fields are in Validate method", func(*testing.T) {
-		// Test that Validate() doesn't return an error when all times are equal
-		testConfig := upgrade.Config{}
-		configValue := reflect.ValueOf(&testConfig).Elem()
-
-		testTime := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
-		for _, fieldName := range timeFields {
-			field := configValue.FieldByName(fieldName)
-			require.True(t, field.IsValid(), "Field %s should exist", fieldName)
-			field.Set(reflect.ValueOf(testTime))
-		}
-
-		// If all times are equal, Validate should pass
-		require.NoError(t, testConfig.Validate(), "Validate should pass when all upgrade times are equal")
-
-		// Now test that Validate catches out-of-order times
-		// Set the last time field to before the first
-		if len(timeFields) >= 2 {
-			lastField := configValue.FieldByName(timeFields[len(timeFields)-1])
-			lastField.Set(reflect.ValueOf(testTime.Add(-time.Hour)))
-			err := testConfig.Validate()
-			require.ErrorIs(t, err, upgrade.ErrInvalidUpgradeTimes, "Validate should fail when upgrade times are out of order")
-		}
-	})
-
 	t.Run("all time fields are set in network configs", func(t *testing.T) {
 		for _, tc := range []struct {
 			name   string
