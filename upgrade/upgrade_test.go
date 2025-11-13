8b5497ce312aeadb8d1979eb72bbc06fb9ba1363
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-11-13 18:46:36 -0500

    get rid of last reflect test

--- upgrade/upgrade_test.go
+++ upgrade/upgrade_test.go
@@ -4,7 +4,6 @@
 package upgrade
 
 import (
-	"reflect"
 	"testing"
 	"time"
 
@@ -45,39 +44,4 @@ func TestInvalidUpgrade(t *testing.T) {
 	}
 	err := upgrade.Validate()
 	require.ErrorIs(t, err, ErrInvalidUpgradeTimes)
-}
-
-func TestValidUpgradeTimeOrdering(t *testing.T) {
-	configType := reflect.TypeOf(Config{})
-	var timeFields []string
-
-	for i := 0; i < configType.NumField(); i++ {
-		field := configType.Field(i)
-		if field.Type == reflect.TypeOf(time.Time{}) {
-			timeFields = append(timeFields, field.Name)
-		}
-	}
-
-	// Test that Validate() doesn't return an error when all times are equal
-	testConfig := Config{}
-	configValue := reflect.ValueOf(&testConfig).Elem()
-
-	testTime := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
-	for _, fieldName := range timeFields {
-		field := configValue.FieldByName(fieldName)
-		require.True(t, field.IsValid(), "Field %s should exist", fieldName)
-		field.Set(reflect.ValueOf(testTime))
-	}
-
-	// If all times are equal, Validate should pass
-	require.NoError(t, testConfig.Validate(), "Validate should pass when all upgrade times are equal")
-
-	// Now test that Validate catches out-of-order times
-	// Set the last time field to before the first
-	if len(timeFields) >= 2 {
-		lastField := configValue.FieldByName(timeFields[len(timeFields)-1])
-		lastField.Set(reflect.ValueOf(testTime.Add(-time.Hour)))
-		err := testConfig.Validate()
-		require.ErrorIs(t, err, ErrInvalidUpgradeTimes, "Validate should fail when upgrade times are out of order")
-	}
 }
