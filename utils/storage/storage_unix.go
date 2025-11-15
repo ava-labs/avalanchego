3ff6d1ce010167b9fdc8eda69ef20125e40c6312
Yacov Manevich <yacov.manevich@avalabs.org>
2025-11-15 23:53:15 +0100

    Monitor free disk percentage, not just absolute space
    Signed-off-by: Yacov Manevich <yacov.manevich@avalabs.org>

--- utils/storage/storage_unix.go
+++ utils/storage/storage_unix.go
@@ -8,12 +8,13 @@ package storage
 
 import "syscall"
 
-func AvailableBytes(storagePath string) (uint64, error) {
+func AvailableBytes(storagePath string) (uint64, uint64, error) {
 	var stat syscall.Statfs_t
 	err := syscall.Statfs(storagePath, &stat)
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
 	avail := stat.Bavail * uint64(stat.Bsize)
-	return avail, nil
+	percentage := stat.Bavail * 100 / stat.Blocks
+	return avail, percentage, nil
 }
