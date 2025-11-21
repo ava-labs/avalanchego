13670b469c0f8d5a8d81c918a76591e6861803a1
github-action-benchmark <github@users.noreply.github.com>
2025-11-21 12:37:37 +0000

    add Benchmark (go) benchmark result for 3b08d6131a00bd55349321a58ae7e6cfcff8635e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1763651524690,
+  "lastUpdate": 1763728657582,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -2070,6 +2070,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast",
             "value": 43.44,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Austin Larson",
+            "username": "alarso16",
+            "email": "78000745+alarso16@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "3b08d6131a00bd55349321a58ae7e6cfcff8635e",
+          "message": "test: Remove unnecessary libevm test (#4536)",
+          "timestamp": "2025-11-20T20:28:46Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/3b08d6131a00bd55349321a58ae7e6cfcff8635e"
+        },
+        "date": 1763728657124,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd",
+            "value": 73.66,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
           }
