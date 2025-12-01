4596c7afe4fd0c170c06b2e05f0e9d0d88bcdc41
github-action-benchmark <github@users.noreply.github.com>
2025-12-01 15:23:56 +0000

    add Benchmark (go) benchmark result for c267010674046ad04ea1d693fe92f37cde77105f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764594247089,
+  "lastUpdate": 1764602636541,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -2686,6 +2686,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd",
             "value": 65.87,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Elvis",
+            "username": "Elvis339",
+            "email": "43846394+Elvis339@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c267010674046ad04ea1d693fe92f37cde77105f",
+          "message": "chore: task reexecution (#4443)",
+          "timestamp": "2025-12-01T14:19:43Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/c267010674046ad04ea1d693fe92f37cde77105f"
+        },
+        "date": 1764602635064,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast",
+            "value": 42.37,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
           }
