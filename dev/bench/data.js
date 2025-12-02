00f8f876c7041e0f48cd6f89771110ed49a0e485
github-action-benchmark <github@users.noreply.github.com>
2025-12-02 12:42:33 +0000

    add Benchmark (go) benchmark result for cfb6dcf47fbd8a1ace209142acc15c018066f5ca

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764626596323,
+  "lastUpdate": 1764679353970,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -2742,6 +2742,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[101,250000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd-jp6dz-runner-6m4sr",
             "value": 104.7,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Stephen Buttolph",
+            "username": "StephenButtolph",
+            "email": "stephen@avalabs.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "cfb6dcf47fbd8a1ace209142acc15c018066f5ca",
+          "message": "Skip incrementing bloom count after noop Add (#4620)",
+          "timestamp": "2025-12-01T21:04:17Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/cfb6dcf47fbd8a1ace209142acc15c018066f5ca"
+        },
+        "date": 1764679353395,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd-jp6dz-runner-qnf47",
+            "value": 72.35,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
           }
