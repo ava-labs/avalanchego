5802ebd4394aa40e15cb937f1d4604a25cea7605
github-action-benchmark <github@users.noreply.github.com>
2025-12-06 12:31:01 +0000

    add Benchmark (go) benchmark result for c3002e844f3ab71ec47bce5cc3bf7c803252d1ab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764947271698,
+  "lastUpdate": 1765024261375,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -2966,6 +2966,34 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast-79qqw-runner-mtgdj",
             "value": 44.06,
+            "unit": "mgas/s",
+            "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Austin Larson",
+            "username": "alarso16",
+            "email": "78000745+alarso16@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c3002e844f3ab71ec47bce5cc3bf7c803252d1ab",
+          "message": "chore: Cleanup coreth post-merge (#4578)",
+          "timestamp": "2025-12-05T16:24:51Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/c3002e844f3ab71ec47bce5cc3bf7c803252d1ab"
+        },
+        "date": 1765024260904,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd-jp6dz-runner-f9zxw",
+            "value": 75.76,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
           }
