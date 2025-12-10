e976327992d25e96608266b3abdb3799af164b79
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-12-10 17:45:21 -0500

    [migrate-subnet-evm][7] releasing

--- .github/workflows/build-tgz-pkg.sh
+++ .github/workflows/build-tgz-pkg.sh
@@ -12,8 +12,18 @@ if [[ $OK -ne 0 ]]; then
 fi
 
 
-echo "Build tgz package..."
+# Build avalanchego tgzpackage
+echo "Build avalanchego tgz package..."
 cd "$PKG_ROOT"
 echo "Tag: $TAG"
 tar -czvf "avalanchego-linux-$ARCH-$TAG.tar.gz" "avalanchego-$TAG"
 aws s3 cp "avalanchego-linux-$ARCH-$TAG.tar.gz" "s3://$BUCKET/linux/binaries/ubuntu/$RELEASE/$ARCH/"
+
+# Build subnet-evm tgzpackage
+echo "Build subnet-evm tgz package..."
+SUBNET_EVM_ROOT=$PKG_ROOT/subnet-evm-$TAG
+mkdir -p "$SUBNET_EVM_ROOT"
+cp "${GITHUB_WORKSPACE:-$PWD}/build/subnet-evm" "$SUBNET_EVM_ROOT/"
+cd "$PKG_ROOT"
+tar -czvf "subnet-evm-linux-$ARCH-$TAG.tar.gz" "subnet-evm-$TAG"
+aws s3 cp "subnet-evm-linux-$ARCH-$TAG.tar.gz" "s3://$BUCKET/linux/binaries/ubuntu/$RELEASE/$ARCH/"
