f682b20f8e08be3b88db74d146305950e558172f
Rodrigo Villar <rodrigo.villar@avalabs.org>
2025-12-11 11:08:41 -0500

    chore: ignoring defining flag if arg is empty

--- scripts/benchmark_cchain_range.sh
+++ scripts/benchmark_cchain_range.sh
@@ -20,7 +20,6 @@ set -euo pipefail
 : "${END_BLOCK:?END_BLOCK must be set}"
 
 go run github.com/ava-labs/avalanchego/tests/reexecute/c \
-  --benchmark-output-file="${BENCHMARK_OUTPUT_FILE}" \
   --block-dir="${BLOCK_DIR}" \
   --current-state-dir="${CURRENT_STATE_DIR}" \
   ${RUNNER_NAME:+--runner="${RUNNER_NAME}"} \
@@ -28,6 +27,7 @@ go run github.com/ava-labs/avalanchego/tests/reexecute/c \
   --start-block="${START_BLOCK}" \
   --end-block="${END_BLOCK}" \
   ${LABELS:+--labels="${LABELS}"} \
+  ${BENCHMARK_OUTPUT_FILE:+--benchmark-output-file="${BENCHMARK_OUTPUT_FILE}"} \
   ${METRICS_SERVER_ENABLED:+--metrics-server-enabled="${METRICS_SERVER_ENABLED}"} \
   ${METRICS_SERVER_PORT:+--metrics-server-port="${METRICS_SERVER_PORT}"} \
   ${METRICS_COLLECTOR_ENABLED:+--metrics-collector-enabled="${METRICS_COLLECTOR_ENABLED}"}
