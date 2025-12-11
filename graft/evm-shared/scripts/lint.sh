580fce93abc8987eb023becd41004f1af4f59063
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-12-11 13:06:47 -0500

    style: fix shellcheck

--- graft/evm-shared/scripts/lint.sh
+++ graft/evm-shared/scripts/lint.sh
@@ -21,6 +21,7 @@ grep -P 'lint.sh' "$0" &>/dev/null || (
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Library for file list generation.
+# shellcheck disable=SC1091
 source "$SCRIPT_DIR/lint_setup.sh"
 
 # by default, "./scripts/lint.sh" runs all lint tests
