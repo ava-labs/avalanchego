cb4d688fdad6988f74296f81eb05ee8a332badac
github-action-benchmark <github@users.noreply.github.com>
2025-12-16 13:05:07 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c6096926d86fc94aad663bb54af42a2cac7cc49a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765811521606,
+  "lastUpdate": 1765890307483,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -3612,6 +3612,33 @@ window.BENCHMARK_DATA = {
             "value": 43.9,
             "unit": "mgas/s",
             "extra": "1 times\n16 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "rodrigo",
+            "username": "RodrigoVillar",
+            "email": "77309055+RodrigoVillar@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c6096926d86fc94aad663bb54af42a2cac7cc49a",
+          "message": "chore(reexecute/c): remove go bench from benchmark (#4640)",
+          "timestamp": "2025-12-16T08:28:03Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/c6096926d86fc94aad663bb54af42a2cac7cc49a"
+        },
+        "date": 1765890306955,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd-jp6dz-runner-8jpv4 - mgas/s",
+            "value": 65.1772929442747,
+            "unit": "mgas/s"
           }
         ]
       }
