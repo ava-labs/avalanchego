9324494ae87b2d2625bc5fa3090a213e952d1c9e
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-12-19 17:19:20 -0500

    ci: ensure generated files up to date in CI

---
+++ graft/coreth/precompile/contracts/warp/warpbindings/compile.go
@@ -0,0 +1,7 @@
+// Copyright (C) 2019-2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package warpbindings
+
+// Step 1: Compile IWarpMessenger interface to generate ABI at parent level
+//go:generate solc-v0.8.30 -o .. --overwrite --abi --pretty-json --evm-version cancun IWarpMessenger.sol
---
+++ graft/subnet-evm/core/compile.go
@@ -0,0 +1,7 @@
+// Copyright (C) 2019-2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package core
+
+// Step 1: Compile TrieStressTest contract to generate ABI and bin files
+//go:generate solc-v0.8.30 -o . --overwrite --abi --bin --pretty-json --evm-version cancun TrieStressTest.sol
