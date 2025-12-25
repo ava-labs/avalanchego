464499dfad3be33062901ef3313eb7c7a64fb3aa
github-action-benchmark <github@users.noreply.github.com>
2025-12-25 15:12:29 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 8ba1d4dc00e82751545dcf244a910e834ec9dc06

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766667676163,
+  "lastUpdate": 1766675549627,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -516,6 +516,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_accept_ms/ggas",
             "value": 70.5831372396765,
+            "unit": "block_accept_ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "sukantoraymond",
+            "username": "sukantoraymond",
+            "email": "rsukanto@umich.edu"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "8ba1d4dc00e82751545dcf244a910e834ec9dc06",
+          "message": "Apply latest tag only when the image tag matches a semantic version (#4469)",
+          "timestamp": "2025-12-22T19:26:29Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/8ba1d4dc00e82751545dcf244a910e834ec9dc06"
+        },
+        "date": 1766675546689,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast - mgas/s",
+            "value": 43.429705773857556,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast - ms/ggas",
+            "value": 23025.71436258609,
+            "unit": "ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast - block_parse_ms/ggas",
+            "value": 69.57513352214895,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast - block_verify_ms/ggas",
+            "value": 22901.148951455347,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast - block_accept_ms/ggas",
+            "value": 52.68008081251034,
             "unit": "block_accept_ms/ggas"
           }
         ]
