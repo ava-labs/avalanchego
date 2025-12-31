a632c4ebd4b932251623ce90c8ea3178c775d702
Austin Larson <austin.larson@avalabs.org>
2025-12-31 09:54:09 -0800

    style: lint

--- firewood/syncer/syncertest/network.go
+++ firewood/syncer/syncertest/network.go
@@ -6,17 +6,19 @@ package syncertest
 import (
 	"testing"
 
+	"github.com/ava-labs/firewood-go-ethhash/ffi"
+
 	"github.com/ava-labs/avalanchego/firewood/syncer"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/network/p2p"
 	"github.com/ava-labs/avalanchego/network/p2p/p2ptest"
 	"github.com/ava-labs/avalanchego/x/sync"
-	"github.com/ava-labs/firewood-go-ethhash/ffi"
 )
 
 func NewTestRangeProofHandler(t *testing.T, db *ffi.Database) *p2p.Client {
 	return p2ptest.NewSelfClient(t, t.Context(), ids.EmptyNodeID, sync.NewGetRangeProofHandler(syncer.NewDatabase(db), syncer.RangeProofMarshaler{}))
 }
+
 func NewTestChangeProofHandler(t *testing.T, db *ffi.Database) *p2p.Client {
 	return p2ptest.NewSelfClient(t, t.Context(), ids.EmptyNodeID, sync.NewGetChangeProofHandler(syncer.NewDatabase(db), syncer.RangeProofMarshaler{}, syncer.ChangeProofMarshaler{}))
 }
