dfb01e09c7ae8e1e74af88faad78500a2a2cb52f
github-action-benchmark <github@users.noreply.github.com>
2026-01-12 22:18:12 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 874bc6c9d3059aa4c87cb86ba51793a9c921191b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768232258117,
+  "lastUpdate": 1768256292485,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -2396,6 +2396,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast - block_accept_ms/ggas",
             "value": 67.42450462488601,
+            "unit": "block_accept_ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Elvis",
+            "username": "Elvis339",
+            "email": "43846394+Elvis339@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "874bc6c9d3059aa4c87cb86ba51793a9c921191b",
+          "message": "fix(c-chain-reexecution): support genesis execution (#4841)",
+          "timestamp": "2026-01-12T19:48:42Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/874bc6c9d3059aa4c87cb86ba51793a9c921191b"
+        },
+        "date": 1768256290249,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-io2-xfast-10ti - mgas/s",
+            "value": 47.422260520768795,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-io2-xfast-10ti - ms/ggas",
+            "value": 21087.14323228108,
+            "unit": "ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-io2-xfast-10ti - block_parse_ms/ggas",
+            "value": 65.71184940204705,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-io2-xfast-10ti - block_verify_ms/ggas",
+            "value": 20969.680016704835,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-io2-xfast-10ti - block_accept_ms/ggas",
+            "value": 50.091118510794914,
             "unit": "block_accept_ms/ggas"
           }
         ]
