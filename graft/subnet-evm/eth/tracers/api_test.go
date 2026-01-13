ec1cf107d83200587f88260a37517fa259d41967
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-01-13 12:41:42 -0500

    fix: apply TraceCall fix in both coreth and subnet-evm (#4846)
    Co-authored-by: yacovm <yacovm@users.noreply.github.com>

--- graft/subnet-evm/eth/tracers/api_test.go
+++ graft/subnet-evm/eth/tracers/api_test.go
@@ -428,6 +428,20 @@ func testTraceCall(t *testing.T, scheme string) {
 		{"pc":0,"op":"NUMBER","gas":24946982,"gasCost":2,"depth":1,"stack":[]},
 		{"pc":1,"op":"STOP","gas":24946980,"gasCost":0,"depth":1,"stack":["0x1337"]}]}`,
 		},
+		// Tests issue #33014 where accessing nil block number override panics.
+		{
+			blockNumber: rpc.BlockNumber(0),
+			call: ethapi.TransactionArgs{
+				From:  &accounts[0].addr,
+				To:    &accounts[1].addr,
+				Value: (*hexutil.Big)(big.NewInt(1000)),
+			},
+			config: &TraceCallConfig{
+				BlockOverrides: &ethapi.BlockOverrides{},
+			},
+			expectErr: nil,
+			expect:    `{"gas":21000,"failed":false,"returnValue":"","structLogs":[]}`,
+		},
 	}
 	for i, testspec := range testSuite {
 		result, err := api.TraceCall(context.Background(), testspec.call, rpc.BlockNumberOrHash{BlockNumber: &testspec.blockNumber}, testspec.config)
