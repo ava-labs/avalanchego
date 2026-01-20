57ddae13721fc6683a807e158f9ccd7caee9a8e0
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-01-20 16:41:21 -0500

    refactor: use root level shellcheck

--- graft/coreth/scripts/shellcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-REPO_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
-
-find "${REPO_ROOT}" -type f -name "*.sh" -print0 | xargs -0 shellcheck "${@}"
