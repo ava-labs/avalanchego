30024ff380802d764964cd164d80ba6347c0f7c6
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-01-21 10:31:01 -0500

    test: explicitly use shanghai

--- graft/coreth/plugin/evm/compile.go
+++ graft/coreth/plugin/evm/compile.go
@@ -3,5 +3,8 @@
 
 package evm
 
+// Using shanghai EVM version is necessary in this case, as more recent solc optimizations change the binary output.
+// See https://github.com/ava-labs/avalanchego/actions/runs/21188583091/job/60949476635?pr=4888 as an example.
+
 // Step 1: Compile ExampleWarp contract to generate ABI and bin files
-//go:generate sh -c "solc -o . --overwrite --abi --bin --pretty-json --base-path ../.. ExampleWarp.sol && rm -f IWarpMessenger.abi IWarpMessenger.bin"
+//go:generate sh -c "solc -o . --overwrite --abi --bin --pretty-json --base-path ../.. --evm-version shanghai ExampleWarp.sol && rm -f IWarpMessenger.abi IWarpMessenger.bin"
