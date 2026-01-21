1d315372c9f642a40ee9dc4aa49a2259f9461435
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-01-21 11:37:13 -0500

    test: test rpc without registration

--- graft/subnet-evm/plugin/evm/libevm.go
+++ graft/subnet-evm/plugin/evm/libevm.go
@@ -3,23 +3,41 @@
 
 package evm
 
-import "github.com/ava-labs/avalanchego/graft/subnet-evm/plugin/evm/extras"
+import (
+	"github.com/ava-labs/libevm/libevm"
+
+	"github.com/ava-labs/avalanchego/graft/subnet-evm/core"
+	"github.com/ava-labs/avalanchego/graft/subnet-evm/params"
+	"github.com/ava-labs/avalanchego/graft/subnet-evm/plugin/evm/customtypes"
+)
 
 // RegisterAllLibEVMExtras is a convenience wrapper for calling
 // [core.RegisterExtras], [customtypes.Register], and [params.RegisterExtras].
 // Together these are necessary and sufficient for configuring libevm for
-// Subnet-EVM behaviour.
+// C-Chain behaviour.
 //
 // It MUST NOT be called more than once and therefore is only allowed to be used
 // in tests and `package main`, to avoid polluting other packages that
 // transitively depend on this one but don't need registration.
 func RegisterAllLibEVMExtras() {
-	extras.RegisterAllLibEVMExtras()
+	core.RegisterExtras()
+	customtypes.Register()
+	params.RegisterExtras()
 }
 
 // WithTempRegisteredLibEVMExtras runs `fn` with temporary registration
 // otherwise equivalent to a call to [RegisterAllLibEVMExtras], but limited to
 // the life of `fn`.
 func WithTempRegisteredLibEVMExtras(fn func() error) error {
-	return extras.WithTempRegisteredLibEVMExtras(fn)
+	return libevm.WithTemporaryExtrasLock(func(lock libevm.ExtrasLock) error {
+		for _, wrap := range []func(libevm.ExtrasLock, func() error) error{
+			core.WithTempRegisteredExtras,
+			customtypes.WithTempRegisteredExtras,
+			params.WithTempRegisteredExtras,
+		} {
+			inner := fn
+			fn = func() error { return wrap(lock, inner) }
+		}
+		return fn()
+	})
 }
