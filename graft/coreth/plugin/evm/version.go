cc5e24172332946484f00d7b19ddd44de7fde37a
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-01-21 10:19:30 -0500

    refactor: share version across codebase

--- graft/coreth/plugin/evm/version.go
+++ graft/coreth/plugin/evm/version.go
@@ -3,16 +3,21 @@
 
 package evm
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ava-labs/avalanchego/version"
+)
 
 var (
 	// GitCommit is set by the build script
 	GitCommit string
-	// Version is the version of Coreth
-	Version string = "v0.15.4"
+	// Version is the version of Coreth, derived from AvalancheGo version
+	Version string
 )
 
 func init() {
+	Version = fmt.Sprintf("v%d.%d.%d", version.Current.Major, version.Current.Minor, version.Current.Patch)
 	if len(GitCommit) != 0 {
 		Version = fmt.Sprintf("%s@%s", Version, GitCommit)
 	}
