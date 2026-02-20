297d98eeff7f31dc18fc41c72ccc027ae2f9b4e2
Austin Larson <austin.larson@avalabs.org>
2026-02-20 15:10:27 -0500

    attempt 11

--- graft/coreth/scripts/build_test.sh
+++ graft/coreth/scripts/build_test.sh
@@ -16,4 +16,4 @@ fi
 
 cd "$REPO_ROOT/graft/coreth"
 # shellcheck disable=SC2046
-GOTRACEBACK=crash go test -shuffle=on ${race:-} -timeout="${TIMEOUT:-900s}" -coverprofile=coverage.out -covermode=atomic "$@" github.com/ava-labs/avalanchego/graft/coreth/core -count 5
+GOTRACEBACK=crash go test -shuffle=on ${race:-} -timeout="${TIMEOUT:-900s}" -coverprofile=coverage.out -covermode=atomic "$@" github.com/ava-labs/avalanchego/graft/coreth/core 
