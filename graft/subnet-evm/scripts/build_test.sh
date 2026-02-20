67c477fa542ff008232059354e07630f162f573c
Austin Larson <austin.larson@avalabs.org>
2026-02-20 16:27:18 -0500

    attempt 12

--- graft/subnet-evm/scripts/build_test.sh
+++ graft/subnet-evm/scripts/build_test.sh
@@ -16,4 +16,4 @@ fi
 
 cd "$REPO_ROOT/graft/subnet-evm"
 # shellcheck disable=SC2046
-GOTRACEBACK=crash go test -shuffle=on ${race:-} -timeout="${TIMEOUT:-900s}" -coverprofile=coverage.out -covermode=atomic "$@" github.com/ava-labs/avalanchego/graft/subnet-evm/core
+go test -shuffle=on ${race:-} -timeout="${TIMEOUT:-900s}" -coverprofile=coverage.out -covermode=atomic "$@" github.com/ava-labs/avalanchego/graft/subnet-evm/core
