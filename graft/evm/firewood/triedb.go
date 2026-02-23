45f45b789890252913dc8796b8acc78ce1377430
Austin Larson <austin.larson@avalabs.org>
2026-02-23 11:08:03 -0500

    attempt 14

--- graft/evm/firewood/triedb.go
+++ graft/evm/firewood/triedb.go
@@ -270,7 +270,7 @@ func (t *TrieDB) Close() error {
 
 	// We must provide a context to close since it may hang while waiting for the finalizers to complete.
 	runtime.GC()
-	time.Sleep(5 * time.Second)
+	time.Sleep(1 * time.Second)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	return t.Firewood.Close(ctx)
