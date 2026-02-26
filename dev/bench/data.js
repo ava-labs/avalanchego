65c430aeaaf0251ec95a05b791a763ea6cb16c88
github-action-benchmark <github@users.noreply.github.com>
2026-02-26 10:43:07 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 078c8f5d85e93bcf75ada6a9af64768f804e4640

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772036096273,
+  "lastUpdate": 1772102587519,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -7895,6 +7895,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-16xlarge-local-ssd - block_accept_ms/ggas",
             "value": 701.5010870474141,
+            "unit": "block_accept_ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "PlatCore",
+            "username": "PlatCore",
+            "email": "240789909+PlatCore@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "078c8f5d85e93bcf75ada6a9af64768f804e4640",
+          "message": "Migrate subnet-evm antithesis builder to the root (#4836)",
+          "timestamp": "2026-02-25T16:48:19Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/078c8f5d85e93bcf75ada6a9af64768f804e4640"
+        },
+        "date": 1772102586578,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - mgas/s",
+            "value": 80.09863089772752,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - ms/ggas",
+            "value": 12484.607898939394,
+            "unit": "ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - block_parse_ms/ggas",
+            "value": 70.50889007347628,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - block_verify_ms/ggas",
+            "value": 11815.973367715795,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - block_accept_ms/ggas",
+            "value": 595.9128418025749,
             "unit": "block_accept_ms/ggas"
           }
         ]
