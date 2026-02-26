03fc03d6e0ab3cbeb6a544d1a96e12f8f99f05f1
github-action-benchmark <github@users.noreply.github.com>
2026-02-26 13:45:13 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for e9f7e75e2b97fd64ef82e2b7f9c1d0c79a5dce8e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772108300593,
+  "lastUpdate": 1772113513586,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -7989,6 +7989,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_accept_ms/ggas",
             "value": 152.94299512839825,
+            "unit": "block_accept_ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ceyhun Onur",
+            "username": "ceyonur",
+            "email": "ceyhun.onur@avalabs.org"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "e9f7e75e2b97fd64ef82e2b7f9c1d0c79a5dce8e",
+          "message": "rename coreth api.md to service.md (#5024)\n\nCo-authored-by: Meaghan FitzGerald <meag.fitz@avalabs.org>",
+          "timestamp": "2026-02-26T10:42:29Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/e9f7e75e2b97fd64ef82e2b7f9c1d0c79a5dce8e"
+        },
+        "date": 1772113512639,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - mgas/s",
+            "value": 58.19156133159226,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - ms/ggas",
+            "value": 17184.62225651091,
+            "unit": "ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_parse_ms/ggas",
+            "value": 69.98416916312739,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_verify_ms/ggas",
+            "value": 17061.051212075225,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_accept_ms/ggas",
+            "value": 51.25683775762165,
             "unit": "block_accept_ms/ggas"
           }
         ]
