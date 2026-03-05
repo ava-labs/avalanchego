80bd80db92c4d563572c22fb2d7de3900bdad4e2
github-action-benchmark <github@users.noreply.github.com>
2026-03-05 13:33:04 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for a5c1d62e7e0f7d69f94cc31c42e38ffb56bcbc96

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772712991644,
+  "lastUpdate": 1772717584467,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -8553,6 +8553,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_accept_ms/ggas",
             "value": 153.0505534706426,
+            "unit": "block_accept_ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Sam Liokumovich",
+            "username": "samliok",
+            "email": "65994425+samliok@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "a5c1d62e7e0f7d69f94cc31c42e38ffb56bcbc96",
+          "message": "Add simplex consensus parameters (#4870)\n\nSigned-off-by: Sam Liokumovich <65994425+samliok@users.noreply.github.com>\nCo-authored-by: yacovm <yacovm@users.noreply.github.com>",
+          "timestamp": "2026-03-04T18:36:18Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/a5c1d62e7e0f7d69f94cc31c42e38ffb56bcbc96"
+        },
+        "date": 1772717583525,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - mgas/s",
+            "value": 60.35898155024332,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - ms/ggas",
+            "value": 16567.542631043096,
+            "unit": "ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_parse_ms/ggas",
+            "value": 66.45628881075895,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_verify_ms/ggas",
+            "value": 16452.806713419483,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_accept_ms/ggas",
+            "value": 46.6064794678166,
             "unit": "block_accept_ms/ggas"
           }
         ]
