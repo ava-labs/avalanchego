fd381140eba4b393a4b697fa585ad959dacbebc1
samliok <samliok@stanford.edu>
2026-03-30 16:18:42 -0400

    reduce diff

--- snow/engine/common/no_ops_handlers.go
+++ snow/engine/common/no_ops_handlers.go
@@ -231,23 +231,6 @@ func (nop *noOpQueryHandler) PushQuery(_ context.Context, nodeID ids.NodeID, req
 	return nil
 }
 
-type noOpSimplexHandler struct {
-	log logging.Logger
-}
-
-func NewNoOpSimplexHandler(log logging.Logger) SimplexHandler {
-	return &noOpSimplexHandler{log: log}
-}
-
-func (nop *noOpSimplexHandler) Simplex(_ context.Context, nodeID ids.NodeID, _ *p2p.Simplex) error {
-	nop.log.Debug("dropping request",
-		zap.String("reason", "unhandled by this gear"),
-		zap.Stringer("messageOp", message.SimplexOp),
-		zap.Stringer("nodeID", nodeID),
-	)
-	return nil
-}
-
 type noOpChitsHandler struct {
 	log logging.Logger
 }
@@ -380,6 +363,23 @@ func (nop *noOpInternalHandler) Notify(_ context.Context, msg Message) error {
 		zap.String("reason", "unhandled by this gear"),
 		zap.Stringer("messageOp", message.NotifyOp),
 		zap.Stringer("message", msg),
+	)
+	return nil
+}
+
+type noOpSimplexHandler struct {
+	log logging.Logger
+}
+
+func NewNoOpSimplexHandler(log logging.Logger) SimplexHandler {
+	return &noOpSimplexHandler{log: log}
+}
+
+func (nop *noOpSimplexHandler) Simplex(_ context.Context, nodeID ids.NodeID, _ *p2p.Simplex) error {
+	nop.log.Debug("dropping request",
+		zap.String("reason", "unhandled by this gear"),
+		zap.Stringer("messageOp", message.SimplexOp),
+		zap.Stringer("nodeID", nodeID),
 	)
 	return nil
 }
