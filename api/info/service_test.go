1e3a0e4f97ad3fdb4a9371f6903e4f04b788c0b1
samliok <samliok@stanford.edu>
2026-04-07 15:13:56 -0400

    Add back TestGetVMsGetAliasesFails using injected failing aliaser

--- api/info/service_test.go
+++ api/info/service_test.go
@@ -4,6 +4,7 @@
 package info
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,6 +13,8 @@ import (
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/vms"
 )
+
+var errTest = errors.New("non-nil error")
 
 type testFactory struct{}
 
@@ -22,7 +25,7 @@ type getVMsTest struct {
 	vmManager *vms.Manager
 }
 
-func initGetVMsTest(_ *testing.T) *getVMsTest {
+func initGetVMsTest(*testing.T) *getVMsTest {
 	vmManager := vms.NewManager(logging.NoLog{}, ids.NewAliaser())
 	return &getVMsTest{
 		info: &Info{
@@ -61,4 +64,35 @@ func TestGetVMsSuccess(t *testing.T) {
 	reply := GetVMsReply{}
 	require.NoError(resources.info.GetVMs(nil, nil, &reply))
 	require.Equal(expectedVMRegistry, reply.VMs)
+}
+
+// failingAliaser wraps an Aliaser and returns an error from Aliases.
+type failingAliaser struct {
+	ids.Aliaser
+	err error
+}
+
+func (f *failingAliaser) Aliases(ids.ID) ([]string, error) {
+	return nil, f.err
+}
+
+// Tests GetVMs if we can't get our vm aliases.
+func TestGetVMsGetAliasesFails(t *testing.T) {
+	resources := initGetVMsTest(t)
+
+	id1 := ids.GenerateTestID()
+	id2 := ids.GenerateTestID()
+
+	require.NoError(t, resources.vmManager.RegisterFactory(t.Context(), id1, testFactory{}))
+	require.NoError(t, resources.vmManager.RegisterFactory(t.Context(), id2, testFactory{}))
+
+	// Inject a failing aliaser so Aliases returns an error
+	resources.vmManager.Aliaser = &failingAliaser{
+		Aliaser: resources.vmManager.Aliaser,
+		err:     errTest,
+	}
+
+	reply := GetVMsReply{}
+	err := resources.info.GetVMs(nil, nil, &reply)
+	require.ErrorIs(t, err, errTest)
 }
