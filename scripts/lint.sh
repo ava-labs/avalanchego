dad0ce8720591e7614a218b475c95cb0be0218d7
maru <maru.newby@avalabs.org>
2026-04-07 12:58:06 -0400

    lint: exclude git metadata from source scans

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -9,6 +9,7 @@ fi
 
 # Root-level directories to exclude from linting
 EXCLUDE_DIRS=(
+  .git    # Git metadata and remote refs are not source files
   graft    # Grafted modules have their own lint config
   .direnv  # direnv cache
   .idea    # GoLand
