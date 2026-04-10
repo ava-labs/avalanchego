3d833bbdb430fa75718bfa3934d334ac33792b3f
Stephen Buttolph <stephen@avalabs.org>
2026-04-10 09:57:12 -0400

    remove unimplemented

--- vms/transitionvm/unimplemented.go
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2019, Ava Labs, Inc. All rights reserved.
-// See the file LICENSE for licensing terms.
-
-package transitionvm
-
-import "errors"
-
-var errUnimplemented = errors.New("unimplemented")
