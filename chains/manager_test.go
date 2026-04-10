173da686c3464599a8c80a34759f95d6c3339221
samliok <samliok@stanford.edu>
2026-04-10 17:07:11 -0400

    no more mocks

--- chains/manager_test.go
+++ chains/manager_test.go
@@ -4,25 +4,17 @@
 package chains
 
 import (
-	"context"
 	"errors"
 	"testing"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/mock/gomock"
 
 	"github.com/ava-labs/avalanchego/api/health"
 	"github.com/ava-labs/avalanchego/api/metrics"
-	"github.com/ava-labs/avalanchego/database"
 	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/snow"
 	"github.com/ava-labs/avalanchego/snow/consensus/simplex"
-	"github.com/ava-labs/avalanchego/snow/consensus/snowman"
-	"github.com/ava-labs/avalanchego/snow/consensus/snowman/snowmantest"
-	"github.com/ava-labs/avalanchego/snow/engine/common"
-	"github.com/ava-labs/avalanchego/snow/engine/snowman/block/blocktest"
 	"github.com/ava-labs/avalanchego/snow/networking/benchlist"
 	"github.com/ava-labs/avalanchego/snow/networking/router"
 	"github.com/ava-labs/avalanchego/snow/networking/timeout"
@@ -37,7 +29,6 @@ import (
 	"github.com/ava-labs/avalanchego/utils/set"
 	"github.com/ava-labs/avalanchego/utils/timer"
 	"github.com/ava-labs/avalanchego/vms"
-	"github.com/ava-labs/avalanchego/vms/vmsmock"
 )
 
 // startChainCreatorNoPChain is used for testing to bypass setting up the pchain
@@ -88,36 +79,6 @@ func newTimeoutManager(t *testing.T) *timeout.Manager {
 	require.NoError(err)
 
 	return timeoutManager
-}
-
-// newMockVMManager returns a VM manager that always returns a mock VM with
-// only the genesis block defined.
-func newMockVMManager(t *testing.T) vms.Manager {
-	ctrl := gomock.NewController(t)
-	vm := &blocktest.VM{}
-	vm.InitializeF = func(_ context.Context, _ *snow.Context, _ database.Database, _ []byte, _ []byte, _ []byte, _ []*common.Fx, _ common.AppSender) error {
-		return nil
-	}
-
-	vm.GetBlockIDAtHeightF = func(_ context.Context, height uint64) (ids.ID, error) {
-		require.Zero(t, height)
-		return snowmantest.GenesisID, nil
-	}
-	vm.GetBlockF = func(_ context.Context, blkID ids.ID) (snowman.Block, error) {
-		require.Equal(t, snowmantest.GenesisID, blkID)
-		return snowmantest.Genesis, nil
-	}
-	vm.LastAcceptedF = func(_ context.Context) (ids.ID, error) {
-		return snowmantest.GenesisID, nil
-	}
-
-	factory := vmsmock.NewFactory(ctrl)
-	factory.EXPECT().New(gomock.Any()).Return(vm, nil).AnyTimes()
-
-	manager := vmsmock.NewManager(ctrl)
-	manager.EXPECT().GetFactory(gomock.Any()).Return(factory, nil).AnyTimes()
-
-	return manager
 }
 
 func newTestSubnets(t *testing.T, subnetID ids.ID) *Subnets {
@@ -181,7 +142,7 @@ func TestCreateSimplexChain(t *testing.T) {
 			LoggerName: "chain_logger",
 		}),
 
-		VMManager:  newMockVMManager(t),
+		VMManager:  *vms.NewManager(logging.NoLog{}, ids.NewAliaser()),
 		Validators: validators,
 
 		// For handler initialization
