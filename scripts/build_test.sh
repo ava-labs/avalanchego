6695fb2723878405e009b8cc454a42d41ecb4df2
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-04-27 09:51:02 +0000

    chore: increase fuzz timeout to 5m

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -30,4 +30,4 @@ fi
 TEST_TARGETS="$(eval "go list ./... ${EXCLUDED_TARGETS}")"
 
 # shellcheck disable=SC2086
-go test -tags test ${shuffle:-} ${race:-} -timeout="${TIMEOUT:-120s}" -coverprofile="coverage.out" -covermode="atomic" ${TEST_TARGETS}
+go test -tags test ${shuffle:-} ${race:-} -timeout="${TIMEOUT:-5m}" -coverprofile="coverage.out" -covermode="atomic" ${TEST_TARGETS}
