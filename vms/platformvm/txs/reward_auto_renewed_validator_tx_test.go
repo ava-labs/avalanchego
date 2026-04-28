41feac1a127a99abf337fd6d1635f4f84ff921d6
razvan.angheluta <razvan@angheluta.dev>
2026-04-28 16:29:19 +0300

    Add GetInputOutputs coverage for auto-renewed validator transactions and some nits

--- vms/platformvm/txs/reward_auto_renewed_validator_tx_test.go
+++ vms/platformvm/txs/reward_auto_renewed_validator_tx_test.go
@@ -7,70 +7,61 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"go.uber.org/mock/gomock"
 
 	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/snow"
 )
 
 func TestRewardAutoRenewedValidatorTxSyntacticVerify(t *testing.T) {
 	type test struct {
-		name   string
-		txFunc func(*gomock.Controller) *RewardAutoRenewedValidatorTx
-		err    error
-	}
-
-	ctx := &snow.Context{
-		ChainID:   ids.GenerateTestID(),
-		NetworkID: uint32(1337),
+		name    string
+		txFunc  func() *RewardAutoRenewedValidatorTx
+		wantErr error
 	}
 
 	tests := []test{
 		{
 			name: "nil tx",
-			txFunc: func(*gomock.Controller) *RewardAutoRenewedValidatorTx {
+			txFunc: func() *RewardAutoRenewedValidatorTx {
 				return nil
 			},
-			err: ErrNilTx,
+			wantErr: ErrNilTx,
 		},
 		{
 			name: "missing timestamp",
-			txFunc: func(*gomock.Controller) *RewardAutoRenewedValidatorTx {
+			txFunc: func() *RewardAutoRenewedValidatorTx {
 				return &RewardAutoRenewedValidatorTx{
 					TxID:      ids.GenerateTestID(),
 					Timestamp: 0,
 				}
 			},
-			err: errMissingTimestamp,
+			wantErr: errMissingTimestamp,
 		},
 		{
 			name: "missing tx id",
-			txFunc: func(*gomock.Controller) *RewardAutoRenewedValidatorTx {
+			txFunc: func() *RewardAutoRenewedValidatorTx {
 				return &RewardAutoRenewedValidatorTx{
 					Timestamp: 1,
 				}
 			},
-			err: errMissingTxID,
+			wantErr: errMissingTxID,
 		},
 		{
 			name: "valid auto-renewed validator",
-			txFunc: func(*gomock.Controller) *RewardAutoRenewedValidatorTx {
+			txFunc: func() *RewardAutoRenewedValidatorTx {
 				return &RewardAutoRenewedValidatorTx{
 					TxID:      ids.GenerateTestID(),
 					Timestamp: 1,
 				}
 			},
-			err: nil,
+			wantErr: nil,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-
-			tx := tt.txFunc(ctrl)
-			err := tx.SyntacticVerify(ctx)
-			require.ErrorIs(t, err, tt.err)
+			tx := tt.txFunc()
+			gotErr := tx.SyntacticVerify(nil)
+			require.ErrorIs(t, gotErr, tt.wantErr)
 		})
 	}
 }
