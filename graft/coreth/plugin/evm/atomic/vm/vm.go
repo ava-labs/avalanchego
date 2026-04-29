f7be4b981f70a1a09a5f1bca139e0f62f954fcc9
Rodrigo Villar <rodrigo.villar@avalabs.org>
2026-04-29 08:43:24 -0400

    chore: update comment

--- graft/coreth/plugin/evm/atomic/vm/vm.go
+++ graft/coreth/plugin/evm/atomic/vm/vm.go
@@ -641,7 +641,7 @@ func (vm *VM) onExtraStateChange(block *types.Block, parent *types.Header, state
 
 	// If [atomicBackend] is nil, the VM is still initializing and is reprocessing accepted blocks.
 	if vm.AtomicBackend != nil {
-		// If we are reprocessing state to serve a request, we don't need to verify either.
+		// Only verify and insert when processing at the chain tip; skip during historical replay.
 		if lastAccepted := vm.LastAcceptedExtendedBlock(); lastAccepted != nil && lastAccepted.Height() <= block.NumberU64() {
 			if vm.AtomicBackend.IsBonus(block.NumberU64(), block.Hash()) {
 				log.Info("skipping atomic tx verification on bonus block", "block", block.Hash())
