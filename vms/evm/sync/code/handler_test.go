9b1bc07604049082c2093230bcbb753f27a4191b
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2026-05-20 15:16:58 +0300

    feat(vms/evm/sync/code): add code-by-hash request handler
    - Serves code-by-hash requests at `p2p.EVMCodeRequestHandlerID`.
    - `code.Handler` / `code.Responder` bind `syncpb.GetCodeRequest` and
      `syncpb.GetCodeResponse` to the generic shell.
    - `code.Stats` + `code.NoopStats` for the metrics surface.
    - `synctest.CodeRecorder` for assertions in tests.
    
    resolves #5401
    
    Signed-off-by: Tsvetan Dimitrov (tsvetan.dimitrov@avalabs.org)

---
+++ vms/evm/sync/code/handler_test.go
@@ -0,0 +1,165 @@
+// Copyright (C) 2019, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package code_test
+
+import (
+	"crypto/rand"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/ava-labs/libevm/common"
+	"github.com/ava-labs/libevm/core/rawdb"
+	"github.com/ava-labs/libevm/crypto"
+	"github.com/ava-labs/libevm/ethdb/memorydb"
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/testing/protocmp"
+
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/vms/evm/sync/code"
+	"github.com/ava-labs/avalanchego/vms/evm/sync/synctest"
+
+	syncpb "github.com/ava-labs/avalanchego/proto/pb/sync"
+)
+
+func TestHandler_RoundTrip(t *testing.T) {
+	wantResp := &syncpb.GetCodeResponse{
+		Data: [][]byte{{0xaa, 0xbb}, {0xcc, 0xdd}},
+	}
+	responder := &synctest.FakeCodeResponder{Resp: wantResp}
+	h := code.NewHandler(responder)
+
+	req := &syncpb.GetCodeRequest{
+		Hashes: [][]byte{{0x01}, {0x02}},
+	}
+	respBytes, appErr := h.AppRequest(t.Context(), ids.GenerateTestNodeID(), time.Time{}, synctest.MustMarshal(t, req))
+	require.Nil(t, appErr)
+
+	got := &syncpb.GetCodeResponse{}
+	require.NoError(t, proto.Unmarshal(respBytes, got))
+	require.Empty(t, cmp.Diff(wantResp, got, protocmp.Transform()))
+	require.Empty(t, cmp.Diff(req, responder.GotReq, protocmp.Transform()))
+}
+
+func TestHandler_FailurePaths(t *testing.T) {
+	tests := []struct {
+		name       string
+		resp       *syncpb.GetCodeResponse
+		err        error
+		wantAppErr bool
+	}{
+		{
+			name: "inner returns nil drops the response",
+		},
+		{
+			name:       "inner error surfaces as AppError",
+			err:        errors.New("boom"),
+			wantAppErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			responder := &synctest.FakeCodeResponder{Resp: tt.resp, Err: tt.err}
+			h := code.NewHandler(responder)
+
+			respBytes, appErr := h.AppRequest(t.Context(), ids.GenerateTestNodeID(), time.Time{}, synctest.MustMarshal(t, &syncpb.GetCodeRequest{}))
+			if tt.wantAppErr {
+				require.NotNil(t, appErr)
+			} else {
+				require.Nil(t, appErr)
+			}
+			require.Empty(t, respBytes)
+		})
+	}
+}
+
+func TestHandler_MalformedRequestBytes(t *testing.T) {
+	responder := &synctest.FakeCodeResponder{}
+	h := code.NewHandler(responder)
+
+	respBytes, appErr := h.AppRequest(t.Context(), ids.GenerateTestNodeID(), time.Time{}, []byte{0xff, 0xff})
+	require.Nil(t, respBytes)
+	require.NotNil(t, appErr)
+	require.Nil(t, responder.GotReq, "responder must not be invoked on malformed request")
+}
+
+func TestResponder(t *testing.T) {
+	t.Parallel()
+
+	db := memorydb.New()
+	codeBytes := []byte("contract bytecode")
+	codeHash := crypto.Keccak256Hash(codeBytes)
+	rawdb.WriteCode(db, codeHash, codeBytes)
+
+	// A second code blob to test multi-hash requests.
+	other := make([]byte, 256)
+	_, err := rand.Read(other)
+	require.NoError(t, err)
+	otherHash := crypto.Keccak256Hash(other)
+	rawdb.WriteCode(db, otherHash, other)
+
+	tests := []struct {
+		name      string
+		hashes    []common.Hash
+		wantData  [][]byte
+		wantDrop  bool
+		wantStats synctest.CodeRecorder
+	}{
+		{
+			name:      "single hash",
+			hashes:    []common.Hash{codeHash},
+			wantData:  [][]byte{codeBytes},
+			wantStats: synctest.CodeRecorder{Requests: 1, CodeBytesReturned: uint32(len(codeBytes))},
+		},
+		{
+			name:      "multiple hashes preserve order",
+			hashes:    []common.Hash{codeHash, otherHash},
+			wantData:  [][]byte{codeBytes, other},
+			wantStats: synctest.CodeRecorder{Requests: 1, CodeBytesReturned: uint32(len(codeBytes) + len(other))},
+		},
+		{
+			name:      "missing hash drops",
+			hashes:    []common.Hash{{0xde, 0xad}},
+			wantDrop:  true,
+			wantStats: synctest.CodeRecorder{Requests: 1, MissingHash: 1},
+		},
+		{
+			name:      "duplicate hashes drop",
+			hashes:    []common.Hash{codeHash, codeHash},
+			wantDrop:  true,
+			wantStats: synctest.CodeRecorder{Requests: 1, DuplicateHashes: 1},
+		},
+		{
+			name:      "too many hashes drops",
+			hashes:    []common.Hash{{1}, {2}, {3}, {4}, {5}, {6}},
+			wantDrop:  true,
+			wantStats: synctest.CodeRecorder{Requests: 1, TooManyHashes: 1},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			stats := &synctest.CodeRecorder{}
+			r := code.NewResponder(db, stats)
+
+			rawHashes := make([][]byte, len(tt.hashes))
+			for i, h := range tt.hashes {
+				rawHashes[i] = h.Bytes()
+			}
+			resp, err := r.Respond(t.Context(), ids.GenerateTestNodeID(), &syncpb.GetCodeRequest{Hashes: rawHashes})
+			require.NoError(t, err)
+
+			if tt.wantDrop {
+				require.Nil(t, resp)
+			} else {
+				require.NotNil(t, resp)
+				require.Equal(t, tt.wantData, resp.Data)
+			}
+			require.Equal(t, tt.wantStats, *stats)
+		})
+	}
+}
---
+++ vms/evm/sync/synctest/stats.go
@@ -0,0 +1,22 @@
+// Copyright (C) 2019, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package synctest
+
+import "github.com/ava-labs/avalanchego/vms/evm/sync/code"
+
+// CodeRecorder counts [code.Stats] invocations.
+type CodeRecorder struct {
+	code.NoopStats
+	Requests          uint32
+	MissingHash       uint32
+	TooManyHashes     uint32
+	DuplicateHashes   uint32
+	CodeBytesReturned uint32
+}
+
+func (s *CodeRecorder) IncCodeRequest()                  { s.Requests++ }
+func (s *CodeRecorder) IncMissingCodeHash()              { s.MissingHash++ }
+func (s *CodeRecorder) IncTooManyHashesRequested()       { s.TooManyHashes++ }
+func (s *CodeRecorder) IncDuplicateHashesRequested()     { s.DuplicateHashes++ }
+func (s *CodeRecorder) UpdateCodeBytesReturned(b uint32) { s.CodeBytesReturned += b }
