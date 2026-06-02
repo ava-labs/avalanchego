64cbae5bd6ad79a23678f4880f7691bca21ff99d
Stephen Buttolph <stephen@avalabs.org>
2026-06-02 15:59:55 -0400

    Add push after pull test

--- vms/saevm/cchain/gossip_test.go
+++ vms/saevm/cchain/gossip_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/snow/engine/common"
 	"github.com/ava-labs/avalanchego/utils/bloom"
 	"github.com/ava-labs/avalanchego/utils/set"
 	"github.com/ava-labs/avalanchego/vms/saevm/cchain/tx"
@@ -27,6 +28,17 @@ func (s *SUT) assertTxBloomContains(tb testing.TB, txIDs ...ids.ID) {
 	filter, salt := s.gossipSet.BloomFilter()
 	for i, txID := range txIDs {
 		assert.Truef(tb, bloom.Contains(filter, txID[:], salt[:]), "bloom filter should contain %s (%d)", txID, i)
+	}
+}
+
+// assertTxBloomNotContains asserts that the transaction bloom does not contain
+// the given transaction IDs.
+func (s *SUT) assertTxBloomNotContains(tb testing.TB, txIDs ...ids.ID) {
+	tb.Helper()
+
+	filter, salt := s.gossipSet.BloomFilter()
+	for i, txID := range txIDs {
+		assert.Falsef(tb, bloom.Contains(filter, txID[:], salt[:]), "bloom filter should not contain %s (%d)", txID, i)
 	}
 }
 
@@ -81,6 +93,52 @@ func TestPullGossip(t *testing.T) {
 	stx := w.newMinimalTx(t)
 	require.NoErrorf(t, api.IssueTx(apiCtx, stx), "%T.IssueTx()", api.Client)
 	api.assertTxBloomContains(t, stx.ID())
+
+	blk := vdrB.runConsensusLoop(vdrBCtx, t)
+	if diff := cmp.Diff([]*tx.Tx{stx}, blockTxs(t, blk), txtest.CmpOpt()); diff != "" {
+		t.Errorf("%T built by vdrB after gossip (-want +got):\n%s", blk, diff)
+	}
+	vdrA.assertTxBloomContains(t, stx.ID())
+}
+
+// TestPushGossipAfterPullGossip verifies that a validator which previously
+// received a cross-chain transaction via pull gossip will share it via push
+// gossip to another connected validator.
+//
+// The API node is only transitively connected to vdrB, and vdrB doesn't
+// consider vdrA a validator, so vdrB can only learn about the transaction if
+// vdrA decides to push it.
+func TestPushGossipAfterPullGossip(t *testing.T) {
+	var (
+		sk        = txtest.NewKey(t)
+		withAlloc = options.Func[sutConfig](func(c *sutConfig) {
+			c.genesis.Alloc = saetest.MaxAllocFor(sk.EthAddress())
+		})
+		vdrIDA = ids.GenerateTestNodeID()
+		vdrIDB = ids.GenerateTestNodeID()
+		vdrs   = set.Of(vdrIDA, vdrIDB)
+	)
+	apiCtx, api := newSUT(t, withAlloc, withValidators(vdrs))
+	vdrACtx, vdrA := newSUT(t, withAlloc, withNodeID(vdrIDA), withValidators(vdrs))
+	vdrBCtx, vdrB := newSUT(t, withAlloc, withNodeID(vdrIDB)) // vdrB doesn't consider vdrA a validator
+	saetest.Connect(t, api, vdrA)
+	saetest.Connect(t, vdrA, vdrB)
+
+	w := newWallet(sk, api.ctx, api.Client)
+	stx := w.newMinimalTx(t)
+	require.NoErrorf(t, api.IssueTx(apiCtx, stx), "%T.IssueTx()", api.Client)
+	api.assertTxBloomContains(t, stx.ID())
+
+	// Wait for vdrA to learn about stx via pull gossip.
+	e, err := vdrA.WaitForEvent(vdrACtx)
+	require.NoErrorf(t, err, "%T.WaitForEvent()", vdrA.VM)
+	assert.Equalf(t, common.PendingTxs, e, "%T.WaitForEvent() event", vdrA.VM)
+	vdrA.assertTxBloomContains(t, stx.ID())
+	vdrB.assertTxBloomNotContains(t, stx.ID())
+
+	// Even though the validator learned about stx via pull gossip, they should
+	// still push gossip stx to vdrB if they see it over the API.
+	require.NoErrorf(t, vdrA.IssueTx(vdrACtx, stx), "%T.IssueTx()", vdrA.VM)
 
 	blk := vdrB.runConsensusLoop(vdrBCtx, t)
 	if diff := cmp.Diff([]*tx.Tx{stx}, blockTxs(t, blk), txtest.CmpOpt()); diff != "" {
