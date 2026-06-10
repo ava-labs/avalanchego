3a5cba4a618c0f6639f8bafa7b29f916120588b6
Austin Larson <78000745+alarso16@users.noreply.github.com>
2026-06-10 17:08:49 -0400

    refactor(gastime): Accept baseFee in `New()` (#5485)
    Co-authored-by: Arran Schlosberg <me@arranschlosberg.com>

--- vms/saevm/sae/blocks.go
+++ vms/saevm/sae/blocks.go
@@ -226,10 +226,7 @@ func (vm *VM) settledBlockFromDB(db ethdb.Reader, hash common.Hash, num uint64) 
 	if err != nil {
 		return nil, err
 	}
-	// Excess is only used for executing the next block, which can never
-	// be the case if `b` isn't actually the last synchronous block, so
-	// passing the same value for all is OK.
-	if err := b.MarkSynchronous(vm.hooks, vm.db, vm.xdb, vm.config.ExcessAfterLastSynchronous); err != nil {
+	if err := b.MarkSynchronous(vm.hooks, vm.db, vm.xdb); err != nil {
 		return nil, err
 	}
 	return b, nil
