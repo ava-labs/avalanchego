22a52f60113f3a21837bb6bceb3cca179fcf30ca
Bora Erinc <bora.erinc@avalabs.org>
2026-06-17 11:02:15 -0400

    lint fixes

--- graft/subnet-evm/sidecar/policy_test.go
+++ graft/subnet-evm/sidecar/policy_test.go
@@ -24,8 +24,8 @@ func (s *stubRule) Validate(_ context.Context, _ *external.ExternalEvent) (bool,
 }
 
 var (
-	pass         = &stubRule{pass: true}
-	fail         = &stubRule{pass: false}
+	pass          = &stubRule{pass: true}
+	fail          = &stubRule{pass: false}
 	errRPCTimeout = errors.New("rpc timeout")
 	infraErr      = &stubRule{err: errRPCTimeout}
 )
