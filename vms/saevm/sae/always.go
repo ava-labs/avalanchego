db7d9c5460c935b4a62a05d1772b17865fcdd276
Austin Larson <austin.larson@avalabs.org>
2026-06-18 15:37:36 -0400

    remove last synchronous from recovery

--- vms/saevm/sae/always.go
+++ vms/saevm/sae/always.go
@@ -9,6 +9,8 @@ import (
 	"fmt"
 
 	"github.com/ava-labs/libevm/core"
+	"github.com/ava-labs/libevm/core/rawdb"
+	"github.com/ava-labs/libevm/ethdb"
 	"github.com/ava-labs/libevm/triedb"
 
 	"github.com/ava-labs/avalanchego/database"
@@ -17,6 +19,8 @@ import (
 	"github.com/ava-labs/avalanchego/vms/saevm/adaptor"
 	"github.com/ava-labs/avalanchego/vms/saevm/blocks"
 	"github.com/ava-labs/avalanchego/vms/saevm/hook"
+
+	ethcommon "github.com/ava-labs/libevm/common"
 )
 
 var _ adaptor.ChainVM[*blocks.Block] = (*SinceGenesis[hook.Transaction])(nil)
@@ -58,17 +62,28 @@ func (vm *SinceGenesis[_]) Initialize(
 	if err := json.Unmarshal(genesisBytes, genesis); err != nil {
 		return fmt.Errorf("json.Unmarshal(%T): %v", genesis, err)
 	}
-	config, _, err := core.SetupGenesisBlock(db, tdb, genesis)
+	config, hash, err := core.SetupGenesisBlock(db, tdb, genesis)
 	if err != nil {
 		return fmt.Errorf("core.SetupGenesisBlock(...): %v", err)
 	}
+	canonicaliseLastSynchronous(db, hash)
 
-	inner, err := NewVM(ctx, vm.hooks, vm.config, snowCtx, config, db, genesis.ToBlock(), appSender)
+	inner, err := NewVM(ctx, vm.hooks, vm.config, snowCtx, config, db, appSender)
 	if err != nil {
 		return err
 	}
 	vm.VM = inner
 	return nil
+}
+
+// canonicaliseLastSynchronous writes the genesis block's hash
+// as finalized in the case there wasn't anything already written.
+func canonicaliseLastSynchronous(db ethdb.Database, hash ethcommon.Hash) {
+	// If any other block has been accepted then the last synchronous block
+	// must have been canonicalised in a previous initialisation.
+	if rawdb.ReadFinalizedBlockHash(db) == (ethcommon.Hash{}) {
+		rawdb.WriteFinalizedBlockHash(db, hash)
+	}
 }
 
 // Shutdown gracefully closes the VM.
