db940a98e9c97d26b55864836534c7457b455269
Stephen Buttolph <stephen@avalabs.org>
2026-06-19 12:53:30 -0400

    wip

---
+++ vms/saevm/cchain/factory.go
@@ -0,0 +1,24 @@
+// Copyright (C) 2019, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package cchain
+
+import (
+	"time"
+
+	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/ava-labs/avalanchego/vms"
+	"github.com/ava-labs/avalanchego/vms/saevm/adaptor"
+)
+
+var _ vms.Factory = (*Factory)(nil)
+
+type Factory struct{}
+
+func (*Factory) New(logger logging.Logger) (interface{}, error) {
+	logger.Info("Creating new SAE VM")
+	return adaptor.Convert(&VM{
+		pullGossipPeriod: time.Second,
+		pushGossipPeriod: 100 * time.Millisecond,
+	}), nil
+}
