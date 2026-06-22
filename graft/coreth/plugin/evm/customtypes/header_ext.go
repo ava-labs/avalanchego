9632f65a3e60f2e191567a811d264c258e486fe1
Stephen Buttolph <stephen@avalabs.org>
2026-06-22 14:49:36 -0400

    feat(saevm): Update libevm to support libevm APIs

--- graft/coreth/plugin/evm/customtypes/header_ext.go
+++ graft/coreth/plugin/evm/customtypes/header_ext.go
@@ -127,6 +127,22 @@ func (h *HeaderExtra) PostCopy(dst *ethtypes.Header) {
 	SetHeaderExtra(dst, cp)
 }
 
+func (h *HeaderExtra) PostRPCMarshal(_ *ethtypes.Header, m map[string]any) {
+	m["extDataHash"] = h.ExtDataHash
+	if h.ExtDataGasUsed != nil {
+		m["extDataGasUsed"] = (*hexutil.Big)(h.ExtDataGasUsed)
+	}
+	if h.BlockGasCost != nil {
+		m["blockGasCost"] = (*hexutil.Big)(h.BlockGasCost)
+	}
+	if h.TimeMilliseconds != nil {
+		m["timestampMilliseconds"] = hexutil.Uint64(*h.TimeMilliseconds)
+	}
+	if h.MinDelayExcess != nil {
+		m["minDelayExcess"] = hexutil.Uint64(*h.MinDelayExcess)
+	}
+}
+
 func (h *HeaderSerializable) updateFromEth(eth *ethtypes.Header) {
 	h.ParentHash = eth.ParentHash
 	h.UncleHash = eth.UncleHash
