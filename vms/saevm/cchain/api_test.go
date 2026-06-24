9922ceff325db644dce31ced56b1adcce4df5923
Stephen Buttolph <stephen@avalabs.org>
2026-06-24 10:26:55 -0400

    test

--- vms/saevm/cchain/api_test.go
+++ vms/saevm/cchain/api_test.go
@@ -173,14 +173,24 @@ func TestRPCExtras(t *testing.T) {
 	require.NotNilf(t, extra.BlockGasCost, "%T.BlockGasCost", extra)
 	require.NotNilf(t, extra.TimeMilliseconds, "%T.TimeMilliseconds", extra)
 	require.NotNilf(t, extra.MinDelayExcess, "%T.MinDelayExcess", extra)
+	require.NotNilf(t, extra.TargetExponent, "%T.TargetExponent", extra)
 	require.NotNilf(t, extra.MinPriceExponent, "%T.MinPriceExponent", extra)
+	require.NotNilf(t, extra.SettledHeight, "%T.SettledHeight", extra)
+	require.NotNilf(t, extra.SettledGasUnix, "%T.SettledGasUnix", extra)
+	require.NotNilf(t, extra.SettledGasNumerator, "%T.SettledGasNumerator", extra)
+	require.NotNilf(t, extra.SettledExcess, "%T.SettledExcess", extra)
 	wantHeaderExtras := map[string]string{
 		"extDataHash":           extra.ExtDataHash.Hex(),
 		"extDataGasUsed":        hexutil.EncodeBig(extra.ExtDataGasUsed),
 		"blockGasCost":          hexutil.EncodeBig(extra.BlockGasCost),
 		"timestampMilliseconds": hexutil.EncodeUint64(*extra.TimeMilliseconds),
 		"minDelayExcess":        hexutil.EncodeUint64(uint64(*extra.MinDelayExcess)),
+		"targetExponent":        hexutil.EncodeUint64(uint64(*extra.TargetExponent)),
 		"minPriceExponent":      hexutil.EncodeUint64(uint64(*extra.MinPriceExponent)),
+		"settledHeight":         hexutil.EncodeUint64(*extra.SettledHeight),
+		"settledGasUnix":        hexutil.EncodeUint64(*extra.SettledGasUnix),
+		"settledGasNumerator":   hexutil.EncodeUint64(uint64(*extra.SettledGasNumerator)),
+		"settledExcess":         hexutil.EncodeUint64(uint64(*extra.SettledExcess)),
 	}
 	numHeaderExtras := reflect.TypeFor[customtypes.HeaderExtra]().NumField()
 	require.Lenf(t, wantHeaderExtras, numHeaderExtras, "%T field count", customtypes.HeaderExtra{})
