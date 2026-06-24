2d7417e721ff183414a1adfe5ac59d753dbc9476
Stephen Buttolph <stephen@avalabs.org>
2026-06-24 13:41:01 -0400

    nit

--- vms/transitionvm/vm_network.go
+++ vms/transitionvm/vm_network.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow/engine/common"
 	"github.com/ava-labs/avalanchego/snow/validators"
+	"github.com/ava-labs/avalanchego/utils/set"
 	"github.com/ava-labs/avalanchego/version"
 )
 
@@ -42,6 +43,49 @@ func (c *connections) reconnect(ctx context.Context, connector validators.Connec
 		}
 	}
 	return nil
+}
+
+var _ common.AppSender = (*sender)(nil)
+
+//nolint:unused // False positive
+type request struct {
+	nodeID    ids.NodeID
+	requestID uint32
+}
+
+type requests struct {
+	lock sync.Mutex
+	set  set.Set[request]
+}
+
+func (r *requests) add(nodeIDs set.Set[ids.NodeID], requestID uint32) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	for nodeID := range nodeIDs {
+		r.set.Add(request{nodeID, requestID})
+	}
+}
+
+func (r *requests) remove(nodeID ids.NodeID, requestID uint32) bool {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	req := request{nodeID, requestID}
+	had := r.set.Contains(req)
+	r.set.Remove(req)
+	return had
+}
+
+type sender struct {
+	common.AppSender
+
+	requests *requests
+}
+
+func (s *sender) SendAppRequest(ctx context.Context, nodeIDs set.Set[ids.NodeID], requestID uint32, appRequestBytes []byte) error {
+	s.requests.add(nodeIDs, requestID)
+	return s.AppSender.SendAppRequest(ctx, nodeIDs, requestID, appRequestBytes)
 }
 
 func (v *VM) Connected(ctx context.Context, nodeID ids.NodeID, version *version.Application) error {
