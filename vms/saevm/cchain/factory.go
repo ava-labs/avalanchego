ea34d6a66c85b6bc62401794e6165e304c9735aa
Stephen Buttolph <stephen@avalabs.org>
2026-06-26 16:23:00 -0400

    Add SAE factory with state-sync stubs

---
+++ vms/saevm/cchain/factory.go
@@ -0,0 +1,36 @@
+// Copyright (C) 2019, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package cchain
+
+import (
+	"time"
+
+	"github.com/ava-labs/avalanchego/snow/engine/snowman/block"
+	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/ava-labs/avalanchego/vms"
+	"github.com/ava-labs/avalanchego/vms/saevm/adaptor"
+)
+
+var _ vms.Factory = (*Factory)(nil)
+
+// Factory creates new C-Chain VMs.
+type Factory struct{}
+
+// New creates a new C-Chain VM.
+func (*Factory) New(logger logging.Logger) (interface{}, error) {
+	logger.Info("Creating new SAE VM")
+	vm := &VM{
+		pullGossipPeriod: time.Second,
+		pushGossipPeriod: 100 * time.Millisecond,
+		now:              time.Now,
+	}
+	type fullVM struct {
+		adaptor.ChainVMWithContext
+		block.StateSyncableVM
+	}
+	return fullVM{
+		adaptor.Convert(vm),
+		adaptor.ConvertStateSync(&syncer{}),
+	}, nil
+}
