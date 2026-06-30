ed5d64f845face76743bbf0cf13ac248ecc7fed9
maru <maru.newby@avalabs.org>
2026-06-30 02:32:54 +0100

    [ci] Minimize the testing performed on PR and pre-merge runs
    Previously PR and pre-merge runs ran unit tests against all supported platforms with
    non-cacheable `go test` options `-race` and `-shuffle`. Testing all platforms meant
    running more jobs that could flake and uncachable options meant each run had to
    re-test everything no matter what had changed.
    
    Instead:
    
    - As much as possible, limit PR runs to Bazel jobs running on ubuntu-24.04/amd64.
      - This provides essential coverage for minimal cost.
    
    - Limit pre-merge runs to Bazel and Go jobs running on ubuntu-24.04/amd64 (production
    target) and darwin/arm64 (developer target).
      - This insures against breaking master for developers
    
    - For both PR and pre-merge runs, ensure unit test runs are cachable by avoiding the
    use of the `-race` and `-shuffle` options.
      - This maximizes the cacheability of test runs for both Go and Bazel
    
    - Move jobs running with the race-shuffle configuration to scheduled jobs and run on
    each supported platform: ubuntu-24.04/{amd64,arm64}, ubuntu-22.04/{amd64,arm64}, macos-26.
      - This ensures coverage of these configurations without penalizing PR iteration
    
    Change-Id: I1e8b56d291870550d60f1645683c44836bae792b

--- graft/coreth/scripts/build_test.sh
+++ graft/coreth/scripts/build_test.sh
@@ -7,13 +7,18 @@ REPO_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd ../../../ && pwd )
 source "$REPO_ROOT"/scripts/constants.sh
 
 # We pass in the arguments to this script directly to enable easily passing parameters such as enabling race detection,
-# parallelism, and test coverage.
+# shuffling, parallelism, and test coverage.
 # DO NOT RUN tests from the top level "tests" directory since they are run by ginkgo
 race="-race"
 if [[ -n "${NO_RACE:-}" ]]; then
     race=""
 fi
 
+shuffle="on"
+if [[ -n "${NO_SHUFFLE:-}" ]]; then
+    shuffle="off"
+fi
+
 cd "$REPO_ROOT/graft/coreth"
 # shellcheck disable=SC2046
-go test -shuffle=on ${race:-} -timeout="${TIMEOUT:-900s}" -coverprofile=coverage.out -covermode=atomic "$@" $(go list .//... | grep -v github.com/ava-labs/avalanchego/graft/coreth/tests)
+go test -shuffle="$shuffle" ${race:-} -timeout="${TIMEOUT:-900s}" -coverprofile=coverage.out -covermode=atomic "$@" $(go list .//... | grep -v github.com/ava-labs/avalanchego/graft/coreth/tests)
