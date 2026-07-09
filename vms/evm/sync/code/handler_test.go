6260515fa2a91c3113c28887f8fac772179e71bc
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2026-07-09 12:53:26 +0300

    feat(vms/evm/sync/code): add verifying code syncer and RegisterHandler
    - Collapse handler API into a single `RegisterHandler` entry.
    - Unexport the responder plumbing.
    - Verify each blob against its hash, re-request on mismatch.
    - Integration-test the handler to client round trip.
    - Add `synctest.NewSelfNetwork` loopback helper.

--- vms/evm/sync/code/handler_test.go
+++ vms/evm/sync/code/handler_test.go
@@ -1,63 +1,29 @@
 // Copyright (C) 2019, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-package code_test
+package code
 
 import (
-	"crypto/rand"
 	"testing"
-	"time"
 
 	"github.com/ava-labs/libevm/common"
-	"github.com/ava-labs/libevm/core/rawdb"
-	"github.com/ava-labs/libevm/crypto"
 	"github.com/ava-labs/libevm/ethdb/memorydb"
-	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/testing/protocmp"
 
 	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/utils/logging"
-	"github.com/ava-labs/avalanchego/vms/evm/sync/code"
-	"github.com/ava-labs/avalanchego/vms/evm/sync/synctest"
 
 	syncpb "github.com/ava-labs/avalanchego/proto/pb/sync"
 )
-
-func TestHandler_RoundTrip(t *testing.T) {
-	wantResp := &syncpb.GetCodeResponse{
-		Data: [][]byte{{0xaa, 0xbb}, {0xcc, 0xdd}},
-	}
-	responder := &synctest.FakeCodeResponder{Resp: wantResp}
-	h := code.NewHandler(logging.NoLog{}, responder)
-
-	req := &syncpb.GetCodeRequest{
-		Hashes: [][]byte{{0x01}, {0x02}},
-	}
-	respBytes, appErr := h.AppRequest(t.Context(), ids.GenerateTestNodeID(), time.Time{}, synctest.MustMarshal(t, req))
-	require.Nil(t, appErr)
-
-	got := &syncpb.GetCodeResponse{}
-	require.NoError(t, proto.Unmarshal(respBytes, got))
-	require.Empty(t, cmp.Diff(wantResp, got, protocmp.Transform()))
-	require.Empty(t, cmp.Diff(req, responder.GotReq, protocmp.Transform()))
-}
 
 func TestResponder(t *testing.T) {
 	t.Parallel()
 
 	db := memorydb.New()
 	codeBytes := []byte("contract bytecode")
-	codeHash := crypto.Keccak256Hash(codeBytes)
-	rawdb.WriteCode(db, codeHash, codeBytes)
+	codeHash := writeCode(t, db, codeBytes)
 
-	// A second code blob to test multi-hash requests.
-	other := make([]byte, 256)
-	_, err := rand.Read(other)
-	require.NoError(t, err)
-	otherHash := crypto.Keccak256Hash(other)
-	rawdb.WriteCode(db, otherHash, other)
+	other := randomCode(t)
+	otherHash := writeCode(t, db, other)
 
 	tests := []struct {
 		name     string
@@ -96,7 +62,7 @@ func TestResponder(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			r := code.NewResponder(db)
+			r := newResponder(db)
 
 			rawHashes := make([][]byte, len(tt.hashes))
 			for i, h := range tt.hashes {
