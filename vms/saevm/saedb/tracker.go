33ff290d5d0f6c3c46b2e12be9dd48fd3bed877b
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-07-10 19:03:59 +0000

    chore: fix copilot bug

--- vms/saevm/saedb/tracker.go
+++ vms/saevm/saedb/tracker.go
@@ -234,6 +234,7 @@ func protectTrieIndex(db ethdb.KeyValueStore, c Config) error {
 	}
 	if !pruningDisabled {
 		return nil
+	}
 	if !c.AllowMissingTries {
 		return fmt.Errorf("%w (set allow-missing-tries=true to proceed)", ErrRefuseToCorruptArchiver)
 	}
