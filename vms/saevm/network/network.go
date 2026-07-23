bcc851822ddc795f2d9dd39eb91c99040c45c4c4
Austin Larson <78000745+alarso16@users.noreply.github.com>
2026-07-23 16:38:17 -0400

    refactor(sae): Separate network (#5616)

---
+++ vms/saevm/network/network.go
@@ -0,0 +1,65 @@
+// Copyright (C) 2019, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// Package network provides the P2P network for the SAE VM.
+package network
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ava-labs/avalanchego/api/metrics"
+	"github.com/ava-labs/avalanchego/network/p2p"
+	"github.com/ava-labs/avalanchego/snow"
+	"github.com/ava-labs/avalanchego/snow/engine/common"
+	"github.com/ava-labs/avalanchego/snow/validators"
+)
+
+var (
+	_ validators.Connector = (*Network)(nil)
+	_ common.AppHandler    = (*Network)(nil)
+)
+
+// Network contains the [p2p.Network] and all coupled state for use by the SAE
+// VM. It should only be constructed with [New].
+type Network struct {
+	*p2p.Network
+	ValidatorPeers *p2p.Validators
+	Peers          *p2p.Peers
+}
+
+// New creates the P2P network with a registered validator set.
+func New(
+	snowCtx *snow.Context,
+	sender common.AppSender,
+) (*Network, error) {
+	reg, err := metrics.MakeAndRegister(snowCtx.Metrics, "p2p")
+	if err != nil {
+		return nil, fmt.Errorf("registering metrics: %w", err)
+	}
+	peers := &p2p.Peers{}
+	const maxValidatorSetStaleness = time.Minute
+	validatorPeers := p2p.NewValidators(
+		snowCtx.Log,
+		snowCtx.SubnetID,
+		snowCtx.ValidatorState,
+		maxValidatorSetStaleness,
+	)
+	const namespace = "network"
+	network, err := p2p.NewNetwork(
+		snowCtx.Log,
+		sender,
+		reg,
+		namespace,
+		peers,
+		validatorPeers,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &Network{
+		Network:        network,
+		Peers:          peers,
+		ValidatorPeers: validatorPeers,
+	}, nil
+}
