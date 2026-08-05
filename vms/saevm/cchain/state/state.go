1b667e3ca9cd363be7eedca9e9a464b4952c7022
Austin Larson <78000745+alarso16@users.noreply.github.com>
2026-08-05 11:57:31 -0400

    feat(cchain/state): bonus blocks (#5641)
    Co-authored-by: Stephen Buttolph <stephen@avalabs.org>

--- vms/saevm/cchain/state/state.go
+++ vms/saevm/cchain/state/state.go
@@ -152,8 +152,24 @@ func (s *State) Apply(height uint64, txs []*tx.Tx) error {
 		return fmt.Errorf("applying trie on root %s: %w", s.currentRoot, err)
 	}
 
-	batch := s.db.NewBatch()
+	var (
+		isBonus = isBonusBlock(s.snowCtx.NetworkID, height)
+		batch   = s.db.NewBatch()
+	)
 	for _, t := range txs {
+		if isBonus {
+			// To provide consistent API behavior with databases made by Coreth,
+			// we report the first height a transaction was accepted at as the
+			// canonical height.
+			txID := t.ID()
+			has, err := s.db.Has(txKey(txID))
+			if err != nil {
+				return fmt.Errorf("checking for existing tx %s: %w", txID, err)
+			}
+			if has {
+				continue
+			}
+		}
 		if err := writeTx(batch, height, t); err != nil {
 			return fmt.Errorf("writing tx %s: %w", t.ID(), err)
 		}
@@ -164,11 +180,16 @@ func (s *State) Apply(height uint64, txs []*tx.Tx) error {
 	if err := database.PutID(batch, rootKey(height), ids.ID(newRoot)); err != nil {
 		return fmt.Errorf("writing root: %w", err)
 	}
+
+	// Applying the same operation multiple times to shared memory MAY result in
+	// an error. Since bonus blocks consume the same UTXOs multiple times, we
+	// MUST skip applying their operations to shared memory.
+	if isBonus {
+		ops = nil
+	}
+
 	// Committing the batch atomically with shared memory prevents duplicate
 	// shared memory operations in the event of a crash.
-	//
-	// TODO(StephenButtolph): Skip applying shared memory operations for bonus
-	// blocks.
 	if err := s.snowCtx.SharedMemory.Apply(ops, batch); err != nil {
 		return fmt.Errorf("applying shared memory: %w", err)
 	}
