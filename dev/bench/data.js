1b2e77977f4ebd3ec2d38b1f054be046145e491e
github-action-benchmark <github@users.noreply.github.com>
2026-08-11 16:01:56 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for ba28021d8c399e252c484bd5cff3a22593cb8843

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786455877027,
+  "lastUpdate": 1786464116020,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -45871,6 +45871,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-i4i-4xlarge-local-ssd - block_accept_ms/ggas",
             "value": 51.60180338649703,
+            "unit": "block_accept_ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Austin Larson",
+            "username": "alarso16",
+            "email": "78000745+alarso16@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "ba28021d8c399e252c484bd5cff3a22593cb8843",
+          "message": "fix(sae): Allow `BLOCKHASH` op code on recovery (#5772)\n\nCo-authored-by: Arran Schlosberg <519948+ARR4N@users.noreply.github.com>",
+          "timestamp": "2026-08-11T14:53:37Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/ba28021d8c399e252c484bd5cff3a22593cb8843"
+        },
+        "date": 1786464114316,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-16xlarge-local-ssd - mgas/s",
+            "value": 26.117968312319896,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-16xlarge-local-ssd - ms/ggas",
+            "value": 38287.81733869774,
+            "unit": "ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-16xlarge-local-ssd - block_parse_ms/ggas",
+            "value": 67.9637563628798,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-16xlarge-local-ssd - block_verify_ms/ggas",
+            "value": 37499.99095400389,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-16xlarge-local-ssd - block_accept_ms/ggas",
+            "value": 718.4078102190083,
             "unit": "block_accept_ms/ggas"
           }
         ]
