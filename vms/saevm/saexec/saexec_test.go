27333b7b9bf9a05cf14bcfa5467a2025388d4722
Arran Schlosberg <me@arranschlosberg.com>
2026-08-26 17:10:06 +0100

    test: precise assertion of interim execution time

--- vms/saevm/saexec/saexec_test.go
+++ vms/saevm/saexec/saexec_test.go
@@ -13,6 +13,7 @@ import (
 	"math/rand/v2"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/arr4n/shed/testerr"
 	"github.com/ava-labs/libevm/common"
@@ -36,6 +37,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 
+	"github.com/ava-labs/avalanchego/utils"
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/utils/logging/loggingtest"
 	"github.com/ava-labs/avalanchego/vms/components/gas"
@@ -532,56 +534,72 @@ func TestExecuteRejectsInvalidOptions(t *testing.T) {
 // TestExecuteRecordsOnlyCanonicalProgress verifies that non-canonical execution
 // does not overwrite an in-memory block's canonical progress.
 func TestExecuteRecordsOnlyCanonicalProgress(t *testing.T) {
+	makeTxs := func(t *testing.T, w *saetest.Wallet) types.Transactions {
+		t.Helper()
+		return types.Transactions{w.SetNonceAndSign(t, 0, &types.LegacyTx{
+			To:       &common.Address{},
+			Gas:      params.TxGas,
+			GasPrice: big.NewInt(1),
+		})}
+	}
+
 	tests := []struct {
-		name   string
-		withTx bool
-		ops    []saehookstest.Op
-		opts   []Option
-		want   bool
+		name            string
+		txs             func(*testing.T, *saetest.Wallet) types.Transactions
+		ops             []saehookstest.Op
+		opts            []Option
+		wantInterimTick *gas.Gas // nil implies nil interim execution time, not no tick
 	}{
 		{
-			name:   "non-canonical transaction and end-of-block operation",
-			withTx: true,
-			ops:    []saehookstest.Op{{Gas: 1}},
+			name:            "non-canonical with transaction and end-of-block operation",
+			txs:             makeTxs,
+			ops:             []saehookstest.Op{{Gas: 1}},
+			wantInterimTick: nil, // not canonical
 		},
 		{
-			name:   "canonical transaction",
-			withTx: true,
-			opts:   []Option{asCanonical()},
-			want:   true,
+			name:            "canonical with transaction only",
+			txs:             makeTxs,
+			ops:             nil,
+			opts:            []Option{asCanonical()},
+			wantInterimTick: utils.PointerTo(gas.Gas(params.TxGas)),
 		},
 		{
-			name: "canonical end-of-block operation",
-			ops:  []saehookstest.Op{{Gas: 1}},
-			opts: []Option{asCanonical()},
-			want: true,
+			name:            "canonical with end-of-block operation only",
+			txs:             nil,
+			ops:             []saehookstest.Op{{Gas: 1}},
+			opts:            []Option{asCanonical()},
+			wantInterimTick: utils.PointerTo[gas.Gas](1),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			_, sut := newSUT(t)
+
 			var txs types.Transactions
-			if tt.withTx {
-				txs = types.Transactions{sut.wallet.SetNonceAndSign(t, 0, &types.LegacyTx{
-					To:       &common.Address{},
-					Gas:      params.TxGas,
-					GasPrice: big.NewInt(1),
-				})}
+			if tt.txs != nil {
+				txs = tt.txs(t, sut.wallet)
 			}
-			b := sut.chain.NewBlock(
-				t,
-				txs,
-				blockstest.WithEthBlockOptions(blockstest.WithOps(tt.ops)),
-			)
+			withOps := blockstest.WithEthBlockOptions(blockstest.WithOps(tt.ops))
+			b := sut.chain.NewBlock(t, txs, withOps)
+
 			stateDB, err := sut.StateDB(b.ParentBlock().PostExecutionStateRoot())
 			require.NoError(t, err, "Executor.StateDB(parent root)")
 
 			_, err = Execute(b, stateDB, sut.hooks, sut.chainConfig, sut.chainContext, sut.logger, tt.opts...)
 			require.NoError(t, err, "Execute()")
 
-			interimExecutionTime := proxytime.New[gas.Gas](math.MaxUint64, 0, 1)
-			got := b.SwapInterimExecutionTime(interimExecutionTime) != nil
-			require.Equal(t, tt.want, got, "Block.SwapInterimExecutionTime()")
+			got := b.SwapInterimExecutionTime(proxytime.Of[gas.Gas](time.Time{}))
+			if tick := tt.wantInterimTick; tick == nil {
+				require.Nilf(t, got, "%T.SwapInterimExecutionTime(...)", b)
+			} else {
+				want := b.ParentBlock().ExecutedByGasTime()
+				want.BeforeBlock(b.PreciseTime())
+				want.Tick(*tick)
+
+				if diff := cmp.Diff(want.Time, got, proxytime.CmpOpt[gas.Gas]()); diff != "" {
+					t.Errorf("%T.SwapInterimExecutionTime(...); diff (-want +got):\n%s", b, diff)
+				}
+			}
 		})
 	}
 }
