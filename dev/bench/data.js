e434708f8eee0aa6934ce8c68b538e42ed0f096f
github-action-benchmark <github@users.noreply.github.com>
2026-09-04 10:32:28 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 04f69d20976c7fcd23c9cea9be9fe08d0325b9d0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788460026059,
+  "lastUpdate": 1788517948190,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -52357,6 +52357,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast - block_accept_ms/ggas",
             "value": 57.92047815362381,
+            "unit": "block_accept_ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Owen",
+            "username": "owenwahlgren",
+            "email": "owenwahlgren@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "04f69d20976c7fcd23c9cea9be9fe08d0325b9d0",
+          "message": "fix(subnet-evm/params): use semantic equality for state upgrade compat check (#5765)",
+          "timestamp": "2026-09-04T00:25:03Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/04f69d20976c7fcd23c9cea9be9fe08d0325b9d0"
+        },
+        "date": 1788517946160,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - mgas/s",
+            "value": 81.9217245885112,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - ms/ggas",
+            "value": 12206.77427169595,
+            "unit": "ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - block_parse_ms/ggas",
+            "value": 71.13421837217241,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - block_verify_ms/ggas",
+            "value": 11502.533364413495,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - block_accept_ms/ggas",
+            "value": 630.8002696124386,
             "unit": "block_accept_ms/ggas"
           }
         ]
