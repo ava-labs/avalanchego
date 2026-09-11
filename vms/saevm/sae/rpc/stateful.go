00eb537b5cfa1c66ebf76fd60f18d539105b9efb
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-09-11 18:59:02 +0000

    feat(rpc): bound concurrent historical state re-execution

--- vms/saevm/sae/rpc/stateful.go
+++ vms/saevm/sae/rpc/stateful.go
@@ -125,6 +125,15 @@ func (b *backend) stateAtBlock(ctx context.Context, num uint64) (*state.StateDB,
 		return nil, nil, err
 	}
 
+	if len(toReexec) > 0 {
+		select {
+		case b.replaySlots <- struct{}{}:
+			defer func() { <-b.replaySlots }()
+		case <-ctx.Done():
+			return nil, nil, context.Cause(ctx)
+		}
+	}
+
 	for _, stored := range toReexec {
 		if ctx.Err() != nil {
 			return nil, nil, context.Cause(ctx)
