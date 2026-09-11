c9d0265a1792f37f20744a44b715de698e4cb982
razvan.angheluta <razvan@angheluta.dev>
2026-09-11 15:20:01 +0300

    rollback unexporting of getRewardsCalculator

--- vms/platformvm/txs/executor/standard_tx_executor_test.go
+++ vms/platformvm/txs/executor/standard_tx_executor_test.go
@@ -4479,7 +4479,7 @@ func TestStandardExecutorAddAutoRenewedValidatorTx(t *testing.T) {
 	currentSupply, err := env.state.GetCurrentSupply(constants.PrimaryNetworkID)
 	require.NoError(t, err)
 
-	rewards, err := getRewardsCalculator(
+	rewards, err := GetRewardsCalculator(
 		env.config.RewardConfig,
 		env.config.UpgradeConfig,
 		env.state,
