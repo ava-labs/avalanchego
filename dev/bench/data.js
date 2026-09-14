253d3887b7bf081dedff8811f7e1b325f83578c4
github-action-benchmark <github@users.noreply.github.com>
2026-09-14 10:33:13 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for e9a8b75024717aeec0a6f8297f6d8a0069975cc9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789324991340,
+  "lastUpdate": 1789381993687,
   "repoUrl": "https://github.com/ava-labs/avalanchego",
   "entries": {
     "Benchmark": [
@@ -55177,6 +55177,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[33000001,33500000]-Config-default-Runner-avago-runner-m6i-4xlarge-ebs-fast - block_accept_ms/ggas",
             "value": 57.08823101421701,
+            "unit": "block_accept_ms/ggas"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Radeonares32",
+            "username": "Radeonares32",
+            "email": "56337258+Radeonares32@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "e9a8b75024717aeec0a6f8297f6d8a0069975cc9",
+          "message": "feat(sae): executor health feeds `VM.HealthCheck()` (#5934)\n\nCo-authored-by: Arran Schlosberg <me@arranschlosberg.com>",
+          "timestamp": "2026-09-09T19:55:09Z",
+          "url": "https://github.com/ava-labs/avalanchego/commit/e9a8b75024717aeec0a6f8297f6d8a0069975cc9"
+        },
+        "date": 1789381991506,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - mgas/s",
+            "value": 82.56748759751397,
+            "unit": "mgas/s"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - ms/ggas",
+            "value": 12111.30469264889,
+            "unit": "ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - block_parse_ms/ggas",
+            "value": 72.21940985495127,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - block_verify_ms/ggas",
+            "value": 11395.6256764317,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[69000001,69100000]-Config-archive-Runner-avago-runner-i4i-4xlarge-local-ssd - block_accept_ms/ggas",
+            "value": 641.2375653679145,
             "unit": "block_accept_ms/ggas"
           }
         ]
