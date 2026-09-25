5322e6210b1a8a8265cd478e42e61d34626aa5b0
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2026-09-25 23:50:53 +0000

    feat(subnet-evm): encode the SAE settled block marker in headers

--- graft/subnet-evm/plugin/evm/customtypes/gen_header_serializable_rlp.go
+++ graft/subnet-evm/plugin/evm/customtypes/gen_header_serializable_rlp.go
@@ -44,7 +44,11 @@ func (obj *HeaderSerializable) EncodeRLP(_w io.Writer) error {
 	_tmp5 := obj.ParentBeaconRoot != nil
 	_tmp6 := obj.TimeMilliseconds != nil
 	_tmp7 := obj.MinDelayExcess != nil
-	if _tmp1 || _tmp2 || _tmp3 || _tmp4 || _tmp5 || _tmp6 || _tmp7 {
+	_tmp8 := obj.SettledHeight != nil
+	_tmp9 := obj.SettledGasUnix != nil
+	_tmp10 := obj.SettledGasNumerator != nil
+	_tmp11 := obj.SettledExcess != nil
+	if _tmp1 || _tmp2 || _tmp3 || _tmp4 || _tmp5 || _tmp6 || _tmp7 || _tmp8 || _tmp9 || _tmp10 || _tmp11 {
 		if obj.BaseFee == nil {
 			w.Write(rlp.EmptyString)
 		} else {
@@ -54,7 +58,7 @@ func (obj *HeaderSerializable) EncodeRLP(_w io.Writer) error {
 			w.WriteBigInt(obj.BaseFee)
 		}
 	}
-	if _tmp2 || _tmp3 || _tmp4 || _tmp5 || _tmp6 || _tmp7 {
+	if _tmp2 || _tmp3 || _tmp4 || _tmp5 || _tmp6 || _tmp7 || _tmp8 || _tmp9 || _tmp10 || _tmp11 {
 		if obj.BlockGasCost == nil {
 			w.Write(rlp.EmptyString)
 		} else {
@@ -64,39 +68,67 @@ func (obj *HeaderSerializable) EncodeRLP(_w io.Writer) error {
 			w.WriteBigInt(obj.BlockGasCost)
 		}
 	}
-	if _tmp3 || _tmp4 || _tmp5 || _tmp6 || _tmp7 {
+	if _tmp3 || _tmp4 || _tmp5 || _tmp6 || _tmp7 || _tmp8 || _tmp9 || _tmp10 || _tmp11 {
 		if obj.BlobGasUsed == nil {
 			w.Write([]byte{0x80})
 		} else {
 			w.WriteUint64((*obj.BlobGasUsed))
 		}
 	}
-	if _tmp4 || _tmp5 || _tmp6 || _tmp7 {
+	if _tmp4 || _tmp5 || _tmp6 || _tmp7 || _tmp8 || _tmp9 || _tmp10 || _tmp11 {
 		if obj.ExcessBlobGas == nil {
 			w.Write([]byte{0x80})
 		} else {
 			w.WriteUint64((*obj.ExcessBlobGas))
 		}
 	}
-	if _tmp5 || _tmp6 || _tmp7 {
+	if _tmp5 || _tmp6 || _tmp7 || _tmp8 || _tmp9 || _tmp10 || _tmp11 {
 		if obj.ParentBeaconRoot == nil {
 			w.Write([]byte{0x80})
 		} else {
 			w.WriteBytes(obj.ParentBeaconRoot[:])
 		}
 	}
-	if _tmp6 || _tmp7 {
+	if _tmp6 || _tmp7 || _tmp8 || _tmp9 || _tmp10 || _tmp11 {
 		if obj.TimeMilliseconds == nil {
 			w.Write([]byte{0x80})
 		} else {
 			w.WriteUint64((*obj.TimeMilliseconds))
 		}
 	}
-	if _tmp7 {
+	if _tmp7 || _tmp8 || _tmp9 || _tmp10 || _tmp11 {
 		if obj.MinDelayExcess == nil {
 			w.Write([]byte{0x80})
 		} else {
 			w.WriteUint64((*obj.MinDelayExcess))
+		}
+	}
+	if _tmp8 || _tmp9 || _tmp10 || _tmp11 {
+		if obj.SettledHeight == nil {
+			w.Write([]byte{0x80})
+		} else {
+			w.WriteUint64((*obj.SettledHeight))
+		}
+	}
+	if _tmp9 || _tmp10 || _tmp11 {
+		if obj.SettledGasUnix == nil {
+			w.Write([]byte{0x80})
+		} else {
+			w.WriteUint64((*obj.SettledGasUnix))
+		}
+	}
+	if _tmp10 || _tmp11 {
+		if obj.SettledGasNumerator == nil {
+			w.Write([]byte{0x80})
+		} else {
+			w.WriteUint64((*obj.SettledGasNumerator))
+		}
+	}
+	if _tmp11 {
+		if obj.SettledExcess == nil {
+			w.Write([]byte{0x80})
+		} else {
+			w.WriteUint64((*obj.SettledExcess))
 		}
 	}
 	w.ListEnd(_tmp0)
